a51ed80b02e719deba8010c0a270030ed4814fbe
Le Vivilet <levivlet@pm.me>
2022-06-18 17:57:43 +0200

    fix syntax highlighting in static build

--- packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostSemanticTokens.js
+++ packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostSemanticTokens.js
@@ -1,7 +1,11 @@
 import * as ExtensionHost from './ExtensionHostCore.js'
 import * as ExtensionHostManagement from './ExtensionHostManagement.js'
+import * as Platform from '../Platform/Platform.js'
 
 export const executeSemanticTokenProvider = async (editor) => {
+  if (Platform.getPlatform() === 'web') {
+    return []
+  }
   await ExtensionHostManagement.activateByEvent(
     `onSemanticTokens:${editor.languageId}`
   )
