60a753a1768ca6c03611adce5cf886e11eca6c93
Le Vivilet <levivlet@pm.me>
2022-06-19 22:24:13 +0200

    add script to update dependencies and update dependencies

---
+++ scripts/update-dependencies.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname "$0")
+cd ..
+
+command_exists(){
+  command -v "$1" &> /dev/null
+}
+
+if ! command_exists "ncu"; then
+    echo "npm-check-updates is not installed"
+    npm i -g npm-check-updates
+else
+    echo "ncu is installed"
+fi
+
+function updateDependencies {
+  echo "updating dependencies..."
+  OUTPUT=`ncu -u`
+  SUB='All dependencies match the latest package versions'
+  if [[ "$OUTPUT" == *"$SUB"* ]]; then
+    echo "$OUTPUT"
+  else
+    rm -rf node_modules package-lock.json dist
+    npm install
+  fi
+}
+
+                                                       updateDependencies             &&
+cd packages/extension-host                          && updateDependencies && cd ../.. &&
+cd packages/main-process                            && updateDependencies && cd ../.. &&
+cd packages/pty-host                                && updateDependencies && cd ../.. &&
+cd packages/renderer-process                        && updateDependencies && cd ../.. &&
+cd packages/renderer-worker                         && updateDependencies && cd ../.. &&
+cd packages/shared-process                          && updateDependencies && cd ../.. &&
+cd packages/web                                     && updateDependencies && cd ../.. &&
+
+cd build                                            && updateDependencies && cd ..    &&
+
+echo "Great Success!"
+
+sleep 2
