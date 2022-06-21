c21a79637337259c00419d31bc0df5dc0b78ad21
Le Vivilet <levivlet@pm.me>
2022-06-21 15:57:29 +0200

    add vscode icons extension

--- build/src/parts/DownloadBuiltinExtensions/DownloadBuiltinExtensions.js
+++ build/src/parts/DownloadBuiltinExtensions/DownloadBuiltinExtensions.js
@@ -103,6 +103,10 @@ const main = () => {
       name: 'builtin.language-basics-elixir',
       path: 'https://github.com/lvce-editor/language-basics-elixir/releases/download/v0.0.1/language-basics-elixir-v0.0.1.tar.br',
     },
+    {
+      name: 'builtin.vscode-icons',
+      path: 'https://github.com/lvce-editor/vscode-icons/releases/download/v0.0.1/vscode-icons-v0.0.1.tar.br',
+    },
   ]
   downloadExtensions(extensions)
 }
