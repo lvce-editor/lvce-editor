387100759ced714e3a26c1f64c03321998a55e19
Le Vivilet <levivlet@pm.me>
2022-06-23 15:34:57 +0200

    fix path to server binary, make git tag overrideable during build with environment variable

--- build/src/parts/Tag/Tag.js
+++ build/src/parts/Tag/Tag.js
@@ -2,6 +2,12 @@ import * as Exec from '../Exec/Exec.js'
 
 export const getGitTag = async () => {
   try {
+    if (process.env.GIT_TAG) {
+      if (process.env.GIT_TAG.startsWith('v')) {
+        return process.env.GIT_TAG.slice(1)
+      }
+      return process.env.GIT_TAG
+    }
     const { stdout } = await Exec.exec('git', [
       'describe',
       '--exact-match',
