ef995ce539f2858a26b0cc5c1119b64b447322d1
Le Vivilet <levivlet@pm.me>
2022-06-23 17:50:55 +0200

    try to fix build variables

--- build/src/parts/Tag/Tag.js
+++ build/src/parts/Tag/Tag.js
@@ -2,6 +2,12 @@ import * as Exec from '../Exec/Exec.js'
 
 export const getGitTag = async () => {
   try {
+    if (process.env.RG_VERSION) {
+      if (process.env.RG_VERSION.startsWith('v')) {
+        return process.env.RG_VERSION.slice(1)
+      }
+      return process.env.RG_VERSION
+    }
     if (process.env.GIT_TAG) {
       if (process.env.GIT_TAG.startsWith('v')) {
         return process.env.GIT_TAG.slice(1)
