4bef33183e4c93765d65db971c782c94dd828bad
Le Vivilet <levivlet@pm.me>
2022-06-25 09:18:43 +0200

    fix: native open folder command

--- packages/shared-process/src/parts/Native/Native.ipc.js
+++ packages/shared-process/src/parts/Native/Native.ipc.js
@@ -2,5 +2,5 @@ import * as Command from '../Command/Command.js'
 import * as Native from './Native.js'
 
 export const __initialize__ = () => {
-  Command.register(221, Native.openFolder)
+  Command.register('Native.openFolder', Native.openFolder)
 }
