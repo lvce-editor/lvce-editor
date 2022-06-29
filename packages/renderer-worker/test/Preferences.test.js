edf08a798cd3b500853a24554dabb093f2e2c8e6
Le Vivilet <levivlet@pm.me>
2022-06-29 21:54:58 +0200

    fix preferences tests

--- packages/renderer-worker/test/Preferences.test.js
+++ packages/renderer-worker/test/Preferences.test.js
@@ -1,75 +1,76 @@
 import { jest } from '@jest/globals'
-import * as RendererProcess from '../src/parts/RendererProcess/RendererProcess.js'
-import * as SharedProcess from '../src/parts/SharedProcess/SharedProcess.js'
-
-const Main = {
-  openUri: jest.fn(),
-}
 
 beforeEach(() => {
+  jest.resetAllMocks()
   for (const key in Preferences.state) {
     delete Preferences.state[key]
   }
 })
 
-jest.unstable_mockModule('../src/parts/Viewlet/ViewletMain.js', () => {
+jest.unstable_mockModule(
+  '../src/parts/RendererProcess/RendererProcess.js',
+  () => {
+    return {
+      invoke: jest.fn(() => {
+        throw new Error('not implemented')
+      }),
+    }
+  }
+)
+jest.unstable_mockModule('../src/parts/SharedProcess/SharedProcess.js', () => {
   return {
-    openUri(uri) {
-      Main.openUri(uri)
-    },
+    invoke: jest.fn(() => {
+      throw new Error('not implemented')
+    }),
   }
 })
 
+const RendererProcess = await import(
+  '../src/parts/RendererProcess/RendererProcess.js'
+)
+const SharedProcess = await import(
+  '../src/parts/SharedProcess/SharedProcess.js'
+)
+
 const Preferences = await import('../src/parts/Preferences/Preferences.js')
 
+const Main = await import('../src/parts/Viewlet/ViewletMain.js')
+
 test.skip('openSettingsJson', async () => {
-  RendererProcess.state.send = jest.fn()
-  Main.openUri = jest.fn()
+  // @ts-ignore
+  RendererProcess.invoke.mockImplementation(() => {})
+  // @ts-ignore
+  Main.openUri.mockImplementation(() => {})
   await Preferences.openSettingsJson()
   expect(Main.openUri).toHaveBeenCalledTimes(1)
   expect(Main.openUri).toHaveBeenCalledWith('app://settings.json')
 })
 
 test.skip('openKeyBindingsJson', async () => {
-  RendererProcess.state.send = jest.fn()
-  Main.openUri = jest.fn()
+  // @ts-ignore
+  RendererProcess.invoke.mockImplementation(() => {})
+  // @ts-ignore
+  Main.openUri.mockImplementation(() => {})
   await Preferences.openKeyBindingsJson()
   expect(Main.openUri).toHaveBeenCalledTimes(1)
   expect(Main.openUri).toHaveBeenCalledWith('app://keyBindings.json')
 })
 
 test('hydrate', async () => {
-  SharedProcess.state.send = jest.fn((message) => {
-    switch (message.method) {
+  // @ts-ignore
+  SharedProcess.invoke.mockImplementation((method, ...params) => {
+    switch (method) {
       case 'Preferences.getAll':
-        SharedProcess.state.receive({
-          jsonrpc: '2.0',
-          id: message.id,
-          result: {
-            'editor.fontSize': 14,
-            'editor.fontFamily': "'Fira Code'",
-          },
-        })
-        break
-      default:
-        console.log(message)
-        throw new Error('unexpected message')
-    }
-  })
-  RendererProcess.state.send = jest.fn((message) => {
-    switch (message[0]) {
-      case 909090:
-        const callbackId = message[1]
-        RendererProcess.state.handleMessage([
-          /* Callback.resolve */ 67330,
-          /* callbackId */ callbackId,
-          /* result */ undefined,
-        ])
-        break
+        return {
+          'editor.fontSize': 14,
+          'editor.fontFamily': "'Fira Code'",
+        }
       default:
         throw new Error('unexpected message')
     }
   })
+  // @ts-ignore
+  RendererProcess.invoke.mockImplementation(() => {})
   await Preferences.hydrate()
   expect(Preferences.state).toEqual({
     'editor.fontSize': 14,
@@ -78,20 +79,12 @@ test('hydrate', async () => {
 })
 
 test('hydrate - error', async () => {
-  SharedProcess.state.send = jest.fn((message) => {
-    switch (message.method) {
+  // @ts-ignore
+  SharedProcess.invoke.mockImplementation((method, ...params) => {
+    switch (method) {
       case 'Preferences.getAll':
-        SharedProcess.state.receive({
-          jsonrpc: '2.0',
-          id: message.id,
-          error: {
-            message: 'x is not a function',
-            data: 'x is not a function',
-          },
-        })
-        break
+        throw new TypeError('x is not a function')
       default:
-        console.log(message)
         throw new Error('unexpected message')
     }
   })
@@ -107,95 +100,58 @@ test('get', () => {
 })
 
 test('set', async () => {
-  SharedProcess.state.send = jest.fn((message) => {
-    switch (message.method) {
+  // @ts-ignore
+  SharedProcess.invoke.mockImplementation((method, ...params) => {
+    switch (method) {
       case 'Platform.getUserSettingsPath':
-        SharedProcess.state.receive({
-          jsonrpc: '2.0',
-          id: message.id,
-          result: '',
-        })
-        break
+        return ''
       case 'FileSystem.writeFile':
-        SharedProcess.state.receive({
-          jsonrpc: '2.0',
-          id: message.id,
-          result: null,
-        })
-        break
+        return null
       default:
-        console.log(message)
         throw new Error('unexpected message')
     }
   })
-
   await Preferences.set('x', 42)
-  expect(SharedProcess.state.send).toHaveBeenCalledTimes(2)
-  expect(SharedProcess.state.send).toHaveBeenNthCalledWith(1, {
-    id: expect.any(Number),
-    jsonrpc: '2.0',
-    method: 'Platform.getUserSettingsPath',
-    params: [],
-  })
-  expect(SharedProcess.state.send).toHaveBeenNthCalledWith(2, {
-    id: expect.any(Number),
-    jsonrpc: '2.0',
-    method: 'FileSystem.writeFile',
-    params: [
-      '',
-      `{
+  expect(SharedProcess.invoke).toHaveBeenCalledTimes(2)
+  expect(SharedProcess.invoke).toHaveBeenNthCalledWith(
+    1,
+    'Platform.getUserSettingsPath'
+  )
+  expect(SharedProcess.invoke).toHaveBeenNthCalledWith(
+    2,
+    'FileSystem.writeFile',
+    '',
+    `{
   \"x\": 42
 }
-`,
-    ],
-  })
+`
+  )
 })
 
 test('set - error - getUserSettingsPath', async () => {
-  SharedProcess.state.send = jest.fn((message) => {
-    switch (message.method) {
+  // @ts-ignore
+  SharedProcess.invoke.mockImplementation((method, ...params) => {
+    switch (method) {
       case 'Platform.getUserSettingsPath':
-        SharedProcess.state.receive({
-          jsonrpc: '2.0',
-          id: message.id,
-          error: {
-            message: 'x is not a function',
-            data: 'x is not a function',
-          },
-        })
-        break
+        throw new TypeError('x is not a function')
       default:
-        console.log(message)
         throw new Error('unexpected message')
     }
   })
   await expect(Preferences.set('x', 42)).rejects.toThrowError(
-    new Error('x is not a function')
+    new TypeError('x is not a function')
   )
 })
 
 test('set - error - writeFile', async () => {
-  SharedProcess.state.send = jest.fn((message) => {
-    switch (message.method) {
+  // @ts-ignore
+  SharedProcess.invoke.mockImplementation((method, ...params) => {
+    switch (method) {
       case 'Platform.getUserSettingsPath':
-        SharedProcess.state.receive({
-          jsonrpc: '2.0',
-          id: message.id,
-          result: '',
-        })
-        break
+        return ''
       case 'FileSystem.writeFile':
-        SharedProcess.state.receive({
-          jsonrpc: '2.0',
-          id: message.id,
-          error: {
-            message: 'x is not a function',
-            data: 'x is not a function',
-          },
-        })
-        break
+        throw new TypeError('x is not a function')
       default:
-        console.log(message)
         throw new Error('unexpected message')
     }
   })
