834e8ec1319c7729b679ec88bf02294c3c7edd95
Le Vivilet <levivlet@pm.me>
2022-07-04 16:59:45 +0200

    fix e2e tests

--- packages/renderer-process/src/parts/WebWorker/WebWorkerWithMessagePort.js
+++ packages/renderer-process/src/parts/WebWorker/WebWorkerWithMessagePort.js
@@ -1,10 +1,8 @@
 export const create = async (url) => {
-  console.log({ url })
-  await import(url)
-  // const port = await new Promise((resolve) => {
-  //   globalThis.acceptPort = resolve
-  //   import(url)
-  // })
-  // delete globalThis.acceptPort
-  // return port
+  const port = await new Promise((resolve) => {
+    globalThis.acceptPort = resolve
+    import(url)
+  })
+  delete globalThis.acceptPort
+  return port
 }
