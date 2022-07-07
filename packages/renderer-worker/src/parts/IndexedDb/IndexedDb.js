e37ea7b01a05aba2ea429e6377545f4c6ab7cc13
Le Vivilet <levivlet@pm.me>
2022-07-07 18:56:08 +0200

    cache indexeddb database for improved performance

--- packages/renderer-worker/src/parts/IndexedDb/IndexedDb.js
+++ packages/renderer-worker/src/parts/IndexedDb/IndexedDb.js
@@ -1,27 +1,37 @@
-import {
-  openDB,
-  deleteDB,
-  wrap,
-  unwrap,
-} from '../../../../../static/js/idb/index.js'
+import { openDB } from '../../../../../static/js/idb/index.js'
 
 export const state = {
   databases: Object.create(null),
   eventId: 0,
   dbVersion: 1,
+  /**
+   * @type {any}
+   */
+  cachedDb: undefined,
+}
+
+const getDb = async () => {
+  // @ts-ignore
+  const db = await openDB('session', state.dbVersion, {
+    async upgrade(db, oldVersion) {
+      if (!db.objectStoreNames.contains('session')) {
+        await db.createObjectStore('session', { autoIncrement: true })
+      }
+    },
+  })
+  return db
+}
+
+const getDbMemoized = async () => {
+  if (!state.cachedDb) {
+    state.cachedDb = await getDb()
+  }
+  return state.cachedDb
 }
 
 export const saveValue = async (storeId, value) => {
   try {
-    // @ts-ignore
-    const db = await openDB('session', state.dbVersion, {
-      async upgrade(db, oldVersion) {
-        if (!db.objectStoreNames.contains('session')) {
-          await db.createObjectStore('session', { autoIncrement: true })
-        }
-      },
-    })
-    const eventId = state.eventId++
+    const db = await getDbMemoized()
     await db.add('session', value)
   } catch (error) {
     throw new Error('Failed to save value to indexed db', {
@@ -33,14 +43,7 @@ export const saveValue = async (storeId, value) => {
 
 export const getValues = async (storeId) => {
   try {
-    console.log({ storeId })
-    // @ts-ignore
-    const db = await openDB('session', state.dbVersion, {
-      async upgrade(db) {
-        await db.createObjectStore(storeId)
-      },
-    })
-
+    const db = await getDbMemoized()
     const tx = db.transaction(storeId, 'readwrite')
     const [objects] = await Promise.all([tx.store.getAll(), tx.done])
     console.log({ objects })
