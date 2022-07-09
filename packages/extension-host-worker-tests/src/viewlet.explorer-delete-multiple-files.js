c4590ee527e6becc5e048e9aba3b66b71d05caf6
Le Vivilet <levivlet@pm.me>
2022-07-09 19:32:12 +0200

    add explorer delete multiple files tests, add assertions for invalid arguments

---
+++ packages/extension-host-worker-tests/src/viewlet.explorer-delete-multiple-files.js
@@ -0,0 +1,40 @@
+import {
+  expect,
+  getTmpDir,
+  runWithExtension,
+  test,
+  writeFile,
+} from './_testFrameWork.js'
+
+test('viewlet.explorer-delete-multiple-files', async () => {
+  const tmpDir = await getTmpDir()
+  await writeFile(`${tmpDir}/file1.txt`, 'content 1')
+  await writeFile(`${tmpDir}/file2.txt`, 'content 2')
+  await writeFile(`${tmpDir}/file3.txt`, 'content 3')
+  const page = await runWithExtension({
+    folder: tmpDir,
+    name: '',
+  })
+  const explorer = page.locator('.Viewlet[data-viewlet-id="Explorer"]')
+  const file1 = explorer.locator('text=file1.txt')
+  const file2 = explorer.locator('text=file2.txt')
+  const file3 = explorer.locator('text=file3.txt')
+
+  await explorer.click()
+  await expect(explorer).toHaveClass('FocusOutline')
+
+  await page.keyboard.press('ArrowUp')
+  await expect(file3).toHaveClass('FocusOutline')
+
+  await page.keyboard.press('Delete')
+  await expect(file3).toBeHidden()
+  await expect(file2).toHaveClass('FocusOutline')
+
+  await page.keyboard.press('Delete')
+  await expect(file2).toBeHidden()
+  await expect(file1).toHaveClass('FocusOutline')
+
+  await page.keyboard.press('Delete')
+  await expect(file1).toBeHidden()
+  await expect(explorer).toHaveClass('FocusOutline')
+})
