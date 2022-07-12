b662f700349810e12b2f5533c2e5f309ffd447f4
Le Vivilet <levivlet@pm.me>
2022-07-12 18:30:31 +0200

    refactor FileSystemApp module

--- packages/renderer-worker/src/parts/FileSystem/FileSystemApp.js
+++ packages/renderer-worker/src/parts/FileSystem/FileSystemApp.js
@@ -1,109 +1,28 @@
-import * as Command from '../Command/Command.js'
-import * as Platform from '../Platform/Platform.js'
-import * as FileSystemDisk from './FileSystemDisk.js'
-import * as Workspace from '../Workspace/Workspace.js'
-import * as Assert from '../Assert/Assert.js'
-import * as LocalStorage from '../LocalStorage/LocalStorage.js'
-
 export const name = 'App'
 
-const readFileInternal = async (getPath) => {
-  const path = await getPath()
-  Assert.string(path)
-  if (Platform.getPlatform() === 'web') {
-    const settingsJsonContent = await LocalStorage.getText(path)
-    if (settingsJsonContent) {
-      return settingsJsonContent
-    }
-    const assetDir = Platform.getAssetDir()
-    const url = `${assetDir}/config/defaultSettings.json`
-    return Command.execute(/* Ajax.getText */ 'Ajax.getText', /* url */ url)
-  }
-  // TODO handle enoent and other errors gracefully
-  const userSettingsContent = await FileSystemDisk.readFile(path)
-  return userSettingsContent
-}
-
-// TODO ask preferences module to do this
-const readFileSettingsJson = async () => {
-  return readFileInternal(Platform.getUserSettingsPath)
-}
-
-const readFileRecentlyOpenedJson = async () => {
-  return readFileInternal(Platform.getRecentlyOpenedPath)
-}
-
-export const readFile = (path) => {
-  switch (path) {
+const getModule = (uri) => {
+  switch (uri) {
     case 'app://startup-performance':
-      return Command.execute(/* Developer.getStartupPerformanceContent */ 820)
+      return import('./FileSystemAppStartupPerformance.js')
     case 'app://memory-usage':
-      return Command.execute(/* Developer.getMemoryUsageContent */ 821)
+      return import('./FileSystemAppMemoryUsage.js')
     case 'app://settings.json':
-      return readFileSettingsJson()
+      return import('./FileSystemAppSettings.js')
     case 'app://recently-opened.json':
-      return readFileRecentlyOpenedJson()
+      return import('./FileSystemAppRecentlyOpened.js')
     default:
-      return ''
+      throw new Error(`unsupported file: ${uri}`)
   }
 }
 
-const writeFileInternal = async (getPath, content) => {
-  const path = await getPath()
-  Assert.string(path)
-  if (Platform.getPlatform() === 'web') {
-    await Command.execute(
-      /* LocalStorage.setText */ 6904,
-      /* key */ path,
-      /* value */ content
-    )
-    return
-  }
-  // TODO handle enoent and other errors gracefully
-  try {
-    await FileSystemDisk.writeFile(path, content)
-  } catch (error) {
-    // TODO error should just have enoent code that could be checked
-
-    // @ts-ignore
-    if (error.message.includes('ENOENT')) {
-      try {
-        const dirname = Workspace.pathDirName(path)
-        await FileSystemDisk.mkdir(dirname)
-        await FileSystemDisk.writeFile(path, content)
-      } catch (error) {
-        // @ts-ignore
-        error.message = `Failed to write ${path}: ${error.message}`
-        throw error
-      }
-    } else {
-      // @ts-ignore
-      error.message = `Failed to write ${path}: ${error.message}`
-      throw error
-    }
-  }
+export const readFile = async (uri) => {
+  const module = await getModule(uri)
+  return module.readFile()
 }
 
-const writeFileSettingsJson = (content) => {
-  return writeFileInternal(Platform.getUserSettingsPath, content)
-}
-
-const writeFileRecentlyOpenedJson = (content) => {
-  return writeFileInternal(Platform.getRecentlyOpenedPath, content)
-}
-
-export const writeFile = async (path, content) => {
-  switch (path) {
-    case 'app://settings.json':
-      // TODO in web write to local storage, in electron write to config file
-      return writeFileSettingsJson(content)
-    case 'app://startup-performance':
-      break
-    case 'app://recently-opened.json':
-      return writeFileRecentlyOpenedJson(content)
-    default:
-      break
-  }
+export const writeFile = async (uri, content) => {
+  const module = await getModule(uri)
+  return module.writeFile(content)
 }
 
 export const readDirWithFileTypes = () => {
