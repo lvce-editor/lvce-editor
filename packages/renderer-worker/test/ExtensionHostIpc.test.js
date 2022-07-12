a64bdf24d746379a9c945ec3c0b9f2c6be9cea54
Le Vivilet <levivlet@pm.me>
2022-07-12 21:54:47 +0200

    fix failing tests

--- packages/renderer-worker/test/ExtensionHostIpc.test.js
+++ packages/renderer-worker/test/ExtensionHostIpc.test.js
@@ -1,7 +1,7 @@
 import * as ExtensionHostIpc from '../src/parts/ExtensionHostIpc/ExtensionHostIpc.js'
 
-test('listen - error - unexpected extension host type', () => {
-  expect(() => ExtensionHostIpc.listen(123)).toThrowError(
+test('listen - error - unexpected extension host type', async () => {
+  await expect(ExtensionHostIpc.listen(123)).rejects.toThrowError(
     new Error('unexpected extension host type: 123')
   )
 })
