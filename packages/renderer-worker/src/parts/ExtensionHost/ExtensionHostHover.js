c7af616f1f214d2cedab758c82b8f79937440c82
Le Vivilet <levivlet@pm.me>
2022-07-12 16:37:52 +0200

    refactor hover provider to allow multiple providers

--- packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostHover.js
+++ packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostHover.js
@@ -1,12 +1,16 @@
 import * as ExtensionHostManagement from './ExtensionHostManagement.js'
+import * as ExtensionHostShared from './ExtensionHostShared.js'
+
+const combineResults = (results) => {
+  return results[0]
+}
 
 export const executeHoverProvider = async (editor, offset) => {
-  const extensionHost = await ExtensionHostManagement.activateByEvent(
-    `onHover:${editor.languageId}`
-  )
-  return extensionHost.invoke(
-    /* ExtensionHostHover.execute */ 'ExtensionHostHover.execute',
-    /* documentId */ editor.id,
-    /* offset */ offset
-  )
+  return ExtensionHostShared.executeProviders({
+    event: `onHover:${editor.languageId}`,
+    method: 'ExtensionHostHover.execute',
+    params: [editor.id, offset],
+    noProviderFoundMessage: 'No hover provider found',
+    combineResults,
+  })
 }
