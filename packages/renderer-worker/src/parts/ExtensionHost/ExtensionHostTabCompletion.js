14412ae836ec34ec3058d3271d3091fff4d6ab97
Le Vivilet <levivlet@pm.me>
2022-07-12 17:07:42 +0200

    refactor extension host tab completion

--- packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostTabCompletion.js
+++ packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostTabCompletion.js
@@ -1,12 +1,15 @@
-import * as ExtensionHostManagement from './ExtensionHostManagement.js'
+import * as ExtensionHostShared from './ExtensionHostShared.js'
 
-export const executeTabCompletionProvider = async (textDocument, offset) => {
-  const extensionHost = await ExtensionHostManagement.activateByEvent(
-    `onTabCompletion:${textDocument.languageId}`
-  )
-  return extensionHost.invoke(
-    /* ExtensionHostTabCompletion.execute */ 'ExtensionHost.executeTabCompletionProvider',
-    /* textDocumentId */ textDocument.id,
-    /* offset */ offset
-  )
+const combineResults = (results) => {
+  return results[0]
+}
+
+export const executeTabCompletionProvider = async (editor, offset) => {
+  return ExtensionHostShared.executeProviders({
+    event: `onTabCompletion:${editor.languageId}`,
+    method: 'ExtensionHost.executeTabCompletionProvider',
+    params: [editor.id, offset],
+    noProviderFoundMessage: 'No tab completion provider found',
+    combineResults,
+  })
 }
---
+++ packages/renderer-worker/test/ExtensionHostTabCompletion.test.js
@@ -0,0 +1,70 @@
+import { jest } from '@jest/globals'
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule(
+  '../src/parts/ExtensionHost/ExtensionHostShared.js',
+  () => {
+    return {
+      executeProviders: jest.fn(() => {
+        throw new Error('not implemented')
+      }),
+    }
+  }
+)
+
+const ExtensionHostTabCompletion = await import(
+  '../src/parts/ExtensionHost/ExtensionHostTabCompletion.js'
+)
+const ExtensionHostShared = await import(
+  '../src/parts/ExtensionHost/ExtensionHostShared.js'
+)
+
+test('executeTabCompletionProvider - no tab completion', async () => {
+  // @ts-ignore
+  ExtensionHostShared.executeProviders.mockImplementation(() => {
+    return undefined
+  })
+  expect(
+    await ExtensionHostTabCompletion.executeTabCompletionProvider(
+      { id: 1, languageId: 'test' },
+      0
+    )
+  ).toBe(undefined)
+  expect(ExtensionHostShared.executeProviders).toHaveBeenCalledTimes(1)
+  expect(ExtensionHostShared.executeProviders).toHaveBeenCalledWith({
+    combineResults: expect.any(Function),
+    event: 'onTabCompletion:test',
+    method: 'ExtensionHost.executeTabCompletionProvider',
+    noProviderFoundMessage: 'No tab completion provider found',
+    params: [1, 0],
+  })
+})
+
+test('executeTabCompletionProvider - tab completion', async () => {
+  // @ts-ignore
+  ExtensionHostShared.executeProviders.mockImplementation(() => {
+    return {}
+  })
+  expect(
+    await ExtensionHostTabCompletion.executeTabCompletionProvider(
+      { id: 1, languageId: 'test' },
+      0
+    )
+  ).toEqual({})
+})
+
+test('executeTabCompletionProvider - error - tabCompletionProvider throws error', async () => {
+  // @ts-ignore
+  ExtensionHostShared.executeProviders.mockImplementation(async () => {
+    throw new TypeError('x is not a function')
+  })
+  await expect(
+    ExtensionHostTabCompletion.executeTabCompletionProvider(
+      { id: 1, languageId: 'test' },
+      0
+    )
+  ).rejects.toThrowError(new TypeError('x is not a function'))
+})
