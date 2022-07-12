a27d2134e60f1f0c241486c56fb2fcce4749bd82
Le Vivilet <levivlet@pm.me>
2022-07-12 16:53:39 +0200

    refactor rename provider

--- packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostRename.js
+++ packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostRename.js
@@ -1,25 +1,29 @@
-import * as ExtensionHost from '../ExtensionHost/ExtensionHostCore.js'
-import * as ExtensionHostManagement from './ExtensionHostManagement.js'
+import * as ExtensionHostShared from './ExtensionHostShared.js'
+
+const combineResultsPrepareRename = (results) => {
+  return results[0]
+}
 
 export const executePrepareRenameProvider = async (editor, offset) => {
-  const extensionHost = await ExtensionHostManagement.activateByEvent(
-    `onRename:${editor.languageId}`
-  )
-  return extensionHost.invoke(
-    /* ExtensionHost.prepareRename */ 'ExtensionHostRename.executePrepareRename',
-    /* documentId */ editor.id,
-    /* offset */ offset
-  )
+  return ExtensionHostShared.executeProviders({
+    event: `onRename:${editor.languageId}`,
+    method: 'ExtensionHostRename.executePrepareRename',
+    params: [editor.id, offset],
+    noProviderFoundMessage: 'No rename provider found',
+    combineResults: combineResultsPrepareRename,
+  })
+}
+
+const combineResultsRename = (results) => {
+  return results[0]
 }
 
 export const executeRenameProvider = async (editor, offset, newName) => {
-  const extensionHost = await ExtensionHostManagement.activateByEvent(
-    `onRename:${editor.languageId}`
-  )
-  return extensionHost.invoke(
-    /* ExtensionHost.rename */ 'ExtensionHostRename.executeRename',
-    /* documentId */ editor.id,
-    /* offset */ offset,
-    /* newName */ newName
-  )
+  return ExtensionHostShared.executeProviders({
+    event: `onRename:${editor.languageId}`,
+    method: 'ExtensionHostRename.executeRename',
+    params: [editor.id, offset, newName],
+    noProviderFoundMessage: 'No rename provider found',
+    combineResults: combineResultsPrepareRename,
+  })
 }
