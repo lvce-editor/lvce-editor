a3bf8cb9150bac4383a359bb49f2f448d8121002
Le Vivilet <levivlet@pm.me>
2022-07-13 12:50:39 +0200

    improve error message when main cannot be found

--- packages/extension-host-worker/src/parts/ExtensionHostExtension/ExtensionHostExtension.js
+++ packages/extension-host-worker/src/parts/ExtensionHostExtension/ExtensionHostExtension.js
@@ -1,4 +1,5 @@
 import * as Assert from '../Assert/Assert.js'
+import { ExtensionActivationError } from '../Errors/Errors.js'
 
 const getAbsolutePath = (path, relativePath) => {
   if (path.endsWith('/')) {
@@ -7,24 +8,32 @@ const getAbsolutePath = (path, relativePath) => {
   return new URL(relativePath, path + '/').toString()
 }
 
+const baseName = (path) => {
+  const slashIndex = path.lastIndexOf('/')
+  return path.slice(slashIndex + 1)
+}
+
+const getId = (extension) => {
+  if (extension && extension.id) {
+    return extension.id
+  }
+  if (extension && extension.path) {
+    return baseName(extension.path)
+  }
+  return '<unknown>'
+}
+
 export const activate = async (extension) => {
   // console.log('activating ', path)
   try {
     Assert.string(extension.path)
     Assert.string(extension.browser)
     const absolutePath = getAbsolutePath(extension.path, extension.browser)
-    console.log({
-      browser: extension.browser,
-      path: extension.path,
-      absolutePath,
-    })
     const module = await import(absolutePath)
     await module.activate()
   } catch (error) {
-    throw new Error(`Failed to activate extension`, {
-      // @ts-ignore
-      cause: error,
-    })
+    const id = getId(extension)
+    throw new Error(`Failed to activate extension ${id}: ${error}`)
   }
   // console.info('activated', path)
 }
