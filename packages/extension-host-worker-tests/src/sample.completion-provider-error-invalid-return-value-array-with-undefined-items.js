21a1b62bd0ed7a47c693aa022d9b5d7e1f75374f
Le Vivilet <levivlet@pm.me>
2022-07-13 15:53:11 +0200

    improve error handling when completion item is of type number

---
+++ packages/extension-host-worker-tests/src/sample.completion-provider-error-invalid-return-value-array-with-undefined-items.js
@@ -0,0 +1,39 @@
+import {
+  getTmpDir,
+  runWithExtension,
+  test,
+  expect,
+  writeFile,
+} from './_testFrameWork.js'
+
+test('sample.completion-provider-error-invalid-return-value-array-with-undefined-items', async () => {
+  const tmpDir = await getTmpDir()
+  await writeFile(
+    `${tmpDir}/test.xyz`,
+    `export const add = () => {}
+`
+  )
+  const page = await runWithExtension({
+    name: 'sample.completion-provider-error-invalid-return-value-array-with-undefined-items',
+    folder: tmpDir,
+  })
+  const testTxt = page.locator('text=test.xyz')
+  await testTxt.click()
+
+  const token = page.locator('.Token').first()
+  await expect(token).toBeVisible()
+  await token.click()
+
+  const cursor = page.locator('.EditorCursor')
+  await expect(cursor).toHaveCount(1)
+
+  await page.keyboard.press('Control+Space')
+
+  const overlayMessage = page.locator('.EditorOverlayMessage')
+  await expect(overlayMessage).toBeVisible()
+  // TODO should say failed to load completions because of invalid return value
+  // or just handle undefined value gracefully
+  await expect(overlayMessage).toHaveText(
+    `Cannot read properties of undefined (reading 'filter')`
+  )
+})
