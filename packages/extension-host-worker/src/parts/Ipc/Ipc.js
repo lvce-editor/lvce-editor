f04b302f81caeb1b16d1a647fe7b679b9e0e9fe3
Le Vivilet <levivlet@pm.me>
2022-07-15 21:57:13 +0200

    fix extension host ipc

--- packages/extension-host-worker/src/parts/Ipc/Ipc.js
+++ packages/extension-host-worker/src/parts/Ipc/Ipc.js
@@ -1,3 +1,5 @@
+import * as Command from '../Command/Command.js'
+
 const getIpc = () => {
   postMessage('ready')
   return {
@@ -19,13 +21,13 @@ const improveError = (error) => {
   }
 }
 
-export const listen = (command) => {
+export const listen = () => {
   const ipc = getIpc()
   const handleMessage = async (event) => {
     const message = event.data
     if (message.method) {
       try {
-        const result = await command.execute(message.method, ...message.params)
+        const result = await Command.execute(message.method, ...message.params)
         ipc.send({
           jsonrpc: '2.0',
           id: message.id,
@@ -61,7 +63,4 @@ export const listen = (command) => {
     }
   }
   ipc.onmessage = handleMessage
-  return {
-    register(method, fn) {},
-  }
 }
