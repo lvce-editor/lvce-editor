1dd0ff4cda31949183aeda1214d4cf13d53f0338
Le Vivilet <levivlet@pm.me>
2022-07-22 17:57:58 +0200

    fix editor paste

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandPaste.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandPaste.js
@@ -3,7 +3,9 @@ import * as EditorPasteText from './EditorCommandPasteText.js'
 import * as Assert from '../Assert/Assert.js'
 
 export const editorPaste = async (editor) => {
-  const text = await Command.execute(/* ClipBoard.readText */ 240)
+  const text = await Command.execute(
+    /* ClipBoard.readText */ 'ClipBoard.readText'
+  )
   Assert.string(text)
   return EditorPasteText.editorPasteText(editor, text)
 }
