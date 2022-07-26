85196bb9e6f729a7a5cc61fce3234644118b2d0b
Le Vivilet <levivlet@pm.me>
2022-07-26 17:02:51 +0200

    fix errors in firefox

--- packages/renderer-process/src/parts/IpcParent/IpcParentWithMessagePort.js
+++ packages/renderer-process/src/parts/IpcParent/IpcParentWithMessagePort.js
@@ -1,4 +1,7 @@
-export const create = async (url) => {
+import * as Assert from '../Assert/Assert.js'
+
+export const create = async ({ url }) => {
+  Assert.string(url)
   const port = await new Promise((resolve) => {
     globalThis.acceptPort = resolve
     import(url)
