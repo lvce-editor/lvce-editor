eee7ca4e2bc3d0e2e26a71d4deb04617a67e2bfe
Le Vivilet <levivlet@pm.me>
2022-07-26 10:16:53 +0200

    remove failing tests in renderer process

--- packages/renderer-process/test/WebWorkerWithModuleWorker.test.js
+++
@@ -1,6 +0,0 @@
-import * as WebWorkerWithModuleWorker from '../src/parts/WebWorker/WebWorkerWithModuleWorker.js'
-
-// TODO
-test.skip('create', async () => {
-  await WebWorkerWithModuleWorker.create()
-})
