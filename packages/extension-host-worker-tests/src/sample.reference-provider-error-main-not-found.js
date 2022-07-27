4722ccda36d0268d2b51f6d8487264e8ef5d4978
Le Vivilet <levivlet@pm.me>
2022-07-28 00:44:06 +0200

    refactor reference provider error main not found test

--- packages/extension-host-worker-tests/src/sample.reference-provider-error-main-not-found.js
+++ packages/extension-host-worker-tests/src/sample.reference-provider-error-main-not-found.js
@@ -1,23 +1,32 @@
 import {
-  getTmpDir,
-  runWithExtension,
-  test,
   expect,
-  writeFile,
-} from './_testFrameWork.js'
+  Locator,
+  test,
+} from '../../renderer-worker/src/parts/TestFrameWork/TestFrameWork.js'
+import {
+  Editor,
+  Extension,
+  FileSystem,
+  Main,
+  Workspace,
+  ContextMenu,
+} from '../../renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponent.js'
+
+const name = 'sample.reference-provider-error-main-not-found'
 
 test('sample.reference-provider-error-main-not-found', async () => {
   // arrange
-  const tmpDir = await getTmpDir()
-  await writeFile(
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
     `${tmpDir}/test.xyz`,
     `export const add = () => {}
 `
   )
-  const { Main, Editor, ContextMenu, locator } = await runWithExtension({
-    name: 'sample.reference-provider-error-main-not-found',
-    folder: tmpDir,
-  })
+
+  await Workspace.setPath(tmpDir)
+  await Extension.addWebExtension(
+    new URL(`../fixtures/${name}`, import.meta.url).toString()
+  )
 
   // act
   await Main.openUri(`${tmpDir}/test.xyz`)
@@ -26,7 +35,7 @@ test('sample.reference-provider-error-main-not-found', async () => {
   await ContextMenu.selectItem('Find all references')
 
   // assert
-  const viewletLocations = locator('.Viewlet[data-viewlet-id="Locations"]')
+  const viewletLocations = Locator('.Viewlet[data-viewlet-id="Locations"]')
   await expect(viewletLocations).toBeVisible()
 
   // TODO should improve error message
@@ -34,7 +43,7 @@ test('sample.reference-provider-error-main-not-found', async () => {
   // const origin = location.origin
   const mainUrl = new URL(
     '../fixtures/sample.reference-provider-error-main-not-found/not-found.js',
-    location.href.slice(0, location.href.lastIndexOf('/')) + '/'
+    import.meta.url
   ).toString()
   await expect(viewletLocations).toHaveText(
     `Error: Failed to activate extension sample.reference-provider-error-main-not-found: TypeError: Failed to fetch dynamically imported module: ${mainUrl}`
