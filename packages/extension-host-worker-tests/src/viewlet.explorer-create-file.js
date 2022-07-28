3cb9beca101c26697d06515626daf2ef5668d61f
Le Vivilet <levivlet@pm.me>
2022-07-28 11:05:04 +0200

    fix explorer create file test

--- packages/extension-host-worker-tests/src/viewlet.explorer-create-file.js
+++ packages/extension-host-worker-tests/src/viewlet.explorer-create-file.js
@@ -1,40 +1,40 @@
 import {
   expect,
-  getTmpDir,
-  runWithExtension,
+  Locator,
   test,
-  writeFile,
-} from './_testFrameWork.js'
+} from '../../renderer-worker/src/parts/TestFrameWork/TestFrameWork.js'
+import {
+  ContextMenu,
+  Explorer,
+  FileSystem,
+  KeyBoard,
+  Workspace,
+} from '../../renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponent.js'
 
-// TODO test is flaky https://github.com/lvce-editor/lvce-editor/runs/7490211933?check_suite_focus=true
-test.skip('viewlet.explorer-create-file', async () => {
+// TODO test might be flaky https://github.com/lvce-editor/lvce-editor/runs/7490211933?check_suite_focus=true
+test('viewlet.explorer-create-file', async () => {
   // arrange
-  const tmpDir = await getTmpDir()
-  await writeFile(`${tmpDir}/file1.txt`, 'content 1')
-  await writeFile(`${tmpDir}/file2.txt`, 'content 2')
-  await writeFile(`${tmpDir}/file3.txt`, 'content 3')
-  const { Main, locator, ContextMenu, keyboard } = await runWithExtension({
-    folder: tmpDir,
-    name: '',
-  })
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'content 1')
+  await FileSystem.writeFile(`${tmpDir}/file2.txt`, 'content 2')
+  await FileSystem.writeFile(`${tmpDir}/file3.txt`, 'content 3')
+
+  await Workspace.setPath(tmpDir)
 
   // act
-  const explorer = locator('.Viewlet[data-viewlet-id="Explorer"]')
-  await explorer.click({
-    button: 'right',
-  })
+  await Explorer.openContextMenu(-1)
   await ContextMenu.selectItem('New File')
 
   // assert
-  const inputBox = explorer.locator('input')
+  const inputBox = Locator('input')
   await expect(inputBox).toBeVisible()
   await expect(inputBox).toBeFocused()
 
   // act
   await inputBox.type('created.txt')
-  await keyboard.press('Enter')
+  await KeyBoard.press('Enter')
 
   // assert
-  const newFile = explorer.locator('text=created.txt')
+  const newFile = Locator('text=created.txt')
   await expect(newFile).toBeVisible()
 })
