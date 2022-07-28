2e685a30bb6b7ea02e8dd7367603bc77c6ba1c7a
Le Vivilet <levivlet@pm.me>
2022-07-28 11:15:26 +0200

    refactor explorer keyboard navigation test

--- packages/renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponentExplorer.js
+++ packages/renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponentExplorer.js
@@ -3,3 +3,7 @@ import * as Command from '../Command/Command.js'
 export const openContextMenu = async (index) => {
   await Command.execute('Explorer.handleContextMenu', 0, 0, index)
 }
+
+export const focus = async () => {
+  await Command.execute('Explorer.focusIndex', -1)
+}
