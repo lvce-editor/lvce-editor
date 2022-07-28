46cf23a11aa70ba8d4e283c7b400afdc7a2ec360
Le Vivilet <levivlet@pm.me>
2022-07-28 11:06:28 +0200

    fix explorer delete file test

--- packages/extension-host-worker-tests/src/viewlet.explorer-delete-file.js
+++ packages/extension-host-worker-tests/src/viewlet.explorer-delete-file.js
@@ -1,30 +1,29 @@
 import {
   expect,
-  getTmpDir,
-  runWithExtension,
+  Locator,
   test,
-  writeFile,
-} from './_testFrameWork.js'
+} from '../../renderer-worker/src/parts/TestFrameWork/TestFrameWork.js'
+import {
+  ContextMenu,
+  Explorer,
+  FileSystem,
+  Workspace,
+} from '../../renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponent.js'
 
 test('viewlet.explorer-delete-file', async () => {
   // arrange
-  const tmpDir = await getTmpDir()
-  await writeFile(`${tmpDir}/file1.txt`, 'content 1')
-  await writeFile(`${tmpDir}/file2.txt`, 'content 2')
-  await writeFile(`${tmpDir}/file3.txt`, 'content 3')
-  const { Main, locator, ContextMenu } = await runWithExtension({
-    folder: tmpDir,
-    name: '',
-  })
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'content 1')
+  await FileSystem.writeFile(`${tmpDir}/file2.txt`, 'content 2')
+  await FileSystem.writeFile(`${tmpDir}/file3.txt`, 'content 3')
+
+  await Workspace.setPath(tmpDir)
 
   // act
-  const explorer = locator('.Viewlet[data-viewlet-id="Explorer"]')
-  const file1 = explorer.locator('text=file1.txt')
-  await file1.click({
-    button: 'right',
-  })
+  await Explorer.openContextMenu(0)
 
   // assert
   await ContextMenu.selectItem('Delete')
+  const file1 = Locator('text=file1.txt')
   await expect(file1).toBeHidden()
 })
