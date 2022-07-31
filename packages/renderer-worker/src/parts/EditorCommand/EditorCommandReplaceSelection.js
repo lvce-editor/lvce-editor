493b77f8adf940ada731a35f902d1667ad0e3c0c
Le Vivilet <levivlet@pm.me>
2022-07-31 17:07:51 +0200

    fix delete character right

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandReplaceSelection.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandReplaceSelection.js
@@ -2,10 +2,27 @@ import * as TextDocument from '../TextDocument/TextDocument.js'
 
 export const editorReplaceSelections = (editor, replacement, origin) => {
   const changes = []
-  for (const selection of editor.selections) {
+  const selections = editor.selections
+  for (let i = 0; i < selections.length; i += 4) {
+    const selectionStartRow = selections[i]
+    const selectionStartColumn = selections[i + 1]
+    const selectionEndRow = selections[i + 2]
+    const selectionEndColumn = selections[i + 3]
+    const start = {
+      rowIndex: selectionStartRow,
+      columnIndex: selectionStartColumn,
+    }
+    const end = {
+      rowIndex: selectionEndRow,
+      columnIndex: selectionEndColumn,
+    }
+    const selection = {
+      start,
+      end,
+    }
     changes.push({
-      start: selection.start,
-      end: selection.end,
+      start: start,
+      end: end,
       inserted: replacement,
       deleted: TextDocument.getSelectionText(editor, selection),
       origin,
