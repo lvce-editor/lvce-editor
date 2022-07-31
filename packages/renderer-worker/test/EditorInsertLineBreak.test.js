18ad7f262466e2b68f8c95adea04b33050bf490a
Le Vivilet <levivlet@pm.me>
2022-07-31 15:17:44 +0200

    fix: insert line break

--- packages/renderer-worker/test/EditorInsertLineBreak.test.js
+++ packages/renderer-worker/test/EditorInsertLineBreak.test.js
@@ -1,142 +1,68 @@
 import * as EditorInsertLineBreak from '../src/parts/EditorCommand/EditorCommandInsertLineBreak.js'
 import * as TokenizePlainText from '../src/parts/Tokenizer/TokenizePlainText.js'
+import * as EditorSelection from '../src/parts/EditorSelection/EditorSelection.js'
 
 test('editorInsertLineBreak', () => {
-  const cursor = {
-    rowIndex: 0,
-    columnIndex: 0,
-  }
   const editor = {
     lines: ['11111', '22222'],
-    cursor,
-    selections: [
-      {
-        start: cursor,
-        end: cursor,
-      },
-    ],
-    lineCache: [],
-    tokenizer: TokenizePlainText,
+    primarySelectionIndex: 0,
+    selections: EditorSelection.fromRange(0, 0, 0, 0),
     undoStack: [],
   }
   expect(EditorInsertLineBreak.editorInsertLineBreak(editor)).toMatchObject({
     lines: ['', '11111', '22222'],
-    cursor: {
-      rowIndex: 1,
-      columnIndex: 0,
-    },
+    selections: EditorSelection.fromRange(1, 0, 1, 0),
   })
 })
 
 test('editorInsertLineBreak - in middle', () => {
-  const cursor = {
-    rowIndex: 0,
-    columnIndex: 2,
-  }
   const editor = {
     lines: ['11111', '22222'],
-    cursor,
-    selections: [
-      {
-        start: cursor,
-        end: cursor,
-      },
-    ],
-    lineCache: [],
-    tokenizer: TokenizePlainText,
+    primarySelectionIndex: 0,
+    selections: EditorSelection.fromRange(0, 2, 0, 2),
     undoStack: [],
   }
   expect(EditorInsertLineBreak.editorInsertLineBreak(editor)).toMatchObject({
     lines: ['11', '111', '22222'],
-    cursor: {
-      rowIndex: 1,
-      columnIndex: 0,
-    },
+    selections: EditorSelection.fromRange(1, 0, 1, 0),
   })
 })
 
 test('editorInsertLineBreak - with whitespace at start', () => {
-  const cursor = {
-    rowIndex: 0,
-    columnIndex: 2,
-  }
   const editor = {
     lines: ['    11111', '22222'],
-    cursor,
-    selections: [
-      {
-        start: cursor,
-        end: cursor,
-      },
-    ],
-    lineCache: [],
-    tokenizer: TokenizePlainText,
+    primarySelectionIndex: 0,
+    selections: EditorSelection.fromRange(0, 2, 0, 2),
     undoStack: [],
   }
   expect(EditorInsertLineBreak.editorInsertLineBreak(editor)).toMatchObject({
     lines: ['  ', '    11111', '22222'],
-    cursor: {
-      rowIndex: 1,
-      columnIndex: 2,
-    },
+    selections: EditorSelection.fromRange(1, 2, 1, 2),
   })
 })
 
 test('editorInsertLineBreak - with selection', () => {
   const editor = {
     lines: ['11111', '22222'],
-    cursor: {
-      rowIndex: 0,
-      columnIndex: 2,
-    },
-    selections: [
-      {
-        start: {
-          rowIndex: 0,
-          columnIndex: 0,
-        },
-        end: {
-          rowIndex: 0,
-          columnIndex: 2,
-        },
-      },
-    ],
-    lineCache: [],
-    tokenizer: TokenizePlainText,
+    primarySelectionIndex: 0,
+    selections: EditorSelection.fromRange(0, 0, 0, 2),
     undoStack: [],
   }
   expect(EditorInsertLineBreak.editorInsertLineBreak(editor)).toMatchObject({
     lines: ['', '111', '22222'],
-    cursor: {
-      rowIndex: 1,
-      columnIndex: 0,
-    },
+    selections: EditorSelection.fromRange(1, 0, 1, 0),
   })
 })
 
 test('editorInsertLineBreak - cursor at end of line', () => {
-  const cursor = {
-    rowIndex: 0,
-    columnIndex: 9,
-  }
   const editor = {
     lines: ['    11111', '22222'],
-    cursor,
-    selections: [
-      {
-        start: cursor,
-        end: cursor,
-      },
-    ],
-    lineCache: [],
-    tokenizer: TokenizePlainText,
+    primarySelectionIndex: 0,
+    selections: EditorSelection.fromRange(0, 9, 0, 9),
     undoStack: [],
   }
   expect(EditorInsertLineBreak.editorInsertLineBreak(editor)).toMatchObject({
     lines: ['    11111', '    ', '22222'],
-    cursor: {
-      rowIndex: 1,
-      columnIndex: 4,
-    },
+    selections: EditorSelection.fromRange(1, 4, 1, 4),
   })
 })
