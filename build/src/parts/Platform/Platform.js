af6f873aca279794b7046371d9cb21e7540575c9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-07-31 14:04:47 +0200

    add path prefix so that deployment on github pages can work (#58)

--- build/src/parts/Platform/Platform.js
+++ build/src/parts/Platform/Platform.js
@@ -9,3 +9,27 @@ export const isMacos = () => {
 export const isLinux = () => {
   return process.platform === 'linux'
 }
+
+const normalizePathPrefix = (prefix) => {
+  if (prefix === '') {
+    return ''
+  }
+  if (prefix === '/') {
+    return ''
+  }
+  if (prefix.endsWith('/')) {
+    if (prefix.startsWith('/')) {
+      return prefix.slice(0, -1)
+    }
+    return '/' + prefix.slice(0, -1)
+  }
+  if (!prefix.startsWith('/')) {
+    prefix = '/' + prefix
+  }
+  return prefix
+}
+
+export const getPathPrefix = () => {
+  const prefix = process.env.PATH_PREFIX || ''
+  return normalizePathPrefix(prefix)
+}
