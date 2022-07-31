45237b47e273b385d88653a2de9197b0e97f26de
Le Vivilet <levivlet@pm.me>
2022-07-31 16:59:07 +0200

    fix indentMore

--- packages/renderer-worker/test/EditorIndentMore.test.js
+++ packages/renderer-worker/test/EditorIndentMore.test.js
@@ -1,129 +1,42 @@
 import * as EditorIndentMore from '../src/parts/EditorCommand/EditorCommandIndentMore.js'
-import * as TokenizePlainText from '../src/parts/Tokenizer/TokenizePlainText.js'
+import * as EditorSelection from '../src/parts/EditorSelection/EditorSelection.js'
 
 test('editorIndentMore - indent empty selection at start of line', () => {
-  const cursor = {
-    rowIndex: 0,
-    columnIndex: 0,
-  }
   const editor = {
     lines: ['line 1'],
-    cursor: cursor,
-    selections: [
-      {
-        start: cursor,
-        end: cursor,
-      },
-    ],
+    selections: EditorSelection.fromRange(0, 0, 0, 0),
     minLineY: 0,
     lineCache: [],
-    tokenizer: TokenizePlainText,
     undoStack: [],
   }
   expect(EditorIndentMore.editorIndentMore(editor)).toMatchObject({
     lines: ['  line 1'],
-    cursor: {
-      rowIndex: 0,
-      columnIndex: 2,
-    },
-    selections: [
-      {
-        start: {
-          rowIndex: 0,
-          columnIndex: 2,
-        },
-        end: {
-          rowIndex: 0,
-          columnIndex: 2,
-        },
-      },
-    ],
+    selections: EditorSelection.fromRange(0, 2, 0, 2),
   })
 })
 
 test.skip('editorIndentMore - indent one selection - single line', async () => {
   const editor = {
     lines: ['line 1'],
-    cursor: {
-      rowIndex: 0,
-      columnIndex: 6,
-    },
-    selections: [
-      {
-        start: {
-          rowIndex: 0,
-          columnIndex: 0,
-        },
-        end: {
-          rowIndex: 0,
-          columnIndex: 6,
-        },
-      },
-    ],
+    primarySelectionIndex: 0,
+    selections: EditorSelection.fromRange(0, 0, 0, 6),
     minLineY: 0,
     lineCache: [],
-    tokenizer: TokenizePlainText,
   }
   expect(EditorIndentMore.editorIndentMore(editor)).toMatchObject({
     lines: ['  line 1'],
-    cursor: {
-      rowIndex: 0,
-      columnIndex: 8,
-    },
-    selections: [
-      {
-        start: {
-          rowIndex: 0,
-          columnIndex: 2,
-        },
-        end: {
-          rowIndex: 0,
-          columnIndex: 8,
-        },
-      },
-    ],
+    selections: EditorSelection.fromRange(0, 2, 0, 8),
   })
 })
 
 test.skip('editorIndentMore - indent one selection - multiple lines', async () => {
   const editor = {
     lines: ['line 1', 'line 2', 'line 3', 'line 4'],
-    cursor: {
-      rowIndex: 2,
-      columnIndex: 2,
-    },
-    selections: [
-      {
-        start: {
-          rowIndex: 1,
-          columnIndex: 1,
-        },
-        end: {
-          rowIndex: 2,
-          columnIndex: 2,
-        },
-      },
-    ],
-    lineCache: [],
-    tokenizer: TokenizePlainText,
+    primarySelectionIndex: 0,
+    selections: EditorSelection.fromRange(1, 1, 2, 2),
   }
   expect(EditorIndentMore.editorIndentMore(editor)).toMatchObject({
     lines: ['line 1', '  line 2', '  line 3', 'line 4'],
-    selections: [
-      {
-        start: {
-          rowIndex: 1,
-          columnIndex: 3,
-        },
-        end: {
-          rowIndex: 2,
-          columnIndex: 4,
-        },
-      },
-    ],
-    cursor: {
-      rowIndex: 2,
-      columnIndex: 4,
-    },
+    selections: EditorSelection.fromRange(1, 3, 2, 4),
   })
 })
