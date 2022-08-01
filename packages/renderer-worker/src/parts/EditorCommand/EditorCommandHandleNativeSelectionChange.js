ddd6f726d48db1a82d646a0b47042aab81d0fd6a
Le Vivilet <levivlet@pm.me>
2022-08-01 14:34:07 +0200

    fix handleNativeSelectionChange

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleNativeSelectionChange.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleNativeSelectionChange.js
@@ -2,8 +2,7 @@ import * as Editor from '../Editor/Editor.js'
 import * as EditorGetSelectionFromNativeRange from './EditorCommandGetSelectionFromNativeRange.js'
 
 export const editorHandleNativeSelectionChange = (editor, range) => {
-  const selection =
+  const selections =
     EditorGetSelectionFromNativeRange.getSelectionFromNativeRange(editor, range)
-  const selections = [selection]
   return Editor.scheduleSelections(editor, selections)
 }
