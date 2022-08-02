76e35aac870f7da3750d6265ff3740b33e715b87
Le Vivilet <levivlet@pm.me>
2022-08-02 14:41:30 +0200

    fix: handleBeforeInput

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleNativeBeforeInputFromContentEditable.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleNativeBeforeInputFromContentEditable.js
@@ -2,7 +2,7 @@ import * as Editor from '../Editor/Editor.js'
 import * as TextDocument from '../TextDocument/TextDocument.js'
 import * as EditorGetSelectionFromNativeRange from './EditorCommandGetSelectionFromNativeRange.js'
 
-export const handleBeforeInputFromContentEditable = (editor, data, range) => {
+const getChanges = (editor, data, range) => {
   const selection =
     EditorGetSelectionFromNativeRange.getSelectionFromNativeRange(editor, range)
   const changes = [
@@ -14,5 +14,10 @@ export const handleBeforeInputFromContentEditable = (editor, data, range) => {
       origin: 'contentEditableInput',
     },
   ]
+  return changes
+}
+
+export const handleBeforeInputFromContentEditable = (editor, data, range) => {
+  const changes = getChanges(editor, data, range)
   return Editor.scheduleDocumentAndCursorsSelections(editor, changes)
 }
