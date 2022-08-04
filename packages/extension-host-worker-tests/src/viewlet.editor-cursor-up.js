75eaf4b553a34cde9ff7abafeb28db1497f84d3e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-08-04 13:26:05 +0200

    add tests for cursor up and cursor down (#69)

---
+++ packages/extension-host-worker-tests/src/viewlet.editor-cursor-up.js
@@ -0,0 +1,36 @@
+import {
+  expect,
+  Locator,
+  test,
+} from '../../renderer-worker/src/parts/TestFrameWork/TestFrameWork.js'
+import {
+  Editor,
+  FileSystem,
+  Main,
+  Workspace,
+} from '../../renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponent.js'
+
+test('viewlet.editor-cursor-up', async () => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file1.txt`,
+    `content 1
+content 2`
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+
+  // act
+  await Editor.setCursor(1, 1)
+
+  // assert
+  const cursor = Locator('.EditorCursor')
+  await expect(cursor).toHaveCSS('top', '20px')
+
+  // act
+  await Editor.cursorUp()
+
+  // assert
+  await expect(cursor).toHaveCSS('top', '0px')
+})
