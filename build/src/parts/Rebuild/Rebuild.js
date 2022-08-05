468053a0bc7fa14c64ead7d8039f3a2ab33e59e1
Le Vivilet <levivlet@pm.me>
2022-08-05 19:03:14 +0200

    improve error message when rebuilding native dependencies fails

--- build/src/parts/Rebuild/Rebuild.js
+++ build/src/parts/Rebuild/Rebuild.js
@@ -1,11 +1,20 @@
 import * as ElectronRebuild from 'electron-rebuild'
+import VError from 'verror'
 
 export const rebuild = async ({ electronVersion, buildPath, arch }) => {
   console.log({ electronVersion, buildPath, arch })
-  await ElectronRebuild.rebuild({
-    buildPath,
-    electronVersion,
-    arch,
-    force: true,
-  })
+  try {
+    await ElectronRebuild.rebuild({
+      buildPath,
+      electronVersion,
+      arch,
+      force: true,
+    })
+  } catch (error) {
+    throw new VError(
+      // @ts-ignore
+      error,
+      `Failed to rebuild native dependendencies in ${buildPath}`
+    )
+  }
 }
