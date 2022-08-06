986da57cdb81682f41d666d2305b1476240eb4be
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-08-06 14:15:52 +0200

    build electron app in ci to avoid regressions (#82)

--- build/src/parts/Rebuild/Rebuild.js
+++ build/src/parts/Rebuild/Rebuild.js
@@ -1,11 +1,26 @@
 import * as ElectronRebuild from 'electron-rebuild'
+import VError from 'verror'
 
 export const rebuild = async ({ electronVersion, buildPath, arch }) => {
   console.log({ electronVersion, buildPath, arch })
-  await ElectronRebuild.rebuild({
-    buildPath,
-    electronVersion,
-    arch,
-    force: true,
-  })
+  try {
+    await ElectronRebuild.rebuild({
+      buildPath,
+      electronVersion,
+      arch,
+      force: true,
+    })
+  } catch (error) {
+    if (process.env.IGNORE_NATIVE_DEPENDENCY_ERRORS) {
+      console.error(
+        `Failed to rebuild native dependendencies in ${buildPath}: ${error}`
+      )
+    } else {
+      throw new VError(
+        // @ts-ignore
+        error,
+        `Failed to rebuild native dependendencies in ${buildPath}`
+      )
+    }
+  }
 }
