65ca0655c48eb1bcac488e8b02fbc6c96e80acd8
Le Vivilet <levivlet@pm.me>
2022-08-11 22:50:57 +0200

    make all electron invocations async so that error can be handled by caller if error occurs

--- packages/shared-process/test/Electron.test.js
+++ packages/shared-process/test/Electron.test.js
@@ -1,93 +1,65 @@
 import { jest } from '@jest/globals'
 import * as Electron from '../src/parts/Electron/Electron.js'
 
-test('toggleDevtools', () => {
-  Electron.state.send = jest.fn()
-  Electron.toggleDevtools()
-  expect(Electron.state.send).toHaveBeenCalledWith({
-    jsonrpc: '2.0',
-    method: 'Window.toggleDevtools',
-    params: [],
-  })
+test('toggleDevtools', async () => {
+  Electron.state.invoke = jest.fn(async () => {})
+  await Electron.toggleDevtools()
+  expect(Electron.state.invoke).toHaveBeenCalledTimes(1)
+  expect(Electron.state.invoke).toHaveBeenCalledWith('Window.toggleDevtools')
 })
 
-test('windowMinimize', () => {
-  Electron.state.send = jest.fn()
-  Electron.windowMinimize()
-  expect(Electron.state.send).toHaveBeenCalledWith({
-    jsonrpc: '2.0',
-    method: 'Window.minimize',
-    params: [],
-  })
+test('windowMinimize', async () => {
+  Electron.state.invoke = jest.fn(async () => {})
+  await Electron.windowMinimize()
+  expect(Electron.state.invoke).toHaveBeenCalledTimes(1)
+  expect(Electron.state.invoke).toHaveBeenCalledWith('Window.minimize')
 })
 
-test('windowMaximize', () => {
-  Electron.state.send = jest.fn()
-  Electron.windowMaximize()
-  expect(Electron.state.send).toHaveBeenCalledWith({
-    jsonrpc: '2.0',
-    method: 'Window.maximize',
-    params: [],
-  })
+test('windowMaximize', async () => {
+  Electron.state.invoke = jest.fn(async () => {})
+  await Electron.windowMaximize()
+  expect(Electron.state.invoke).toHaveBeenCalledTimes(1)
+  expect(Electron.state.invoke).toHaveBeenCalledWith('Window.maximize')
 })
 
-test('windowUnmaximize', () => {
-  Electron.state.send = jest.fn()
-  Electron.windowUnmaximize()
-  expect(Electron.state.send).toHaveBeenCalledWith({
-    jsonrpc: '2.0',
-    method: 'Window.unmaximize',
-    params: [],
-  })
+test('windowUnmaximize', async () => {
+  Electron.state.invoke = jest.fn(async () => {})
+  await Electron.windowUnmaximize()
+  expect(Electron.state.invoke).toHaveBeenCalledTimes(1)
+  expect(Electron.state.invoke).toHaveBeenCalledWith('Window.unmaximize')
 })
 
-test('windowClose', () => {
-  Electron.state.send = jest.fn()
-  Electron.windowClose()
-  expect(Electron.state.send).toBeCalledWith({
-    jsonrpc: '2.0',
-    method: 'Window.close',
-    params: [],
-  })
+test('windowClose', async () => {
+  Electron.state.invoke = jest.fn(async () => {})
+  await Electron.windowClose()
+  expect(Electron.state.invoke).toHaveBeenCalledTimes(1)
+  expect(Electron.state.invoke).toBeCalledWith('Window.close')
 })
 
-test('windowReload', () => {
-  Electron.state.send = jest.fn()
-  Electron.windowReload()
-  expect(Electron.state.send).toBeCalledWith({
-    jsonrpc: '2.0',
-    method: 'Window.reload',
-    params: [],
-  })
+test('windowReload', async () => {
+  Electron.state.invoke = jest.fn(async () => {})
+  await Electron.windowReload()
+  expect(Electron.state.invoke).toHaveBeenCalledTimes(1)
+  expect(Electron.state.invoke).toBeCalledWith('Window.reload')
 })
 
-test('about', () => {
-  Electron.state.send = jest.fn()
-  Electron.about()
-  expect(Electron.state.send).toBeCalledWith({
-    jsonrpc: '2.0',
-    method: 'About.open',
-    params: [],
-  })
+test('about', async () => {
+  Electron.state.invoke = jest.fn(async () => {})
+  await Electron.about()
+  expect(Electron.state.invoke).toHaveBeenCalledTimes(1)
+  expect(Electron.state.invoke).toHaveBeenCalledWith('About.open')
 })
 
-test('showOpenDialog', () => {
-  Electron.state.send = jest.fn()
-  Electron.showOpenDialog()
-  expect(Electron.state.send).toBeCalledWith({
-    jsonrpc: '2.0',
-    method: 'Dialog.showOpenDialog',
-    params: [],
-    id: expect.any(Number),
-  })
+test('showOpenDialog', async () => {
+  Electron.state.invoke = jest.fn(async () => {})
+  await Electron.showOpenDialog()
+  expect(Electron.state.invoke).toHaveBeenCalledTimes(1)
+  expect(Electron.state.invoke).toBeCalledWith('Dialog.showOpenDialog')
 })
 
-test('crashMainProcess', () => {
-  Electron.state.send = jest.fn()
-  Electron.crashMainProcess()
-  expect(Electron.state.send).toBeCalledWith({
-    jsonrpc: '2.0',
-    method: 'Developer.crashMainProcess',
-    params: [],
-  })
+test('crashMainProcess', async () => {
+  Electron.state.invoke = jest.fn(async () => {})
+  await Electron.crashMainProcess()
+  expect(Electron.state.invoke).toHaveBeenCalledTimes(1)
+  expect(Electron.state.invoke).toBeCalledWith('Developer.crashMainProcess')
 })
