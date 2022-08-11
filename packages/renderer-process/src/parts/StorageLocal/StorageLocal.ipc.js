aea4c33d2f24646468de6defa5e450bfee76d0cd
Le Vivilet <levivlet@pm.me>
2022-08-11 12:04:31 +0200

    remove unused code

--- packages/renderer-process/src/parts/StorageLocal/StorageLocal.ipc.js
+++
@@ -1,8 +0,0 @@
-import * as Command from '../Command/Command.js'
-import * as StorageLocal from './StorageLocal.js'
-
-export const __initialize__ = () => {
-  Command.register('StorageLocal.clear', StorageLocal.clear)
-  Command.register('StorageLocal.getItem', StorageLocal.getItem)
-  Command.register('StorageLocal.setItem', StorageLocal.setItem)
-}
--- packages/renderer-process/src/parts/StorageSession/StorageSession.ipc.js
+++
@@ -1,8 +0,0 @@
-import * as Command from '../Command/Command.js'
-import * as StorageSession from './StorageSession.js'
-
-export const __initialize__ = () => {
-  Command.register('StorageSession.clear', StorageSession.clear)
-  Command.register('StorageSession.getItem', StorageSession.getItem)
-  Command.register('StorageSession.setItem', StorageSession.setItem)
-}
