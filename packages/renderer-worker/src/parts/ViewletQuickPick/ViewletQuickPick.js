b250b7794b69944545286bd081f5fe9b44e5582a
Le Vivilet <levivlet@pm.me>
2022-08-11 22:40:34 +0200

    fix: showing about window from quickpick

--- packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPick.js
+++ packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPick.js
@@ -220,7 +220,7 @@ export const selectIndex = async (state, index, button = /* left */ 0) => {
   switch (command) {
     case 'hide':
       await Viewlet.closeWidget('QuickPick')
-      break
+      return state
     default:
       break
   }
