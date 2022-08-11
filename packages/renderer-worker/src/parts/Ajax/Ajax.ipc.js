486898ca8c4b56b4f60f6410b3186955b8bc49b6
Le Vivilet <levivlet@pm.me>
2022-08-11 16:51:12 +0200

    refactor: add option to make command registration more functional

--- packages/renderer-worker/src/parts/Ajax/Ajax.ipc.js
+++ packages/renderer-worker/src/parts/Ajax/Ajax.ipc.js
@@ -1,8 +1,8 @@
-import * as Command from '../Command/Command.js'
 import * as Ajax from './Ajax.js'
 
 // TODO only use Ajax module via ipc -> that way is is always lazyloaded
-export const __initialize__ = () => {
-  Command.register('Ajax.getJson', Ajax.getJson)
-  Command.register('Ajax.getText', Ajax.getText)
+
+export const Commands = {
+  'Ajax.getJson': Ajax.getJson,
+  'Ajax.getText': Ajax.getText,
 }
--- packages/renderer-worker/src/parts/Command/Command.js
+++ packages/renderer-worker/src/parts/Command/Command.js
@@ -165,7 +165,15 @@ const loadModule = (moduleId) => {
 
 const initializeModule = (module) => {
   if (typeof module.__initialize__ !== 'function') {
-    throw new Error(`module ${module.name} is missing an initialize function`)
+    if (module.Commands) {
+      for (const [key, value] of module.Commands) {
+        register(key, value)
+      }
+    } else {
+      throw new Error(
+        `module ${module.name} is missing an initialize function and commands`
+      )
+    }
   }
   return module.__initialize__()
 }
