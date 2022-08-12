74837d89e526883d07875f983441e4ecbab6b01b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-08-12 09:53:13 +0200

    refactor: simplify local storage code (#146)

--- packages/renderer-worker/src/parts/LocalStorage/LocalStorage.js
+++ packages/renderer-worker/src/parts/LocalStorage/LocalStorage.js
@@ -1,29 +1,23 @@
-import * as StorageBrowser from '../WebStorage/WebStorage.js'
+import * as WebStorage from '../WebStorage/WebStorage.js'
+
+const storageType = WebStorage.StorageType.LocalStorage
 
 export const clear = () => {
-  return StorageBrowser.clear(StorageBrowser.StorageType.LocalStorage)
+  return WebStorage.clear(storageType)
 }
 
 export const getText = (key) => {
-  return StorageBrowser.getText(StorageBrowser.StorageType.LocalStorage, key)
+  return WebStorage.getText(storageType, key)
 }
 
 export const getJson = (key) => {
-  return StorageBrowser.getJson(StorageBrowser.StorageType.LocalStorage, key)
+  return WebStorage.getJson(storageType, key)
 }
 
 export const setText = (key, value) => {
-  return StorageBrowser.setText(
-    StorageBrowser.StorageType.LocalStorage,
-    key,
-    value
-  )
+  return WebStorage.setText(WebStorage.StorageType.LocalStorage, key, value)
 }
 
 export const setJson = (key, value) => {
-  return StorageBrowser.setJson(
-    StorageBrowser.StorageType.LocalStorage,
-    key,
-    value
-  )
+  return WebStorage.setJson(WebStorage.StorageType.LocalStorage, key, value)
 }
--- packages/renderer-worker/src/parts/SessionStorage/SessionStorage.js
+++ packages/renderer-worker/src/parts/SessionStorage/SessionStorage.js
@@ -1,21 +1,23 @@
 import * as WebStorage from '../WebStorage/WebStorage.js'
 
+const storageType = WebStorage.StorageType.SessionStorage
+
 export const clear = () => {
-  return WebStorage.clear(WebStorage.StorageType.SessionStorage)
+  return WebStorage.clear(storageType)
 }
 
 export const getText = (key) => {
-  return WebStorage.getText(WebStorage.StorageType.SessionStorage, key)
+  return WebStorage.getText(storageType, key)
 }
 
 export const getJson = (key) => {
-  return WebStorage.getJson(WebStorage.StorageType.SessionStorage, key)
+  return WebStorage.getJson(storageType, key)
 }
 
 export const setText = (key, value) => {
-  return WebStorage.setText(WebStorage.StorageType.SessionStorage, key, value)
+  return WebStorage.setText(storageType, key, value)
 }
 
 export const setJson = (key, value) => {
-  return WebStorage.setJson(WebStorage.StorageType.SessionStorage, key, value)
+  return WebStorage.setJson(storageType, key, value)
 }
