40da48e6c51be11d90271fe76d9c3e0016a22d55
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-08-12 10:07:29 +0200

    improve error handling for open module (#148)

--- packages/renderer-worker/src/parts/Open/Open.js
+++ packages/renderer-worker/src/parts/Open/Open.js
@@ -1,13 +1,25 @@
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
 import * as RendererProcess from '../RendererProcess/RendererProcess.js'
+import { VError } from '../VError/VError.js'
 
 export const openNativeFolder = async (absolutePath) => {
-  await SharedProcess.invoke(
-    /* Native.openFolder */ 'Native.openFolder',
-    /* path */ absolutePath
-  )
+  try {
+    await SharedProcess.invoke(
+      /* Native.openFolder */ 'Native.openFolder',
+      /* path */ absolutePath
+    )
+  } catch (error) {
+    throw new VError(error, `Failed to open folder ${absolutePath}`)
+  }
 }
 
 export const openUrl = async (url) => {
-  await RendererProcess.invoke(/* Open.openUrl */ 'Open.openUrl', /* url */ url)
+  try {
+    await RendererProcess.invoke(
+      /* Open.openUrl */ 'Open.openUrl',
+      /* url */ url
+    )
+  } catch (error) {
+    throw new VError(error, `Failed to open url ${url}`)
+  }
 }
