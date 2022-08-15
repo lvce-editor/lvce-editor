30888fff4487472d3a23e298ba96445aea870560
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-08-15 19:48:56 +0200

    simplify configuration for downloading builtin extensions (#190)

--- build/src/parts/DownloadBuiltinExtensions/DownloadBuiltinExtensions.js
+++ build/src/parts/DownloadBuiltinExtensions/DownloadBuiltinExtensions.js
@@ -8,8 +8,9 @@ import VError from 'verror'
 import * as Path from '../Path/Path.js'
 import extensions from './builtinExtensions.json' assert { type: 'json' }
 import pMap from 'p-map'
+import * as Assert from '../Assert/Assert.js'
 
-const download = async (url, outFile) => {
+const downloadUrl = async (url, outFile) => {
   try {
     await mkdir(Path.dirname(outFile), { recursive: true })
     await pipeline(got.stream(url), createWriteStream(outFile))
@@ -24,14 +25,24 @@ const download = async (url, outFile) => {
 
 const downloadExtension = async (extension) => {
   try {
-    const baseName = Path.baseName(extension.path)
+    Assert.string(extension.repository)
+    Assert.string(extension.name)
+    Assert.string(extension.version)
+    if (!extension.repository.startsWith('github.com/')) {
+      throw new VError(
+        'currenly only extensions from github releases are supported'
+      )
+    }
+    const baseName = Path.baseName(extension.repository)
+    const cacheName = baseName + '-' + extension.version + '.tar.br'
     const cachedPath = Path.absolute(
-      Path.join('build', '.tmp', `cachedExtensions`, baseName)
+      Path.join('build', '.tmp', `cachedExtensions`, cacheName)
     )
     if (existsSync(cachedPath)) {
       return
     }
-    await download(extension.path, cachedPath)
+    const url = `https://${extension.repository}/releases/download/v${extension.version}/${baseName}-v${extension.version}.tar.br`
+    await downloadUrl(url, cachedPath)
     await extract(
       cachedPath,
       Path.absolute(Path.join(`extensions`, extension.name))
@@ -43,12 +54,17 @@ const downloadExtension = async (extension) => {
 }
 
 export const extract = async (inFile, outDir) => {
-  await mkdir(outDir, { recursive: true })
-  await pipeline(
-    createReadStream(inFile),
-    createBrotliDecompress(),
-    tar.extract(outDir)
-  )
+  try {
+    await mkdir(outDir, { recursive: true })
+    await pipeline(
+      createReadStream(inFile),
+      createBrotliDecompress(),
+      tar.extract(outDir)
+    )
+  } catch (error) {
+    // @ts-ignore
+    throw new VError(error, `Failed to extract ${inFile}`)
+  }
 }
 
 const downloadExtensionAndLog = async (extension) => {
