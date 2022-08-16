16c498f33276629f17e6376614bb50c12a4e465a
Le Vivilet <levivlet@pm.me>
2022-08-17 00:37:37 +0200

    feat: add another cursor with ctrl+click

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleSingleClick.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleSingleClick.js
@@ -4,6 +4,68 @@ import * as EditorCursorSet from './EditorCommandCursorSet.js'
 import * as EditorPosition from './EditorCommandPosition.js'
 import * as EditorMoveSelection from './EditorCommandMoveSelection.js'
 import * as EditorGoToDefinition from './EditorCommandGoToDefinition.js'
+import * as EditorSelection from '../EditorSelection/EditorSelection.js'
+import * as Editor from '../Editor/Editor.js'
+
+const Modifier = {
+  Ctrl: 'ctrl',
+  Alt: 'alt',
+}
+
+const handleSingleClickWithAlt = async (editor, position) => {
+  // TODO rectangular selection with alt click,
+  // but also go to definition with alt click
+  console.log('alt key pressed')
+  const newEditor = await EditorGoToDefinition.editorGoToDefinition(
+    editor,
+    position,
+    /* explicit */ false
+  )
+  return newEditor
+}
+
+const handleSingleClickWithCtrl = async (editor, position) => {
+  const selections = editor.selections
+  for (let i = 0; i < selections.length; i += 4) {
+    const selectionStartRow = selections[i]
+    const selectionStartColumn = selections[i + 1]
+    const selectionEndRow = selections[i + 2]
+    const selectionEndColumn = selections[i + 3]
+    if (
+      selectionStartRow === position.rowIndex &&
+      selectionStartColumn === position.columnIndex &&
+      selectionEndRow === position.rowIndex &&
+      selectionEndColumn === position.columnIndex
+    ) {
+      // selection exists -> remove
+      const newSelections = new Uint32Array(selections.length - 4)
+      newSelections.set(selections.subarray(0, i), 0)
+      newSelections.set(selections.subarray(i + 4), i)
+      return Editor.scheduleSelections(editor, newSelections)
+    }
+  }
+  // TODO selection does not exist -> add
+  // TODO insert in order
+  const newSelections = new Uint32Array(selections.length + 4)
+  newSelections.set(selections, 0)
+  const insertIndex = selections.length
+  newSelections[insertIndex] = position.rowIndex
+  newSelections[insertIndex + 1] = position.columnIndex
+  newSelections[insertIndex + 2] = position.rowIndex
+  newSelections[insertIndex + 3] = position.columnIndex
+  return Editor.scheduleSelections(editor, newSelections)
+}
+
+const handleSingleClickDefault = async (editor, position) => {
+  EditorMoveSelection.state.position = position
+  // TODO this should be declarative like in quickpick
+  await RendererProcess.invoke('Viewlet.invoke', 'EditorText', 'focus')
+  return EditorCursorSet.editorCursorSet(
+    editor,
+    position.rowIndex,
+    position.columnIndex
+  )
+}
 
 export const editorHandleSingleClick = async (
   editor,
@@ -18,26 +80,12 @@ export const editorHandleSingleClick = async (
   Assert.number(y)
   Assert.number(offset)
   const position = EditorPosition.at(editor, x, y, offset)
-  if (modifier === 'alt') {
-    // TODO rectangular selection with alt click,
-    // but also go to definition with alt click
-    console.log('alt key pressed')
-    const newEditor = await EditorGoToDefinition.editorGoToDefinition(
-      editor,
-      position,
-      /* explicit */ false
-    )
-    if (editor !== newEditor) {
-      return newEditor
-    }
+  switch (modifier) {
+    case Modifier.Alt:
+      return handleSingleClickWithAlt(editor, position)
+    case Modifier.Ctrl:
+      return handleSingleClickWithCtrl(editor, position)
+    default:
+      return handleSingleClickDefault(editor, position)
   }
-  console.log({ position })
-  EditorMoveSelection.state.position = position
-  // TODO also focus editor input
-  await RendererProcess.invoke('Viewlet.invoke', 'EditorText', 'focus')
-  return EditorCursorSet.editorCursorSet(
-    editor,
-    position.rowIndex,
-    position.columnIndex
-  )
 }
