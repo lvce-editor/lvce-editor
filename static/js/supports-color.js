27847cff2d46671fa0ac9b95c36e0c5c933f1638
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-08-16 23:04:43 +0200

    add babel codeframe dependency (#205)

---
+++ static/js/supports-color.js
@@ -0,0 +1,8 @@
+var browser = {
+  stdout: false,
+  stderr: false
+};
+export default browser;
+var stderr = browser.stderr;
+var stdout = browser.stdout;
+export {browser as __moduleExports, stderr, stdout};
