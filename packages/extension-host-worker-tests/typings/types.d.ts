7e13bcb367eba8ed7440228e4cc7f7f063036665
Le Vivilet <levivlet@pm.me>
2022-08-18 17:36:07 +0200

    refactor e2e tests

--- packages/extension-host-worker-tests/typings/types.d.ts
+++ packages/extension-host-worker-tests/typings/types.d.ts
@@ -7,7 +7,26 @@ declare const Editor: {
   readonly openCompletion: () => Promise<void>
   readonly executeTabCompletion: () => Promise<void>
   readonly openEditorContextMenu: () => Promise<void>
-  readonly executeBraceCompletion: () => Promise<void>
+  readonly executeBraceCompletion: (brace: string) => Promise<void>
+  readonly cursorUp: () => Promise<void>
+  readonly cursorDown: () => Promise<void>
+  readonly cursorWordLeft: () => Promise<void>
+  readonly cursorWordRight: () => Promise<void>
+  readonly cursorCharacterRight: () => Promise<void>
+  readonly cursorCharacterLeft: () => Promise<void>
+  readonly copyLineDown: () => Promise<void>
+}
+
+declare const Explorer: {
+  readonly openContextMenu: (index: number) => Promise<void>
+  readonly removeDirent: () => Promise<void>
+  readonly focusFirst: () => Promise<void>
+  readonly focusLast: () => Promise<void>
+  readonly focusNext: () => Promise<void>
+  readonly clickCurrent: () => Promise<void>
+
+  // TODO maybe rename this to collapse
+  readonly handleArrowLeft: () => Promise<void>
 }
 
 declare const Extension: {
@@ -17,6 +36,30 @@ declare const Extension: {
 declare const FileSystem: {
   readonly getTmpDir: () => Promise<string>
   readonly writeFile: (uri: string, content: string) => Promise<void>
+  readonly mkdir: (uri: string) => Promise<void>
+}
+
+declare const KeyBoard: {
+  /**
+   * @deprecated this can lead to flaky tests, it might
+   * be better execute the corresponding command instead
+   *
+   */
+  readonly press: (key: string) => Promise<void>
+}
+
+declare const QuickPick: {
+  readonly focusNext: () => Promise<void>
+  readonly open: () => Promise<void>
+  readonly setValue: (value: string) => Promise<void>
+}
+
+declare const SideBar: {
+  readonly open: (id: string) => Promise<void>
+}
+
+declare const Search: {
+  readonly setValue: (value: string) => Promise<void>
 }
 
 declare const Workspace: {
@@ -31,14 +74,16 @@ declare const expect: (locator: any) => {
   readonly toBeVisible: () => Promise<void>
   readonly toHaveCount: (count: number) => Promise<void>
   readonly toHaveText: (text: string) => Promise<void>
-  readonly toHaveAttribute: (key: string, value: string) => Promise<void>
+  readonly toHaveAttribute: (key: string, value: string | null) => Promise<void>
   readonly toHaveClass: (className: string) => Promise<void>
   readonly toBeFocused: () => Promise<void>
+  readonly toHaveCSS: (key: string, value: string) => Promise<void>
+  readonly toBeHidden: () => Promise<void>
 }
 
 declare const Locator: (selector: string) => any
 
 declare const test: {
-   (name: string, fn: () => Promise<void>) => void
+  (name: string, fn: () => Promise<void>): void
   readonly skip: (name: string, fn: () => Promise<void>) => {}
 }
