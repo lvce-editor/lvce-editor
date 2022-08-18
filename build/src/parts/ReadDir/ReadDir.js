8ae0f2e991517b202b6cbe34e2c96b3adc57cdef
Le Vivilet <levivlet@pm.me>
2022-08-18 23:45:03 +0200

    adjust way of building test files for static build

--- build/src/parts/ReadDir/ReadDir.js
+++ build/src/parts/ReadDir/ReadDir.js
@@ -1,8 +1,19 @@
 import * as fs from 'fs/promises'
+import VError from 'verror'
 import * as Path from '../Path/Path.js'
 
+/**
+ *
+ * @param {string} relativePath
+ * @returns
+ */
 export const readDir = async (relativePath) => {
-  const absolutePath = Path.absolute(relativePath)
-  const dirents = await fs.readdir(absolutePath, { withFileTypes: true })
-  return dirents
+  try {
+    const absolutePath = Path.absolute(relativePath)
+    const dirents = await fs.readdir(absolutePath, { withFileTypes: true })
+    return dirents
+  } catch (error) {
+    // @ts-ignore
+    throw new VError(error, `Failed to read directory ${relativePath}`)
+  }
 }
