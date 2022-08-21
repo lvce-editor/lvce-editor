8c73dd64153915cbb062069bc32214981931ed00
Le Vivilet <levivlet@pm.me>
2022-08-21 17:10:41 +0200

    refactor languages module

--- packages/renderer-worker/src/parts/Languages/Languages.js
+++ packages/renderer-worker/src/parts/Languages/Languages.js
@@ -5,44 +5,31 @@ import * as ExtensionHostLanguages from '../ExtensionHost/ExtensionHostLanguages
 import * as GlobalEventBus from '../GlobalEventBus/GlobalEventBus.js'
 
 export const state = {
-  /**
-   * @type {any[]}
-   */
-  languages: [],
   loadState: false,
   isHydrating: false,
+  fileNameMap: Object.create(null),
+  extensionMap: Object.create(null),
+  tokenizerMap: Object.create(null),
 }
 
 export const getLanguageId = (fileName) => {
   Assert.string(fileName)
+  // TODO this is inefficient for icon theme, as file extension is computed twice
   const extension = fileName.slice(fileName.lastIndexOf('.'))
-  // console.log(state.languages)
-  const { languages } = state
-  for (const language of languages) {
-    if (language.extensions && language.extensions.includes(extension)) {
-      return language.id
-    }
+  const fileNameLower = fileName.toLowerCase()
+  const { extensionMap, fileNameMap } = state
+  if (extensionMap[extension]) {
+    return extensionMap[extension]
   }
-  for (const language of languages) {
-    if (
-      language.fileNames &&
-      Array.isArray(language.fileNames) &&
-      language.fileNames.includes(fileName)
-    ) {
-      return language.id
-    }
+  if (fileName[fileNameLower]) {
+    return fileNameMap[fileNameLower]
   }
   return 'unknown'
 }
 
 export const getTokenizeFunctionPath = (languageId) => {
-  for (const language of state.languages) {
-    if (language.id === languageId && language.tokenize) {
-      // TODO what if language.tokenize is not of type string? -> handle error gracefully
-      return language.tokenize
-    }
-  }
-  return ''
+  // TODO what if language.tokenize is not of type string? -> handle error gracefully
+  return state.tokenizerMap[languageId] || ''
 }
 
 const getLanguages = () => {
@@ -61,16 +48,85 @@ export const hydrate = async () => {
   state.loaded = true
 }
 
-export const addLanguages = async (languages) => {
-  for (const language of languages) {
-    if (language.id && language.fileExtensions) {
+// TODO make contribution points not rely on globals and side effects
+const contributionPointFileExtensions = {
+  key: 'fileExtensions',
+  handle(value, languageId) {
+    if (value) {
       console.warn(
         `[renderer-worker] unsupported property "fileExtensions" for language ${language.id}, use the property "extensions" instead`
       )
     }
+  },
+}
+
+const contributionPointFileNames = {
+  key: 'fileNames',
+  handle(value, languageId) {
+    if (value && Array.isArray(value)) {
+      for (const fileName of value) {
+        if (typeof fileName === 'string') {
+          const fileNameLower = fileName.toLowerCase()
+          state.fileNameMap[fileNameLower] = languageId
+        } else {
+          console.warn(
+            `[renderer-worker] language.fileNames for ${languageId} should be an array of strings but includes ${typeof fileName}`
+          )
+        }
+      }
+      state.fileNameMap
+    }
+  },
+}
+
+const contributionPointExtensions = {
+  key: 'extensions',
+  handle(value, languageId) {
+    if (value && Array.isArray(value)) {
+      for (const extension of value) {
+        if (typeof extension === 'string') {
+          state.extensionMap[extension] = languageId
+        } else {
+          console.warn(
+            `[renderer-worker] language.extensions for ${languageId} should be an array of strings but includes ${typeof extension}`
+          )
+        }
+      }
+    }
+  },
+}
+
+const contributionPointTokenize = {
+  key: 'tokenize',
+  handle(value, languageId) {
+    state.tokenizerMap[languageId] = value
+  },
+}
+
+const contributionPoints = [
+  contributionPointFileExtensions,
+  contributionPointExtensions,
+  contributionPointFileNames,
+  contributionPointTokenize,
+]
+
+const addLanguage = (language) => {
+  const languageId = language.id
+  if (!languageId) {
+    console.warn(`[renderer-worker] language is missing id`, language)
+    return
   }
-  // @ts-ignore
-  state.languages.push(...languages)
+  // TODO could use object destructuing here
+  for (const contributionPoint of contributionPoints) {
+    const value = language[contributionPoint.key]
+    contributionPoint.handle(value, languageId)
+  }
+}
+
+export const addLanguages = async (languages) => {
+  for (const language of languages) {
+    addLanguage(language)
+  }
   await GlobalEventBus.emitEvent('languages.changed')
 }
 
