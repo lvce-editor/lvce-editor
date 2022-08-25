162dcef777cbfed995ac13259566d61b0489d357
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-08-25 17:00:02 +0200

    refactor: localize settings menu entries (#305)

--- packages/renderer-worker/src/parts/MenuEntries/MenuEntriesSettings.js
+++ packages/renderer-worker/src/parts/MenuEntries/MenuEntriesSettings.js
@@ -1,26 +1,35 @@
+import * as I18nString from '../I18NString/I18NString.js'
+
+export const UiStrings = {
+  Settings: 'Settings',
+  KeyboardShortcuts: 'Keyboard Shortcuts',
+  ColorTheme: 'Color Theme',
+  CheckForUpdates: 'Check For Updates',
+}
+
 export const getMenuEntries = () => {
   return [
     {
       id: 'settings',
-      label: 'Settings',
+      label: I18nString.i18nString(UiStrings.Settings),
       flags: /* None */ 0,
       command: 'Preferences.openSettingsJson',
     },
     {
       id: 'keyboardShortcuts',
-      label: 'Keyboard Shortcuts',
+      label: I18nString.i18nString(UiStrings.KeyboardShortcuts),
       flags: /* None */ 0,
       command: /* TODO */ -1,
     },
     {
       id: 'colorTheme',
-      label: 'Color Theme',
+      label: I18nString.i18nString(UiStrings.ColorTheme),
       flags: /* None */ 0,
       command: 'QuickPick.openColorTheme',
     },
     {
       id: 'checkForUpdates',
-      label: 'Check For Updates',
+      label: I18nString.i18nString(UiStrings.CheckForUpdates),
       flags: /* None */ 0,
       command: /* TODO */ -1,
     },
