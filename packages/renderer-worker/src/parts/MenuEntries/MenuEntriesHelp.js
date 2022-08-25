e42e307cd61612bbf16c7d16f96fdce69cda4260
Le Vivilet <levivlet@pm.me>
2022-08-25 19:20:24 +0200

    add process explorer to help menu entries

--- packages/renderer-worker/src/parts/MenuEntries/MenuEntriesHelp.js
+++ packages/renderer-worker/src/parts/MenuEntries/MenuEntriesHelp.js
@@ -3,21 +3,28 @@ import * as I18nString from '../I18NString/I18NString.js'
 export const UiStrings = {
   About: 'About',
   ToggleDeveloperTools: 'Toggle Developer Tools',
+  OpenProcessExplorer: 'Open Process Explorer',
 }
 
 export const getMenuEntries = () => {
   return [
     {
-      id: 'about',
-      label: I18nString.i18nString(UiStrings.About),
-      flags: /* None */ 0,
-      command: 'Dialog.showAbout',
-    },
-    {
       id: 'toggleDeveloperTools',
       label: I18nString.i18nString(UiStrings.ToggleDeveloperTools),
       flags: /* None */ 0,
       command: 'Developer.toggleDeveloperTools',
+    },
+    {
+      id: 'openProcessExplorer',
+      label: I18nString.i18nString(UiStrings.OpenProcessExplorer),
+      flags: /* None */ 0,
+      command: 'Developer.openProcessExplorer',
+    },
+    {
+      id: 'about',
+      label: I18nString.i18nString(UiStrings.About),
+      flags: /* None */ 0,
+      command: 'Dialog.showAbout',
     },
   ]
 }
