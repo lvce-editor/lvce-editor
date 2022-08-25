d9ee009c9fcaddf6ecb4ebfda3e5bd1772078bbc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-08-25 19:35:10 +0200

    refactor: use menu items flags for tab menu items (#319)

--- packages/renderer-worker/test/MenuEntriesTab.test.js
+++ packages/renderer-worker/test/MenuEntriesTab.test.js
@@ -1,10 +1,11 @@
 import * as MenuEntriesTab from '../src/parts/MenuEntries/MenuEntriesTab.js'
+import * as MenuItemFlags from '../src/parts/MenuItemFlags/MenuItemFlags.js'
 
 test.skip('getMenuEntries', () => {
   const menuEntries = MenuEntriesTab.getMenuEntries()
   expect(menuEntries).toContainEqual({
     command: 'Main.closeFocusedTab',
-    flags: 0,
+    flags: MenuItemFlags.None,
     id: 'tabClose',
     label: 'Close',
   })
