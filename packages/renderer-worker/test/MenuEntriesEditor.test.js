06142891393ef1afe496a2d73f0fbbd29f884736
Le Vivilet <levivlet@pm.me>
2022-08-25 19:30:45 +0200

    refactor: use menu items flags for editor menu items

--- packages/renderer-worker/test/MenuEntriesEditor.test.js
+++ packages/renderer-worker/test/MenuEntriesEditor.test.js
@@ -1,22 +1,23 @@
 import * as MenuEntriesEditor from '../src/parts/MenuEntries/MenuEntriesEditor.js'
+import * as MenuItemFlags from '../src/parts/MenuItemFlags/MenuItemFlags.js'
 
 test('getMenuEntries', () => {
   const menuEntries = MenuEntriesEditor.getMenuEntries()
   expect(menuEntries).toContainEqual({
     command: 'Editor.cut',
-    flags: 0,
+    flags: MenuItemFlags.None,
     id: 'cut',
     label: 'Cut',
   })
   expect(menuEntries).toContainEqual({
     command: 'Editor.copy',
-    flags: 0,
+    flags: MenuItemFlags.None,
     id: 'copy',
     label: 'Copy',
   })
   expect(menuEntries).toContainEqual({
     command: 'Editor.paste',
-    flags: 0,
+    flags: MenuItemFlags.None,
     id: 'paste',
     label: 'Paste',
   })
