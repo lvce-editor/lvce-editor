8d4420d31cebc1fd9b0ddd93f3f4f1c04ef75e8f
Le Vivilet <levivlet@pm.me>
2022-08-25 15:32:58 +0200

    localize help menu entries

--- packages/renderer-worker/src/parts/MenuEntries/MenuEntriesHelp.js
+++ packages/renderer-worker/src/parts/MenuEntries/MenuEntriesHelp.js
@@ -1,14 +1,21 @@
+import * as I18nString from '../I18NString/I18NString.js'
+
+export const UiStrings = {
+  About: 'About',
+  ToggleDeveloperTools: 'Toggle Developer Tools',
+}
+
 export const getMenuEntries = () => {
   return [
     {
       id: 'about',
-      label: 'About',
+      label: I18nString.i18nString(UiStrings.About),
       flags: /* None */ 0,
       command: 'Dialog.showAbout',
     },
     {
       id: 'toggleDeveloperTools',
-      label: 'Toggle Developer Tools',
+      label: I18nString.i18nString(UiStrings.ToggleDeveloperTools),
       flags: /* None */ 0,
       command: 'Developer.toggleDeveloperTools',
     },
