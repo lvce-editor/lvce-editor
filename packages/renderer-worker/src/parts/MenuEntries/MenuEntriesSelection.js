667def198f441a7958f7f1205a7de8b33159ff98
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-08-25 19:26:38 +0200

    add more entries for selection context menu (#314)

--- packages/renderer-worker/src/parts/MenuEntries/MenuEntriesSelection.js
+++ packages/renderer-worker/src/parts/MenuEntries/MenuEntriesSelection.js
@@ -2,6 +2,11 @@ import * as I18nString from '../I18NString/I18NString.js'
 
 export const UiStrings = {
   SelectAll: 'Select All',
+  CopyLineUp: 'Copy Line Up',
+  CopyLineDown: 'Copy Line Down',
+  MoveLineUp: 'Move Line Up',
+  MoveLineDown: 'Move Line Down',
+  DuplicateSelection: 'Duplicate Selection',
 }
 
 export const getMenuEntries = () => {
@@ -11,6 +16,30 @@ export const getMenuEntries = () => {
       label: I18nString.i18nString(UiStrings.SelectAll),
       flags: /* None */ 0,
       command: 'Editor.selectAll',
+    },
+    {
+      id: 'copyLineUp',
+      label: I18nString.i18nString(UiStrings.CopyLineUp),
+      flags: /* None */ 0,
+      command: 'Editor.copyLineUp',
+    },
+    {
+      id: 'copyLineDown',
+      label: I18nString.i18nString(UiStrings.CopyLineDown),
+      flags: /* None */ 0,
+      command: 'Editor.copyLineDown',
+    },
+    {
+      id: 'moveLineUp',
+      label: I18nString.i18nString(UiStrings.MoveLineUp),
+      flags: /* Disabled */ 5,
+      command: 'Editor.moveLineUp',
+    },
+    {
+      id: 'moveLineDown',
+      label: I18nString.i18nString(UiStrings.MoveLineDown),
+      flags: /* Disabled */ 5,
+      command: 'Editor.moveLineDown',
     },
   ]
 }
