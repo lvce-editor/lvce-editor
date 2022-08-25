5e6229063f17b54be35d7693f5739cc7aaa187ef
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-08-25 19:41:53 +0200

    refactor: use menu items flags for file menu items (#323)

--- packages/renderer-worker/src/parts/MenuEntries/MenuEntriesFile.js
+++ packages/renderer-worker/src/parts/MenuEntries/MenuEntriesFile.js
@@ -1,4 +1,5 @@
 import * as I18nString from '../I18NString/I18NString.js'
+import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
 
 export const UiStrings = {
   NewFile: 'New File',
@@ -15,49 +16,49 @@ export const getMenuEntries = () => {
     {
       id: 'newFile',
       label: I18nString.i18nString(UiStrings.NewFile),
-      flags: /* None */ 0,
+      flags: MenuItemFlags.None,
       command: -1,
     },
     {
       id: 'newWindow',
       label: I18nString.i18nString(UiStrings.NewWindow),
-      flags: /* None */ 0,
+      flags: MenuItemFlags.None,
       command: /* Window.openNew */ 'Window.openNew',
     },
     {
       id: 'separator',
       label: I18nString.i18nString(UiStrings.Separator),
       flags: /* Separator */ 1,
-      command: /* None */ 0,
+      command: MenuItemFlags.None,
     },
     {
       id: 'openFile',
       label: I18nString.i18nString(UiStrings.OpenFile),
-      flags: /* None */ 0,
+      flags: MenuItemFlags.None,
       command: 'Dialog.openFile',
     },
     {
       id: 'openFolder',
       label: I18nString.i18nString(UiStrings.OpenFolder),
-      flags: /* None */ 0,
+      flags: MenuItemFlags.None,
       command: 'Dialog.openFolder',
     },
     {
       id: 'openRecent',
       label: I18nString.i18nString(UiStrings.OpenRecent),
-      flags: /* SubMenu */ 4,
-      command: /* None */ 0,
+      flags: MenuItemFlags.SubMenu,
+      command: MenuItemFlags.None,
     },
     {
       id: 'separator',
       label: I18nString.i18nString(UiStrings.Separator),
-      flags: /* Separator */ 1,
-      command: /* None */ 0,
+      flags: MenuItemFlags.Separator,
+      command: MenuItemFlags.None,
     },
     {
       id: 'exit',
       label: I18nString.i18nString(UiStrings.Exit),
-      flags: /* None */ 0,
+      flags: MenuItemFlags.None,
       command: /* Window.exit */ 'Window.exit',
     },
   ]
