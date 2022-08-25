8c64a1678cc92a9410863bdaee1b492386cfe8f2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-08-25 19:55:41 +0200

    refactor: use menu items flags for edit menu items (#316)

--- packages/renderer-worker/src/parts/MenuEntries/MenuEntriesEdit.js
+++ packages/renderer-worker/src/parts/MenuEntries/MenuEntriesEdit.js
@@ -1,4 +1,5 @@
 import * as I18nString from '../I18NString/I18NString.js'
+import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
 
 export const UiStrings = {
   Undo: 'Undo',
@@ -16,55 +17,55 @@ export const getMenuEntries = () => {
     {
       id: 'undo',
       label: I18nString.i18nString(UiStrings.Undo),
-      flags: /* Disabled */ 5,
+      flags: MenuItemFlags.Disabled,
       command: /* TODO */ -1,
     },
     {
       id: 'redo',
       label: I18nString.i18nString(UiStrings.Redo),
-      flags: /* Disabled */ 5,
+      flags: MenuItemFlags.Disabled,
       command: /* TODO */ -1,
     },
     {
       id: 'separator',
       label: I18nString.i18nString(UiStrings.Separator),
-      flags: /* separator */ 1,
+      flags: MenuItemFlags.Separator,
       command: /* TODO */ -1,
     },
     {
       id: 'cut',
       label: I18nString.i18nString(UiStrings.Cut),
-      flags: /* None */ 0,
+      flags: MenuItemFlags.None,
       command: /* Editor.cut */ 'Editor.cut',
     },
     {
       id: 'copy',
       label: I18nString.i18nString(UiStrings.Copy),
-      flags: /* None */ 0,
+      flags: MenuItemFlags.None,
       command: /* Editor.copy */ 'Editor.copy',
     },
     {
       id: 'paste',
       label: I18nString.i18nString(UiStrings.Paste),
-      flags: /* None */ 0,
+      flags: MenuItemFlags.None,
       command: /* Editor.paste */ 'Editor.paste',
     },
     {
       id: 'separator',
       label: I18nString.i18nString(UiStrings.Separator),
-      flags: /* separator */ 1,
+      flags: MenuItemFlags.None,
       command: /* TODO */ -1,
     },
     {
       id: 'toggle-line-comment',
       label: I18nString.i18nString(UiStrings.ToggleLineComment),
-      flags: /* None */ 0,
+      flags: MenuItemFlags.None,
       command: /* Editor.toggleLineComment */ 'Editor.toggleLineComment',
     },
     {
       id: 'toggle-block-comment',
       label: I18nString.i18nString(UiStrings.ToggleBlockComment),
-      flags: /* None */ 0,
+      flags: MenuItemFlags.None,
       command: /* Editor.toggleBlockComment */ 'Editor.toggleBlockComment',
     },
   ]
--- packages/renderer-worker/test/MenuEntriesEdit.test.js
+++ packages/renderer-worker/test/MenuEntriesEdit.test.js
@@ -1,22 +1,23 @@
 import * as MenuEntriesEdit from '../src/parts/MenuEntries/MenuEntriesEdit.js'
+import * as MenuItemFlags from '../src/parts/MenuItemFlags/MenuItemFlags.js'
 
 test('getMenuEntries', () => {
   const menuEntries = MenuEntriesEdit.getMenuEntries()
   expect(menuEntries).toContainEqual({
     command: 'Editor.cut',
-    flags: 0,
+    flags: MenuItemFlags.None,
     id: 'cut',
     label: 'Cut',
   })
   expect(menuEntries).toContainEqual({
     command: 'Editor.copy',
-    flags: 0,
+    flags: MenuItemFlags.None,
     id: 'copy',
     label: 'Copy',
   })
   expect(menuEntries).toContainEqual({
     command: 'Editor.paste',
-    flags: 0,
+    flags: MenuItemFlags.None,
     id: 'paste',
     label: 'Paste',
   })
