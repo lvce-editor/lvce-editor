069d05cd11a9af5f6c48d94241ecc0bf7895cc15
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-01 19:07:12 +0200

    fix: hide menu focus indicator on mouseleave (#334)

--- packages/renderer-worker/src/parts/Menu/Menu.ipc.js
+++ packages/renderer-worker/src/parts/Menu/Menu.ipc.js
@@ -1,14 +1,15 @@
 import * as Menu from './Menu.js'
 
 export const Commands = {
-  'Menu.show': Menu.show,
-  'Menu.hide': Menu.hide,
-  'Menu.selectIndex': Menu.selectIndex,
+  'Menu.focusFirst': Menu.focusFirst,
+  'Menu.focusIndex': Menu.focusIndex,
+  'Menu.focusLast': Menu.focusLast,
   'Menu.focusNext': Menu.focusNext,
   'Menu.focusPrevious': Menu.focusPrevious,
-  'Menu.focusFirst': Menu.focusFirst,
-  'Menu.focusLast': Menu.focusLast,
-  'Menu.focusIndex': Menu.focusIndex,
   'Menu.handleMouseEnter': Menu.handleMouseEnter,
+  'Menu.handleMouseLeave': Menu.handleMouseLeave,
+  'Menu.hide': Menu.hide,
+  'Menu.selectIndex': Menu.selectIndex,
   'Menu.selectItem': Menu.selectItem,
+  'Menu.show': Menu.show,
 }
