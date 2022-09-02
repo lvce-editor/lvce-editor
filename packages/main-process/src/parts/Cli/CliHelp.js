85b0d2819c00cfecaa92ead4136fbc620cf8817e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-02 18:35:22 +0200

    fix: make help cli output more useful (#351)

--- packages/main-process/src/parts/Cli/CliHelp.js
+++ packages/main-process/src/parts/Cli/CliHelp.js
@@ -1,7 +1,17 @@
 const Electron = require('electron')
+const Platform = require('../Platform/Platform.js')
+
+const getHelpString = () => {
+  return `${Platform.ProductName} v${Platform.version}
+
+Usage:
+  ${Platform.applicationName} [path]
+`
+}
 
 const handleCliArgs = (parsedArgs) => {
-  console.info('TODO print help')
+  const helpString = getHelpString()
+  console.info(helpString)
   Electron.app.exit(0)
   return true
 }
