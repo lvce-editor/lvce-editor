ab5cee2121bbf9991942a038fdf710bfe5cf3dde
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-02 18:14:37 +0200

    make cli version output more useful by including electron, chrome and… (#349)

--- packages/main-process/src/parts/Cli/CliVersion.js
+++ packages/main-process/src/parts/Cli/CliVersion.js
@@ -1,11 +1,49 @@
 const Electron = require('electron')
 const Platform = require('../Platform/Platform.js')
 
+const getName = (object) => {
+  return object.name
+}
+
+const getVersions = () => {
+  return [
+    {
+      name: Platform.ProductName,
+      version: Platform.version,
+    },
+    {
+      name: 'Electron',
+      version: process.versions.electron,
+    },
+    {
+      name: 'Chrome',
+      version: process.versions.chrome,
+    },
+    {
+      name: 'Node',
+      version: process.versions.node,
+    },
+  ]
+}
+const getLength = (string) => {
+  return string.length
+}
+
+const getVersionString = () => {
+  const versions = getVersions()
+  const names = versions.map(getName)
+  const lengths = names.map(getLength)
+  const longestLength = Math.max(...lengths)
+  const lines = []
+  for (const version of versions) {
+    lines.push(version.name.padEnd(longestLength, ' ') + ': ' + version.version)
+  }
+  return lines.join('\n')
+}
+
 const handleCliArgs = (parsedArgs) => {
-  const version = Platform.version
-  const commit = Platform.commit
-  console.info(`${version}
-${commit}`)
+  const versionString = getVersionString()
+  console.info(versionString)
   Electron.app.exit(0)
   return true
 }
--- packages/main-process/test/CliVersion.test.js
+++ packages/main-process/test/CliVersion.test.js
@@ -10,12 +10,33 @@ jest.mock('electron', () => {
   }
 })
 
+jest.mock('../src/parts/Platform/Platform.js', () => {
+  return {
+    ProductName: 'Test',
+    version: '0.0.0-dev',
+  }
+})
+
 const CliVersion = require('../src/parts/Cli/CliVersion.js')
 
 test('handleCliArgs', async () => {
-  const spy = jest.spyOn(console, 'info')
+  const spy = jest.spyOn(console, 'info').mockImplementation()
+  const mockVersions = {
+    electron: '0.0.0-dev',
+    chrome: '0.0.0-dev',
+    node: '0.0.0-dev',
+  }
+  globalThis.process = {
+    // @ts-ignore
+    versions: mockVersions,
+  }
   expect(CliVersion.handleCliArgs({})).toBe(true)
   expect(spy).toHaveBeenCalledTimes(1)
-  expect(spy).toHaveBeenCalledWith(`0.0.0-dev
-unknown commit`)
+  expect(spy).toHaveBeenCalledWith(
+    `
+Test    : 0.0.0-dev
+Electron: 0.0.0-dev
+Chrome  : 0.0.0-dev
+Node    : 0.0.0-dev`.trim()
+  )
 })
