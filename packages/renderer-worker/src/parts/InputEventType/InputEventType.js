84efb072d0d9b5fc1faea1d87d77e18d4cd03a2a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-02 17:44:57 +0200

    refactor: add input event type enum (#347)

---
+++ packages/renderer-worker/src/parts/InputEventType/InputEventType.js
@@ -0,0 +1,9 @@
+export const InsertText = 'insertText'
+
+export const DeleteContentBackward = 'deleteContentBackward'
+
+export const DeleteContentForward = 'deleteContentForward'
+
+export const DeleteWordForward = 'deleteWordForward'
+
+export const DeleteWordBackward = 'deleteWordBackward'
