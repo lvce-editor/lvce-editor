97b77ba4675fdab64df2fb836b9baef811c71121
Le Vivilet <levivlet@pm.me>
2022-09-06 09:05:38 +0200

    fix index computation when index is out of bounds

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.js
@@ -797,6 +797,9 @@ export const handleClick = async (state, index) => {
 const getIndexFromPosition = (state, x, y) => {
   const { top, itemHeight, dirents } = state
   const index = Math.floor((y - top) / itemHeight)
+  if (index < 0) {
+    return 0
+  }
   if (index > dirents.length) {
     return -1
   }
