385d705490622fbd02b55b3e5576ef26e7a72ad9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-07 23:19:38 +0200

    refactor: extract main process modules into separate file (#417)

---
+++ packages/main-process/src/parts/Module/Module.js
@@ -0,0 +1,24 @@
+const ModuleId = require('../ModuleId/ModuleId.js')
+
+exports.load = async (moduleId) => {
+  switch (moduleId) {
+    case ModuleId.Window:
+      return require('../ElectronWindow/ElectronWindow.ipc.js')
+    case ModuleId.ElectronWindowAbout:
+      return require('../ElectronWindowAbout/ElectronWindowAbout.ipc.js')
+    case ModuleId.Dialog:
+      return require('../ElectronDialog/ElectronDialog.ipc.js')
+    case ModuleId.Developer:
+      return require('../ElectronDeveloper/ElectronDeveloper.ipc.js')
+    case ModuleId.Beep:
+      return require('../ElectronBeep/ElectronBeep.js')
+    case ModuleId.AppWindow:
+      return require('../AppWindow/AppWindow.ipc.js')
+    case ModuleId.App:
+      return require('../App/App.ipc.js')
+    case ModuleId.ElectronWindowProcessExplorer:
+      return require('../ElectronWindowProcessExplorer/ElectronWindowProcessExplorer.ipc.js')
+    default:
+      throw new Error('unknown module')
+  }
+}
