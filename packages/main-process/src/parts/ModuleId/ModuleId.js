e983c2c9aba159f48804f266383aa3e42470ef2d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-07 22:48:31 +0200

    refactor: use module id enum in main process (#411)

---
+++ packages/main-process/src/parts/ModuleId/ModuleId.js
@@ -0,0 +1,8 @@
+exports.Window = 1
+exports.ElectronWindowAbout = 2
+exports.Dialog = 3
+exports.Developer = 4
+exports.Beep = 5
+exports.AppWindow = 6
+exports.App = 7
+exports.ElectronWindowProcessExplorer = 8
