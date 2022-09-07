873429b209a5897defb4655df1124f7fb424b587
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-07 23:47:41 +0200

    refactor: extract module map in main process (#423)

--- packages/main-process/src/parts/Command/Command.js
+++ packages/main-process/src/parts/Command/Command.js
@@ -1,8 +1,7 @@
-const ModuleId = require('../ModuleId/ModuleId.js')
 const Module = require('../Module/Module.js')
+const ModuleMap = require('../ModuleMap/ModuleMap.js')
 
 const commands = Object.create(null)
-
 const pendingModules = Object.create(null)
 
 const initializeModule = (module) => {
@@ -23,39 +22,7 @@ const getOrLoadModule = (moduleId) => {
   return pendingModules[moduleId]
 }
 
-const getModuleId = (commandId) => {
-  switch (commandId) {
-    case 'ElectronApp.exit':
-    case 'App.exit':
-      return ModuleId.App
-    case 'ElectronWindow.minimize':
-    case 'ElectronWindow.maximize':
-    case 'ElectronWindow.toggleDevtools':
-    case 'ElectronWindow.toggleDevtools':
-    case 'ElectronWindow.unmaximize':
-    case 'ElectronWindow.close':
-    case 'ElectronWindow.reload':
-      return ModuleId.Window
-    case 'ElectronDeveloper.getPerformanceEntries':
-    case 'ElectronDeveloper.crashMainProcess':
-      return ModuleId.Developer
-    case 'AppWindow.createAppWindow':
-      return ModuleId.AppWindow
-    case 'ElectronWindowProcessExplorer.open':
-      return ModuleId.ElectronWindowProcessExplorer
-    case 'ElectronWindowAbout.open':
-      return ModuleId.ElectronWindowAbout
-    case 'ElectronDialog.showOpenDialog':
-    case 'ElectronDialog.showMessageBox':
-      return ModuleId.Dialog
-    case 'ElectronBeep.beep':
-      return ModuleId.Beep
-    default:
-      throw new Error(`method not found ${commandId}`)
-  }
-}
-
-const loadCommand = (command) => getOrLoadModule(getModuleId(command))
+const loadCommand = (command) => getOrLoadModule(ModuleMap.getModuleId(command))
 
 const register = (exports.register = (commandId, listener) => {
   commands[commandId] = listener
