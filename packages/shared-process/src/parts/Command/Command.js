ceadfb88100c8d7babaf8d9f64dd8f1715053d44
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-07 23:18:11 +0200

    refactor: remove shared process modules from commands (#416)

--- packages/shared-process/src/parts/Command/Command.js
+++ packages/shared-process/src/parts/Command/Command.js
@@ -1,47 +1,9 @@
 import * as ModuleId from '../ModuleId/ModuleId.js'
+import * as Module from '../Module/Module.js'
 
 const commands = Object.create(null)
 const invocables = Object.create(null)
 const pendingModules = Object.create(null)
-
-const loadModule = (moduleId) => {
-  switch (moduleId) {
-    case ModuleId.FileSystem:
-      return import('../FileSystem/FileSystem.ipc.js')
-    case ModuleId.Workspace:
-      return import('../Workspace/Workspace.ipc.js')
-    case ModuleId.Terminal:
-      return import('../Terminal/Terminal.ipc.js')
-    case ModuleId.Search:
-      return import('../Search/Search.ipc.js')
-    case ModuleId.SearchFile:
-      return import('../SearchFile/SearchFile.ipc.js')
-    case ModuleId.OutputChannel:
-      return import('../OutputChannel/OutputChannel.ipc.js')
-    case ModuleId.Developer:
-      return import('../Developer/Developer.ipc.js')
-    case ModuleId.ExtensionManagement:
-      return import('../ExtensionManagement/ExtensionManagement.ipc.js')
-    case ModuleId.ExtensionHost:
-      return import('../ExtensionHost/ExtensionHost.ipc.js')
-    case ModuleId.Preferences:
-      return import('../Preferences/Preferences.ipc.js')
-    case ModuleId.Native:
-      return import('../Native/Native.ipc.js')
-    case ModuleId.ClipBoard:
-      return import('../ClipBoard/ClipBoard.ipc.js')
-    case ModuleId.TextDocument:
-      return import('../TextDocument/TextDocument.ipc.js')
-    case ModuleId.WebSocketServer:
-      return import('../WebSocketServer/WebSocketServer.ipc.js')
-    case ModuleId.Platform:
-      return import('../Platform/Platform.ipc.js')
-    case ModuleId.RecentlyOpened:
-      return import('../RecentlyOpened/RecentlyOpened.ipc.js')
-    default:
-      throw new Error(`unknown module ${moduleId}`)
-  }
-}
 
 const initializeModule = (module) => {
   if (module.Commands) {
@@ -55,7 +17,7 @@ const initializeModule = (module) => {
 
 const getOrLoadModule = (moduleId) => {
   if (!pendingModules[moduleId]) {
-    const importPromise = loadModule(moduleId)
+    const importPromise = Module.load(moduleId)
     pendingModules[moduleId] = importPromise
       .then(initializeModule)
       .catch((error) => {
