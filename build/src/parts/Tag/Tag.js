6506c8ff1025fc79168d3dfccffc90a5835d61d4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-08 15:44:34 +0200

    refactor: use more exact version when no tag exists for commit, use v… (#447)

--- build/src/parts/Tag/Tag.js
+++ build/src/parts/Tag/Tag.js
@@ -1,28 +1,63 @@
 import * as Exec from '../Exec/Exec.js'
 
+const getGitTagFromGitFallback = async () => {
+  const { stdout, stderr, exitCode } = await Exec.exec(
+    'git',
+    ['describe', '--tags'],
+    {
+      reject: false,
+    }
+  )
+  if (exitCode) {
+    if (
+      exitCode === 128 &&
+      stderr.startsWith('fatal: no tag exactly matches')
+    ) {
+      return '0.0.0-dev'
+    }
+    return '0.0.0-dev'
+  }
+  if (stdout.startsWith('v')) {
+    return stdout.slice(1)
+  }
+  return stdout
+}
+
+const getGitTagFromGit = async () => {
+  const { stdout, stderr, exitCode } = await Exec.exec(
+    'git',
+    ['describe', '--exact-match', '--tags'],
+    {
+      reject: false,
+    }
+  )
+  if (exitCode) {
+    if (
+      exitCode === 128 &&
+      stderr.startsWith('fatal: no tag exactly matches')
+    ) {
+      return getGitTagFromGitFallback()
+    }
+    return '0.0.0-dev'
+  }
+  if (stdout.startsWith('v')) {
+    return stdout.slice(1)
+  }
+  return stdout
+}
+
 export const getGitTag = async () => {
-  try {
-    if (process.env.RG_VERSION) {
-      if (process.env.RG_VERSION.startsWith('v')) {
-        return process.env.RG_VERSION.slice(1)
-      }
-      return process.env.RG_VERSION
+  if (process.env.RG_VERSION) {
+    if (process.env.RG_VERSION.startsWith('v')) {
+      return process.env.RG_VERSION.slice(1)
     }
-    if (process.env.GIT_TAG) {
-      if (process.env.GIT_TAG.startsWith('v')) {
-        return process.env.GIT_TAG.slice(1)
-      }
-      return process.env.GIT_TAG
+    return process.env.RG_VERSION
+  }
+  if (process.env.GIT_TAG) {
+    if (process.env.GIT_TAG.startsWith('v')) {
+      return process.env.GIT_TAG.slice(1)
     }
-    const { stdout } = await Exec.exec('git', [
-      'describe',
-      '--exact-match',
-      '--tags',
-    ])
-    if (stdout.startsWith('v')) {
-      return stdout.slice(1)
-    }
-    return stdout
-  } catch {}
-  return '0.0.0-dev'
+    return process.env.GIT_TAG
+  }
+  return getGitTagFromGit()
 }
