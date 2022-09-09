dddf5a47c439feb3e58f5618758f87d947c9ad00
Le Vivilet <levivlet@pm.me>
2022-09-09 18:49:47 +0200

    refactor: don't set extra focused class for focused tree item

--- packages/renderer-process/src/parts/ViewletExplorer/ViewletExplorer.js
+++ packages/renderer-process/src/parts/ViewletExplorer/ViewletExplorer.js
@@ -359,7 +359,6 @@ export const setFocusedIndex = (state, oldIndex, newIndex) => {
     default:
       const $Dirent = $Viewlet.children[oldIndex]
       if ($Dirent) {
-        $Dirent.classList.remove('FocusOutline')
         $Dirent.removeAttribute('id')
       }
       break
@@ -376,7 +375,6 @@ export const setFocusedIndex = (state, oldIndex, newIndex) => {
       break
     default:
       const $Dirent = $Viewlet.children[newIndex]
-      $Dirent.classList.add('FocusOutline')
       $Dirent.id = activeId
       $Viewlet.focus()
       $Viewlet.setAttribute('aria-activedescendant', activeId)
