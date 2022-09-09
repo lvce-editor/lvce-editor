71f67551c89ce087cdaa5ea325bd838beab62090
Le Vivilet <levivlet@pm.me>
2022-09-09 12:50:55 +0200

    implement electron shell module in renderer worker

--- packages/renderer-worker/src/parts/ElectronShell/ElectronShell.js
+++ packages/renderer-worker/src/parts/ElectronShell/ElectronShell.js
@@ -1,1 +1,9 @@
-export const showItemInFolder = (fullPath) => {}
+import * as ElectronProcess from '../ElectronProcess/ElectronProcess.js'
+
+export const showItemInFolder = async (fullPath) => {
+  await ElectronProcess.invoke('ElectronShell.showItemInFolder', fullPath)
+}
+
+export const beep = async () => {
+  await ElectronProcess.invoke('ElectronShell.beep')
+}
