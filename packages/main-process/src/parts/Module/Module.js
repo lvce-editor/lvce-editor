fdea605a3e52d5631ba35054e8991d407a861cbc
Le Vivilet <levivlet@pm.me>
2022-09-10 12:13:54 +0200

    refactor: sort module files

--- packages/main-process/src/parts/Module/Module.js
+++ packages/main-process/src/parts/Module/Module.js
@@ -8,27 +8,27 @@ exports.load = async (moduleId) => {
       return require('../AppWindow/AppWindow.ipc.js')
     case ModuleId.Beep:
       return require('../ElectronBeep/ElectronBeep.js')
+    case ModuleId.ElectronContentTracing:
+      return require('../ElectronContentTracing/ElectronContentTracing.ipc.js')
     case ModuleId.Developer:
       return require('../ElectronDeveloper/ElectronDeveloper.ipc.js')
     case ModuleId.Dialog:
       return require('../ElectronDialog/ElectronDialog.ipc.js')
-    case ModuleId.ElectronWindowAbout:
-      return require('../ElectronWindowAbout/ElectronWindowAbout.ipc.js')
-    case ModuleId.ElectronWindowProcessExplorer:
-      return require('../ElectronWindowProcessExplorer/ElectronWindowProcessExplorer.ipc.js')
-    case ModuleId.Window:
-      return require('../ElectronWindow/ElectronWindow.ipc.js')
-    case ModuleId.ElectronShell:
-      return require('../ElectronShell/ElectronShell.ipc.js')
+    case ModuleId.ElectronNetLog:
+      return require('../ElectronNetLog/ElectronNetLog.ipc.js')
     case ModuleId.ElectronPowerSaveBlocker:
       return require('../ElectronPowerSaveBlocker/ElectronPowerSaveBlocker.ipc.js')
     case ModuleId.ElectronSafeStorage:
       return require('../ElectronSafeStorage/ElectronSafeStorage.ipc.js')
-    case ModuleId.ElectronContentTracing:
-      return require('../ElectronContentTracing/ElectronContentTracing.ipc.js')
-    case ModuleId.ElectronNetLog:
-      return require('../ElectronNetLog/ElectronNetLog.ipc.js')
+    case ModuleId.ElectronShell:
+      return require('../ElectronShell/ElectronShell.ipc.js')
+    case ModuleId.Window:
+      return require('../ElectronWindow/ElectronWindow.ipc.js')
+    case ModuleId.ElectronWindowAbout:
+      return require('../ElectronWindowAbout/ElectronWindowAbout.ipc.js')
+    case ModuleId.ElectronWindowProcessExplorer:
+      return require('../ElectronWindowProcessExplorer/ElectronWindowProcessExplorer.ipc.js')
     default:
-      throw new Error('unknown module')
+      throw new Error(`module ${moduleId} not found`)
   }
 }
