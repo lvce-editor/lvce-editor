5ec7ee200e5f27980eed81af138a3f8305b46906
Le Vivilet <levivlet@pm.me>
2022-09-10 11:34:24 +0200

    refactor: extract renderer process viewlet modules into separate file

---
+++ packages/renderer-process/src/parts/ViewletModuleId/ViewletModuleId.js
@@ -0,0 +1,47 @@
+export const Explorer = 'Explorer'
+
+export const RunAndDebug = 'Run And Debug'
+
+export const Search = 'Search'
+
+export const SourceControl = 'Source Control'
+
+export const Terminal = 'Terminal'
+
+export const Extensions = 'Extensions'
+
+export const DebugConsole = 'Debug Console'
+
+export const Output = 'Output'
+
+export const Problems = 'Problems'
+
+export const Empty = 'Empty'
+
+export const EditorText = 'EditorText'
+
+export const EditorImage = 'EditorImage'
+
+export const Clock = 'Clock'
+
+export const SideBar = 'SideBar'
+
+export const Panel = 'Panel'
+
+export const ActivityBar = 'ActivityBar'
+
+export const ImagePreview = 'ImagePreview'
+
+export const QuickPick = 'QuickPick'
+
+export const StatusBar = 'StatusBar'
+
+export const TitleBar = 'TitleBar'
+
+export const Main = 'Main'
+
+export const EditorCompletion = 'EditorCompletion'
+
+export const References = 'References'
+
+export const Implementations = 'Implementations'
