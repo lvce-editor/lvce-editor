aa1a61e7c771f8706df0b24bbc36842b438b0a20
Le Vivilet <levivlet@pm.me>
2022-09-11 11:51:31 +0200

    pass dirent to drop function

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDropIndex.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDropIndex.js
@@ -1,3 +1,10 @@
 export const handleDropIndex = (state, index, files) => {
+  const { dirents } = state
+  const dirent = dirents[index]
+  // TODO if it is a file, drop into the folder of the file
+  // TODO if it is a folder, drop into the folder
+  // TODO if it is a symlink, read symlink and determine if file can be dropped
+  console.log({ dirent, files })
+  globalThis.file = files[0]
   return state
 }
