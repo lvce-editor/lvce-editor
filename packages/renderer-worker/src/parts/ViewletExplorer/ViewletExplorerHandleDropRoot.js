461ea0fc32fd874cc5c9b947a29d3e57a1476e9b
Le Vivilet <levivlet@pm.me>
2022-09-11 23:04:56 +0200

    remove unused code

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDropRoot.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDropRoot.js
@@ -1,42 +1,8 @@
-import * as DirentType from '../DirentType/DirentType.js'
 import * as FileSystem from '../FileSystem/FileSystem.js'
 import { getChildDirents } from './ViewletExplorerShared.js'
 
 const mergeDirents = (oldDirents, newDirents) => {
-  const mergedDirents = []
-  const newFolders = []
-  for (let i = 0; i < oldDirents.length; i++) {
-    const oldDirent = oldDirents[i]
-    if (oldDirent.type === DirentType.DirectoryExpanded) {
-      let childStartIndex = i
-      let childEndIndex = i + 1
-      while (childEndIndex < oldDirents.length) {
-        if (oldDirents[childEndIndex].depth <= oldDirent.depth) {
-          break
-        }
-        childEndIndex++
-      }
-      console.log({ childStartIndex, childEndIndex })
-      // for (let j = i + 1; j < oldDirents.length; j++) {
-      //   const child = oldDirents[j]
-      //   if (child.depth > oldDirent.depth) {
-      //   }
-      // }
-    }
-  }
-  for (const oldDirent of oldDirents) {
-    if (oldDirent.type === DirentType.DirectoryExpanded) {
-    }
-  }
-  const childMap = Object.create(null)
-  for (const newDirent of newDirents) {
-    // newFolders.push(Workspace.pathDirName(newDirent.path))
-    mergedDirents.push(newDirent)
-  }
-  // for (const oldDirent of oldDirents) {
-  //   const parentPath = Workspace.pathDirName(oldDirent.path)
-  // }
-  return mergedDirents
+  return newDirents
 }
 
 // TODO copy files in parallel
