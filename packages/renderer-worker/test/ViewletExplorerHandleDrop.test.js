0288e8e809605ae835474702074eeee6934ede71
Le Vivilet <levivlet@pm.me>
2022-09-11 11:01:57 +0200

    try to implement dropping file into folder

--- packages/renderer-worker/test/ViewletExplorerHandleDrop.test.js
+++ packages/renderer-worker/test/ViewletExplorerHandleDrop.test.js
@@ -51,7 +51,7 @@ test('handleDrop - single file', async () => {
   expect(
     await ViewletExplorerHandleDrop.handleDrop(state, [
       {
-        lastModified: 1662556917899,
+        lastModified: 0,
         lastModifiedDate: new Date(),
         name: 'file.txt',
         path: '/source/file.txt',
@@ -126,7 +126,7 @@ test('handleDrop - single file - merge with existing files', async () => {
   expect(
     await ViewletExplorerHandleDrop.handleDrop(state, [
       {
-        lastModified: 1662556917899,
+        lastModified: 0,
         lastModifiedDate: new Date(),
         name: 'file-2.txt',
         path: '/source/file-2.txt',
@@ -170,6 +170,106 @@ test('handleDrop - single file - merge with existing files', async () => {
   )
 })
 
+test('handleDrop - single file - into folder', async () => {
+  // @ts-ignore
+  FileSystem.copy.mockImplementation(() => {})
+  // @ts-ignore
+  FileSystem.readDirWithFileTypes.mockImplementation((uri) => {
+    switch (uri) {
+      case '/test':
+        return [
+          {
+            name: 'a',
+            type: DirentType.Directory,
+          },
+        ]
+      case '/test/a':
+        return [
+          {
+            name: 'file-1.txt',
+            type: DirentType.File,
+          },
+          {
+            name: 'file-2.txt',
+            type: DirentType.File,
+          },
+        ]
+      default:
+        throw new Error(`file not found ${uri}`)
+    }
+  })
+  const state = {
+    ...ViewletExplorer.create(),
+    root: '/test',
+    focusedIndex: 1,
+    dirents: [
+      {
+        depth: 1,
+        posInSet: 1,
+        setSize: 2,
+        name: 'a',
+        path: '/test/a',
+        type: DirentType.Directory,
+      },
+      {
+        depth: 2,
+        posInSet: 1,
+        setSize: 1,
+        name: 'file-1.txt',
+        path: '/test/a/file-1.txt',
+        type: DirentType.File,
+      },
+    ],
+    pathSeparator: '/',
+  }
+  expect(
+    await ViewletExplorerHandleDrop.handleDrop(state, [
+      {
+        lastModified: 0,
+        lastModifiedDate: new Date(),
+        name: 'file-2.txt',
+        path: '/source/file-2.txt',
+        size: 4,
+        type: 'text/plain',
+        webkitRelativePath: '',
+      },
+    ])
+  ).toMatchObject({
+    dirents: [
+      {
+        depth: 1,
+        posInSet: 1,
+        setSize: 1,
+        name: 'a',
+        icon: '',
+        path: '/test/a',
+        type: DirentType.Directory,
+      },
+      {
+        depth: 2,
+        posInSet: 1,
+        setSize: 2,
+        name: 'file-1.txt',
+        path: '/test/a/file-1.txt',
+        type: DirentType.File,
+      },
+      {
+        depth: 2,
+        posInSet: 2,
+        setSize: 2,
+        name: 'file-2.txt',
+        path: '/test/a/file-2.txt',
+        type: DirentType.File,
+      },
+    ],
+  })
+  expect(FileSystem.copy).toHaveBeenCalledTimes(1)
+  expect(FileSystem.copy).toHaveBeenCalledWith(
+    '/source/file-2.txt',
+    '/test/file-2.txt'
+  )
+})
+
 test('handleDrop - error', async () => {
   // @ts-ignore
   FileSystem.copy.mockImplementation(() => {
@@ -183,7 +283,7 @@ test('handleDrop - error', async () => {
   await expect(
     ViewletExplorerHandleDrop.handleDrop(state, [
       {
-        lastModified: 1662556917899,
+        lastModified: 0,
         lastModifiedDate: new Date(),
         name: 'file.txt',
         path: '/test/file.txt',
