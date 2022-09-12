6685640e27cfd3d7efdc540711eb2cb650ae888d
Le Vivilet <levivlet@pm.me>
2022-09-12 14:01:34 +0200

    lazy load quick pick commands

--- packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPick.ipc.js
+++ packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPick.ipc.js
@@ -1,13 +1,23 @@
 import * as ViewletQuickPick from './ViewletQuickPick.js'
 import * as Viewlet from '../Viewlet/Viewlet.js'
+import * as LazyCommand from '../LazyCommand/LazyCommand.js'
+
+// prettier-ignore
+const Imports = {
+  FocusFirst: () => import('./ViewletQuickPickFocusFirst.js'),
+  FocusIndex: () => import('./ViewletQuickPickFocusIndex.js'),
+  FocusLast: () => import('./ViewletQuickPickFocusLast.js'),
+  FocusNext: () => import('./ViewletQuickPickFocusNext.js'),
+  FocusPrevious: () => import('./ViewletQuickPickFocusPrevious.js'),
+}
 
 // prettier-ignore
 export const Commands = {
-  'QuickPick.focusFirst': Viewlet.wrapViewletCommand('QuickPick', ViewletQuickPick.focusFirst),
-  'QuickPick.focusIndex': Viewlet.wrapViewletCommand('QuickPick', ViewletQuickPick.focusIndex),
-  'QuickPick.focusLast': Viewlet.wrapViewletCommand('QuickPick', ViewletQuickPick.focusLast),
-  'QuickPick.focusNext': Viewlet.wrapViewletCommand('QuickPick', ViewletQuickPick.focusNext),
-  'QuickPick.focusPrevious': Viewlet.wrapViewletCommand('QuickPick', ViewletQuickPick.focusPrevious),
+  'QuickPick.focusFirst': LazyCommand.create('QuickPick', Imports.FocusFirst, 'focusFirst'),
+  'QuickPick.focusIndex': LazyCommand.create('QuickPick', Imports.FocusIndex, 'focusIndex'),
+  'QuickPick.focusLast': LazyCommand.create('QuickPick', Imports.FocusLast, 'focusLast'),
+  'QuickPick.focusNext': LazyCommand.create('QuickPick', Imports.FocusNext, 'focusNext'),
+  'QuickPick.focusPrevious': LazyCommand.create('QuickPick', Imports.FocusPrevious, 'focusPrevious'),
   'QuickPick.handleBeforeInput': Viewlet.wrapViewletCommand('QuickPick', ViewletQuickPick.handleBeforeInput),
   'QuickPick.handleBlur': Viewlet.wrapViewletCommand('QuickPick', ViewletQuickPick.handleBlur),
   'QuickPick.handleInput': Viewlet.wrapViewletCommand('QuickPick', ViewletQuickPick.handleInput),
