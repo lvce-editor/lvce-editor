2f665cab2975982fd99d2930b1dd55ea59c6ff50
Le Vivilet <levivlet@pm.me>
2022-09-15 15:20:02 +0200

    add tests for keybindings editor

--- packages/renderer-worker/test/ViewletKeyBindings.test.js
+++ packages/renderer-worker/test/ViewletKeyBindings.test.js
@@ -1,4 +1,26 @@
-import * as ViewletKeyBindings from '../src/parts/ViewletKeyBindings/ViewletKeyBindings.js'
+import { jest } from '@jest/globals'
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule(
+  '../src/parts/KeyBindingsInitial/KeyBindingsInitial.js',
+  () => {
+    return {
+      getKeyBindings: jest.fn(() => {
+        throw new Error('not implemented')
+      }),
+    }
+  }
+)
+
+const ViewletKeyBindings = await import(
+  '../src/parts/ViewletKeyBindings/ViewletKeyBindings.js'
+)
+const KeyBindingsInitial = await import(
+  '../src/parts/KeyBindingsInitial/KeyBindingsInitial.js'
+)
 
 test('name', () => {
   expect(ViewletKeyBindings.name).toBe('KeyBindings')
@@ -6,4 +28,27 @@ test('name', () => {
 
 test('create', () => {
   expect(ViewletKeyBindings.create()).toBeDefined()
+})
+
+test('loadContent', async () => {
+  // @ts-ignore
+  KeyBindingsInitial.getKeyBindings.mockImplementation(() => {
+    return [
+      {
+        key: 'Enter',
+        command: 'EditorCompletion.selectCurrent',
+        when: 'focus.editorCompletions',
+      },
+    ]
+  })
+  const state = ViewletKeyBindings.create()
+  expect(await ViewletKeyBindings.loadContent(state)).toMatchObject({
+    keyBindings: [
+      {
+        key: 'Enter',
+        command: 'EditorCompletion.selectCurrent',
+        when: 'focus.editorCompletions',
+      },
+    ],
+  })
 })
