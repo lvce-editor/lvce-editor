177c8ca9ccfe27ce71a68e6aa458a9df52d1c2e4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-15 12:22:02 +0200

    refactor: make title bar rendering more functional (#530)

--- packages/renderer-worker/test/ViewletTitleBar.test.js
+++ packages/renderer-worker/test/ViewletTitleBar.test.js
@@ -89,111 +89,6 @@ test('loadContent', async () => {
   })
 })
 
-test('contentLoaded', async () => {
-  const state = {
-    ...ViewletTitleBar.create(),
-    titleBarEntries: [
-      {
-        flags: MenuItemFlags.None,
-        id: 'file',
-        name: 'File',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'edit',
-        name: 'Edit',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'selection',
-        name: 'Selection',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'view',
-        name: 'View',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'go',
-        name: 'Go',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'run',
-        keyboardShortCut: 'Alt+r',
-        name: 'Run',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'terminal',
-        keyboardShortCut: 'Alt+t',
-        name: 'Terminal',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'help',
-        keyboardShortCut: 'Alt+h',
-        name: 'Help',
-      },
-    ],
-  }
-  // @ts-ignore
-  RendererProcess.invoke.mockImplementation(() => {})
-  await ViewletTitleBar.contentLoaded(state)
-  expect(RendererProcess.invoke).toHaveBeenCalledTimes(1)
-  expect(RendererProcess.invoke).toHaveBeenCalledWith(
-    'Viewlet.send',
-    'TitleBar',
-    'menuSetEntries',
-    [
-      {
-        flags: MenuItemFlags.None,
-        id: 'file',
-        name: 'File',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'edit',
-        name: 'Edit',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'selection',
-        name: 'Selection',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'view',
-        name: 'View',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'go',
-        name: 'Go',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'run',
-        keyboardShortCut: 'Alt+r',
-        name: 'Run',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'terminal',
-        keyboardShortCut: 'Alt+t',
-        name: 'Terminal',
-      },
-      {
-        flags: MenuItemFlags.None,
-        id: 'help',
-        keyboardShortCut: 'Alt+h',
-        name: 'Help',
-      },
-    ]
-  )
-})
-
 test('dispose', () => {
   const state = ViewletTitleBar.create()
   expect(ViewletTitleBar.dispose(state)).toEqual({
@@ -204,7 +99,7 @@ test('dispose', () => {
 
 test('resize', () => {
   const state = ViewletTitleBar.create()
-  const { newState } = ViewletTitleBar.resize(state, {
+  const newState = ViewletTitleBar.resize(state, {
     top: 200,
     left: 200,
     width: 200,
