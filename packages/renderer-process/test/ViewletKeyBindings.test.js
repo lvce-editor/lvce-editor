88b15c3875e5c3082037962f29ab1286de662c9c
Le Vivilet <levivlet@pm.me>
2022-09-15 15:23:14 +0200

    add tests for keybindings editor in renderer process

--- packages/renderer-process/test/ViewletKeyBindings.test.js
+++ packages/renderer-process/test/ViewletKeyBindings.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import * as ViewletKeyBindings from '../src/parts/ViewletKeyBindings/ViewletKeyBindings.js'
 
 test('name', () => {
@@ -6,4 +9,31 @@ test('name', () => {
 
 test('create', () => {
   expect(ViewletKeyBindings.create()).toBeDefined()
+})
+
+test('setKeyBindings', () => {
+  const state = ViewletKeyBindings.create()
+  ViewletKeyBindings.setKeyBindings(state, [
+    {
+      key: 'Enter',
+      command: 'EditorCompletion.selectCurrent',
+      when: 'focus.editorCompletions',
+    },
+  ])
+  const { $Viewlet } = state
+  expect($Viewlet.textContent).toBe(`[
+  {
+    \"key\": \"Enter\",
+    \"command\": \"EditorCompletion.selectCurrent\",
+    \"when\": \"focus.editorCompletions\"
+  }
+]
+`)
+})
+
+test.skip('setKeyBindings - error - keyBindings is not of type array', () => {
+  const state = ViewletKeyBindings.create()
+  expect(() => ViewletKeyBindings.setKeyBindings(state, 'abc')).toThrowError(
+    new TypeError('abc')
+  )
 })
