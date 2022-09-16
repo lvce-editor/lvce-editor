fd42d980fa1b52a43094f067f1e11ff5cd379a57
Le Vivilet <levivlet@pm.me>
2022-09-16 16:17:09 +0200

    implement efficient virtual dom rendering with for loop

--- packages/renderer-process/src/parts/VirtualDom/VirtualDom.js
+++ packages/renderer-process/src/parts/VirtualDom/VirtualDom.js
@@ -33,45 +33,19 @@ const renderElement = (element) => {
  * @param {any[]} elements
  * @returns
  */
-export const renderInternal = ($Parent, elements, startIndex) => {
-  let i = startIndex
-  const element = elements[i]
-  for (let i = 0; i < elements.length; i++) {
-    const element1 = elements[i]
-    const $Element1 = renderElement(element1)
-    for (let j = 0; j < element1.childCount; j++) {
-      i++
-      const element2 = elements[i]
-      const $Element2 = renderElement(element2)
-      $Element1.append($Element2)
-      for (let l = 0; l < element2.childCount; l++) {
-        i++
-        const element3 = elements[i]
-        const $Element3 = renderElement(element3)
-        $Element2.append($Element3)
-        for (let m = 0; m < element3.childCount; m++) {
-          i++
-          const element4 = elements[i]
-          const $Element4 = renderElement(element4)
-          $Element3.append($Element4)
-          for (let n = 0; n < element4.childCount; n++) {
-            i++
-            const element5 = elements[i]
-            const $Element5 = renderElement(element5)
-            $Element4.append($Element5)
-            for (let o = 0; o < element5.childCount; o++) {
-              i++
-              const element6 = elements[i]
-              const $Element6 = renderElement(element6)
-              $Element5.append($Element6)
-            }
-          }
-        }
-      }
+export const renderInternal2 = ($Parent, elements, startIndex) => {
+  const max = elements.length - 1
+  let stack = []
+  for (let i = max; i >= 0; i--) {
+    const element = elements[i]
+    const $Element = renderElement(element)
+    if (element.childCount > 0) {
+      $Element.append(...stack)
+      stack = []
     }
-    $Parent.append($Element1)
+    stack.unshift($Element)
   }
-  return i
+  $Parent.append(...stack)
 }
 /**
  *
@@ -80,6 +54,6 @@ export const renderInternal = ($Parent, elements, startIndex) => {
  */
 export const render = (elements) => {
   const $Root = document.createElement('div')
-  renderInternal($Root, elements, 0)
+  renderInternal2($Root, elements, 0)
   return $Root
 }
