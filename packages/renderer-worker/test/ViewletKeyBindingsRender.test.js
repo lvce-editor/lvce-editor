74c9336730326ef0d5594b70de31f6db09201ebf
Le Vivilet <levivlet@pm.me>
2022-09-16 19:21:27 +0200

    fix one render test

---
+++ packages/renderer-worker/test/ViewletKeyBindingsRender.test.js
@@ -0,0 +1,136 @@
+import * as ViewletManager from '../src/parts/ViewletManager/ViewletManager.js'
+import * as ViewletKeyBindings from '../src/parts/ViewletKeyBindings/ViewletKeyBindings.js'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.js'
+
+const render = (oldState, newState) => {
+  return ViewletManager.render(ViewletKeyBindings, oldState, newState)
+}
+
+test.skip('render - add one keybinding', () => {
+  const oldState = {
+    ...ViewletKeyBindings.create(),
+    filteredKeyBindings: [],
+    minLineY: 2,
+    maxLineY: 3,
+    maxVisibleItems: 1,
+  }
+  const newState = {
+    ...oldState,
+    filteredKeyBindings: [
+      {
+        rawKey: 'Enter',
+        isCtrl: false,
+        isShift: false,
+        key: 'Enter',
+        command: 'EditorCompletion.selectCurrent',
+        when: 'focus.editorCompletions',
+      },
+    ],
+  }
+  expect(render(oldState, newState)).toEqual(['addTableDom'])
+})
+
+test('render - remove one keybinding', () => {
+  const oldState = {
+    ...ViewletKeyBindings.create(),
+    filteredKeyBindings: [
+      {
+        rawKey: 'Enter',
+        isCtrl: false,
+        isShift: false,
+        key: 'Enter',
+        command: 'EditorCompletion.selectCurrent',
+        when: 'focus.editorCompletions',
+      },
+    ],
+    minLineY: 2,
+    maxLineY: 3,
+    maxVisibleItems: 1,
+  }
+  const newState = {
+    ...oldState,
+    filteredKeyBindings: [],
+  }
+  expect(render(oldState, newState)).toEqual([
+    [
+      'Viewlet.send',
+      'KeyBindings',
+      'setTableDom',
+      [
+        {
+          childCount: 2,
+          props: {
+            ariaLabel: 'KeyBindings',
+            ariaRowCount: 0,
+            className: 'KeyBindingsTable',
+          },
+          type: VirtualDomElements.Table,
+        },
+        {
+          childCount: 1,
+          props: {
+            className: 'KeyBindingsTableHead',
+          },
+          type: VirtualDomElements.THead,
+        },
+        {
+          childCount: 3,
+          props: {
+            ariaRowIndex: 1,
+            className: 'KeyBindingsTableRow',
+          },
+          type: VirtualDomElements.Tr,
+        },
+        {
+          childCount: 1,
+          props: {
+            className: 'KeyBindingsTableCell',
+          },
+          type: VirtualDomElements.Th,
+        },
+        {
+          childCount: 0,
+          props: {
+            text: 'Command',
+          },
+          type: VirtualDomElements.Text,
+        },
+        {
+          childCount: 1,
+          props: {
+            className: 'KeyBindingsTableCell',
+          },
+          type: VirtualDomElements.Th,
+        },
+        {
+          childCount: 0,
+          props: {
+            text: 'Key',
+          },
+          type: VirtualDomElements.Text,
+        },
+        {
+          childCount: 1,
+          props: {
+            className: 'KeyBindingsTableCell',
+          },
+          type: VirtualDomElements.Th,
+        },
+        {
+          childCount: 0,
+          props: {
+            text: 'When',
+          },
+          type: VirtualDomElements.Text,
+        },
+        {
+          childCount: 0,
+          props: {
+            className: 'KeyBindingsTableBody',
+          },
+          type: VirtualDomElements.TBody,
+        },
+      ],
+    ],
+  ])
+})
