a4d72238e3c78824c91db1685cea5c4693250000
Le Vivilet <levivlet@pm.me>
2022-09-18 13:18:50 +0200

    try to implement file uploading for web

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDropRoot.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDropRoot.js
@@ -1,4 +1,7 @@
 import * as FileSystem from '../FileSystem/FileSystem.js'
+import * as Path from '../Path/Path.js'
+import * as Platform from '../Platform/Platform.js'
+import * as PlatformType from '../PlatformType/PlatformType.js'
 import { getChildDirents } from './ViewletExplorerShared.js'
 
 const mergeDirents = (oldDirents, newDirents) => {
@@ -6,25 +9,67 @@ const mergeDirents = (oldDirents, newDirents) => {
 }
 
 // TODO copy files in parallel
-const copyFiles = async (root, pathSeparator, files) => {
+const copyFilesElectron = async (root, pathSeparator, files) => {
   for (const file of files) {
     const from = file.path
-    const to = root + pathSeparator + file.name
+    const to = Path.join(pathSeparator, root, file.name)
     await FileSystem.copy(from, to)
   }
 }
 
-export const handleDropRoot = async (state, files) => {
-  const { root, pathSeparator, dirents } = state
-  await copyFiles(root, pathSeparator, files)
+const getMergedDirents = async (root, pathSeparator, dirents) => {
   const childDirents = await getChildDirents(root, pathSeparator, {
     path: root,
     depth: 0,
   })
   const mergedDirents = mergeDirents(dirents, childDirents)
+  return mergedDirents
+}
+
+const handleDropRootElectron = async (state, files) => {
+  const { root, pathSeparator, dirents } = state
+  await copyFilesElectron(root, pathSeparator, files)
+  const mergedDirents = await getMergedDirents(root, pathSeparator, dirents)
   return {
     ...state,
     dirents: mergedDirents,
     dropTargets: [],
+  }
+}
+
+const getFileContent = async (file) => {
+  const fileReader = new FileReader()
+  const content = await new Promise((resolve, reject) => {
+    const handleLoad = (event) => {
+      const fileContent = event.target.result
+      resolve(fileContent)
+    }
+    fileReader.onload = handleLoad
+    fileReader.readAsText(file)
+  })
+  return content
+}
+
+const handleDropRootDefault = async (state, files) => {
+  const { root, pathSeparator, dirents } = state
+  for (const file of files) {
+    const content = await getFileContent(file)
+    const to = Path.join(pathSeparator, root, file.name)
+    await FileSystem.writeFile(to, content)
+  }
+  const mergedDirents = await getMergedDirents(root, pathSeparator, dirents)
+  return {
+    ...state,
+    dirents: mergedDirents,
+    dropTargets: [],
+  }
+}
+
+export const handleDropRoot = (state, files) => {
+  switch (Platform.platform) {
+    case PlatformType.Electron:
+      return handleDropRootElectron(state, files)
+    default:
+      return handleDropRootDefault(state, files)
   }
 }
