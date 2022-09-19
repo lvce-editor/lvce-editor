24856576eeb7c91433062530fd86c1e81e2c5963
Le Vivilet <levivlet@pm.me>
2022-09-19 09:51:15 +0200

    fix: error handling in verror module

--- packages/renderer-worker/src/parts/VError/VError.js
+++ packages/renderer-worker/src/parts/VError/VError.js
@@ -9,23 +9,11 @@ const mergeStacks = (parent, child) => {
   if (!child) {
     return parent
   }
-  let i = 1
-  const childLines = child.split('\n')
-  const parentLines = parent.split('\n')
-  const childLinesLength = childLines.length
-  const parentLinesLength = parentLines.length
-  const minLines = Math.min(childLinesLength, parentLinesLength)
-  while (i++ < minLines) {
-    if (
-      childLines[childLinesLength - i] !== parentLines[parentLinesLength - i]
-    ) {
-      break
-    }
-  }
-  const topPart = parentLines.slice(0, parentLinesLength - i + 1)
-  const bottomPart = childLines.slice(childLinesLength - i)
-  const combined = topPart.join('\n') + '\n' + bottomPart.join('\n')
-  return combined
+  const parentNewLineIndex = parent.indexOf('\n')
+  const childNewLineIndex = child.indexOf('\n')
+  const parentFirstLine = parent.slice(0, parentNewLineIndex)
+  const childRest = child.slice(childNewLineIndex)
+  return parentFirstLine + childRest
 }
 
 export class VError extends Error {
--- packages/renderer-worker/test/VError.test.js
+++ packages/renderer-worker/test/VError.test.js
@@ -8,13 +8,28 @@ test('VError - missing child stack', () => {
       this.name = 'DOMException'
     }
   }
-  const error = new VError(
-    new DOMException(
-      'The requested version (1) is less than the existing version (6).'
-    ),
-    `Failed to save IndexedDb value`
+  const cause = new DOMException(
+    'The requested version (1) is less than the existing version (6).'
   )
+  const error = new VError(cause, `Failed to save IndexedDb value`)
   expect(error.stack).toMatch(
     'VError: Failed to save IndexedDb value: DOMException: The requested version (1) is less than the existing version (6).'
+  )
+})
+
+test('VError - merging stacks', () => {
+  const cause = new TypeError()
+  cause.message = `Cannot read properties of undefined (reading 'match')`
+  cause.stack = `Cannot read properties of undefined (reading 'match')
+  at getProtocol (http://localhost:3000/packages/renderer-worker/src/parts/FileSystem/FileSystem.js:18:29)
+  at Module.copy (http://localhost:3000/packages/renderer-worker/src/parts/FileSystem/FileSystem.js:110:20)
+  at handleDropIntoFolder (http://localhost:3000/packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDropIndex.js:14:22)`
+  const error = new VError(cause, `Failed to drop files`)
+  console.log(error.stack)
+  expect(error.stack).toBe(
+    `VError: Failed to drop files: TypeError: Cannot read properties of undefined (reading \'match\')
+  at getProtocol (http://localhost:3000/packages/renderer-worker/src/parts/FileSystem/FileSystem.js:18:29)
+  at Module.copy (http://localhost:3000/packages/renderer-worker/src/parts/FileSystem/FileSystem.js:110:20)
+  at handleDropIntoFolder (http://localhost:3000/packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDropIndex.js:14:22)`
   )
 })
