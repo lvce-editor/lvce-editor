3d9bb224b26bac9a90fcc81a20b014df5058ecdb
Le Vivilet <levivlet@pm.me>
2022-09-19 21:53:12 +0200

    clean up code

--- packages/renderer-process/src/parts/Window/Window.js
+++ packages/renderer-process/src/parts/Window/Window.js
@@ -18,45 +18,24 @@ export const setTitle = (title) => {
   document.title = title
 }
 
+const sendVisibilityChangeHint = () => {
+  RendererWorker.send(
+    /* SaveState.handleVisibilityChange */ 'SaveState.handleVisibilityChange',
+    /* visibilityState */ 'hidden'
+  )
+}
+
+const handleBeforeUnload = () => {
+  sendVisibilityChangeHint()
+}
+const handlePointerLeave = () => {
+  sendVisibilityChangeHint()
+}
+
 // cannot use visibilty change event because worker cannot process events when page closes
 // https://stackoverflow.com/questions/20084348/what-happens-to-a-web-worker-if-i-close-the-page-that-created-this-web-worker/20105455#20105455
+// beforeunload event has the same problem, pointerleave event sometimes works
 export const onVisibilityChange = () => {
-  const handleVisibilityChange = async () => {
-    RendererWorker.send(
-      /* SaveState.handleVisibilityChange */ 'SaveState.handleVisibilityChange',
-      /* visibilityState */ document.visibilityState
-    )
-    await new Promise((resolve, reject) => {
-      setTimeout(resolve, 100)
-    })
-    console.log('timeout done')
-  }
-  // document.addEventListener('visibilitychange', handleVisibilityChange)
-
-  const sendVisibilityChangeHint = () => {
-    RendererWorker.send(
-      /* SaveState.handleVisibilityChange */ 'SaveState.handleVisibilityChange',
-      /* visibilityState */ 'hidden'
-    )
-  }
-
-  const handleBeforeUnload = () => {
-    sendVisibilityChangeHint()
-  }
-  const handlePointerLeave = () => {
-    sendVisibilityChangeHint()
-  }
   window.addEventListener('beforeunload', handleBeforeUnload)
-
   document.addEventListener('pointerleave', handlePointerLeave)
-  // document.addEventListener('visibilitychange', handleVisibilityChange)
-
-  // const handlePageHide = () => {
-  //   console.log('page hide')
-  //   RendererWorker.send(
-  //     /* SaveState.handleVisibilityChange */ 'SaveState.handleVisibilityChange',
-  //     /* visibilityState */ 'hidden'
-  //   )
-  // }
-  // window.addEventListener('pagehide', handlePageHide)
 }
