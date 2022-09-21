10897c628dfb3f97dcb2b58646749961cf7fb29e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-21 23:51:02 +0200

    format code (#614)

--- packages/renderer-worker/src/parts/ClipBoard/ClipBoard.ipc.js
+++ packages/renderer-worker/src/parts/ClipBoard/ClipBoard.ipc.js
@@ -3,7 +3,7 @@ import * as ClipBoard from './ClipBoard.js'
 export const Commands = {
   'ClipBoard.readNativeFiles': ClipBoard.readNativeFiles,
   'ClipBoard.readText': ClipBoard.readText,
+  'ClipBoard.writeImage': ClipBoard.writeImage,
   'ClipBoard.writeNativeFiles': ClipBoard.writeNativeFiles,
   'ClipBoard.writeText': ClipBoard.writeText,
-  'ClipBoard.writeImage': ClipBoard.writeImage,
 }
