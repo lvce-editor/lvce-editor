b5d077094b0ecad26c42eb9529f49ea707ccb072
Le Vivilet <levivlet@pm.me>
2022-09-22 21:09:03 +0200

    refactor: extract viewlet output events into separate module

---
+++ packages/renderer-process/test/ViewletOutputEvents.test.js
@@ -0,0 +1,55 @@
+/**
+ * @jest-environment jsdom
+ */
+import { jest } from '@jest/globals'
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule(
+  '../src/parts/RendererWorker/RendererWorker.js',
+  () => {
+    return {
+      send: jest.fn(() => {
+        throw new Error('not implemented')
+      }),
+    }
+  }
+)
+
+const RendererWorker = await import(
+  '../src/parts/RendererWorker/RendererWorker.js'
+)
+
+const ViewletOutput = await import(
+  '../src/parts/ViewletOutput/ViewletOutput.js'
+)
+
+test('event - change', () => {
+  // @ts-ignore
+  RendererWorker.send.mockImplementation(() => {})
+  const state = ViewletOutput.create()
+  ViewletOutput.setOptions(state, [
+    {
+      name: 'Shared Process',
+      file: '/test/log-shared-process.txt',
+    },
+    {
+      name: 'Extension Host',
+      file: '/test/log-extension-host.txt',
+    },
+  ])
+  const { $Select } = state
+  const event = new InputEvent('change', {
+    bubbles: true,
+  })
+  $Select.children[1].dispatchEvent(event)
+  expect(RendererWorker.send).toHaveBeenCalledTimes(1)
+  expect(RendererWorker.send).toHaveBeenCalledWith(
+    'Viewlet.send',
+    'Output',
+    'setOutputChannel',
+    '/test/log-extension-host.txt'
+  )
+})
