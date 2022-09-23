a519c6e8e57cd8ab425a0047ef0867b201997b96
Le Vivilet <levivlet@pm.me>
2022-09-23 23:22:54 +0200

    add basic find widget buttons

--- packages/renderer-process/src/parts/ViewletEditorFindWidget/ViewletEditorFindWidget.js
+++ packages/renderer-process/src/parts/ViewletEditorFindWidget/ViewletEditorFindWidget.js
@@ -4,25 +4,73 @@ import * as Focus from '../Focus/Focus.js'
 
 export const name = 'EditorFindWidget'
 
+const UiStrings = {
+  Close: 'Close',
+  PreviousMatch: 'Previous Match',
+  NextMatch: 'Next Match',
+}
+
+const Icons = {
+  Close: '/icons/icon-close.svg',
+  PreviousMatch: '/icons/arrow-up.svg',
+  NextMatch: '/icons/arrow-down.svg',
+}
+
+const create$Button = (label, icon) => {
+  const $Icon = document.createElement('div')
+  $Icon.className = 'MaskIcon'
+  $Icon.style.webkitMaskImage = `url('${icon}')`
+  // @ts-ignore
+  $Icon.role = 'none'
+
+  const $Button = document.createElement('div')
+  // @ts-ignore
+  $Button.role = 'button'
+  $Button.ariaLabel = label
+  $Button.title = label
+  $Button.tabIndex = 0
+  $Button.className = `EditorFindWidgetButton`
+  $Button.append($Icon)
+  return $Button
+}
+
 export const create = () => {
   // TODO recycle nodes
   const $InputBox = InputBox.create()
   $InputBox.oninput = ViewletEditorFindWidgetEvents.handleInput
+  $InputBox.ariaLabel = 'Find'
 
   const $MatchCountText = document.createTextNode('')
   const $MatchCount = document.createElement('div')
   $MatchCount.className = 'EditorFindWidgetMatchCount'
   $MatchCount.append($MatchCountText)
 
+  const $ButtonFocusNext = create$Button(UiStrings.NextMatch, Icons.NextMatch)
+  const $ButtonFocusPrevious = create$Button(
+    UiStrings.PreviousMatch,
+    Icons.PreviousMatch
+  )
+  const $ButtonClose = create$Button(UiStrings.Close, Icons.Close)
+
   const $Viewlet = document.createElement('div')
+  $Viewlet.onclick = ViewletEditorFindWidgetEvents.handleClick
   $Viewlet.className = 'Viewlet'
   $Viewlet.dataset.viewletId = 'EditorFindWidget'
-  $Viewlet.append($InputBox, $MatchCount)
+  $Viewlet.append(
+    $InputBox,
+    $MatchCount,
+    $ButtonFocusPrevious,
+    $ButtonFocusNext,
+    $ButtonClose
+  )
   return {
     $Viewlet,
     $InputBox,
     $MatchCount,
     $MatchCountText,
+    $ButtonClose,
+    $ButtonFocusNext,
+    $ButtonFocusPrevious,
   }
 }
 
