4749ad3b5fea9dac40af6abdeb16ce9c810755fe
Le Vivilet <levivlet@pm.me>
2022-09-23 19:48:33 +0200

    fix: scroll editor up when necessary

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandSetSelections.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandSetSelections.js
@@ -1,6 +1,14 @@
 export const setSelections = (editor, selections) => {
   const { minLineY, maxLineY } = editor
   const startRowIndex = selections[0]
+  if (startRowIndex < minLineY) {
+    return {
+      ...editor,
+      minLineY: startRowIndex,
+      maxLineY: startRowIndex + 1,
+      selections,
+    }
+  }
   if (startRowIndex > maxLineY) {
     return {
       ...editor,
--- packages/renderer-worker/test/EditorCommandSetSelection.test.js
+++ packages/renderer-worker/test/EditorCommandSetSelection.test.js
@@ -33,3 +33,22 @@ test('setSelections - scroll down', () => {
     maxLineY: 3,
   })
 })
+
+test('setSelections - scroll up', () => {
+  const editor = {
+    lines: ['line 1', 'line 2', 'line 3'],
+    selections: new Uint32Array([2, 0, 0, 2]),
+    minLineY: 2,
+    maxLineY: 3,
+  }
+  expect(
+    EditorCommandSetSelections.setSelections(
+      editor,
+      new Uint32Array([0, 0, 0, 0])
+    )
+  ).toMatchObject({
+    selections: new Uint32Array([0, 0, 0, 0]),
+    minLineY: 0,
+    maxLineY: 1,
+  })
+})
