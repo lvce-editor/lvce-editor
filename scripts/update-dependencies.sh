de70d34672de137407cec4ad91f472bce8a47aa6
Le Vivilet <levivlet@pm.me>
2022-09-24 11:18:53 +0200

    update dependencies

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -18,7 +18,7 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u`
+  OUTPUT=`ncu -u -x msw`
   SUB='All dependencies match the latest package versions'
   if [[ "$OUTPUT" == *"$SUB"* ]]; then
     echo "$OUTPUT"
