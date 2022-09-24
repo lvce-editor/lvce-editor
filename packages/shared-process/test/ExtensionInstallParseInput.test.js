52d339cd09106d26a8f01304dd458ac35abe51ea
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-24 14:41:06 +0200

    add option to install from github branch (#662)

--- packages/shared-process/test/ExtensionInstallParseInput.test.js
+++ packages/shared-process/test/ExtensionInstallParseInput.test.js
@@ -21,3 +21,44 @@ test('parse - error - could not parse github url', () => {
     },
   })
 })
+
+test('parse - error - cannot install from pull request', () => {
+  expect(
+    ExtensionInstallParseInput.parse('https://github.com/user/repo/pull/1')
+  ).toEqual({
+    type: ExtensionInstallParseInput.InstallType.ParsingError,
+    options: {
+      message: `Cannot download from Pull Request`,
+    },
+  })
+})
+
+test('parse - install from branch', () => {
+  expect(
+    ExtensionInstallParseInput.parse(
+      'https://github.com/user/repo/tree/branch-1'
+    )
+  ).toEqual({
+    type: ExtensionInstallParseInput.InstallType.GithubRepository,
+    options: {
+      user: 'user',
+      repo: 'repo',
+      branch: 'branch-1',
+    },
+  })
+})
+
+test('parse - install from commit', () => {
+  expect(
+    ExtensionInstallParseInput.parse(
+      'https://github.com/user/repo/tree/1981bbd'
+    )
+  ).toEqual({
+    type: ExtensionInstallParseInput.InstallType.GithubRepository,
+    options: {
+      user: 'user',
+      repo: 'repo',
+      branch: '1981bbd',
+    },
+  })
+})
