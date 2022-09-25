54f973383bced9329f8387c8b003a59900227df5
Le Vivilet <levivlet@pm.me>
2022-09-25 22:28:21 +0200

    remove unused dependency

--- static/js/supports-color.js
+++
@@ -1,8 +0,0 @@
-var browser = {
-  stdout: false,
-  stderr: false
-};
-export default browser;
-var stderr = browser.stderr;
-var stdout = browser.stdout;
-export {browser as __moduleExports, stderr, stdout};
