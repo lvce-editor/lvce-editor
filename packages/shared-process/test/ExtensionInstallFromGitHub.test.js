0f42bff640273c33863ee3ff4c65938aafe3fa93
Le Vivilet <levivlet@pm.me>
2022-09-25 12:39:22 +0200

    refactor: adjust extension install code

--- packages/shared-process/test/ExtensionInstallFromGitHub.test.js
+++ packages/shared-process/test/ExtensionInstallFromGitHub.test.js
@@ -1,12 +1,16 @@
 import { jest } from '@jest/globals'
 
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
 jest.unstable_mockModule('../src/parts/Platform/Platform.js', () => ({
-  getExtensionsPath: jest.fn(() => {
+  getExtensionsPath: () => {
     return '/test/extensions'
-  }),
-  getCachedExtensionsPath: jest.fn(() => {
+  },
+  getCachedExtensionsPath: () => {
     return '/test/cached-extensions'
-  }),
+  },
 }))
 
 jest.unstable_mockModule(
@@ -17,13 +21,30 @@ jest.unstable_mockModule(
     }),
   })
 )
+jest.unstable_mockModule('../src/parts/Path/Path.js', () => ({
+  join: jest.fn(() => {
+    throw new Error('not implemented')
+  }),
+}))
 
 jest.unstable_mockModule('../src/parts/TmpFile/TmpFile.js', () => ({
-  getTmpFile: jest.fn(() => {
+  getTmpFile: () => {
     return '/test/tmp-file'
-  }),
-  getTmpDir: jest.fn(() => {
+  },
+  getTmpDir: () => {
     return '/test/tmp-dir'
+  },
+}))
+
+jest.unstable_mockModule('../src/parts/FileSystem/FileSystem.js', () => ({
+  readFile: jest.fn(() => {
+    throw new Error('not implemented')
+  }),
+  remove: jest.fn(() => {
+    throw new Error('not implemented')
+  }),
+  rename: jest.fn(() => {
+    throw new Error('not implemented')
   }),
 }))
 
@@ -33,6 +54,8 @@ const ExtensionInstallFromGitHub = await import(
 const DownloadAndExtract = await import(
   '../src/parts/DownloadAndExtract/DownloadAndExtract.js'
 )
+const FileSystem = await import('../src/parts/FileSystem/FileSystem.js')
+const Path = await import('../src/parts/Path/Path.js')
 
 test('install - error with download', async () => {
   // @ts-ignore
@@ -63,5 +86,99 @@ test('install - error with extraction', async () => {
     })
   ).rejects.toThrowError(
     new Error(`Failed to install user/repo: Failed to extract file`)
+  )
+})
+
+test('install - error - missing id in extension manifest', async () => {
+  // @ts-ignore
+  DownloadAndExtract.downloadAndExtractTarGz.mockImplementation(() => {})
+  // @ts-ignore
+  FileSystem.readFile.mockImplementation(() => {
+    return `{}`
+  })
+  // @ts-ignore
+  Path.join.mockImplementation((...parts) => {
+    return parts.join('/')
+  })
+  // @ts-ignore
+  FileSystem.remove.mockImplementation(() => {})
+  // @ts-ignore
+  FileSystem.rename.mockImplementation(() => {})
+  await expect(
+    ExtensionInstallFromGitHub.install({
+      user: 'user',
+      repo: 'repo',
+      branch: 'HEAD',
+    })
+  ).rejects.toThrowError(
+    new Error(`Failed to install user/repo: missing id in extension manifest`)
+  )
+})
+
+// TODO improve error handling
+test('install - error - manifest contains null', async () => {
+  // @ts-ignore
+  DownloadAndExtract.downloadAndExtractTarGz.mockImplementation(() => {})
+  // @ts-ignore
+  FileSystem.readFile.mockImplementation(() => {
+    return `null`
+  })
+  // @ts-ignore
+  Path.join.mockImplementation((...parts) => {
+    return parts.join('/')
+  })
+  // @ts-ignore
+  FileSystem.remove.mockImplementation(() => {})
+  // @ts-ignore
+  FileSystem.rename.mockImplementation(() => {})
+  await expect(
+    ExtensionInstallFromGitHub.install({
+      user: 'user',
+      repo: 'repo',
+      branch: 'HEAD',
+    })
+  ).rejects.toThrowError(
+    new Error(
+      "Failed to install user/repo: Cannot read properties of null (reading 'id')"
+    )
+  )
+})
+
+test('install', async () => {
+  // @ts-ignore
+  DownloadAndExtract.downloadAndExtractTarGz.mockImplementation(() => {})
+  // @ts-ignore
+  Path.join.mockImplementation((...parts) => {
+    return parts.join('/')
+  })
+  // @ts-ignore
+  FileSystem.remove.mockImplementation(() => {})
+  // @ts-ignore
+  FileSystem.rename.mockImplementation(() => {})
+  // @ts-ignore
+  FileSystem.readFile.mockImplementation(() => {
+    return `{
+  "id": "test.test-extension"
+}`
+  })
+  await ExtensionInstallFromGitHub.install({
+    user: 'user',
+    repo: 'repo',
+    branch: 'HEAD',
+  })
+  expect(DownloadAndExtract.downloadAndExtractTarGz).toHaveBeenCalledTimes(1)
+  expect(DownloadAndExtract.downloadAndExtractTarGz).toHaveBeenCalledWith({
+    outDir: '/test/tmp-dir',
+    strip: 1,
+    url: 'https://codeload.github.com/user/repo/tar.gz/HEAD',
+  })
+  expect(FileSystem.readFile).toHaveBeenCalledTimes(1)
+  expect(FileSystem.readFile).toHaveBeenCalledWith(
+    `/test/tmp-dir/extension.json`
+  )
+  expect(FileSystem.rename).toHaveBeenCalledTimes(1)
+  expect(FileSystem.rename).toHaveBeenCalledWith(
+    '/test/tmp-dir',
+    '/test/extensions/test.test-extension'
   )
 })
