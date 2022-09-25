e27872dd9a86f5b00ef2655732c1834e76e3af85
Le Vivilet <levivlet@pm.me>
2022-09-25 15:46:46 +0200

    undo caching for downloads from github branch, because request to github api is still necessary to determine to which commit the branch points to

--- packages/shared-process/src/parts/ExtensionInstallFromGitHub/ExtensionInstallFromGitHub.js
+++ packages/shared-process/src/parts/ExtensionInstallFromGitHub/ExtensionInstallFromGitHub.js
@@ -1,43 +1,22 @@
 import VError from 'verror'
-import * as Download from '../Download/Download.js'
-import * as Extract from '../Extract/Extract.js'
+import * as DownloadAndExtract from '../DownloadAndExtract/DownloadAndExtract.js'
 import * as FileSystem from '../FileSystem/FileSystem.js'
 import * as Path from '../Path/Path.js'
 import * as Platform from '../Platform/Platform.js'
 
-export const install = async ({ user, repo, branch, outDir = '/tmp' }) => {
-  console.time('install')
+export const install = async ({ user, repo, branch }) => {
   try {
     const cachedExtensionsPath = Platform.getCachedExtensionsPath()
-    const getCachePath = (etag) => {
-      const cacheKey = `github-${user}-${repo}-${etag.slice(3, -1)}.tar.gz`
-      const cacheKeyPath = Path.join(cachedExtensionsPath, cacheKey)
-      console.log({ cacheKeyPath })
-      return cacheKeyPath
-    }
-
     const url = `https://codeload.github.com/${user}/${repo}/tar.gz/${branch}`
-
-    console.log({ url })
-    console.time('request')
-    const { cachePath, fromCache, etag } = await Download.downloadCached({
-      url,
-      getCachePath,
-    })
-
     const cachedExtensionPath = Path.join(
       cachedExtensionsPath,
-      `github-${user}-${repo}-${etag.slice(3)}`
+      `github-${user}-${repo}-${branch}`
     )
-    console.timeEnd('request')
-    console.log({ fromCache, cachePath })
-    console.time('extract')
-    await Extract.extractTarGz({
-      inFile: cachePath,
+    await DownloadAndExtract.downloadAndExtractTarGz({
+      url,
       outDir: cachedExtensionPath,
       strip: 1,
     })
-    console.timeEnd('extract')
     const extensionsPath = Platform.getExtensionsPath()
     const manifestPath = Path.join(cachedExtensionPath, 'extension.json')
     const manifestContent = await FileSystem.readFile(manifestPath)
@@ -52,5 +31,4 @@ export const install = async ({ user, repo, branch, outDir = '/tmp' }) => {
   } catch (error) {
     throw new VError(error, `Failed to install ${user}/${repo}`)
   }
-  console.timeEnd('install')
 }
