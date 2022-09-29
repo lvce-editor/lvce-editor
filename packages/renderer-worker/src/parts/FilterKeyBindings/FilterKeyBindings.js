9eda4410c41bc0ef04aff68c7ec5c095cb34f15f
Le Vivilet <levivlet@pm.me>
2022-09-29 23:16:17 +0200

    add fuzzy search to keybindings editor

--- packages/renderer-worker/src/parts/FilterKeyBindings/FilterKeyBindings.js
+++ packages/renderer-worker/src/parts/FilterKeyBindings/FilterKeyBindings.js
@@ -1,7 +1,13 @@
+import * as FuzzySearch from '../FuzzySearch/FuzzySearch.js'
+
 export const getFilteredKeyBindings = (keyBindings, value) => {
   const filteredKeyBindings = []
   for (const keyBinding of keyBindings) {
-    if (keyBinding.command.includes(value) || keyBinding.key.includes(value)) {
+    const { command, key } = keyBinding
+    if (
+      FuzzySearch.fuzzySearch(value, command) ||
+      FuzzySearch.fuzzySearch(value, key)
+    ) {
       filteredKeyBindings.push(keyBinding)
     }
   }
