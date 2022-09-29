ddce1a4f4a885045d66d422a34da86fff5bf63ae
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-29 23:15:40 +0200

    fix: extensions handle click focusing wrong item when scrolled (#733)

--- packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsHandleClick.js
+++ packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsHandleClick.js
@@ -2,9 +2,10 @@ import { focusIndex } from './ViewletExtensionsFocusIndex.js'
 import * as Command from '../Command/Command.js'
 
 export const handleClick = async (state, index) => {
-  const { filteredExtensions } = state
-  const extension = filteredExtensions[index]
+  const { filteredExtensions, minLineY } = state
+  const actualIndex = index + minLineY
+  const extension = filteredExtensions[actualIndex]
   const uri = `extension-detail://${extension.id}`
   await Command.execute('Main.openUri', uri)
-  return focusIndex(state, index)
+  return focusIndex(state, actualIndex)
 }
