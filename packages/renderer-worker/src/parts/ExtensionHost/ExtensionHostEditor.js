0b3765817584567aad3a4c2bf755b8f41d0c49c1
Le Vivilet <levivlet@pm.me>
2022-10-03 17:06:43 +0200

    refactor: load languages before restoring editors to avoid editors waiting for languages to load

--- packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostEditor.js
+++ packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostEditor.js
@@ -1,8 +1,6 @@
-import * as Languages from '../Languages/Languages.js'
-import * as ViewletStates from '../ViewletStates/ViewletStates.js'
 import * as ExtensionHostShared from './ExtensionHostShared.js'
 
-export const execute = async ({
+export const execute = ({
   editor,
   args,
   event,
@@ -10,26 +8,6 @@ export const execute = async ({
   combineResults,
   noProviderFoundMessage,
 }) => {
-  // TODO maybe load languages before showing editor
-  // then could skip this if else statement for
-  // all editor requests, only downside would be that
-  // loading languages takes ~6ms and that would slow down
-  // loading editor content and showing editor by 6ms
-  // also extension manifests need to be read already
-  // because of color theme and languages and color
-  // theme could be cached in localStorage/IndexedDB
-  if (!Languages.hasLoaded()) {
-    await Languages.waitForLoad()
-    const newEditor = ViewletStates.getState('EditorText')
-    return ExtensionHostShared.executeProviders({
-      event: `${event}:${newEditor.languageId}`,
-      method: method,
-      params: [newEditor.id, ...args],
-      noProviderFoundMessage,
-      combineResults: combineResults,
-      noProviderFoundResult: [],
-    })
-  }
   return ExtensionHostShared.executeProviders({
     event: `${event}:${editor.languageId}`,
     method: method,
