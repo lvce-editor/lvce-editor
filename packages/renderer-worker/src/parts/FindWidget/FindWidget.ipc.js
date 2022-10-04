4da8ad93abea02c391842ee31ae88f7f17cb6427
Le Vivilet <levivlet@pm.me>
2022-10-04 23:54:57 +0200

    remove unused code

--- packages/renderer-worker/src/parts/FindWidget/FindWidget.ipc.js
+++
@@ -1,7 +0,0 @@
-import * as FindWidget from './FindWidget.js'
-
-export const Commands = {
-  'FindWidget.create': FindWidget.create,
-  'FindWidget.dispose': FindWidget.dispose,
-  'FindWidget.setValue': FindWidget.setValue,
-}
