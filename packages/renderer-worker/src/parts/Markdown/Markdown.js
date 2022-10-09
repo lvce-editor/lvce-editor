f8f6b30e4e7f29417107b2b7f3395b7025734588
Le Vivilet <levivlet@pm.me>
2022-10-09 20:21:44 +0200

    fix: use correct base url when rendering extension images in extension detail view

--- packages/renderer-worker/src/parts/Markdown/Markdown.js
+++ packages/renderer-worker/src/parts/Markdown/Markdown.js
@@ -1,13 +1,7 @@
 import { marked } from '../../../../../static/js/marked.js'
 
-const renderer = {
-  image(href, title, text) {
-    return `<img src="${href}" title="${title}" alt="${text}" crossorigin="anonymous" />`
-  },
-}
-
-marked.use({ renderer })
-
-export const toHtml = (html) => {
-  return marked(html)
+export const toHtml = (html, { baseUrl = '' } = {}) => {
+  return marked(html, {
+    baseUrl,
+  })
 }
