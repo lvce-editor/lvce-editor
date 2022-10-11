1469b0dd8b4d2dd535eb050b5b012ec7b6743e55
Le Vivilet <levivlet@pm.me>
2022-10-11 11:04:28 +0200

    format code

--- packages/renderer-worker/src/parts/ColorTheme/ColorTheme.ipc.js
+++ packages/renderer-worker/src/parts/ColorTheme/ColorTheme.ipc.js
@@ -2,6 +2,6 @@ import * as ColorTheme from './ColorTheme.js'
 
 export const Commands = {
   'ColorTheme.hydrate': ColorTheme.hydrate,
-  'ColorTheme.setColorTheme': ColorTheme.setColorTheme,
   'ColorTheme.reload': ColorTheme.reload,
+  'ColorTheme.setColorTheme': ColorTheme.setColorTheme,
 }
