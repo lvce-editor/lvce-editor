4f06826b45f5924723824e9705fb29fe67b81bb3
Le Vivilet <levivlet@pm.me>
2022-10-20 17:32:19 +0200

    fix source control provider e2e test

--- packages/renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponentSideBar.js
+++ packages/renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponentSideBar.js
@@ -1,5 +1,5 @@
 import * as Command from '../Command/Command.js'
 
 export const open = async (id) => {
-  await Command.execute('SideBar.showOrHideViewlet', id)
+  await Command.execute('SideBar.openViewlet', id)
 }
