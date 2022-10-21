f886c157bf5273a545812f655e9400c1344d8bc9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-10-21 16:43:36 +0200

    fix: improve file picker error handling when showDirectoryPicker is n… (#923)

--- packages/renderer-worker/src/parts/FilePicker/FilePicker.js
+++ packages/renderer-worker/src/parts/FilePicker/FilePicker.js
@@ -1,7 +1,21 @@
 import * as RendererProcess from '../RendererProcess/RendererProcess.js'
 
-export const showDirectoryPicker = (options) => {
-  return RendererProcess.invoke('FilePicker.showDirectoryPicker', options)
+export const showDirectoryPicker = async (options) => {
+  try {
+    return await RendererProcess.invoke(
+      'FilePicker.showDirectoryPicker',
+      options
+    )
+  } catch (error) {
+    if (
+      error &&
+      // @ts-ignore
+      error.message === 'window.showDirectoryPicker is not a function'
+    ) {
+      throw new Error(`showDirectoryPicker not supported on this browser`)
+    }
+    throw error
+  }
 }
 
 export const showFilePicker = (options) => {
