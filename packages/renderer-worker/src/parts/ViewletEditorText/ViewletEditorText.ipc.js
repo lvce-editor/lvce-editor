ef7521ca0a024c171bc871d314a7399cae3cb4b7
Le Vivilet <levivlet@pm.me>
2022-10-24 22:40:23 +0200

    refactor: rename Editor.css to ViewletEditor.css to be more consistent

--- packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorText.ipc.js
+++ packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorText.ipc.js
@@ -187,6 +187,6 @@ export const Events = {
   // 'tokenizer.changed': ViewletEditorText.handleTokenizeChange,
 }
 
-export const Css = '/css/parts/Editor.css'
+export const Css = '/css/parts/ViewletEditor.css'
 
 export * from './ViewletEditorText.js'
