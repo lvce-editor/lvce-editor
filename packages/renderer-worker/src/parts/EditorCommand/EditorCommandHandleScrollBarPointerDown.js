f1767f21d4042c11939fb970d20ac79191217829
Le Vivilet <levivlet@pm.me>
2022-10-27 11:50:55 +0200

    move getNewDeltaPercent function to scrollbar functions

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleScrollBarPointerDown.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleScrollBarPointerDown.js
@@ -7,29 +7,6 @@ import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.js
 // when clicked at y > editor.height - editor.scrollBarHeight/2, position scrollbar at (y - scrollbarHeight/2)
 // additionally, when clicked on scrollbar, scrollbar position shouldn't move
 
-const getNewDeltaPercent = (height, scrollBarHeight, relativeY) => {
-  const halfScrollBarHeight = scrollBarHeight / 2
-  if (relativeY <= halfScrollBarHeight) {
-    // clicked at top
-    return {
-      percent: 0,
-      handleOffset: relativeY,
-    }
-  }
-  if (relativeY <= height - halfScrollBarHeight) {
-    // clicked in middle
-    return {
-      percent: (relativeY - halfScrollBarHeight) / (height - scrollBarHeight),
-      handleOffset: halfScrollBarHeight,
-    }
-  }
-  // clicked at bottom
-  return {
-    percent: 1,
-    handleOffset: scrollBarHeight - height + relativeY,
-  }
-}
-
 export const handleScrollBarPointerDown = (state, y) => {
   const { top, deltaY, finalDeltaY, height, scrollBarHeight } = state
   const relativeY = y - top
@@ -46,7 +23,7 @@ export const handleScrollBarPointerDown = (state, y) => {
       handleOffset: diff,
     }
   }
-  const { percent, handleOffset } = getNewDeltaPercent(
+  const { percent, handleOffset } = ScrollBarFunctions.getNewDeltaPercent(
     height,
     scrollBarHeight,
     relativeY
--- packages/renderer-worker/src/parts/ScrollBarFunctions/ScrollBarFunctions.js
+++ packages/renderer-worker/src/parts/ScrollBarFunctions/ScrollBarFunctions.js
@@ -20,3 +20,26 @@ export const getScrollBarY = (deltaY, finalDeltaY, height, scrollBarHeight) => {
   const scrollBarY = (deltaY / finalDeltaY) * (height - scrollBarHeight)
   return scrollBarY
 }
+
+export const getNewDeltaPercent = (height, scrollBarHeight, relativeY) => {
+  const halfScrollBarHeight = scrollBarHeight / 2
+  if (relativeY <= halfScrollBarHeight) {
+    // clicked at top
+    return {
+      percent: 0,
+      handleOffset: relativeY,
+    }
+  }
+  if (relativeY <= height - halfScrollBarHeight) {
+    // clicked in middle
+    return {
+      percent: (relativeY - halfScrollBarHeight) / (height - scrollBarHeight),
+      handleOffset: halfScrollBarHeight,
+    }
+  }
+  // clicked at bottom
+  return {
+    percent: 1,
+    handleOffset: scrollBarHeight - height + relativeY,
+  }
+}
