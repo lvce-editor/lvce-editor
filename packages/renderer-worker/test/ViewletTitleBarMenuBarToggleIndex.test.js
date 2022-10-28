6e508aa7ced66fcfea4eb1e71c31f29d7f956c7a
Le Vivilet <levivlet@pm.me>
2022-10-28 15:46:20 +0200

    fix failing test

--- packages/renderer-worker/test/ViewletTitleBarMenuBarToggleIndex.test.js
+++ packages/renderer-worker/test/ViewletTitleBarMenuBarToggleIndex.test.js
@@ -1,3 +1,86 @@
+import * as MenuEntryId from '../src/parts/MenuEntryId/MenuEntryId.js'
+import * as MenuItemFlags from '../src/parts/MenuItemFlags/MenuItemFlags.js'
+import * as ViewletTitleBarMenuBar from '../src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.js'
+
+import { jest } from '@jest/globals'
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule(
+  '../src/parts/RendererProcess/RendererProcess.js',
+  () => {
+    return {
+      invoke: () => {
+        return {
+          left: 0,
+          bottom: 0,
+        }
+      },
+    }
+  }
+)
+
+jest.unstable_mockModule('../src/parts/MenuEntries/MenuEntries.js', () => {
+  return {
+    getMenuEntries: (id) => {
+      switch (id) {
+        case MenuEntryId.File:
+          return [
+            {
+              flags: MenuItemFlags.Disabled,
+              id: 'newFile',
+              label: 'New File',
+            },
+            {
+              flags: MenuItemFlags.Disabled,
+              id: 'newWindow',
+              label: 'New Window',
+            },
+            {
+              flags: MenuItemFlags.SubMenu,
+              id: MenuEntryId.OpenRecent,
+              label: 'Open Recent',
+            },
+          ]
+        case MenuEntryId.Edit:
+          return [
+            {
+              flags: MenuItemFlags.Disabled,
+              id: 'undo',
+              label: 'Undo',
+            },
+            {
+              flags: MenuItemFlags.Disabled,
+              id: 'redo',
+              label: 'Redo',
+            },
+          ]
+        case MenuEntryId.Selection:
+          return []
+        case MenuEntryId.OpenRecent:
+          return [
+            {
+              flags: MenuItemFlags.None,
+              label: 'file-1.txt',
+            },
+            {
+              flags: MenuItemFlags.None,
+              label: 'file-2.txt',
+            },
+          ]
+        default:
+          throw new Error(`no menu entries found for ${id}`)
+      }
+    },
+  }
+})
+
+const ViewletTitleBarMenuBarToggleIndex = await import(
+  '../src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBarToggleIndex.js'
+)
+
 test('toggleIndex - when open - when same index', async () => {
   const state = {
     ...ViewletTitleBarMenuBar.create(),
@@ -14,7 +97,9 @@ test('toggleIndex - when open - when same index', async () => {
       },
     ],
   }
-  expect(await ViewletTitleBarMenuBar.toggleIndex(state, 0)).toMatchObject({
+  expect(
+    await ViewletTitleBarMenuBarToggleIndex.toggleIndex(state, 0)
+  ).toMatchObject({
     focusedIndex: 0,
     isMenuOpen: false,
   })
@@ -36,7 +121,9 @@ test('toggleIndex - when open - when different index', async () => {
       },
     ],
   }
-  expect(await ViewletTitleBarMenuBar.toggleIndex(state, 1)).toMatchObject({
+  expect(
+    await ViewletTitleBarMenuBarToggleIndex.toggleIndex(state, 1)
+  ).toMatchObject({
     focusedIndex: 1,
     menus: [
       {
@@ -74,7 +161,9 @@ test('toggleIndex - when closed - when same index', async () => {
       },
     ],
   }
-  expect(await ViewletTitleBarMenuBar.toggleIndex(state, 0)).toMatchObject({
+  expect(
+    await ViewletTitleBarMenuBarToggleIndex.toggleIndex(state, 0)
+  ).toMatchObject({
     focusedIndex: 0,
     isMenuOpen: true,
     menus: [
@@ -118,7 +207,9 @@ test('toggleIndex - when closed - when different index', async () => {
       },
     ],
   }
-  expect(await ViewletTitleBarMenuBar.toggleIndex(state, 1)).toMatchObject({
+  expect(
+    await ViewletTitleBarMenuBarToggleIndex.toggleIndex(state, 1)
+  ).toMatchObject({
     focusedIndex: 1,
     menus: [
       {
