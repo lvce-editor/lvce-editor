58dbbe4f71af18bf9f152ed4dc67e4edd20f6c6e
Le Vivilet <levivlet@pm.me>
2022-10-28 15:47:56 +0200

    fix failing test

--- packages/renderer-worker/test/ViewletTitleBarMenuBar.test.js
+++ packages/renderer-worker/test/ViewletTitleBarMenuBar.test.js
@@ -1,80 +1,11 @@
-import { jest } from '@jest/globals'
-import * as MenuItemFlags from '../src/parts/MenuItemFlags/MenuItemFlags.js'
-import * as MenuEntryId from '../src/parts/MenuEntryId/MenuEntryId.js'
+import * as ViewletModuleId from '../src/parts/ViewletModuleId/ViewletModuleId.js'
+import * as ViewletTitleBarMenuBar from '../src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.js'
 
-beforeEach(() => {
-  jest.resetAllMocks()
+test('name', () => {
+  expect(ViewletTitleBarMenuBar.name).toBe(ViewletModuleId.TitleBarMenuBar)
 })
 
-jest.unstable_mockModule(
-  '../src/parts/RendererProcess/RendererProcess.js',
-  () => {
-    return {
-      invoke: () => {
-        return {
-          left: 0,
-          bottom: 0,
-        }
-      },
-    }
-  }
-)
-
-jest.unstable_mockModule('../src/parts/MenuEntries/MenuEntries.js', () => {
-  return {
-    getMenuEntries: (id) => {
-      switch (id) {
-        case MenuEntryId.File:
-          return [
-            {
-              flags: MenuItemFlags.Disabled,
-              id: 'newFile',
-              label: 'New File',
-            },
-            {
-              flags: MenuItemFlags.Disabled,
-              id: 'newWindow',
-              label: 'New Window',
-            },
-            {
-              flags: MenuItemFlags.SubMenu,
-              id: MenuEntryId.OpenRecent,
-              label: 'Open Recent',
-            },
-          ]
-        case MenuEntryId.Edit:
-          return [
-            {
-              flags: MenuItemFlags.Disabled,
-              id: 'undo',
-              label: 'Undo',
-            },
-            {
-              flags: MenuItemFlags.Disabled,
-              id: 'redo',
-              label: 'Redo',
-            },
-          ]
-        case MenuEntryId.Selection:
-          return []
-        case MenuEntryId.OpenRecent:
-          return [
-            {
-              flags: MenuItemFlags.None,
-              label: 'file-1.txt',
-            },
-            {
-              flags: MenuItemFlags.None,
-              label: 'file-2.txt',
-            },
-          ]
-        default:
-          throw new Error(`no menu entries found for ${id}`)
-      }
-    },
-  }
+test('create', () => {
+  const state = ViewletTitleBarMenuBar.create()
+  expect(state).toBeDefined()
 })
-
-const ViewletTitleBarMenuBar = await import(
-  '../src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.js'
-)
