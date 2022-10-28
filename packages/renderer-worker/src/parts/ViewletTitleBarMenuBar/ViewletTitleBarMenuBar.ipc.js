3cf5d4acce39e9c03da8de106a6d668ea5ee951d
Le Vivilet <levivlet@pm.me>
2022-10-28 15:38:56 +0200

    lazy load menu bar commands

--- packages/renderer-worker/src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.ipc.js
+++ packages/renderer-worker/src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.ipc.js
@@ -1,4 +1,5 @@
 import * as ViewletTitleBarMenuBar from './ViewletTitleBarMenuBar.js'
+import * as LazyCommand from '../LazyCommand/LazyCommand.js'
 
 // prettier-ignore
 const Imports = {
@@ -27,27 +28,27 @@ const Imports = {
 
 // prettier-ignore
 export const Commands = {
-  'TitleBarMenuBar.closeMenu': ViewletTitleBarMenuBar.closeMenu,
-  'TitleBarMenuBar.focus': ViewletTitleBarMenuBar.focus,
-  'TitleBarMenuBar.focusFirst': ViewletTitleBarMenuBar.focusFirst,
-  'TitleBarMenuBar.focusIndex': ViewletTitleBarMenuBar.focusIndex,
-  'TitleBarMenuBar.focusLast': ViewletTitleBarMenuBar.focusLast,
-  'TitleBarMenuBar.focusNext': ViewletTitleBarMenuBar.focusNext,
-  'TitleBarMenuBar.focusPrevious': ViewletTitleBarMenuBar.focusPrevious,
-  'TitleBarMenuBar.handleKeyArrowDown': ViewletTitleBarMenuBar.handleKeyArrowDown,
-  'TitleBarMenuBar.handleKeyArrowLeft': ViewletTitleBarMenuBar.handleKeyArrowLeft,
-  'TitleBarMenuBar.handleKeyArrowRight': ViewletTitleBarMenuBar.handleKeyArrowRight,
-  'TitleBarMenuBar.handleKeyArrowUp': ViewletTitleBarMenuBar.handleKeyArrowUp,
-  'TitleBarMenuBar.handleKeyEnd': ViewletTitleBarMenuBar.handleKeyEnd,
-  'TitleBarMenuBar.handleKeyEnter': ViewletTitleBarMenuBar.handleKeyEnter,
-  'TitleBarMenuBar.handleKeyEscape': ViewletTitleBarMenuBar.handleKeyEscape,
-  'TitleBarMenuBar.handleKeyHome': ViewletTitleBarMenuBar.handleKeyHome,
-  'TitleBarMenuBar.handleKeySpace': ViewletTitleBarMenuBar.handleKeySpace,
-  'TitleBarMenuBar.handleMenuMouseDown': ViewletTitleBarMenuBar.handleMenuMouseDown,
-  'TitleBarMenuBar.handleMenuMouseOver': ViewletTitleBarMenuBar.handleMenuMouseOver,
-  'TitleBarMenuBar.handleMouseOver': ViewletTitleBarMenuBar.handleMouseOver,
-  'TitleBarMenuBar.toggleIndex': ViewletTitleBarMenuBar.toggleIndex,
-  'TitleBarMenuBar.toggleMenu': ViewletTitleBarMenuBar.toggleMenu,
+  'TitleBarMenuBar.closeMenu': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.CloseMenu, 'closeMenu'),
+  'TitleBarMenuBar.focus': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.Focus, 'focus'),
+  'TitleBarMenuBar.focusFirst': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.FocusFirst, 'focusFirst'),
+  'TitleBarMenuBar.focusIndex': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.FocusIndex, 'focusIndex'),
+  'TitleBarMenuBar.focusLast': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.FocusLast, 'focusLast'),
+  'TitleBarMenuBar.focusNext': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.FocusNext, 'focusNext'),
+  'TitleBarMenuBar.focusPrevious': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.FocusPrevious, 'focusPrevious'),
+  'TitleBarMenuBar.handleKeyArrowDown': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleKeyArrowDown, 'handleKeyArrowDown'),
+  'TitleBarMenuBar.handleKeyArrowLeft': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleKeyArrowLeft, 'handleKeyArrowLeft'),
+  'TitleBarMenuBar.handleKeyArrowRight': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleKeyArrowRight, 'handleKeyArrowRight'),
+  'TitleBarMenuBar.handleKeyArrowUp': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleKeyArrowUp, 'handleKeyArrowUp'),
+  'TitleBarMenuBar.handleKeyEnd': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleKeyEnd, 'handleKeyEnd'),
+  'TitleBarMenuBar.handleKeyEnter': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleKeyEnter, 'handleKeyEnter'),
+  'TitleBarMenuBar.handleKeyEscape': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleKeyEscape, 'handleKeyEscape'),
+  'TitleBarMenuBar.handleKeyHome': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleKeyHome, 'handleKeyHome'),
+  'TitleBarMenuBar.handleKeySpace': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleKeySpace, 'handleKeySpace'),
+  'TitleBarMenuBar.handleMenuMouseDown': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleMenuMouseDown, 'handleMenuMouseDown'),
+  'TitleBarMenuBar.handleMenuMouseOver': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleMenuMouseOver, 'handleMenuMouseOver'),
+  'TitleBarMenuBar.handleMouseOver': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.HandleMouseOver, 'handleMouseOver'),
+  'TitleBarMenuBar.toggleIndex': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.ToggleIndex, 'toggleIndex'),
+  'TitleBarMenuBar.toggleMenu': LazyCommand.create(ViewletTitleBarMenuBar.name, Imports.ToggleMenu, 'toggleMenu'),
 }
 
 export const Css = '/css/parts/ViewletTitleBarMenuBar.css'
