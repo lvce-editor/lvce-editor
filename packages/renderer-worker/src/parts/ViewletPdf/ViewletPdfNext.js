bdcbeb4c05f448b3c11750dcc6b6f2e2d45c4981
Le Vivilet <levivlet@pm.me>
2022-11-02 10:24:41 +0100

    fix: don't go to next page when there is no next page

--- packages/renderer-worker/src/parts/ViewletPdf/ViewletPdfNext.js
+++ packages/renderer-worker/src/parts/ViewletPdf/ViewletPdfNext.js
@@ -1,6 +1,9 @@
 import { focusPage } from './ViewletPdfFocusPage.js'
 
-export const next = async (state) => {
-  const { page } = state
+export const next = (state) => {
+  const { page, numberOfPages } = state
+  if (page >= numberOfPages - 1) {
+    return state
+  }
   return focusPage(state, page + 1)
 }
--- packages/renderer-worker/src/parts/ViewletPdf/ViewletPdfPrevious.js
+++ packages/renderer-worker/src/parts/ViewletPdf/ViewletPdfPrevious.js
@@ -2,5 +2,8 @@ import { focusPage } from './ViewletPdfFocusPage.js'
 
 export const previous = (state) => {
   const { page } = state
+  if (page <= 0) {
+    return state
+  }
   return focusPage(state, page - 1)
 }
