8394a636c2078e79abc0475e0d7312023b6d5cd3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-11-02 10:05:57 +0100

    refactor: use encoding type enum (#1050)

--- build/src/parts/ReadFile/ReadFile.js
+++ build/src/parts/ReadFile/ReadFile.js
@@ -1,8 +1,9 @@
 import * as fs from 'node:fs/promises'
 import * as Path from '../Path/Path.js'
+import * as EncodingType from '../EncodingType/EncodingType.js'
 
 export const readFile = async (relativePath) => {
   const absolutePath = Path.absolute(relativePath)
-  const content = await fs.readFile(absolutePath, 'utf8')
+  const content = await fs.readFile(absolutePath, EncodingType.Utf8)
   return content
 }
