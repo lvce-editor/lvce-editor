4f9a8bcb74d04f8246d2e95d03598b6d0d6e8c92
Le Vivilet <levivlet@pm.me>
2022-11-02 17:17:10 +0100

    fix failing tests

--- packages/renderer-process/test/ViewletPdf.test.js
+++ packages/renderer-process/test/ViewletPdf.test.js
@@ -1,12 +1,9 @@
+/**
+ * @jest-environment jsdom
+ */
 import * as ViewletPdf from '../src/parts/ViewletPdf/ViewletPdf.js'
 
 test('create', () => {
   const state = ViewletPdf.create()
   expect(state).toBeDefined()
-})
-
-test('text', () => {
-  const state = ViewletPdf.create()
-  const { $Viewlet } = state
-  expect($Viewlet.textContent).toBe('pdf')
 })
