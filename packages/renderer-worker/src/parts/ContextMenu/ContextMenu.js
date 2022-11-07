f52c8090a9abb2881d53aeb70ab5379f5b24c414
Le Vivilet <levivlet@pm.me>
2022-11-07 18:54:13 +0100

    use preference for context menu style

--- packages/renderer-worker/src/parts/ContextMenu/ContextMenu.js
+++ packages/renderer-worker/src/parts/ContextMenu/ContextMenu.js
@@ -1,9 +1,18 @@
 import * as Assert from '../Assert/Assert.js'
 import * as Platform from '../Platform/Platform.js'
 import * as PlatformType from '../PlatformType/PlatformType.js'
+import * as Preferences from '../Preferences/Preferences.js'
+
+const hasContextMenuNativePreference = () => {
+  const value = Preferences.get('window.titleBarStyle')
+  return value === 'native'
+}
 
 const getModule = () => {
-  if (Platform.platform === PlatformType.Electron) {
+  if (
+    Platform.platform === PlatformType.Electron &&
+    hasContextMenuNativePreference()
+  ) {
     return import('./ContextMenuElectron.js')
   }
   return import('./ContextMenuBrowser.js')
