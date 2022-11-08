0d836b75f2c3aa5eecce277b85809c5db8d9fea6
Le Vivilet <levivlet@pm.me>
2022-11-08 22:54:47 +0100

    refactor: simplify code for lazyloading explorer commands

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.ipc.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.ipc.js
@@ -1,4 +1,3 @@
-import * as LazyCommand from '../LazyCommand/LazyCommand.js'
 import * as ViewletExplorer from './ViewletExplorer.js'
 
 // prettier-ignore
@@ -31,14 +30,6 @@ export const Commands = {
   copyPath:                       ViewletExplorer.copyPath,
   copyRelativePath:               ViewletExplorer.copyRelativePath,
   expandAll:                      ViewletExplorer.expandAll,
-  expandRecursively:              LazyCommand.create(ViewletExplorer.name, Imports.ExpandRecursively, 'expandRecursively'),
-  focus:                          LazyCommand.create(ViewletExplorer.name, Imports.Focus, 'focus'),
-  focusFirst:                     LazyCommand.create(ViewletExplorer.name, Imports.FocusFirst, 'focusFirst'),
-  focusIndex:                     LazyCommand.create(ViewletExplorer.name, Imports.FocusIndex, 'focusIndex'),
-  focusLast:                      LazyCommand.create(ViewletExplorer.name, Imports.FocusLast, 'focusLast'),
-  focusNext:                      LazyCommand.create(ViewletExplorer.name, Imports.FocusNext, 'focusNext'),
-  focusNone:                      LazyCommand.create(ViewletExplorer.name, Imports.FocusNone, 'focusNone'),
-  focusPrevious:                  LazyCommand.create(ViewletExplorer.name, Imports.FocusPrevious, 'focusPrevious'),
   getFocusedDirent:               ViewletExplorer.getFocusedDirent,
   handleArrowLeft:                ViewletExplorer.handleArrowLeft,
   handleArrowRight:               ViewletExplorer.handleArrowRight,
@@ -47,14 +38,9 @@ export const Commands = {
   handleClickAt:                  ViewletExplorer.handleClickAt,
   handleClickCurrent:             ViewletExplorer.handleClickCurrent,
   handleClickCurrentButKeepFocus: ViewletExplorer.handleClickCurrentButKeepFocus,
-  handleContextMenuKeyboard:      LazyCommand.create(ViewletExplorer.name, Imports.HandleContextMenuKeyboard, 'handleContextMenuKeyboard'),
-  handleContextMenuMouseAt:       LazyCommand.create(ViewletExplorer.name, Imports.HandleContextMenuMouseAt, 'handleContextMenuMouseAt'),
   handleCopy:                     ViewletExplorer.handleCopy,
-  handleDragOver:                 LazyCommand.create(ViewletExplorer.name, Imports.HandleDragOver, 'handleDragOver'),
-  handleDrop:                     LazyCommand.create(ViewletExplorer.name, Imports.HandleDrop, 'handleDrop'),
   handleMouseEnter:               ViewletExplorer.handleMouseEnter,
   handleMouseLeave:               ViewletExplorer.handleMouseLeave,
-  handlePaste:                    LazyCommand.create(ViewletExplorer.name, Imports.HandlePaste, 'handlePaste'),
   handleWheel:                    ViewletExplorer.handleWheel,
   newFile:                        ViewletExplorer.newFile,
   newFolder:                      ViewletExplorer.newFolder,
@@ -67,6 +53,22 @@ export const Commands = {
   scrollUp:                       ViewletExplorer.scrollUp,
   setDeltaY:                      ViewletExplorer.setDeltaY,
   updateEditingValue:             ViewletExplorer.updateEditingValue,
+}
+
+export const LazyCommands = {
+  expandRecursively: Imports.ExpandRecursively,
+  focus: Imports.Focus,
+  focusFirst: Imports.FocusFirst,
+  focusIndex: Imports.FocusIndex,
+  focusLast: Imports.FocusLast,
+  focusNext: Imports.FocusNext,
+  focusNone: Imports.FocusNone,
+  focusPrevious: Imports.FocusPrevious,
+  handleContextMenuKeyboard: Imports.HandleContextMenuKeyboard,
+  handleContextMenuMouseAt: Imports.HandleContextMenuMouseAt,
+  handleDragOver: Imports.HandleDragOver,
+  handleDrop: Imports.HandleDrop,
+  handlePaste: Imports.HandlePaste,
 }
 
 export const Css = '/css/parts/ViewletExplorer.css'
