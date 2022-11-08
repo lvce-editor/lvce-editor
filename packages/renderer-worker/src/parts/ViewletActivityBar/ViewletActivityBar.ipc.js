005ae041518fe67c4f5c9c9eea0367b081701b3b
Le Vivilet <levivlet@pm.me>
2022-11-08 23:02:55 +0100

    refactor: simplify lazyloading of commands

--- packages/renderer-worker/src/parts/ViewletActivityBar/ViewletActivityBar.ipc.js
+++ packages/renderer-worker/src/parts/ViewletActivityBar/ViewletActivityBar.ipc.js
@@ -1,33 +1,26 @@
 import * as ViewletActivityBar from './ViewletActivityBar.js'
-import * as LazyCommand from '../LazyCommand/LazyCommand.js'
-
-const Imports = {
-  Focus: () => import('./ViewletActivityBarFocus.js'),
-  FocusFirst: () => import('./ViewletActivityBarFocusFirst.js'),
-  FocusIndex: () => import('./ViewletActivityBarFocusIndex.js'),
-  FocusLast: () => import('./ViewletActivityBarFocusLast.js'),
-  FocusNext: () => import('./ViewletActivityBarFocusNext.js'),
-  FocusPrevious: () => import('./ViewletActivityBarFocusPrevious.js'),
-  SelectCurrent: () => import('./ViewletActivityBarSelectCurrent.js'),
-  HandleClick: () => import('./ViewletActivityBarHandleClick.js'),
-}
 
 // prettier-ignore
 export const Commands = {
-  focus: LazyCommand.create(ViewletActivityBar.name, Imports.Focus, 'focus'),
-  focusFirst: LazyCommand.create(ViewletActivityBar.name, Imports.FocusFirst, 'focusFirst'),
-  focusLast: LazyCommand.create(ViewletActivityBar.name, Imports.FocusLast, 'focusLast'),
-  focusNext: LazyCommand.create(ViewletActivityBar.name, Imports.FocusNext, 'focusNext'),
-  focusPrevious: LazyCommand.create(ViewletActivityBar.name, Imports.FocusPrevious, 'focusPrevious'),
   getHiddenItems: ViewletActivityBar.getHiddenItems,
   handleBlur:  ViewletActivityBar.handleBlur,
-  handleClick: LazyCommand.create(ViewletActivityBar.name, Imports.HandleClick, 'handleClick'),
   handleContextMenu:  ViewletActivityBar.handleContextMenu,
   handleSideBarHidden: ViewletActivityBar.handleSideBarHidden,
   handleSideBarViewletChange: ViewletActivityBar.handleSideBarViewletChange,
-  selectCurrent: LazyCommand.create(ViewletActivityBar.name,Imports.SelectCurrent, 'selectCurrent'),
   toggleItem: ViewletActivityBar.toggleItem,
   updateSourceControlCount:  ViewletActivityBar.updateSourceControlCount,
+}
+
+// prettier-ignore
+export const LazyCommands={
+  focus: () => import('./ViewletActivityBarFocus.js'),
+  focusFirst: () => import('./ViewletActivityBarFocusFirst.js'),
+  focusIndex: () => import('./ViewletActivityBarFocusIndex.js'),
+  focusLast: () => import('./ViewletActivityBarFocusLast.js'),
+  focusNext: () => import('./ViewletActivityBarFocusNext.js'),
+  focusPrevious: () => import('./ViewletActivityBarFocusPrevious.js'),
+  selectCurrent: () => import('./ViewletActivityBarSelectCurrent.js'),
+  handleClick: () => import('./ViewletActivityBarHandleClick.js'),
 }
 
 export const Events = {
