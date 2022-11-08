91869979da449abd2d067c4bee1cf1da37c20187
Le Vivilet <levivlet@pm.me>
2022-11-08 22:44:27 +0100

    try to simplify lazyloading of commands

--- packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensions.ipc.js
+++ packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensions.ipc.js
@@ -1,5 +1,4 @@
 import * as ViewletExtensions from './ViewletExtensions.js'
-import * as LazyCommand from '../LazyCommand/LazyCommand.js'
 
 // prettier-ignore
 const Imports = {
@@ -25,29 +24,32 @@ const Imports = {
 // prettier-ignore
 export const Commands = {
   closeSuggest: ViewletExtensions.closeSuggest,
-  focusFirst: LazyCommand.create(ViewletExtensions.name, Imports.FocusFirst, 'focusFirst'),
-  focusIndex: LazyCommand.create(ViewletExtensions.name, Imports.FocusIndex, 'focusIndex'),
-  focusLast: LazyCommand.create(ViewletExtensions.name, Imports.FocusLast, 'focusLast'),
-  focusNext: LazyCommand.create(ViewletExtensions.name, Imports.FocusNext, 'focusNext'),
-  focusNextPage: LazyCommand.create(ViewletExtensions.name, Imports.FocusNextPage, 'focusNextPage'),
-  focusPrevious: LazyCommand.create(ViewletExtensions.name, Imports.FocusPrevious, 'focusPrevious'),
-  focusPreviousPage: LazyCommand.create(Imports.FocusPreviousPage, 'focusPreviousPage'),
-  handleClick: LazyCommand.create(ViewletExtensions.name, Imports.HandleClick, 'handleClick'),
-  handleClickCurrent: LazyCommand.create(ViewletExtensions.name, Imports.HandleClickCurrent, 'handleClickCurrent'),
-  handleClickCurrentButKeepFocus: LazyCommand.create(ViewletExtensions.name, Imports.HandleClickCurrentButKeepFocus, 'handleClickCurrentButKeepFocus'),
   handleContextMenu: ViewletExtensions.handleContextMenu,
   handleInput: ViewletExtensions.handleInput,
   handleInstall: ViewletExtensions.handleInstall,
-  handleScrollBarClick: LazyCommand.create(ViewletExtensions.name, Imports.HandleScrollBarClick, 'handleScrollBarClick'),
-  handleScrollBarMove: LazyCommand.create(ViewletExtensions.name, Imports.HandleScrollBarMove, 'handleScrollBarMove'),
-  handleTouchEnd: LazyCommand.create(ViewletExtensions.name, Imports.HandleTouchEnd, 'handleTouchEnd'),
-  handleTouchMove: LazyCommand.create(ViewletExtensions.name, Imports.HandleTouchMove, 'handleTouchMove'),
-  handleTouchStart: LazyCommand.create(ViewletExtensions.name, Imports.HandleTouchStart, 'handleTouchStart'),
   handleUninstall: ViewletExtensions.handleUninstall,
-  handleWheel: LazyCommand.create(ViewletExtensions.name, Imports.HandleWheel, 'handleWheel'),
   openSuggest: ViewletExtensions.openSuggest,
-  setDeltaY: LazyCommand.create(ViewletExtensions.name, Imports.SetDeltaY, 'setDeltaY'),
   toggleSuggest: ViewletExtensions.toggleSuggest,
+}
+
+export const LazyCommands = {
+  focusFirst: Imports.FocusFirst,
+  focusIndex: Imports.FocusIndex,
+  focusLast: Imports.FocusLast,
+  focusNext: Imports.FocusNext,
+  focusNextPage: Imports.FocusNextPage,
+  focusPrevious: Imports.FocusPrevious,
+  focusPreviousPage: Imports.FocusPreviousPage,
+  handleClick: Imports.HandleClick,
+  handleClickCurrent: Imports.HandleClickCurrent,
+  handleClickCurrentButKeepFocus: Imports.HandleClickCurrentButKeepFocus,
+  handleScrollBarClick: Imports.HandleScrollBarClick,
+  handleScrollBarMove: Imports.HandleScrollBarMove,
+  handleTouchEnd: Imports.HandleTouchEnd,
+  handleTouchMove: Imports.HandleTouchMove,
+  handleTouchStart: Imports.HandleTouchStart,
+  handleWheel: Imports.HandleWheel,
+  setDeltaY: Imports.SetDeltaY,
 }
 
 export const Css = '/css/parts/ViewletExtensions.css'
