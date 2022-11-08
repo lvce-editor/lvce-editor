a0bb52c66fe036e7e1f5bb2863f4abaa428a4839
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-11-08 23:12:25 +0100

    refactor: simplify lazyloading of commands (#1101)

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.ipc.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.ipc.js
@@ -1,22 +1,4 @@
-import * as LazyCommand from '../LazyCommand/LazyCommand.js'
 import * as ViewletExplorer from './ViewletExplorer.js'
-
-// prettier-ignore
-const Imports = {
-  ExpandRecursively: () => import('./ViewletExplorerExpandRecursively.js'),
-  Focus: () => import('./ViewletExplorerFocus.js'),
-  FocusFirst: () => import('./ViewletExplorerFocusFirst.js'),
-  FocusIndex: () => import('./ViewletExplorerFocusIndex.js'),
-  FocusLast: () => import('./ViewletExplorerFocusLast.js'),
-  FocusNext: () => import('./ViewletExplorerFocusNext.js'),
-  FocusNone: () => import('./ViewletExplorerFocusNone.js'),
-  FocusPrevious: () => import('./ViewletExplorerFocusPrevious.js'),
-  HandleContextMenuKeyboard: () => import('./ViewletExplorerHandleContextMenuKeyboard.js'),
-  HandleContextMenuMouseAt: ()=>import('./ViewletExplorerHandleContextMenuMouseAt.js'),
-  HandleDragOver: () => import('./ViewletExplorerHandleDragOver.js'),
-  HandleDrop: () => import('./ViewletExplorerHandleDrop.js'),
-  HandlePaste: () => import('./ViewletExplorerHandlePaste.js'),
-}
 
 export const Events = {
   'languages.changed': ViewletExplorer.handleLanguagesChanged,
@@ -31,14 +13,6 @@ export const Commands = {
   copyPath:                       ViewletExplorer.copyPath,
   copyRelativePath:               ViewletExplorer.copyRelativePath,
   expandAll:                      ViewletExplorer.expandAll,
-  expandRecursively:              LazyCommand.create(ViewletExplorer.name, Imports.ExpandRecursively, 'expandRecursively'),
-  focus:                          LazyCommand.create(ViewletExplorer.name, Imports.Focus, 'focus'),
-  focusFirst:                     LazyCommand.create(ViewletExplorer.name, Imports.FocusFirst, 'focusFirst'),
-  focusIndex:                     LazyCommand.create(ViewletExplorer.name, Imports.FocusIndex, 'focusIndex'),
-  focusLast:                      LazyCommand.create(ViewletExplorer.name, Imports.FocusLast, 'focusLast'),
-  focusNext:                      LazyCommand.create(ViewletExplorer.name, Imports.FocusNext, 'focusNext'),
-  focusNone:                      LazyCommand.create(ViewletExplorer.name, Imports.FocusNone, 'focusNone'),
-  focusPrevious:                  LazyCommand.create(ViewletExplorer.name, Imports.FocusPrevious, 'focusPrevious'),
   getFocusedDirent:               ViewletExplorer.getFocusedDirent,
   handleArrowLeft:                ViewletExplorer.handleArrowLeft,
   handleArrowRight:               ViewletExplorer.handleArrowRight,
@@ -47,14 +21,9 @@ export const Commands = {
   handleClickAt:                  ViewletExplorer.handleClickAt,
   handleClickCurrent:             ViewletExplorer.handleClickCurrent,
   handleClickCurrentButKeepFocus: ViewletExplorer.handleClickCurrentButKeepFocus,
-  handleContextMenuKeyboard:      LazyCommand.create(ViewletExplorer.name, Imports.HandleContextMenuKeyboard, 'handleContextMenuKeyboard'),
-  handleContextMenuMouseAt:       LazyCommand.create(ViewletExplorer.name, Imports.HandleContextMenuMouseAt, 'handleContextMenuMouseAt'),
   handleCopy:                     ViewletExplorer.handleCopy,
-  handleDragOver:                 LazyCommand.create(ViewletExplorer.name, Imports.HandleDragOver, 'handleDragOver'),
-  handleDrop:                     LazyCommand.create(ViewletExplorer.name, Imports.HandleDrop, 'handleDrop'),
   handleMouseEnter:               ViewletExplorer.handleMouseEnter,
   handleMouseLeave:               ViewletExplorer.handleMouseLeave,
-  handlePaste:                    LazyCommand.create(ViewletExplorer.name, Imports.HandlePaste, 'handlePaste'),
   handleWheel:                    ViewletExplorer.handleWheel,
   newFile:                        ViewletExplorer.newFile,
   newFolder:                      ViewletExplorer.newFolder,
@@ -67,6 +36,23 @@ export const Commands = {
   scrollUp:                       ViewletExplorer.scrollUp,
   setDeltaY:                      ViewletExplorer.setDeltaY,
   updateEditingValue:             ViewletExplorer.updateEditingValue,
+}
+
+// prettier-ignore
+export const LazyCommands = {
+  expandRecursively: () => import('./ViewletExplorerExpandRecursively.js'),
+  focus: () => import('./ViewletExplorerFocus.js'),
+  focusFirst: () => import('./ViewletExplorerFocusFirst.js'),
+  focusIndex: () => import('./ViewletExplorerFocusIndex.js'),
+  focusLast: () => import('./ViewletExplorerFocusLast.js'),
+  focusNext: () => import('./ViewletExplorerFocusNext.js'),
+  focusNone: () => import('./ViewletExplorerFocusNone.js'),
+  focusPrevious: () => import('./ViewletExplorerFocusPrevious.js'),
+  handleContextMenuKeyboard: () => import('./ViewletExplorerHandleContextMenuKeyboard.js'),
+  handleContextMenuMouseAt: ()=>import('./ViewletExplorerHandleContextMenuMouseAt.js'),
+  handleDragOver: () => import('./ViewletExplorerHandleDragOver.js'),
+  handleDrop: () => import('./ViewletExplorerHandleDrop.js'),
+  handlePaste: () => import('./ViewletExplorerHandlePaste.js'),
 }
 
 export const Css = '/css/parts/ViewletExplorer.css'
--- packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPick.ipc.js
+++ packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPick.ipc.js
@@ -1,22 +1,7 @@
 import * as ViewletQuickPick from './ViewletQuickPick.js'
-import * as LazyCommand from '../LazyCommand/LazyCommand.js'
-
-// prettier-ignore
-const Imports = {
-  FocusFirst: () => import('./ViewletQuickPickFocusFirst.js'),
-  FocusIndex: () => import('./ViewletQuickPickFocusIndex.js'),
-  FocusLast: () => import('./ViewletQuickPickFocusLast.js'),
-  FocusNext: () => import('./ViewletQuickPickFocusNext.js'),
-  FocusPrevious: () => import('./ViewletQuickPickFocusPrevious.js'),
-}
 
 // prettier-ignore
 export const Commands = {
-  focusFirst: LazyCommand.create(ViewletQuickPick.name, Imports.FocusFirst, 'focusFirst'),
-  focusIndex: LazyCommand.create(ViewletQuickPick.name, Imports.FocusIndex, 'focusIndex'),
-  focusLast: LazyCommand.create(ViewletQuickPick.name, Imports.FocusLast, 'focusLast'),
-  focusNext: LazyCommand.create(ViewletQuickPick.name, Imports.FocusNext, 'focusNext'),
-  focusPrevious: LazyCommand.create(ViewletQuickPick.name, Imports.FocusPrevious, 'focusPrevious'),
   handleBeforeInput: ViewletQuickPick.handleBeforeInput,
   handleBlur: ViewletQuickPick.handleBlur,
   handleClickAt: ViewletQuickPick.handleClickAt,
@@ -25,6 +10,15 @@ export const Commands = {
   selectCurrentIndex: ViewletQuickPick.selectCurrentIndex,
   selectIndex: ViewletQuickPick.selectIndex,
   selectItem: ViewletQuickPick.selectItem,
+}
+
+// prettier-ignore
+export const LazyCommands={
+  focusFirst: () => import('./ViewletQuickPickFocusFirst.js'),
+  focusIndex: () => import('./ViewletQuickPickFocusIndex.js'),
+  focusLast: () => import('./ViewletQuickPickFocusLast.js'),
+  focusNext: () => import('./ViewletQuickPickFocusNext.js'),
+  focusPrevious: () => import('./ViewletQuickPickFocusPrevious.js'),
 }
 
 export const Css = '/css/parts/ViewletQuickPick.css'
