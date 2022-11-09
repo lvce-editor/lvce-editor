0102428c546a93bc51aac7832c3850561520ba80
Le Vivilet <levivlet@pm.me>
2022-11-09 15:58:32 +0100

    fix: editor selection not working

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandMoveSelectionPx.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandMoveSelectionPx.js
@@ -2,7 +2,7 @@ import * as Assert from '../Assert/Assert.js'
 import * as EditorMoveSelection from './EditorCommandMoveSelection.js'
 import * as EditorPosition from './EditorCommandPosition.js'
 
-export const editorMoveSelectionPx = (editor, x, y, offset) => {
+export const moveSelectionPx = (editor, x, y, offset) => {
   Assert.object(editor)
   Assert.number(x)
   Assert.number(y)
