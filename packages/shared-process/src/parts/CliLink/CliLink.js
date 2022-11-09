be6fb1260b47f52b32ff01256321edfdb5b02d3e
Le Vivilet <levivlet@pm.me>
2022-11-09 19:29:16 +0100

    fix: improve error handling when linking cli fails

--- packages/shared-process/src/parts/CliLink/CliLink.js
+++ packages/shared-process/src/parts/CliLink/CliLink.js
@@ -1,6 +1,16 @@
 import * as ExtensionLink from '../ExtensionLink/ExtensionLink.js'
+import * as FileSystemErrorCodes from '../FileSystemErrorCodes/FileSystemErrorCodes.js'
 
-export const handleCliArgs = async (argv, console) => {
-  const path = argv.length > 1 ? argv[1] : process.cwd()
-  await ExtensionLink.link(path)
+export const handleCliArgs = async (argv) => {
+  try {
+    const path = argv.length > 1 ? argv[1] : process.cwd()
+    await ExtensionLink.link(path)
+  } catch (error) {
+    if (error && error.code === FileSystemErrorCodes.E_MANIFEST_NOT_FOUND) {
+      console.error(error.message)
+      process.exitCode = 128
+      return
+    }
+    throw error
+  }
 }
