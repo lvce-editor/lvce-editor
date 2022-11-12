a5f5175075b2fcf49c46cad6c6349e836dd3f002
Le Vivilet <levivlet@pm.me>
2022-11-12 18:11:55 +0100

    fix: improve build error handling when electron fails to download with 503 error

--- build/src/parts/DownloadElectron/DownloadElectron.js
+++ build/src/parts/DownloadElectron/DownloadElectron.js
@@ -1,4 +1,5 @@
 import * as ElectronGet from '@electron/get'
+import { VError } from 'verror'
 import * as ExtractZip from '../ExtractZip/ExtractZip.js'
 
 export const downloadElectron = async ({
@@ -7,14 +8,22 @@ export const downloadElectron = async ({
   platform,
   arch,
 }) => {
-  const zipFilePath = await ElectronGet.downloadArtifact({
-    version: electronVersion,
-    platform,
-    artifactName: 'electron',
-    arch,
-  })
-  await ExtractZip.extractZip({
-    inFile: zipFilePath,
-    outDir,
-  })
+  try {
+    const zipFilePath = await ElectronGet.downloadArtifact({
+      version: electronVersion,
+      platform,
+      artifactName: 'electron',
+      arch,
+    })
+    await ExtractZip.extractZip({
+      inFile: zipFilePath,
+      outDir,
+    })
+  } catch (error) {
+    // @ts-ignore
+    throw new VError(
+      error,
+      `Failed to download electron version ${electronVersion}`
+    )
+  }
 }
