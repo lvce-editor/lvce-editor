4a7ad7055a026178f1fccbf4ccdff7f30cb7fc01
Le Vivilet <levivlet@pm.me>
2022-11-14 22:01:50 +0100

    try to implement ad blocker for youtube (wip)

--- packages/main-process/src/parts/ElectronSessionForBrowserView/ElectronSessionForBrowserView.js
+++ packages/main-process/src/parts/ElectronSessionForBrowserView/ElectronSessionForBrowserView.js
@@ -5,6 +5,8 @@ const Platform = require('../Platform/Platform.js')
 const FileSystem = require('../FileSystem/FileSystem.js')
 const ElectronPermissionType = require('../ElectronPermissionType/ElectronPermissionType.js')
 const FileSystemErrorCodes = require('../FileSystemErrorCodes/FileSystemErrorCodes.js')
+const ElectronResourceType = require('../ElectronResourceType/ElectronResourceType.js')
+const HttpMethod = require('../HttpMethod/HttpMethod.js')
 
 const state = {
   /**
@@ -80,13 +82,118 @@ const addSessionChromeExtensions = async (session) => {
   }
 }
 
+const getBeforeRequestResponseMainFrame = (url) => {
+  return {}
+}
+
+const cancelIfStartsWith = (url, canceledUrls) => {
+  for (const canceledUrl of canceledUrls) {
+    if (url.startsWith(canceledUrl)) {
+      return {
+        cancel: true,
+      }
+    }
+  }
+  return {}
+}
+
+const getBeforeRequestResponseXhrPost = (url) => {
+  const canceledUrls = [
+    'https://www.youtube.com/api/stats/qoe',
+    'https://www.youtube.com/youtubei/v1/log_event',
+  ]
+  return cancelIfStartsWith(url, canceledUrls)
+}
+
+const getBeforeRequestResponseXhrGet = (url) => {
+  const canceledUrls = [
+    'https://www.youtube.com/pagead/',
+    'https://www.youtube.com/api/stats/qoe',
+    'https://www.youtube.com/api/stats/ads',
+    'https://www.youtube.com/api/stats/delayplay',
+    'https://www.youtube.com/ptracking',
+    'https://www.youtube.com/api/timedtext',
+    'https://www.youtube.com/pcs/activeview',
+  ]
+  return cancelIfStartsWith(url, canceledUrls)
+}
+
+const getBeforeRequestResponseXhrHead = (url) => {
+  const canceledUrls = ['https://www.youtube.com/generate_204']
+  return cancelIfStartsWith(url, canceledUrls)
+}
+
+const getBeforeRequestResponseXhr = (method, url) => {
+  switch (method) {
+    case HttpMethod.Post:
+      return getBeforeRequestResponseXhrPost(url)
+    case HttpMethod.Get:
+      return getBeforeRequestResponseXhrGet(url)
+    case HttpMethod.Head:
+      return getBeforeRequestResponseXhrHead(url)
+    default:
+      return {}
+  }
+}
+
+const getBeforeRequestResponse = (details) => {
+  const {
+    id,
+    method,
+    referrer,
+    resourceType,
+    timestamp,
+    uploadData,
+    url,
+    frame,
+  } = details
+
+  switch (resourceType) {
+    case ElectronResourceType.MainFrame:
+      return getBeforeRequestResponseMainFrame(url)
+    case ElectronResourceType.Stylesheet:
+      return {}
+    case ElectronResourceType.Script:
+      return {}
+    case ElectronResourceType.Xhr:
+      return getBeforeRequestResponseXhr(method, url)
+    default:
+      return {
+        cancel: true,
+      }
+  }
+}
+
+/**
+ *
+ * @param {Electron.OnBeforeRequestListenerDetails } details
+ * @param {(response: globalThis.Electron.CallbackResponse) => void} callback
+ */
+const handleBeforeRequest = (details, callback) => {
+  const response = getBeforeRequestResponse(details)
+  if (!response.cancel && details.resourceType === ElectronResourceType.Xhr) {
+    console.log({
+      resourceType: details.resourceType,
+      url: details.url,
+      method: details.method,
+    })
+  }
+  callback(response)
+}
+
 const createSession = () => {
-  const session = Electron.session.fromPartition('persist:browserView', {
+  const sessionId = `persist:browserView` + Math.random()
+  const session = Electron.session.fromPartition(sessionId, {
     cache: true,
   })
   session.setPermissionRequestHandler(handlePermissionRequest)
   session.setPermissionCheckHandler(handlePermissionCheck)
-  addSessionChromeExtensions(session)
+  const filter = {
+    urls: ['https://*.youtube.com/*'],
+    // urls: ['<all_urls>'],
+  }
+  session.webRequest.onBeforeRequest(filter, handleBeforeRequest)
+  // session.webRequest.addSessionChromeExtensions(session)
   return session
 }
 
