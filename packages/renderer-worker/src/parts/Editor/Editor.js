d01fdc2619094f2fa6bdcadca274b79ecb2a7496
Le Vivilet <levivlet@pm.me>
2022-11-17 21:31:23 +0100

    refactor: move split lines function into separate module

--- packages/renderer-worker/src/parts/Editor/Editor.js
+++ packages/renderer-worker/src/parts/Editor/Editor.js
@@ -3,6 +3,7 @@ import * as GlobalEventBus from '../GlobalEventBus/GlobalEventBus.js'
 import * as RendererProcess from '../RendererProcess/RendererProcess.js'
 import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.js'
 import * as TextDocument from '../TextDocument/TextDocument.js'
+import * as SplitLines from '../SplitLines/SplitLines.js'
 import * as Tokenizer from '../Tokenizer/Tokenizer.js'
 import * as EditorCursor from './EditorCursor.js'
 import * as EditorScrolling from './EditorScrolling.js'
@@ -19,7 +20,7 @@ export const create = (id, uri, languageId, content) => {
   return {
     uri,
     languageId,
-    lines: content.split('\n'),
+    lines: SplitLines.splitLines(content),
     // TODO cursor should be part of selection,
     // codemirror has selections: Selection[]
     //            selectionIndex: number
@@ -304,7 +305,7 @@ export const setBounds = (editor, top, left, height, columnWidth) => {
 }
 
 export const setText = (editor, text) => {
-  const lines = text.split('\n')
+  const lines = SplitLines.splitLines(text)
   const { itemHeight, numberOfVisibleLines } = editor
   const total = lines.length
   const maxLineY = Math.min(numberOfVisibleLines, total)
