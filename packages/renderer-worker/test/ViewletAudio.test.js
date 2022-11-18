930bd30b59622ddf4dbfa1529a2f2426fbe252b2
Le Vivilet <levivlet@pm.me>
2022-11-19 00:15:30 +0100

    add more tests for audio component

---
+++ packages/renderer-worker/test/ViewletAudio.test.js
@@ -0,0 +1,48 @@
+import { jest } from '@jest/globals'
+import * as ActivityBarItemFlags from '../src/parts/ActivityBarItemFlags/ActivityBarItemFlags.js'
+import * as ViewletStates from '../src/parts/ViewletStates/ViewletStates.js'
+import * as Icon from '../src/parts/Icon/Icon.js'
+import * as ViewletModuleId from '../src/parts/ViewletModuleId/ViewletModuleId.js'
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule('../src/parts/BlobSrc/BlobSrc.js', () => {
+  return {
+    getSrc(uri) {
+      return '/remote' + uri
+    },
+    disposeSrc: jest.fn(),
+  }
+})
+
+const ViewletAudio = await import('../src/parts/ViewletAudio/ViewletAudio.js')
+const BlobSrc = await import('../src/parts/BlobSrc/BlobSrc.js')
+
+test('create', () => {
+  const state = ViewletAudio.create()
+  expect(state).toBeDefined()
+})
+
+test('loadContent', async () => {
+  const state = {
+    ...ViewletAudio.create(),
+    uri: '/test.mp3',
+  }
+  expect(await ViewletAudio.loadContent(state)).toMatchObject({
+    src: '/remote/test.mp3',
+  })
+})
+
+test('dispose', async () => {
+  // @ts-ignore
+  BlobSrc.disposeSrc.mockImplementation(() => {})
+  const state = {
+    ...ViewletAudio.create(),
+    src: '/remote/test.mp3',
+  }
+  await ViewletAudio.dispose(state)
+  expect(BlobSrc.disposeSrc).toBeCalledTimes(1)
+  expect(BlobSrc.disposeSrc).toHaveBeenCalledWith('/remote/test.mp3')
+})
