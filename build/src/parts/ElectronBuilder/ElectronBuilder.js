c19cd0fed615e5ee15b9c8380f352fb3874d17f3
Le Vivilet <levivlet@pm.me>
2022-11-20 18:31:55 +0100

    simplify build code: use jsonfile.readJson

--- build/src/parts/ElectronBuilder/ElectronBuilder.js
+++ build/src/parts/ElectronBuilder/ElectronBuilder.js
@@ -1,14 +1,14 @@
+import * as ElectronBuilder from 'electron-builder'
 import { existsSync } from 'node:fs'
 import { readdir } from 'node:fs/promises'
-import * as ElectronBuilder from 'electron-builder'
 import * as Copy from '../Copy/Copy.js'
+import * as JsonFile from '../JsonFile/JsonFile.js'
 import * as Path from '../Path/Path.js'
 import * as Product from '../Product/Product.js'
-import * as ReadFile from '../ReadFile/ReadFile.js'
-import * as Stat from '../Stat/Stat.js'
-import * as Template from '../Template/Template.js'
 import * as Rename from '../Rename/Rename.js'
+import * as Stat from '../Stat/Stat.js'
 import * as Tag from '../Tag/Tag.js'
+import * as Template from '../Template/Template.js'
 
 // TODO don't need to include whole node-pty module
 // TODO maybe don't need to include nan module
@@ -24,10 +24,7 @@ const bundleElectronMaybe = async () => {
 }
 
 const getElectronVersion = async () => {
-  const packageJson = await ReadFile.readFile(
-    'packages/main-process/package.json'
-  )
-  const parsed = JSON.parse(packageJson)
+  const parsed = await JsonFile.readJson('packages/main-process/package.json')
   const version = parsed.devDependencies.electron
   const parsedVersion = version.startsWith('^') ? version.slice(1) : version
   return parsedVersion
