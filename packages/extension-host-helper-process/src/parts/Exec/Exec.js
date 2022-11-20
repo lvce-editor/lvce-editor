dadf2d0707e48f4ba35641439c862aa449e0dfdc
Le Vivilet <levivlet@pm.me>
2022-11-20 18:29:13 +0100

    remove console logs

--- packages/extension-host-helper-process/src/parts/Exec/Exec.js
+++ packages/extension-host-helper-process/src/parts/Exec/Exec.js
@@ -2,7 +2,6 @@ import { execa } from 'execa'
 import * as Assert from '../Assert/Assert.js'
 
 export const exec = async (file, args, options) => {
-  console.log({ file, args, options })
   Assert.string(file)
   Assert.array(args)
   const { stdout, stderr, exitCode } = await execa(file, args, options)
