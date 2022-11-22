bdbbd7c7f7154f5f7f98ea9cc959edf8b79e3db2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-11-23 00:42:45 +0100

    refactor: clean up duplicate json rpc error handling code in renderer… (#1243)

--- packages/renderer-worker/src/parts/JsonRpc/JsonRpc.js
+++ packages/renderer-worker/src/parts/JsonRpc/JsonRpc.js
@@ -1,23 +1,93 @@
 import * as Callback from '../Callback/Callback.js'
+import { JsonRpcError } from '../Errors/Errors.js'
+import * as JsonRpcErrorCode from '../JsonRpcErrorCode/JsonRpcErrorCode.js'
 import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
+import { VError } from '../VError/VError.js'
 
-export const send = (transport, method, ...parameters) => {
+export const send = (transport, method, ...params) => {
   transport.send({
     jsonrpc: JsonRpcVersion.Two,
     method,
-    params: parameters,
+    params: params,
   })
 }
 
-export const invoke = (transport, method, ...parameters) => {
-  return new Promise((resolve, reject) => {
+const getErrorConstructor = (message) => {
+  if (message.startsWith('TypeError: ')) {
+    return TypeError
+  }
+  if (message.startsWith('SyntaxError: ')) {
+    return SyntaxError
+  }
+  if (message.startsWith('ReferenceError: ')) {
+    return ReferenceError
+  }
+  return Error
+}
+
+const constructError = (message) => {
+  const ErrorConstructor = getErrorConstructor(message)
+  if (ErrorConstructor === Error) {
+    return new Error(message)
+  }
+  const shortMessage = message.slice(ErrorConstructor.name.length + 2)
+  return new ErrorConstructor(shortMessage)
+}
+
+const restoreError = (error) => {
+  if (error && error instanceof Error) {
+    return error
+  }
+  if (error && error.code && error.code === JsonRpcErrorCode.MethodNotFound) {
+    const restoredError = new JsonRpcError(error.message)
+    restoredError.stack = error.stack
+    return restoredError
+  }
+  if (error && error.message) {
+    const restoredError = constructError(error.message)
+    if (error.data) {
+      if (error.data.stack) {
+        restoredError.stack = error.data.stack
+
+        if (error.data.codeFrame) {
+          // @ts-ignore
+          restoredError.codeFrame = error.data.codeFrame
+        }
+      }
+    } else if (error.stack) {
+      // TODO accessing stack might be slow
+      const lowerStack = restoredError.stack
+      // @ts-ignore
+      const indexNewLine = lowerStack.indexOf('\n')
+      // @ts-ignore
+      restoredError.stack = error.stack + lowerStack.slice(indexNewLine)
+    }
+    return restoredError
+  }
+  if (typeof error === 'string') {
+    return new Error(`JsonRpc Error: ${error}`)
+  }
+  return new Error(`JsonRpc Error: ${error}`)
+}
+
+export const invoke = async (transport, method, ...params) => {
+  const responseMessage = await new Promise((resolve, reject) => {
     // TODO use one map instead of two
     const callbackId = Callback.register(resolve, reject)
     transport.send({
       jsonrpc: JsonRpcVersion.Two,
       method,
-      params: parameters,
+      params: params,
       id: callbackId,
     })
   })
+  if ('error' in responseMessage) {
+    const restoredError = restoreError(responseMessage.error)
+    throw restoredError
+  }
+  if ('result' in responseMessage) {
+    return responseMessage.result
+  }
+
+  throw new Error('unexpected response message')
 }
--- packages/renderer-worker/test/RendererProcess.test.js
+++ packages/renderer-worker/test/RendererProcess.test.js
@@ -31,10 +31,6 @@ test('invoke - error', async () => {
       })
     },
   }
-  const expectedError = new Error(
-    'Test failed: sample.tab-completion-provider: expected selector .Viewlet.Editor to have text "test3" but was "test"'
-  )
-  expectedError.stack = ''
   const error = await getError(
     RendererProcess.invoke(
       'TestFrameWork.checkSingleElementCondition',
@@ -55,12 +51,14 @@ test('invoke - error', async () => {
   expect(error.message).toBe(
     `Test failed: sample.tab-completion-provider: expected selector .Viewlet.Editor to have text \"test3\" but was \"test\"`
   )
+  // TODO constructError stack is not really relevant, should only include stack from
+  // rendererprocess.invoke onwards
   // @ts-ignore
   expect(error.stack).toMatch(
     `Error: expected selector .Viewlet.Editor to have text \"test3\" but was \"test\"
     at Object.checkSingleElementCondition [as TestFrameWork.checkSingleElementCondition] (http://localhost/packages/renderer-process/src/parts/TestFrameWork/TestFrameWork.js:122:9)
     at async Worker.handleMessageFromRendererWorker (http://localhost/packages/renderer-process/src/parts/RendererWorker/RendererWorker.js:46:24)
-    at Module.invoke`
+    at constructError`
   )
 })
 
