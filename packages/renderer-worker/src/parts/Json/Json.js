59e0b5671ca14a07530e86d9ae7d22cb459b3a32
Le Vivilet <levivlet@pm.me>
2022-11-23 20:31:04 +0100

    fix: don't prettify localstorage json, decreases localstorage string size by ~50%

--- packages/renderer-worker/src/parts/Json/Json.js
+++ packages/renderer-worker/src/parts/Json/Json.js
@@ -4,6 +4,10 @@ export const stringify = (value) => {
   return JSON.stringify(value, null, 2) + '\n'
 }
 
+export const stringifyCompact = (value) => {
+  return JSON.stringify(value)
+}
+
 export const parse = (content) => {
   // TODO use better json parse to throw more helpful error messages if json is invalid
   try {
