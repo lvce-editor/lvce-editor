b0aed616ed819a8d89a71f9d3f5382caf78acb73
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-11-25 01:24:21 +0100

    fix: improve error handling when process explorer window fails to open (#1274)

--- packages/renderer-worker/src/parts/ElectronWindowProcessExplorer/ElectronWindowProcessExplorer.js
+++ packages/renderer-worker/src/parts/ElectronWindowProcessExplorer/ElectronWindowProcessExplorer.js
@@ -1,5 +1,10 @@
 import * as ElectronProcess from '../ElectronProcess/ElectronProcess.js'
+import { VError } from '../VError/VError.js'
 
 export const open = async () => {
-  await ElectronProcess.invoke('ElectronWindowProcessExplorer.open')
+  try {
+    await ElectronProcess.invoke('ElectronWindowProcessExplorer.open')
+  } catch (error) {
+    throw new VError(error, `Failed to open process explorer`)
+  }
 }
