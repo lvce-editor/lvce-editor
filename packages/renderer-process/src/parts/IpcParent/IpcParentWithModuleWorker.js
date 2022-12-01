3a67da383fb8771dada8e41202996beaf8322088
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-12-01 10:18:18 +0100

    Restore IpcParentWithModuleWorker.js

--- packages/renderer-process/src/parts/IpcParent/IpcParentWithModuleWorker.js
+++ packages/renderer-process/src/parts/IpcParent/IpcParentWithModuleWorker.js
@@ -64,8 +64,6 @@ export const create = async ({ url, name }) => {
         }
       }
       const handleFirstError = async (event) => {
-        console.log({ event })
-        console.log(event.message)
         cleanup()
         if (isFirefoxError(event.message)) {
           event.preventDefault()
