006453d626b86b02db19011429304634e80f4202
Le Vivilet <levivlet@pm.me>
2022-12-06 14:14:07 +0100

    try to make about window a modal dialog similar to vscode (wip)

--- packages/main-process/src/parts/ElectronWindowAbout/ElectronWindowAbout.js
+++ packages/main-process/src/parts/ElectronWindowAbout/ElectronWindowAbout.js
@@ -1,47 +1,16 @@
-const Electron = require('electron')
+const { dialog } = require('electron')
 const Session = require('../ElectronSession/ElectronSession.js')
 const Platform = require('../Platform/Platform.js')
 const Logger = require('../Logger/Logger.js')
-
-// const __dirname = dirname(fileURLToPath(import.meta.url))
+const { BrowserWindow } = require('electron')
 
 /**
  * Show an about window, similar to https://github.com/rhysd/electron-about-window
  */
 exports.open = async () => {
-  const focusedWindow = Electron.BrowserWindow.getFocusedWindow()
+  const focusedWindow = BrowserWindow.getFocusedWindow()
   if (!focusedWindow) {
     return
   }
-  const aboutWindow = new Electron.BrowserWindow({
-    width: 400,
-    height: 400,
-    minWidth: 350,
-    minHeight: 350,
-    titleBarStyle: 'hiddenInset',
-    parent: focusedWindow,
-    title: 'About Code Editor',
-    center: true,
-    // autoHideMenuBar: true,
-    webPreferences: {
-      session: Session.get(),
-      contextIsolation: false,
-      sandbox: false,
-      nodeIntegration: true,
-    },
-    show: false,
-  })
-  aboutWindow.setMenuBarVisibility(false)
-  // aboutWindow.removeMenu()
-  const handleReadyToShow = () => {
-    aboutWindow.show()
-  }
-  aboutWindow.once('ready-to-show', handleReadyToShow)
-  const aboutWindowUrl = `${Platform.scheme}://-/packages/main-process/pages/about/about.html`
-  try {
-    await aboutWindow.loadURL(aboutWindowUrl)
-  } catch (error) {
-    Logger.warn('Failed to load about window')
-    Logger.warn(error)
-  }
+  await dialog.showMessageBox(focusedWindow, { message: 'abc' })
 }
