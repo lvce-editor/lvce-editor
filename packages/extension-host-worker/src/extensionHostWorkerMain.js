95fc68a7f3ff50d31fcda08b6c5f69691d096fa3
Le Vivilet <levivlet@pm.me>
2022-12-06 14:43:30 +0100

    refactor: simplify extension host worker ipc code

--- packages/extension-host-worker/src/extensionHostWorkerMain.js
+++ packages/extension-host-worker/src/extensionHostWorkerMain.js
@@ -1,10 +1,11 @@
 import * as Api from './parts/Api/Api.js'
 import * as IpcChild from './parts/IpcChild/IpcChild.js'
+import * as IpcChildType from './parts/IpcChildType/IpcChildType.js'
 import * as Rpc from './parts/Rpc/Rpc.js'
 
 const main = async () => {
   globalThis.vscode = Api.api
-  const ipc = await IpcChild.listen({ method: IpcChild.Methods.Auto })
+  const ipc = await IpcChild.listen({ method: IpcChildType.Auto() })
   Rpc.listen(ipc)
 }
 
