2b14aefa64518a53f034e2b99b0c1f5dfedf5390
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-12-08 14:28:45 +0100

    refactor: extract ExtensionHostWorker.getResponse function into separ… (#1380)

--- packages/extension-host-worker/src/parts/Rpc/Rpc.js
+++ packages/extension-host-worker/src/parts/Rpc/Rpc.js
@@ -1,46 +1,11 @@
-import * as Command from '../Command/Command.js'
-import * as JsonRpcErrorCode from '../JsonRpcErrorCode/JsonRpcErrorCode.js'
-import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
 import * as Callback from '../Callback/Callback.js'
-
-const getResponse = async (message) => {
-  try {
-    const result = await Command.execute(message.method, ...message.params)
-    return {
-      jsonrpc: JsonRpcVersion.Two,
-      id: message.id,
-      result,
-    }
-  } catch (error) {
-    if (
-      error &&
-      error instanceof Error &&
-      error.message &&
-      error.message.startsWith('method not found')
-    ) {
-      return {
-        jsonrpc: JsonRpcVersion.Two,
-        id: message.id,
-        error: {
-          code: JsonRpcErrorCode.MethodNotFound,
-          message: error.message,
-          data: error.stack,
-        },
-      }
-    }
-    return {
-      jsonrpc: JsonRpcVersion.Two,
-      id: message.id,
-      error,
-    }
-  }
-}
+import * as GetResponse from '../GetResponse/GetResponse.js'
 
 export const listen = (ipc) => {
   const handleMessage = async (event) => {
     const message = event.data
     if ('method' in message) {
-      const response = await getResponse(message)
+      const response = await GetResponse.getResponse(message)
       ipc.send(response)
     } else if ('result' in message) {
       Callback.resolve(message.id, message.result)
