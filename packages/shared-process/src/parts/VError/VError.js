bfb8167faa685edbde770f4717dd7553b6239983
Le Vivilet <levivlet@pm.me>
2022-12-08 17:19:08 +0100

    fix: improve error handling when extension symlink fails

---
+++ packages/shared-process/src/parts/VError/VError.js
@@ -0,0 +1,44 @@
+const getCombinedMessage = (error, message) => {
+  let stringifiedError = `${error}`
+  if (stringifiedError.startsWith('Error: ')) {
+    stringifiedError = stringifiedError.slice(`Error: `.length)
+  } else if (stringifiedError.startsWith('VError: ')) {
+    stringifiedError = stringifiedError.slice(`VError: `.length)
+  }
+  if (message) {
+    return `${message}: ${stringifiedError}`
+  }
+  return stringifiedError
+}
+
+const mergeStacks = (parent, child) => {
+  if (!child) {
+    return parent
+  }
+  const parentNewLineIndex = parent.indexOf('\n')
+  const childNewLineIndex = child.indexOf('\n')
+  const parentFirstLine = parent.slice(0, parentNewLineIndex)
+  const childRest = child.slice(childNewLineIndex)
+  const childFirstLine = child.slice(0, childNewLineIndex)
+  if (parentFirstLine.includes(childFirstLine)) {
+    return parentFirstLine + childRest
+  }
+  return child
+}
+
+export class VError extends Error {
+  constructor(error, message) {
+    const combinedMessage = getCombinedMessage(error, message)
+    super(combinedMessage)
+    this.name = 'VError'
+    if (error instanceof Error) {
+      this.stack = mergeStacks(this.stack, error.stack)
+    }
+    if (error.codeFrame) {
+      this.codeFrame = error.codeFrame
+    }
+    if (error.code) {
+      this.code = error.code
+    }
+  }
+}
