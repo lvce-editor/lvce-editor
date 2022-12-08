a9ffaf08b5352cb5bd8dcb09103eed0b9261930a
Le Vivilet <levivlet@pm.me>
2022-12-08 14:14:19 +0100

    refactor: extract ExtensionHostWorker.getResponse function into separate module

---
+++ packages/extension-host-worker/src/parts/GetResponse/GetResponse.js
@@ -0,0 +1,36 @@
+import * as Command from '../Command/Command.js'
+import * as JsonRpcErrorCode from '../JsonRpcErrorCode/JsonRpcErrorCode.js'
+import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
+
+export const getResponse = async (message) => {
+  try {
+    const result = await Command.execute(message.method, ...message.params)
+    return {
+      jsonrpc: JsonRpcVersion.Two,
+      id: message.id,
+      result,
+    }
+  } catch (error) {
+    if (
+      error &&
+      error instanceof Error &&
+      error.message &&
+      error.message.startsWith('method not found')
+    ) {
+      return {
+        jsonrpc: JsonRpcVersion.Two,
+        id: message.id,
+        error: {
+          code: JsonRpcErrorCode.MethodNotFound,
+          message: error.message,
+          data: error.stack,
+        },
+      }
+    }
+    return {
+      jsonrpc: JsonRpcVersion.Two,
+      id: message.id,
+      error,
+    }
+  }
+}
