bc1f9305af61b55f11a1eb86e352d7caf5fff809
Le Vivilet <levivlet@pm.me>
2022-12-10 10:18:13 +0100

    refactor: extract renderer process error handling into separate module

--- packages/renderer-process/src/rendererProcessMain.js
+++ packages/renderer-process/src/rendererProcessMain.js
@@ -1,30 +1,17 @@
 import * as Command from './parts/Command/Command.js'
+import * as ErrorHandling from './parts/ErrorHandling/ErrorHandling.js'
 import * as Module from './parts/Module/Module.js'
 import * as Platform from './parts/Platform/Platform.js'
 import * as PlatformType from './parts/PlatformType/PlatformType.js'
 import * as RendererWorker from './parts/RendererWorker/RendererWorker.js'
-
-const handleError = (error) => {
-  if (`${error}` === 'Script error.') {
-    // ignore errors from chrome extensions
-    return
-  }
-  console.info(`[renderer-process] Unhandled Error: ${error}`)
-  alert(error)
-}
-
-const handleUnhandledRejection = (event) => {
-  console.info(`[renderer-process] Unhandled Rejection: ${event.reason}`)
-  alert(event.reason)
-}
 
 const handleBeforeInstallPrompt = (event) => {
   event.preventDefault()
 }
 
 const main = async () => {
-  onerror = handleError
-  onunhandledrejection = handleUnhandledRejection
+  onerror = ErrorHandling.handleError
+  onunhandledrejection = ErrorHandling.handleUnhandledRejection
   Command.setLoad(Module.load)
   if (Platform.platform === PlatformType.Web) {
     // disable prompt to download app as pwa
