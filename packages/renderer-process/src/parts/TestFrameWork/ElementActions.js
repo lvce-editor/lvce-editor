ec26c1be9a30247068b447c39242ca57e781796f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-12-11 08:30:50 +0100

    Restore ElementActions.js

--- packages/renderer-process/src/parts/TestFrameWork/ElementActions.js
+++ packages/renderer-process/src/parts/TestFrameWork/ElementActions.js
@@ -1,25 +1,23 @@
-import * as DomEventType from '../DomEventType/DomEventType.js'
-
 export const mouseEvent = (element, eventType, options) => {
   const event = new MouseEvent(eventType, options)
   element.dispatchEvent(event)
 }
 
 export const mouseDown = (element, options) => {
-  mouseEvent(element, DomEventType.MouseDown, options)
+  mouseEvent(element, 'mousedown', options)
 }
 
 export const mouseUp = (element, options) => {
-  mouseEvent(element, DomEventType.MouseUp, options)
+  mouseEvent(element, 'mouseup', options)
 }
 
 export const contextMenu = (element, options) => {
-  mouseEvent(element, DomEventType.ContextMenu,, options)
+  mouseEvent(element, 'contextmenu', options)
 }
 
 export const click = (element, options) => {
   mouseDown(element, options)
-  mouseEvent(element, DomEventType.Click, options)
+  mouseEvent(element, 'click', options)
   mouseUp(element, options)
   if (options.button === 2 /* right */) {
     contextMenu(element, options)
@@ -27,7 +25,7 @@ export const click = (element, options) => {
 }
 
 export const hover = (element, options) => {
-  mouseEvent(element, DomEventType.MouseEnter, options)
+  mouseEvent(element, 'mouseenter', options)
 }
 
 export const type = (element, options) => {
@@ -40,20 +38,20 @@ export const keyboardEvent = (element, eventType, options) => {
 }
 
 export const keyDown = (element, options) => {
-  keyboardEvent(element, DomEventType.KeyDown, options)
+  keyboardEvent(element, 'keydown', options)
 }
 
 export const keyUp = (element, options) => {
-  keyboardEvent(element, DomEventType.KeyUp,, options)
+  keyboardEvent(element, 'keyup', options)
 }
 
 const getEventClass = (eventType) => {
   switch (eventType) {
-    case DomEventType.Wheel:
+    case 'wheel':
       return WheelEvent
-    case DomEventType.PointerDown:
-    case DomEventType.PointerUp:
-    case DomEventType.PointerMove:
+    case 'pointerdown':
+    case 'pointerup':
+    case 'pointermove':
       return PointerEvent
     default:
       return Event
@@ -61,7 +59,7 @@ const getEventClass = (eventType) => {
 }
 
 export const dispatchEvent = (element, options) => {
-  const EventClass = getEventClass(options.type)
-  const event = new EventClass(options.type, options.init)
+  const eventClass = getEventClass(options.type)
+  const event = new eventClass(options.type, options.init)
   element.dispatchEvent(event)
 }
