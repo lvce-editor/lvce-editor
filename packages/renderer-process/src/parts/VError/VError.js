818f2caf9ddef757cdf5a566f1c647fd1b61f8a5
Le Vivilet <levivlet@pm.me>
2022-12-11 08:34:13 +0100

    remove console log

--- packages/renderer-process/src/parts/VError/VError.js
+++ packages/renderer-process/src/parts/VError/VError.js
@@ -14,11 +14,6 @@ const mergeStacks = (parent, child) => {
   const parentFirstLine = parent.slice(0, parentNewLineIndex)
   const childRest = child.slice(childNewLineIndex)
   const childFirstLine = child.slice(0, childNewLineIndex)
-  console.log({
-    parentFirstLine,
-    childFirstLine,
-    eq: parentFirstLine.includes(childFirstLine),
-  })
   if (parentFirstLine.includes(childFirstLine)) {
     return parentFirstLine + childRest
   }
@@ -29,12 +24,9 @@ const getErrorStack = (error) => {
   if (error && error.stack) {
     return error.stack
   }
-  console.log('else')
   if (error && error.lineNumber && error.columnNumber && error.fileName) {
-    console.log(error.lineNumber)
     const normalStackLooksLike = new Error().stack
     if (/^[a-zA-Z\$\_\d]+@.*/.test(normalStackLooksLike)) {
-      console.log({ e: `${error}` })
       // firefox stack trace
       return `${error}\nunknown@${error.fileName}:${error.lineNumber}:${error.columnNumber}`
     }
