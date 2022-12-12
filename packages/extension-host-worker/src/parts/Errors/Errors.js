725ec5b1205850087511d4c6850d42f243c23621
Le Vivilet <levivlet@pm.me>
2022-12-12 12:41:33 +0100

    fix: improve extension host helper process ipc error handling

--- packages/extension-host-worker/src/parts/Errors/Errors.js
+++ packages/extension-host-worker/src/parts/Errors/Errors.js
@@ -4,3 +4,10 @@ export class ExtensionActivationError extends Error {
     this.name = 'ExtensionActivationError'
   }
 }
+
+export class JsonRpcError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = 'JsonRpcError'
+  }
+}
