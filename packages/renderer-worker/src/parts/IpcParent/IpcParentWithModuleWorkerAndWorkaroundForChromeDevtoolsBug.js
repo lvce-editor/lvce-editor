e63a64f8d088b3b1d103c1e81c50cb8456ed966b
Le Vivilet <levivlet@pm.me>
2022-12-14 19:43:02 +0100

    fix: properly send message port from renderer process to renderer worker

--- packages/renderer-worker/src/parts/IpcParent/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.js
+++ packages/renderer-worker/src/parts/IpcParent/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.js
@@ -3,15 +3,53 @@ import * as RendererProcess from '../RendererProcess/RendererProcess.js'
 
 export const create = async ({ url, name }) => {
   try {
-    RendererProcess.send({
-      jsonrpc: '2.0',
-      method: 'get-port',
-      params: [
-        'worker',
-        { method: /* ModuleWorkerWithMessagePort */ 4, url, name },
-      ],
+    const originalOnMessage = RendererProcess.state.ipc.onmessage
+    const port = await new Promise((resolve, reject) => {
+      RendererProcess.state.ipc.onmessage = (event) => {
+        if (event.data === 'port') {
+          const port = event.ports[0]
+          resolve(port)
+        } else {
+          originalOnMessage(event)
+        }
+      }
+      RendererProcess.send({
+        jsonrpc: '2.0',
+        method: 'get-port',
+        params: [
+          'worker',
+          { method: /* ModuleWorkerWithMessagePort */ 4, url, name },
+        ],
+      })
     })
-    await new Promise((resolve) => {})
+    let handleMessage
+    return {
+      get onmessage() {
+        return port.onmessage
+      },
+      set onmessage(listener) {
+        if (listener) {
+          handleMessage = (event) => {
+            // TODO why are some events not instance of message event?
+            if (event instanceof MessageEvent) {
+              const message = event.data
+              listener(message)
+            } else {
+              listener(event)
+            }
+          }
+        } else {
+          handleMessage = null
+        }
+        port.onmessage = handleMessage
+      },
+      send(message) {
+        port.postMessage(message)
+      },
+      sendAndTransfer(message, transfer) {
+        port.postMessage(message, transfer)
+      },
+    }
   } catch (error) {
     throw error
   }
