67322362265cf3d8b2e576e2cff91e65b4348498
Le Vivilet <levivilet@pm.me>
2022-12-18 18:54:47 +0100

    fix: improve arch linux build size

--- build/src/parts/BuildArchLinux/BuildArchLinux.js
+++ build/src/parts/BuildArchLinux/BuildArchLinux.js
@@ -10,6 +10,7 @@ import * as Stat from '../Stat/Stat.js'
 import * as Tag from '../Tag/Tag.js'
 import * as Template from '../Template/Template.js'
 import * as ReadDir from '../ReadDir/ReadDir.js'
+import * as Remove from '../Remove/Remove.js'
 
 const bundleElectronMaybe = async () => {
   const { build } = await import('../BundleElectronApp/BundleElectronApp.js')
@@ -22,6 +23,12 @@ const copyElectronResult = async () => {
     from: `build/.tmp/electron-bundle/x64/resources/app`,
     to: `build/.tmp/arch-linux/x64/usr/lib/${Product.applicationName}`,
   })
+  await Remove.remove(
+    `build/.tmp/arch-linux/x64/usr/lib/${Product.applicationName}/packages/shared-process/node_modules/keytar`,
+  )
+  await Remove.remove(
+    `build/.tmp/arch-linux/x64/usr/lib/${Product.applicationName}/packages/shared-process/node_modules/vscode-ripgrep-with-github-api-error-fix`,
+  )
 }
 
 const copyMetaFiles = async () => {
