92d28bdcb8cbc3bcb28e3057646190d1c371bbf9
Le Vivilet <levivlet@pm.me>
2022-12-19 12:09:08 +0100

    refactor: use ripgrep as deb dependency instead of including it with application to reduce package size

--- build/src/parts/Deb/Deb.js
+++ build/src/parts/Deb/Deb.js
@@ -3,6 +3,7 @@ import VError from 'verror'
 import * as Compress from '../Compress/Compress.js'
 import * as Copy from '../Copy/Copy.js'
 import * as Exec from '../Exec/Exec.js'
+import * as Logger from '../Logger/Logger.js'
 import * as Mkdir from '../Mkdir/Mkdir.js'
 import * as Path from '../Path/Path.js'
 import * as Product from '../Product/Product.js'
@@ -10,7 +11,7 @@ import * as Rename from '../Rename/Rename.js'
 import * as Stat from '../Stat/Stat.js'
 import * as Tag from '../Tag/Tag.js'
 import * as Template from '../Template/Template.js'
-import * as Logger from '../Logger/Logger.js'
+import * as Remove from '../Remove/Remove.js'
 
 const getDebPackageArch = (arch) => {
   switch (arch) {
@@ -41,6 +42,9 @@ const copyElectronResult = async () => {
     from: `build/.tmp/electron-bundle/x64`,
     to: `build/.tmp/linux/deb/${debArch}/app/usr/lib/${Product.applicationName}`,
   })
+  await Remove.remove(
+    `build/.tmp/linux/deb/${debArch}/app/usr/lib/${Product.applicationName}/resources/app/packages/shared-process/node_modules/vscode-ripgrep-with-github-api-error-fix`
+  )
 }
 
 // see https://stackoverflow.com/questions/19029008/how-to-create-a-simply-debian-package-just-compress-extract-sources-or-any-file
@@ -55,6 +59,7 @@ const getInstalledSize = async (cwd) => {
 
 const copyMetaFiles = async () => {
   const debArch = 'amd64'
+
   await Template.write(
     'linux_desktop',
     `build/.tmp/linux/deb/${debArch}/app/usr/share/applications/${Product.applicationName}.desktop`,
@@ -90,6 +95,18 @@ const copyMetaFiles = async () => {
     Path.absolute(`build/.tmp/linux/deb/${debArch}/app`)
   )
   const tag = await Tag.getGitTag()
+  const defaultDepends = [
+    'libnss3 (>= 2:3.26)',
+    'gnupg',
+    'apt',
+    'libxkbfile1',
+    'libsecret-1-0',
+    'libgtk-3-0 (>= 3.10.0)',
+    'libxss1',
+    'libgbm1',
+  ]
+  const additionalDepends = ['ripgrep']
+  const depends = [...defaultDepends, ...additionalDepends].join(', ')
   await Template.write(
     'debian_control',
     `build/.tmp/linux/deb/${debArch}/DEBIAN/control`,
@@ -100,6 +117,7 @@ const copyMetaFiles = async () => {
       '@@INSTALLED_SIZE@@': `${installedSize}`,
       '@@HOMEPAGE@@': Product.homePage,
       '@@MAINTAINER@@': Product.linuxMaintainer,
+      '@@DEPENDS@@': depends,
     }
   )
   await Template.write(
