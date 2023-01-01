25ad4901717967666d1ec67dd3fef48572b5f745
Le Vivilet <levivlet@pm.me>
2023-01-01 22:21:44 +0100

    fix: make fonts module work in webworker and window

--- packages/renderer-worker/src/parts/Font/Font.js
+++ packages/renderer-worker/src/parts/Font/Font.js
@@ -1,7 +1,13 @@
 import { VError } from '../VError/VError.js'
 
+const getFonts = () => {
+  // @ts-ignore
+  return self.fonts || document.fonts
+}
+
 export const load = async (fontSize, fontFamily) => {
   try {
+    const fonts = getFonts()
     await fonts.load(`${fontSize}px ${fontFamily}`, '')
   } catch (error) {
     throw new VError(error, `Failed to load font ${fontFamily}`)
