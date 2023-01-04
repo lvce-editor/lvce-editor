515d07920797f0ed5e517efbc422594e127a2ca1
Le Vivilet <levivlet@pm.me>
2023-01-04 09:34:23 +0100

    fix failing tests

--- packages/extension-host-worker-tests/src/viewlet.editor-cursor-character-right.js
+++ packages/extension-host-worker-tests/src/viewlet.editor-cursor-character-right.js
@@ -10,11 +10,11 @@ test('viewlet.editor-cursor-character-right', async () => {
 
   // assert
   const cursor = Locator('.EditorCursor')
-  await expect(cursor).toHaveCSS('left', '0px')
+  await expect(cursor).toHaveCSS('translate', /^0px$/)
 
   // act
   await Editor.cursorCharacterRight()
 
   // assert
-  await expect(cursor).toHaveCSS('left', /^(9px|10px)$/)
+  await expect(cursor).toHaveCSS('translate', /^(8|9|10).*?px$/)
 })
