211578c28314cc37664533b521d309759e1e539d
Le Vivilet <levivilet@pm.me>
2023-01-04 16:54:12 +0000

    fix: adjust content security policy to allow loading fonts inside of webworker

--- packages/renderer-worker/src/parts/Font/Font.js
+++ packages/renderer-worker/src/parts/Font/Font.js
@@ -5,8 +5,10 @@ export const load = async (fontName, fontUrl) => {
   try {
     Assert.string(fontName)
     Assert.string(fontUrl)
-    const fontFace = new FontFace(fontName, fontUrl)
+    const fontFace = new FontFace(fontName, fontUrl, {})
     await fontFace.load()
+    self.fonts.add(fontFace)
+    console.log('added font')
   } catch (error) {
     throw new VError(error, `Failed to load font ${fontName}`)
   }
