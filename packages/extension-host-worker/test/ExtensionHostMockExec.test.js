bf0845b79f75d6287a592f8f2ecc16714a4522b9
Le Vivilet <levivlet@pm.me>
2023-01-08 10:25:46 +0100

    fix failing test

--- packages/extension-host-worker/test/ExtensionHostMockExec.test.js
+++ packages/extension-host-worker/test/ExtensionHostMockExec.test.js
@@ -4,9 +4,9 @@ beforeEach(() => {
   jest.resetAllMocks()
 })
 
-jest.unstable_mockModule('../src/parts/FunctionFromString/FunctionFromString.js', () => {
+jest.unstable_mockModule('../src/parts/JsonRpc/JsonRpc.js', () => {
   return {
-    create: jest.fn(() => {
+    invoke: jest.fn(() => {
       throw new Error('not implemented')
     }),
   }
@@ -21,43 +21,25 @@ jest.unstable_mockModule('../src/parts/Api/Api.js', () => {
 })
 
 const ExtensionHostMockExec = await import('../src/parts/ExtensionHostMockExec/ExtensionHostMockExec.js')
-const FunctionFromString = await import('../src/parts/FunctionFromString/FunctionFromString.js')
+const JsonRpc = await import('../src/parts/JsonRpc/JsonRpc.js')
 const Api = await import('../src/parts/Api/Api.js')
 
-test('mockExec - content security policy error', () => {
-  // @ts-ignore
-  FunctionFromString.create.mockImplementation(() => {
-    throw new EvalError(
-      `Refused to evaluate a string as JavaScript because 'unsafe-eval' is not an allowed source of script in the following Content Security Policy directive: "script-src 'self'".`
-    )
-  })
-  expect(() => ExtensionHostMockExec.mockExec('() => {}')).toThrowError(
-    new Error(
-      "Failed to mock exec function: EvalError: Refused to evaluate a string as JavaScript because 'unsafe-eval' is not an allowed source of script in the following Content Security Policy directive: \"script-src 'self'\"."
-    )
-  )
-})
-
 test('mockExec', async () => {
-  const mockFunction = jest.fn(() => {
+  // @ts-ignore
+  JsonRpc.invoke.mockImplementation(() => {
     return {
       stdout: '',
       stderr: '',
       exitCode: 0,
     }
   })
-  // @ts-ignore
-  FunctionFromString.create.mockImplementation(() => {
-    return mockFunction
-  })
-  ExtensionHostMockExec.mockExec('() => {}')
-  expect(FunctionFromString.create).toHaveBeenCalledTimes(1)
-  expect(FunctionFromString.create).toHaveBeenCalledWith('() => {}')
+
+  ExtensionHostMockExec.mockExec()
   expect(await Api.api.exec('test')).toEqual({
     stdout: '',
     stderr: '',
     exitCode: 0,
   })
-  expect(mockFunction).toHaveBeenCalledTimes(1)
-  expect(mockFunction.name).toBe('mockExec')
+  expect(JsonRpc.invoke).toHaveBeenCalledTimes(1)
+  expect(JsonRpc.invoke).toHaveBeenCalledWith(undefined, 'Test.executeMockExecFunction', 'test', undefined, undefined)
 })
