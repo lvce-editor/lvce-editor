a0b982af92310206a477823e0317c04ec7a198e2
Le Vivilet <levivlet@pm.me>
2023-01-08 12:02:57 +0100

    refactor: rename Timeout.setTimeout to Timeout.wait to conform with the default behaviour of setTimeout accepting a callback function

--- packages/main-process/src/parts/Timeout/Timeout.js
+++ packages/main-process/src/parts/Timeout/Timeout.js
@@ -1,5 +1,9 @@
 const NodeTimers = require('node:timers/promises')
 
-exports.setTimeout = async (timeout) => {
+exports.wait = async (timeout) => {
   await NodeTimers.setTimeout(timeout)
+}
+
+exports.setTimeout = (fn, timeout) => {
+  setTimeout(fn, timeout)
 }
