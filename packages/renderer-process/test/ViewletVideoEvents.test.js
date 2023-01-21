72e2ec824a0633618977f96546f3595eb0574bc0
Le Vivilet <levivlet@pm.me>
2023-01-21 08:58:27 +0100

    fix failing test

--- packages/renderer-process/test/ViewletVideoEvents.test.js
+++ packages/renderer-process/test/ViewletVideoEvents.test.js
@@ -19,14 +19,14 @@ const ViewletVideo = await import('../src/parts/ViewletVideo/ViewletVideo.js')
 
 test('event - error', () => {
   const state = ViewletVideo.create()
-  const { $Audio } = state
+  const { $Video } = state
   // @ts-ignore
-  $Audio.error = {
+  $Video.error = {
     code: 4,
     message: 'MEDIA_ELEMENT_ERROR: Media load rejected by URL safety check',
   }
   const event = new Event('error')
-  $Audio.dispatchEvent(event)
+  $Video.dispatchEvent(event)
   expect(RendererWorker.send).toHaveBeenCalledTimes(1)
-  expect(RendererWorker.send).toHaveBeenCalledWith('Audio.handleAudioError', 4, 'MEDIA_ELEMENT_ERROR: Media load rejected by URL safety check')
+  expect(RendererWorker.send).toHaveBeenCalledWith('Video.handleVideoError', 4, 'MEDIA_ELEMENT_ERROR: Media load rejected by URL safety check')
 })
