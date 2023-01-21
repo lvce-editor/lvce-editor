c384dc940b110c1e15cb46d414b7fefb1c274483
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-21 17:33:53 +0100

    refactor: extract explorer sorting into separate file (#1646)

---
+++ packages/renderer-worker/test/SortExplorerItems.test.js
@@ -0,0 +1,32 @@
+import * as DirentType from '../src/parts/DirentType/DirentType.js'
+import * as SortExplorerItems from '../src/parts/SortExplorerItems/SortExplorerItems.js'
+
+test('compare dirent - symlink to file and file should be ordered next to each other', async () => {
+  expect(
+    SortExplorerItems.compareDirent(
+      {
+        name: 'file-1.txt',
+        type: DirentType.File,
+      },
+      {
+        name: 'file-1.txt',
+        type: DirentType.SymLinkFile,
+      }
+    )
+  ).toBe(0)
+})
+
+test('compare dirent - symlink to directory and directory should be ordered next to each other', async () => {
+  expect(
+    SortExplorerItems.compareDirent(
+      {
+        name: 'folder-1',
+        type: DirentType.Directory,
+      },
+      {
+        name: 'folder-1',
+        type: DirentType.SymLinkFolder,
+      }
+    )
+  ).toBe(0)
+})
