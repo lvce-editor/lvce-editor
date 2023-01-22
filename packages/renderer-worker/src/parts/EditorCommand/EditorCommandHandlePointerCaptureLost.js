b82ca1796fda57f1f3a9dba0bdc2d454802794c3
Le Vivilet <levivlet@pm.me>
2023-01-22 21:57:39 +0100

    remove console log

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandlePointerCaptureLost.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandlePointerCaptureLost.js
@@ -1,7 +1,6 @@
 import * as EditorSelectionAutoMoveState from '../EditorSelectionAutoMoveState/EditorSelectionAutoMoveState.js'
 
 export const handlePointerCaptureLost = (editor) => {
-  console.log('lost pointer capture')
   EditorSelectionAutoMoveState.clearEditor()
   return editor
 }
