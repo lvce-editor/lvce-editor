fa33f297f0f0c83430b0f966f44308f9bf9e051d
Le Vivilet <levivlet@pm.me>
2023-01-23 22:59:54 +0100

    fix: improve error handling when extension contains syntax error

--- packages/extension-host-worker/src/parts/TryToGetActualImportErrorMessage/TryToGetActualImportErrorMessage.js
+++ packages/extension-host-worker/src/parts/TryToGetActualImportErrorMessage/TryToGetActualImportErrorMessage.js
@@ -1,23 +1,20 @@
-import * as IsImportError from '../IsImportError/IsImportError.js'
+import * as TryToGetActualErrorMessageWhenNetworkRequestSucceeds from '../TryToGetActualErrorMessageWhenNetworkRequestSucceeds/TryToGetActualErrorMessageWhenNetworkRequestSucceeds.js'
 
 export const tryToGetActualImportErrorMessage = async (url) => {
   try {
     await import(url)
     return `Failed to import ${url}: Unknown Error`
   } catch (error) {
-    if (IsImportError.isImportError(error)) {
-      try {
-        const response = await fetch(url)
-        switch (response.status) {
-          case 404:
-            return `Failed to import ${url}: Not found (404)`
-          default:
-            return `Failed to import ${url}: Unknown Network Error`
-        }
-      } catch {
-        return `Failed to import ${url}: Unknown Network Error`
+    try {
+      const response = await fetch(url)
+      switch (response.status) {
+        case 404:
+          return `Failed to import ${url}: Not found (404)`
+        default:
+          return TryToGetActualErrorMessageWhenNetworkRequestSucceeds.tryToGetActualErrorMessage(error, url, response)
       }
+    } catch {
+      return `Failed to import ${url}: ${error}`
     }
-    return `Failed to import ${url}: ${error}`
   }
 }
---
+++ packages/extension-host-worker/test/TryToGetActualErrorMessageWhenNetworkRequestSucceeds.test.js
@@ -0,0 +1,92 @@
+import { jest } from '@jest/globals'
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule('../src/parts/BabelParser/BabelParser.js', () => {
+  return {
+    parse: jest.fn(() => {
+      throw new Error('not implemented')
+    }),
+  }
+})
+
+const TryToGetActualErrorMessageWhenNetworkRequestSucceeds = await import(
+  '../src/parts/TryToGetActualErrorMessageWhenNetworkRequestSucceeds/TryToGetActualErrorMessageWhenNetworkRequestSucceeds.js'
+)
+
+const BabelParser = await import('../src/parts/BabelParser/BabelParser.js')
+
+class NoErrorThrownError extends Error {}
+
+/**
+ *
+ * @param {any} promise
+ * @returns {Promise<Error>}
+ *  */
+const getError = async (promise) => {
+  try {
+    await promise
+    throw new NoErrorThrownError()
+  } catch (error) {
+    // @ts-ignore
+    return error
+  }
+}
+
+class BabelSyntaxError extends SyntaxError {
+  constructor(message, loc) {
+    super(message)
+    this.code = 'BABEL_PARSER_SYNTAX_ERROR'
+    this.loc = loc
+  }
+}
+
+test('tryToGetActualErrorMessage - syntax error - identifier has already been declared', async () => {
+  // @ts-ignore
+  BabelParser.parse.mockImplementation(() => {
+    throw new BabelSyntaxError(`Identifier 'x' has already been declared. (3:4)`, {
+      line: 3,
+      column: 4,
+    })
+  })
+  const error = await getError(
+    TryToGetActualErrorMessageWhenNetworkRequestSucceeds.tryToGetActualErrorMessage(
+      null,
+      'test://extension.js',
+      new Response(`let x = 1
+
+let x = 2
+
+export const activate = () => {}
+`)
+    )
+  )
+  expect(error).toBeInstanceOf(SyntaxError)
+  expect(error.message).toBe("Identifier 'x' has already been declared.")
+  expect(error.stack).toMatch(`Identifier 'x' has already been declared.
+  at test://extension.js:3:5`)
+})
+
+test('tryToGetActualErrorMessage - syntax error - missing semicolon', async () => {
+  // @ts-ignore
+  BabelParser.parse.mockImplementation(() => {
+    throw new BabelSyntaxError(`SyntaxError: Missing semicolon. (1:2)`, {
+      line: 1,
+      column: 2,
+    })
+  })
+  const error = await getError(
+    TryToGetActualErrorMessageWhenNetworkRequestSucceeds.tryToGetActualErrorMessage(
+      null,
+      'test://extension.js',
+      new Response(`[]0
+`)
+    )
+  )
+  expect(error).toBeInstanceOf(SyntaxError)
+  expect(error.message).toBe('SyntaxError: Missing semicolon.')
+  expect(error.stack).toMatch(`SyntaxError: Missing semicolon.
+  at test://extension.js:1:3`)
+})
