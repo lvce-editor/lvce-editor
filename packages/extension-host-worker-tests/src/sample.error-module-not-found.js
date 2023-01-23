31393f9dd1bdde5d878b6f26d6c19e3c3081bddf
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-23 17:48:27 +0100

    test: add tests for when extensions have syntax error or invalid import (#1666)

---
+++ packages/extension-host-worker-tests/src/sample.error-module-not-found.js
@@ -0,0 +1,22 @@
+const name = 'sample.error-module-not-found'
+
+test('sample.error-module-not-found', async () => {
+  // arrange
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+
+  // act
+  await QuickPick.open()
+  await QuickPick.setValue('>Sample Command')
+  await QuickPick.selectItem('Sample Command')
+
+  // assert
+  const dialog = Locator('#Dialog')
+  const errorMessage = dialog.locator('#DialogBodyErrorMessage')
+  await expect(errorMessage).toHaveText(
+    `Error: Failed to activate extension sample.error-module-not-found: Failed to import http://localhost:3000/packages/extension-host-worker-tests/fixtures/sample.error-module-not-found/main.js: Unknown Network Error`
+  )
+  // TODO error message is not good
+  // TODO code frame has wrong location
+})
+
+export {}
