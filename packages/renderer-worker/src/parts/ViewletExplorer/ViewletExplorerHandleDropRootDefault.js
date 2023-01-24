acee58164bbd8a9176f056b03335dfcd92b535ee
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-24 20:28:04 +0100

    refactor: move file system handle upload to separate folder (#1696)

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDropRootDefault.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDropRootDefault.js
@@ -1,9 +1,4 @@
-import * as Command from '../Command/Command.js'
-import * as EncodingType from '../EncodingType/EncodingType.js'
-import * as FileHandleType from '../FileHandleType/FileHandleType.js'
-import * as FileSystem from '../FileSystem/FileSystem.js'
-import * as FilesystemHandle from '../FileSystemHandle/FileSystemHandle.js'
-import * as Path from '../Path/Path.js'
+import * as UploadFileSystemHandles from '../UploadFileSystemHandles/UploadFileSystemHandles.js'
 import { getChildDirents } from './ViewletExplorerShared.js'
 
 const mergeDirents = (oldDirents, newDirents) => {
@@ -19,31 +14,9 @@ const getMergedDirents = async (root, pathSeparator, dirents) => {
   return mergedDirents
 }
 
-const uploadFilesDefault = async (root, pathSeparator, fileSystemHandles) => {
-  if (fileSystemHandles.length === 1) {
-    const file = fileSystemHandles[0]
-    const { name, kind } = file
-    if (kind === FileHandleType.Directory) {
-      await Command.execute('PersistentFileHandle.addHandle', `html://${name}`, file)
-      await Command.execute('Workspace.setPath', `html://${name}`)
-      return true
-    }
-  }
-  for (const fileSystemHandle of fileSystemHandles) {
-    const { name, kind } = fileSystemHandle
-    if (kind === FileHandleType.Directory) {
-      throw new Error('folder upload is not yet supported')
-    }
-    const content = await FilesystemHandle.getBinaryString(fileSystemHandle)
-    const to = Path.join(pathSeparator, root, fileSystemHandle.name)
-    await FileSystem.writeFile(to, content, EncodingType.Binary)
-  }
-  return false
-}
-
 export const handleDrop = async (state, files) => {
   const { root, pathSeparator, items } = state
-  const handled = await uploadFilesDefault(root, pathSeparator, files)
+  const handled = await UploadFileSystemHandles.uploadFileSystemHandles(root, pathSeparator, files)
   if (handled) {
     return state
   }
