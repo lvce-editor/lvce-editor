d69337f3d01f4cbebdd85a7791416c22ce9aeff8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-24 17:01:53 +0100

    Delete HtmlFileSystemSyncAccessHandle.js

--- packages/renderer-worker/src/parts/HtmlFileSystemSyncAccessHandle/HtmlFileSystemSyncAccessHandle.js
+++
@@ -1,8 +0,0 @@
-export const read = (handle) => {
-  const fileSize = handle.getSize()
-  const buffer = new DataView(new ArrayBuffer(fileSize))
-  handle.read(buffer)
-  const decoder = new TextDecoder()
-  const text = decoder.decode(buffer)
-  return text
-}
