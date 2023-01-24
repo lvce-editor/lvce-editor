e5b5122a44b60e4408f2b0a671c2651b71bfa977
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-24 12:25:49 +0100

    test: add extensions importing json e2e tests (#1678)

---
+++ packages/extension-host-worker-tests/src/sample.import-json.js
@@ -0,0 +1,16 @@
+const name = 'sample.import-json'
+
+test.skip('sample.import-json', async () => {
+  // arrange
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+
+  // act
+  await QuickPick.open()
+  await QuickPick.setValue('>Sample Command')
+  await QuickPick.selectItem('Sample Command')
+
+  // assert
+  // TODO
+})
+
+export {}
