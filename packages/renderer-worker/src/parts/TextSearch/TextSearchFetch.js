50b6f61571ba6664d3c84f2b8d9cbc17320742e2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-24 19:20:26 +0100

    fix: add text search for fetch file system (#1693)

---
+++ packages/renderer-worker/src/parts/TextSearch/TextSearchFetch.js
@@ -0,0 +1,49 @@
+import * as Command from '../Command/Command.js'
+import * as Platform from '../Platform/Platform.js'
+import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.js'
+import * as Assert from '../Assert/Assert.js'
+
+const textSearchInFile = (file, content, query) => {
+  const results = []
+  const lines = content.split('\n')
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i]
+    const index = line.indexOf(query)
+    if (index !== -1) {
+      results.push({
+        type: TextSearchResultType.Match,
+        text: line,
+        start: index,
+        end: index + query.length,
+        lineNumber: i,
+      })
+    }
+  }
+  if (results.length !== 0) {
+    results.unshift({
+      type: TextSearchResultType.File,
+      text: file,
+      start: 0,
+      end: 0,
+      lineNumber: 0,
+    })
+  }
+  return results
+}
+
+export const textSearch = async (scheme, root, query, options) => {
+  Assert.string(scheme)
+  Assert.string(root)
+  Assert.string(query)
+  const assetDir = Platform.getAssetDir()
+  const fetchUri = `${assetDir}/config/fileMap.json`
+  const fileList = await Command.execute('Ajax.getJson', fetchUri)
+  const allResults = []
+  for (const uri of fileList) {
+    const fetchUri = `${assetDir}${uri}`
+    const content = await Command.execute('Ajax.getText', fetchUri)
+    const results = textSearchInFile(fetchUri, content, query)
+    allResults.push(...results)
+  }
+  return allResults
+}
---
+++ packages/renderer-worker/test/TextSearchFetch.test.js
@@ -0,0 +1,50 @@
+import { jest } from '@jest/globals'
+import * as TextSearchResultType from '../src/parts/TextSearchResultType/TextSearchResultType.js'
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule('../src/parts/Command/Command.js', () => {
+  return {
+    execute: jest.fn(() => {
+      throw new Error('not implemented')
+    }),
+  }
+})
+
+const TextSearchFetch = await import('../src/parts/TextSearch/TextSearchFetch.js')
+const Command = await import('../src/parts/Command/Command.js')
+
+test('textSearch', async () => {
+  // @ts-ignore
+  Command.execute.mockImplementation((command, ...args) => {
+    switch (command) {
+      case 'Ajax.getJson':
+        return ['/test/file-1.txt', '/test/file-2.txt']
+      case 'Ajax.getText':
+        if (args[0] === '/test/file-1.txt') {
+          return ' test'
+        }
+        return ''
+      default:
+        break
+    }
+  })
+  expect(await TextSearchFetch.textSearch('', '', 'test')).toEqual([
+    {
+      type: TextSearchResultType.File,
+      text: '/test/file-1.txt',
+      start: 0,
+      end: 0,
+      lineNumber: 0,
+    },
+    {
+      type: TextSearchResultType.Match,
+      text: ' test',
+      start: 1,
+      end: 5,
+      lineNumber: 0,
+    },
+  ])
+})
