af7992cd4f6c8663b868cf03e3db31f07d97fb2b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-24 12:45:49 +0100

    fix: starting extension host worker in firefox, add workaround with m… (#1679)

--- packages/renderer-process/src/parts/IpcParentWithModuleWorkerWithMessagePort/IpcParentWithModuleWorkerWithMessagePort.js
+++ packages/renderer-process/src/parts/IpcParentWithModuleWorkerWithMessagePort/IpcParentWithModuleWorkerWithMessagePort.js
@@ -1,40 +1,83 @@
-import * as WorkerType from '../WorkerType/WorkerType.js'
+import * as GetWorkerDisplayName from '../GetWorkerDisplayName/GetWorkerDisplayName.js'
+import * as IsFirefoxWorkerError from '../IsFirefoxWorkerError/IsFirefoxWorkerError.js'
 import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
+import * as WorkerType from '../WorkerType/WorkerType.js'
 
-export const create = async ({ url, name }) => {
-  const worker = new Worker(url, {
-    type: WorkerType.Module,
-    name,
-  })
-  await new Promise((resolve, reject) => {
-    const cleanup = () => {
-      worker.onmessage = null
-      worker.onerror = null
-    }
-    const handleFirstMessage = (event) => {
-      cleanup()
-      if (event.data === 'ready') {
-        resolve(undefined)
-      } else {
-        reject(new Error('unexpected first message from renderer worker'))
+const tryToGetActualErrorMessage = async ({ url, name }) => {
+  const displayName = GetWorkerDisplayName.getWorkerDisplayName(name)
+  try {
+    globalThis.DONT_EXECUTE = 1
+    await import(url)
+    return `Failed to start ${displayName}: Unknown Error`
+  } catch (error) {
+    if (error && error instanceof Error && error.message.startsWith('Failed to fetch dynamically imported module')) {
+      try {
+        const response = await fetch(url)
+        switch (response.status) {
+          case 404:
+            return `Failed to start ${displayName}: Not found (404)`
+          default:
+            return `Failed to start ${displayName}: Unknown Network Error`
+        }
+      } catch {
+        return `Failed to start ${displayName}: Unknown Network Error`
       }
     }
-    const handleFirstError = async (event) => {
-      cleanup()
-      reject(new Error('failed to start worker'))
+    return `Failed to start ${displayName}: ${error}`
+  }
+}
+
+export const create = async ({ url, name }) => {
+  try {
+    const worker = new Worker(url, {
+      type: WorkerType.Module,
+      name,
+    })
+    await new Promise((resolve, reject) => {
+      const cleanup = () => {
+        worker.onmessage = null
+        worker.onerror = null
+      }
+      const handleFirstMessage = (event) => {
+        cleanup()
+        if (event.data === 'ready') {
+          resolve(undefined)
+        } else {
+          reject(new Error('unexpected first message from renderer worker'))
+        }
+      }
+      const handleFirstError = async (event) => {
+        cleanup()
+        if (IsFirefoxWorkerError.isFirefoxWorkerError(event.message)) {
+          event.preventDefault()
+          reject(new Error('module workers are not supported in firefox'))
+        } else {
+          const actualErrorMessage = await tryToGetActualErrorMessage({
+            url,
+            name,
+          })
+          reject(new Error(actualErrorMessage))
+        }
+      }
+      worker.onmessage = handleFirstMessage
+      worker.onerror = handleFirstError
+    })
+    const channel = new MessageChannel()
+    const { port1, port2 } = channel
+    worker.postMessage(
+      {
+        jsonrpc: JsonRpcVersion.Two,
+        method: 'initialize',
+        params: ['message-port', port1],
+      },
+      [port1]
+    )
+    return port2
+  } catch (error) {
+    if (error && error instanceof Error && error.message === 'module workers are not supported in firefox') {
+      const IpcParentWithMessagePort = await import('../IpcParentWithMessagePort/IpcParentWithMessagePort.js')
+      return IpcParentWithMessagePort.create({ url })
     }
-    worker.onmessage = handleFirstMessage
-    worker.onerror = handleFirstError
-  })
-  const channel = new MessageChannel()
-  const { port1, port2 } = channel
-  worker.postMessage(
-    {
-      jsonrpc: JsonRpcVersion.Two,
-      method: 'initialize',
-      params: ['message-port', port1],
-    },
-    [port1]
-  )
-  return port2
+    throw error
+  }
 }
