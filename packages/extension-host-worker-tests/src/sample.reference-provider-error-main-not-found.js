1e38c8eca0401b84b0ec199e4cba1b3e31f3bc4a
Le Vivilet <levivlet@pm.me>
2023-01-24 15:20:11 +0100

    fix failing e2e test

--- packages/extension-host-worker-tests/src/sample.reference-provider-error-main-not-found.js
+++ packages/extension-host-worker-tests/src/sample.reference-provider-error-main-not-found.js
@@ -10,9 +10,7 @@ test('sample.reference-provider-error-main-not-found', async () => {
   )
 
   await Workspace.setPath(tmpDir)
-  await Extension.addWebExtension(
-    new URL(`../fixtures/${name}`, import.meta.url).toString()
-  )
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
 
   // act
   await Main.openUri(`${tmpDir}/test.xyz`)
@@ -25,12 +23,9 @@ test('sample.reference-provider-error-main-not-found', async () => {
 
   // TODO should show part of stack trace maybe?
   // const origin = location.origin
-  const mainUrl = new URL(
-    '../fixtures/sample.reference-provider-error-main-not-found/not-found.js',
-    import.meta.url
-  ).toString()
+  const mainUrl = new URL('../fixtures/sample.reference-provider-error-main-not-found/not-found.js', import.meta.url).toString()
   await expect(viewletLocations).toHaveText(
-    `Error: Failed to activate extension sample.reference-provider-error-main-not-found: Failed to import ${mainUrl}: Not found (404)`
+    `Error: Failed to activate extension sample.reference-provider-error-main-not-found: NotFoundError: Failed to import ${mainUrl}: Not found (404)`
   )
 })
 
