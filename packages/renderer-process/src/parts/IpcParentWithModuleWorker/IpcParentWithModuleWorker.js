749f6e64550ce7e9bbedb7d56d178719a2dada90
Le Vivilet <levivlet@pm.me>
2023-01-24 11:59:22 +0100

    refactor: move getWorkerDisplayName function to seperate module

--- packages/renderer-process/src/parts/IpcParentWithModuleWorker/IpcParentWithModuleWorker.js
+++ packages/renderer-process/src/parts/IpcParentWithModuleWorker/IpcParentWithModuleWorker.js
@@ -1,17 +1,8 @@
+import * as GetWorkerDisplayName from '../GetWorkerDisplayName/GetWorkerDisplayName.js'
 import * as WorkerType from '../WorkerType/WorkerType.js'
 
-const getDisplayName = (name) => {
-  if (!name) {
-    return '<unknown> worker'
-  }
-  if (name.endsWith('Worker')) {
-    return name.toLowerCase()
-  }
-  return `${name} worker`
-}
-
 const tryToGetActualErrorMessage = async ({ url, name }) => {
-  const displayName = getDisplayName(name)
+  const displayName = GetWorkerDisplayName.getWorkerDisplayName(name)
   try {
     globalThis.DONT_EXECUTE = 1
     await import(url)
