04a1f2b72f8b04a29b20ffd27dcf55f9e9476ed0
Le Vivilet <levivlet@pm.me>
2023-01-26 11:22:55 +0100

    add function to get dependencies from babel ast

---
+++ packages/extension-host-worker/src/parts/GetBabelAstDependencies/GetBabelAstDependencies.js
@@ -0,0 +1,14 @@
+export const getBabelAstDependencies = (code, ast) => {
+  const { program } = ast
+  const { body } = program
+  const dependencies = []
+  for (const node of body) {
+    if (node.type === 'ImportDeclaration') {
+      const relativePath = node.source.extra.rawValue
+      const start = node.source.start
+      const end = node.source.end
+      dependencies.push({ relativePath, code, start, end })
+    }
+  }
+  return dependencies
+}
