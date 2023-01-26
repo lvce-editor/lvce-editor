3f9c97a3d2cfe8037d6af02c9f712993fda8bc2f
Le Vivilet <levivlet@pm.me>
2023-01-26 14:06:04 +0100

    fix failing e2e test

--- packages/extension-host-worker-tests/src/sample.error-identifier-has-already-been-declared-other-file.js
+++ packages/extension-host-worker-tests/src/sample.error-identifier-has-already-been-declared-other-file.js
@@ -1,6 +1,6 @@
 export const name = 'sample.error-identifier-has-already-been-declared-other-file'
 
-export const test = async () => {
+export const test = async ({ Extension, QuickPick, Locator, expect }) => {
   // arrange
   await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
 
