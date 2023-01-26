cdf34e56c14a5b5faec46f76c7484dc1e3eb17e1
Le Vivilet <levivlet@pm.me>
2023-01-26 18:21:18 +0100

    fix: csp errors

--- packages/extension-host-worker/src/parts/ImportScript/ImportScript.js
+++ packages/extension-host-worker/src/parts/ImportScript/ImportScript.js
@@ -1,5 +1,8 @@
-import * as TryToGetActualImportErrorMessage from '../TryToGetActualImportErrorMessage/TryToGetActualImportErrorMessage.js'
+import { ContentSecurityPolicyError } from '../ContentSecurityPolicyError/ContentSecurityPolicyError.js'
+import * as ContentSecurityPolicyErrorState from '../ContentSecurityPolicyErrorState/ContentSecurityPolicyErrorState.js'
 import * as IsUnhelpfulImportError from '../IsUnhelpfulImportError/IsUnhelpfulImportError.js'
+import * as Timeout from '../Timeout/Timeout.js'
+import * as TryToGetActualImportErrorMessage from '../TryToGetActualImportErrorMessage/TryToGetActualImportErrorMessage.js'
 
 export const importScript = async (url) => {
   try {
@@ -8,6 +11,12 @@ export const importScript = async (url) => {
     if (IsUnhelpfulImportError.isUnhelpfulImportError(error)) {
       const actualErrorMessage = await TryToGetActualImportErrorMessage.tryToGetActualImportErrorMessage(url, error)
       throw new Error(actualErrorMessage)
+    }
+    // csp errors arrive a little bit later
+    await Timeout.sleep(0)
+    if (ContentSecurityPolicyErrorState.hasRecentErrors()) {
+      const recentError = ContentSecurityPolicyErrorState.getRecentError()
+      throw new ContentSecurityPolicyError(recentError.violatedDirective, recentError.sourceFile, recentError.lineNumber, recentError.columnNumber)
     }
     throw error
   }
