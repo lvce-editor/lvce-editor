d542c930489de14cce6f51702fb8be35977b6794
Le Vivilet <levivlet@pm.me>
2023-01-27 18:55:42 +0100

    fix failing e2e test

--- packages/extension-host-worker-tests/src/sample.debug-provider-scope.js
+++ packages/extension-host-worker-tests/src/sample.debug-provider-scope.js
@@ -11,9 +11,9 @@ export const test = async ({ FileSystem, Workspace, Extension, SideBar, Locator,
 
   // assert
   const debugButtonOne = Locator('.DebugButton').nth(0)
-  await expect(debugButtonOne).toHaveAttribute('title', 'continue')
-  const rows = Locator('.DebugSectionRow')
-  await expect(rows).toHaveCount(8)
+  await expect(debugButtonOne).toHaveAttribute('title', 'Resume')
+  const rows = Locator('.DebugRow')
+  await expect(rows).toHaveCount(9)
   await expect(rows.nth(0)).toHaveText('Local')
   await expect(rows.nth(1)).toHaveText('this: process')
   await expect(rows.nth(2)).toHaveText('now: 1985388')
@@ -22,4 +22,5 @@ export const test = async ({ FileSystem, Workspace, Extension, SideBar, Locator,
   await expect(rows.nth(5)).toHaveText('Closure (getTimerCallbacks)')
   await expect(rows.nth(6)).toHaveText('Closure')
   await expect(rows.nth(7)).toHaveText('Global')
+  await expect(rows.nth(8)).toHaveText('processTimers')
 }
