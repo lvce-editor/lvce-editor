0b51cbf391b5ea561e961f95e8eb70eac30c8a7f
Le Vivilet <levivlet@pm.me>
2023-01-27 13:08:15 +0100

    test: add e2e test for extension export not found

---
+++ packages/extension-host-worker-tests/src/sample.error-export-not-found.js
@@ -0,0 +1,21 @@
+export const name = 'sample.error-export-not-found'
+
+export const test = async ({ Extension, QuickPick, Locator, expect }) => {
+  // arrange
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+
+  // act
+  await QuickPick.open()
+  await QuickPick.setValue('>Sample Command')
+  await QuickPick.selectItem('Sample Command')
+
+  // assert
+  const errorMessage = Locator('#DialogBodyErrorMessage')
+  await expect(errorMessage).toHaveText(`Error: Failed to activate extension sample.error-export-not-found: module not found ./add.js`)
+  const codeFrame = Locator('#DialogBodyErrorCodeFrame')
+  await expect(codeFrame).toHaveText(
+    `> 1 | export * from './add.js'
+  2 |`
+  )
+  // TODO stack could be improved
+}
