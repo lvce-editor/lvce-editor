501906a750d859ff4729fa9126b12195a27f824a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-27 13:21:08 +0100

    test: add e2e test for extension export not found (#1739)

--- packages/extension-host-worker/src/parts/GetBabelAstDependencies/GetBabelAstDependencies.js
+++ packages/extension-host-worker/src/parts/GetBabelAstDependencies/GetBabelAstDependencies.js
@@ -3,7 +3,7 @@ export const getBabelAstDependencies = (code, ast) => {
   const { body } = program
   const dependencies = []
   for (const node of body) {
-    if (node.type === 'ImportDeclaration') {
+    if (node.type === 'ImportDeclaration' || node.type === 'ExportAllDeclaration') {
       const relativePath = node.source.extra.rawValue
       const start = node.source.start
       const end = node.source.end
