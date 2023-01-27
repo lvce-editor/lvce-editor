0d0f26215c41ed2d235ee5cc818095e08200415d
Le Vivilet <levivlet@pm.me>
2023-01-27 18:05:19 +0100

    test: add worker e2e test

---
+++ packages/extension-host-worker-tests/src/sample.worker.js
@@ -0,0 +1,5 @@
+export const name = 'sample.worker'
+
+export const test = async ({ Extension, QuickPick, Locator, expect }) => {
+  // TODO invoke command, worker should successfully respond with message
+}
