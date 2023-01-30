65bf4f16a7825284fe6a74a499f02ee36cf02681
Le Vivilet <levivlet@pm.me>
2023-01-30 09:31:17 +0100

    refactor: add typescript jsdoc comment for type checking

--- packages/main-process/src/parts/ElectronProtocol/ElectronProtocol.js
+++ packages/main-process/src/parts/ElectronProtocol/ElectronProtocol.js
@@ -1,5 +1,9 @@
 const { protocol } = require('electron')
 
+/**
+ *
+ * @param {Electron.CustomScheme[]} schemes
+ */
 exports.registerSchemesAsPrivileged = (schemes) => {
   protocol.registerSchemesAsPrivileged(schemes)
 }
