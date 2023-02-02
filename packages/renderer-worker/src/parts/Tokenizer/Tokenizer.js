65aa308c164df314ad30522b6850f2f39fb8e1dc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-02 16:11:58 +0100

    refactor: format code (#1797)

--- packages/renderer-worker/src/parts/Tokenizer/Tokenizer.js
+++ packages/renderer-worker/src/parts/Tokenizer/Tokenizer.js
@@ -1,5 +1,5 @@
-import * as Languages from '../Languages/Languages.js'
 import * as GlobalEventBus from '../GlobalEventBus/GlobalEventBus.js'
+import * as Languages from '../Languages/Languages.js'
 import * as TokenizePlainText from './TokenizePlainText.js'
 
 export const state = {
@@ -29,19 +29,11 @@ export const loadTokenizer = async (languageId) => {
     // 2. getTokenClass should be of type function
     const tokenizer = await import(tokenizePath)
     if (typeof tokenizer.tokenizeLine !== 'function') {
-      console.warn(
-        `tokenizer.tokenizeLine should be a function in "${tokenizePath}"`
-      )
+      console.warn(`tokenizer.tokenizeLine should be a function in "${tokenizePath}"`)
       return
     }
-    if (
-      !tokenizer.TokenMap ||
-      typeof tokenizer.TokenMap !== 'object' ||
-      Array.isArray(tokenizer.TokenMap)
-    ) {
-      console.warn(
-        `tokenizer.TokenMap should be an object in "${tokenizePath}"`
-      )
+    if (!tokenizer.TokenMap || typeof tokenizer.TokenMap !== 'object' || Array.isArray(tokenizer.TokenMap)) {
+      console.warn(`tokenizer.TokenMap should be an object in "${tokenizePath}"`)
       return
     }
     state.tokenizers[languageId] = tokenizer
