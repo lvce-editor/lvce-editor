0cbd674d0ee7a7c3d8f632bab389e0fbb86dc2a2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-03 10:49:20 +0100

    refactor: add jsdoc for file system handles for better intellisense a… (#1807)

--- packages/renderer-worker/src/parts/FileSystemFileHandle/FileSystemFileHandle.js
+++ packages/renderer-worker/src/parts/FileSystemFileHandle/FileSystemFileHandle.js
@@ -1,22 +1,45 @@
 import * as HtmlFile from '../HtmlFile/HtmlFile.js'
 
+/**
+ *
+ * @param {FileSystemFileHandle} handle
+ * @returns
+ */
 export const getFile = (handle) => {
   return handle.getFile()
 }
 
+/**
+ *
+ * @param {FileSystemFileHandle} handle
+ * @returns
+ */
 export const getBinaryString = async (handle) => {
   const file = await getFile(handle)
   const text = await HtmlFile.getBinaryString(file)
   return text
 }
 
+/**
+ *
+ * @param {FileSystemFileHandle} handle
+ * @param {string} content
+ */
 export const write = async (handle, content) => {
+  // @ts-ignore
   const writable = await handle.createWritable()
   await writable.write(content)
   await writable.close()
 }
 
+/**
+ *
+ * @param {FileSystemFileHandle} handle
+ * @param {Response} response
+ */
 export const writeResponse = async (handle, response) => {
+  // @ts-ignore
   const writable = await handle.createWritable()
+  // @ts-ignore
   await response.body.pipeTo(writable)
 }
--- packages/renderer-worker/src/parts/UploadFileSystemHandle/UploadFileSystemHandle.js
+++ packages/renderer-worker/src/parts/UploadFileSystemHandle/UploadFileSystemHandle.js
@@ -2,12 +2,21 @@ import * as FileHandleType from '../FileHandleType/FileHandleType.js'
 import * as UploadFileSystemDirectoryHandle from '../UploadFileSystemDirectoryHandle/UploadFileSystemDirectoryHandle.js'
 import * as UploadFileSystemFileHandle from '../UploadFileSystemFileHandle/UploadFileSystemFileHandle.js'
 
+/**
+ *
+ * @param {FileSystemHandle} fileSystemHandle
+ * @param {string} pathSeparator
+ * @param {string} root
+ * @param {*} uploadHandles
+ * @returns
+ */
 export const uploadHandle = (fileSystemHandle, pathSeparator, root, uploadHandles) => {
   const { kind } = fileSystemHandle
   switch (kind) {
     case FileHandleType.File:
       return UploadFileSystemFileHandle.uploadFile(fileSystemHandle, pathSeparator, root)
     case FileHandleType.Directory:
+      // @ts-ignore
       return UploadFileSystemDirectoryHandle.uploadDirectory(fileSystemHandle, pathSeparator, root, uploadHandles)
     default:
       throw new Error(`unsupported file system handle type ${kind}`)
