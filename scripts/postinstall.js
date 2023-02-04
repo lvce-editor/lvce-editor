f975b75f3acde643e4fa3867b09b11f65179880d
Le Vivilet <levivlet@pm.me>
2023-02-04 09:59:48 +0100

    remove unused code

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,13 +1,3 @@
-import { execa } from 'execa'
-
-const exec = async (command) => {
-  console.info(command)
-  await execa(command, {
-    shell: true,
-    stdio: 'inherit',
-  })
-}
-
 const main = async () => {
   await import('./use-sample-data.js')
   if (process.env.DOWNLOAD_BUILTIN_EXTENSIONS !== '0') {
