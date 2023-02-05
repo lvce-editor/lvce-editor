6a8abd5e91c4dd98847c451fd286d20eb6195332
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-05 16:48:07 +0100

    fix: use electron dialog nolink option so that dialog buttons look no… (#1836)

---
+++ packages/main-process/test/ElectronDialog.test.js
@@ -0,0 +1,50 @@
+const ElectronMessageBoxType = require('../src/parts/ElectronMessageBoxType/ElectronMessageBoxType.js')
+
+beforeEach(() => {
+  jest.resetModules()
+})
+
+jest.mock('electron', () => {
+  return {
+    dialog: {
+      showMessageBox: jest.fn(),
+    },
+    BrowserWindow: {
+      getFocusedWindow() {
+        return {}
+      },
+    },
+  }
+})
+
+jest.mock('../src/parts/Platform/Platform.js', () => {
+  return {
+    applicationName: 'test-app',
+  }
+})
+
+const ElectronDialog = require('../src/parts/ElectronDialog/ElectronDialog.js')
+const Electron = require('electron')
+
+test('showMessageBox', async () => {
+  // @ts-ignore
+  Electron.dialog.showMessageBox.mockImplementation(() => {
+    return {
+      response: 1,
+    }
+  })
+  await ElectronDialog.showMessageBox('test', ['copy', 'ok'], ElectronMessageBoxType.Info, 'test detail')
+  expect(Electron.dialog.showMessageBox).toHaveBeenCalledTimes(1)
+  expect(Electron.dialog.showMessageBox).toHaveBeenCalledWith(
+    {},
+    {
+      buttons: ['copy', 'ok'],
+      cancelId: 1,
+      detail: 'test detail',
+      message: 'test',
+      noLink: true,
+      title: 'test-app',
+      type: 'info',
+    }
+  )
+})
