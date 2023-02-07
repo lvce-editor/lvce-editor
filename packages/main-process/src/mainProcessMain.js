e2d53945fb423cddc3ae7ef20b6fc67ddbeefdab
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-07 23:25:42 +0100

    fix: increase default stack trace limit to show more parts of the sta… (#1888)

--- packages/main-process/src/mainProcessMain.js
+++ packages/main-process/src/mainProcessMain.js
@@ -4,12 +4,11 @@ const Command = require('./parts/Command/Command.js')
 const Module = require('./parts/Module/Module.js')
 const ErrorHandling = require('./parts/ErrorHandling/ErrorHandling.js')
 const App = require('./parts/App/App.js')
+const SetStackTraceLimit = require('./parts/SetStackTraceLimit/SetStackTraceLimit.js')
 
 const main = async () => {
-  process.on(
-    'uncaughtExceptionMonitor',
-    ErrorHandling.handleUncaughtExceptionMonitor
-  )
+  SetStackTraceLimit.setStackTraceLimit(20)
+  process.on('uncaughtExceptionMonitor', ErrorHandling.handleUncaughtExceptionMonitor)
   // workaround for https://github.com/electron/electron/issues/36526
   process.on('unhandledRejection', ErrorHandling.handleUnhandledRejection)
   Command.setLoad(Module.load)
