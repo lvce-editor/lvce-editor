4328fc8c81955b6ba95160087343ed304783764f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-07 22:52:31 +0100

    refactor: move ipcChildModule into separate file in extension host (#1886)

--- packages/extension-host/src/parts/IpcChild/IpcChild.js
+++ packages/extension-host/src/parts/IpcChild/IpcChild.js
@@ -1,21 +1,6 @@
-import * as Assert from '../Assert/Assert.js'
-import * as IpcChildType from '../IpcChildType/IpcChildType.js'
-
-const getModule = (method) => {
-  Assert.number(method)
-  switch (method) {
-    case IpcChildType.WebSocket:
-      return import('./IpcChildWithWebSocket.js')
-    case IpcChildType.Worker:
-      return import('./IpcChildWithWorker.js')
-    case IpcChildType.ChildProcess:
-      return import('./IpcChildWithChildProcess.js')
-    default:
-      throw new Error(`unexpected ipc type ${method}`)
-  }
-}
+import * as IpcChildModule from '../IpcChildModule/IpcChildModule.js'
 
 export const listen = async ({ method }) => {
-  const module = await getModule(method)
+  const module = await IpcChildModule.getModule(method)
   return module.listen()
 }
