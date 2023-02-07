66acb890a529f53d458c1937af09acfaee1845f9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-07 22:26:59 +0100

    refactor: use enum for desktop type (#1876)

--- packages/shared-process/src/parts/ClipBoard/ClipBoard.js
+++ packages/shared-process/src/parts/ClipBoard/ClipBoard.js
@@ -1,11 +1,12 @@
 import * as Platform from '../Platform/Platform.js'
+import * as DesktopType from '../DesktopType/DesktopType.js'
 
 const getClipboard = () => {
   const desktop = Platform.getDesktop()
   switch (desktop) {
-    case 'gnome':
+    case DesktopType.Gnome:
       return import('./ClipBoardGnome.js')
-    case 'windows':
+    case DesktopType.Windows:
       return import('./ClipBoardWindows.js')
     default:
       return import('./ClipBoardNoop.js')
