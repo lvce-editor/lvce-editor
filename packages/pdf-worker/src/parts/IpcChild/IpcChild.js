570638bfa5233becea6117817e58cdff2cf55bcc
Le Vivilet <levivlet@pm.me>
2023-02-07 22:41:11 +0100

    refactor: move ipcChildModule into separate file in pdf worker

--- packages/pdf-worker/src/parts/IpcChild/IpcChild.js
+++ packages/pdf-worker/src/parts/IpcChild/IpcChild.js
@@ -1,19 +1,6 @@
-import * as IpcChildType from '../IpcChildType/IpcChildType.js'
-
-const getModule = (method) => {
-  switch (method) {
-    case IpcChildType.MessagePort:
-      return import('./IpcChildWithMessagePort.js')
-    case IpcChildType.ModuleWorker:
-      return import('./IpcChildWithModuleWorker.js')
-    case IpcChildType.ReferencePort:
-      return import('./IpcChildWithReferencePort.js')
-    default:
-      throw new Error('unexpected ipc type')
-  }
-}
+import * as IpcChildModule from '../IpcChildModule/IpcChildModule.js'
 
 export const listen = async ({ method }) => {
-  const module = await getModule(method)
+  const module = await IpcChildModule.getModule(method)
   return module.listen()
 }
