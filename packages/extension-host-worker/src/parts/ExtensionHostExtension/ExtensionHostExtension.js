4899b2db00df68a4711dfc495a763ad960dcbc4e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-07 16:16:05 +0100

    refactor: extract getExtensionAbsolutePath function into separate module (#1856)

--- packages/extension-host-worker/src/parts/ExtensionHostExtension/ExtensionHostExtension.js
+++ packages/extension-host-worker/src/parts/ExtensionHostExtension/ExtensionHostExtension.js
@@ -1,31 +1,16 @@
 import * as Assert from '../Assert/Assert.js'
+import * as GetExtensionAbsolutePath from '../GetExtensionAbsolutePath/GetExtensionAbsolutePath.js'
 import * as GetExtensionId from '../GetExtensionId/GetExtensionId.js'
 import * as ImportScript from '../ImportScript/ImportScript.js'
 import * as IsImportError from '../IsImportError/IsImportError.js'
 import * as TryToGetActualImportErrorMessage from '../TryToGetActualImportErrorMessage/TryToGetActualImportErrorMessage.js'
 import { VError } from '../VError/VError.js'
 
-const getAbsolutePath = (isWeb, path, relativePath, origin) => {
-  if (path.startsWith('http')) {
-    if (path.endsWith('/')) {
-      return new URL(relativePath, path).toString()
-    }
-    return new URL(relativePath, path + '/').toString()
-  }
-  if (!path.startsWith('/')) {
-    path = '/' + path
-  }
-  if (isWeb) {
-    return path + '/' + relativePath
-  }
-  return new URL('/remote' + path + '/' + relativePath, origin).toString()
-}
-
 export const activate = async (extension) => {
   try {
     Assert.string(extension.path)
     Assert.string(extension.browser)
-    const absolutePath = getAbsolutePath(extension.isWeb, extension.path, extension.browser, location.origin)
+    const absolutePath = GetExtensionAbsolutePath.getExtensionAbsolutePath(extension.isWeb, extension.path, extension.browser, location.origin)
     const module = await ImportScript.importScript(absolutePath)
     try {
       await module.activate()
