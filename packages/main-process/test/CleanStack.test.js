17f33b24c36370cf2a03304dde9c4cdf088c4d23
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-07 20:13:48 +0100

    fix: improve error handling of syntax error unexpected token export i… (#1873)

--- packages/main-process/test/CleanStack.test.js
+++ packages/main-process/test/CleanStack.test.js
@@ -17,11 +17,28 @@ Require stack:
     at Module.load (node:internal/modules/cjs/loader:1044:32)
     at Module._load (node:internal/modules/cjs/loader:885:12)`
   expect(CleanStack.cleanStack(stack)).toEqual([
-    `Error: Cannot find module 'graceful-fs'`,
-    `Require stack:`,
-    `- /test/packages/shared-process/node_modules/fs-extra/lib/copy/copy-sync.js`,
-    `- /test/packages/shared-process/node_modules/rename-overwrite/index.js`,
-    `- /test/packages/shared-process/node_modules/symlink-dir/dist/index.js`,
     `    at Object.<anonymous> (/test/packages/shared-process/node_modules/fs-extra/lib/copy/copy-sync.js:3:12)`,
+  ])
+})
+
+test('cleanStack - syntax error', () => {
+  const stack = `/test/packages/main-process/src/parts/GetFirstNodeWorkerEvent/GetFirstNodeWorkerEvent.js:3
+export const getFirstNodeWorkerEvent = async (worker) => {
+^^^^^^
+
+SyntaxError: Unexpected token 'export'
+    at Object.compileFunction (node:vm:360:18)
+    at wrapSafe (node:internal/modules/cjs/loader:1095:15)
+    at Module._compile (node:internal/modules/cjs/loader:1130:27)
+    at Module._extensions..js (node:internal/modules/cjs/loader:1229:10)
+    at Module.load (node:internal/modules/cjs/loader:1044:32)
+    at Module._load (node:internal/modules/cjs/loader:885:12)
+    at f._load (node:electron/js2c/asar_bundle:2:13330)
+    at Module.require (node:internal/modules/cjs/loader:1068:19)
+    at require (node:internal/modules/cjs/helpers:103:18)
+    at Object.<anonymous> (/test/packages/main-process/src/parts/CliForwardToSharedProcess/CliForwardToSharedProcess.js:4:33)`
+  expect(CleanStack.cleanStack(stack)).toEqual([
+    `    at /test/packages/main-process/src/parts/GetFirstNodeWorkerEvent/GetFirstNodeWorkerEvent.js:3`,
+    `    at Object.<anonymous> (/test/packages/main-process/src/parts/CliForwardToSharedProcess/CliForwardToSharedProcess.js:4:33)`,
   ])
 })
