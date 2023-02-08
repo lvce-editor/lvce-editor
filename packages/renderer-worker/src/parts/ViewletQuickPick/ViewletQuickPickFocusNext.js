f0527a5f88c82522b0059591a0380efc85485263
Le Vivilet <levivlet@pm.me>
2023-02-08 19:10:17 +0100

    fix tests

--- packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickFocusNext.js
+++ packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickFocusNext.js
@@ -2,7 +2,7 @@ import { focusIndex } from './ViewletQuickPickFocusIndex.js'
 import * as ListIndex from '../ListIndex/ListIndex.js'
 
 export const focusNext = (state) => {
-  const { focusedIndex, items } = state
-  const nextIndex = ListIndex.next(focusedIndex, items)
+  const { items, focusedIndex } = state
+  const nextIndex = ListIndex.next(items, focusedIndex)
   return focusIndex(state, nextIndex)
 }
--- packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickFocusPrevious.js
+++ packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickFocusPrevious.js
@@ -2,7 +2,7 @@ import { focusIndex } from './ViewletQuickPickFocusIndex.js'
 import * as ListIndex from '../ListIndex/ListIndex.js'
 
 export const focusPrevious = (state) => {
-  const { focusedIndex, items } = state
+  const { items, focusedIndex } = state
   const previousIndex = ListIndex.previous(items, focusedIndex)
   return focusIndex(state, previousIndex)
 }
