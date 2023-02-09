36833c7a52c0c99094be39826c0ec009a0b059d9
Le Vivilet <levivlet@pm.me>
2023-02-09 15:30:42 +0100

    fix: clamp color picker values to min/max

--- packages/renderer-worker/src/parts/ViewletColorPicker/ViewletColorPicker.js
+++ packages/renderer-worker/src/parts/ViewletColorPicker/ViewletColorPicker.js
@@ -1,40 +1,52 @@
+import * as Clamp from '../Clamp/Clamp.js'
+
 export const create = () => {
   return {
     color: '',
     offsetX: 0,
+    min: 0,
+    max: 0,
   }
 }
 
-export const loadContent = (state) => {
-  return {
-    ...state,
-    color: 'blue',
-  }
-}
-
-const getNewColor = (x) => {
-  const max = 300
+const getNewColor = (x, max) => {
   const percent = x / max
   const hue = percent * 360
   const newColor = `hsl(${hue}, 100%, 50%)`
   return newColor
 }
 
+export const loadContent = (state) => {
+  const max = 300
+  const x = 20
+  const color = getNewColor(x, max)
+  return {
+    ...state,
+    offsetX: x,
+    color,
+    max,
+  }
+}
+
 export const handleSliderPointerDown = (state, x, y) => {
-  const newColor = getNewColor(x)
+  const { min, max } = state
+  const newX = Clamp.clamp(x, min, max)
+  const newColor = getNewColor(newX, max)
   return {
     ...state,
     color: newColor,
-    offsetX: x,
+    offsetX: newX,
   }
 }
 
 export const handleSliderPointerMove = (state, x, y) => {
-  const newColor = getNewColor(x)
+  const { min, max } = state
+  const newX = Clamp.clamp(x, min, max)
+  const newColor = getNewColor(newX, max)
   return {
     ...state,
     color: newColor,
-    offsetX: x,
+    offsetX: newX,
   }
 }
 
