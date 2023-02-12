d631924cfbf0b4972649ce2efedb539f2f000273
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-12 17:56:04 +0100

    refactor: add jsdoc to events module (#1987)

--- packages/renderer-process/src/parts/Event/Event.js
+++ packages/renderer-process/src/parts/Event/Event.js
@@ -1,7 +1,13 @@
+/**
+ * @param {Event} event
+ */
 export const preventDefault = (event) => {
   event.preventDefault()
 }
 
+/**
+ * @param {Event} event
+ */
 export const stopPropagation = (event) => {
   event.stopPropagation()
 }
