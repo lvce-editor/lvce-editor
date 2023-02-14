8267fee4b58049ad390e6715ae741535ca1b906d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-14 23:13:57 +0100

    refactor: align editor context menu implementation with other components (#2013)

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleContextMenu.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleContextMenu.js
@@ -1,12 +1,7 @@
 import * as Command from '../Command/Command.js'
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.js'
 
-export const handleContextMenu = async (editor, x, y) => {
-  await Command.execute(
-    /* ContextMenu.show */ 'ContextMenu.show',
-    /* x */ x,
-    /* y */ y,
-    /* id */ MenuEntryId.Editor
-  )
+export const handleContextMenu = async (editor, button, x, y) => {
+  await Command.execute(/* ContextMenu.show */ 'ContextMenu.show', /* x */ x, /* y */ y, /* id */ MenuEntryId.Editor)
   return editor
 }
