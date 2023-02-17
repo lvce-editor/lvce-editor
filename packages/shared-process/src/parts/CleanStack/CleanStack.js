09294e86633a0f7fda932c27fcaddbba4ba95dcb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-17 19:45:40 +0100

    fix: improve error handling for exports not defined error (#2033)

--- packages/shared-process/src/parts/CleanStack/CleanStack.js
+++ packages/shared-process/src/parts/CleanStack/CleanStack.js
@@ -1,5 +1,44 @@
-import npmCleanStack from 'clean-stack'
+import * as SplitLines from '../SplitLines/SplitLines.js'
+
+const isInternalLine = (line) => {
+  return line.includes('node:')
+}
+
+const isRelevantLine = (line) => {
+  return !isInternalLine(line)
+}
+
+const RE_AT = /^\s+at/
+
+const isNormalStackLine = (line) => {
+  return RE_AT.test(line)
+}
+
+const getDetails = (lines) => {
+  const index = lines.findIndex(isNormalStackLine)
+  return {
+    custom: lines.slice(0, index),
+    actualStack: lines.slice(index),
+  }
+}
+
+const RE_PATH_1 = /^\/(.*)(\d+)$/
+
+const mergeCustom = (custom, relevantStack) => {
+  if (custom.length === 0) {
+    return relevantStack
+  }
+  const firstLine = custom[0]
+  if (RE_PATH_1.test(firstLine)) {
+    return [`    at ${firstLine}`, ...relevantStack]
+  }
+  return relevantStack
+}
 
 export const cleanStack = (stack) => {
-  return npmCleanStack(stack)
+  const lines = SplitLines.splitLines(stack)
+  const { custom, actualStack } = getDetails(lines)
+  const relevantStack = actualStack.filter(isRelevantLine)
+  const merged = mergeCustom(custom, relevantStack)
+  return merged
 }
