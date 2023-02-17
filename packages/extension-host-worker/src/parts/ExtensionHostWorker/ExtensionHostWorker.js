0049251b4747d578a7c09e95d22b3b9f9df14963
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-17 16:21:55 +0100

    refactor: simplify jsonrpc code in extension host worker (#2027)

--- packages/extension-host-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.js
+++ packages/extension-host-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.js
@@ -6,10 +6,9 @@ export const createWorker = async ({ method, url, name }) => {
   Assert.string(method)
   Assert.string(url)
   Assert.string(name)
-  const response = await new Promise((resolve, reject) => {
-    const id = Callback.register(resolve, reject)
-    Rpc.state.ipc.send({ jsonrpc: '2.0', method: 'get-port', params: [url, name], id })
-  })
+  const { id, promise } = Callback.registerPromise()
+  Rpc.state.ipc.send({ jsonrpc: '2.0', method: 'get-port', params: [url, name], id })
+  const response = await promise
   const ipc = response.result
   return ipc
 }
