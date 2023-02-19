40ba29e8d0c98296c3a7dfaeb83c39f115091545
Le Vivilet <levivlet@pm.me>
2023-02-19 14:40:19 +0100

    refactor: add renderer process confirm prompt

--- packages/renderer-process/src/parts/Module/Module.js
+++ packages/renderer-process/src/parts/Module/Module.js
@@ -60,6 +60,8 @@ export const load = (moduleId) => {
       return import('../WebStorage/WebStorage.ipc.js')
     case ModuleId.Window:
       return import('../Window/Window.ipc.js')
+    case ModuleId.ConfirmPrompt:
+      return import('../ConfirmPrompt/ConfirmPrompt.ipc.js')
     default:
       throw new Error(`module ${moduleId} not found`)
   }
