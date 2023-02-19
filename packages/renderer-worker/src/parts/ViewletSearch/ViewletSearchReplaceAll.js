1263ba6bc8b69ca6ad8eea374f06e5e8c952e6ab
Le Vivilet <levivlet@pm.me>
2023-02-19 17:10:16 +0100

    fix: improve replace all message

--- packages/renderer-worker/src/parts/ViewletSearch/ViewletSearchReplaceAll.js
+++ packages/renderer-worker/src/parts/ViewletSearch/ViewletSearchReplaceAll.js
@@ -11,8 +11,14 @@ const getConfirmMessage = (matchCount, fileCount, replacement) => {
   return ViewletSearchStrings.confirmReplaceManyOccurrencesInManyFiles(matchCount, fileCount, replacement)
 }
 
-const getReplacedAllMessage = () => {
-  return 'Replaced all items'
+const getReplacedAllMessage = (matchCount, fileCount, replacement) => {
+  if (matchCount === 1) {
+    return ViewletSearchStrings.replacedOneOccurrenceInOneFile(replacement)
+  }
+  if (fileCount === 1) {
+    return ViewletSearchStrings.replacedManyOccurrencesInOneFile(matchCount, replacement)
+  }
+  return ViewletSearchStrings.replacedManyOccurrencesInManyFiles(matchCount, fileCount, replacement)
 }
 
 export const replaceAll = async (state) => {
