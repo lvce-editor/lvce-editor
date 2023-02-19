e88f7fc408f1717267f5b2aff46c80d5315d5276
Le Vivilet <levivlet@pm.me>
2023-02-19 16:52:31 +0100

    refactor: add search strings for replacing occurrences

--- packages/renderer-worker/src/parts/ViewletSearch/ViewletSearchStrings.js
+++ packages/renderer-worker/src/parts/ViewletSearch/ViewletSearchStrings.js
@@ -1,13 +1,19 @@
+import * as Assert from '../Assert/Assert.js'
 import * as I18nString from '../I18NString/I18NString.js'
 
 /**
  * @enum {string}
  */
-export const UiStrings = {
+const UiStrings = {
   NoResults: 'No results found',
   Oneresult: 'Found 1 result in 1 file',
   ManyResultsInOneFile: `Found {PH1} results in 1 file`,
   ManyResultsInManyFiles: `Found {PH1} results in {PH2} files`,
+  ReplaceAll: 'Replace All',
+  ConfirmReplaceAll: 'Replace All?',
+  ConfirmReplaceOneOccurrenceInOneFile: "Replace 1 occurrence across 1 file with '{PH1}'",
+  ConfirmReplaceManyOccurrencesInOneFile: "Replace {PH1} occurrences across 1 file with '{PH2}'",
+  ConfirmReplaceManyOccurrencesInManyFiles: "Replace {PH1} occurrences across {PH2} files with '{PH3}'",
 }
 
 export const noResults = () => {
@@ -28,5 +34,29 @@ export const manyResultsInManyFiles = (resultCount, fileResultCount) => {
   return I18nString.i18nString(UiStrings.ManyResultsInManyFiles, {
     PH1: resultCount,
     PH2: fileResultCount,
+  })
+}
+
+export const confirmReplaceOneOccurrenceInOneFile = (replacement) => {
+  return I18nString.i18nString(UiStrings.ConfirmReplaceOneOccurrenceInOneFile, {
+    PH1: replacement,
+  })
+}
+
+export const confirmReplaceManyOccurrencesInOneFile = (matchCount, replacement) => {
+  return I18nString.i18nString(UiStrings.ConfirmReplaceManyOccurrencesInOneFile, {
+    PH1: matchCount,
+    PH2: replacement,
+  })
+}
+
+export const confirmReplaceManyOccurrencesInManyFiles = (matchCount, fileCount, replacement) => {
+  Assert.number(matchCount)
+  Assert.number(fileCount)
+  Assert.string(replacement)
+  return I18nString.i18nString(UiStrings.ConfirmReplaceManyOccurrencesInManyFiles, {
+    PH1: matchCount,
+    PH2: fileCount,
+    PH3: replacement,
   })
 }
