8847cb46d8a147926cde9758912ad0cc58faf61b
Le Vivilet <levivlet@pm.me>
2023-02-19 19:49:03 +0100

    refactor: lazyload file system providers and error handling modules

--- packages/renderer-worker/src/parts/FileSystem/FileSystem.js
+++ packages/renderer-worker/src/parts/FileSystem/FileSystem.js
@@ -1,20 +1,18 @@
-// TODO file system providers should be lazy loaded (maybe)
-
 import * as EncodingType from '../EncodingType/EncodingType.js'
 import * as GetFileSystem from '../GetFileSystem/GetFileSystem.js'
 import * as GetProtocol from '../GetProtocol/GetProtocol.js'
 
-export const readFile = (uri, encoding = EncodingType.Utf8) => {
+export const readFile = async (uri, encoding = EncodingType.Utf8) => {
   const protocol = GetProtocol.getProtocol(uri)
   const path = GetProtocol.getPath(protocol, uri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   return fileSystem.readFile(path, encoding)
 }
 
 export const remove = async (uri) => {
   const protocol = GetProtocol.getProtocol(uri)
   const path = GetProtocol.getPath(protocol, uri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   await fileSystem.remove(path)
 }
 
@@ -22,21 +20,21 @@ export const rename = async (oldUri, newUri) => {
   const protocol = GetProtocol.getProtocol(oldUri)
   const oldPath = GetProtocol.getPath(protocol, oldUri)
   const newPath = GetProtocol.getPath(protocol, newUri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   await fileSystem.rename(oldPath, newPath)
 }
 
 export const mkdir = async (uri) => {
   const protocol = GetProtocol.getProtocol(uri)
   const path = GetProtocol.getPath(protocol, uri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   await fileSystem.mkdir(path)
 }
 
 export const writeFile = async (uri, content, encoding = EncodingType.Utf8) => {
   const protocol = GetProtocol.getProtocol(uri)
   const path = GetProtocol.getPath(protocol, uri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   await fileSystem.writeFile(path, content, encoding)
 }
 
@@ -44,10 +42,10 @@ export const createFile = (uri) => {
   return writeFile(uri, '')
 }
 
-export const readDirWithFileTypes = (uri) => {
+export const readDirWithFileTypes = async (uri) => {
   const protocol = GetProtocol.getProtocol(uri)
   const path = GetProtocol.getPath(protocol, uri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   return fileSystem.readDirWithFileTypes(path)
 }
 
@@ -59,52 +57,52 @@ export const unwatchAll = () => {
   throw new Error('not implemented')
 }
 
-export const getBlobUrl = (uri) => {
+export const getBlobUrl = async (uri) => {
   const protocol = GetProtocol.getProtocol(uri)
   const path = GetProtocol.getPath(protocol, uri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   return fileSystem.getBlobUrl(path)
 }
 
-export const copy = (sourceUri, targetUri) => {
+export const copy = async (sourceUri, targetUri) => {
   // TODO what if it is not the same file system?
   const protocol = GetProtocol.getProtocol(sourceUri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   const sourcePath = GetProtocol.getPath(protocol, sourceUri)
   const targetPath = GetProtocol.getPath(protocol, targetUri)
   return fileSystem.copy(sourcePath, targetPath)
 }
 
-export const getPathSeparator = (uri) => {
+export const getPathSeparator = async (uri) => {
   const protocol = GetProtocol.getProtocol(uri)
   const path = GetProtocol.getPath(protocol, uri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   return fileSystem.getPathSeparator(path)
 }
 
-export const getRealPath = (uri) => {
+export const getRealPath = async (uri) => {
   const protocol = GetProtocol.getProtocol(uri)
   const path = GetProtocol.getPath(protocol, uri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   return fileSystem.getRealPath(path)
 }
 
-export const stat = (uri) => {
+export const stat = async (uri) => {
   const protocol = GetProtocol.getProtocol(uri)
   const path = GetProtocol.getPath(protocol, uri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   return fileSystem.stat(path)
 }
 
-export const chmod = (uri, permissions) => {
+export const chmod = async (uri, permissions) => {
   const protocol = GetProtocol.getProtocol(uri)
   const path = GetProtocol.getPath(protocol, uri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   return fileSystem.chmod(path, permissions)
 }
 
-export const canBeRestored = (uri) => {
+export const canBeRestored = async (uri) => {
   const protocol = GetProtocol.getProtocol(uri)
-  const fileSystem = GetFileSystem.getFileSystem(protocol)
+  const fileSystem = await GetFileSystem.getFileSystem(protocol)
   return fileSystem.canBeRestored
 }
--- packages/renderer-worker/src/parts/ViewletModule/ViewletModule.js
+++ packages/renderer-worker/src/parts/ViewletModule/ViewletModule.js
@@ -1,12 +1,12 @@
-import * as IsImportError from '../IsImportError/IsImportError.js'
-import * as TryToGetActualImportErrorMessage from '../TryToGetActualImportErrorMessage/TryToGetActualImportErrorMessage.js'
 import * as ViewletModuleInternal from '../ViewletModuleInternal/ViewletModuleInternal.js'
 
 export const load = async (moduleId) => {
   try {
     return await ViewletModuleInternal.load(moduleId)
   } catch (error) {
+    const IsImportError = await import('../IsImportError/IsImportError.js')
     if (IsImportError.isImportError(error)) {
+      const TryToGetActualImportErrorMessage = await import('../TryToGetActualImportErrorMessage/TryToGetActualImportErrorMessage.js')
       const actualErrorMessage = await TryToGetActualImportErrorMessage.tryToGetActualImportErrorMessage('', error)
       throw new Error(actualErrorMessage)
     }
