eb6c13966b07f6010d441369baed3301ebe3695f
Le Vivilet <levivlet@pm.me>
2023-02-19 14:08:43 +0100

    remove console logs

--- packages/renderer-worker/src/parts/ViewletSearch/ViewletSearchReplaceAll.js
+++ packages/renderer-worker/src/parts/ViewletSearch/ViewletSearchReplaceAll.js
@@ -3,17 +3,15 @@ import * as GetBulkReplacementEdits from '../GetBulkReplacementEdits/GetBulkRepl
 
 const actuallyReplaceAll = async (matches, replacement) => {
   const { files, ranges } = GetBulkReplacementEdits.getBulkReplacementEdits(matches)
-  console.log({ files, ranges })
   await Command.execute('BulkReplacement.applyBulkReplacement', files, ranges, replacement)
 }
 
 export const replaceAll = async (state) => {
   const shouldReplace = await Command.execute('ConfirmPrompt.prompt', 'Replace all?', 'Replace All')
   if (!shouldReplace) {
-    return
+    return state
   }
   const { items, replacement } = state
-  console.log({ replacement, items })
   await actuallyReplaceAll(items, replacement)
   return state
 }
