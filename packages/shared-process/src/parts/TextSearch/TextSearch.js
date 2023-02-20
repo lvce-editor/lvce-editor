7ae4d282a276eb0141f576eaf8d285a394ef092d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-20 17:08:42 +0100

    fix: improve text search error handling by using nodejs streams and p… (#2076)

--- packages/shared-process/src/parts/TextSearch/TextSearch.js
+++ packages/shared-process/src/parts/TextSearch/TextSearch.js
@@ -1,9 +1,12 @@
+import { Writable } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
+import * as EncodingType from '../EncodingType/EncodingType.js'
+import * as GetNewLineIndex from '../GetNewLineIndex/GetNewLineIndex.js'
 import * as GetTextSearchRipGrepArgs from '../GetTextSearchRipGrepArgs/GetTextSearchRipGrepArgs.js'
 import * as RipGrep from '../RipGrep/RipGrep.js'
 import * as RipGrepParsedLineType from '../RipGrepParsedLineType/RipGrepParsedLineType.js'
 import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.js'
 import * as ToTextSearchResult from '../ToTextSearchResult/ToTextSearchResult.js'
-
 // TODO update vscode-ripgrep when https://github.com/mhinz/vim-grepper/issues/244, https://github.com/BurntSushi/ripgrep/issues/1892 is fixed
 
 // need to use '.' as last argument for ripgrep
@@ -16,73 +19,94 @@ import * as ToTextSearchResult from '../ToTextSearchResult/ToTextSearchResult.js
 // TODO not always run nice, maybe configure nice via flag/options
 
 export const search = async (searchDir, searchString, { threads = 1, maxSearchResults = 20_000, isCaseSensitive = false } = {}) => {
+  const ripGrepArgs = GetTextSearchRipGrepArgs.getRipGrepArgs({
+    threads,
+    isCaseSensitive,
+    searchString,
+  })
+  const childProcess = RipGrep.spawn(ripGrepArgs, {
+    cwd: searchDir,
+  })
+  const allSearchResults = Object.create(null)
+  let buffer = ''
+  let stats = {}
+  let limitHit = false
+  let numberOfResults = 0
+
+  childProcess.stdout.setEncoding(EncodingType.Utf8)
+
+  const handleLine = (line) => {
+    const parsedLine = JSON.parse(line)
+    switch (parsedLine.type) {
+      case RipGrepParsedLineType.Begin:
+        allSearchResults[parsedLine.data.path.text] = [
+          {
+            type: TextSearchResultType.File,
+            start: 0,
+            end: 0,
+            lineNumber: 0,
+            text: parsedLine.data.path.text,
+          },
+        ]
+        break
+      case RipGrepParsedLineType.Match:
+        numberOfResults++
+        allSearchResults[parsedLine.data.path.text].push(...ToTextSearchResult.toTextSearchResult(parsedLine))
+        break
+      case RipGrepParsedLineType.Summary:
+        stats = parsedLine.data
+        break
+      default:
+        break
+    }
+  }
+
+  let total = 0
+  const handleData = (chunk) => {
+    let newLineIndex = GetNewLineIndex.getNewLineIndex(chunk)
+    const dataString = buffer + chunk
+    if (newLineIndex === -1) {
+      buffer = dataString
+      return
+    }
+    total += chunk.length
+    newLineIndex += buffer.length
+    let previousIndex = 0
+    while (newLineIndex >= 0) {
+      const line = dataString.slice(previousIndex, newLineIndex)
+      handleLine(line)
+      previousIndex = newLineIndex + 1
+      newLineIndex = GetNewLineIndex.getNewLineIndex(dataString, previousIndex)
+    }
+    buffer = dataString.slice(previousIndex)
+
+    if (numberOfResults > maxSearchResults) {
+      limitHit = true
+      childProcess.kill()
+    }
+  }
+
+  const pipeLinePromise = pipeline(
+    childProcess.stdout,
+    new Writable({
+      decodeStrings: false,
+      construct(callback) {
+        callback()
+      },
+      write(chunk, encoding, callback) {
+        try {
+          handleData(chunk)
+          callback()
+        } catch (error) {
+          callback(error)
+        }
+      },
+    })
+  )
+
   // TODO reject promise when ripgrep search fails
-  return new Promise((resolve, reject) => {
-    const ripGrepArgs = GetTextSearchRipGrepArgs.getRipGrepArgs({
-      threads,
-      isCaseSensitive,
-      searchString,
-    })
-    const childProcess = RipGrep.spawn(ripGrepArgs, {
-      cwd: searchDir,
-    })
-    const allSearchResults = Object.create(null)
-    let buffer = ''
-    let stats = {}
-    let limitHit = false
-    let numberOfResults = 0
+  const closePromise = new Promise((resolve, reject) => {
     // TODO use pipeline / transform stream maybe
-
-    const handleLine = (line) => {
-      const parsedLine = JSON.parse(line)
-      switch (parsedLine.type) {
-        case RipGrepParsedLineType.Begin:
-          allSearchResults[parsedLine.data.path.text] = [
-            {
-              type: TextSearchResultType.File,
-              start: 0,
-              end: 0,
-              lineNumber: 0,
-              text: parsedLine.data.path.text,
-            },
-          ]
-          break
-        case RipGrepParsedLineType.Match:
-          numberOfResults++
-          allSearchResults[parsedLine.data.path.text].push(...ToTextSearchResult.toTextSearchResult(parsedLine))
-          break
-        case RipGrepParsedLineType.Summary:
-          stats = parsedLine.data
-          break
-        default:
-          break
-      }
-    }
-
-    let total = 0
-    const handleData = (chunk) => {
-      let newLineIndex = chunk.indexOf('\n')
-      const dataString = buffer + chunk
-      if (newLineIndex === -1) {
-        buffer = dataString
-        return
-      }
-      total += chunk.length
-      newLineIndex += buffer.length
-      let previousIndex = 0
-      while (newLineIndex >= 0) {
-        const line = dataString.slice(previousIndex, newLineIndex)
-        handleLine(line)
-        previousIndex = newLineIndex + 1
-        newLineIndex = dataString.indexOf('\n', previousIndex)
-      }
-      buffer = dataString.slice(previousIndex)
-
-      if (numberOfResults > maxSearchResults) {
-        limitHit = true
-        childProcess.kill()
-      }
-    }
 
     const handleClose = () => {
       const results = Object.values(allSearchResults).flat(1)
@@ -101,9 +125,10 @@ export const search = async (searchDir, searchString, { threads = 1, maxSearchRe
         limitHit,
       })
     }
-    childProcess.stdout.setEncoding('utf8')
-    childProcess.stdout.on('data', handleData)
+
     childProcess.once('close', handleClose)
     childProcess.once('error', handleError)
   })
+  await Promise.all([pipeLinePromise, closePromise])
+  return await closePromise
 }
