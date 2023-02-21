54fce7f3a6696cba4aaba21453dfee894bab5e0f
Le Vivilet <levivlet@pm.me>
2023-02-21 21:05:44 +0100

    fix: improve cleanStack by removing exports. prefix from stack lines

--- packages/main-process/src/parts/CleanStack/CleanStack.js
+++ packages/main-process/src/parts/CleanStack/CleanStack.js
@@ -14,11 +14,21 @@ const isNormalStackLine = (line) => {
   return RE_AT.test(line)
 }
 
+const cleanLine = (line) => {
+  if (line.startsWith('    at exports.')) {
+    return '    at ' + line.slice('    at exports.'.length)
+  }
+  if (line.startsWith('    at async exports.')) {
+    return '    at async ' + line.slice('    at async exports.'.length)
+  }
+  return line
+}
+
 const getDetails = (lines) => {
   const index = lines.findIndex(isNormalStackLine)
   return {
     custom: lines.slice(0, index),
-    actualStack: lines.slice(index),
+    actualStack: lines.slice(index).map(cleanLine),
   }
 }
 
