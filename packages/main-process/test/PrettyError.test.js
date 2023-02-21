90232df128ca495106d4541ee1155859599f7a44
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-21 17:19:02 +0100

    test: add file url error test (#2081)

--- packages/main-process/test/PrettyError.test.js
+++ packages/main-process/test/PrettyError.test.js
@@ -1029,3 +1029,219 @@ exports.findById = (id) => {
     type: 'Error',
   })
 })
+
+test('prepare - error - file url must be absolute', async () => {
+  const error = new TypeError(`File URL path must be absolute`)
+  error.stack = `TypeError [ERR_INVALID_FILE_URL_PATH]: File URL path must be absolute
+    at new NodeError (node:internal/errors:393:5)
+    at getPathFromURLWin32 (node:internal/url:1458:11)
+    at fileURLToPath (node:internal/url:1490:22)
+    at getAbsolutePath (C:\\test\\packages\\main-process\\src\\parts\\ElectronSession\\ElectronSession.js:138:14)
+    at Function.handleRequest (C:\\test\\packages\\main-process\\src\\parts\\ElectronSession\\ElectronSession.js:152:16`
+  // @ts-ignore
+  fs.readFileSync.mockImplementation(() => {
+    return `const ContentSecurityPolicy = require('../ContentSecurityPolicy/ContentSecurityPolicy.js')
+const ContentSecurityPolicyWorker = require('../ContentSecurityPolicyWorker/ContentSecurityPolicyWorker.js')
+const CrossOriginEmbedderPolicy = require('../CrossOriginEmbedderPolicy/CrossOriginEmbedderPolicy.js')
+const CrossOriginOpenerPolicy = require('../CrossOriginOpenerPolicy/CrossOriginOpenerPolicy.js')
+const Electron = require('electron')
+const ElectronPermissionType = require('../ElectronPermissionType/ElectronPermissionType.js')
+const ElectronResourceType = require('../ElectronResourceType/ElectronResourceType.js')
+const Path = require('../Path/Path.js')
+const Platform = require('../Platform/Platform.js')
+const Root = require('../Root/Root.js')
+const { existsSync } = require('node:fs')
+const { join } = require('node:path')
+const HttpStatusCode = require('../HttpStatusCode/HttpStatusCode.js')
+const { fileURLToPath } = require('node:url')
+
+const state = {
+  /**
+   * @type {import('electron').Session|undefined}
+   */
+  session: undefined,
+}
+
+const handleHeadersReceivedMainFrame = (responseHeaders) => {
+  return {
+    responseHeaders: {
+      ...responseHeaders,
+      [ContentSecurityPolicy.key]: ContentSecurityPolicy.value,
+      [CrossOriginOpenerPolicy.key]: CrossOriginOpenerPolicy.value,
+      [CrossOriginEmbedderPolicy.key]: CrossOriginEmbedderPolicy.value,
+    },
+  }
+}
+
+const handleHeadersReceivedSubFrame = (responseHeaders) => {
+  return {
+    responseHeaders: {
+      ...responseHeaders,
+      [CrossOriginOpenerPolicy.key]: CrossOriginOpenerPolicy.value,
+      [CrossOriginEmbedderPolicy.key]: CrossOriginEmbedderPolicy.value,
+    },
+  }
+}
+
+const handleHeadersReceivedDefault = (responseHeaders, url) => {
+  if (url.endsWith('WorkerMain.js')) {
+    return {
+      responseHeaders: {
+        ...responseHeaders,
+        [CrossOriginEmbedderPolicy.key]: CrossOriginEmbedderPolicy.value,
+        [ContentSecurityPolicyWorker.key]: ContentSecurityPolicyWorker.value,
+      },
+    }
+  }
+  return {
+    responseHeaders,
+  }
+}
+
+const handleHeadersReceivedXhr = (responseHeaders, url) => {
+  // workaround for electron bug
+  // when using fetch, it doesn't return a response for 404
+  // console.log({ url, responseHeaders })
+  return {
+    responseHeaders: {
+      ...responseHeaders,
+    },
+  }
+}
+
+const getHeadersReceivedFunction = (resourceType) => {
+  // console.log({ resourceType })
+  switch (resourceType) {
+    case ElectronResourceType.MainFrame:
+      return handleHeadersReceivedMainFrame
+    case ElectronResourceType.SubFrame:
+      return handleHeadersReceivedSubFrame
+    case ElectronResourceType.Xhr:
+      return handleHeadersReceivedXhr
+    default:
+      return handleHeadersReceivedDefault
+  }
+}
+
+/**
+ *
+ * @param {import('electron').OnHeadersReceivedListenerDetails} details
+ * @param {(headersReceivedResponse: import('electron').HeadersReceivedResponse)=>void} callback
+ */
+const handleHeadersReceived = (details, callback) => {
+  const { responseHeaders, resourceType, url } = details
+  const fn = getHeadersReceivedFunction(resourceType)
+  callback(fn(responseHeaders, url))
+}
+
+const isAllowedPermission = (permission) => {
+  switch (permission) {
+    case ElectronPermissionType.ClipBoardRead:
+    case ElectronPermissionType.ClipBoardSanitizedWrite:
+    case ElectronPermissionType.FullScreen:
+    case ElectronPermissionType.WindowPlacement:
+      return true
+    default:
+      return false
+  }
+}
+
+const handlePermissionRequest = (webContents, permission, callback, details) => {
+  callback(isAllowedPermission(permission))
+}
+
+const handlePermissionCheck = (webContents, permission, origin, details) => {
+  return isAllowedPermission(permission)
+}
+
+// TODO use Platform.getScheme() instead of Product.getTheme()
+
+const getAbsolutePath = (requestUrl) => {
+  const decoded = decodeURI(requestUrl)
+  const { scheme } = Platform
+  const pathName = decoded.slice(\`\${scheme}://-\`.length)
+  // TODO remove if/else in prod (use replacement)
+  if (pathName === \`/\` || pathName.startsWith(\`/?\`)) {
+    return Path.join(Root.root, 'static', 'index.html')
+  }
+  if (pathName.startsWith(\`/packages\`)) {
+    return Path.join(Root.root, pathName)
+  }
+  if (pathName.startsWith(\`/static\`)) {
+    return Path.join(Root.root, pathName)
+  }
+  if (pathName.startsWith(\`/extensions\`)) {
+    return Path.join(Root.root, pathName)
+  }
+  // TODO maybe have a separate protocol for remote, e.g. vscode has vscode-remote
+  if (pathName.startsWith(\`/remote\`)) {
+    const uri = pathName.slice('/remote'.length)
+    if (Platform.isWindows) {
+      return fileURLToPath(\`file://\` + uri)
+    }
+    return uri
+  }
+  return Path.join(Root.root, 'static', pathName)
+}
+/**
+ *
+ * @param {globalThis.Electron.ProtocolRequest} request
+ * @param {(response: string | globalThis.Electron.ProtocolResponse) => void} callback
+ */
+
+const handleRequest = (request, callback) => {
+  // const path = join(__dirname, request.url.slice(6))
+  const path = getAbsolutePath(request.url)
+  if (!existsSync(path)) {
+    // TODO doing this for every request is really slow
+    // but without this, fetch would not received a response for 404 requests
+    return callback({
+      statusCode: HttpStatusCode.NotFound,
+      path: join(__dirname, 'not-found.txt'),
+    })
+  }
+  callback({
+    path,
+    headers: {
+      'Cache-Control': 'public, max-age=31536000, immutable', // TODO caching is not working, see https://github.com/electron/electron/issues/27075 and https://github.com/electron/electron/issues/23482
+    },
+  })
+}
+
+const createSession = () => {
+  const sessionId = Platform.getSessionId()
+  const session = Electron.session.fromPartition(sessionId, {
+    cache: Platform.isProduction,
+  })
+  session.webRequest.onHeadersReceived(handleHeadersReceived)
+  session.setPermissionRequestHandler(handlePermissionRequest)
+  session.setPermissionCheckHandler(handlePermissionCheck)
+  session.protocol.registerFileProtocol(Platform.scheme, handleRequest)
+  return session
+}
+
+exports.state = state
+
+exports.get = () => {
+  if (!state.session) {
+    state.session = createSession()
+  }
+  return state.session
+}
+`
+  })
+  const prettyError = PrettyError.prepare(error)
+  expect(prettyError).toEqual({
+    codeFrame: `  136 |     const uri = pathName.slice('/remote'.length)
+  137 |     if (Platform.isWindows) {
+> 138 |       return fileURLToPath(\`file://\` + uri)
+      |              ^
+  139 |     }
+  140 |     return uri
+  141 |   }`,
+    message: 'File URL path must be absolute',
+    stack: `    at getAbsolutePath (C:\\test\\packages\\main-process\\src\\parts\\ElectronSession\\ElectronSession.js:138:14)
+    at Function.handleRequest (C:\\test\\packages\\main-process\\src\\parts\\ElectronSession\\ElectronSession.js:152:16`,
+    type: 'TypeError',
+  })
+})
