747375b8d0dafce628e020bef66731ee96d28d84
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-02-27 20:05:24 +0100

    refactor: improve cleanStackTrace (#2149)

--- packages/shared-process/test/CleanStack.test.js
+++ packages/shared-process/test/CleanStack.test.js
@@ -13,6 +13,7 @@ test('cleanStack - remove useless Promise.all', () => {
     `    at async create (file:///test/packages/shared-process/src/parts/Terminal/Terminal.js:80:23)`,
   ])
 })
+
 test('cleanStack - remove Module prefix', () => {
   const stack = `AssertionError: expected value to be of type string
     at Module.string (file:///test/packages/shared-process/src/parts/Assert/Assert.js:50:11)
@@ -22,9 +23,21 @@ test('cleanStack - remove Module prefix', () => {
     at async WebSocket.handleMessage (file:///test/packages/shared-process/src/parts/Socket/Socket.js:32:22)`
   expect(CleanStack.cleanStack(stack)).toEqual([
     '    at string (file:///test/packages/shared-process/src/parts/Assert/Assert.js:50:11)',
-    '    at Object.getColorThemeJson [as ExtensionHost.getColorThemeJson] (file:///test/packages/shared-process/src/parts/ExtensionManagement/ExtensionManagementColorTheme.js:32:10)',
+    '    at ExtensionHost.getColorThemeJson (file:///test/packages/shared-process/src/parts/ExtensionManagement/ExtensionManagementColorTheme.js:32:10)',
     '    at executeCommandAsync (file:///test/packages/shared-process/src/parts/Command/Command.js:68:33)',
     '    at async getResponse (file:///test/packages/shared-process/src/parts/GetResponse/GetResponse.js:21:9)',
     '    at async WebSocket.handleMessage (file:///test/packages/shared-process/src/parts/Socket/Socket.js:32:22)',
+  ])
+})
+
+test('cleanStack - remove Object prefix', () => {
+  const stack = `VError: Failed to read file "/test/settings.json": EACCES: permission denied, open '/test/settings.json'
+    at Object.readFile [as FileSystem.readFile] (file:///test/packages/shared-process/src/parts/FileSystem/FileSystem.js:50:11)
+    at async Module.getResponse (file:///test/packages/shared-process/src/parts/GetResponse/GetResponse.js:21:9)
+    at async WebSocket.handleMessage (file:///test/packages/shared-process/src/parts/Socket/Socket.js:27:22)`
+  expect(CleanStack.cleanStack(stack)).toEqual([
+    '    at FileSystem.readFile (file:///test/packages/shared-process/src/parts/FileSystem/FileSystem.js:50:11)',
+    '    at async getResponse (file:///test/packages/shared-process/src/parts/GetResponse/GetResponse.js:21:9)',
+    '    at async WebSocket.handleMessage (file:///test/packages/shared-process/src/parts/Socket/Socket.js:27:22)',
   ])
 })
