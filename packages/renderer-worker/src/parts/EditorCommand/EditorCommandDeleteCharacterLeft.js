9fc8489dc2975c342a093c86ef83b4cc49491a56
Le Vivilet <levivlet@pm.me>
2023-03-03 18:45:32 +0100

    fix: auto delete bracket when deleting word

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteCharacterLeft.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteCharacterLeft.js
@@ -1,21 +1,7 @@
-import * as IsAllAutoClosingPairDelete from '../IsAllAutoClosingPairDelete/IsAllAutoClosingPairDelete.js'
 import * as EditorDeleteHorizontalLeft from './EditorCommandDeleteHorizontalLeft.js'
 import * as EditorDelta from './EditorCommandDelta.js'
 
-const deleteCharacterLeftWithAutoClose = (editor) => {
-  const { selections } = editor
-  for (let i = 0; i < selections.length; i += 4) {
-    selections[i + 1]++
-    selections[i + 3]++
-  }
-  return EditorDeleteHorizontalLeft.editorDeleteHorizontalLeft(editor, EditorDelta.twoCharactersLeft)
-}
-
 export const deleteCharacterLeft = (editor) => {
-  const { autoClosingRanges = [], selections } = editor
-  if (IsAllAutoClosingPairDelete.isAllAutoClosingPairDelete(autoClosingRanges, selections)) {
-    return deleteCharacterLeftWithAutoClose(editor)
-  }
   return EditorDeleteHorizontalLeft.editorDeleteHorizontalLeft(editor, EditorDelta.characterLeft)
 }
 
