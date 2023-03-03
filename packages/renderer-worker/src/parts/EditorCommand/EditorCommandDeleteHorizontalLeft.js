549d9df8f3e1c0d03508ce1eea5f4ac9ff90f716
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-03 18:55:47 +0100

    fix: auto delete bracket when deleting word (#2173)

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteHorizontalLeft.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteHorizontalLeft.js
@@ -1,25 +1,17 @@
 import * as Editor from '../Editor/Editor.js'
-import * as TextDocument from '../TextDocument/TextDocument.js'
-import { editorReplaceSelections } from './EditorCommandReplaceSelection.js'
-import * as EditorGetPositionLeft from './EditorCommandGetPositionLeft.js'
-import * as EditorSelection from '../EditorSelection/EditorSelection.js'
 import * as EditOrigin from '../EditOrigin/EditOrigin.js'
+import * as EditorSelection from '../EditorSelection/EditorSelection.js'
+import * as IsAllAutoClosingPairDelete from '../IsAllAutoClosingPairDelete/IsAllAutoClosingPairDelete.js'
+import * as TextDocument from '../TextDocument/TextDocument.js'
+import * as EditorDelta from './EditorCommandDelta.js'
+import * as EditorGetPositionLeft from './EditorCommandGetPositionLeft.js'
+import { editorReplaceSelections } from './EditorCommandReplaceSelection.js'
 
 // TODO optimize this function by profiling and not allocating too many objects
 const getChanges = (lines, selections, getDelta) => {
   const changes = []
-  const deleteSelection = (
-    selectionStartRow,
-    selectionStartColumn,
-    selectionEndRow,
-    selectionEndColumn
-  ) => {
-    const positionLeft = EditorGetPositionLeft.editorGetPositionLeft(
-      selectionStartRow,
-      selectionStartColumn,
-      lines,
-      getDelta
-    )
+  const deleteSelection = (selectionStartRow, selectionStartColumn, selectionEndRow, selectionEndColumn) => {
+    const positionLeft = EditorGetPositionLeft.editorGetPositionLeft(selectionStartRow, selectionStartColumn, lines, getDelta)
     const selectionEnd = {
       rowIndex: selectionEndRow,
       columnIndex: selectionEndColumn,
@@ -42,9 +34,21 @@ const getChanges = (lines, selections, getDelta) => {
   return changes
 }
 
+const deleteLeftWithAutoClose = (editor) => {
+  const { selections, lines } = editor
+  for (let i = 0; i < selections.length; i += 4) {
+    selections[i + 1]++
+    selections[i + 3]++
+  }
+  const changes = getChanges(lines, selections, EditorDelta.twoCharactersLeft)
+  return Editor.scheduleDocumentAndCursorsSelections(editor, changes)
+}
+
 export const editorDeleteHorizontalLeft = (editor, getDelta) => {
-  const lines = editor.lines
-  const selections = editor.selections
+  const { autoClosingRanges = [], selections, lines } = editor
+  if (IsAllAutoClosingPairDelete.isAllAutoClosingPairDelete(autoClosingRanges, selections)) {
+    return deleteLeftWithAutoClose(editor)
+  }
   if (EditorSelection.isEverySelectionEmpty(selections)) {
     const changes = getChanges(lines, selections, getDelta)
     return Editor.scheduleDocumentAndCursorsSelections(editor, changes)
