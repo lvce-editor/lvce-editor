d65dc46f27e7d2aa1b8f4d6d8365c42b9fdebc9b
Le Vivilet <levivlet@pm.me>
2023-03-05 23:09:03 +0100

    fix failing test

--- packages/renderer-worker/test/SplitLines.test.js
+++ packages/renderer-worker/test/SplitLines.test.js
@@ -2,5 +2,5 @@ import { AssertionError } from '../src/parts/AssertionError/AssertionError.js'
 import * as SplitLines from '../src/parts/SplitLines/SplitLines.js'
 
 test('splitLines - undefined', () => {
-  expect(() => SplitLines.splitLines(undefined)).toThrowError(new AssertionError('expected value to be of type array'))
+  expect(() => SplitLines.splitLines(undefined)).toThrowError(new AssertionError('expected value to be of type string'))
 })
