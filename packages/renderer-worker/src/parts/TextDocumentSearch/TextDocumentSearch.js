a54ca32aaea1667e9ccf57b8a231a6b8a7d8de30
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-07 14:58:05 +0100

    fix: browser crashing with out of memory error when searching for emp… (#2190)

--- packages/renderer-worker/src/parts/TextDocumentSearch/TextDocumentSearch.js
+++ packages/renderer-worker/src/parts/TextDocumentSearch/TextDocumentSearch.js
@@ -1,16 +1,14 @@
 export const findMatches = (lines, searchString) => {
+  if (searchString.length === 0) {
+    return new Uint32Array([])
+  }
   const { length } = lines
   const matches = []
   const searchStringLength = searchString.length
   for (let i = 0; i < length; i++) {
     const line = lines[i]
     let lastMatchIndex = -searchStringLength
-    while (
-      (lastMatchIndex = line.indexOf(
-        searchString,
-        lastMatchIndex + searchStringLength
-      )) !== -1
-    ) {
+    while ((lastMatchIndex = line.indexOf(searchString, lastMatchIndex + searchStringLength)) !== -1) {
       matches.push(i, lastMatchIndex)
     }
   }
