b854de40e765fd4b9a7dfeb33399c289963b099f
Le Vivilet <levivlet@pm.me>
2023-03-07 14:46:32 +0100

    fix: browser crashing with out of memory error when searching for empty string

--- packages/renderer-worker/test/TextDocumentSearch.test.js
+++ packages/renderer-worker/test/TextDocumentSearch.test.js
@@ -4,28 +4,24 @@ test('findMatches - no results', () => {
   expect(TextDocumentSearch.findMatches([], '')).toEqual(new Uint32Array([]))
 })
 
+test('findMatches - empty string', () => {
+  expect(TextDocumentSearch.findMatches(['abc'], '')).toEqual(new Uint32Array([]))
+})
+
 test('findMatches - one result', () => {
-  expect(TextDocumentSearch.findMatches(['line 1'], 'line')).toEqual(
-    new Uint32Array([0, 0])
-  )
+  expect(TextDocumentSearch.findMatches(['line 1'], 'line')).toEqual(new Uint32Array([0, 0]))
 })
 
 test('findMatches - two results in two rows', () => {
-  expect(
-    TextDocumentSearch.findMatches(['line 1', 'not match', 'line 3'], 'line')
-  ).toEqual(new Uint32Array([0, 0, 2, 0]))
+  expect(TextDocumentSearch.findMatches(['line 1', 'not match', 'line 3'], 'line')).toEqual(new Uint32Array([0, 0, 2, 0]))
 })
 
 test('findMatches - two results in one row', () => {
-  expect(TextDocumentSearch.findMatches(['line 1 line 2'], 'line')).toEqual(
-    new Uint32Array([0, 0, 0, 7])
-  )
+  expect(TextDocumentSearch.findMatches(['line 1 line 2'], 'line')).toEqual(new Uint32Array([0, 0, 0, 7]))
 })
 
 test('findPreviousMatch', () => {
-  expect(
-    TextDocumentSearch.findPreviousMatch(['line 1', 'not match', 'line 3'], 2)
-  ).toEqual({
+  expect(TextDocumentSearch.findPreviousMatch(['line 1', 'not match', 'line 3'], 2)).toEqual({
     rowIndex: 0,
     columnIndex: 0,
   })
