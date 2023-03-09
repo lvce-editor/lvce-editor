13a386b584f9b4ba74b84523536190188f16833c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-09 21:05:24 +0100

    refactor: move extensionUninstall function into separate module (#2230)

--- packages/shared-process/src/parts/ExtensionManagement/ExtensionManagement.js
+++ packages/shared-process/src/parts/ExtensionManagement/ExtensionManagement.js
@@ -1,23 +1,10 @@
-import { mkdir, rename, rm } from 'node:fs/promises'
+import { mkdir, rename } from 'node:fs/promises'
 import * as Debug from '../Debug/Debug.js'
 import * as ExtensionManifestInputType from '../ExtensionManifestInputType/ExtensionManifestInputType.js'
 import * as ExtensionManifests from '../ExtensionManifests/ExtensionManifests.js'
 import * as Path from '../Path/Path.js'
 import * as Platform from '../Platform/Platform.js'
 import { VError } from '../VError/VError.js'
-
-export const uninstall = async (id) => {
-  try {
-    Debug.debug(`ExtensionManagement#uninstall ${id}`)
-    const extensionsPath = Platform.getExtensionsPath()
-    await rm(Path.join(extensionsPath, id), { recursive: true })
-  } catch (error) {
-    // if (error.code === 'ENOENT') {
-    //   return
-    // }
-    throw new VError(error, `Failed to uninstall extension "${id}"`)
-  }
-}
 
 export const enable = async (id) => {
   try {
@@ -90,3 +77,5 @@ export const getDisabledExtensions = () => {
     },
   ])
 }
+
+export * from '../ExtensionUninstall/ExtensionUninstall.js'
