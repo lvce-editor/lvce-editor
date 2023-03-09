858781996a33585a5c598d1c45bd04ed70b3ba92
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-09 14:12:59 +0100

    refactor: use logger in editorCommandToggleComment (#2220)

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandToggleComment.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandToggleComment.js
@@ -1,19 +1,19 @@
 import * as EditorToggleBlockComment from './EditorCommandToggleBlockComment.js'
 import * as EditorToggleLineComment from './EditorCommandToggleLineComment.js'
 import * as EditorShowMessage from './EditorCommandShowMessage.js'
+import * as Logger from '../Logger/Logger.js'
+
 // TODO rename file
 // TODO when handle case when editor has selection
 export const toggleComment = async (editor) => {
   try {
-    const newEditor = await EditorToggleLineComment.editorToggleLineComment(
-      editor
-    )
+    const newEditor = await EditorToggleLineComment.editorToggleLineComment(editor)
     if (editor !== newEditor) {
       return newEditor
     }
     return EditorToggleBlockComment.toggleBlockComment(editor)
   } catch (error) {
-    console.error(error)
+    Logger.error(error)
     // TODO use correct position
     await EditorShowMessage.editorShowMessage(
       /* editor */ editor,
