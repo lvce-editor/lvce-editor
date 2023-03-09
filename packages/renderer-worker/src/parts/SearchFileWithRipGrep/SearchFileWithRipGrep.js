4ea6ef721bdef2c57665d44b6c71a340890dc7b2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-09 19:25:18 +0100

    fix: wrong quick pick command for searching (#2224)

--- packages/renderer-worker/src/parts/SearchFileWithRipGrep/SearchFileWithRipGrep.js
+++ packages/renderer-worker/src/parts/SearchFileWithRipGrep/SearchFileWithRipGrep.js
@@ -3,7 +3,12 @@ import * as SplitLines from '../SplitLines/SplitLines.js'
 import * as SharedProcessCommandType from '../SharedProcessCommandType/SharedProcessCommandType.js'
 
 export const searchFile = async (path, value) => {
-  const stdout = await SharedProcess.invoke(SharedProcessCommandType.SearchSearchFile, /* path */ path, /* searchTerm */ value, /* limit */ 9999999)
+  const stdout = await SharedProcess.invoke(
+    SharedProcessCommandType.SearchFileSearchFile,
+    /* path */ path,
+    /* searchTerm */ value,
+    /* limit */ 9999999
+  )
   const lines = SplitLines.splitLines(stdout)
   return lines
 }
