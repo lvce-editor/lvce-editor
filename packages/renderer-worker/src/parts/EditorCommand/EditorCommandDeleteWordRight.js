60e5f7851efc5fabaa2dc1098c4ce03e1269aaf6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-10 12:43:01 +0100

    fix: deleteWordRight command not found (#2232)

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteWordRight.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteWordRight.js
@@ -1,9 +1,6 @@
 import * as EditorDeleteHorizontalRight from './EditorCommandDeleteHorizontalRight.js'
 import * as EditorDelta from './EditorCommandDelta.js'
 
-export const editorDeleteWordRight = (editor) => {
-  return EditorDeleteHorizontalRight.editorDeleteHorizontalRight(
-    editor,
-    EditorDelta.wordRight
-  )
+export const deleteWordRight = (editor) => {
+  return EditorDeleteHorizontalRight.editorDeleteHorizontalRight(editor, EditorDelta.wordRight)
 }
