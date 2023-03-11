52bcde5079083994357f75491f6c1243d1d66ff0
Le Vivilet <levivlet@pm.me>
2023-03-11 20:42:47 +0100

    fix failing test

--- packages/extension-host-worker-tests/src/sample.source-control-provider.js
+++ packages/extension-host-worker-tests/src/sample.source-control-provider.js
@@ -13,7 +13,7 @@ export const test = async ({ FileSystem, Workspace, Extension, SideBar, Locator,
   await expect(viewletTree).toBeVisible()
 
   const treeItem1 = Locator('.TreeItem').nth(0)
-  await expect(treeItem1).toHaveText('/test/file-1.txt')
+  await expect(treeItem1).toHaveText('file-1.txt /test')
   const treeItem2 = Locator('.TreeItem').nth(1)
-  await expect(treeItem2).toHaveText('/test/file-2.txt')
+  await expect(treeItem2).toHaveText('file-2.txt /test')
 }
