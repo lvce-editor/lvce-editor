5c7a7d07fc1a771028113d6c0b1194226127441e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-11 16:10:59 +0100

    feature: add timeout to extension activation to not wait indefinitely… (#2245)

--- packages/extension-host-worker/src/parts/ExtensionHostExtension/ExtensionHostExtension.js
+++ packages/extension-host-worker/src/parts/ExtensionHostExtension/ExtensionHostExtension.js
@@ -3,8 +3,16 @@ import * as GetExtensionAbsolutePath from '../GetExtensionAbsolutePath/GetExtens
 import * as GetExtensionId from '../GetExtensionId/GetExtensionId.js'
 import * as ImportScript from '../ImportScript/ImportScript.js'
 import * as IsImportError from '../IsImportError/IsImportError.js'
+import * as Timeout from '../Timeout/Timeout.js'
 import * as TryToGetActualImportErrorMessage from '../TryToGetActualImportErrorMessage/TryToGetActualImportErrorMessage.js'
 import { VError } from '../VError/VError.js'
+
+const activationTimeout = 10_000
+
+const rejectAfterTimeout = async (timeout) => {
+  await Timeout.sleep(timeout)
+  throw new Error(`activation timeout of ${timeout}ms exceeded`)
+}
 
 export const activate = async (extension) => {
   try {
@@ -13,6 +21,7 @@ export const activate = async (extension) => {
     const absolutePath = GetExtensionAbsolutePath.getExtensionAbsolutePath(extension.isWeb, extension.path, extension.browser, location.origin)
     const module = await ImportScript.importScript(absolutePath)
     try {
+      await Promise.race([module.activate(), rejectAfterTimeout(activationTimeout)])
       await module.activate()
     } catch (error) {
       if (IsImportError.isImportError(error)) {
