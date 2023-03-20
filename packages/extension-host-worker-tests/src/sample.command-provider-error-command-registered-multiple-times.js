6a5104c222016aba0d39332e824bcda877501548
Le Vivilet <levivlet@pm.me>
2023-03-20 16:35:29 +0100

    fix: improve error message when command is registered multiple times

---
+++ packages/extension-host-worker-tests/src/sample.command-provider-error-command-registered-multiple-times.js
@@ -0,0 +1,22 @@
+export const name = 'sample.command-provider-error-command-registered-multiple-times'
+
+export const test = async ({ Extension, QuickPick, Locator, expect }) => {
+  // arrange
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+
+  // act
+  await QuickPick.open()
+  await QuickPick.setValue('>Sample Command')
+  await QuickPick.selectItem('Sample Command')
+
+  // assert
+  const dialogErrorMessage = Locator('#DialogBodyErrorMessage')
+  await expect(dialogErrorMessage).toBeVisible()
+  // TODO better error message
+  // TODO less obstrusive error message, maybe notification
+  // TODO should show stack trace
+  // TODO should show babel code frame
+  await expect(dialogErrorMessage).toHaveText(
+    'Error: Failed to activate extension sample.command-provider-error-command-registered-multiple-times: Failed to register command xyz.sampleCommand: command cannot be registered multiple times'
+  )
+}
