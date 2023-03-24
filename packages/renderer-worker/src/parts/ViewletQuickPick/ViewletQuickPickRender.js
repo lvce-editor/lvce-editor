5e2940bb16e8ad723e68155b95e80973bf9b6cb4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-24 23:45:38 +0100

    refactor: move quickpick render code into separate module (#2365)

---
+++ packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickRender.js
@@ -0,0 +1,75 @@
+const getVisible = (provider, items, minLineY, maxLineY) => {
+  const visibleItems = []
+  const setSize = items.length
+  const max = Math.min(items.length, maxLineY)
+  for (let i = minLineY; i < max; i++) {
+    const item = items[i]
+    const label = provider.getPickLabel(item)
+    const icon = provider.getPickIcon(item)
+    visibleItems.push({
+      label,
+      icon,
+      posInSet: i + 1,
+      setSize,
+    })
+  }
+  return visibleItems
+}
+
+export const hasFunctionalRender = true
+
+const renderValue = {
+  isEqual(oldState, newState) {
+    return oldState.value === newState.value
+  },
+  apply(oldState, newState) {
+    return [/* method */ 'setValue', /* value */ newState.value]
+  },
+}
+
+const renderCursorOffset = {
+  isEqual(oldState, newState) {
+    oldState.cursorOffset === newState.cursorOffset || newState.cursorOffset === newState.value.length
+  },
+  apply(oldState, newState) {
+    return [/* method */ 'setCursorOffset', /* cursorOffset */ newState.cursorOffset]
+  },
+}
+
+const renderItems = {
+  isEqual(oldState, newState) {
+    return oldState.items === newState.items && oldState.minLineY === newState.minLineY && oldState.maxLineY === newState.maxLineY
+  },
+  apply(oldState, newState) {
+    if (newState.items.length === 0) {
+      return [/* method */ 'showNoResults']
+    }
+    const visibleItems = getVisible(newState.provider, newState.items, newState.minLineY, newState.maxLineY)
+    return [/* method */ 'setVisiblePicks', /* visiblePicks */ visibleItems]
+  },
+}
+
+const renderFocusedIndex = {
+  isEqual(oldState, newState) {
+    return oldState.focusedIndex === newState.focusedIndex
+  },
+  apply(oldState, newState) {
+    const oldFocusedIndex = oldState.focusedIndex - oldState.minLineY
+    const newFocusedIndex = newState.focusedIndex - newState.minLineY
+    return [/* method */ 'setFocusedIndex', /* oldFocusedIndex */ oldFocusedIndex, /* newFocusedIndex */ newFocusedIndex]
+  },
+}
+
+const renderHeight = {
+  isEqual(oldState, newState) {
+    return oldState.items.length === newState.items.length
+  },
+  apply(oldState, newState) {
+    const maxLineY = Math.min(newState.maxLineY, newState.items.length)
+    const itemCount = maxLineY - newState.minLineY
+    const height = itemCount * newState.itemHeight
+    return [/* method */ 'setItemsHeight', /* height */ height]
+  },
+}
+
+export const render = [renderItems, renderValue, renderCursorOffset, renderFocusedIndex, renderHeight]
