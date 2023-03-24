d01a26a7ab158cfb8b24e1d70251313b86533b19
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-24 23:13:17 +0100

    refactor: move audio render code into separate module (#2358)

--- packages/renderer-worker/src/parts/ViewletAudio/ViewletAudio.js
+++ packages/renderer-worker/src/parts/ViewletAudio/ViewletAudio.js
@@ -44,25 +44,3 @@ export const dispose = async (state) => {
   await BlobSrc.disposeSrc(src)
   return state
 }
-
-export const hasFunctionalRender = true
-
-export const renderSrc = {
-  isEqual(oldState, newState) {
-    return oldState.src === newState.src
-  },
-  apply(oldState, newState) {
-    return [/* method */ 'setSrc', /* src */ newState.src]
-  },
-}
-
-const renderAudioErrorMessage = {
-  isEqual(oldState, newState) {
-    return oldState.audioErrorMessage === newState.audioErrorMessage
-  },
-  apply(oldState, newState) {
-    return [/* method */ 'setAudioErrorMessage', /* src */ newState.audioErrorMessage]
-  },
-}
-
-export const render = [renderSrc, renderAudioErrorMessage]
