94de4fcb1812e81b679bd93a7f6e6b3512f286ef
Le Vivilet <levivlet@pm.me>
2023-03-24 22:50:10 +0100

    refactor: move title bar icon render code into separate module

--- packages/renderer-worker/src/parts/ViewletTitleBarIcon/ViewletTitleBarIcon.ipc.js
+++ packages/renderer-worker/src/parts/ViewletTitleBarIcon/ViewletTitleBarIcon.ipc.js
@@ -2,3 +2,5 @@ export const name = 'TitleBarIcon'
 
 export * from './ViewletTitleBarIcon.js'
 export * from './ViewletTitleBarIconCss.js'
+export * from './ViewletTitleBarIconRender.js'
+export * from './ViewletTitleBarIconResize.js'
--- packages/renderer-worker/src/parts/ViewletTitleBarIcon/ViewletTitleBarIcon.js
+++ packages/renderer-worker/src/parts/ViewletTitleBarIcon/ViewletTitleBarIcon.js
@@ -10,13 +10,3 @@ export const create = (id, uri, x, y, width, height) => {
 export const loadContent = async (state) => {
   return state
 }
-
-export const hasFunctionalResize = true
-
-export const resize = (state, dimensions) => {
-  return state
-}
-
-export const hasFunctionalRender = true
-
-export const render = []
