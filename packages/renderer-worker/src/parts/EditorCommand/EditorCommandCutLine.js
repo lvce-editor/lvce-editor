2a00a0651f8c8c29c8bafb387b6c2f7767c44f21
Le Vivilet <levivlet@pm.me>
2023-03-24 19:03:12 +0100

    feature: cut whole line when selection is empty

---
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandCutLine.js
@@ -0,0 +1,15 @@
+import * as Command from '../Command/Command.js'
+import * as Editor from '../Editor/Editor.js'
+import * as EditOrigin from '../EditOrigin/EditOrigin.js'
+import * as EditorCommandReplaceRange from './EditorCommandReplaceRange.js'
+
+export const cutLine = async (editor) => {
+  const { lines, selections } = editor
+  const [startRowIndex] = selections
+  const line = lines[startRowIndex]
+  const replaceRange = new Uint32Array([startRowIndex, 0, startRowIndex, line.length])
+  const changes = EditorCommandReplaceRange.editorReplaceRange(editor, replaceRange, [''], EditOrigin.EditorCut)
+  const selectionChanges = new Uint32Array([startRowIndex, 0, startRowIndex, 0])
+  await Command.execute(/* ClipBoard.writeText */ 'ClipBoard.writeText', /* text */ line)
+  return Editor.scheduleDocumentAndCursorsSelections(editor, changes, selectionChanges)
+}
---
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandCutSelectedText.js
@@ -0,0 +1,16 @@
+import * as Command from '../Command/Command.js'
+import * as Editor from '../Editor/Editor.js'
+import * as EditOrigin from '../EditOrigin/EditOrigin.js'
+import * as JoinLines from '../JoinLines/JoinLines.js'
+import { editorReplaceSelections } from './EditorCommandReplaceSelection.js'
+
+export const cutSelectedText = async (editor) => {
+  const { selections } = editor
+  const [startRowIndex, startColumnIndex, endRowIndex, endColumnIndex] = selections
+  const changes = editorReplaceSelections(editor, [''], EditOrigin.EditorCut)
+  const selectionChanges = new Uint32Array([startRowIndex, startColumnIndex, endRowIndex, endColumnIndex])
+  const text = JoinLines.joinLines(changes[0].deleted)
+  // TODO remove selected text from document
+  await Command.execute(/* ClipBoard.writeText */ 'ClipBoard.writeText', /* text */ text)
+  return Editor.scheduleDocumentAndCursorsSelections(editor, changes, selectionChanges)
+}
