54b60ff4622ba7a966a455d5d5671d9f6e6c6f01
Le Vivilet <levivlet@pm.me>
2023-03-24 23:09:20 +0100

    refactor: move color picker render code into separate module

--- packages/renderer-worker/src/parts/ViewletColorPicker/ViewletColorPicker.js
+++ packages/renderer-worker/src/parts/ViewletColorPicker/ViewletColorPicker.js
@@ -49,25 +49,3 @@ export const handleSliderPointerMove = (state, x, y) => {
     offsetX: newX,
   }
 }
-
-export const hasFunctionalRender = true
-
-const renderColor = {
-  isEqual(oldState, newState) {
-    return oldState.color === newState.color
-  },
-  apply(oldState, newState) {
-    return [/* method */ 'setColor', /* color */ newState.color]
-  },
-}
-
-const renderOffsetX = {
-  isEqual(oldState, newState) {
-    return oldState.offsetX === newState.offsetX
-  },
-  apply(oldState, newState) {
-    return [/* method */ 'setOffsetX', /* offsetX */ newState.offsetX]
-  },
-}
-
-export const render = [renderColor, renderOffsetX]
