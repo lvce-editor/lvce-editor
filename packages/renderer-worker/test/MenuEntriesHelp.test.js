f7167af4031895953581b0f2e4ef1a1c2cc0e721
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-25 16:24:08 +0100

    fix failing test

--- packages/renderer-worker/test/MenuEntriesHelp.test.js
+++ packages/renderer-worker/test/MenuEntriesHelp.test.js
@@ -1,8 +1,22 @@
-import * as MenuEntriesHelp from '../src/parts/MenuEntriesHelp/MenuEntriesHelp.js'
+import { jest } from '@jest/globals'
 import * as MenuItemFlags from '../src/parts/MenuItemFlags/MenuItemFlags.js'
 
-test('getMenuEntries', () => {
-  const menuEntries = MenuEntriesHelp.getMenuEntries()
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule('../src/parts/IsAutoUpdateSupported/IsAutoUpdateSupported.js', () => {
+  return {
+    isAutoUpdateSupported: jest.fn(() => {
+      return false
+    }),
+  }
+})
+
+const MenuEntriesHelp = await import('../src/parts/MenuEntriesHelp/MenuEntriesHelp.js')
+
+test('getMenuEntries', async () => {
+  const menuEntries = await MenuEntriesHelp.getMenuEntries()
   expect(menuEntries).toContainEqual({
     command: 'About.showAbout',
     flags: MenuItemFlags.RestoreFocus,
