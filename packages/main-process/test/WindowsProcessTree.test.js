e05bbe539d0502978af5cb257f56e66392aad608
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-25 22:56:33 +0100

    fix: improve windows process tree error message

---
+++ packages/main-process/test/WindowsProcessTree.test.js
@@ -0,0 +1,22 @@
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.mock('windows-process-tree', () => {
+  return {
+    getProcessList: jest.fn(),
+  }
+})
+
+const WindowsProcessTree = require('../src/parts/WindowsProcessTree/WindowsProcessTree.js')
+const WindowsProcessTreeModule = require('windows-process-tree')
+
+test('getProcessList', async () => {
+  // @ts-ignore
+  WindowsProcessTreeModule.getProcessList.mockImplementation((pid, callback, flags) => {
+    callback([])
+  })
+  const rootPid = 1
+  const flags = 1
+  expect(await WindowsProcessTree.getProcessList(rootPid, flags)).toEqual([])
+})
