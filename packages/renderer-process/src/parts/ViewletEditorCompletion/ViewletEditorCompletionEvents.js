a789d9f40e72e54c4168be568b2eed490a2d5c74
Le Vivilet <levivlet@pm.me>
2023-03-26 22:27:32 +0200

    refactor: remove unused code

--- packages/renderer-process/src/parts/ViewletEditorCompletion/ViewletEditorCompletionEvents.js
+++ packages/renderer-process/src/parts/ViewletEditorCompletion/ViewletEditorCompletionEvents.js
@@ -1,21 +1,6 @@
 import * as Event from '../Event/Event.js'
 import * as ViewletEditorCompletionFunctions from './ViewletEditorCompletionFunctions.js'
 
-const getNodeIndex = ($Node) => {
-  let index = 0
-  while (($Node = $Node.previousElementSibling)) {
-    index++
-  }
-  return index
-}
-
-const getIndex = ($Target) => {
-  if ($Target.classList.contains('EditorCompletionItem')) {
-    return getNodeIndex($Target)
-  }
-  return -1
-}
-
 export const handleMousedown = (event) => {
   Event.preventDefault(event)
   const { clientX, clientY } = event
