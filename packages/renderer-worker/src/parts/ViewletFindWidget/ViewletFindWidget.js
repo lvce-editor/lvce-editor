9dab09e848fd04d471a4d740c5e7e24405444908
Le Vivilet <levivlet@pm.me>
2023-03-27 11:26:29 +0200

    refactor: move find render function into separate module

--- packages/renderer-worker/src/parts/ViewletFindWidget/ViewletFindWidget.js
+++ packages/renderer-worker/src/parts/ViewletFindWidget/ViewletFindWidget.js
@@ -1,8 +1,6 @@
 import * as Command from '../Command/Command.js'
 import * as TextDocumentSearch from '../TextDocumentSearch/TextDocumentSearch.js'
-import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
 import * as ViewletStates from '../ViewletStates/ViewletStates.js'
-import * as ViewletFindWidgetStrings from './ViewletFindWidgetStrings.js'
 
 export const create = () => {
   return {
@@ -124,63 +122,3 @@ export const focusPrevious = (state) => {
   }
   return focusIndex(state, matchIndex - 1)
 }
-
-export const hasFunctionalRender = true
-
-const renderValue = {
-  isEqual(oldState, newState) {
-    return oldState.value === newState.value
-  },
-  apply(oldState, newState) {
-    return [/* Viewlet.invoke */ 'Viewlet.send', /* id */ ViewletModuleId.FindWidget, /* method */ 'setValue', /* value */ newState.value]
-  },
-}
-
-const getMatchCountText = (matchIndex, matchCount) => {
-  if (matchCount === 0) {
-    return ViewletFindWidgetStrings.noResults()
-  }
-  return ViewletFindWidgetStrings.matchOf(matchIndex + 1, matchCount)
-}
-
-const renderMatchCount = {
-  isEqual(oldState, newState) {
-    return oldState.matchIndex === newState.matchIndex && oldState.matchCount === newState.matchCount
-  },
-  apply(oldState, newState) {
-    const matchCountText = getMatchCountText(newState.matchIndex, newState.matchCount)
-    return [/* method */ 'setMatchCountText', /* value */ matchCountText]
-  },
-}
-
-const renderButtonsEnabled = {
-  isEqual(oldState, newState) {
-    return oldState.matchCount === newState.matchCount
-  },
-  apply(oldState, newState) {
-    const enabled = newState.matchCount > 0
-    return [/* method */ 'setButtonsEnabled', /* enabled */ enabled]
-  },
-}
-
-const getAriaLabel = (state) => {
-  const { matchIndex, matchCount, value } = state
-  return ViewletFindWidgetStrings.matchesFoundFor(matchIndex, matchCount, value)
-}
-
-const renderAriaAnnouncement = {
-  isEqual(oldState, newState) {
-    return (
-      oldState.ariaAnnouncement === newState.ariaAnnouncement &&
-      oldState.matchIndex === newState.matchIndex &&
-      oldState.matchCount === newState.matchCount &&
-      oldState.value === newState.value
-    )
-  },
-  apply(oldState, newState) {
-    const ariaLabel = getAriaLabel(newState)
-    return [/* Viewlet.invoke */ 'Viewlet.ariaAnnounce', /* text */ ariaLabel]
-  },
-}
-
-export const render = [renderValue, renderMatchCount, renderAriaAnnouncement, renderButtonsEnabled]
