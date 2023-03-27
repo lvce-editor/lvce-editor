9a49aa645f5b74c113e89adc962ae7e897e3c114
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-27 17:27:17 +0200

    fix: use event.preventDefault on title bar context menu (#2403)

--- packages/renderer-process/src/parts/ViewletTitleBar/ViewletTitleBarEvents.js
+++ packages/renderer-process/src/parts/ViewletTitleBar/ViewletTitleBarEvents.js
@@ -1,24 +1,5 @@
-import * as ViewletTitleBarFunctions from './ViewletTitleBarFunctions.js'
+import * as Event from '../Event/Event.js'
 
-/**
- *
- * @param {MouseEvent} event
- */
-export const handleTitleBarButtonsClick = (event) => {
-  const { target } = event
-  // @ts-ignore
-  const { id } = target
-  switch (id) {
-    case 'TitleBarButtonMinimize':
-      ViewletTitleBarFunctions.handleTitleBarButtonClickMinmize()
-      break
-    case 'TitleBarButtonToggleMaximize':
-      ViewletTitleBarFunctions.handleTitleBarButtonClickToggleMaximize()
-      break
-    case 'TitleBarButtonClose':
-      ViewletTitleBarFunctions.handleTitleBarButtonClickClose()
-      break
-    default:
-      break
-  }
+export const handleContextMenu = (event) => {
+  Event.preventDefault(event)
 }
