9aeb8fd05f5198e58184b010f936819a8e448c29
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-27 20:17:56 +0200

    feature: add option to use separate layer for completion items (#2415)

--- packages/renderer-worker/src/parts/ViewletEditorCompletion/ViewletEditorCompletionCss.js
+++ packages/renderer-worker/src/parts/ViewletEditorCompletion/ViewletEditorCompletionCss.js
@@ -1,1 +1,18 @@
+import * as JoinLines from '../JoinLines/JoinLines.js'
+
 export const Css = ['/css/parts/ViewletEditorCompletion.css', '/css/parts/Symbol.css', '/css/parts/ViewletList.css']
+
+export const getDynamicCss = (preferences) => {
+  const styles = []
+  const useLayer = preferences['completion.useLayer']
+  if (useLayer) {
+    styles.push(`  will-change: transform;`)
+  }
+  if (styles.length === 0) {
+    return ''
+  }
+  const css = `.EditorCompletion {
+${JoinLines.joinLines(styles)}
+}`
+  return css
+}
