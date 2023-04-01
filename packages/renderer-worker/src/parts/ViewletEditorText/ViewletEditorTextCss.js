48914e2e86b919bd12d4c9e8932fa18d6bb2ba05
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-01 19:16:56 +0200

    refactor: move editor dynamic css function to editor css module (#2473)

--- packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextCss.js
+++ packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextCss.js
@@ -1,1 +1,35 @@
+import * as JoinLines from '../JoinLines/JoinLines.js'
+
 export const Css = ['/css/parts/ViewletEditor.css', '/css/parts/ScrollBar.css']
+
+export const getDynamicCss = (preferences) => {
+  const styles = []
+  const fontSize = preferences['editor.fontSize']
+  if (fontSize) {
+    styles.push(`  --EditorFontSize: ${fontSize}px;`)
+  }
+  const fontFamily = preferences['editor.fontFamily']
+  if (fontFamily) {
+    styles.push(`  --EditorFontFamily: ${fontFamily};`)
+  }
+  const lineHeight = preferences['editor.lineHeight']
+  if (lineHeight) {
+    styles.push(`  --EditorLineHeight: ${lineHeight}px;`)
+  }
+  const letterSpacing = preferences['editor.letterSpacing']
+  if (typeof letterSpacing === 'number') {
+    styles.push(`  --EditorLetterSpacing: ${letterSpacing}px;`)
+  }
+  const fontLigatures = preferences['editor.fontLigatures']
+  if (fontLigatures) {
+    styles.push(`  --EditorFontFeatureSettings: "liga" 1, "calt" 1;`)
+  }
+  const tabSize = preferences['editor.tabSize']
+  if (tabSize) {
+    styles.push(` --EditorTabSize: ${tabSize}`)
+  }
+  const css = `:root {
+${JoinLines.joinLines(styles)}
+}`
+  return css
+}
