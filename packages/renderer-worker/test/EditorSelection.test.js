56cc9f40bc3390dde0287ffea0fc7e0d65f7f9b1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-01 18:25:08 +0200

    fix: hide cursor when editor is not focused (#2466)

--- packages/renderer-worker/test/EditorSelection.test.js
+++ packages/renderer-worker/test/EditorSelection.test.js
@@ -33,6 +33,7 @@ test('getVisible', () => {
     tabSize: 2,
     width: 800,
     differences: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    focused: true,
   }
   const { cursorInfos, selectionInfos } = EditorSelection.getVisible(editor)
   expect(cursorInfos).toEqual(new Float32Array([/*x */ 48, /* y */ 40, /* x */ 24, /* y */ 80]))
@@ -65,6 +66,7 @@ test('getVisible - bug with two lines', () => {
     tabSize: 2,
     width: 800,
     differences: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    focused: true,
   }
   const { cursorInfos, selectionInfos } = EditorSelection.getVisible(editor)
   expect(cursorInfos).toEqual(new Float32Array([/*x */ 32, /* y */ 20]))
@@ -95,6 +97,7 @@ test('getVisible - cursors should be treated separately', () => {
     tabSize: 2,
     width: 800,
     differences: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    focused: true,
   }
   const { cursorInfos, selectionInfos } = EditorSelection.getVisible(editor)
   expect(cursorInfos).toEqual(new Float32Array([/*x */ 32, /* y */ 0]))
@@ -119,6 +122,7 @@ test('getVisible - bug with multiple lines', () => {
     tabSize: 2,
     width: 800,
     differences: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    focused: true,
   }
   const { cursorInfos, selectionInfos } = EditorSelection.getVisible(editor)
   expect(cursorInfos).toEqual(new Float32Array([/* x */ 24, /* y */ 40]))
@@ -249,6 +253,7 @@ test('getVisible - only start of selection visible', () => {
     tabSize: 2,
     width: 800,
     differences: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    focused: true,
   }
   const { cursorInfos, selectionInfos } = EditorSelection.getVisible(editor)
   expect(cursorInfos).toEqual(new Float32Array([]))
@@ -285,6 +290,7 @@ test('getVisible - selection out of range', () => {
     tabSize: 2,
     width: 800,
     differences: [0, 0, 0, 0, 0, 0, 0, 0],
+    focused: true,
   }
   const { cursorInfos, selectionInfos } = EditorSelection.getVisible(editor)
   expect(cursorInfos).toEqual(new Float32Array([0, 0, 0, 140]))
