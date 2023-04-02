a615e3308360816a45e901acafa20d113184e303
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-02 19:04:22 +0200

    try to fix restarting electron app (#2492)

--- packages/shared-process/src/parts/Restart/Restart.js
+++ packages/shared-process/src/parts/Restart/Restart.js
@@ -1,6 +1,11 @@
 import { spawn } from 'node:child_process'
 
 export const restart = (downloadPath) => {
+  const { ELECTRON_RUN_AS_NODE, ...env } = process.env
   // TODO handle errors
-  spawn(downloadPath, { stdio: 'inherit' })
+  const childProcess = spawn(downloadPath, {
+    stdio: 'inherit',
+    env,
+  })
+  childProcess.unref()
 }
