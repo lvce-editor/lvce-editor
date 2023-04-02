5aab35a7b8f23753d20137ec796199b67b0b02bc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-02 15:04:13 +0200

    fix: app image update error: path must be of type string (#2486)

--- packages/shared-process/src/parts/AutoUpdaterAppImage/AutoUpdaterAppImage.js
+++ packages/shared-process/src/parts/AutoUpdaterAppImage/AutoUpdaterAppImage.js
@@ -1,11 +1,12 @@
 import { spawn } from 'node:child_process'
-import { chmod, rename } from 'node:fs/promises'
+import { rename } from 'node:fs/promises'
 import { tmpdir } from 'node:os'
 import { join } from 'node:path'
 import * as Assert from '../Assert/Assert.js'
 import * as CompareVersion from '../CompareVersion/CompareVersion.js'
 import * as Download from '../Download/Download.js'
 import * as GetLatestReleaseVersion from '../GetLatestReleaseVersion/GetLatestReleaseVersion.js'
+import * as MakeExecutable from '../MakeExecutable/MakeExecutable.js'
 import * as Platform from '../Platform/Platform.js'
 import { VError } from '../VError/VError.js'
 
@@ -28,6 +29,7 @@ export const downloadUpdate = async (version) => {
     const downLoadUrl = getDownloadUrl(repository, version, appImageName)
     const outFile = getOutfilePath(version)
     await Download.download(downLoadUrl, outFile)
+    return outFile
   } catch (error) {
     // @ts-ignore
     throw new VError(error, `Failed to download new version ${version}`)
@@ -65,17 +67,14 @@ const restart = (downloadPath) => {
   spawn(downloadPath, { stdio: 'inherit' })
 }
 
-const makeExecutable = async (file) => {
-  await chmod(file, 0o755)
-}
-
 export const installAndRestart = async (downloadPath) => {
   try {
+    Assert.string(downloadPath)
     const appImageFile = getAppImagePath()
     if (!appImageFile) {
       throw new Error(`AppImage path not found`)
     }
-    await makeExecutable(downloadPath)
+    await MakeExecutable.makeExecutable(downloadPath)
     await installNewAppImage(appImageFile, downloadPath)
     await restart(downloadPath)
   } catch (error) {
