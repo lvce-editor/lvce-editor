4caae60be77010f2280267db1ea840af8de89e1f
Le Vivilet <levivlet@pm.me>
2023-04-02 18:06:02 +0200

    fix: use same file name for updated app image to better integrate with appImageLauncher

--- packages/shared-process/src/parts/AutoUpdaterAppImage/AutoUpdaterAppImage.js
+++ packages/shared-process/src/parts/AutoUpdaterAppImage/AutoUpdaterAppImage.js
@@ -1,11 +1,12 @@
 import { spawn } from 'node:child_process'
 import { rename } from 'node:fs/promises'
 import { tmpdir } from 'node:os'
-import { basename, dirname, join } from 'node:path'
+import { join } from 'node:path'
 import * as Assert from '../Assert/Assert.js'
 import * as CompareVersion from '../CompareVersion/CompareVersion.js'
 import * as Download from '../Download/Download.js'
 import * as GetAppImageDownloadUrl from '../GetAppImageDownloadUrl/GetAppImageDownloadUrl.js'
+import * as GetAppImagePath from '../GetAppImagePath/GetAppImagePath.js'
 import * as GetLatestReleaseVersion from '../GetLatestReleaseVersion/GetLatestReleaseVersion.js'
 import * as MakeExecutable from '../MakeExecutable/MakeExecutable.js'
 import * as Platform from '../Platform/Platform.js'
@@ -45,22 +46,10 @@ export const checkForUpdatesAndNotify = async () => {
   }
 }
 
-const getAppImagePath = () => {
-  return process.env.APPIMAGE
-}
-
 const installNewAppImage = async (currentAppImageFile, downloadPath) => {
   try {
-    const currentFileName = basename(currentAppImageFile)
-    const newFileName = basename(downloadPath)
-    if (currentFileName === newFileName) {
-      await rename(downloadPath, currentAppImageFile)
-      return currentAppImageFile
-    }
-    const destinationFolder = dirname(currentAppImageFile)
-    const destinationPath = join(destinationFolder, newFileName)
-    await rename(downloadPath, destinationPath)
-    return destinationPath
+    await rename(downloadPath, currentAppImageFile)
+    return currentAppImageFile
   } catch (error) {
     // @ts-ignore
     throw new VError(error, `Failed to rename AppImage file`)
@@ -75,7 +64,7 @@ const restart = (downloadPath) => {
 export const installAndRestart = async (downloadPath) => {
   try {
     Assert.string(downloadPath)
-    const currentAppImageFile = getAppImagePath()
+    const currentAppImageFile = GetAppImagePath.getAppImagePath()
     if (!currentAppImageFile) {
       throw new Error(`AppImage path not found`)
     }
---
+++ packages/shared-process/src/parts/GetAppImagePath/GetAppImagePath.js
@@ -0,0 +1,3 @@
+export const getAppImagePath = () => {
+  return process.env.APPIMAGE
+}
