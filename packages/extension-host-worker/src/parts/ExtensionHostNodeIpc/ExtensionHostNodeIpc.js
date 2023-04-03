253e5a9134e77a03177c3abd16b8fdd804ec30d0
Le Vivilet <levivlet@pm.me>
2023-04-03 23:56:43 +0200

    allow spawning nodejs process from extension host helper process

--- packages/extension-host-worker/src/parts/ExtensionHostNodeIpc/ExtensionHostNodeIpc.js
+++ packages/extension-host-worker/src/parts/ExtensionHostNodeIpc/ExtensionHostNodeIpc.js
@@ -1,5 +1,16 @@
-import { NotImplementedError } from '../NotImplementedError/NotImplementedError.js'
+import * as IpcParent from '../IpcParent/IpcParent.js'
+import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 
-export const createNodeIpc = () => {
-  throw new NotImplementedError()
+const getTsPath = () => {
+  return ``
+}
+
+export const createNodeIpc = async () => {
+  const tsPath = getTsPath()
+  const ipc = await IpcParent.create({
+    method: IpcParentType.ElectronMessagePort,
+    type: `custom:${tsPath}`,
+  })
+  console.log({ ipc })
+  return ipc
 }
