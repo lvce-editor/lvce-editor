fafd17493eedbb11b4a404876fa93534c4854e23
Le Vivilet <levivlet@pm.me>
2023-04-04 01:48:53 +0200

    refactor: reimplement electron ipc to allow for better error handling

--- packages/main-process/src/parts/HandleMessagePort/HandleMessagePort.js
+++ packages/main-process/src/parts/HandleMessagePort/HandleMessagePort.js
@@ -1,3 +1,8 @@
+const { MessageChannelMain } = require('electron')
+const Assert = require('../Assert/Assert.js')
+const ErrorHandling = require('../ErrorHandling/ErrorHandling.js')
+const GetErrorResponse = require('../GetErrorResponse/GetErrorResponse.js')
+const GetSuccessResponse = require('../GetSuccessResponse/GetSuccessResponse.js')
 const Logger = require('../Logger/Logger.js')
 
 const getModule = (type) => {
@@ -23,12 +28,25 @@ const getModule = (type) => {
 /**
  * @param {import('electron').IpcMainEvent} event
  */
-exports.handlePort = async (event, data) => {
-  const module = getModule(data)
-  if (!module) {
-    Logger.error(`[main-process] unexpected port type ${data}`)
-    return
+exports.handlePort = async (event, message) => {
+  Assert.object(event)
+  Assert.object(message)
+  const sender = event.sender
+  const data = message.params[0]
+  try {
+    const module = getModule(data)
+    if (!module) {
+      Logger.error(`[main-process] unexpected port type ${data}`)
+      return
+    }
+    const channel = new MessageChannelMain()
+    const { port1, port2 } = channel
+    await module.handlePort(event, port1, data)
+    const response = GetSuccessResponse.getSuccessResponse(message, null)
+    sender.postMessage('port', response, [port2])
+  } catch (error) {
+    await ErrorHandling.handleError(error)
+    const response = GetErrorResponse.getErrorResponse(message, error)
+    sender.postMessage('port', response)
   }
-  const port = event.ports[0]
-  await module.handlePort(event, port, data)
 }
--- packages/renderer-process/src/parts/IpcParentWithElectron/IpcParentWithElectron.js
+++ packages/renderer-process/src/parts/IpcParentWithElectron/IpcParentWithElectron.js
@@ -1,19 +1,36 @@
-export const create = ({ type }) => {
-  return new Promise((resolve, reject) => {
-    const handleMessageFromWindow = (event) => {
-      const port = event.ports[0]
-      resolve(port)
-    }
+import * as Callback from '../Callback/Callback.js'
+import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
+import * as RestoreJsonRpcError from '../RestoreJsonRpcError/RestoreJsonRpcError.js'
 
-    // @ts-ignore
-    window.addEventListener('message', handleMessageFromWindow, {
-      once: true,
-    })
-    // @ts-ignore
-    if (typeof window.myApi === 'undefined') {
-      reject(new Error('Electron api was requested but is not available'))
-    }
-    // @ts-ignore
-    window.myApi.ipcConnect(type)
-  })
+const handleMessageFromWindow = (event) => {
+  const { data } = event
+  Callback.resolve(data.id, data)
+}
+
+// @ts-ignore
+window.addEventListener('message', handleMessageFromWindow)
+
+export const create = async ({ type }) => {
+  const { id, promise } = Callback.registerPromise()
+  const message = {
+    jsonrpc: JsonRpcVersion.Two,
+    id,
+    method: 'CreateMessagePort.createMessagePort',
+    params: [type],
+  }
+  // @ts-ignore
+  if (typeof window.myApi === 'undefined') {
+    throw new Error('Electron api was requested but is not available')
+  }
+  // @ts-ignore
+  window.myApi.ipcConnect(message)
+  const responseMessage = await promise
+  if ('error' in responseMessage) {
+    const restoredError = RestoreJsonRpcError.restoreJsonRpcError(responseMessage.error)
+    throw restoredError
+  }
+  if ('result' in responseMessage) {
+    return responseMessage.result
+  }
+  return responseMessage
 }
