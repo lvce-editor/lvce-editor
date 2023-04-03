89d2475542beb6d09c38e9d64d19dbf5832e2aa9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-04 01:43:09 +0200

    refactor: separate getSuccessResponse and getErrorResponse into separ… (#2529)

--- packages/main-process/src/parts/GetResponse/GetResponse.js
+++ packages/main-process/src/parts/GetResponse/GetResponse.js
@@ -1,44 +1,12 @@
-const { CommandNotFoundError } = require('../CommandNotFoundError/CommandNotFoundError.js')
 const Command = require('../Command/Command.js')
-const JsonRpc = require('../JsonRpc/JsonRpc.js')
-const JsonRpcVersion = require('../JsonRpcVersion/JsonRpcVersion.js')
-const PrettyError = require('../PrettyError/PrettyError.js')
+const GetErrorResponse = require('../GetErrorResponse/GetErrorResponse.js')
+const GetSuccessResponse = require('../GetSuccessResponse/GetSuccessResponse.js')
 
 exports.getResponse = async (message) => {
   try {
     const result = await Command.invoke(message.method, ...message.params)
-    return {
-      jsonrpc: JsonRpcVersion.Two,
-      result,
-      id: message.id,
-    }
+    return GetSuccessResponse.getSuccessResponse(message, result)
   } catch (error) {
-    const prettyError = await PrettyError.prepare(error)
-    PrettyError.print(prettyError)
-    if (error && error instanceof CommandNotFoundError) {
-      return {
-        jsonrpc: JsonRpcVersion.Two,
-        id: message.id,
-        error: {
-          code: JsonRpc.ErrorMethodNotFound,
-          message: error.message,
-          data: error.stack,
-        },
-      }
-    }
-    return {
-      jsonrpc: JsonRpcVersion.Two,
-      id: message.id,
-      error: {
-        // TODO actually check that error.message and error.stack exist
-        // @ts-ignore
-        message: prettyError.message,
-        data: {
-          // @ts-ignore
-          stack: prettyError.stack,
-          codeFrame: prettyError.codeFrame,
-        },
-      },
-    }
+    return GetErrorResponse.getErrorResponse(message, error)
   }
 }
