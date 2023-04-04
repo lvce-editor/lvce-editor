0b582c2fc1739a88fc14950a55381e1337fa4fca
Le Vivilet <levivlet@pm.me>
2023-04-04 22:26:18 +0200

    refactor: simplify getResponse code in shared process

--- packages/shared-process/src/parts/GetResponse/GetResponse.js
+++ packages/shared-process/src/parts/GetResponse/GetResponse.js
@@ -1,68 +1,15 @@
 import * as Command from '../Command/Command.js'
-import { CommandNotFoundError } from '../CommandNotFoundError/CommandNotFoundError.js'
-import * as ErrorCodes from '../ErrorCodes/ErrorCodes.js'
-import * as JsonRpcErrorCode from '../JsonRpcErrorCode/JsonRpcErrorCode.js'
-import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
-import * as PrettyError from '../PrettyError/PrettyError.js'
-import * as PrintPrettyError from '../PrintPrettyError/PrintPrettyError.js'
+import * as GetErrorResponse from '../GetErrorResponse/GetErrorResponse.js'
+import * as GetSuccessResponse from '../GetSuccessResponse/GetSuccessResponse.js'
 import * as RequiresSocket from '../RequiresSocket/RequiresSocket.js'
-
-const shouldLogError = (error) => {
-  if (error && error.code === ErrorCodes.ENOENT) {
-    return false
-  }
-  return true
-}
 
 export const getResponse = async (message, handle) => {
   try {
     const result = RequiresSocket.requiresSocket(message.method)
       ? await Command.execute(message.method, handle, ...message.params)
       : await Command.execute(message.method, ...message.params)
-    return {
-      jsonrpc: JsonRpcVersion.Two,
-      id: message.id,
-      result: result ?? null,
-    }
+    return GetSuccessResponse.getSuccessResponse(message, result)
   } catch (error) {
-    if (error && error instanceof CommandNotFoundError) {
-      return {
-        jsonrpc: JsonRpcVersion.Two,
-        id: message.id,
-        error: {
-          code: JsonRpcErrorCode.MethodNotFound,
-          message: error.message,
-          data: error.stack,
-        },
-      }
-    }
-    if (!shouldLogError(error)) {
-      return {
-        jsonrpc: JsonRpcVersion.Two,
-        id: message.id,
-        error: {
-          code: JsonRpcErrorCode.Custom,
-          message: `${error}`,
-          data: {
-            code: error.code,
-          },
-        },
-      }
-    }
-    const prettyError = PrettyError.prepare(error)
-    PrintPrettyError.printPrettyError(prettyError, `[shared-process] `)
-    return {
-      jsonrpc: JsonRpcVersion.Two,
-      id: message.id,
-      error: {
-        code: JsonRpcErrorCode.Custom,
-        message: prettyError.message,
-        data: {
-          stack: prettyError.stack,
-          codeFrame: prettyError.codeFrame,
-          type: prettyError.type,
-        },
-      },
-    }
+    return GetErrorResponse.getErrorResponse(message, error)
   }
 }
