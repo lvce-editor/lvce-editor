9b0639170c342d8b5b31cf2ceb00364fcba9cf15
Le Vivilet <levivlet@pm.me>
2023-04-04 20:31:36 +0200

    fix failing test

--- packages/renderer-worker/test/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.test.js
+++ packages/renderer-worker/test/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.test.js
@@ -6,50 +6,22 @@ beforeEach(() => {
 
 jest.unstable_mockModule('../src/parts/RendererProcess/RendererProcess.js', () => {
   return {
-    state: {
-      ipc: {
-        set onmessage(listener) {
-          this._onmessage = listener
-        },
-        get onmessage() {
-          return this._onmessage
-        },
-      },
-    },
-    send: jest.fn(),
-  }
-})
-jest.unstable_mockModule('../src/parts/Callback/Callback.js', () => {
-  return {
-    registerPromise: jest.fn(),
+    invoke: jest.fn(),
   }
 })
 
 const RendererProcess = await import('../src/parts/RendererProcess/RendererProcess.js')
-const Callback = await import('../src/parts/Callback/Callback.js')
 const IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug = await import(
   '../src/parts/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.js'
 )
 
 test('create', async () => {
-  let _resolve
-  // @ts-ignore
-  Callback.registerPromise.mockImplementation(() => {
-    return {
-      id: 1,
-      promise: new Promise((resolve) => {
-        _resolve = resolve
-      }),
-    }
-  })
   const port = {
     __isMessagePort: true,
   }
   // @ts-ignore
-  RendererProcess.send.mockImplementation(() => {
-    _resolve({
-      result: port,
-    })
+  RendererProcess.invoke.mockImplementation(() => {
+    return port
   })
   const ipc = await IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.create({
     url: 'https://example.com/worker.js',
