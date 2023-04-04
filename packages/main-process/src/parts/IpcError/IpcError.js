f2b83e2ab44e70e2f679411375889ed75253813b
Le Vivilet <levivlet@pm.me>
2023-04-04 16:56:16 +0200

    refactor: add getHelpfulChildProcessError function to main process

--- packages/main-process/src/parts/IpcError/IpcError.js
+++ packages/main-process/src/parts/IpcError/IpcError.js
@@ -1,6 +1,9 @@
+const GetHelpfulChildProcessError = require('../GetHelpfulChildProcessError/GetHelpfulChildProcessError.js')
+
 exports.IpcError = class extends Error {
   constructor(message, stdout = '', stderr = '') {
-    super(message)
+    const betterMessage = GetHelpfulChildProcessError.getHelpfulChildProcessError(message, stdout, stderr)
+    super(betterMessage)
     this.name = 'IpcError'
     this.stdout = stdout
     this.stderr = stderr
---
+++ packages/main-process/src/parts/MergeStacks/MergeStacks.js
@@ -0,0 +1,20 @@
+const GetNewLineIndex = require('../GetNewLineIndex/GetNewLineIndex.js')
+const NormalizeErrorLine = require('../NormalizeErrorLine/NormalizeErrorLine.js')
+
+exports.mergeStacks = (parent, child) => {
+  if (!child) {
+    return parent
+  }
+  const parentNewLineIndex = GetNewLineIndex.getNewLineIndex(parent)
+  const childNewLineIndex = GetNewLineIndex.getNewLineIndex(child)
+  if (childNewLineIndex === -1) {
+    return parent
+  }
+  const parentFirstLine = parent.slice(0, parentNewLineIndex)
+  const childRest = child.slice(childNewLineIndex)
+  const childFirstLine = NormalizeErrorLine.normalizeLine(child.slice(0, childNewLineIndex))
+  if (parentFirstLine.includes(childFirstLine)) {
+    return parentFirstLine + childRest
+  }
+  return child
+}
