0bb685ae35c131740afc44e88836fe9dfcbe1362
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-04 22:52:36 +0200

    refactor: simplify rpc code between extension host worker and rendere… (#2550)

--- packages/extension-host-worker/src/parts/Rpc/Rpc.js
+++ packages/extension-host-worker/src/parts/Rpc/Rpc.js
@@ -2,6 +2,8 @@ import * as Callback from '../Callback/Callback.js'
 import * as ErrorHandling from '../ErrorHandling/ErrorHandling.js'
 import * as GetErrorResponse from '../GetErrorResponse/GetErrorResponse.js'
 import * as GetResponse from '../GetResponse/GetResponse.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+import { JsonRpcError } from '../JsonRpcError/JsonRpcError.js'
 
 export const state = {
   /**
@@ -12,33 +14,33 @@ export const state = {
 
 export const send = (method, ...params) => {
   const { ipc } = state
-  ipc.send({
-    jsonrpc: '2.0',
-    method,
-    params,
-  })
+  JsonRpc.send(ipc, method, ...params)
 }
 
-export const listen = (ipc) => {
-  const handleMessage = async (event) => {
-    const message = event.data
+const handleMessageFromRendererWorker = async (event) => {
+  const message = event.data
+  if ('id' in message) {
     if ('method' in message) {
       const response = await GetResponse.getResponse(message)
       try {
-        ipc.send(response)
+        state.ipc.send(response)
       } catch (error) {
         await ErrorHandling.logError(error)
         const errorResponse = GetErrorResponse.getErrorResponse(message, error)
-        ipc.send(errorResponse)
+        state.ipc.send(errorResponse)
       }
-    } else if ('result' in message) {
-      Callback.resolve(message.id, message)
-    } else if ('error' in message) {
-      Callback.reject(message.id, message)
-    } else {
-      console.log({ message })
+      return
     }
+    Callback.resolve(message.id, message)
   }
-  ipc.onmessage = handleMessage
+  throw new JsonRpcError('unexpected message from renderer worker')
+}
+
+export const invoke = (method, ...params) => {
+  return JsonRpc.invoke(state.ipc, method, ...params)
+}
+
+export const listen = (ipc) => {
+  ipc.onmessage = handleMessageFromRendererWorker
   state.ipc = ipc
 }
