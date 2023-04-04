a2d75a4a5de84609c123939e62238f2a6a725b1a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-04 16:39:46 +0200

    refactor: move getHelpfulChildProcessError function into separate module (#2535)

---
+++ packages/shared-process/src/parts/GetHelpfulChildProcessError/GetHelpfulChildProcessError.js
@@ -0,0 +1,40 @@
+import * as SplitLines from '../SplitLines/SplitLines.js'
+
+const RE_NATIVE_MODULE_ERROR = /^innerError Error: Cannot find module '.*.node'/
+const RE_NATIVE_MODULE_ERROR_2 = /was compiled against a different Node.js version/
+
+const RE_MASSAGE_CODE_BLOCK_START = /^Error: The module '.*'$/
+const RE_MASSAGE_CODE_BLOCK_END = /^\s* at/
+
+const isUnhelpfulNativeModuleError = (stderr) => {
+  return RE_NATIVE_MODULE_ERROR.test(stderr) && RE_NATIVE_MODULE_ERROR_2.test(stderr)
+}
+
+const isMessageCodeBlockStartIndex = (line) => {
+  return RE_MASSAGE_CODE_BLOCK_START.test(line)
+}
+
+const isMessageCodeBlockEndIndex = (line) => {
+  return RE_MASSAGE_CODE_BLOCK_END.test(line)
+}
+
+const getMessageCodeBlock = (stderr) => {
+  const lines = SplitLines.splitLines(stderr)
+  const startIndex = lines.findIndex(isMessageCodeBlockStartIndex)
+  const endIndex = startIndex + lines.slice(startIndex).findIndex(isMessageCodeBlockEndIndex, startIndex)
+  const relevantLines = lines.slice(startIndex, endIndex)
+  const relevantMessage = relevantLines.join(' ').slice('Error: '.length)
+  return relevantMessage
+}
+
+const getNativeModuleErrorMessage = (stderr) => {
+  const message = getMessageCodeBlock(stderr)
+  return `incompatible native node module: ${message}`
+}
+
+export const getHelpfulChildProcessError = (stdout, stderr) => {
+  if (isUnhelpfulNativeModuleError(stderr)) {
+    return getNativeModuleErrorMessage(stderr)
+  }
+  return 'child process error'
+}
