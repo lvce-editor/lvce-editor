161bad18ed5c062bf00eea99c59cbd31b68e6a8c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-05 13:31:04 +0200

    refactor: simplify extension host helper process ipc (#2562)

--- packages/extension-host-helper-process/src/parts/IpcChildWithWebSocket/IpcChildWithWebSocket.js
+++ packages/extension-host-helper-process/src/parts/IpcChildWithWebSocket/IpcChildWithWebSocket.js
@@ -1,10 +1,32 @@
 import { once } from 'events'
-import * as CreateWebSocketIpc from '../CreateWebSocketIpc/CreateWebSocketIpc.js'
 import * as GetWebSocket from '../GetWebSocket/GetWebSocket.js'
+import * as Json from '../Json/Json.js'
 
 export const listen = async () => {
   const [message, handle] = await once(process, 'message')
   const webSocket = await GetWebSocket.getWebSocket(message, handle)
-  const ipc = CreateWebSocketIpc.createWebSocketIpc(webSocket)
-  return ipc
+  return webSocket
+}
+
+export const wrap = (webSocket) => {
+  return {
+    webSocket,
+    send(message) {
+      const stringifiedMessage = Json.stringify(message)
+      this.webSocket.send(stringifiedMessage)
+    },
+    on(event, listener) {
+      switch (event) {
+        case 'message':
+          const wrappedListener = (message) => {
+            const parsed = Json.parse(message.toString())
+            listener(parsed)
+          }
+          this.webSocket.on('message', wrappedListener)
+          break
+        default:
+          throw new Error('unknown event listener type')
+      }
+    },
+  }
 }
