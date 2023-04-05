548d0a3c41b2be41adf6a71995ac622e6632dd8c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-05 23:32:24 +0200

    fix: don't try to request text from file:// scheme (#2583)

--- packages/renderer-worker/src/parts/IsActualSourceFile/IsActualSourceFile.js
+++ packages/renderer-worker/src/parts/IsActualSourceFile/IsActualSourceFile.js
@@ -1,5 +1,12 @@
 export const isActualSourceFile = (path) => {
-  if (path === '<anonymous>' || path === 'debugger eval code' || path.startsWith('"') || path.startsWith(`'`) || path.startsWith(')')) {
+  if (
+    path === '<anonymous>' ||
+    path === 'debugger eval code' ||
+    path.startsWith('"') ||
+    path.startsWith(`'`) ||
+    path.startsWith(')') ||
+    path.startsWith('file://')
+  ) {
     return false
   }
   return true
