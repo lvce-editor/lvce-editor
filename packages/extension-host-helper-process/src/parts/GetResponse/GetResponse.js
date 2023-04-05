e883210f6e2511930fbb2c2f4e60a53eef9b35bf
Le Vivilet <levivlet@pm.me>
2023-04-05 12:37:45 +0200

    refactor: simplify getResponse function in extension host helper process

--- packages/extension-host-helper-process/src/parts/GetResponse/GetResponse.js
+++ packages/extension-host-helper-process/src/parts/GetResponse/GetResponse.js
@@ -1,40 +1,12 @@
 import * as Command from '../Command/Command.js'
-import * as JsonRpc from '../JsonRpc/JsonRpc.js'
-import * as PrettyError from '../PrettyError/PrettyError.js'
+import * as GetErrorResponse from '../GetErrorResponse/GetErrorResponse.js'
+import * as GetSuccessResponse from '../GetSuccessResponse/GetSuccessResponse.js'
 
 export const getResponse = async (message) => {
   try {
     const result = await Command.invoke(message.method, ...message.params)
-    return {
-      jsonrpc: JsonRpc.Version,
-      id: message.id,
-      result,
-    }
+    return GetSuccessResponse.getSuccessResponse(message, result)
   } catch (error) {
-    if (error && error instanceof Error && error.message && error.message.startsWith('method not found')) {
-      return {
-        jsonrpc: JsonRpc.Version,
-        id: message.id,
-        error: {
-          code: JsonRpc.ErrorMethodNotFound,
-          message: error.message,
-          data: error.stack,
-        },
-      }
-    }
-    const prettyError = PrettyError.prepare(error)
-    return {
-      jsonrpc: JsonRpc.Version,
-      id: message.id,
-      error: {
-        code: -32001,
-        message: prettyError.message,
-        data: {
-          stack: prettyError.stack,
-          codeFrame: prettyError.codeFrame,
-          code: prettyError.code,
-        },
-      },
-    }
+    return GetErrorResponse.getErrorResponse(message, error)
   }
 }
--- packages/extension-host-helper-process/src/parts/JsonRpc/JsonRpc.js
+++ packages/extension-host-helper-process/src/parts/JsonRpc/JsonRpc.js
@@ -1,3 +1,1 @@
-export const Version = '2.0'
-
 export const ErrorMethodNotFound = -32601
