dde431b5aec832fee6f7a7c45a69ee0b2426628b
Le Vivilet <levivlet@pm.me>
2023-04-06 15:23:06 +0200

    fix: improve csp error stack trace when source file is not available

--- packages/extension-host-worker/src/parts/ContentSecurityPolicyError/ContentSecurityPolicyError.js
+++ packages/extension-host-worker/src/parts/ContentSecurityPolicyError/ContentSecurityPolicyError.js
@@ -2,7 +2,12 @@ export class ContentSecurityPolicyError extends Error {
   constructor(violatedDirective, sourceFile, lineNumber, columnNumber) {
     super(`Content Security Policy Violation: ${violatedDirective}`)
     this.name = 'ContentSecurityPolicyError'
-    this.stack = `Content Security Policy Violation
-  at ${sourceFile}:${lineNumber}:${columnNumber}`
+    if (sourceFile) {
+      this.stack = `Content Security Policy Violation
+    at ${sourceFile}:${lineNumber}:${columnNumber}`
+    } else {
+      this.stack = `Content Security Policy Violation
+    at <unknown>`
+    }
   }
 }
