aff0aaba137d380aca3d4b640acd755912a7a97c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-07 10:21:13 +0200

    test: add uri error e2e test (#2591)

--- packages/renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponentPlatform.js
+++ packages/renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponentPlatform.js
@@ -3,3 +3,7 @@ import * as Platform from '../Platform/Platform.js'
 export const getNodePath = () => {
   return Platform.getNodePath()
 }
+
+export const isFirefox = () => {
+  return Platform.isFirefox
+}
