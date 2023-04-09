db9dec7c38b6d1a4ac651b1fe901f8ef03fbe583
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-09 14:17:55 +0200

    fix: improve error message when replacing occurrence fails (#2603)

--- build/src/parts/Replace/Replace.js
+++ build/src/parts/Replace/Replace.js
@@ -2,6 +2,10 @@ import VError from 'verror'
 import * as ReadFile from '../ReadFile/ReadFile.js'
 import * as WriteFile from '../WriteFile/WriteFile.js'
 
+const snippet = (string) => {
+  return string.replaceAll(/\s+/, ' ').slice(0, 15)
+}
+
 export const replace = async ({ path, occurrence, replacement }) => {
   try {
     const content = await ReadFile.readFile(path)
@@ -14,7 +18,8 @@ export const replace = async ({ path, occurrence, replacement }) => {
       content: newContent,
     })
   } catch (error) {
+    const occurrenceSnippet = snippet(occurrence)
     // @ts-ignore
-    throw new VError(error, `Failed to replace occurrence in ${path}`)
+    throw new VError(error, `Failed to replace occurrence ${occurrenceSnippet} in ${path}`)
   }
 }
