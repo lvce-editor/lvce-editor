b18298b9118ef2047c0ef11417f768052bef9f21
Le Vivilet <levivlet@pm.me>
2023-04-11 09:55:35 +0200

    fix: improve error handling when creating node rpc fails

--- packages/extension-host-worker/src/parts/ExtensionHostNodeIpc/ExtensionHostNodeIpc.js
+++ packages/extension-host-worker/src/parts/ExtensionHostNodeIpc/ExtensionHostNodeIpc.js
@@ -3,19 +3,24 @@ import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as RpcParent from '../RpcParent/RpcParent.js'
 import * as RpcParentType from '../RpcParentType/RpcParentType.js'
+import { VError } from '../VError/VError.js'
 
 export const createNodeRpc = async ({ path }) => {
-  Assert.string(path)
-  const ipc = await IpcParent.create({
-    method: IpcParentType.ElectronMessagePort,
-    type: `custom:${path}`,
-  })
-  const rpc = await RpcParent.create({
-    ipc,
-    method: RpcParentType.JsonRpc,
-  })
-  await rpc.invoke('LoadFile.loadFile', path)
-  console.log({ path })
+  try {
+    Assert.string(path)
+    const ipc = await IpcParent.create({
+      method: IpcParentType.ElectronMessagePort,
+      type: `custom:${path}`,
+    })
+    const rpc = await RpcParent.create({
+      ipc,
+      method: RpcParentType.JsonRpc,
+    })
+    await rpc.invoke('LoadFile.loadFile', path)
+    console.log({ path })
 
-  return rpc
+    return rpc
+  } catch (error) {
+    throw new VError(error, `Failed to create node rpc`)
+  }
 }
