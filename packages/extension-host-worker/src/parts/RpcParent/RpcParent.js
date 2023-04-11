ec9b8a204e5a912dcd02f9b51c1f73175e4effc2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-11 10:17:54 +0200

    Restore packages/extension-host-worker/src/parts/RpcParent/RpcParent.js

--- packages/extension-host-worker/src/parts/RpcParent/RpcParent.js
+++ packages/extension-host-worker/src/parts/RpcParent/RpcParent.js
@@ -1,13 +1,7 @@
 import * as RpcParentModule from '../RpcParentModule/RpcParentModule.js'
-import { VError } from '../VError/VError.js'
 
 export const create = async ({ method, ...options }) => {
-  try {
-    const module = await RpcParentModule.getModule(method)
-    const rpc = module.create(options)
-    return rpc
-  } catch (error) {
-    console.log({ error })
-    throw new VError(error, `Failed to create rpc`)
-  }
+  const module = await RpcParentModule.getModule(method)
+  const rpc = module.create(options)
+  return rpc
 }
