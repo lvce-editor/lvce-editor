2f05b1f3f8036beb21a3fd56dd3587b2c3c0f11d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-11 11:46:44 +0200

    refactor: remove console logs (#2630)

--- packages/extension-host-helper-process/src/parts/IpcChildWithElectronMessagePort/IpcChildWithElectronMessagePort.js
+++ packages/extension-host-helper-process/src/parts/IpcChildWithElectronMessagePort/IpcChildWithElectronMessagePort.js
@@ -2,7 +2,6 @@ import { once } from 'events'
 
 export const listen = async () => {
   // const [message, handle] = await once(process, 'message')
-  // console.log({ message, handle })
   // if (process.send) {
   //   process.send('ready')
   // }
