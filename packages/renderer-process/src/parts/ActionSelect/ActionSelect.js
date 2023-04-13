93d2c88a3d19d6a39be1a8eef50c898713bf5f40
Le Vivilet <levivlet@pm.me>
2023-04-13 19:44:34 +0200

    improve select css

--- packages/renderer-process/src/parts/ActionSelect/ActionSelect.js
+++ packages/renderer-process/src/parts/ActionSelect/ActionSelect.js
@@ -6,9 +6,11 @@ export const create = ({ options }) => {
     $Option.textContent = option
     $Select.append($Option)
   }
+  const $SelectIcon = document.createElement('div')
+  $SelectIcon.className = 'SelectIcon'
   // TODO figure out if it is possible to get rid of select wrapper element
   const $SelectWrapper = document.createElement('div')
   $SelectWrapper.className = 'SelectWrapper'
-  $SelectWrapper.append($Select)
+  $SelectWrapper.append($Select, $SelectIcon)
   return $SelectWrapper
 }
