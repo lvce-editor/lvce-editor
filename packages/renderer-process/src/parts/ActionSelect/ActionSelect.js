c9e3961673745b359a5a33e8b088226f44dd7129
Le Vivilet <levivlet@pm.me>
2023-04-13 19:07:45 +0200

    refactor: try to improve select css

--- packages/renderer-process/src/parts/ActionSelect/ActionSelect.js
+++ packages/renderer-process/src/parts/ActionSelect/ActionSelect.js
@@ -1,9 +1,14 @@
 export const create = ({ options }) => {
   const $Select = document.createElement('select')
+  $Select.className = 'Select'
   for (const option of options) {
     const $Option = document.createElement('option')
     $Option.textContent = option
     $Select.append($Option)
   }
-  return $Select
+  // TODO figure out if it is possible to get rid of select wrapper element
+  const $SelectWrapper = document.createElement('div')
+  $SelectWrapper.className = 'SelectWrapper'
+  $SelectWrapper.append($Select)
+  return $SelectWrapper
 }
