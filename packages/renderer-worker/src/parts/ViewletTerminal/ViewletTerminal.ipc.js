51baaa08c7728ee02cbefbbd23b64267c0908524
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-14 22:40:16 +0200

    add more terminal icons to panel (#2664)

--- packages/renderer-worker/src/parts/ViewletTerminal/ViewletTerminal.ipc.js
+++ packages/renderer-worker/src/parts/ViewletTerminal/ViewletTerminal.ipc.js
@@ -1,16 +1,6 @@
-import * as ViewletTerminal from './ViewletTerminal.js'
-
 export const name = 'Terminal'
 
-// prettier-ignore
-export const Commands = {
-  clear: ViewletTerminal.clear,
-  write: ViewletTerminal.write,
-}
-
-export const Css = [
-  '/css/parts/ViewletTerminal.css',
-  '/static/lib-css/termterm.css',
-]
-
 export * from './ViewletTerminal.js'
+export * from './ViewletTerminalCommands.js'
+export * from './ViewletTermincalCss.js'
+export * from './ViewletTerminalActions.js'
