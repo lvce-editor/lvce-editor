244d1cc211af4944b62a59c5e4473340ae245e8e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-14 23:26:24 +0200

    refactor: use classnames instead of ids for sidebar styles for simpli… (#2669)

--- packages/renderer-process/src/parts/ViewletSideBar/ViewletSideBar.js
+++ packages/renderer-process/src/parts/ViewletSideBar/ViewletSideBar.js
@@ -5,17 +5,14 @@ import * as Assert from '../Assert/Assert.js'
 
 export const create = () => {
   const $SideBarTitleAreaTitle = document.createElement('h2')
-  $SideBarTitleAreaTitle.id = 'SideBarTitleAreaTitle'
+  $SideBarTitleAreaTitle.className = 'SideBarTitleAreaTitle'
 
   const $SideBarTitleAreaButtons = document.createElement('div')
-  $SideBarTitleAreaButtons.id = 'SideBarTitleAreaButtons'
+  $SideBarTitleAreaButtons.className = 'SideBarTitleAreaButtons'
 
   const $SideBarTitleArea = document.createElement('div')
-  $SideBarTitleArea.id = 'SideBarTitleArea'
+  $SideBarTitleArea.className = 'SideBarTitleArea'
   $SideBarTitleArea.append($SideBarTitleAreaTitle, $SideBarTitleAreaButtons)
-
-  // const $SideBarContent = document.createElement('div')
-  // $SideBarContent.id = 'SideBarContent'
 
   const $Viewlet = document.createElement('div')
   $Viewlet.id = 'SideBar'
@@ -38,22 +35,6 @@ export const create = () => {
 export const dispose = (state) => {
   Assert.object(state)
   state.$SideBar.replaceChildren()
-}
-
-export const appendViewlet = (state, name, $Viewlet) => {
-  Assert.object(state)
-  Assert.string(name)
-  const $SideBarTitleAreaTitle = state.$SideBarTitleAreaTitle
-  $SideBarTitleAreaTitle.title = name
-  $SideBarTitleAreaTitle.textContent = name
-  // TODO is it a problem that the id is duplicated for a short amount of time here?
-  $Viewlet.id = 'SideBarContent'
-  if (state.$SideBarContent) {
-    state.$SideBarContent.replaceWith($Viewlet)
-  } else {
-    state.$SideBar.append($Viewlet)
-  }
-  state.$SideBarContent = $Viewlet
 }
 
 export const setTitle = (state, name) => {
--- packages/renderer-process/test/ViewletSideBar.test.js
+++ packages/renderer-process/test/ViewletSideBar.test.js
@@ -8,7 +8,7 @@ test('create', () => {
   // at the same time so that there is only one layout and recalc style
   const state = ViewletSideBar.create()
   expect(state.$SideBar.innerHTML).toBe(
-    '<div id="SideBarTitleArea"><h2 id="SideBarTitleAreaTitle"></h2><div id="SideBarTitleAreaButtons"></div></div>'
+    '<div class="SideBarTitleArea"><h2 class="SideBarTitleAreaTitle"></h2><div class="SideBarTitleAreaButtons"></div></div>'
   )
 })
 
@@ -18,29 +18,4 @@ test('dispose', () => {
   const state = ViewletSideBar.create()
   // TODO what to test?
   ViewletSideBar.dispose(state)
-})
-
-test('appendViewlet', () => {
-  const state = ViewletSideBar.create()
-  const $TestViewlet = document.createElement('div')
-  $TestViewlet.id = 'TestViewlet'
-  ViewletSideBar.appendViewlet(state, 'Test', $TestViewlet)
-  const $SideBarContent = state.$SideBarContent
-  const $SideBarTitleAreaTitle = state.$SideBarTitleAreaTitle
-  expect($SideBarTitleAreaTitle.textContent).toBe('Test')
-  expect($SideBarContent).toBe($TestViewlet)
-})
-
-test('appendViewlet - a viewlet already exists', () => {
-  const state = ViewletSideBar.create()
-  const $TestViewlet1 = document.createElement('div')
-  $TestViewlet1.id = 'TestViewlet1'
-  ViewletSideBar.appendViewlet(state, 'Test 1', $TestViewlet1)
-  const $TestViewlet2 = document.createElement('div')
-  $TestViewlet2.id = 'TestViewlet2'
-  ViewletSideBar.appendViewlet(state, 'Test 2', $TestViewlet2)
-  const $SideBarContent = state.$SideBarContent
-  const $SideBarTitleAreaTitle = state.$SideBarTitleAreaTitle
-  expect($SideBarTitleAreaTitle.textContent).toBe('Test 2')
-  expect($SideBarContent).toBe($TestViewlet2)
 })
