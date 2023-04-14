4bacca1dad8edd0b772525730116d29514caec21
Le Vivilet <levivlet@pm.me>
2023-04-14 12:24:40 +0200

    refactor: add button to maximize panel

--- packages/renderer-process/src/parts/ViewletPanel/ViewletPanelEvents.js
+++ packages/renderer-process/src/parts/ViewletPanel/ViewletPanelEvents.js
@@ -3,3 +3,7 @@ import * as ViewletPanelFunctions from './ViewletPanelFunctions.js'
 export const handleClickClose = (event) => {
   ViewletPanelFunctions.hidePanel()
 }
+
+export const handleClickMaximize = (event) => {
+  ViewletPanelFunctions.toggleMaximize()
+}
--- packages/renderer-process/src/parts/ViewletPanel/ViewletPanelFunctions.js
+++ packages/renderer-process/src/parts/ViewletPanel/ViewletPanelFunctions.js
@@ -3,3 +3,7 @@ import * as RendererWorker from '../RendererWorker/RendererWorker.js'
 export const hidePanel = () => {
   RendererWorker.send('Layout.hidePanel')
 }
+
+export const toggleMaximize = () => {
+  RendererWorker.send('Layout.maximizePanel')
+}
