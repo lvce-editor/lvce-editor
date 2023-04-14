17ba40964a4f51ad29b3cdaa80a0949ede32ef90
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-14 17:14:25 +0200

    fix: improve unlink error message (#2653)

--- packages/shared-process/src/parts/CliUnlink/CliUnlink.js
+++ packages/shared-process/src/parts/CliUnlink/CliUnlink.js
@@ -1,7 +1,19 @@
+import * as ErrorCodes from '../ErrorCodes/ErrorCodes.js'
+import * as ExitCode from '../ExitCode/ExitCode.js'
 import * as ExtensionUnlink from '../ExtensionUnlink/ExtensionUnlink.js'
+import * as Logger from '../Logger/Logger.js'
 import * as Process from '../Process/Process.js'
 
 export const handleCliArgs = async (argv) => {
-  const path = argv.length > 1 ? argv[1] : Process.cwd()
-  await ExtensionUnlink.unlink(path)
+  try {
+    const path = argv.length > 1 ? argv[1] : Process.cwd()
+    await ExtensionUnlink.unlink(path)
+  } catch (error) {
+    if (error && error.code === ErrorCodes.E_MANIFEST_NOT_FOUND) {
+      Logger.error(error.message)
+      Process.setExitCode(ExitCode.ExpectedError)
+      return
+    }
+    throw error
+  }
 }
