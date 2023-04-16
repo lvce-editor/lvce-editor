fdab4b266a0bb06c01179207a8d08175734ce75a
Le Vivilet <levivlet@pm.me>
2023-04-16 23:17:22 +0200

    refactor: use uid for problems events

--- packages/renderer-process/src/parts/ViewletProblems/ViewletProblemsEvents.js
+++ packages/renderer-process/src/parts/ViewletProblems/ViewletProblemsEvents.js
@@ -1,7 +1,9 @@
+import * as ComponentUid from '../ComponentUid/ComponentUid.js'
 import * as Event from '../Event/Event.js'
 import * as ViewletProblemsFunctions from './ViewletProblemsFunctions.js'
 
 export const handlePointerDown = (event) => {
   Event.preventDefault(event)
-  ViewletProblemsFunctions.focusIndex(-1)
+  const uid = ComponentUid.fromEvent(event)
+  ViewletProblemsFunctions.focusIndex(uid, -1)
 }
