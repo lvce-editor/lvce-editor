31d27d820953fa40ad49a1c3770fff79499df2fe
Le Vivilet <levivlet@pm.me>
2023-04-16 22:18:41 +0200

    fix failing tests

--- packages/renderer-process/test/ViewletTitleBarButtonsEvents.test.js
+++ packages/renderer-process/test/ViewletTitleBarButtonsEvents.test.js
@@ -2,66 +2,59 @@
  * @jest-environment jsdom
  */
 import { jest } from '@jest/globals'
+import * as ComponentUid from '../src/parts/ComponentUid/ComponentUid.js'
 
 beforeEach(() => {
   jest.resetAllMocks()
 })
 
-jest.unstable_mockModule(
-  '../src/parts/RendererWorker/RendererWorker.js',
-  () => {
-    return {
-      send: jest.fn(),
-    }
+jest.unstable_mockModule('../src/parts/ExecuteViewletCommand/ExecuteViewletCommand.js', () => {
+  return {
+    executeViewletCommand: jest.fn(() => {}),
   }
-)
+})
 
-const RendererWorker = await import(
-  '../src/parts/RendererWorker/RendererWorker.js'
-)
-
-const ViewletTitleBarButtons = await import(
-  '../src/parts/ViewletTitleBarButtons/ViewletTitleBarButtons.js'
-)
+const ViewletTitleBarButtons = await import('../src/parts/ViewletTitleBarButtons/ViewletTitleBarButtons.js')
+const ExecuteViewletCommand = await import('../src/parts/ExecuteViewletCommand/ExecuteViewletCommand.js')
 
 test('event - click - minimize', () => {
   const state = ViewletTitleBarButtons.create()
+  const { $Viewlet } = state
+  ComponentUid.set($Viewlet, 1)
   const { $TitleBarButtons } = state
   const event = new MouseEvent('mousedown', {
     bubbles: true,
     cancelable: true,
   })
   $TitleBarButtons.children[0].dispatchEvent(event)
-  expect(RendererWorker.send).toHaveBeenCalledTimes(1)
-  expect(RendererWorker.send).toHaveBeenCalledWith(
-    'TitleBarButtons.handleClickMinimize'
-  )
+  expect(ExecuteViewletCommand.executeViewletCommand).toHaveBeenCalledTimes(1)
+  expect(ExecuteViewletCommand.executeViewletCommand).toHaveBeenCalledWith(1, 'handleClickMinimize')
 })
 
 test('event - click - toggleMaximize', () => {
   const state = ViewletTitleBarButtons.create()
   const { $TitleBarButtons } = state
+  const { $Viewlet } = state
+  ComponentUid.set($Viewlet, 1)
   const event = new MouseEvent('mousedown', {
     bubbles: true,
     cancelable: true,
   })
   $TitleBarButtons.children[1].dispatchEvent(event)
-  expect(RendererWorker.send).toHaveBeenCalledTimes(1)
-  expect(RendererWorker.send).toHaveBeenCalledWith(
-    'TitleBarButtons.handleClickToggleMaximize'
-  )
+  expect(ExecuteViewletCommand.executeViewletCommand).toHaveBeenCalledTimes(1)
+  expect(ExecuteViewletCommand.executeViewletCommand).toHaveBeenCalledWith(1, 'handleClickToggleMaximize')
 })
 
 test('event - click - close', () => {
   const state = ViewletTitleBarButtons.create()
+  const { $Viewlet } = state
+  ComponentUid.set($Viewlet, 1)
   const { $TitleBarButtons } = state
   const event = new MouseEvent('mousedown', {
     bubbles: true,
     cancelable: true,
   })
   $TitleBarButtons.children[2].dispatchEvent(event)
-  expect(RendererWorker.send).toHaveBeenCalledTimes(1)
-  expect(RendererWorker.send).toHaveBeenCalledWith(
-    'TitleBarButtons.handleClickClose'
-  )
+  expect(ExecuteViewletCommand.executeViewletCommand).toHaveBeenCalledTimes(1)
+  expect(ExecuteViewletCommand.executeViewletCommand).toHaveBeenCalledWith(1, 'handleClickClose')
 })
