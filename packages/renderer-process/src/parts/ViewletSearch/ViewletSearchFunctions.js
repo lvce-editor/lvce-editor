21c25cb4f08e091992e6d7716905b1a38a8e9abd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-16 22:34:29 +0200

    refactor: use uid for search events (#2703)

--- packages/renderer-process/src/parts/ViewletSearch/ViewletSearchFunctions.js
+++ packages/renderer-process/src/parts/ViewletSearch/ViewletSearchFunctions.js
@@ -1,65 +1,16 @@
-import * as RendererWorker from '../RendererWorker/RendererWorker.js'
+import * as ForwardViewletCommand from '../ForwardViewletCommand/ForwardViewletCommand.js'
 
-/**
- *
- * @param {string} value
- */
-export const handleInput = (value) => {
-  RendererWorker.send('Search.handleInput', value)
-}
-
-/**
- *
- * @param {number} index
- */
-export const handleClick = (index) => {
-  RendererWorker.send('Search.handleClick', index)
-}
-
-export const handleContextMenu = (button, x, y) => {
-  RendererWorker.send('Search.handleContextMenu', button, x, y)
-}
-
-export const handleScrollBarMove = (y) => {
-  RendererWorker.send('Search.handleScrollBarMove', y)
-}
-
-export const handleScrollBarClick = (y) => {
-  RendererWorker.send('Search.handleScrollBarClick', y)
-}
-
-export const handleWheel = (deltaMode, deltaY) => {
-  RendererWorker.send('Search.handleWheel', deltaMode, deltaY)
-}
-
-export const toggleReplace = () => {
-  RendererWorker.send('Search.handleToggleButtonClick')
-}
-
-export const toggleMatchCase = () => {
-  RendererWorker.send('Search.toggleMatchCase')
-}
-
-export const toggleMatchWholeWord = () => {
-  RendererWorker.send('Search.toggleMatchWholeWord')
-}
-
-export const replaceAll = () => {
-  RendererWorker.send('Search.replaceAll')
-}
-
-export const toggleUseRegularExpression = () => {
-  RendererWorker.send('Search.toggleUseRegularExpression')
-}
-
-export const handleReplaceInput = (value) => {
-  RendererWorker.send('Search.handleReplaceInput', value)
-}
-
-export const handleListFocus = () => {
-  RendererWorker.send('Search.handleListFocus')
-}
-
-export const handleListBlur = () => {
-  RendererWorker.send('Search.handleListBlur')
-}
+export const handleInput = ForwardViewletCommand.forwardViewletCommand('handleInput')
+export const handleClick = ForwardViewletCommand.forwardViewletCommand('handleClick')
+export const handleContextMenu = ForwardViewletCommand.forwardViewletCommand('handleContextMenu')
+export const handleScrollBarMove = ForwardViewletCommand.forwardViewletCommand('handleScrollBarMove')
+export const handleScrollBarClick = ForwardViewletCommand.forwardViewletCommand('handleScrollBarClick')
+export const handleWheel = ForwardViewletCommand.forwardViewletCommand('handleWheel')
+export const toggleReplace = ForwardViewletCommand.forwardViewletCommand('toggleReplace')
+export const toggleMatchCase = ForwardViewletCommand.forwardViewletCommand('toggleMatchCase')
+export const toggleMatchWholeWord = ForwardViewletCommand.forwardViewletCommand('toggleMatchWholeWord')
+export const replaceAll = ForwardViewletCommand.forwardViewletCommand('replaceAll')
+export const toggleUseRegularExpression = ForwardViewletCommand.forwardViewletCommand('toggleUseRegularExpression')
+export const handleReplaceInput = ForwardViewletCommand.forwardViewletCommand('handleReplaceInput')
+export const handleListFocus = ForwardViewletCommand.forwardViewletCommand('handleListFocus')
+export const handleListBlur = ForwardViewletCommand.forwardViewletCommand('handleListBlur')
