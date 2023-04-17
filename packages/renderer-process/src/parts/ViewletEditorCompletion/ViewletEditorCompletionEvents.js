9ba168e7a14b43c25c4e9074d0390e6037138c8b
Le Vivilet <levivlet@pm.me>
2023-04-17 16:38:52 +0200

    refactor: use uid for completion events

--- packages/renderer-process/src/parts/ViewletEditorCompletion/ViewletEditorCompletionEvents.js
+++ packages/renderer-process/src/parts/ViewletEditorCompletion/ViewletEditorCompletionEvents.js
@@ -1,3 +1,4 @@
+import * as ComponentUid from '../ComponentUid/ComponentUid.js'
 import * as DomEventOptions from '../DomEventOptions/DomEventOptions.js'
 import * as DomEventType from '../DomEventType/DomEventType.js'
 import * as Event from '../Event/Event.js'
@@ -6,17 +7,20 @@ import * as ViewletEditorCompletionFunctions from './ViewletEditorCompletionFunc
 export const handleMousedown = (event) => {
   Event.preventDefault(event)
   const { clientX, clientY } = event
-  ViewletEditorCompletionFunctions.handleClickAt(clientX, clientY)
+  const uid = ComponentUid.fromEvent(event)
+  ViewletEditorCompletionFunctions.handleClickAt(uid, clientX, clientY)
 }
 
 export const handleWheel = (event) => {
   const { deltaY, deltaMode } = event
-  ViewletEditorCompletionFunctions.handleWheel(deltaMode, deltaY)
+  const uid = ComponentUid.fromEvent(event)
+  ViewletEditorCompletionFunctions.handleWheel(uid, deltaMode, deltaY)
 }
 
 export const handleScrollBarThumbPointerMove = (event) => {
   const { clientY } = event
-  ViewletEditorCompletionFunctions.handleScrollBarThumbPointerMove(clientY)
+  const uid = ComponentUid.fromEvent(event)
+  ViewletEditorCompletionFunctions.handleScrollBarThumbPointerMove(uid, clientY)
 }
 
 const handlePointerCaptureLost = (event) => {
@@ -30,5 +34,6 @@ export const handleScrollBarPointerDown = (event) => {
   target.setPointerCapture(pointerId)
   target.addEventListener(DomEventType.PointerMove, handleScrollBarThumbPointerMove, DomEventOptions.Active)
   target.addEventListener(DomEventType.LostPointerCapture, handlePointerCaptureLost)
-  ViewletEditorCompletionFunctions.handleScrollBarClick(clientY)
+  const uid = ComponentUid.fromEvent(event)
+  ViewletEditorCompletionFunctions.handleScrollBarClick(uid, clientY)
 }
