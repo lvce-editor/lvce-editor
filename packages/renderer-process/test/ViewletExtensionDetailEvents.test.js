01a47e7fdba9613d1af7d0d2ec9d422ef5da76f6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-17 16:40:20 +0200

    refactor: use uid for extension detail events (#2710)

--- packages/renderer-process/test/ViewletExtensionDetailEvents.test.js
+++ packages/renderer-process/test/ViewletExtensionDetailEvents.test.js
@@ -2,26 +2,29 @@
  * @jest-environment jsdom
  */
 import { jest } from '@jest/globals'
+import * as ComponentUid from '../src/parts/ComponentUid/ComponentUid.js'
 
 beforeEach(() => {
   jest.resetAllMocks()
 })
 
-jest.unstable_mockModule('../src/parts/RendererWorker/RendererWorker.js', () => {
+jest.unstable_mockModule('../src/parts/ExecuteViewletCommand/ExecuteViewletCommand.js', () => {
   return {
-    send: jest.fn(() => {}),
+    executeViewletCommand: jest.fn(() => {}),
   }
 })
 
+const ExecuteViewletCommand = await import('../src/parts/ExecuteViewletCommand/ExecuteViewletCommand.js')
 const ViewletExtensionDetail = await import('../src/parts/ViewletExtensionDetail/ViewletExtensionDetail.js')
-const RendererWorker = await import('../src/parts/RendererWorker/RendererWorker.js')
 
 test('event - image does not load', () => {
   const state = ViewletExtensionDetail.create()
+  const { $Viewlet } = state
+  ComponentUid.set($Viewlet, 1)
   ViewletExtensionDetail.attachEvents(state)
   const { $ExtensionDetailIcon } = state
   const event = new ErrorEvent('error')
   $ExtensionDetailIcon.dispatchEvent(event)
-  expect(RendererWorker.send).toHaveBeenCalledTimes(1)
-  expect(RendererWorker.send).toHaveBeenCalledWith('ExtensionDetail.handleIconError')
+  expect(ExecuteViewletCommand.executeViewletCommand).toHaveBeenCalledTimes(1)
+  expect(ExecuteViewletCommand.executeViewletCommand).toHaveBeenCalledWith(1, 'handleIconError')
 })
