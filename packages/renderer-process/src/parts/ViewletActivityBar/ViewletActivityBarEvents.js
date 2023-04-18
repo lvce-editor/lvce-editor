07922420efaa9d58ab502f4039c8c3e07a705fc7
Le Vivilet <levivlet@pm.me>
2023-04-18 21:18:06 +0200

    refactor: move activity bar click logic to renderer worker

--- packages/renderer-process/src/parts/ViewletActivityBar/ViewletActivityBarEvents.js
+++ packages/renderer-process/src/parts/ViewletActivityBar/ViewletActivityBarEvents.js
@@ -1,7 +1,6 @@
 import * as ComponentUid from '../ComponentUid/ComponentUid.js'
 import * as Event from '../Event/Event.js'
 import * as Focus from '../Focus/Focus.js'
-import * as MouseEventTypes from '../MouseEventType/MouseEventType.js'
 import * as ViewletActivityBarFunctions from './ViewletActivityBarFunctions.js'
 
 const get$ItemFromEvent = (event) => {
@@ -25,9 +24,6 @@ const getNodeIndex = ($Node) => {
 
 export const handleMousedown = (event) => {
   const { button, clientX, clientY } = event
-  if (button !== MouseEventTypes.LeftClick) {
-    return
-  }
   const $Item = get$ItemFromEvent(event)
   if (!$Item) {
     return
@@ -36,7 +32,7 @@ export const handleMousedown = (event) => {
   Event.preventDefault(event)
   Event.stopPropagation(event)
   const index = getNodeIndex($Item)
-  ViewletActivityBarFunctions.handleClick(uid, index, clientX, clientY)
+  ViewletActivityBarFunctions.handleClick(uid, button, index, clientX, clientY)
 }
 
 export const handleContextMenu = (event) => {
