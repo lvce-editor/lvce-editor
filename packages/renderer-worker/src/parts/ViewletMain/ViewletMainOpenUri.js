b908c4fe97b1e12263f5c611678e19367b078db1
Le Vivilet <levivlet@pm.me>
2023-04-18 17:53:45 +0200

    refactor: clean up duplicate code

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMainOpenUri.js
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainOpenUri.js
@@ -1,33 +1,21 @@
 import * as Assert from '../Assert/Assert.js'
 import * as Id from '../Id/Id.js'
+import * as PathDisplay from '../PathDisplay/PathDisplay.js'
 import * as RendererProcess from '../RendererProcess/RendererProcess.js'
 import * as ViewletManager from '../ViewletManager/ViewletManager.js'
 import * as ViewletMap from '../ViewletMap/ViewletMap.js'
 import * as ViewletModule from '../ViewletModule/ViewletModule.js'
 import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
 import * as ViewletStates from '../ViewletStates/ViewletStates.js'
-import * as Workspace from '../Workspace/Workspace.js'
-
-const TAB_HEIGHT = 35
-
-const getTabTitle = (uri) => {
-  const homeDir = Workspace.getHomeDir()
-  // TODO tree shake this out in web
-  if (homeDir && uri.startsWith(homeDir)) {
-    return `~${uri.slice(homeDir.length)}`
-  }
-  return uri
-}
 
 export const openUri = async (state, uri, focus = true, options = {}) => {
   Assert.object(state)
   Assert.string(uri)
   const x = state.x
-  const y = state.y + TAB_HEIGHT
+  const y = state.y + state.tabHeight
   const width = state.width
-  const height = state.height - TAB_HEIGHT
+  const height = state.height - state.tabHeight
   const moduleId = ViewletMap.getModuleId(uri)
-
   for (const editor of state.editors) {
     if (editor.uri === uri) {
       console.log('found existing editor')
@@ -53,8 +41,8 @@ export const openUri = async (state, uri, focus = true, options = {}) => {
   const oldActiveIndex = state.activeIndex
   state.editors.push({ uri, uid: instanceUid })
   state.activeIndex = state.editors.length - 1
-  const tabLabel = Workspace.pathBaseName(uri)
-  const tabTitle = getTabTitle(uri)
+  const tabLabel = PathDisplay.getLabel(uri)
+  const tabTitle = PathDisplay.getTitle(uri)
   await RendererProcess.invoke(
     /* Viewlet.send */ 'Viewlet.send',
     /* id */ state.uid,
