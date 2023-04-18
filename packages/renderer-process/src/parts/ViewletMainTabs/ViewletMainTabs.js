6f527055ed31eaff288ec80e5cc70aab165464fb
Le Vivilet <levivlet@pm.me>
2023-04-18 12:57:47 +0200

    refactor: move tab event listeners into separate function

--- packages/renderer-process/src/parts/ViewletMainTabs/ViewletMainTabs.js
+++ packages/renderer-process/src/parts/ViewletMainTabs/ViewletMainTabs.js
@@ -1,18 +1,22 @@
-import * as ViewletMainTabEvents from './ViewletMainTabEvents.js'
-import * as Tab from '../Tab/Tab.js'
 import * as AriaRoles from '../AriaRoles/AriaRoles.js'
+import * as Tab from '../Tab/Tab.js'
+import * as ViewletMainTabEvents from './ViewletMainTabEvents.js'
 
 export const create = () => {
   const $MainTabs = document.createElement('div')
   $MainTabs.className = 'Viewlet MainTabs'
   $MainTabs.role = AriaRoles.TabList
-  $MainTabs.onmousedown = ViewletMainTabEvents.handleTabsMouseDown
-  $MainTabs.oncontextmenu = ViewletMainTabEvents.handleTabsContextMenu
-  $MainTabs.ondragstart = ViewletMainTabEvents.handleDragStart
   // TODO race condition: what if tab has already been closed?
   return {
     $Viewlet: $MainTabs,
   }
+}
+
+export const attachEvents = (state) => {
+  const { $MainTabs } = state
+  $MainTabs.onmousedown = ViewletMainTabEvents.handleTabsMouseDown
+  $MainTabs.oncontextmenu = ViewletMainTabEvents.handleTabsContextMenu
+  $MainTabs.ondragstart = ViewletMainTabEvents.handleDragStart
 }
 
 export const setTabs = (state, tabs) => {
