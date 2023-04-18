cf5b043f92b637f50a7f3c34f8e14b21061e5cf4
Le Vivilet <levivlet@pm.me>
2023-04-18 14:11:43 +0200

    fix: path display for empty strings

--- packages/renderer-worker/src/parts/PathDisplay/PathDisplay.js
+++ packages/renderer-worker/src/parts/PathDisplay/PathDisplay.js
@@ -1,6 +1,9 @@
 import * as Workspace from '../Workspace/Workspace.js'
 
 export const getTitle = (uri) => {
+  if (!uri) {
+    return ''
+  }
   const homeDir = Workspace.getHomeDir()
   // TODO tree shake this out in web
   if (homeDir && uri.startsWith(homeDir)) {
