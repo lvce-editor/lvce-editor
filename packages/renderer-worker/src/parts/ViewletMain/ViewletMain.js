8960629c54d40a21b3c86291e28dbbb5c285d067
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-18 14:30:01 +0200

    refactor: move openUri function into separate file (#2729)

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMain.js
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMain.js
@@ -21,6 +21,7 @@ import * as ViewletModule from '../ViewletModule/ViewletModule.js'
 import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
 import * as ViewletStates from '../ViewletStates/ViewletStates.js'
 import * as Workspace from '../Workspace/Workspace.js'
+import { openUri } from './ViewletMainOpenUri.js'
 
 const COLUMN_WIDTH = 9 // TODO compute this automatically once
 
@@ -231,78 +232,6 @@ export const contentLoaded = async (state) => {
   commands.push(['Viewlet.appendViewlet', state.uid, childUid])
   // }
   return commands
-}
-
-export const openUri = async (state, uri, focus = true, options = {}) => {
-  Assert.object(state)
-  Assert.string(uri)
-  const x = state.x
-  const y = state.y + state.tabHeight
-  const width = state.width
-  const height = state.height - state.tabHeight
-  const moduleId = ViewletMap.getModuleId(uri)
-
-  for (const editor of state.editors) {
-    if (editor.uri === uri) {
-      console.log('found existing editor')
-      const childUid = Id.create()
-      // TODO if the editor is already open, nothing needs to be done
-      const instance = ViewletManager.create(ViewletModule.load, moduleId, ViewletModuleId.Main, uri, x, y, width, height)
-      instance.show = false
-      instance.setBounds = false
-      instance.uid = childUid
-      // @ts-ignore
-      const commands = await ViewletManager.load(instance, focus, false, options)
-      commands.push(['Viewlet.appendViewlet', state.uid, childUid])
-      return {
-        newState: state,
-        commands,
-      }
-    }
-  }
-
-  const instanceUid = Id.create()
-  const instance = ViewletManager.create(ViewletModule.load, moduleId, ViewletModuleId.Main, uri, x, y, width, height)
-  instance.uid = instanceUid
-  const oldActiveIndex = state.activeIndex
-  state.editors.push({ uri, uid: instanceUid })
-  state.activeIndex = state.editors.length - 1
-  const tabLabel = PathDisplay.getLabel(uri)
-  const tabTitle = PathDisplay.getTitle(uri)
-  await RendererProcess.invoke(
-    /* Viewlet.send */ 'Viewlet.send',
-    /* id */ state.uid,
-    /* method */ 'openViewlet',
-    /* tabLabel */ tabLabel,
-    /* tabTitle */ tabTitle,
-    /* oldActiveIndex */ oldActiveIndex
-  )
-  // @ts-ignore
-  instance.show = false
-  instance.setBounds = false
-  // @ts-ignore
-  const commands = await ViewletManager.load(instance, focus)
-  // if (commands[0].includes(ViewletModuleId.Error)) {
-  //   commands.push(['Viewlet.appendViewlet', state.uid, ViewletModuleId.Error])
-  // } else {
-  commands.push(['Viewlet.appendViewlet', state.uid, instanceUid || moduleId])
-  if (focus) {
-    commands.push(['Viewlet.focus', instanceUid])
-  }
-  // }
-  if (!ViewletStates.hasInstance(moduleId)) {
-    return {
-      newState: state,
-      commands,
-    }
-  }
-  const actualUri = ViewletStates.getState(moduleId).uri
-  const index = state.editors.findIndex((editor) => editor.uid === instanceUid)
-  state.editors[index].uri = actualUri
-  return {
-    newState: state,
-    commands,
-  }
 }
 
 export const openBackgroundTab = async (state, initialUri, props) => {
