f4312f543479da3b654841b7ef645b91629f5c90
Le Vivilet <levivlet@pm.me>
2023-04-19 23:00:15 +0200

    fix: editor resizing

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMain.js
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMain.js
@@ -672,19 +672,21 @@ export const resize = (state, dimensions) => {
   const x = dimensions.x
   const y = dimensions.y + state.tabHeight
   const width = dimensions.width
-  const height = dimensions.height - state.tabHeight
+  const contentHeight = dimensions.height - state.tabHeight
   const childDimensions = {
     x,
     y,
     width,
-    height,
+    height: contentHeight,
   }
   const editor = editors[0]
   let commands = []
+  // console.log(editor)
   if (editor) {
-    const editorUid = editor.uid
+    const editorUid = getUid(editor)
     Assert.number(editorUid)
     commands = Viewlet.resize(editorUid, childDimensions)
+    commands.push(['Viewlet.setBounds', editorUid, x, state.tabHeight, width, contentHeight])
   }
   return {
     newState: {
