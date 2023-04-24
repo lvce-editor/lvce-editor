b6fb499638a111bd596332fb4649cd09f8ff3165
Le Vivilet <levivlet@pm.me>
2023-04-24 21:24:00 +0200

    fix icon theme e2e test

--- packages/extension-host-worker-tests/src/sample.icon-theme.js
+++ packages/extension-host-worker-tests/src/sample.icon-theme.js
@@ -4,15 +4,13 @@ test('sample.icon-theme', async () => {
   await FileSystem.writeFile(`${tmpDir}/test.xyz`, 'test')
   await FileSystem.mkdir(`${tmpDir}/test-folder`)
   await Workspace.setPath(tmpDir)
-  await Extension.addWebExtension(
-    new URL('../fixtures/sample.icon-theme', import.meta.url).toString()
-  )
+  await Extension.addWebExtension(new URL('../fixtures/sample.icon-theme', import.meta.url).toString())
 
   // act
   await IconTheme.setIconTheme('test-icon-theme')
 
   // assert
-  const iconFile = Locator('.TreeItem[aria-label="test.xyz"] i')
+  const iconFile = Locator('.TreeItem[aria-label="test.xyz"] .FileIcon')
   await expect(iconFile).toHaveClass('FileIcon_file')
   const baseUrl = BaseUrl.getBaseUrl()
   await expect(iconFile).toHaveCSS(
