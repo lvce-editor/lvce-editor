f2979ccfdee61a28e5015ac9adf4701539fc747d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-04-24 18:20:22 +0200

    fix: log info message when dialog cannot be shown because there is no… (#2804)

--- packages/main-process/src/parts/ElectronDialog/ElectronDialog.js
+++ packages/main-process/src/parts/ElectronDialog/ElectronDialog.js
@@ -1,6 +1,7 @@
 const Assert = require('../Assert/Assert.js')
 const Electron = require('electron')
 const ElectronMessageBoxType = require('../ElectronMessageBoxType/ElectronMessageBoxType.js')
+const Logger = require('../Logger/Logger.js')
 const Platform = require('../Platform/Platform.js')
 const Window = require('../ElectronWindow/ElectronWindow.js')
 
@@ -34,6 +35,7 @@ exports.showMessageBox = async (message, buttons, type = ElectronMessageBoxType.
   Assert.array(buttons)
   const focusedWindow = Window.getFocusedWindow()
   if (!focusedWindow) {
+    Logger.info(`[main-process] cannot show dialog message because there is no focused window`)
     return
   }
   if (message.message) {
