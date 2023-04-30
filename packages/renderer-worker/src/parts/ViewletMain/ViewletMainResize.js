74806fabc54e08646d69a49276431ac3e3ffc01c
Le Vivilet <levivlet@pm.me>
2023-04-30 21:46:45 +0200

    fix: resizing focused editor

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMainResize.js
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainResize.js
@@ -2,26 +2,28 @@ import * as Assert from '../Assert/Assert.js'
 import * as Viewlet from '../Viewlet/Viewlet.js'
 
 export const resize = (state, dimensions) => {
-  const { editors, tabsUid } = state
+  const { editors, tabsUid, activeIndex, tabHeight } = state
   const x = dimensions.x
-  const contentY = dimensions.y + state.tabHeight
+  const contentY = dimensions.y + tabHeight
   const width = dimensions.width
-  const contentHeight = dimensions.height - state.tabHeight
+  const contentHeight = dimensions.height - tabHeight
   const childDimensions = {
     x,
     y: contentY,
     width,
     height: contentHeight,
   }
-  const editor = editors[0]
+  const editor = editors[activeIndex]
   const commands = []
   if (editor) {
     const editorUid = editor.uid
     Assert.number(editorUid)
-    commands.push(...Viewlet.resize(editorUid, childDimensions))
+    const resizeCommands = Viewlet.resize(editorUid, childDimensions)
+    commands.push(...resizeCommands)
+    commands.push(['Viewlet.setBounds', editorUid, dimensions.x, tabHeight, dimensions.width, contentHeight])
   }
   if (tabsUid !== -1) {
-    commands.push(['Viewlet.setBounds', tabsUid, dimensions.x, 0, dimensions.width, dimensions.tabHeight])
+    commands.push(['Viewlet.setBounds', tabsUid, dimensions.x, 0, dimensions.width, tabHeight])
   }
   return {
     newState: {
