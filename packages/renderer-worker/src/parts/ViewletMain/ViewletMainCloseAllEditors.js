49dfa6e61cae2284b099ca5e782206dac0feced3
Le Vivilet <levivlet@pm.me>
2023-05-01 00:35:03 +0200

    fix: closing tabs

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMainCloseAllEditors.js
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainCloseAllEditors.js
@@ -4,25 +4,20 @@ const getUid = (editor) => {
   return editor.uid
 }
 
-const getUids = (editors) => {
-  return editors.map(getUid)
+const getEditors = (group) => {
+  return group.editors
 }
 
 export const closeAllEditors = (state) => {
-  const ids = getUids(state.editors)
-  const uid = state.uid
+  const { groups, uid } = state
+  const editors = groups.flatMap(getEditors)
+  const ids = editors.map(getUid)
   const commands = [['Viewlet.send', uid, 'dispose'], ...ids.flatMap(Viewlet.disposeFunctional)]
-  const newEditors = []
-  const newFocusedIndex = -1
-  const newSelectedIndex = -1
-  const newTabsUid = -1
   return {
     newState: {
       ...state,
-      editors: newEditors,
-      focusedIndex: newFocusedIndex,
-      selectedIndex: newSelectedIndex,
-      tabsUid: newTabsUid,
+      groups: [],
+      activeGroupIndex: -1,
     },
     commands,
   }
