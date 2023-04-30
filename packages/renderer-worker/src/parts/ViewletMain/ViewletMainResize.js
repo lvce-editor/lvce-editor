0643f1ac5ba8259c9ceb12890311d4116b56248e
Le Vivilet <levivlet@pm.me>
2023-05-01 00:36:34 +0200

    fix: resizing

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMainResize.js
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainResize.js
@@ -2,7 +2,7 @@ import * as Assert from '../Assert/Assert.js'
 import * as Viewlet from '../Viewlet/Viewlet.js'
 
 export const resize = (state, dimensions) => {
-  const { editors, tabsUid, activeIndex, tabHeight } = state
+  const { groups, tabHeight } = state
   const x = dimensions.x
   const contentY = dimensions.y + tabHeight
   const width = dimensions.width
@@ -13,17 +13,20 @@ export const resize = (state, dimensions) => {
     width,
     height: contentHeight,
   }
-  const editor = editors[activeIndex]
   const commands = []
-  if (editor) {
-    const editorUid = editor.uid
-    Assert.number(editorUid)
-    const resizeCommands = Viewlet.resize(editorUid, childDimensions)
-    commands.push(...resizeCommands)
-    commands.push(['Viewlet.setBounds', editorUid, dimensions.x, tabHeight, dimensions.width, contentHeight])
-  }
-  if (tabsUid !== -1) {
-    commands.push(['Viewlet.setBounds', tabsUid, dimensions.x, 0, dimensions.width, tabHeight])
+  for (const group of groups) {
+    const { activeIndex, editors, tabsUid } = group
+    const editor = editors[activeIndex]
+    if (editor) {
+      const editorUid = editor.uid
+      Assert.number(editorUid)
+      const resizeCommands = Viewlet.resize(editorUid, childDimensions)
+      commands.push(...resizeCommands)
+      commands.push(['Viewlet.setBounds', editorUid, dimensions.x, tabHeight, dimensions.width, contentHeight])
+    }
+    if (tabsUid !== -1) {
+      commands.push(['Viewlet.setBounds', tabsUid, dimensions.x, 0, dimensions.width, tabHeight])
+    }
   }
   return {
     newState: {
