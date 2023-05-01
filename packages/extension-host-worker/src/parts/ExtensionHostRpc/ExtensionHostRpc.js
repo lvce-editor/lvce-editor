59f769816136890c73375d4e7fdc37debc4c9183
Le Vivilet <levivlet@pm.me>
2023-05-01 21:13:25 +0200

    try to create rpc connection

--- packages/extension-host-worker/src/parts/ExtensionHostRpc/ExtensionHostRpc.js
+++ packages/extension-host-worker/src/parts/ExtensionHostRpc/ExtensionHostRpc.js
@@ -5,13 +5,19 @@ import * as RpcParent from '../RpcParent/RpcParent.js'
 import * as RpcParentType from '../RpcParentType/RpcParentType.js'
 import { VError } from '../VError/VError.js'
 
+const getExtensionHostHelperProcessUrl = () => {
+  return new URL('../../../../extension-host-helper-process/src/extensionHostHelperProcessMain.js', import.meta.url).toString()
+}
+
 export const createRpc = async ({ url, name }) => {
   try {
     Assert.string(url)
     Assert.string(name)
+    const helperProcessUrl = getExtensionHostHelperProcessUrl()
+    console.log({ helperProcessUrl })
     const ipc = await IpcParent.create({
       method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
-      url,
+      url: helperProcessUrl,
       name,
     })
     const rpc = await RpcParent.create({
