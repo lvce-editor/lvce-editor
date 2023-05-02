62fe4dbf3656da800f0d8f4817ff65366beb790d
Le Vivilet <levivlet@pm.me>
2023-05-02 23:29:10 +0200

    fix: set correct headers for worker

--- packages/extension-host-helper-process/src/parts/GetResponse/GetResponse.js
+++ packages/extension-host-helper-process/src/parts/GetResponse/GetResponse.js
@@ -1,4 +1,3 @@
-import * as GetErrorResponse from '../GetErrorResponse/GetErrorResponse.js'
 import * as GetSuccessResponse from '../GetSuccessResponse/GetSuccessResponse.js'
 
 export const getResponse = async (message, execute) => {
@@ -6,6 +5,7 @@ export const getResponse = async (message, execute) => {
     const result = await execute(message.method, ...message.params)
     return GetSuccessResponse.getSuccessResponse(message, result)
   } catch (error) {
+    const GetErrorResponse = await import('../GetErrorResponse/GetErrorResponse.js')
     return GetErrorResponse.getErrorResponse(message, error)
   }
 }
