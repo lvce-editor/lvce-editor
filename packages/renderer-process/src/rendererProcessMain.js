bebb10b5b2a204d8a02c74397960db98cd45b17b
Le Vivilet <levivlet@pm.me>
2023-05-02 23:30:01 +0200

    refactor: add csp violation event listener to renderer process

--- packages/renderer-process/src/rendererProcessMain.js
+++ packages/renderer-process/src/rendererProcessMain.js
@@ -1,10 +1,11 @@
 import * as Command from './parts/Command/Command.js'
 import * as ErrorHandling from './parts/ErrorHandling/ErrorHandling.js'
+import * as Event from './parts/Event/Event.js'
+import * as HandleContentSecurityPolicyViolation from './parts/HandleContentSecurityPolicyViolation/HandleContentSecurityPolicyViolation.js'
 import * as Module from './parts/Module/Module.js'
 import * as Platform from './parts/Platform/Platform.js'
 import * as PlatformType from './parts/PlatformType/PlatformType.js'
 import * as RendererWorker from './parts/RendererWorker/RendererWorker.js'
-import * as Event from './parts/Event/Event.js'
 
 const handleBeforeInstallPrompt = (event) => {
   Event.preventDefault(event)
@@ -13,6 +14,9 @@ const handleBeforeInstallPrompt = (event) => {
 const main = async () => {
   onerror = ErrorHandling.handleError
   onunhandledrejection = ErrorHandling.handleUnhandledRejection
+  if ('SecurityPolicyViolationEvent' in self) {
+    self.addEventListener('securitypolicyviolation', HandleContentSecurityPolicyViolation.handleContentSecurityPolicyViolation)
+  }
   Command.setLoad(Module.load)
   if (Platform.platform === PlatformType.Web) {
     // disable prompt to download app as pwa
