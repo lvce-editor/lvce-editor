c62b323d9f6a32b89b9be6393bec3417a622283d
Le Vivilet <levivlet@pm.me>
2023-05-05 00:46:48 +0200

    refactor: move keybindings virtual dom creation into separate file

---
+++ packages/renderer-worker/src/parts/GetVisibleKeyBindings/GetVisibleKeyBindings.js
@@ -0,0 +1,17 @@
+export const getVisibleKeyBindings = (filteredKeyBindings, minLineY, maxLineY, selectedIndex) => {
+  const visibleKeyBindings = []
+  const slicedKeyBindings = filteredKeyBindings.slice(minLineY, maxLineY)
+  for (let i = 0; i < slicedKeyBindings.length; i++) {
+    const slicedKeyBinding = slicedKeyBindings[i]
+    visibleKeyBindings.push({
+      rowIndex: minLineY + i + 2,
+      isCtrl: slicedKeyBinding.isCtrl,
+      isShift: slicedKeyBinding.isShift,
+      key: slicedKeyBinding.key,
+      when: slicedKeyBinding.when,
+      command: slicedKeyBinding.command,
+      selected: i === selectedIndex,
+    })
+  }
+  return visibleKeyBindings
+}
