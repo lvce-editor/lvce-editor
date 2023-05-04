d79d67779ea4cae7b9bf36852e426290269e39e9
Le Vivilet <levivlet@pm.me>
2023-05-05 00:44:15 +0200

    refactor: improve dom diffing

--- packages/renderer-worker/test/DiffDom.test.js
+++ packages/renderer-worker/test/DiffDom.test.js
@@ -1,6 +1,6 @@
 import * as DiffDom from '../src/parts/DiffDom/DiffDom.js'
 import * as DiffDomType from '../src/parts/DiffDomType/DiffDomType.js'
-import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.js'
+import { div, text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.js'
 
 test('diffDom - empty', () => {
   const oldDom = []
@@ -26,6 +26,71 @@ test('diffDom - add a text node', () => {
     {
       type: DiffDomType.Insert,
       nodes: [text('hello world')],
+    },
+  ])
+})
+
+test('diffDom - sub node attribute modified', () => {
+  const oldDom = [
+    div({ className: 'List' }, 2),
+    div({ className: 'ListItems' }, 0),
+    div({ className: 'ScrollBar' }, 1),
+    div({ className: 'ScrollBarThumb', height: 10 }, 0),
+  ]
+  const newDom = [
+    div({ className: 'List' }, 2),
+    div({ className: 'ListItems' }, 0),
+    div({ className: 'ScrollBar' }, 1),
+    div({ className: 'ScrollBarThumb', height: 20 }, 0),
+  ]
+  expect(DiffDom.diffDom(oldDom, newDom)).toEqual([
+    {
+      type: DiffDomType.UpdateProp,
+      index: 3,
+      key: 'height',
+      value: 20,
+    },
+  ])
+})
+
+test('diffDom - sub node removed at end', () => {
+  const oldDom = [
+    div({ className: 'List' }, 2),
+    div({ className: 'ListItems' }, 0),
+    div({ className: 'ScrollBar' }, 1),
+    div({ className: 'ScrollBarThumb', height: 10 }, 0),
+  ]
+  const newDom = [div({ className: 'List' }, 2), div({ className: 'ListItems' }, 0)]
+  expect(DiffDom.diffDom(oldDom, newDom)).toEqual([
+    {
+      type: DiffDomType.Remove,
+      nodes: [2],
+    },
+  ])
+})
+
+test('diffDom - sub node removed at start', () => {
+  const oldDom = [
+    div({ className: 'List' }, 2),
+    div({ className: 'ScrollBar' }, 1),
+    div({ className: 'ScrollBarThumb', height: 10 }, 0),
+    div({ className: 'ListItems' }, 0),
+  ]
+  const newDom = [div({ className: 'List' }, 2), div({ className: 'ListItems' }, 0)]
+  expect(DiffDom.diffDom(oldDom, newDom)).toEqual([
+    {
+      index: 1,
+      key: 'className',
+      type: DiffDomType.UpdateProp,
+      value: 'ListItems',
+    },
+    {
+      type: DiffDomType.Remove,
+      nodes: [2],
+    },
+    {
+      type: DiffDomType.Remove,
+      nodes: [3],
     },
   ])
 })
