5ddb8460c9c0125804e31b5e9cb529e8164a38bd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-05 00:36:19 +0200

    try to implement dom diffing again (wip) (#2852)

--- packages/renderer-worker/test/DiffDom.test.js
+++ packages/renderer-worker/test/DiffDom.test.js
@@ -1,14 +1,30 @@
 import * as DiffDom from '../src/parts/DiffDom/DiffDom.js'
-import { h, text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.js'
+import * as DiffDomType from '../src/parts/DiffDomType/DiffDomType.js'
+import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.js'
 
-test.skip('diffDom - empty', () => {
-  expect(DiffDom.diffDom([], [])).toEqual([])
+test('diffDom - empty', () => {
+  const oldDom = []
+  const newDom = []
+  expect(DiffDom.diffDom(oldDom, newDom)).toEqual([])
 })
 
-test.skip('diffDom - add a text node', () => {
-  expect(DiffDom.diffDom([], [text('hello world')])).toEqual([
+test('diffDom - remove a text node', () => {
+  const oldDom = [text('hello world')]
+  const newDom = []
+  expect(DiffDom.diffDom(oldDom, newDom)).toEqual([
     {
-      type: 'add',
+      type: DiffDomType.Remove,
+      nodes: [0],
+    },
+  ])
+})
+
+test('diffDom - add a text node', () => {
+  const oldDom = []
+  const newDom = [text('hello world')]
+  expect(DiffDom.diffDom(oldDom, newDom)).toEqual([
+    {
+      type: DiffDomType.Insert,
       nodes: [text('hello world')],
     },
   ])
