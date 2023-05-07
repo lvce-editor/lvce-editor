24f84f0e30d7a67cb57207dff44259ff28239e51
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-07 19:46:38 +0200

    Discard changes to packages/extension-host-helper-process/src/parts/IpcChildType/IpcChildType.js

--- packages/extension-host-helper-process/src/parts/IpcChildType/IpcChildType.js
+++ packages/extension-host-helper-process/src/parts/IpcChildType/IpcChildType.js
@@ -1,29 +1,21 @@
+import * as ParseCliArgs from '../ParseCliArgs/ParseCliArgs.js'
+
 export const WebSocket = 1
 export const MessagePort = 2
 export const Parent = 3
 export const ElectronMessagePort = 4
 export const ElectronUtilityProcess = 5
 export const ElectronUtilityProcessMessagePort = 6
-export const ModuleWorker = 7
 
-const getRawIpcTypeNode = async () => {
+const getRawIpcType = () => {
   const { argv } = process
-  const ParseCliArgs = await import('../ParseCliArgs/ParseCliArgs.js')
   const parsedArgs = ParseCliArgs.parseCliArgs(argv.slice(2))
   const ipcType = parsedArgs['ipc-type']
   return ipcType
 }
 
-const getRawIpcType = () => {
-  if (typeof process !== 'undefined') {
-    return getRawIpcTypeNode()
-  }
-  return 'module-worker'
-}
-
-export const Auto = async () => {
-  const ipcType = await getRawIpcType()
-  console.log({ ipcType })
+export const Auto = () => {
+  const ipcType = getRawIpcType()
   switch (ipcType) {
     case 'websocket':
       return WebSocket
@@ -37,8 +29,6 @@ export const Auto = async () => {
       return ElectronUtilityProcess
     case 'electron-utility-process-message-port':
       return ElectronUtilityProcessMessagePort
-    case 'module-worker':
-      return ModuleWorker
     default:
       throw new Error(`[extension-host-helper-process] unknown ipc type ${ipcType}`)
   }
