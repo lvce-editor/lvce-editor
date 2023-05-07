3b06ab4aa052e77a2e08987f90149318d1f2e606
Le Vivilet <levivlet@pm.me>
2023-05-07 23:41:18 +0200

    refactor: simplify sub worker entry point code

--- packages/extension-host-sub-worker/src/extensionHostSubWorkerMain.js
+++ packages/extension-host-sub-worker/src/extensionHostSubWorkerMain.js
@@ -1,33 +1,7 @@
-import * as Command from './parts/Command/Command.js'
-import * as CommandState from './parts/CommandState/CommandState.js'
-import * as GetErrorResponse from './parts/GetErrorResponse/GetErrorResponse.js'
-import * as GetSuccessResponse from './parts/GetSuccessResponse/GetSuccessResponse.js'
-import * as ImportScript from './parts/ImportScript/ImportScript.js'
-import * as IpcChild from './parts/IpcChild/IpcChild.js'
-import * as IpcChildType from './parts/IpcChildType/IpcChildType.js'
-import * as Rpc from './parts/Rpc/Rpc.js'
-import * as WaitForFirstMessage from './parts/WaitForFirstMessage/WaitForFirstMessage.js'
+import * as Listen from './parts/Listen/Listen.js'
 
 const main = async () => {
-  const method = IpcChildType.Auto()
-  const ipc = await IpcChild.listen({ method })
-  const firstMessage = await WaitForFirstMessage.waitForFirstMessage(ipc)
-  let module
-  try {
-    module = await ImportScript.importScript(firstMessage.params[0])
-    if (!module || !module.commandMap) {
-      throw new Error(`Missing export const commandMap`)
-    }
-    const commandMap = module.commandMap
-    CommandState.registerCommands(commandMap)
-    const response = GetSuccessResponse.getSuccessResponse(firstMessage, null)
-    ipc.send(response)
-  } catch (error) {
-    const response = await GetErrorResponse.getErrorResponse(firstMessage, error)
-    ipc.send(response)
-    return
-  }
-  Rpc.listen(ipc, Command.execute)
+  await Listen.listen()
 }
 
 main()
