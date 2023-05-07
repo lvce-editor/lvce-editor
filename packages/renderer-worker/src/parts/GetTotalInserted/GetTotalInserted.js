ab0c379173079beb6c263077162290ba4f3d3cfc
Le Vivilet <levivlet@pm.me>
2023-05-07 12:28:27 +0200

    remove unused code

--- packages/renderer-worker/src/parts/GetTotalInserted/GetTotalInserted.js
+++ packages/renderer-worker/src/parts/GetTotalInserted/GetTotalInserted.js
@@ -1,28 +1,13 @@
-import { div } from '../VirtualDomHelpers/VirtualDomHelpers.js'
-
 export const getTotalInserted = (dom, start, childCount = dom[start].childCount) => {
-  let count = 0
-  // getTotalInserted(dom, i,)
-  let current = 0
   let total = 0
   let pending = 0
   for (let i = start; i < dom.length; i++) {
-    console.log({ childCount })
-    childCount //?
     const element = dom[i]
     total++
     pending += element.childCount
     if (total === pending + 1) {
       break
     }
-    // total += element.childCount
-    // current -= element.childCount
-    // console.log({ current })
-    // if (current === 0) {
-    // break
-    // }
-    // total++
-    // current++
   }
   return total
 }
