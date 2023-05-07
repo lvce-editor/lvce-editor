f79ec4c17f403efc0037a1ad7f1142282665f8cf
Le Vivilet <levivlet@pm.me>
2023-05-07 19:47:29 +0200

    remove console logs

--- packages/extension-host-helper-process/src/parts/GetResponse/GetResponse.js
+++ packages/extension-host-helper-process/src/parts/GetResponse/GetResponse.js
@@ -5,8 +5,6 @@ export const getResponse = async (message, execute) => {
     const result = await execute(message.method, ...message.params)
     return GetSuccessResponse.getSuccessResponse(message, result)
   } catch (error) {
-    console.log({ error })
-    console.log('got err')
     const GetErrorResponse = await import('../GetErrorResponse/GetErrorResponse.js')
     return GetErrorResponse.getErrorResponse(message, error)
   }
