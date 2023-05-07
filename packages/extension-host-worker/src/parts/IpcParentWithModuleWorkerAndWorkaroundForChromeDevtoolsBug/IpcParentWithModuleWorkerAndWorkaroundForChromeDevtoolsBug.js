6f0a4a07bc03d6efc4ae556879cad23eb9efb2f6
Le Vivilet <levivlet@pm.me>
2023-05-07 23:21:22 +0200

    fix: creating module worker ipc in extension host

--- packages/extension-host-worker/src/parts/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.js
+++ packages/extension-host-worker/src/parts/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug/IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug.js
@@ -1,7 +1,10 @@
+import * as Assert from '../Assert/Assert.js'
 import * as RendererWorkerIpcParentType from '../RendererWorkerIpcParentType/RendererWorkerIpcParentType.js'
 import * as Rpc from '../Rpc/Rpc.js'
 
 export const create = async ({ url, name }) => {
+  Assert.string(url)
+  Assert.string(name)
   const port = await Rpc.invoke('IpcParent.create', {
     method: RendererWorkerIpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
     url,
@@ -12,6 +15,36 @@ export const create = async ({ url, name }) => {
 }
 
 export const wrap = (port) => {
-  // TODO
-  return port
+  return {
+    port,
+    /**
+     * @type {any}
+     */
+    handleMessage: undefined,
+    get onmessage() {
+      return this.handleMessage
+    },
+    set onmessage(listener) {
+      if (listener) {
+        this.handleMessage = (event) => {
+          // TODO why are some events not instance of message event?
+          if (event instanceof MessageEvent) {
+            const message = event.data
+            listener(message, event)
+          } else {
+            listener(event)
+          }
+        }
+      } else {
+        this.handleMessage = null
+      }
+      this.port.onmessage = this.handleMessage
+    },
+    send(message) {
+      this.port.postMessage(message)
+    },
+    sendAndTransfer(message, transfer) {
+      this.port.postMessage(message, transfer)
+    },
+  }
 }
