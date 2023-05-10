0114180031be7fe2ebc2035df8fdface86e79934
Le Vivilet <levivlet@pm.me>
2023-05-10 21:20:50 +0200

    fix: horizontal scrolling

--- packages/renderer-process/src/parts/Editor/EditorEvents.js
+++ packages/renderer-process/src/parts/Editor/EditorEvents.js
@@ -206,7 +206,8 @@ export const handleScrollBarVerticalPointerDown = (event) => {
  */
 export const handleScrollBarThumbHorizontalPointerMove = (event) => {
   const { clientX } = event
-  EditorFunctions.handleScrollBarHorizontalMove(clientX)
+  const uid = ComponentUid.fromEvent(event)
+  EditorFunctions.handleScrollBarHorizontalMove(uid, clientX)
 }
 
 /**
