5c36a27f65a4ef9b73661a458929b57e4a6cc371
Le Vivilet <levivlet@pm.me>
2023-05-12 23:45:14 +0200

    forward websocket to terminal process

--- packages/shared-process/src/parts/HandleWebSocketForTerminalProcess/HandleWebSocketForTerminalProcess.js
+++ packages/shared-process/src/parts/HandleWebSocketForTerminalProcess/HandleWebSocketForTerminalProcess.js
@@ -1,8 +1,11 @@
 import * as Command from '../Command/Command.js'
 import * as ErrorCodes from '../ErrorCodes/ErrorCodes.js'
+import * as PtyHost from '../PtyHost/PtyHost.js'
 
 export const handleWebSocket = async (message, handle) => {
-  console.log({ handle })
+  const instance = await PtyHost.getOrCreate()
+  instance.sendAndTransfer({ jsonrpc: '2.0', method: 'handleWebSocket', params: [] }, handle)
+  console.log({ instance })
   // const ipc = await PtyHost.getOrCreate()
   // console.info('[sharedprocess] creating extension ipc')
   // const rpc = await ExtensionHostRpc.create(ipc, handle)
