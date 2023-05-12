cc5de1640d1631294491d4074a3ea2fc092a4757
Le Vivilet <levivlet@pm.me>
2023-05-12 23:07:36 +0200

    refactor: add api to launch shared process as utility process

--- packages/shared-process/src/parts/IpcChildModule/IpcChildModule.js
+++ packages/shared-process/src/parts/IpcChildModule/IpcChildModule.js
@@ -6,6 +6,8 @@ export const getModule = (method) => {
       return import('../IpcChildWithNodeForkedProcess/IpcChildWithNodeForkedProcess.js')
     case IpcChildType.NodeWorker:
       return import('../IpcChildWithNodeWorker/IpcChildWithNodeWorker.js')
+    case IpcChildType.ElectronUtilityProcess:
+      return import('../IpcChildWithElectronUtilityProcess/IpcChildWithElectronUtilityProcess.js')
     default:
       throw new Error('unexpected ipc type')
   }
