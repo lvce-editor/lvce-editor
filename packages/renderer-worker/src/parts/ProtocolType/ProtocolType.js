37c7fbd32425cf50a2a66ac00d3359ca1c2f030a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-12 23:17:49 +0200

    refactor: add protocol type enum to renderer worker (#2965)

---
+++ packages/renderer-worker/src/parts/ProtocolType/ProtocolType.js
@@ -0,0 +1,4 @@
+export const ExtensionHost = 'lvce.extension-host'
+export const ExtensionHostHelperProcess = 'lvce.extension-host-helper-process'
+export const SharedProcess = 'lvce.shared-process'
+export const TerminalProcess = 'lvce.terminal-process'
