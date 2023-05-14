5225ac1a85a64d6c91318aad094009a2a308244d
Le Vivilet <levivlet@pm.me>
2023-05-14 14:02:45 +0200

    fix: improve determining stack trace from child process stderr

--- packages/main-process/test/GetModulesErrorStack.test.js
+++ packages/main-process/test/GetModulesErrorStack.test.js
@@ -1,6 +1,6 @@
 const GetModulesErrorStack = require('../src/parts/GetModulesErrorStack/GetModulesErrorStack.js')
 
-test('getModulesErrorStack', () => {
+test('getModulesErrorStack - esm error', () => {
   const stderr = `node:internal/modules/cjs/loader:1293
       throw err;
       ^
@@ -25,5 +25,29 @@ Node.js v18.14.0
     '    at node:electron/js2c/utility_init:2:5961',
     '    at node:electron/js2c/utility_init:2:5965',
     '    at f._load (node:electron/js2c/asar_bundle:2:13330) {',
+  ])
+})
+
+test('getModulesErrorStack - export error', () => {
+  const stderr = `file:///test/packages/pty-host/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js:5
+import { MessagePortMain } from 'electron'
+          ^^^^^^^^^^^^^^^
+SyntaxError: Named export 'MessagePortMain' not found. The requested module 'electron' is a CommonJS module, which may not support all module.exports as named exports.
+CommonJS modules can always be imported via the default export, for example using:
+
+import pkg from 'electron';
+const { MessagePortMain } = pkg;
+
+    at ModuleJob._instantiate (node:internal/modules/esm/module_job:124:21)
+    at async ModuleJob.run (node:internal/modules/esm/module_job:190:5)
+
+Node.js v18.14.0
+`
+  const stack = GetModulesErrorStack.getModulesErrorStack(stderr)
+  expect(stack).toEqual([
+    `SyntaxError: Named export 'MessagePortMain' not found. The requested module 'electron' is a CommonJS module, which may not support all module.exports as named exports. CommonJS modules can always be imported via the default export, for example using:  import pkg from 'electron'; const { MessagePortMain } = pkg;`,
+    '    at file:///test/packages/pty-host/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js:5',
+    '    at ModuleJob._instantiate (node:internal/modules/esm/module_job:124:21)',
+    '    at async ModuleJob.run (node:internal/modules/esm/module_job:190:5)',
   ])
 })
