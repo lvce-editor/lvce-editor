a89dcef72bf9bcfbb13e7ad1580555c82e7d6db3
Le Vivilet <levivlet@pm.me>
2023-05-16 17:00:23 +0200

    feature: add node message port ipc to terminal process

---
+++ packages/pty-host/src/parts/IpcChildWithNodeMessagePort/IpcChildWithNodeMessagePort.js
@@ -0,0 +1,27 @@
+import { IpcError } from '../IpcError/IpcError.js'
+import * as IsMessagePort from '../isMessagePort/IsMessagePort.js'
+
+export const listen = async ({ messagePort }) => {
+  if (!IsMessagePort.isMessagePort(messagePort)) {
+    throw new IpcError(`port must be of type MessagePort`)
+  }
+  return messagePort
+}
+
+export const wrap = (port) => {
+  return {
+    port,
+    on(event, listener) {
+      this.port.on(event, listener)
+    },
+    off(event, listener) {
+      this.port.off(event, listener)
+    },
+    send(message) {
+      this.port.postMessage(message)
+    },
+    dispose() {
+      this.port.close()
+    },
+  }
+}
