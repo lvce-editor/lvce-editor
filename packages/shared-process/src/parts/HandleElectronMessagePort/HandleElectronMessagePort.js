0742940b0461d0f57ad9a5ef24289eafc7ee20ec
Le Vivilet <levivlet@pm.me>
2023-05-16 22:36:41 +0200

    refactor: add handleNodeMessagePort to shared process

---
+++ packages/shared-process/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js
@@ -0,0 +1,14 @@
+import * as Assert from '../Assert/Assert.js'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcChild from '../IpcChild/IpcChild.js'
+import * as IpcChildType from '../IpcChildType/IpcChildType.js'
+
+export const handleElectronMessagePort = async (messagePort) => {
+  Assert.object(messagePort)
+  const ipc = await IpcChild.listen({
+    method: IpcChildType.ElectronMessagePort,
+    messagePort,
+  })
+  HandleIpc.handleIpc(ipc)
+  messagePort.start()
+}
