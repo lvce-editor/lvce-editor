388bad736644e4fa54e475d4f2bd30d7b7787bd7
Le Vivilet <levivlet@pm.me>
2023-05-16 17:02:58 +0200

    feature: accept node messageports in terminal process

--- packages/pty-host/src/parts/CommandMap/CommandMap.js
+++ packages/pty-host/src/parts/CommandMap/CommandMap.js
@@ -1,10 +1,12 @@
-import * as PtyController from '../PtyController/PtyController.js'
-import * as HandleWebSocket from '../HandleWebSocket/HandleWebSocket.js'
 import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.js'
+import * as HandleNodeMessagePort from '../HandleNodeMessagePort/HandleNodeMessagePort.js'
+import * as HandleWebSocket from '../HandleWebSocket/HandleWebSocket.js'
+import * as PtyController from '../PtyController/PtyController.js'
 
 export const commandMap = {
+  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
+  'HandleNodeMessagePort.handleNodeMessagePort': HandleNodeMessagePort.handleNodeMessagePort,
+  'HandleWebSocket.handleWebSocket': HandleWebSocket.handleWebSocket,
   'Terminal.create': PtyController.create,
   'Terminal.write': PtyController.write,
-  'HandleWebSocket.handleWebSocket': HandleWebSocket.handleWebSocket,
-  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
 }
