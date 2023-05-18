f8bb580a173aabf25ca79249a25032ffcf380159
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-18 09:24:32 +0200

    refactor: simplify websocket ipc code (#3049)

--- packages/shared-process/src/parts/IpcChildType/IpcChildType.js
+++ packages/shared-process/src/parts/IpcChildType/IpcChildType.js
@@ -3,6 +3,7 @@ export const NodeForkedProcess = 2
 export const ElectronUtilityProcess = 3
 export const ElectronMessagePort = 4
 export const NodeMessagePort = 5
+export const WebSocket = 6
 
 export const Auto = () => {
   const { argv } = process
