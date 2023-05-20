487262476df9746ce88aef2c642ae2c8b978588f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-20 13:46:16 +0200

    refactor: simplify quickpick color theme code (#3071)

--- packages/renderer-worker/src/parts/QuickPickEntriesColorTheme/QuickPickEntriesColorTheme.js
+++ packages/renderer-worker/src/parts/QuickPickEntriesColorTheme/QuickPickEntriesColorTheme.js
@@ -1,40 +1,22 @@
 import * as Command from '../Command/Command.js'
-import * as Platform from '../Platform/Platform.js'
-import * as PlatformType from '../PlatformType/PlatformType.js'
+import * as GetColorThemeNames from '../GetColorThemeNames/GetColorThemeNames.js'
+import * as QuickPickColorThemeUiStrings from '../QuickPickColorThemeUiStrings/QuickPickColorThemeUiStrings.js'
 import * as QuickPickReturnValue from '../QuickPickReturnValue/QuickPickReturnValue.js'
-import * as SharedProcess from '../SharedProcess/SharedProcess.js'
-
-/**
- * @enum {string}
- */
-export const UiStrings = {
-  NoMatchingColorThemesFound: 'No matching color themes found',
-  SelectColorTheme: 'Select Color Theme',
-}
-
-const getColorThemeNames = async () => {
-  if (Platform.platform === PlatformType.Web) {
-    const assetDir = Platform.getAssetDir()
-    const url = `${assetDir}/config/themes.json`
-    return Command.execute(/* Ajax.getJson */ 'Ajax.getJson', /* url */ url)
-  }
-  return SharedProcess.invoke(/* ExtensionHost.getColorThemeNames */ 'ExtensionHost.getColorThemeNames')
-}
 
 const setColorTheme = (id) => {
   return Command.execute(/* ColorTheme.setColorTheme */ 'ColorTheme.setColorTheme', /* colorThemeId */ id)
 }
 
 export const getPlaceholder = () => {
-  return UiStrings.SelectColorTheme
+  return QuickPickColorThemeUiStrings.selectColorTheme()
 }
 
 export const getLabel = () => {
-  return UiStrings.SelectColorTheme
+  return QuickPickColorThemeUiStrings.selectColorTheme()
 }
 
 export const getPicks = async (searchValue) => {
-  const colorThemeNames = await getColorThemeNames()
+  const colorThemeNames = await GetColorThemeNames.getColorThemeNames()
   return colorThemeNames
 }
 
@@ -46,7 +28,6 @@ export const selectPick = async (pick) => {
 }
 
 export const focusPick = async (pick) => {
-  console.log('focus pick', pick)
   await setColorTheme(/* colorThemeId */ pick)
 }
 
@@ -56,7 +37,7 @@ export const getFilterValue = (value) => {
 
 export const getNoResults = () => {
   return {
-    label: UiStrings.NoMatchingColorThemesFound,
+    label: QuickPickColorThemeUiStrings.noMatchingColorThemesFound(),
   }
 }
 
