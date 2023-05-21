ec76a313c7fa77cb7c4cdb10f94561b13791b8aa
Le Vivilet <levivlet@pm.me>
2023-05-21 19:31:33 +0200

    fix: improve error handling for json parsing error in main process

--- packages/main-process/src/parts/Json/Json.js
+++ packages/main-process/src/parts/Json/Json.js
@@ -1,5 +1,16 @@
-exports.parse = (string) => {
-  return JSON.parse(string)
+const { JsonParsingError } = require('../JsonParsingError/JsonParsingError.js')
+const Assert = require('../Assert/Assert.js')
+
+exports.parse = async (string, filePath = '') => {
+  try {
+    Assert.string(string)
+    return JSON.parse(string)
+  } catch (error) {
+    const JsonError = require('../JsonError/JsonError.js')
+    const errorProps = JsonError.getErrorPropsFromError(error, string, filePath)
+    console.log({ errorProps })
+    throw new JsonParsingError(errorProps.message, errorProps.codeFrame, errorProps.stack)
+  }
 }
 
 exports.stringify = (value) => {
