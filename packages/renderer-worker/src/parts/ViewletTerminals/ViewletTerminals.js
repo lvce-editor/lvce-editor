85f96b6eea569a33236edb1c0f193aa1c2a8ff62
Le Vivilet <levivlet@pm.me>
2023-05-21 22:57:37 +0200

    feature: render another terminal when creating another terminal

--- packages/renderer-worker/src/parts/ViewletTerminals/ViewletTerminals.js
+++ packages/renderer-worker/src/parts/ViewletTerminals/ViewletTerminals.js
@@ -1,7 +1,11 @@
 import * as Assert from '../Assert/Assert.js'
-import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
-import * as Preferences from '../Preferences/Preferences.js'
 import * as Icon from '../Icon/Icon.js'
+import * as Id from '../Id/Id.js'
+import * as Preferences from '../Preferences/Preferences.js'
+import * as RendererProcess from '../RendererProcess/RendererProcess.js'
+import * as ViewletManager from '../ViewletManager/ViewletManager.js'
+import * as ViewletModule from '../ViewletModule/ViewletModule.js'
+import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
 
 export const create = (id, uri, x, y, width, height) => {
   Assert.number(id)
@@ -46,14 +50,31 @@ export const loadContent = async (state) => {
   }
 }
 
-export const addTerminal = (state) => {
-  const { tabs } = state
+export const addTerminal = async (state) => {
+  const { tabs, x, y, width, height, uid } = state
   const newTab = {
     label: `tab ${tabs.length + 1}`,
-    icon: Icon.TerminalBash,
+    icon: Icon.Terminal,
   }
   const newTabs = [...tabs, newTab]
   const newSelectedIndex = newTabs.length - 1
+  const childUid = Id.create()
+  const commands = await ViewletManager.load({
+    getModule: ViewletModule.load,
+    x,
+    y,
+    width,
+    height,
+    uid: childUid,
+    moduleId: ViewletModuleId.Terminal,
+    id: ViewletModuleId.Terminal,
+    show: false,
+    append: false,
+    type: 0,
+    parentId: uid,
+  })
+  commands.push(['Viewlet.append', uid, childUid])
+  await RendererProcess.invoke('Viewlet.sendMultiple', commands)
   return {
     ...state,
     tabs: newTabs,
