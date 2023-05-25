356df5ea013cf57cfa87e4f7da00c98d966f75a1
Le Vivilet <levivlet@pm.me>
2023-05-25 13:16:35 +0200

    refactor: lazyload trash module

--- packages/shared-process/src/parts/Trash/Trash.js
+++ packages/shared-process/src/parts/Trash/Trash.js
@@ -1,8 +1,8 @@
-import { default as _trash } from 'trash'
 import { VError } from '../VError/VError.js'
 
 export const trash = async (path) => {
   try {
+    const { default: _trash } = await import('trash')
     await _trash(path)
   } catch (error) {
     throw new VError(error, 'Failed to move item to trash')
