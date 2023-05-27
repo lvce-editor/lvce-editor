f181051092cd8d25e0870c5e758a2f5e6ddc76ca
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-28 00:39:28 +0200

    feature: allow to pass custom execute function to createRpc method (#3122)

--- packages/extension-host-worker/src/parts/ExtensionHostNodeIpc/ExtensionHostNodeIpc.js
+++ packages/extension-host-worker/src/parts/ExtensionHostNodeIpc/ExtensionHostNodeIpc.js
@@ -5,9 +5,14 @@ import * as RpcParent from '../RpcParent/RpcParent.js'
 import * as RpcParentType from '../RpcParentType/RpcParentType.js'
 import { VError } from '../VError/VError.js'
 
-export const createNodeRpc = async ({ path }) => {
+const defaultExecute = () => {
+  throw new Error('not implemented')
+}
+
+export const createNodeRpc = async ({ path, execute = defaultExecute }) => {
   try {
     Assert.string(path)
+    Assert.fn(execute)
     const ipc = await IpcParent.create({
       method: IpcParentType.ElectronMessagePort,
       type: `custom:${path}`,
@@ -15,10 +20,10 @@ export const createNodeRpc = async ({ path }) => {
     const rpc = await RpcParent.create({
       ipc,
       method: RpcParentType.JsonRpc,
+      execute,
     })
     await rpc.invoke('LoadFile.loadFile', path)
     console.log({ path })
-
     return rpc
   } catch (error) {
     throw new VError(error, `Failed to create node rpc`)
