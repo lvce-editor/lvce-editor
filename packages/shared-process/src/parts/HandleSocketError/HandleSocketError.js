b15100e99454de698a70bdd4dbe41b66199fa40f
Le Vivilet <levivlet@pm.me>
2023-05-28 18:03:14 +0200

    refactor: move handleSocket function into separate file

---
+++ packages/shared-process/src/parts/HandleSocketError/HandleSocketError.js
@@ -0,0 +1,9 @@
+import * as ErrorCodes from '../ErrorCodes/ErrorCodes.js'
+import * as Logger from '../Logger/Logger.js'
+
+export const handleSocketError = (error) => {
+  if (error && error.code === ErrorCodes.ECONNRESET) {
+    return
+  }
+  Logger.info('[info shared process: handle error]', error)
+}
