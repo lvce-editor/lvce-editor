9fd65d1639cbc3ef7cceebfa2913c0d4e93c2c93
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-28 13:53:55 +0200

    refactor: move handleJsonRpcMessage into separate file in shared process (#3129)

--- packages/shared-process/src/parts/HandleIpc/HandleIpc.js
+++ packages/shared-process/src/parts/HandleIpc/HandleIpc.js
@@ -1,30 +1,10 @@
 import * as Callback from '../Callback/Callback.js'
 import * as Command from '../Command/Command.js'
-import * as GetResponse from '../GetResponse/GetResponse.js'
-
-const handleJsonRpcResult = (message) => {
-  Callback.resolve(message.id, message.result)
-}
+import * as HandleJsonRpcMessage from '../HandleJsonRpcMessage/HandleJsonRpcMessage.js'
 
 export const handleIpc = (ipc) => {
-  const handleJsonRpcMessage = async (message) => {
-    if (message.method) {
-      const response = await GetResponse.getResponse(message, ipc)
-      ipc.send(response)
-    } else {
-      // TODO handle error
-      Command.execute(message.method, ...message.params)
-    }
+  const handleMessage = (message) => {
+    return HandleJsonRpcMessage.handleJsonRpcMessage(ipc, message, Command.execute, Callback.resolve)
   }
-
-  const handleMessageFromParentProcess = async (message) => {
-    if (message.result) {
-      return handleJsonRpcResult(message)
-    }
-    if (message.method) {
-      return handleJsonRpcMessage(message)
-    }
-    console.warn('unknown message', message)
-  }
-  ipc.on('message', handleMessageFromParentProcess)
+  ipc.on('message', handleMessage)
 }
