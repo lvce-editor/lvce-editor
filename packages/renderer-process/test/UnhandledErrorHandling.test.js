71e2b34f04a15beff760a42bdb629367e503e924
Le Vivilet <levivlet@pm.me>
2023-05-28 13:46:04 +0200

    fix failing tests

--- packages/renderer-process/test/UnhandledErrorHandling.test.js
+++ packages/renderer-process/test/UnhandledErrorHandling.test.js
@@ -18,19 +18,19 @@ const Logger = await import('../src/parts/Logger/Logger.js')
 test('handleError - normal error', () => {
   const mockError = new Error('oops')
   UnhandledErrorHandling.handleUnhandledError(mockError)
-  expect(Logger.info).toHaveBeenCalledTimes(1)
-  expect(Logger.info).toHaveBeenCalledWith(`[renderer-process] Unhandled Error: Error: oops`)
+  expect(Logger.error).toHaveBeenCalledTimes(1)
+  expect(Logger.error).toHaveBeenCalledWith(`[renderer-process] Unhandled Error: Error: oops`)
 })
 
 test('handleError - null', () => {
   const mockError = null
   UnhandledErrorHandling.handleUnhandledError(mockError)
-  expect(Logger.info).toHaveBeenCalledTimes(1)
-  expect(Logger.info).toHaveBeenCalledWith(`[renderer-process] Unhandled Error: null`)
+  expect(Logger.error).toHaveBeenCalledTimes(1)
+  expect(Logger.error).toHaveBeenCalledWith(`[renderer-process] Unhandled Error: null`)
 })
 
 test('handleError - firefox worker error', () => {
   const mockError = `SyntaxError: import declarations may only appear at top level of a module`
   UnhandledErrorHandling.handleUnhandledError(mockError)
-  expect(Logger.info).not.toHaveBeenCalled()
+  expect(Logger.error).not.toHaveBeenCalled()
 })
