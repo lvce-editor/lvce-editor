54ce6cf8cd63c76408276562103af7a7eb7894ae
Le Vivilet <levivlet@pm.me>
2023-05-28 13:34:30 +0200

    fix: improve error handling for unhandled errors in renderer process

---
+++ packages/renderer-process/src/parts/UnhandledErrorHandling/UnhandledErrorHandling.js
@@ -0,0 +1,20 @@
+import * as ErrorHandling from '../ErrorHandling/ErrorHandling.js'
+import * as IsChromeExtensionError from '../IsChromeExtensionError/IsChromeExtensionError.js'
+import * as IsFirefoxWorkerError from '../IsFirefoxWorkerError/IsFirefoxWorkerError.js'
+
+export const handleUnhandledRejection = (event) => {
+  event.preventDefault()
+  ErrorHandling.handleError(event.reason, true, '[renderer process] Unhandled Rejection: ')
+}
+
+export const handleUnhandledError = (message, filename, lineno, colno, error) => {
+  if (IsChromeExtensionError.isChromeExtensionError(error)) {
+    // ignore errors from chrome extensions
+    return
+  }
+  if (IsFirefoxWorkerError.isFirefoxWorkerError(error)) {
+    // ignore firefox worker errors
+    return
+  }
+  ErrorHandling.handleError(error, true, '[renderer-process] Unhandled Error: ')
+}
