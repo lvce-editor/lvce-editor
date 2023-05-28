a365f057f99c816d13528e967a19f3015af36d80
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-28 18:21:14 +0200

    refactor: move handleSocket function into separate file (#3139)

--- packages/shared-process/src/parts/HandleWebSocketForSharedProcess/HandleWebSocketForSharedProcess.js
+++ packages/shared-process/src/parts/HandleWebSocketForSharedProcess/HandleWebSocketForSharedProcess.js
@@ -1,19 +1,11 @@
-import * as ErrorCodes from '../ErrorCodes/ErrorCodes.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as HandleSocketError from '../HandleSocketError/HandleSocketError.js'
 import * as IpcChild from '../IpcChild/IpcChild.js'
 import * as IpcChildType from '../IpcChildType/IpcChildType.js'
-import * as Logger from '../Logger/Logger.js'
 import * as WebSocketServer from '../WebSocketServer/WebSocketServer.js'
 
-const handleSocketError = (error) => {
-  if (error && error.code === ErrorCodes.ECONNRESET) {
-    return
-  }
-  Logger.info('[info shared process: handle error]', error)
-}
-
 export const handleWebSocket = async (message, handle) => {
-  handle.on('error', handleSocketError)
+  handle.on('error', HandleSocketError.handleSocketError)
   const webSocket = await WebSocketServer.handleUpgrade(message, handle)
   const ipc = await IpcChild.listen({
     method: IpcChildType.WebSocket,
