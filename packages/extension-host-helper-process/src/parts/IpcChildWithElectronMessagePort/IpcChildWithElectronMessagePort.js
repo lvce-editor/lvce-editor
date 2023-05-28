abf09fa8c8b5428f8ad35f6eec6d26a536507a9d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-28 18:25:45 +0200

    feature: add handleElectronMessagePort function to extension host hel… (#3140)

--- packages/extension-host-helper-process/src/parts/IpcChildWithElectronMessagePort/IpcChildWithElectronMessagePort.js
+++ packages/extension-host-helper-process/src/parts/IpcChildWithElectronMessagePort/IpcChildWithElectronMessagePort.js
@@ -1,15 +1,39 @@
-import { once } from 'events'
+import { IpcError } from '../IpcError/IpcError.js'
+import * as IsMessagePortMain from '../IsMessagePortMain/IsMessagePortMain.js'
 
-export const listen = async () => {
-  // const [message, handle] = await once(process, 'message')
-  // if (process.send) {
-  //   process.send('ready')
-  // }
-  // return {
-  // }
+export const listen = ({ messagePort }) => {
+  if (!IsMessagePortMain.isMessagePortMain(messagePort)) {
+    throw new IpcError('port must be of type MessagePortMain')
+  }
+  return messagePort
 }
 
-export const wrap = () => {
-  // TODO
-  return {}
+const getActualData = (event) => {
+  return event.data
+}
+
+export const wrap = (messagePort) => {
+  return {
+    messagePort,
+    on(event, listener) {
+      if (event === 'message') {
+        const wrappedListener = (event) => {
+          const actualData = getActualData(event)
+          listener(actualData)
+        }
+        this.messagePort.on(event, wrappedListener)
+      } else {
+        throw new Error('unsupported event type')
+      }
+    },
+    off(event, listener) {
+      this.messagePort.off(event, listener)
+    },
+    send(message) {
+      this.messagePort.postMessage(message)
+    },
+    dispose() {
+      this.messagePort.close()
+    },
+  }
 }
