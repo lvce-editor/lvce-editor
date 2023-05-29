72619cb3d57751e6a27a4cd3413bdbc7a6aa7dcb
Le Vivilet <levivlet@pm.me>
2023-05-29 15:14:54 +0200

    fix: use commonjs

--- packages/main-process/src/parts/UtilityProcessState/UtilityProcessState.js
+++ packages/main-process/src/parts/UtilityProcessState/UtilityProcessState.js
@@ -4,17 +4,17 @@ const state = (exports.state = {
   all: Object.create(null),
 })
 
-export const add = (pid, name) => {
+exports.add = (pid, name) => {
   Assert.number(pid)
   Assert.string(pid)
   state.all[pid] = name
 }
 
-export const remove = (pid) => {
+exports.remove = (pid) => {
   Assert.number(pid)
   delete state.all[pid]
 }
 
-export const getAll = () => {
+exports.getAll = () => {
   return Object.entries(state.all)
 }
