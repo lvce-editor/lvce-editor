4f81b8ae2069fd40cd2c50b5b257c680e338d3a4
Le Vivilet <levivlet@pm.me>
2023-05-30 23:30:02 +0200

    refactor: move handleJsonRpcMessage function into separate file

--- packages/renderer-worker/src/parts/HandleIpc/HandleIpc.js
+++ packages/renderer-worker/src/parts/HandleIpc/HandleIpc.js
@@ -1,36 +1,10 @@
 import * as Callback from '../Callback/Callback.js'
 import * as Command from '../Command/Command.js'
-import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
+import * as HandleJsonRpcMessage from '../HandleJsonRpcMessage/HandleJsonRpcMessage.js'
 
 export const handleIpc = (ipc, source = 'process') => {
-  const handleMessageFromRendererProcess = async (message) => {
-    if (typeof message === 'string') {
-      console.warn(`unexpected message from ${source}: ${message}`)
-      return
-    }
-    if (message.id) {
-      if ('method' in message) {
-        try {
-          const result = await Command.execute(message.method, ...message.params)
-          ipc.send({
-            jsonrpc: JsonRpcVersion.Two,
-            id: message.id,
-            result,
-          })
-          return
-        } catch (error) {
-          ipc.send({
-            jsonrpc: JsonRpcVersion.Two,
-            id: message.id,
-            error,
-          })
-          return
-        }
-      }
-      Callback.resolve(message.id, message)
-      return
-    }
-    await Command.execute(message.method, ...message.params)
+  const handleMessageFromRendererProcess = (message) => {
+    return HandleJsonRpcMessage.handleJsonRpcMessage(ipc, message, Command.execute, Callback.resolve, source)
   }
   ipc.onmessage = handleMessageFromRendererProcess
 }
---
+++ packages/renderer-worker/src/parts/HandleJsonRpcMessage/HandleJsonRpcMessage.js
@@ -0,0 +1,31 @@
+import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
+
+export const handleJsonRpcMessage = async (ipc, message, execute, resolve, source) => {
+  if (typeof message === 'string') {
+    console.warn(`unexpected message from ${source}: ${message}`)
+    return
+  }
+  if (message.id) {
+    if ('method' in message) {
+      try {
+        const result = await execute(message.method, ...message.params)
+        ipc.send({
+          jsonrpc: JsonRpcVersion.Two,
+          id: message.id,
+          result,
+        })
+        return
+      } catch (error) {
+        ipc.send({
+          jsonrpc: JsonRpcVersion.Two,
+          id: message.id,
+          error,
+        })
+        return
+      }
+    }
+    resolve(message.id, message)
+    return
+  }
+  await execute(message.method, ...message.params)
+}
