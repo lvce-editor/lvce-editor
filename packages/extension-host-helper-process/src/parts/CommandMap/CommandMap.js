b454a600c3542254181fc20fb965a0cd181991f8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-30 14:38:57 +0200

    feature: add handleWebSocket function to extension host helper commands (#3169)

--- packages/extension-host-helper-process/src/parts/CommandMap/CommandMap.js
+++ packages/extension-host-helper-process/src/parts/CommandMap/CommandMap.js
@@ -1,7 +1,9 @@
 import * as Ajax from '../Ajax/Ajax.js'
 import * as Exec from '../Exec/Exec.js'
+import * as HandleWebSocket from '../HandleWebSocket/HandleWebSocket.js'
 
 export const commandMap = {
   'Exec.exec': Exec.exec,
   'Ajax.getJson': Ajax.getJson,
+  'HandleWebSocket.handleWebSocket': HandleWebSocket.handleWebSocket,
 }
