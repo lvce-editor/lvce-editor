6fd87c513498eb9f42d00f43174d3d382f336200
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-30 15:11:59 +0200

    feature: add loadFile function to extension host helper process (#3173)

--- packages/extension-host-helper-process/src/parts/CommandMap/CommandMap.js
+++ packages/extension-host-helper-process/src/parts/CommandMap/CommandMap.js
@@ -1,9 +1,11 @@
 import * as Ajax from '../Ajax/Ajax.js'
 import * as Exec from '../Exec/Exec.js'
 import * as HandleWebSocket from '../HandleWebSocket/HandleWebSocket.js'
+import * as LoadFile from '../LoadFile/LoadFile.js'
 
 export const commandMap = {
   'Exec.exec': Exec.exec,
   'Ajax.getJson': Ajax.getJson,
   'HandleWebSocket.handleWebSocket': HandleWebSocket.handleWebSocket,
+  'LoadFile.loadFile': LoadFile.loadFile,
 }
