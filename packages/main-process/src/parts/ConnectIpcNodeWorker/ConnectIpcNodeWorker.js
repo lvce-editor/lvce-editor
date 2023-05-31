1a93f5f4c1d24c05c3805e6bd7fc853eb824b43c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-31 19:07:04 +0200

    refactor: use object destructuring (#3175)

--- packages/main-process/src/parts/ConnectIpcNodeWorker/ConnectIpcNodeWorker.js
+++ packages/main-process/src/parts/ConnectIpcNodeWorker/ConnectIpcNodeWorker.js
@@ -3,10 +3,9 @@ const Performance = require('../Performance/Performance.js')
 const PerformanceMarkerType = require('../PerformanceMarkerType/PerformanceMarkerType.js')
 const TransferMessagePort = require('../TransferMessagePort/TransferMessagePort.js')
 
-exports.connectIpc = async (sharedProcess, browserWindowPort, folder = '') => {
+exports.connectIpc = async (ipc, browserWindowPort, folder = '') => {
   const messageChannel = new MessageChannel()
-  const { port1 } = messageChannel
-  const { port2 } = messageChannel
+  const { port1, port2 } = messageChannel
   Performance.mark(PerformanceMarkerType.DidStartSharedProcess)
   browserWindowPort.on('message', (event) => {
     // console.log('got message from browser window', event.data)
@@ -16,6 +15,6 @@ exports.connectIpc = async (sharedProcess, browserWindowPort, folder = '') => {
     // console.log('send message to browser window', message)
     browserWindowPort.postMessage(message)
   })
-  await TransferMessagePort.transferMessagePort(sharedProcess, port1, folder)
+  await TransferMessagePort.transferMessagePort(ipc, port1, folder)
   browserWindowPort.start()
 }
