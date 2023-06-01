19f856cc3b9d189af0e4e1967781d5b98854c9cd
Le Vivilet <levivlet@pm.me>
2023-06-01 13:37:36 +0200

    fix failing test

--- packages/renderer-worker/src/parts/MeasureLongestLineWidth/MeasureLongestLineWidth.js
+++ packages/renderer-worker/src/parts/MeasureLongestLineWidth/MeasureLongestLineWidth.js
@@ -1,7 +1,6 @@
 import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.js'
 
 export const measureLongestLineWidth = (lines, fontWeight, fontSize, fontFamily, letterSpacing, isMonoSpaceFont, charWidth) => {
-  console.log({ isMonoSpaceFont, charWidth })
   let longest = 0
   for (const line of lines) {
     const lineWidth = MeasureTextWidth.measureTextWidth(line, fontWeight, fontSize, fontFamily, letterSpacing, isMonoSpaceFont, charWidth)
--- packages/renderer-worker/test/MeasureLongestLineWidth.test.js
+++ packages/renderer-worker/test/MeasureLongestLineWidth.test.js
@@ -24,7 +24,9 @@ test('measureLongestLineWidth', () => {
   const fontSize = 15
   const fontFamily = 'Test Font'
   const letterSpacing = 0.5
-  expect(MeasureLongestLineWidth.measureLongestLineWidth(lines, fontWeight, fontSize, fontFamily, letterSpacing)).toBe(16)
+  const isMonospaceFont = false
+  const charWidth = 0
+  expect(MeasureLongestLineWidth.measureLongestLineWidth(lines, fontWeight, fontSize, fontFamily, letterSpacing, isMonospaceFont, charWidth)).toBe(16)
   expect(MeasureTextWidth.measureTextWidth).toHaveBeenCalledTimes(1)
-  expect(MeasureTextWidth.measureTextWidth).toHaveBeenCalledWith('test', 400, 15, 'Test Font', 0.5)
+  expect(MeasureTextWidth.measureTextWidth).toHaveBeenCalledWith('test', 400, 15, 'Test Font', 0.5, false, 0)
 })
