4cd2c70f767aba425ab3cbe8df6239689b94b234
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-06-03 19:07:59 +0200

    feature: make rpc available as global variable to allow calling funct… (#3210)

--- packages/extension-host-worker/src/parts/RpcParentWithJsonRpc/RpcParentWithJsonRpc.js
+++ packages/extension-host-worker/src/parts/RpcParentWithJsonRpc/RpcParentWithJsonRpc.js
@@ -1,20 +1,13 @@
 import * as Assert from '../Assert/Assert.js'
 import * as Callback from '../Callback/Callback.js'
-import * as GetResponse from '../GetResponse/GetResponse.js'
+import * as HandleJsonRpcMessage from '../HandleJsonRpcMessage/HandleJsonRpcMessage.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 
 export const create = ({ ipc, execute }) => {
   Assert.object(ipc)
   Assert.fn(execute)
   const handleMessage = async (message) => {
-    if ('id' in message) {
-      if ('method' in message) {
-        const response = await GetResponse.getResponse(message, execute)
-        ipc.send(response)
-        return
-      }
-      Callback.resolve(message.id, message)
-    }
+    return HandleJsonRpcMessage.handleJsonRpcMessage(ipc, message, execute, Callback.resolve)
   }
   ipc.onmessage = handleMessage
   return {
