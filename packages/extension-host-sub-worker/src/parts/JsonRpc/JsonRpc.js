dff1d765d4a7ad3346d410d521d19be82bdc8579
Le Vivilet <levivlet@pm.me>
2023-06-03 18:50:34 +0200

    feature: make rpc available as global variable to allow calling functions from inside worker

--- packages/extension-host-sub-worker/src/parts/JsonRpc/JsonRpc.js
+++ packages/extension-host-sub-worker/src/parts/JsonRpc/JsonRpc.js
@@ -1,1 +1,24 @@
-export const ErrorMethodNotFound = -32601
+import * as Callback from '../Callback/Callback.js'
+import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
+import * as UnwrapJsonRpcResult from '../UnwrapJsonRpcResult/UnwrapJsonRpcResult.js'
+
+export const send = (transport, method, ...params) => {
+  transport.send({
+    jsonrpc: JsonRpcVersion.Two,
+    method,
+    params: params,
+  })
+}
+
+export const invoke = async (ipc, method, ...params) => {
+  const { id, promise } = Callback.registerPromise()
+  ipc.send({
+    jsonrpc: JsonRpcVersion.Two,
+    method,
+    params,
+    id,
+  })
+  const responseMessage = await promise
+  const result = UnwrapJsonRpcResult.unwrapJsonRpcResult(responseMessage)
+  return result
+}
