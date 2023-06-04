421c986e58c3695e895fffa4ad134a2fed7fb2bc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-06-04 16:14:26 +0200

    fix: exit extension host helper process on disconnect (#3226)

---
+++ packages/extension-host-helper-process/src/parts/ExitCode/ExitCode.js
@@ -0,0 +1,5 @@
+export const Error = 1
+
+export const ExpectedError = 128
+
+export const Success = 0
---
+++ packages/extension-host-helper-process/src/parts/Process/Process.ipc.js
@@ -0,0 +1,9 @@
+import * as Process from './Process.js'
+
+export const name = 'Process'
+
+export const Commands = {
+  crash: Process.crash,
+  crashAsync: Process.crashAsync,
+  getPid: Process.getPid,
+}
