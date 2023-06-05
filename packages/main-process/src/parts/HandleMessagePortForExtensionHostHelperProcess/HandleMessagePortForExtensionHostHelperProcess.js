63380d9218628b6f54a669bb31c51b28ef1ea35c
Le Vivilet <levivlet@pm.me>
2023-06-05 21:33:00 +0200

    fix: dispose extension processes when messageport closes, fixing processes memory leak

--- packages/main-process/src/parts/HandleMessagePortForExtensionHostHelperProcess/HandleMessagePortForExtensionHostHelperProcess.js
+++ packages/main-process/src/parts/HandleMessagePortForExtensionHostHelperProcess/HandleMessagePortForExtensionHostHelperProcess.js
@@ -3,6 +3,11 @@ const IpcParentType = require('../IpcParentType/IpcParentType.js')
 const Logger = require('../Logger/Logger.js')
 const Platform = require('../Platform/Platform.js')
 
+/**
+ *
+ * @param {any} event
+ * @param {import('electron').MessagePortMain} browserWindowPort
+ */
 exports.handlePort = async (event, browserWindowPort) => {
   const extensionHostHelperProcessPath = Platform.getExtensionHostHelperProcessPath()
   const start = performance.now()
@@ -14,11 +19,36 @@ exports.handlePort = async (event, browserWindowPort) => {
   const { pid } = ipc
   const forkTime = end - start
   Logger.info(`[main-process] Starting extension host helper with pid ${pid} (fork took ${forkTime} ms).`)
-  browserWindowPort.on('message', (event) => {
-    ipc.send(event.data)
-  })
-  ipc.on('message', (event) => {
+  const cleanup = () => {
+    browserWindowPort.off('message', handlePortMessage)
+    browserWindowPort.off('close', handlePortClosed)
+    if (ipc.off) {
+      ipc.off('message', handleIpcMessage)
+    }
+    webContents.off('destroyed', handleWebContentsDestroyed)
+    browserWindowPort.close()
+    ipc.dispose()
+  }
+  const handleIpcMessage = (event) => {
     browserWindowPort.postMessage(event)
-  })
+  }
+  const handlePortMessage = (event) => {
+    ipc.send(event.data)
+  }
+  const handleWebContentsDestroyed = () => {
+    cleanup()
+  }
+  const handlePortClosed = () => {
+    cleanup()
+  }
+  const webContents = event.sender
+  browserWindowPort.on('message', handlePortMessage)
+  browserWindowPort.on('close', handlePortClosed)
+  ipc.on('message', handleIpcMessage)
+  webContents.on('destroyed', handleWebContentsDestroyed)
+  if (webContents.isDestroyed()) {
+    cleanup()
+    return
+  }
   browserWindowPort.start()
 }
