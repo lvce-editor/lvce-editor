ad6ee611673ce977cf158b5b3522314f0841187f
Le Vivilet <levivlet@pm.me>
2023-06-11 12:48:40 +0200

    refactor: move getElectronRebuildPath function into separate file

--- packages/shared-process/src/parts/RebuildNodePty/RebuildNodePty.js
+++ packages/shared-process/src/parts/RebuildNodePty/RebuildNodePty.js
@@ -1,12 +1,9 @@
 import { spawn } from 'node:child_process'
+import * as GetElectronRebuildPath from '../GetElectronRebuildPath/GetElectronRebuildPath.js'
 import * as IsElectron from '../IsElectron/IsElectron.js'
 import * as Path from '../Path/Path.js'
 import * as Root from '../Root/Root.js'
 import { VError } from '../VError/VError.js'
-
-const getElectronRebuildPath = () => {
-  return Path.join(Root.root, 'packages', 'main-process', 'node_modules', '.bin', 'electron-rebuild')
-}
 
 const getPtyHostPath = () => {
   return Path.join(Root.root, 'packages', 'pty-host')
@@ -16,7 +13,7 @@ const getPtyHostPath = () => {
  * @param {string} cwd
  */
 const rebuildNodePtyElectron = async (cwd) => {
-  const electronRebuildPath = getElectronRebuildPath()
+  const electronRebuildPath = GetElectronRebuildPath.getElectronRebuildPath()
   const childProcess = spawn(electronRebuildPath, [], {
     cwd,
     stdio: 'inherit',
