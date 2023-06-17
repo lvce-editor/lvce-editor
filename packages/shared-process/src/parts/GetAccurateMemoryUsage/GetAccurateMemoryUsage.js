8b01e583f00fd14dcd363bfd7f94a8aa0cce7bb3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-06-17 23:00:00 +0200

    refactor: move addAccurateMemoryUsage function into separate file (#3315)

--- packages/shared-process/src/parts/GetAccurateMemoryUsage/GetAccurateMemoryUsage.js
+++ packages/shared-process/src/parts/GetAccurateMemoryUsage/GetAccurateMemoryUsage.js
@@ -1,9 +1,10 @@
-import { join } from 'node:path'
 import { readFile } from 'node:fs/promises'
-import { VError } from '../VError/VError.js'
+import { join } from 'node:path'
 import * as Assert from '../Assert/Assert.js'
+import * as Character from '../Character/Character.js'
 import * as EncodingType from '../EncodingType/EncodingType.js'
 import * as ErrorCodes from '../ErrorCodes/ErrorCodes.js'
+import { VError } from '../VError/VError.js'
 
 export const getAccurateMemoryUsage = async (pid) => {
   Assert.number(pid)
@@ -25,14 +26,13 @@ export const getAccurateMemoryUsage = async (pid) => {
       throw error
     }
     const trimmedContent = content.trim()
-    const numberBlocks = trimmedContent.split(' ')
+    const numberBlocks = trimmedContent.split(Character.Space)
     const pageSize = 4096
     const rss = Number.parseInt(numberBlocks[1]) * pageSize
     const shared = Number.parseInt(numberBlocks[2]) * pageSize
     const memory = rss - shared
     return memory
   } catch (error) {
-    // @ts-ignore
     throw new VError(error, 'Failed to get accurate memory usage')
   }
 }
--- packages/shared-process/src/parts/ListProcessesWithMemoryUsageUnix/ListProcessesWithMemoryUsageUnix.js
+++ packages/shared-process/src/parts/ListProcessesWithMemoryUsageUnix/ListProcessesWithMemoryUsageUnix.js
@@ -1,19 +1,8 @@
+import * as AddAccurateMemoryUsage from '../AddAccurateMemoryUsage/AddAccurateMemoryUsage.js'
 import * as CreatePidMap from '../CreatePidMap/CreatePidMap.js'
-import * as GetAccurateMemoryUsage from '../GetAccurateMemoryUsage/GetAccurateMemoryUsage.js'
 import * as GetPsOutput from '../GetPsOutput/GetPsOutput.js'
+import * as HasPositiveMemoryUsage from '../HasPositiveMemoryUsage/HasPositiveMemoryUsage.js'
 import * as ParsePsOutput from '../ParsePsOutput/ParsePsOutput.js'
-
-const addAccurateMemoryUsage = async (process) => {
-  const accurateMemoryUsage = await GetAccurateMemoryUsage.getAccurateMemoryUsage(process.pid)
-  return {
-    ...process,
-    memory: accurateMemoryUsage,
-  }
-}
-
-const hasPositiveMemoryUsage = (process) => {
-  return process.memory >= 0
-}
 
 export const listProcessesWithMemoryUsage = async (rootPid) => {
   // console.time('getPsOutput')
@@ -25,8 +14,8 @@ export const listProcessesWithMemoryUsage = async (rootPid) => {
   const parsed = ParsePsOutput.parsePsOutput(stdout, rootPid, pidMap)
   // console.timeEnd('parsePsOutput')
   // console.time('addAccurateMemoryUsage')
-  const parsedWithAccurateMemoryUsage = await Promise.all(parsed.map(addAccurateMemoryUsage))
+  const parsedWithAccurateMemoryUsage = await Promise.all(parsed.map(AddAccurateMemoryUsage.addAccurateMemoryUsage))
   // console.timeEnd('addAccurateMemoryUsage')
-  const filtered = parsedWithAccurateMemoryUsage.filter(hasPositiveMemoryUsage)
+  const filtered = parsedWithAccurateMemoryUsage.filter(HasPositiveMemoryUsage.hasPositiveMemoryUsage)
   return filtered
 }
