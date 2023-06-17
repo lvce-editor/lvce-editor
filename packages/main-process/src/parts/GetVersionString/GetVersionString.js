77a469692f7bd618729c58f588d1db3db6c2e745
Le Vivilet <levivlet@pm.me>
2023-06-17 15:58:12 +0200

    feature: make cli version command more compact

--- packages/main-process/src/parts/GetVersionString/GetVersionString.js
+++ packages/main-process/src/parts/GetVersionString/GetVersionString.js
@@ -1,46 +1,6 @@
-const JoinLines = require('../JoinLines/JoinLines.js')
 const Platform = require('../Platform/Platform.js')
-const Process = require('../Process/Process.js')
-const Assert = require('../Assert/Assert.js')
-
-const getName = (object) => {
-  return object.name
-}
-
-const getVersions = () => {
-  return [
-    {
-      name: Platform.productNameLong,
-      version: Platform.version,
-    },
-    {
-      name: 'Electron',
-      version: Process.getElectronVersion(),
-    },
-    {
-      name: 'Chrome',
-      version: Process.getChromeVersion(),
-    },
-    {
-      name: 'Node',
-      version: Process.getNodeVersion(),
-    },
-  ]
-}
-
-const getLength = (string) => {
-  Assert.string(string)
-  return string.length
-}
 
 exports.getVersionString = () => {
-  const versions = getVersions()
-  const names = versions.map(getName)
-  const lengths = names.map(getLength)
-  const longestLength = Math.max(...lengths)
-  const lines = []
-  for (const version of versions) {
-    lines.push(version.name.padEnd(longestLength, ' ') + ': ' + version.version)
-  }
-  return JoinLines.joinLines(lines)
+  const version = Platform.version
+  return version
 }
