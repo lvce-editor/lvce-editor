0b4c03c1c16d1169c342f6c322ec999976498eac
Le Vivilet <levivlet@pm.me>
2023-06-18 09:29:40 +0200

    fix failing test

--- packages/renderer-worker/test/ExtensionMeta.test.js
+++ packages/renderer-worker/test/ExtensionMeta.test.js
@@ -13,10 +13,15 @@ jest.unstable_mockModule('../src/parts/Command/Command.js', () => {
     }),
   }
 })
-const ExtensionMeta = await import(
-  '../src/parts/ExtensionMeta/ExtensionMeta.js'
-)
+jest.unstable_mockModule('../src/parts/Logger/Logger.js', () => {
+  return {
+    error: jest.fn(() => {}),
+  }
+})
+
+const ExtensionMeta = await import('../src/parts/ExtensionMeta/ExtensionMeta.js')
 const Command = await import('../src/parts/Command/Command.js')
+const Logger = await import('../src/parts/Logger/Logger.js')
 
 test('organizeExtensions', () => {
   expect(
@@ -24,8 +29,7 @@ test('organizeExtensions', () => {
       {
         id: 'builtin.language-basics-css',
         name: 'Language Basics CSS',
-        description:
-          'Provides syntax highlighting and bracket matching in CSS files.',
+        description: 'Provides syntax highlighting and bracket matching in CSS files.',
         languages: [
           {
             id: 'css',
@@ -40,8 +44,7 @@ test('organizeExtensions', () => {
         path: '/test/language-basics-markdown',
         status: ExtensionManifestStatus.Rejected,
         reason: {
-          jse_shortmsg:
-            'Failed to load extension "language-basics-markdown": Failed to load extension manifest',
+          jse_shortmsg: 'Failed to load extension "language-basics-markdown": Failed to load extension manifest',
           jse_cause: {
             errno: -20,
             code: ErrorCodes.ENOTDIR,
@@ -52,8 +55,7 @@ test('organizeExtensions', () => {
           message:
             'Failed to load extension "language-basics-markdown": Failed to load extension manifest: ENOTDIR: not a directory, open \'/test/language-basics-markdown/extension.json\'',
           code: 'E_LOADING_EXTENSION_MANIFEST_FAilED',
-          originalStack:
-            "Error: ENOTDIR: not a directory, open '/test/language-basics-markdown/extension.json'",
+          originalStack: "Error: ENOTDIR: not a directory, open '/test/language-basics-markdown/extension.json'",
         },
       },
     ])
@@ -70,20 +72,17 @@ test('organizeExtensions', () => {
             syscall: 'open',
           },
           jse_info: {},
-          jse_shortmsg:
-            'Failed to load extension "language-basics-markdown": Failed to load extension manifest',
+          jse_shortmsg: 'Failed to load extension "language-basics-markdown": Failed to load extension manifest',
           message:
             'Failed to load extension "language-basics-markdown": Failed to load extension manifest: ENOTDIR: not a directory, open \'/test/language-basics-markdown/extension.json\'',
-          originalStack:
-            "Error: ENOTDIR: not a directory, open '/test/language-basics-markdown/extension.json'",
+          originalStack: "Error: ENOTDIR: not a directory, open '/test/language-basics-markdown/extension.json'",
         },
         status: ExtensionManifestStatus.Rejected,
       },
     ],
     resolved: [
       {
-        description:
-          'Provides syntax highlighting and bracket matching in CSS files.',
+        description: 'Provides syntax highlighting and bracket matching in CSS files.',
         id: 'builtin.language-basics-css',
         languages: [
           {
@@ -108,8 +107,7 @@ test('handleRejectedExtension - ignore ENOTDIR error', async () => {
       path: '/test/language-basics-markdown',
       status: ExtensionManifestStatus.Rejected,
       reason: {
-        jse_shortmsg:
-          'Failed to load extension "language-basics-markdown": Failed to load extension manifest',
+        jse_shortmsg: 'Failed to load extension "language-basics-markdown": Failed to load extension manifest',
         jse_cause: {
           errno: -20,
           code: ErrorCodes.ENOTDIR,
@@ -120,8 +118,7 @@ test('handleRejectedExtension - ignore ENOTDIR error', async () => {
         message:
           'Failed to load extension "language-basics-markdown": Failed to load extension manifest: ENOTDIR: not a directory, open \'/test/language-basics-markdown/extension.json\'',
         code: ErrorCodes.ENOTDIR,
-        originalStack:
-          "Error: ENOTDIR: not a directory, open '/test/language-basics-markdown/extension.json'",
+        originalStack: "Error: ENOTDIR: not a directory, open '/test/language-basics-markdown/extension.json'",
       },
     },
   ])
@@ -136,8 +133,7 @@ test('handleRejectedExtension - ignore E_MANIFEST_NOT_FOUND error', async () => 
       path: '/test/abc',
       status: ExtensionManifestStatus.Rejected,
       reason: {
-        jse_shortmsg:
-          'Failed to load extension "abc": Failed to load extension manifest',
+        jse_shortmsg: 'Failed to load extension "abc": Failed to load extension manifest',
         jse_cause: {
           errno: -2,
           code: ErrorCodes.E_MANIFEST_NOT_FOUND,
@@ -145,8 +141,7 @@ test('handleRejectedExtension - ignore E_MANIFEST_NOT_FOUND error', async () => 
           path: '/test/abc/extension.json',
         },
         jse_info: {},
-        message:
-          'Failed to load extension "abc": Failed to load extension manifest: File not found \'/test/abc/extension.json\'',
+        message: 'Failed to load extension "abc": Failed to load extension manifest: File not found \'/test/abc/extension.json\'',
         code: ErrorCodes.E_MANIFEST_NOT_FOUND,
         originalStack: "Error: File not found '/test/abc/extension.json'",
       },
@@ -167,12 +162,8 @@ test('handleRejectedExtension - show error message for other error', async () =>
       },
     },
   ])
-  expect(Command.execute).toHaveBeenCalledTimes(1)
-  expect(Command.execute).toHaveBeenCalledWith('Dialog.showMessage', {
-    codeFrame: '',
-    message: 'TypeError: x is not a function',
-    stack: '',
-  })
+  expect(Logger.error).toHaveBeenCalledTimes(1)
+  expect(Logger.error).toHaveBeenCalledWith('TypeError: x is not a function\n\n')
 })
 
 test('filterByMatchingEvent', () => {
