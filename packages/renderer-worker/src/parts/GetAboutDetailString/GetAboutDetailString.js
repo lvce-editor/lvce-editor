4857b69c5d2b1fe31c6fd462efe5763c72ada07d
Le Vivilet <levivlet@pm.me>
2023-06-20 20:22:18 +0200

    feature: display commit date in about dialog

--- packages/renderer-worker/src/parts/GetAboutDetailString/GetAboutDetailString.js
+++ packages/renderer-worker/src/parts/GetAboutDetailString/GetAboutDetailString.js
@@ -1,19 +1,33 @@
+import * as ElectronProcess from '../ElectronProcess/ElectronProcess.js'
+import * as FormatDate from '../FormatDate/FormatDate.js'
 import * as JoinLines from '../JoinLines/JoinLines.js'
 import * as Process from '../Process/Process.js'
-import * as ElectronProcess from '../ElectronProcess/ElectronProcess.js'
+
+const formatDate = (isoDate) => {
+  if (!isoDate) {
+    return 'unknown'
+  }
+  const date = new Date(isoDate).getTime()
+  const now = new Date().getTime()
+  const ago = FormatDate.formatDate(date, now)
+  return `${isoDate} (${ago})`
+}
 
 export const getDetailString = async () => {
-  const [electronVersion, nodeVersion, chromeVersion, version, commit, v8Version] = await Promise.all([
+  const [electronVersion, nodeVersion, chromeVersion, version, commit, v8Version, date] = await Promise.all([
     Process.getElectronVersion(),
     Process.getNodeVersion(),
     Process.getChromeVersion(),
     Process.getVersion(),
     Process.getCommit(),
     Process.getV8Version(),
+    Process.getDate(),
   ])
+  const formattedDate = formatDate(date)
   const lines = [
     `Version: ${version}`,
     `Commit: ${commit}`,
+    `Date: ${formattedDate}`,
     `Electron: ${electronVersion}`,
     `Chromium: ${chromeVersion}`,
     `Node: ${nodeVersion}`,
