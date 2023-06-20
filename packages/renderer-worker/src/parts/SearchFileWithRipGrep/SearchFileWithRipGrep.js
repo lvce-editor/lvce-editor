a382f6a7a22d023fae2e3ebd51c8f6ab8300d372
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-06-20 18:05:59 +0200

    refactor: move getRipGrepArgs function to renderer worker (#3363)

--- packages/renderer-worker/src/parts/SearchFileWithRipGrep/SearchFileWithRipGrep.js
+++ packages/renderer-worker/src/parts/SearchFileWithRipGrep/SearchFileWithRipGrep.js
@@ -1,14 +1,16 @@
+import * as GetFileSearchRipGrepArgs from '../GetFileSearchRipGrepArgs/GetFileSearchRipGrepArgs.js'
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
-import * as SplitLines from '../SplitLines/SplitLines.js'
 import * as SharedProcessCommandType from '../SharedProcessCommandType/SharedProcessCommandType.js'
+import * as SplitLines from '../SplitLines/SplitLines.js'
 
 export const searchFile = async (path, value) => {
-  const stdout = await SharedProcess.invoke(
-    SharedProcessCommandType.SearchFileSearchFile,
-    /* path */ path,
-    /* searchTerm */ value,
-    /* limit */ 9999999
-  )
+  const ripGrepArgs = GetFileSearchRipGrepArgs.getFileSearchRipGrepArgs()
+  const options = {
+    ripGrepArgs,
+    searchPath: path,
+    limit: 9999999,
+  }
+  const stdout = await SharedProcess.invoke(SharedProcessCommandType.SearchFileSearchFile, options)
   const lines = SplitLines.splitLines(stdout)
   return lines
 }
