a623b91acf4f11b28df4c3e2fa45d40b6c4f45f1
Le Vivilet <levivlet@pm.me>
2023-06-21 14:14:01 +0200

    fix: return 404 response in electron net.fetch instead of throwing error to be compatible with web fetch api

--- packages/main-process/src/parts/GetFileResponse/GetFileResponse.js
+++ packages/main-process/src/parts/GetFileResponse/GetFileResponse.js
@@ -1,8 +1,21 @@
 const { net } = require('electron')
 const { pathToFileURL } = require('node:url')
+const HttpStatusCode = require('../HttpStatusCode/HttpStatusCode.js')
+const IsNetFileNotFoundError = require('../IsNetFileNotFoundError/IsNetFileNotFoundError.js')
 
-exports.getFileResponse = (path) => {
-  const url = pathToFileURL(path)
-  const response = net.fetch(url)
-  return response
+exports.getFileResponse = async (path) => {
+  try {
+    const url = pathToFileURL(path)
+    const response = await net.fetch(url)
+    return response
+  } catch (error) {
+    if (IsNetFileNotFoundError.isNetFileNotFoundError(error)) {
+      // @ts-ignore
+      return new Response('not-found', {
+        status: HttpStatusCode.NotFound,
+        statusText: 'not-found',
+      })
+    }
+    throw error
+  }
 }
