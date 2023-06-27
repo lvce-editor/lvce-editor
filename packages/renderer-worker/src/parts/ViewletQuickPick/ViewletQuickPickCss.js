897cfcf01ae91ad08e16265491ab1c5dcb7ffa00
Le Vivilet <levivlet@pm.me>
2023-06-27 10:07:01 +0200

    feature: add css variables declarations to components to only load the minimum required css variables into the renderer process

--- packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickCss.js
+++ packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickCss.js
@@ -1,1 +1,3 @@
 export const Css = ['/css/parts/ViewletQuickPick.css', '/css/parts/InputBox.css', '/css/parts/FileIcon.css']
+
+export const Variables = ['QuickPickBackground', 'ListForeground', 'ListHoverBackground', 'ListHoverForeground', 'ListActiveSelectionBackground']
