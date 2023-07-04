2d1666b94b968139432d3c5e21e975abb4a2a929
Le Vivilet <levivlet@pm.me>
2023-07-04 12:25:09 +0200

    refactor: use virtual dom for title bar buttons

--- packages/renderer-worker/src/parts/ViewletTitleBarButtons/ViewletTitleBarButtonsRender.js
+++ packages/renderer-worker/src/parts/ViewletTitleBarButtons/ViewletTitleBarButtonsRender.js
@@ -1,23 +1,15 @@
-import * as RenderMethod from '../RenderMethod/RenderMethod.js'
+import * as GetTitleBarButtonsVirtualDom from '../GetTitleBarButtonsVirtualDom/GetTitleBarButtonsVirtualDom.js'
 
 export const hasFunctionalRender = true
 
 const renderTitleBarButtons = {
   isEqual(oldState, newState) {
-    return oldState.titleBarButtons === newState.titleBarButtons
+    return oldState.buttons === newState.buttons
   },
   apply(oldState, newState) {
-    return [/* method */ RenderMethod.SetButtons, /* titleBarEntries */ newState.titleBarButtons]
+    const dom = GetTitleBarButtonsVirtualDom.getTitleBarButtonsVirtualDom(newState.buttons)
+    return ['setDom', dom]
   },
 }
 
-const renderMaximized = {
-  isEqual(oldState, newState) {
-    return oldState.isMaximized === newState.isMaximized
-  },
-  apply(oldState, newState) {
-    return [/* method */ RenderMethod.SetMaximized, /* titleBarEntries */ newState.isMaximized]
-  },
-}
-
-export const render = [renderTitleBarButtons, renderMaximized]
+export const render = [renderTitleBarButtons]
