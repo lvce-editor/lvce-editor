2eaf15863cb69901e8f49e5295124438007ca4b1
Le Vivilet <levivlet@pm.me>
2023-07-04 08:55:30 +0200

    feature: use electron utility process for shared process for direct connection

--- packages/main-process/src/parts/ConnectIpcElectronUtilityProcess/ConnectIpcElectronUtilityProcess.js
+++ packages/main-process/src/parts/ConnectIpcElectronUtilityProcess/ConnectIpcElectronUtilityProcess.js
@@ -1,5 +1,8 @@
 const TransferMessagePortMain = require('../TransferMessagePortMain/TransferMessagePortMain.js')
+const Performance = require('../Performance/Performance.js')
+const PerformanceMarkerType = require('../PerformanceMarkerType/PerformanceMarkerType.js')
 
 exports.connectIpc = async (ipc, browserWindowPort) => {
   await TransferMessagePortMain.transferMessagePortMain(ipc, browserWindowPort)
+  Performance.mark(PerformanceMarkerType.DidStartSharedProcess)
 }
