47add0bc4c3331509c692a988a0e58ed3dda7b83
Le Vivilet <levivlet@pm.me>
2023-07-07 13:58:33 +0200

    fix failing tests

--- packages/renderer-worker/src/parts/Viewlet/Viewlet.js
+++ packages/renderer-worker/src/parts/Viewlet/Viewlet.js
@@ -305,10 +305,11 @@ export const closeWidget = async (id) => {
     if (ElectronBrowserView.isOpen() && id === ViewletElectron.isQuickPickOpen()) {
       return ViewletElectron.closeWidgetElectronQuickPick()
     }
-    const child = ViewletStates.getState(id)
-    if (!child) {
+    const childInstance = ViewletStates.getInstance(id)
+    if (!childInstance) {
       return
     }
+    const child = childInstance.state
     const childUid = child.uid
     const commands = disposeFunctional(childUid)
     await RendererProcess.invoke(/* Viewlet.dispose */ 'Viewlet.sendMultiple', commands)
