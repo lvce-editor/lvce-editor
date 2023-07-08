5a2b2f1a4e3b83cada7537ae885223dc99ec6b9f
Le Vivilet <levivlet@pm.me>
2023-07-08 14:02:55 +0200

    feature: lazyload websocket server and ws module

--- packages/pty-host/src/parts/HandleWebSocket/HandleWebSocket.js
+++ packages/pty-host/src/parts/HandleWebSocket/HandleWebSocket.js
@@ -2,11 +2,11 @@ import * as Assert from '../Assert/Assert.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcChild from '../IpcChild/IpcChild.js'
 import * as IpcChildType from '../IpcChildType/IpcChildType.js'
-import * as WebSocketServer from '../WebSocketServer/WebSocketServer.js'
 
 export const handleWebSocket = async (request, handle) => {
   Assert.object(request)
   Assert.object(handle)
+  const WebSocketServer = await import('../WebSocketServer/WebSocketServer.js')
   const webSocket = await WebSocketServer.handleUpgrade(request, handle)
   webSocket.pause()
   const ipc = await IpcChild.listen({
