5fea649f1d118bbe328992d907c0d1ec2c2dc38d
Le Vivilet <levivlet@pm.me>
2023-07-08 14:40:04 +0200

    feature: remove type-fest dependency from main-process during build

--- build/src/parts/BundleMainProcessDependencies/BundleMainProcessDependencies.js
+++ build/src/parts/BundleMainProcessDependencies/BundleMainProcessDependencies.js
@@ -1,32 +1,10 @@
 import * as Copy from '../Copy/Copy.js'
+import * as FilterMainProcessDependencies from '../FilterMainProcessDependencies/FilterMainProcessDependencies.js'
 import * as JsonFile from '../JsonFile/JsonFile.js'
 import * as NodeModulesIgnoredFiles from '../NodeModulesIgnoredFiles/NodeModulesIgnoredFiles.js'
 import * as NpmDependencies from '../NpmDependencies/NpmDependencies.js'
 import * as Path from '../Path/Path.js'
 import * as Platform from '../Platform/Platform.js'
-import * as WalkDependencies from '../WalkDependencies/WalkDependencies.js'
-
-const getNpmDependencies = (rawDependencies, supportsAutoUpdate) => {
-  const dependencyPaths = []
-  const handleDependency = (dependency) => {
-    if (!dependency.path) {
-      return false
-    }
-    if (!dependency.name) {
-      return false
-    }
-    if (dependency.name === 'electron-updater' && !supportsAutoUpdate) {
-      return false
-    }
-    if (dependency.name.includes('@types')) {
-      return false
-    }
-    dependencyPaths.push(dependency.path)
-    return true
-  }
-  WalkDependencies.walkDependencies(rawDependencies, handleDependency)
-  return dependencyPaths.slice(1)
-}
 
 export const bundleMainProcessDependencies = async ({ to, arch, electronVersion, supportsAutoUpdate }) => {
   const mainProcessPath = Path.absolute('packages/main-process')
@@ -40,7 +18,7 @@ export const bundleMainProcessDependencies = async ({ to, arch, electronVersion,
     },
   })
   const npmDependenciesRaw = await NpmDependencies.getNpmDependenciesRawJson('packages/main-process')
-  const npmDependencies = getNpmDependencies(npmDependenciesRaw, supportsAutoUpdate)
+  const npmDependencies = FilterMainProcessDependencies.filterDependencies(npmDependenciesRaw, supportsAutoUpdate)
   for (const dependency of npmDependencies) {
     const dependencyTo = to + dependency.slice(mainProcessPath.length)
     await Copy.copy({
---
+++ build/src/parts/FilterMainProcessDependencies/FilterMainProcessDependencies.js
@@ -0,0 +1,26 @@
+import * as WalkDependencies from '../WalkDependencies/WalkDependencies.js'
+
+export const filterDependencies = (rawDependencies, supportsAutoUpdate) => {
+  const dependencyPaths = []
+  const handleDependency = (dependency) => {
+    if (!dependency.path) {
+      return false
+    }
+    if (!dependency.name) {
+      return false
+    }
+    if (dependency.name === 'electron-updater' && !supportsAutoUpdate) {
+      return false
+    }
+    if (dependency.name.includes('@types')) {
+      return false
+    }
+    if (dependency.name === 'type-fest') {
+      return false
+    }
+    dependencyPaths.push(dependency.path)
+    return true
+  }
+  WalkDependencies.walkDependencies(rawDependencies, handleDependency)
+  return dependencyPaths.slice(1)
+}
