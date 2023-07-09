11bdb4370b77f08529df7a6297f3890e99077840
Le Vivilet <levivlet@pm.me>
2023-07-09 12:47:27 +0200

    feature: remove d.ts files from shared process during build

--- build/src/parts/Remove/Remove.js
+++ build/src/parts/Remove/Remove.js
@@ -1,7 +1,19 @@
+import { glob } from 'glob'
 import { rm } from 'node:fs/promises'
 import * as Path from '../Path/Path.js'
 
 export const remove = async (relativePath) => {
   const absolutePath = Path.absolute(relativePath)
   await rm(absolutePath, { recursive: true, force: true })
+}
+
+export const removeMatching = async (relativePath, pattern) => {
+  const absolutePath = Path.absolute(relativePath)
+  const paths = await glob(pattern, {
+    cwd: absolutePath,
+    absolute: true,
+  })
+  for (const path of paths) {
+    await rm(path)
+  }
 }
