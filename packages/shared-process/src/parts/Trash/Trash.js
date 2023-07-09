efaad062d467220eb7d9f978d9a429962c07e227
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-09 13:20:15 +0200

    feature: use electron's trash function instead of trash npm dependenc… (#3518)

--- packages/shared-process/src/parts/Trash/Trash.js
+++ packages/shared-process/src/parts/Trash/Trash.js
@@ -1,9 +1,17 @@
+import * as IsElectron from '../IsElectron/IsElectron.js'
 import { VError } from '../VError/VError.js'
+
+const getModule = () => {
+  if (IsElectron.isElectron()) {
+    return import('../TrashElectron/TrashElectron.js')
+  }
+  return import('../TrashNode/TrashNode.js')
+}
 
 export const trash = async (path) => {
   try {
-    const { default: _trash } = await import('trash')
-    await _trash(path)
+    const module = await getModule()
+    await module.trash(path)
   } catch (error) {
     throw new VError(error, 'Failed to move item to trash')
   }
