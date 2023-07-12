359ddf0224143a5a666d0f15c45d394d00956992
Le Vivilet <levivlet@pm.me>
2023-07-12 16:02:43 +0200

    remove unused tests

--- packages/renderer-process/test/ViewletEditorCompletion.test.js
+++ packages/renderer-process/test/ViewletEditorCompletion.test.js
@@ -3,46 +3,7 @@
  */
 import * as ViewletEditorCompletion from '../src/parts/ViewletEditorCompletion/ViewletEditorCompletion.js'
 
-const getTextContent = (node) => {
-  return node.textContent
-}
-
 test('create', () => {
   const state = ViewletEditorCompletion.create()
   expect(state).toBeDefined()
-})
-
-test('focusIndex', () => {
-  const state = ViewletEditorCompletion.create()
-  const $CompletionItemOne = document.createElement('li')
-  $CompletionItemOne.className = 'CompletionItem Focused'
-  const $CompletionItemTwo = document.createElement('li')
-  state.$ListItems.append($CompletionItemOne)
-  state.$ListItems.append($CompletionItemTwo)
-  ViewletEditorCompletion.setFocusedIndex(state, 0, 1)
-  expect($CompletionItemOne.classList.contains('Focused')).toBe(false)
-  expect($CompletionItemTwo.classList.contains('Focused')).toBe(true)
-})
-
-test('focusIndex - oldIndex is negative', () => {
-  const state = ViewletEditorCompletion.create()
-  const $CompletionItemOne = document.createElement('li')
-  const $CompletionItemTwo = document.createElement('li')
-  state.$ListItems.append($CompletionItemOne)
-  state.$ListItems.append($CompletionItemTwo)
-  ViewletEditorCompletion.setFocusedIndex(state, -1, 1)
-  expect($CompletionItemOne.classList.contains('Focused')).toBe(false)
-  expect($CompletionItemTwo.classList.contains('Focused')).toBe(true)
-})
-
-test('focusIndex - newIndex is negative', () => {
-  const state = ViewletEditorCompletion.create()
-  const $CompletionItemOne = document.createElement('li')
-  $CompletionItemOne.className = 'CompletionItem Focused'
-  const $CompletionItemTwo = document.createElement('li')
-  state.$ListItems.append($CompletionItemOne)
-  state.$ListItems.append($CompletionItemTwo)
-  ViewletEditorCompletion.setFocusedIndex(state, 0, -1)
-  expect($CompletionItemOne.classList.contains('Focused')).toBe(false)
-  expect($CompletionItemTwo.classList.contains('Focused')).toBe(false)
 })
