c1450c686b728dc3c75234a5f21b8d51153cfde8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-18 14:39:56 +0200

    fix: session replay (#3560)

--- packages/renderer-worker/src/parts/SessionReplay/SessionReplay.js
+++ packages/renderer-worker/src/parts/SessionReplay/SessionReplay.js
@@ -1,13 +1,12 @@
+import * as Assert from '../Assert/Assert.js'
 import * as Command from '../Command/Command.js'
-import * as ErrorHandling from '../ErrorHandling/ErrorHandling.js'
 import * as IndexedDb from '../IndexedDb/IndexedDb.js'
+import * as Json from '../Json/Json.js'
 import * as Location from '../Location/Location.js'
 import * as RendererProcess from '../RendererProcess/RendererProcess.js'
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
-import * as Assert from '../Assert/Assert.js'
-import { VError } from '../VError/VError.js'
-import * as Json from '../Json/Json.js'
 import * as Timestamp from '../Timestamp/Timestamp.js'
+import { VError } from '../VError/VError.js'
 
 export const state = {
   sessionId: '',
@@ -71,11 +70,10 @@ const DONT_REPLAY = new Set(['Open.openUrl', 'Download.downloadFile'])
 export const replaySession = async (sessionId) => {
   const events = await getEvents(sessionId)
   const originalIpc = RendererProcess.state.ipc
-  const originalSend = originalIpc.send
-  const originalOnMessage = originalIpc.onmessage
+  const originalSend = originalIpc.send.bind(originalIpc)
+  const originalOnMessage = originalIpc.onmessage.bind(originalIpc)
   const wrappedSend = () => {}
-  const wrappedOnMessage = async (event) => {
-    const data = event.data
+  const wrappedOnMessage = async (data) => {
     if (typeof data === 'string') {
       return
     }
@@ -124,10 +122,7 @@ export const replaySession = async (sessionId) => {
   }
   let now = 0
   for (const event of events) {
-    if (
-      event.source === 'to-renderer-process' &&
-      !DONT_REPLAY.has(event.method)
-    ) {
+    if (event.source === 'to-renderer-process' && !DONT_REPLAY.has(event.method)) {
       // console.log(event.timestamp)
       const timeDifference = event.timestamp - now
       await new Promise((resolve, reject) => {
@@ -146,11 +141,7 @@ export const replaySession = async (sessionId) => {
 export const getEvents = async (sessionId) => {
   try {
     const timestamp = Timestamp.now()
-    const events = await IndexedDb.getValuesByIndexName(
-      'session',
-      'sessionId',
-      sessionId
-    )
+    const events = await IndexedDb.getValuesByIndexName('session', 'sessionId', sessionId)
     return events
   } catch (error) {
     throw new VError(error, `failed to get session replay events`)
@@ -162,11 +153,7 @@ export const downloadSession = async () => {
     const sessionId = getSessionId()
     const events = await getEvents(sessionId)
     const fileName = `${sessionId}.json`
-    await Command.execute(
-      /* Download.downloadJson */ 'Download.downloadJson',
-      /* json */ events,
-      /* fileName */ fileName
-    )
+    await Command.execute(/* Download.downloadJson */ 'Download.downloadJson', /* json */ events, /* fileName */ fileName)
   } catch (error) {
     throw new VError(error, 'Failed to download session')
   }
@@ -208,21 +195,10 @@ const wrapIpc = (ipc, name, getData) => {
 }
 
 export const startRecording = () => {
-  RendererProcess.state.ipc = wrapIpc(
-    RendererProcess.state.ipc,
-    'renderer-process',
-    (event) => event.data
-  )
-  SharedProcess.state.ipc = wrapIpc(
-    SharedProcess.state.ipc,
-    'shared-process',
-    (event) => event
-  )
+  RendererProcess.state.ipc = wrapIpc(RendererProcess.state.ipc, 'renderer-process', (event) => event.data)
+  SharedProcess.state.ipc = wrapIpc(SharedProcess.state.ipc, 'shared-process', (event) => event)
 }
 
 export const openSession = async () => {
-  await Command.execute(
-    /* Main.openUri */ 'Main.openUri',
-    /* uri */ 'app://session.json'
-  )
+  await Command.execute(/* Main.openUri */ 'Main.openUri', /* uri */ 'app://session.json')
 }
