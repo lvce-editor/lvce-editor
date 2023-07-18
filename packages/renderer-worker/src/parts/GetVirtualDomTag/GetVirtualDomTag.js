f5de88df577d9f78a5122a48ac6c792309f4a4c3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-18 21:40:03 +0200

    refactor: use enum for element tags (#3571)

--- packages/renderer-worker/src/parts/GetVirtualDomTag/GetVirtualDomTag.js
+++ packages/renderer-worker/src/parts/GetVirtualDomTag/GetVirtualDomTag.js
@@ -1,18 +1,19 @@
+import * as ElementTags from '../ElementTags/ElementTags.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 
 export const getVirtualDomTag = (text) => {
   switch (text) {
-    case 'h1':
+    case ElementTags.H1:
       return VirtualDomElements.H1
-    case 'h2':
+    case ElementTags.H2:
       return VirtualDomElements.H2
-    case 'h3':
+    case ElementTags.H3:
       return VirtualDomElements.H3
-    case 'h4':
+    case ElementTags.H4:
       return VirtualDomElements.H4
-    case 'h5':
+    case ElementTags.H5:
       return VirtualDomElements.H5
-    case 'div':
+    case ElementTags.Div:
       return VirtualDomElements.Div
     default:
       return VirtualDomElements.Div
