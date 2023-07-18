38bd3de015ebb50d2f89c70539a5ae35e3b9ab6e
Le Vivilet <levivlet@pm.me>
2023-07-18 23:42:39 +0200

    add back code

--- packages/renderer-worker/test/ParseHtml.test.js
+++ packages/renderer-worker/test/ParseHtml.test.js
@@ -32,6 +32,21 @@ test('element with id', () => {
   ])
 })
 
+test('element with with image', () => {
+  expect(ParseHtml.parseHtml('<p><img alt="demo" src="./demo.png"></p>')).toEqual([
+    {
+      type: VirtualDomElements.P,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Img,
+      childCount: 0,
+      alt: 'demo',
+      src: './demo.png',
+    },
+  ])
+})
+
 test('element with disallowed attribute', () => {
   const html = '<h1 onerror="alert(1)"></h1>'
   const allowedAttributes = []
