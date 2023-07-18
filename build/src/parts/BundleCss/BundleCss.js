d0e703b2f1f49950286ad5208fb02ed714a61ce4
Le Vivilet <levivlet@pm.me>
2023-07-18 15:42:04 +0200

    fix build

--- build/src/parts/BundleCss/BundleCss.js
+++ build/src/parts/BundleCss/BundleCss.js
@@ -1,13 +1,14 @@
+import { existsSync } from 'node:fs'
 import { readdir, readFile, rm } from 'node:fs/promises'
-import { join } from 'node:path'
+import VError from 'verror'
 import * as Copy from '../Copy/Copy.js'
+import * as EagerLoadedCss from '../EagerLoadedCss/EagerLoadedCss.js'
 import * as EncodingType from '../EncodingType/EncodingType.js'
 import * as Path from '../Path/Path.js'
 import * as Replace from '../Replace/Replace.js'
 import * as Root from '../Root/Root.js'
 import * as SplitLines from '../SplitLines/SplitLines.js'
 import * as WriteFile from '../WriteFile/WriteFile.js'
-import * as EagerLoadedCss from '../EagerLoadedCss/EagerLoadedCss.js'
 
 const getParts = (appCss) => {
   const lines = SplitLines.splitLines(appCss)
@@ -26,89 +27,97 @@ const toSorted = (array) => {
 }
 
 export const bundleCss = async ({ outDir, additionalCss = '', assetDir = '', pathPrefix = '' }) => {
-  let css = ``
-  const cssLibNormalize = join(Root.root, 'static', 'lib-css', 'modern-normalize.css')
-  if (additionalCss) {
-    css += additionalCss
-    css += '\n\n'
-  }
-  const cssLibTermTerm = join(Root.root, 'static', 'lib-css', 'termterm.css')
-  css += await readFile(cssLibNormalize, EncodingType.Utf8)
-  css += '\n'
-  css += await readFile(cssLibTermTerm, EncodingType.Utf8)
-  css += '\n'
-  const appCss = await readFile(join(Root.root, 'static', 'css', 'App.css'), EncodingType.Utf8)
-  const parts = getParts(appCss)
-  const cwd = join(Root.root, 'static', 'css', 'parts')
-  const dirents = await readdir(cwd)
-  const sortedDirents = toSorted(dirents)
-  for (const dirent of sortedDirents) {
-    if (parts.includes(dirent)) {
-      // ignore
-    } else {
-      await Copy.copy({
-        from: `static/css/parts/${dirent}`,
-        to: Path.join(outDir, 'parts', dirent),
-      })
+  try {
+    console.log({ outDir })
+    let css = ``
+    const cssLibNormalize = Path.join(Root.root, 'static', 'lib-css', 'modern-normalize.css')
+    if (additionalCss) {
+      css += additionalCss
+      css += '\n\n'
     }
+    const cssLibTermTerm = Path.join(Root.root, 'static', 'lib-css', 'termterm.css')
+    css += await readFile(cssLibNormalize, EncodingType.Utf8)
+    css += '\n'
+    css += await readFile(cssLibTermTerm, EncodingType.Utf8)
+    css += '\n'
+    const appCss = await readFile(Path.join(Root.root, 'static', 'css', 'App.css'), EncodingType.Utf8)
+    const parts = getParts(appCss)
+    const cwd = Path.join(Root.root, 'static', 'css', 'parts')
+    const dirents = await readdir(cwd)
+    const sortedDirents = toSorted(dirents)
+    for (const dirent of sortedDirents) {
+      if (parts.includes(dirent)) {
+        // ignore
+      } else {
+        await Copy.copy({
+          from: `static/css/parts/${dirent}`,
+          to: Path.join(outDir, 'parts', dirent),
+        })
+      }
+    }
+
+    for (const part of parts) {
+      const absolutePath = Path.join(cwd, part)
+      const content = await readFile(absolutePath, EncodingType.Utf8)
+      css += `/*************/\n`
+      css += `/* ${part} */\n`
+      css += `/*************/\n`
+      css += content
+      console.log('exists', existsSync(absolutePath), absolutePath)
+    }
+
+    await Replace.replace({
+      path: Path.join(outDir, 'parts', 'ViewletTitleBarButtons.css'),
+      occurrence: `url(/icons/`,
+      replacement: `url(${assetDir}/icons/`,
+    })
+    await Replace.replace({
+      path: Path.join(outDir, 'parts', 'EditorTabs.css'),
+      occurrence: `url(/icons/`,
+      replacement: `url(${assetDir}/icons/`,
+    })
+    await Replace.replace({
+      path: Path.join(outDir, 'parts', 'ViewletTitleBarIcon.css'),
+      occurrence: `url(/icons/`,
+      replacement: `url(${assetDir}/icons/`,
+    })
+    await Replace.replace({
+      path: Path.join(outDir, 'parts', 'Symbol.css'),
+      occurrence: `url(/icons/`,
+      replacement: `url(${assetDir}/icons/`,
+    })
+
+    for (const part of EagerLoadedCss.eagerLoadedCss) {
+      const absoluteOutDir = Path.absolute(outDir)
+      const absolutePath = Path.join(absoluteOutDir, 'parts', part)
+      const content = await readFile(absolutePath, EncodingType.Utf8)
+      css += `/*************/\n`
+      css += `/* ${part} */\n`
+      css += `/*************/\n`
+      css += content
+      await rm(absolutePath, { recursive: true, force: true })
+    }
+
+    const appCssPath = Path.join(outDir, 'App.css')
+
+    await WriteFile.writeFile({
+      to: appCssPath,
+      content: css,
+    })
+
+    await Replace.replace({
+      path: appCssPath,
+      occurrence: `url(/icons/`,
+      replacement: `url(${assetDir}/icons/`,
+    })
+
+    await Replace.replace({
+      path: appCssPath,
+      occurrence: `url(/fonts/`,
+      replacement: `url(${assetDir}/fonts/`,
+    })
+  } catch (error) {
+    // @ts-ignore
+    throw new VError(error, `Failed to bundle css`)
   }
-
-  for (const part of parts) {
-    const absolutePath = join(cwd, part)
-    const content = await readFile(absolutePath, EncodingType.Utf8)
-    css += `/*************/\n`
-    css += `/* ${part} */\n`
-    css += `/*************/\n`
-    css += content
-  }
-
-  await Replace.replace({
-    path: Path.join(outDir, 'parts', 'ViewletTitleBarButtons.css'),
-    occurrence: `url(/icons/`,
-    replacement: `url(${assetDir}/icons/`,
-  })
-  await Replace.replace({
-    path: Path.join(outDir, 'parts', 'EditorTabs.css'),
-    occurrence: `url(/icons/`,
-    replacement: `url(${assetDir}/icons/`,
-  })
-  await Replace.replace({
-    path: Path.join(outDir, 'parts', 'ViewletTitleBarIcon.css'),
-    occurrence: `url(/icons/`,
-    replacement: `url(${assetDir}/icons/`,
-  })
-  await Replace.replace({
-    path: Path.join(outDir, 'parts', 'Symbol.css'),
-    occurrence: `url(/icons/`,
-    replacement: `url(${assetDir}/icons/`,
-  })
-
-  for (const part of EagerLoadedCss.eagerLoadedCss) {
-    const absolutePath = join(outDir, 'parts', part)
-    const content = await readFile(absolutePath, EncodingType.Utf8)
-    css += `/*************/\n`
-    css += `/* ${part} */\n`
-    css += `/*************/\n`
-    css += content
-    await rm(absolutePath, { recursive: true, force: true })
-  }
-
-  const appCssPath = Path.join(outDir, 'App.css')
-
-  await WriteFile.writeFile({
-    to: appCssPath,
-    content: css,
-  })
-
-  await Replace.replace({
-    path: appCssPath,
-    occurrence: `url(/icons/`,
-    replacement: `url(${assetDir}/icons/`,
-  })
-
-  await Replace.replace({
-    path: appCssPath,
-    occurrence: `url(/fonts/`,
-    replacement: `url(${assetDir}/fonts/`,
-  })
 }
--- build/src/parts/BundleRendererWorker/BundleRendererWorker.js
+++ build/src/parts/BundleRendererWorker/BundleRendererWorker.js
@@ -1,10 +1,11 @@
+import { readFile, writeFile } from 'fs/promises'
+import VError from 'verror'
 import * as BundleJs from '../BundleJs/BundleJs.js'
 import * as Copy from '../Copy/Copy.js'
-import * as Path from '../Path/Path.js'
-import * as Replace from '../Replace/Replace.js'
 import * as EagerLoadedCss from '../EagerLoadedCss/EagerLoadedCss.js'
 import * as GetCssDeclarationFiles from '../GetCssDeclarationFiles/GetCssDeclarationFiles.js'
-import { readFile, writeFile } from 'fs/promises'
+import * as Path from '../Path/Path.js'
+import * as Replace from '../Replace/Replace.js'
 
 const isEagerLoaded = (cssDeclaration) => {
   for (const eagerLoaded of EagerLoadedCss.eagerLoadedCss) {
@@ -45,57 +46,62 @@ const getNewCssDeclarionFile = (content, filteredCss) => {
 }
 
 export const bundleRendererWorker = async ({ cachePath, platform, commitHash, assetDir }) => {
-  await Copy.copy({
-    from: 'packages/renderer-worker/src',
-    to: Path.join(cachePath, 'src'),
-  })
-  const cssDeclarationFiles = await GetCssDeclarationFiles.getCssDeclarationFiles(cachePath)
-  for (const file of cssDeclarationFiles) {
-    const module = await import(file)
-    const Css = module.Css
-    if (Css) {
-      const content = await readFile(file, 'utf8')
-      const filteredDeclarations = getFilteredCssDeclarations(Css)
-      const newContent = getNewCssDeclarionFile(content, filteredDeclarations)
-      await writeFile(file, newContent)
-    }
-  }
-  await Copy.copy({
-    from: 'static/js',
-    to: Path.join(cachePath, 'static', 'js'),
-  })
-  for (const file of ['PrettyBytes', 'BabelParser', 'Blob', 'Base64', 'Ajax', 'Markdown', 'IndexedDb']) {
-    await Replace.replace({
-      path: `${cachePath}/src/parts/${file}/${file}.js`,
-      occurrence: `../../../../../static/`,
-      replacement: `../../../static/`,
+  try {
+    await Copy.copy({
+      from: 'packages/renderer-worker/src',
+      to: Path.join(cachePath, 'src'),
     })
-  }
-  await Replace.replace({
-    path: `${cachePath}/src/parts/Platform/Platform.js`,
-    occurrence: `ASSET_DIR`,
-    replacement: `'${assetDir}'`,
-  })
-  await Replace.replace({
-    path: `${cachePath}/src/parts/Platform/Platform.js`,
-    occurrence: 'export const platform = getPlatform()',
-    replacement: `export const platform = '${platform}'`,
-  })
-  await Replace.replace({
-    path: `${cachePath}/src/parts/Platform/Platform.js`,
-    occurrence: '/packages/extension-host-worker/src/extensionHostWorkerMain.js',
-    replacement: `/packages/extension-host-worker/dist/extensionHostWorkerMain.js`,
-  })
-  await BundleJs.bundleJs({
-    cwd: cachePath,
-    from: `./src/rendererWorkerMain.js`,
-    platform: 'webworker',
-  })
-  // workaround for firefox bug
-  await Replace.replace({
-    path: `${cachePath}/dist/rendererWorkerMain.js`,
-    occurrence: `//# sourceMappingURL`,
-    replacement: `export {}
+    const cssDeclarationFiles = await GetCssDeclarationFiles.getCssDeclarationFiles(cachePath)
+    for (const file of cssDeclarationFiles) {
+      const module = await import(file)
+      const Css = module.Css
+      if (Css) {
+        const content = await readFile(file, 'utf8')
+        const filteredDeclarations = getFilteredCssDeclarations(Css)
+        const newContent = getNewCssDeclarionFile(content, filteredDeclarations)
+        await writeFile(file, newContent)
+      }
+    }
+    await Copy.copy({
+      from: 'static/js',
+      to: Path.join(cachePath, 'static', 'js'),
+    })
+    for (const file of ['PrettyBytes', 'BabelParser', 'Blob', 'Base64', 'Ajax', 'Markdown', 'IndexedDb']) {
+      await Replace.replace({
+        path: `${cachePath}/src/parts/${file}/${file}.js`,
+        occurrence: `../../../../../static/`,
+        replacement: `../../../static/`,
+      })
+    }
+    await Replace.replace({
+      path: `${cachePath}/src/parts/Platform/Platform.js`,
+      occurrence: `ASSET_DIR`,
+      replacement: `'${assetDir}'`,
+    })
+    await Replace.replace({
+      path: `${cachePath}/src/parts/Platform/Platform.js`,
+      occurrence: 'export const platform = getPlatform()',
+      replacement: `export const platform = '${platform}'`,
+    })
+    await Replace.replace({
+      path: `${cachePath}/src/parts/Platform/Platform.js`,
+      occurrence: '/packages/extension-host-worker/src/extensionHostWorkerMain.js',
+      replacement: `/packages/extension-host-worker/dist/extensionHostWorkerMain.js`,
+    })
+    await BundleJs.bundleJs({
+      cwd: cachePath,
+      from: `./src/rendererWorkerMain.js`,
+      platform: 'webworker',
+    })
+    // workaround for firefox bug
+    await Replace.replace({
+      path: `${cachePath}/dist/rendererWorkerMain.js`,
+      occurrence: `//# sourceMappingURL`,
+      replacement: `export {}
 //# sourceMappingURL`,
-  })
+    })
+  } catch (error) {
+    // @ts-ignore
+    throw new VError(error, `Failed to bundle renderer worker`)
+  }
 }
