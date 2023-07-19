b4b3bed35b3c2f69063eef2a8fc26e96872ca447
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-19 23:21:59 +0200

    refactor: move addPath function into separate file (#3606)

---
+++ packages/renderer-worker/src/parts/AddPath/AddPath.js
@@ -0,0 +1,11 @@
+import * as Assert from '../Assert/Assert.js'
+
+export const addPath = (paths, path) => {
+  Assert.array(paths)
+  Assert.string(path)
+  const index = paths.indexOf(path)
+  if (index === -1) {
+    return [path, ...paths]
+  }
+  return [path, ...paths.slice(0, index), ...paths.slice(index + 1)]
+}
--- packages/renderer-worker/src/parts/RecentlyOpened/RecentlyOpened.js
+++ packages/renderer-worker/src/parts/RecentlyOpened/RecentlyOpened.js
@@ -1,12 +1,13 @@
+import * as AddPath from '../AddPath/AddPath.js'
 import * as FileSystem from '../FileSystem/FileSystem.js'
 import * as GetRecentlyOpened from '../GetRecentlyOpened/GetRecentlyOpened.js'
 import * as GlobalEventBus from '../GlobalEventBus/GlobalEventBus.js'
 import * as Json from '../Json/Json.js'
 import * as Platform from '../Platform/Platform.js'
+import * as PlatformType from '../PlatformType/PlatformType.js'
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
 import * as SharedProcessCommandType from '../SharedProcessCommandType/SharedProcessCommandType.js'
 import * as Workspace from '../Workspace/Workspace.js'
-import * as PlatformType from '../PlatformType/PlatformType.js'
 
 export const getRecentlyOpened = GetRecentlyOpened.getRecentlyOpened
 
@@ -19,17 +20,9 @@ export const clearRecentlyOpened = async () => {
   await setRecentlyOpened([])
 }
 
-const getNewRecentlyOpened = (recentlyOpened, path) => {
-  const index = recentlyOpened.indexOf(path)
-  if (index === -1) {
-    return [path, ...recentlyOpened]
-  }
-  return [path, ...recentlyOpened.slice(0, index), ...recentlyOpened.slice(index + 1)]
-}
-
 const addToRecentlyOpenedWeb = async (path) => {
   const recentlyOpened = await getRecentlyOpened()
-  const newRecentlyOpened = getNewRecentlyOpened(recentlyOpened, path)
+  const newRecentlyOpened = AddPath.addPath(recentlyOpened, path)
   await setRecentlyOpened(newRecentlyOpened)
 }
 
