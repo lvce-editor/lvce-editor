11e3cbda5d3da3c00c20bb78591f5bfdf63abd14
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-20 21:31:35 +0200

    refactor: simplify getFirstNodeWorkerEvent function (#3623)

--- packages/shared-process/src/parts/GetFirstNodeWorkerEvent/GetFirstNodeWorkerEvent.js
+++ packages/shared-process/src/parts/GetFirstNodeWorkerEvent/GetFirstNodeWorkerEvent.js
@@ -1,20 +1,9 @@
 import * as FirstNodeWorkerEventType from '../FirstNodeWorkerEventType/FirstNodeWorkerEventType.js'
+import * as GetFirstEvent from '../GetFirstEvent/GetFirstEvent.js'
 
-export const getFirstNodeWorkerEvent = async (worker) => {
-  const { type, event } = await new Promise((resolve, reject) => {
-    const cleanup = (value) => {
-      worker.off('exit', handleExit)
-      worker.off('error', handleError)
-      resolve(value)
-    }
-    const handleExit = (event) => {
-      cleanup({ type: FirstNodeWorkerEventType.Exit, event })
-    }
-    const handleError = (event) => {
-      cleanup({ type: FirstNodeWorkerEventType.Error, event })
-    }
-    worker.on('exit', handleExit)
-    worker.on('error', handleError)
+export const getFirstNodeWorkerEvent = (worker) => {
+  return GetFirstEvent.getFirstEvent(worker, {
+    exit: FirstNodeWorkerEventType.Exit,
+    error: FirstNodeWorkerEventType.Error,
   })
-  return { type, event }
 }
