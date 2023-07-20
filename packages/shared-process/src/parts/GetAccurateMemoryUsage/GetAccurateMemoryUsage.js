aeb3dc923901602f47299777d6e0a7d63bc558f5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-21 00:44:30 +0200

    refactor: simplify getAccurateMemoryUsage function (#3620)

--- packages/shared-process/src/parts/GetAccurateMemoryUsage/GetAccurateMemoryUsage.js
+++ packages/shared-process/src/parts/GetAccurateMemoryUsage/GetAccurateMemoryUsage.js
@@ -7,25 +7,37 @@ import * as IsEnoentError from '../IsEnoentError/IsEnoentError.js'
 import * as IsEsrchError from '../IsEsrchError/IsEsrchError.js'
 import { VError } from '../VError/VError.js'
 
-export const getAccurateMemoryUsage = async (pid) => {
-  Assert.number(pid)
+const getContent = async (pid) => {
   try {
     const filePath = join('/proc', `${pid}`, 'statm')
-    let content
-    try {
-      content = await readFile(filePath, EncodingType.Utf8)
-    } catch (error) {
-      if (IsEnoentError.isEnoentError(error) || IsEsrchError.isEsrchError(error)) {
-        return -1
-      }
-      throw error
+    const content = await readFile(filePath, EncodingType.Utf8)
+    return content
+  } catch (error) {
+    if (IsEnoentError.isEnoentError(error) || IsEsrchError.isEsrchError(error)) {
+      return ''
     }
-    const trimmedContent = content.trim()
-    const numberBlocks = trimmedContent.split(Character.Space)
-    const pageSize = 4096
-    const rss = Number.parseInt(numberBlocks[1]) * pageSize
-    const shared = Number.parseInt(numberBlocks[2]) * pageSize
-    const memory = rss - shared
+    throw error
+  }
+}
+
+const parseMemory = (content) => {
+  const trimmedContent = content.trim()
+  const numberBlocks = trimmedContent.split(Character.Space)
+  const pageSize = 4096
+  const rss = Number.parseInt(numberBlocks[1]) * pageSize
+  const shared = Number.parseInt(numberBlocks[2]) * pageSize
+  const memory = rss - shared
+  return memory
+}
+
+export const getAccurateMemoryUsage = async (pid) => {
+  try {
+    Assert.number(pid)
+    const content = await getContent(pid)
+    if (!content) {
+      return -1
+    }
+    const memory = parseMemory(content)
     return memory
   } catch (error) {
     throw new VError(error, 'Failed to get accurate memory usage')
