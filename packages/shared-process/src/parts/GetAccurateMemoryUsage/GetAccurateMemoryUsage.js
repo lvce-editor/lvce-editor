52a209f5e3cf72b19654c4116a4757c4051580bf
Le Vivilet <levivlet@pm.me>
2023-07-20 20:58:17 +0200

    refactor: simplify getAccurateMemoryUsage function

--- packages/shared-process/src/parts/GetAccurateMemoryUsage/GetAccurateMemoryUsage.js
+++ packages/shared-process/src/parts/GetAccurateMemoryUsage/GetAccurateMemoryUsage.js
@@ -3,34 +3,41 @@ import { join } from 'node:path'
 import * as Assert from '../Assert/Assert.js'
 import * as Character from '../Character/Character.js'
 import * as EncodingType from '../EncodingType/EncodingType.js'
-import * as ErrorCodes from '../ErrorCodes/ErrorCodes.js'
+import * as IsEnoentError from '../IsEnoentError/IsEnoentError.js'
+import * as IsEsrchError from '../IsEsrchError/IsEsrchError.js'
 import { VError } from '../VError/VError.js'
 
-export const getAccurateMemoryUsage = async (pid) => {
-  Assert.number(pid)
+const getContent = async (pid) => {
   try {
     const filePath = join('/proc', `${pid}`, 'statm')
-    let content
-    try {
-      content = await readFile(filePath, EncodingType.Utf8)
-    } catch (error) {
-      if (
-        error &&
-        // @ts-ignore
-        (error.code === ErrorCodes.ENOENT ||
-          // @ts-ignore
-          error.code === ErrorCodes.ESRCH)
-      ) {
-        return -1
-      }
-      throw error
+    const content = await readFile(filePath, EncodingType.Utf8)
+    return content
+  } catch (error) {
+    if (IsEnoentError.isEnoentError(error) || IsEsrchError.isEsrchError(error)) {
+      return ''
     }
-    const trimmedContent = content.trim()
-    const numberBlocks = trimmedContent.split(Character.Space)
-    const pageSize = 4096
-    const rss = Number.parseInt(numberBlocks[1]) * pageSize
-    const shared = Number.parseInt(numberBlocks[2]) * pageSize
-    const memory = rss - shared
+    throw error
+  }
+}
+
+const parseMemory = (content) => {
+  const trimmedContent = content.trim()
+  const numberBlocks = trimmedContent.split(Character.Space)
+  const pageSize = 4096
+  const rss = Number.parseInt(numberBlocks[1]) * pageSize
+  const shared = Number.parseInt(numberBlocks[2]) * pageSize
+  const memory = rss - shared
+  return memory
+}
+
+export const getAccurateMemoryUsage = async (pid) => {
+  try {
+    Assert.number(pid)
+    const content = await getContent(pid)
+    if (!content) {
+      return -1
+    }
+    const memory = parseMemory(content)
     return memory
   } catch (error) {
     throw new VError(error, 'Failed to get accurate memory usage')
