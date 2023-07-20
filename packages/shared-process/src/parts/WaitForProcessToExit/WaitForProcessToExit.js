ad151fa23a5c7256a09ab4234b6acfcfedad92f6
Le Vivilet <levivlet@pm.me>
2023-07-21 00:46:09 +0200

    fix failing test

--- packages/shared-process/src/parts/WaitForProcessToExit/WaitForProcessToExit.js
+++ packages/shared-process/src/parts/WaitForProcessToExit/WaitForProcessToExit.js
@@ -4,6 +4,6 @@ import * as GetFirstEvent from '../GetFirstEvent/GetFirstEvent.js'
 export const waitForProcessToExit = async (childProcess) => {
   return GetFirstEvent.getFirstEvent(childProcess, {
     error: ProcessExitEventType.Error,
-    exit: ProcessExitEventType.Exit,
+    close: ProcessExitEventType.Exit,
   })
 }
