d5caee6a3328bd68b69151bfb5a6aba4fa215590
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-20 16:53:13 +0200

    fix: electron rebuild path on windows (#3615)

--- packages/shared-process/src/parts/GetElectronRebuildPath/GetElectronRebuildPath.js
+++ packages/shared-process/src/parts/GetElectronRebuildPath/GetElectronRebuildPath.js
@@ -1,6 +1,10 @@
+import * as IsElectron from '../IsElectron/IsElectron.js'
 import * as Path from '../Path/Path.js'
 import * as Root from '../Root/Root.js'
 
 export const getElectronRebuildPath = () => {
+  if (IsElectron.isElectron()) {
+    return Path.join(Root.root, 'packages', 'main-process', 'node_modules', '.bin', 'electron-rebuild.cmd')
+  }
   return Path.join(Root.root, 'packages', 'main-process', 'node_modules', '.bin', 'electron-rebuild')
 }
