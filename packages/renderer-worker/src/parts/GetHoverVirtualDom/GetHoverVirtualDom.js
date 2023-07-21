09788c27c180a3aa19749628274bdbffbe6736c4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-22 00:43:33 +0200

    fix: multiline code in hover (#3652)

--- packages/renderer-worker/src/parts/GetHoverVirtualDom/GetHoverVirtualDom.js
+++ packages/renderer-worker/src/parts/GetHoverVirtualDom/GetHoverVirtualDom.js
@@ -2,20 +2,39 @@ import * as GetMarkdownVirtualDom from '../GetMarkdownVirtualDom/GetMarkdownVirt
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
 
+const getLineCount = (dom) => {
+  let depth = 1
+  let lineCount = 0
+  for (const element of dom) {
+    depth += element.childCount - 1
+    if (depth === 0) {
+      lineCount++
+    }
+  }
+  lineCount--
+  return lineCount
+}
+
 export const getHoverVirtualDom = (sanitizedHtml, documentation) => {
   const markdownDom = GetMarkdownVirtualDom.getMarkdownVirtualDom(sanitizedHtml)
-  return [
+  const lineCount = getLineCount(markdownDom)
+  const dom = [
     {
       type: VirtualDomElements.Div,
       className: 'HoverDisplayString',
-      childCount: 1,
+      childCount: lineCount,
     },
     ...markdownDom,
-    {
-      type: VirtualDomElements.Div,
-      className: 'HoverDocumentation',
-      childCount: 1,
-    },
-    text(documentation),
   ]
+  if (documentation) {
+    dom.push(
+      {
+        type: VirtualDomElements.Div,
+        className: 'HoverDocumentation',
+        childCount: 1,
+      },
+      text(documentation)
+    )
+  }
+  return dom
 }
