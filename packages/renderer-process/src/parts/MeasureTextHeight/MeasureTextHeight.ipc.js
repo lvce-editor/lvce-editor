b5c579b1adb354cb13dbd73035aa21396ce1635b
Le Vivilet <levivlet@pm.me>
2023-07-21 14:05:22 +0200

    feature: add measureTextHeight function to renderer process

---
+++ packages/renderer-process/src/parts/MeasureTextHeight/MeasureTextHeight.ipc.js
@@ -0,0 +1,7 @@
+import * as MeasureTextHeight from './MeasureTextHeight.js'
+
+export const name = 'MeasureTextHeight'
+
+export const Commands = {
+  measureTextHeight: MeasureTextHeight.measureTextHeight,
+}
