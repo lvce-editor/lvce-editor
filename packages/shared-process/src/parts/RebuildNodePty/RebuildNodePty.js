1ab4ed2e961f817dfea49a10cee2cd5179a934a2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-26 12:26:39 +0200

    refactor: simplify electron/npm rebuild

--- packages/shared-process/src/parts/RebuildNodePty/RebuildNodePty.js
+++ packages/shared-process/src/parts/RebuildNodePty/RebuildNodePty.js
@@ -1,7 +1,3 @@
-import { spawn } from 'node:child_process'
-import * as FirstNodeWorkerEventType from '../FirstNodeWorkerEventType/FirstNodeWorkerEventType.js'
-import * as GetElectronRebuildPath from '../GetElectronRebuildPath/GetElectronRebuildPath.js'
-import * as GetFirstNodeChildProcessEvent from '../GetFirstNodeChildProcessEvent/GetFirstNodeChildProcessEvent.js'
 import * as IsElectron from '../IsElectron/IsElectron.js'
 import * as Path from '../Path/Path.js'
 import * as Root from '../Root/Root.js'
@@ -11,47 +7,18 @@ const getPtyHostPath = () => {
   return Path.join(Root.root, 'packages', 'pty-host')
 }
 
-/**
- * @param {string} cwd
- */
-const rebuildNodePtyElectron = async (cwd) => {
-  const electronRebuildPath = GetElectronRebuildPath.getElectronRebuildPath()
-  const childProcess = spawn(electronRebuildPath, [], {
-    cwd,
-    stdio: 'inherit',
-  })
-  const { type, event } = await GetFirstNodeChildProcessEvent.getFirstNodeChildProcessEvent(childProcess)
-  if (type === FirstNodeWorkerEventType.Error) {
-    throw new Error(`Failed to rebuild native module: ${event}`)
-  }
-}
-
-/**
- * @param {string} cwd
- */
-const rebuildNodePtyNode = async (cwd) => {
-  const childProcess = spawn('npm', ['rebuild'], {
-    cwd,
-    stdio: 'inherit',
-  })
-  const { type, event } = await GetFirstNodeChildProcessEvent.getFirstNodeChildProcessEvent(childProcess)
-  if (type === FirstNodeWorkerEventType.Error) {
-    throw new Error(`Failed to rebuild native module: ${event}`)
-  }
-}
-
-const getFn = () => {
+const getModule = () => {
   if (IsElectron.isElectron()) {
-    return rebuildNodePtyElectron
+    return import('../RebuildForElectron/RebuildForElectron.js')
   }
-  return rebuildNodePtyNode
+  return import('../RebuildForNode/RebuildForNode.js')
 }
 
 export const rebuildNodePty = async () => {
   try {
     const ptyHostPath = getPtyHostPath()
-    const rebuild = getFn()
-    await rebuild(ptyHostPath)
+    const module = await getModule()
+    await module.rebuild(ptyHostPath)
   } catch (error) {
     throw new VError(error, `Failed to rebuild node-pty`)
   }
