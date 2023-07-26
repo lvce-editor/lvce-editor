b953a6a6abaa41c4687aff6771b685269610134e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-26 20:23:11 +0200

    fix: import file on windows: use pathToFileURL

--- packages/extension-host-helper-process/src/parts/ImportScript/ImportScript.js
+++ packages/extension-host-helper-process/src/parts/ImportScript/ImportScript.js
@@ -1,9 +1,11 @@
-import { VError } from '../VError/VError.js'
+import { pathToFileURL } from 'node:url'
 import * as CleanImportError from '../CleanImportError/CleanImportError.js'
+import { VError } from '../VError/VError.js'
 
 export const importScript = async (path) => {
   try {
-    const module = await import(path)
+    const url = pathToFileURL(path).toString()
+    const module = await import(url)
     return module
   } catch (error) {
     const cleanError = CleanImportError.cleanImportError(error)
