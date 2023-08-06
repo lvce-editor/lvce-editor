6634f9f1ca8aa41e4fed419c2bc16566660abb2a
Le Vivilet <levivlet@pm.me>
2023-08-06 11:32:27 +0200

    refactor: use bit flags for activity bar item focused, selected, enabled, progress, tab, button attributes

--- packages/renderer-worker/src/parts/GetVisibleActivityBarItems/GetVisibleActivityBarItems.js
+++ packages/renderer-worker/src/parts/GetVisibleActivityBarItems/GetVisibleActivityBarItems.js
@@ -3,11 +3,33 @@ import * as Icon from '../Icon/Icon.js'
 import { getNumberOfVisibleItems } from '../ViewletActivityBar/ViewletActivityBarGetHiddenItems.js'
 import * as ViewletActivityBarStrings from '../ViewletActivityBar/ViewletActivityBarStrings.js'
 
+const toVisibleItems = (items, selectedIndex, focusedIndex) => {
+  const visibleItems = []
+  for (let i = 0; i < items.length; i++) {
+    const isSelected = i === selectedIndex
+    const isFocused = i === focusedIndex
+    const item = items[i]
+    let flags = item.flags
+    if (isSelected) {
+      flags |= ActivityBarItemFlags.Selected
+    }
+    if (isFocused) {
+      flags |= ActivityBarItemFlags.Focused
+    }
+    visibleItems.push({
+      ...item,
+      flags,
+    })
+  }
+  return visibleItems
+}
+
 export const getVisibleActivityBarItems = (state) => {
   const numberOfVisibleItems = getNumberOfVisibleItems(state)
-  const items = state.activityBarItems
+  const { activityBarItems, selectedIndex, focusedIndex } = state
+  const items = activityBarItems
   if (numberOfVisibleItems >= items.length) {
-    return items
+    return toVisibleItems(items, selectedIndex, focusedIndex)
   }
   const showMoreItem = {
     id: 'Additional Views',
@@ -17,6 +39,6 @@ export const getVisibleActivityBarItems = (state) => {
     flags: ActivityBarItemFlags.Button,
     keyShortCuts: '',
   }
-  const visibleItems = [...items.slice(0, numberOfVisibleItems - 2), showMoreItem, items.at(-1)]
+  const visibleItems = toVisibleItems([...items.slice(0, numberOfVisibleItems - 2), showMoreItem, items.at(-1)], selectedIndex, focusedIndex)
   return visibleItems
 }
