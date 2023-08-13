f27ed632579267f291daf349a35e847c8f31e634
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-08-13 23:49:20 +0200

    feature: add libasound dependency to deb package (#3733)

--- build/src/parts/LinuxDependencies/LinuxDependencies.js
+++ build/src/parts/LinuxDependencies/LinuxDependencies.js
@@ -1,3 +1,13 @@
-export const defaultDepends = ['libnss3 (>= 2:3.26)', 'gnupg', 'apt', 'libxkbfile1', 'libsecret-1-0', 'libgtk-3-0 (>= 3.10.0)', 'libxss1', 'libgbm1']
+export const defaultDepends = [
+  'libasound2 (>= 1.0.17)',
+  'libnss3 (>= 2:3.26)',
+  'gnupg',
+  'apt',
+  'libxkbfile1',
+  'libsecret-1-0',
+  'libgtk-3-0 (>= 3.10.0)',
+  'libxss1',
+  'libgbm1',
+]
 
 export const additionalDepends = ['ripgrep']
