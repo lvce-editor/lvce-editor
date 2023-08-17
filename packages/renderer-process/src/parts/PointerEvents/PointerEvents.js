5271c73641f0e37d8c2f5ff74404071059f9af9e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-08-17 23:43:04 +0200

    refactor: use helper function for start/stop of tracking pointer events (#3749)

---
+++ packages/renderer-process/src/parts/PointerEvents/PointerEvents.js
@@ -0,0 +1,15 @@
+import * as DomEventType from '../DomEventType/DomEventType.js'
+
+export const startTracking = ($Target, pointerId, handlePointerMove, handlePointerUp) => {
+  $Target.setPointerCapture(pointerId)
+  $Target.addEventListener(DomEventType.PointerMove, handlePointerMove)
+  // TODO use pointerlost event instead
+  $Target.addEventListener(DomEventType.PointerUp, handlePointerUp)
+}
+
+export const stopTracking = ($Target, pointerId, handlePointerMove, handlePointerUp) => {
+  $Target.releasePointerCapture(pointerId)
+  $Target.removeEventListener(DomEventType.PointerMove, handlePointerMove)
+  // TODO use pointerlost event instead
+  $Target.removeEventListener(DomEventType.PointerUp, handlePointerUp)
+}
