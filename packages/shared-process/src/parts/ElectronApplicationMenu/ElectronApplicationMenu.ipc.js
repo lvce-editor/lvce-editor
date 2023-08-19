0d3f846cf6054eead20d17e3f151446062a279cf
Le Vivilet <levivlet@pm.me>
2023-08-19 10:58:13 +0200

    refactor: move electron application menu to shared process

---
+++ packages/shared-process/src/parts/ElectronApplicationMenu/ElectronApplicationMenu.ipc.js
@@ -0,0 +1,7 @@
+import * as ElectronApplicationMenu from './ElectronApplicationMenu.js'
+
+export const name = 'ElectronApplicationMenu'
+
+export const Commands = {
+  setItems: ElectronApplicationMenu.setItems,
+}
