7bfdb1621f932c7c0541c0668303394239f46d8b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-08-19 11:10:04 +0200

    refactor: move safeStorage module to shared process (#3755)

--- packages/renderer-worker/src/parts/ElectronSafeStorage/ElectronSafeStorage.js
+++ packages/renderer-worker/src/parts/ElectronSafeStorage/ElectronSafeStorage.js
@@ -1,13 +1,13 @@
-import * as ElectronProcess from '../ElectronProcess/ElectronProcess.js'
+import * as SharedProcess from '../SharedProcess/SharedProcess.js'
 
 export const isEncryptionAvailable = () => {
-  return ElectronProcess.invoke('ElectronSafeStorage.isEncryptionAvailable')
+  return SharedProcess.invoke('ElectronSafeStorage.isEncryptionAvailable')
 }
 
 export const encryptString = (plainText) => {
-  return ElectronProcess.invoke('ElectronSafeStorage.encryptString', plainText)
+  return SharedProcess.invoke('ElectronSafeStorage.encryptString', plainText)
 }
 
 export const decryptString = (encrypted) => {
-  return ElectronProcess.invoke('ElectronSafeStorage.decryptString', encrypted)
+  return SharedProcess.invoke('ElectronSafeStorage.decryptString', encrypted)
 }
--- packages/renderer-worker/test/ElectronSafeStorage.test.js
+++ packages/renderer-worker/test/ElectronSafeStorage.test.js
@@ -4,88 +4,67 @@ beforeEach(() => {
   jest.resetAllMocks()
 })
 
-jest.unstable_mockModule(
-  '../src/parts/ElectronProcess/ElectronProcess.js',
-  () => {
-    return {
-      invoke: jest.fn(() => {
-        throw new Error('not implemented')
-      }),
-    }
+jest.unstable_mockModule('../src/parts/SharedProcess/SharedProcess.js', () => {
+  return {
+    invoke: jest.fn(() => {
+      throw new Error('not implemented')
+    }),
   }
-)
+})
 
-const ElectronProcess = await import(
-  '../src/parts/ElectronProcess/ElectronProcess.js'
-)
-const ElectronSafeStorage = await import(
-  '../src/parts/ElectronSafeStorage/ElectronSafeStorage.js'
-)
+const SharedProcess = await import('../src/parts/SharedProcess/SharedProcess.js')
+const ElectronSafeStorage = await import('../src/parts/ElectronSafeStorage/ElectronSafeStorage.js')
 
 test('isEncryptionAvailable - error', async () => {
   // @ts-ignore
-  ElectronProcess.invoke.mockImplementation(async () => {
+  SharedProcess.invoke.mockImplementation(async () => {
     throw new TypeError('x is not a function')
   })
-  await expect(
-    ElectronSafeStorage.isEncryptionAvailable()
-  ).rejects.toThrowError(new TypeError('x is not a function'))
+  await expect(ElectronSafeStorage.isEncryptionAvailable()).rejects.toThrowError(new TypeError('x is not a function'))
 })
 
 test('isEncryptionAvailable', async () => {
   // @ts-ignore
-  ElectronProcess.invoke.mockImplementation(() => {
+  SharedProcess.invoke.mockImplementation(() => {
     return true
   })
   expect(await ElectronSafeStorage.isEncryptionAvailable()).toBe(true)
-  expect(ElectronProcess.invoke).toHaveBeenCalledTimes(1)
-  expect(ElectronProcess.invoke).toHaveBeenCalledWith(
-    'ElectronSafeStorage.isEncryptionAvailable'
-  )
+  expect(SharedProcess.invoke).toHaveBeenCalledTimes(1)
+  expect(SharedProcess.invoke).toHaveBeenCalledWith('ElectronSafeStorage.isEncryptionAvailable')
 })
 
 test('encryptString - error', async () => {
   // @ts-ignore
-  ElectronProcess.invoke.mockImplementation(async () => {
+  SharedProcess.invoke.mockImplementation(async () => {
     throw new TypeError('x is not a function')
   })
-  await expect(ElectronSafeStorage.encryptString('test')).rejects.toThrowError(
-    new TypeError('x is not a function')
-  )
+  await expect(ElectronSafeStorage.encryptString('test')).rejects.toThrowError(new TypeError('x is not a function'))
 })
 
 test('encryptString', async () => {
   // @ts-ignore
-  ElectronProcess.invoke.mockImplementation(() => {
+  SharedProcess.invoke.mockImplementation(() => {
     return 'encrypted'
   })
   expect(await ElectronSafeStorage.encryptString('test')).toBe('encrypted')
-  expect(ElectronProcess.invoke).toHaveBeenCalledTimes(1)
-  expect(ElectronProcess.invoke).toHaveBeenCalledWith(
-    'ElectronSafeStorage.encryptString',
-    'test'
-  )
+  expect(SharedProcess.invoke).toHaveBeenCalledTimes(1)
+  expect(SharedProcess.invoke).toHaveBeenCalledWith('ElectronSafeStorage.encryptString', 'test')
 })
 
 test('decryptString - error', async () => {
   // @ts-ignore
-  ElectronProcess.invoke.mockImplementation(async () => {
+  SharedProcess.invoke.mockImplementation(async () => {
     throw new TypeError('x is not a function')
   })
-  await expect(
-    ElectronSafeStorage.decryptString('encrypted')
-  ).rejects.toThrowError(new TypeError('x is not a function'))
+  await expect(ElectronSafeStorage.decryptString('encrypted')).rejects.toThrowError(new TypeError('x is not a function'))
 })
 
 test('decryptString', async () => {
   // @ts-ignore
-  ElectronProcess.invoke.mockImplementation(() => {
+  SharedProcess.invoke.mockImplementation(() => {
     return 'test'
   })
   expect(await ElectronSafeStorage.decryptString('encrypted')).toBe('test')
-  expect(ElectronProcess.invoke).toHaveBeenCalledTimes(1)
-  expect(ElectronProcess.invoke).toHaveBeenCalledWith(
-    'ElectronSafeStorage.decryptString',
-    'encrypted'
-  )
+  expect(SharedProcess.invoke).toHaveBeenCalledTimes(1)
+  expect(SharedProcess.invoke).toHaveBeenCalledWith('ElectronSafeStorage.decryptString', 'encrypted')
 })
