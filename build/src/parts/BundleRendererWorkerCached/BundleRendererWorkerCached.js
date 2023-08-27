7173e0cb7d661c09912e800c51dd72bd056ada0b
Le Vivilet <levivlet@pm.me>
2023-08-27 20:45:21 +0200

    feature: bundle language basics extensions during build so that less file accesses are needed for retrieving all languages

--- build/src/parts/BundleRendererWorkerCached/BundleRendererWorkerCached.js
+++ build/src/parts/BundleRendererWorkerCached/BundleRendererWorkerCached.js
@@ -4,25 +4,14 @@ import * as Path from '../Path/Path.js'
 import * as Remove from '../Remove/Remove.js'
 import * as Logger from '../Logger/Logger.js'
 
-export const bundleRendererWorkerCached = async ({
-  commitHash,
-  platform,
-  assetDir,
-}) => {
-  const rendererWorkerCachePath = await CachePaths.getRendererWorkerCachePath([
-    platform,
-    commitHash,
-  ])
+export const bundleRendererWorkerCached = async ({ commitHash, platform, assetDir }) => {
+  const rendererWorkerCachePath = await CachePaths.getRendererWorkerCachePath([platform, commitHash])
   if (existsSync(rendererWorkerCachePath)) {
     Logger.info('[build step skipped] bundleRendererWorker')
   } else {
     console.time('bundleRendererWorker')
-    await Remove.remove(
-      Path.absolute('build/.tmp/cachedSources/renderer-worker')
-    )
-    const BundleRendererWorker = await import(
-      '../BundleRendererWorker/BundleRendererWorker.js'
-    )
+    await Remove.remove(Path.absolute('build/.tmp/cachedSources/renderer-worker'))
+    const BundleRendererWorker = await import('../BundleRendererWorker/BundleRendererWorker.js')
     await BundleRendererWorker.bundleRendererWorker({
       cachePath: rendererWorkerCachePath,
       platform,
