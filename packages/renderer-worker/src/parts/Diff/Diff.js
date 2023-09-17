a6e470f6ad251b7951f828cb003c9fc223bd2990
Le Vivilet <levivlet@pm.me>
2023-09-17 17:38:20 +0200

    feature: clean up diff code

--- packages/renderer-worker/src/parts/Diff/Diff.js
+++ packages/renderer-worker/src/parts/Diff/Diff.js
@@ -1,44 +1,6 @@
 // Based on https://johnresig.com/projects/javascript-diff-algorithm/ by John Resig (License MIT)
 
-const makeMap = (linesA, linesB) => {
-  const map = Object.create(null)
-  // pass one
-  const na = []
-  for (let i = 0; i < linesB.length; i++) {
-    const line = linesB[i]
-    if (line in map) {
-      map[line].nc++
-    } else {
-      map[line] = {
-        oc: 0,
-        nc: 1,
-        olno: -1,
-      }
-    }
-    na.push(map[line])
-  }
-  // pass two
-  const oa = []
-  for (let i = 0; i < linesA.length; i++) {
-    const line = linesA[i]
-    if (line in map) {
-      map[line].oc++
-    } else {
-      map[line] = {
-        oc: 1,
-        nc: 0,
-        olno: 0,
-      }
-    }
-    map[line].olno = i
-    oa.push(map[line])
-  }
-  return {
-    map,
-    oa,
-    na,
-  }
-}
+import * as MakeDiffMap from '../MakeDiffMap/MakeDiffMap.js'
 
 /**
  *
@@ -48,9 +10,7 @@ const makeMap = (linesA, linesB) => {
  */
 export const diff = (linesA, linesB) => {
   // create hashmaps of which line corresponds to which indices
-  const { map, oa, na } = makeMap(linesA, linesB)
-
-  map
+  const { map, oa, na } = MakeDiffMap.makeDiffMap(linesA, linesB)
 
   // pass 3
   for (let i = 0; i < na.length; i++) {
@@ -79,9 +39,6 @@ export const diff = (linesA, linesB) => {
     }
   }
 
-  oa
-  na
-
   const changesRight = []
   const changesLeft = []
 
@@ -105,7 +62,3 @@ export const diff = (linesA, linesB) => {
 
   return { changesLeft, changesRight }
 }
-
-const linesA = ['a', 'd']
-const linesB = ['a', 'b', 'c']
-diff(linesA, linesB) //?
