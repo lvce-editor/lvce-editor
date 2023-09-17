8f9b8b5f85046892a1f639a92390fff25ac89dd9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-09-17 12:03:50 +0200

    feature: remove workaround for firefox es modules error since firefox… (#3829)

--- packages/renderer-process/src/parts/UnhandledErrorHandling/UnhandledErrorHandling.js
+++ packages/renderer-process/src/parts/UnhandledErrorHandling/UnhandledErrorHandling.js
@@ -1,6 +1,5 @@
 import * as ErrorHandling from '../ErrorHandling/ErrorHandling.js'
 import * as IsChromeExtensionError from '../IsChromeExtensionError/IsChromeExtensionError.js'
-import * as IsFirefoxWorkerError from '../IsFirefoxWorkerError/IsFirefoxWorkerError.js'
 
 export const handleUnhandledRejection = (event) => {
   event.preventDefault()
@@ -10,10 +9,6 @@ export const handleUnhandledRejection = (event) => {
 export const handleUnhandledError = (message, filename, lineno, colno, error) => {
   if (IsChromeExtensionError.isChromeExtensionError(message)) {
     // ignore errors from chrome extensions
-    return
-  }
-  if (IsFirefoxWorkerError.isFirefoxWorkerError(message)) {
-    // ignore firefox worker errors
     return
   }
   ErrorHandling.handleError(error, true, '[renderer-process] Unhandled Error: ')
