99709e899a5f15c6e344eff1f6d5f19146bc6aac
Le Vivilet <levivlet@pm.me>
2023-09-17 10:28:52 +0200

    feature: remove workaround for firefox es modules error since firefox now supports module workers

--- packages/renderer-process/src/parts/IsFirefoxWorkerError/IsFirefoxWorkerError.js
+++
@@ -1,6 +0,0 @@
-export const isFirefoxWorkerError = (message) => {
-  return [
-    'SyntaxError: import declarations may only appear at top level of a module',
-    'SyntaxError: export declarations may only appear at top level of a module',
-  ].includes(message)
-}
