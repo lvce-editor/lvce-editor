80426906ce7a722d1f0da78385f79d8ac7cc9b35
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-10-01 16:00:21 +0200

    refactor: simplify getFirstEvent code in renderer worker (#3910)

--- packages/renderer-worker/src/parts/GetFirstEvent/GetFirstEvent.js
+++ packages/renderer-worker/src/parts/GetFirstEvent/GetFirstEvent.js
@@ -1,22 +1,23 @@
-export const getFirstEvent = async (eventTarget, eventMap) => {
-  const { type, event } = await new Promise((resolve, reject) => {
-    const listenerMap = Object.create(null)
-    const cleanup = (value) => {
-      for (const event of Object.keys(eventMap)) {
-        eventTarget.removeEventListener(event, listenerMap[event])
-      }
-      resolve(value)
+import * as Promises from '../Promises/Promises.js'
+
+export const getFirstEvent = (eventTarget, eventMap) => {
+  const { resolve, promise } = Promises.withResolvers()
+  const listenerMap = Object.create(null)
+  const cleanup = (value) => {
+    for (const event of Object.keys(eventMap)) {
+      eventTarget.removeEventListener(event, listenerMap[event])
     }
-    for (const [event, type] of Object.entries(eventMap)) {
-      const listener = (event) => {
-        cleanup({
-          type,
-          event,
-        })
-      }
-      eventTarget.addEventListener(event, listener)
-      listenerMap[event] = listener
+    resolve(value)
+  }
+  for (const [event, type] of Object.entries(eventMap)) {
+    const listener = (event) => {
+      cleanup({
+        type,
+        event,
+      })
     }
-  })
-  return { type, event }
+    eventTarget.addEventListener(event, listener)
+    listenerMap[event] = listener
+  }
+  return promise
 }
