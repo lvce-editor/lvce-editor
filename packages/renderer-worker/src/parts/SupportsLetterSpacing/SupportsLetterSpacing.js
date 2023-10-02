f9cf0e76b1697dcba0c3e9e7468b7adc6b6652a8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-10-02 08:31:10 +0200

    feature: remove supportsLetterSpacing check, since letterspacing is n… (#3922)

--- packages/renderer-worker/src/parts/SupportsLetterSpacing/SupportsLetterSpacing.js
+++ packages/renderer-worker/src/parts/SupportsLetterSpacing/SupportsLetterSpacing.js
@@ -1,20 +1,3 @@
-import * as IsFirefox from '../IsFirefox/IsFirefox.js'
-import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.js'
-
-// workaround for firefox not supporting letterspacing https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/letterSpacing
 export const supportsLetterSpacing = () => {
-  if (!IsFirefox.isFirefox) {
-    return true
-  }
-  const text = 'abc'
-  const fontWeight = 100
-  const fontSize = 16
-  const fontFamily = 'serif'
-  const noLetterSpacing = 0
-  const largeLetterSpacing = 100
-  const isMonospaceFont = false
-  const charWidth = 0
-  const normalTextWidth = MeasureTextWidth.measureTextWidth(text, fontWeight, fontSize, fontFamily, noLetterSpacing, isMonospaceFont, charWidth)
-  const largeTextWidth = MeasureTextWidth.measureTextWidth(text, fontWeight, fontSize, fontFamily, largeLetterSpacing, isMonospaceFont, charWidth)
-  return normalTextWidth !== largeTextWidth
+  return true
 }
