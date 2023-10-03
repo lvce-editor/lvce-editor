bce3dbada17eaa4372b102292e74cb95832aedfd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-10-03 08:20:32 +0200

    refactor: remove unused ts-ignore (#3935)

--- packages/shared-process/src/parts/LoadWindowsProcessTree/LoadWindowsProcessTree.js
+++ packages/shared-process/src/parts/LoadWindowsProcessTree/LoadWindowsProcessTree.js
@@ -3,12 +3,11 @@ import { VError } from '../VError/VError.js'
 
 export const loadWindowProcessTree = async () => {
   try {
-    // @ts-ignore
     return await import('@vscode/windows-process-tree')
   } catch (error) {
     if (IsDlOpenError.isDlOpenError(error)) {
       throw new VError(
-        `Failed to load windows process tree: The native module "@vscode/windows-process-tree" is not compatible with this node version and must be compiled against a matching electron version using electron-rebuild`
+        `Failed to load windows process tree: The native module "@vscode/windows-process-tree" is not compatible with this node version and must be compiled against a matching electron version using electron-rebuild`,
       )
     }
     throw new VError(error, `Failed to load windows process tree`)
