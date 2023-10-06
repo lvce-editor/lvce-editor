d289f0eb1fd4d00911a394268a4736e86a30bdb9
Le Vivilet <levivlet@pm.me>
2023-10-06 21:33:53 +0200

    fix icon theme e2e test

--- packages/renderer-worker/src/parts/GetAbsoluteIconPath/GetAbsoluteIconPath.js
+++ packages/renderer-worker/src/parts/GetAbsoluteIconPath/GetAbsoluteIconPath.js
@@ -4,6 +4,9 @@ export const getAbsoluteIconPath = (iconTheme, icon) => {
   const result = iconTheme.iconDefinitions[icon]
   const extensionPath = IconThemeState.state.extensionPath || ''
   if (result) {
+    if (extensionPath.startsWith('http://')) {
+      return `${extensionPath}${result}`
+    }
     if (extensionPath.includes('\\')) {
       const extensionUri = extensionPath.replaceAll('\\', '/')
       return `/remote/${extensionUri}/${result}`
