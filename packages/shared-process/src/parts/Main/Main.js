6bf5ae9e30758618ad92d773478baf3b6b0d13b7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-10-08 08:40:57 +0200

    refactor: move shared process main method into separate file (#3969)

---
+++ packages/shared-process/src/parts/Main/Main.js
@@ -0,0 +1,15 @@
+import { setPriority } from 'node:os'
+import * as Command from '../Command/Command.js'
+import * as Module from '../Module/Module.js'
+import * as ParentIpc from '../ParentIpc/ParentIpc.js'
+import * as ProcessListeners from '../ProcessListeners/ProcessListeners.js'
+import * as Signal from '../Signal/Signal.js'
+
+export const main = async () => {
+  Command.setLoad(Module.load)
+  process.on('disconnect', ProcessListeners.handleDisconnect)
+  process.on(Signal.SIGTERM, ProcessListeners.handleSigTerm)
+  process.on('uncaughtExceptionMonitor', ProcessListeners.handleUncaughtExceptionMonitor)
+  await ParentIpc.listen()
+  setPriority(process.pid, 19)
+}
--- packages/shared-process/src/sharedProcessMain.js
+++ packages/shared-process/src/sharedProcessMain.js
@@ -1,17 +1,3 @@
-import { setPriority } from 'node:os'
-import * as Command from './parts/Command/Command.js'
-import * as Module from './parts/Module/Module.js'
-import * as ParentIpc from './parts/ParentIpc/ParentIpc.js'
-import * as ProcessListeners from './parts/ProcessListeners/ProcessListeners.js'
-import * as Signal from './parts/Signal/Signal.js'
+import * as Main from './parts/Main/Main.js'
 
-const main = async () => {
-  Command.setLoad(Module.load)
-  process.on('disconnect', ProcessListeners.handleDisconnect)
-  process.on(Signal.SIGTERM, ProcessListeners.handleSigTerm)
-  process.on('uncaughtExceptionMonitor', ProcessListeners.handleUncaughtExceptionMonitor)
-  await ParentIpc.listen()
-  setPriority(process.pid, 19)
-}
-
-main()
+Main.main()
