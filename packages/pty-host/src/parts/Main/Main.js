fdc2c5d4ac8d571c2386e0019ed67b156ed30f2f
Le Vivilet <levivlet@pm.me>
2023-10-08 19:23:20 +0200

    undo change

--- packages/pty-host/src/parts/Main/Main.js
+++ packages/pty-host/src/parts/Main/Main.js
@@ -3,7 +3,7 @@ import * as CommandState from '../CommandState/CommandState.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcChild from '../IpcChild/IpcChild.js'
 import * as IpcChildType from '../IpcChildType/IpcChildType.js'
-// import * as ProcessListeners from '../ProcessListeners/ProcessListeners.js'
+import * as ProcessListeners from '../ProcessListeners/ProcessListeners.js'
 
 export const main = async () => {
   process.on('uncaughtExceptionMonitor', ProcessListeners.handleUncaughtExceptionMonitor)
