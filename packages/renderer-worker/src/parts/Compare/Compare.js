d0563e052d7ce19bcaad6056124e174ff877ec23
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-10-14 22:20:20 +0200

    feature: improve performance for comparing file name strings (#3993)

--- packages/renderer-worker/src/parts/Compare/Compare.js
+++ packages/renderer-worker/src/parts/Compare/Compare.js
@@ -1,3 +1,5 @@
+const RE_CHARACTERS = /^[a-zA-Z\.\-]+$/
+
 export const compareString = (a, b) => {
   return a.localeCompare(b)
 }
@@ -9,5 +11,8 @@ export const compareString = (a, b) => {
  * @returns boolean
  */
 export const compareStringNumeric = (a, b) => {
+  if (RE_CHARACTERS.test(a) && RE_CHARACTERS.test(b)) {
+    return a < b ? -1 : 1
+  }
   return a.localeCompare(b, 'en', { numeric: true })
 }
