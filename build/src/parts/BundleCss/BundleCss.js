238e4506fee5d722c629b5c8106e1c35299e281c
Le Vivilet <levivlet@pm.me>
2023-10-17 21:29:52 +0200

    fix build

--- build/src/parts/BundleCss/BundleCss.js
+++ build/src/parts/BundleCss/BundleCss.js
@@ -64,7 +64,7 @@ export const bundleCss = async ({ outDir, additionalCss = '', assetDir = '', pat
     }
 
     await Replace.replace({
-      path: Path.join(outDir, 'parts', 'ViewletTitleBarButtons.css'),
+      path: Path.join(outDir, 'parts', 'MaskIcon.css'),
       occurrence: `url(/icons/`,
       replacement: `url(${assetDir}/icons/`,
     })
