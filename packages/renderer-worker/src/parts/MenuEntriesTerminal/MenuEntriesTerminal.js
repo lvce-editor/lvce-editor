7245c4708db26c084edc200fc7f8d666b8efaee0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-10-22 09:25:56 +0200

    feature: add terminal menu entry for new terminal (#4029)

--- packages/renderer-worker/src/parts/MenuEntriesTerminal/MenuEntriesTerminal.js
+++ packages/renderer-worker/src/parts/MenuEntriesTerminal/MenuEntriesTerminal.js
@@ -1,3 +1,21 @@
+import * as I18nString from '../I18NString/I18NString.js'
+import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
+
+/**
+ * @enum {string}
+ */
+export const UiStrings = {
+  NewTerminal: 'New Terminal',
+}
+
 export const getMenuEntries = () => {
-  return []
+  return [
+    {
+      id: 'newTerminal',
+      label: I18nString.i18nString(UiStrings.NewTerminal),
+      flags: MenuItemFlags.None,
+      command: 'Layout.togglePanel',
+      args: ['Terminal'],
+    },
+  ]
 }
