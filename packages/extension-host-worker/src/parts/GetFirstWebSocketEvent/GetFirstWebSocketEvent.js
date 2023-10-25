879d11e44545fee4cfd195c0e957a153a6221cee
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-10-25 21:37:38 +0200

    refactor: simplify getfirstWebSocketEvent code (#4042)

--- packages/extension-host-worker/src/parts/GetFirstWebSocketEvent/GetFirstWebSocketEvent.js
+++ packages/extension-host-worker/src/parts/GetFirstWebSocketEvent/GetFirstWebSocketEvent.js
@@ -1,36 +1,15 @@
 import * as FirstWebSocketEventType from '../FirstWebSocketEventType/FirstWebSocketEventType.js'
+import * as GetFirstEvent from '../GetFirstEvent/GetFirstEvent.js'
 
 /**
  *
  * @param {WebSocket} webSocket
  * @returns
  */
-export const waitForWebSocketToBeOpen = async (webSocket) => {
-  const { type, event } = await new Promise((resolve) => {
-    const cleanup = (value) => {
-      webSocket.onopen = null
-      webSocket.onerror = null
-      webSocket.onclose = null
-      resolve(value)
-    }
-    const handleOpen = (event) => {
-      cleanup({ type: FirstWebSocketEventType.Open, event })
-    }
-    const handleError = (event) => {
-      cleanup({ type: FirstWebSocketEventType.Error, event })
-    }
-    const handleClose = (event) => {
-      cleanup({
-        type: FirstWebSocketEventType.Close,
-        event,
-      })
-    }
-    webSocket.onopen = handleOpen
-    webSocket.onerror = handleError
-    webSocket.onclose = handleClose
+export const waitForWebSocketToBeOpen = (webSocket) => {
+  return GetFirstEvent.getFirstEvent(webSocket, {
+    open: FirstWebSocketEventType.Open,
+    close: FirstWebSocketEventType.Close,
+    error: FirstWebSocketEventType.Error,
   })
-  return {
-    type,
-    event,
-  }
 }
