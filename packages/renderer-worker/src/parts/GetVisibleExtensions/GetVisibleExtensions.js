b5b023e88c4963186e6b1e8cdb311a52021738de
Le Vivilet <levivlet@pm.me>
2023-10-27 20:52:38 +0200

    refactor: simplify getVisibleExtension function

--- packages/renderer-worker/src/parts/GetVisibleExtensions/GetVisibleExtensions.js
+++ packages/renderer-worker/src/parts/GetVisibleExtensions/GetVisibleExtensions.js
@@ -1,3 +1,13 @@
+const getVisibleItem = (item, setSize, itemHeight, minLineY, relative, i, focusedIndex) => {
+  return {
+    ...item,
+    setSize,
+    posInSet: i + 1,
+    top: (i - minLineY) * itemHeight - relative,
+    focused: i === focusedIndex,
+  }
+}
+
 export const getVisible = (state) => {
   const { minLineY, maxLineY, items, itemHeight, deltaY, focusedIndex } = state
   const setSize = items.length
@@ -5,13 +15,7 @@ export const getVisible = (state) => {
   const relative = deltaY % itemHeight
   for (let i = minLineY; i < maxLineY; i++) {
     const item = items[i]
-    visible.push({
-      ...item,
-      setSize,
-      posInSet: i + 1,
-      top: (i - minLineY) * itemHeight - relative,
-      focused: i === focusedIndex,
-    })
+    visible.push(getVisibleItem(item, setSize, itemHeight, minLineY, relative, i, focusedIndex))
   }
   return visible
 }
