a40e75e4d2271885ab4bfe88f3dd32811b0bd40b
Le Vivilet <levivlet@pm.me>
2023-10-28 09:45:49 +0200

    refactor: move applyEdit function into separate file

--- packages/shared-process/src/parts/BulkReplacement/BulkReplacement.js
+++ packages/shared-process/src/parts/BulkReplacement/BulkReplacement.js
@@ -1,11 +1,4 @@
-import * as FileSystem from '../FileSystem/FileSystem.js'
-import * as BulkReplacementContent from '../BulkReplacementContent/BulkReplacementContent.js'
-
-const applyEdit = async (path, ranges, replacement) => {
-  const content = await FileSystem.readFile(path)
-  const newContent = BulkReplacementContent.getNewContent(content, ranges, replacement)
-  await FileSystem.writeFile(path, newContent)
-}
+import * as ApplyEdit from '../ApplyEdit/ApplyEdit.js'
 
 export const applyBulkReplacement = async (files, ranges, replacement) => {
   let rangeIndex = 0
@@ -14,7 +7,7 @@ export const applyBulkReplacement = async (files, ranges, replacement) => {
     const rangeStartIndex = rangeIndex + 1
     const rangeEndIndex = rangeStartIndex + rangeLength
     const fileRanges = ranges.slice(rangeStartIndex, rangeEndIndex)
-    await applyEdit(file, fileRanges, replacement)
+    await ApplyEdit.applyEdit(file, fileRanges, replacement)
     rangeIndex = rangeEndIndex
   }
 }
