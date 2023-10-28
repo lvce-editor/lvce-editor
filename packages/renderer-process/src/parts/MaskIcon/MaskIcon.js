9df08d12c2a1305e30fe3fce5b725d34a3d722a9
Le Vivilet <levivlet@pm.me>
2023-10-28 10:58:28 +0200

    refactor: use classname for arrow icons instead of inline styles

--- packages/renderer-process/src/parts/MaskIcon/MaskIcon.js
+++ packages/renderer-process/src/parts/MaskIcon/MaskIcon.js
@@ -4,11 +4,7 @@ import * as MaskImage from '../MaskImage/MaskImage.js'
 export const create = (icon) => {
   const $Icon = document.createElement('div')
   $Icon.className = 'MaskIcon'
-  if (!icon || icon.includes('/icons')) {
-    MaskImage.setMaskImage($Icon, icon)
-  } else {
-    $Icon.classList.add(`MaskIcon${icon}`)
-  }
+  MaskImage.setMaskImage($Icon, icon)
   $Icon.role = AriaRoles.None
   return $Icon
 }
