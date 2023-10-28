80c871c784af00b6382658569c5d6f1ced07ba6e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-10-28 11:10:23 +0200

    refactor: move getTextSearchResultCounts function into separate file (#4056)

--- packages/renderer-worker/src/parts/ViewletSearch/ViewletSearchHandleUpdate.js
+++ packages/renderer-worker/src/parts/ViewletSearch/ViewletSearchHandleUpdate.js
@@ -1,29 +1,11 @@
 import * as ErrorHandling from '../ErrorHandling/ErrorHandling.js'
 import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOfVisibleItems.js'
+import * as GetTextSearchResultCounts from '../GetTextSearchResultCounts/GetTextSearchResultCounts.js'
 import * as IsEmptyString from '../IsEmptyString/IsEmptyString.js'
 import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.js'
 import * as TextSearch from '../TextSearch/TextSearch.js'
-import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.js'
 import * as Workspace from '../Workspace/Workspace.js'
 import * as ViewletSearchStatusMessage from './ViewletSearchStatusMessage.js'
-
-const getResultCounts = (results) => {
-  let resultCount = 0
-  let fileCount = 0
-  for (const result of results) {
-    switch (result.type) {
-      case TextSearchResultType.File:
-        fileCount++
-        break
-      case TextSearchResultType.Match:
-        resultCount++
-        break
-      default:
-        break
-    }
-  }
-  return { fileCount, resultCount }
-}
 
 export const handleUpdate = async (state, update) => {
   const partialNewState = { ...state, ...update }
@@ -49,7 +31,7 @@ export const handleUpdate = async (state, update) => {
     if (!Array.isArray(results)) {
       throw new Error(`results must be of type array`)
     }
-    const { fileCount, resultCount } = getResultCounts(results)
+    const { fileCount, resultCount } = GetTextSearchResultCounts.getTextSearchResultCounts(results)
     // const displayResults = toDisplayResults(results, itemHeight, resultCount, value)
     const message = ViewletSearchStatusMessage.getStatusMessage(resultCount, fileCount)
     const total = results.length
