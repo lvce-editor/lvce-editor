2d640c8b1c799e57c910a577a8cbdffb6d616fc3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-10-28 17:09:09 +0200

    feature: improve replace all message when replacement value is empty (#4071)

---
+++ packages/renderer-worker/src/parts/GetReplaceAllConfirmText/GetReplaceAllConfirmText.js
@@ -0,0 +1,20 @@
+import * as ViewletSearchStrings from '../ViewletSearch/ViewletSearchStrings.js'
+
+export const getReplaceAllConfirmText = (matchCount, fileCount, replacement) => {
+  if (matchCount === 1) {
+    if (replacement) {
+      return ViewletSearchStrings.confirmReplaceOneOccurrenceInOneFile(replacement)
+    }
+    return ViewletSearchStrings.confirmReplaceOneOccurrenceInOneFileNoValue()
+  }
+  if (fileCount === 1) {
+    if (replacement) {
+      return ViewletSearchStrings.confirmReplaceManyOccurrencesInOneFile(matchCount, replacement)
+    }
+    return ViewletSearchStrings.confirmReplaceManyOccurrencesInOneFileNoValue(matchCount)
+  }
+  if (replacement) {
+    return ViewletSearchStrings.confirmReplaceManyOccurrencesInManyFiles(matchCount, fileCount, replacement)
+  }
+  return ViewletSearchStrings.confirmReplaceManyOccurrencesInManyFilesNoValue(matchCount, fileCount)
+}
--- packages/renderer-worker/src/parts/ReplaceAllAndPrompt/ReplaceAllAndPrompt.js
+++ packages/renderer-worker/src/parts/ReplaceAllAndPrompt/ReplaceAllAndPrompt.js
@@ -1,26 +1,18 @@
 import * as Assert from '../Assert/Assert.js'
 import * as Command from '../Command/Command.js'
+import * as GetReplaceAllConfirmText from '../GetReplaceAllConfirmText/GetReplaceAllConfirmText.js'
 import * as TextSearchReplaceAll from '../TextSearchReplaceAll/TextSearchReplaceAll.js'
 import * as ViewletSearchStrings from '../ViewletSearch/ViewletSearchStrings.js'
 
-const getConfirmText = (matchCount, fileCount, replacement) => {
-  if (matchCount === 1) {
-    return ViewletSearchStrings.confirmReplaceOneOccurrenceInOneFile(replacement)
-  }
-  if (fileCount === 1) {
-    return ViewletSearchStrings.confirmReplaceManyOccurrencesInOneFile(matchCount, replacement)
-  }
-  return ViewletSearchStrings.confirmReplaceManyOccurrencesInManyFiles(matchCount, fileCount, replacement)
-}
-
 export const replaceAllAndPrompt = async (workspacePath, items, replacement, matchCount, fileCount) => {
+  Assert.string(workspacePath)
   Assert.array(items)
   Assert.string(replacement)
   Assert.number(matchCount)
   Assert.number(fileCount)
   const confirmTitle = ViewletSearchStrings.replaceAll()
   const confirmAccept = ViewletSearchStrings.replace()
-  const confirmText = getConfirmText(matchCount, fileCount, replacement)
+  const confirmText = GetReplaceAllConfirmText.getReplaceAllConfirmText(matchCount, fileCount, replacement)
   const shouldReplace = await Command.execute('ConfirmPrompt.prompt', confirmText, { title: confirmTitle, confirmMessage: confirmAccept })
   if (!shouldReplace) {
     return false
