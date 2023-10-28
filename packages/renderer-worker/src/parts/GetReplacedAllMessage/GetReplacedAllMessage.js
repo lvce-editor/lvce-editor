78117f335c18436417eefde272b5a391920034f6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-10-28 10:47:37 +0200

    refactor: move getReplacedAllMessage function into separate file (#4057)

---
+++ packages/renderer-worker/src/parts/GetReplacedAllMessage/GetReplacedAllMessage.js
@@ -0,0 +1,11 @@
+import * as ViewletSearchStrings from '../ViewletSearch/ViewletSearchStrings.js'
+
+export const getReplacedAllMessage = (matchCount, fileCount, replacement) => {
+  if (matchCount === 1) {
+    return ViewletSearchStrings.replacedOneOccurrenceInOneFile(replacement)
+  }
+  if (fileCount === 1) {
+    return ViewletSearchStrings.replacedManyOccurrencesInOneFile(matchCount, replacement)
+  }
+  return ViewletSearchStrings.replacedManyOccurrencesInManyFiles(matchCount, fileCount, replacement)
+}
