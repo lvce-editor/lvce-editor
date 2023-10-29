ff3b2618c24f7b93cc381c4908604ef5d7466056
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-10-29 07:28:14 +0100

    refactor: simplify promise resolve code in main process (#4076)

--- packages/main-process/src/parts/GetFirstUtilityProcessEvent/GetFirstUtilityProcessEvent.js
+++ packages/main-process/src/parts/GetFirstUtilityProcessEvent/GetFirstUtilityProcessEvent.js
@@ -1,4 +1,5 @@
 import * as FirstNodeWorkerEventType from '../FirstNodeWorkerEventType/FirstNodeWorkerEventType.js'
+import * as Promises from '../Promises/Promises.js'
 
 /**
  *
@@ -6,36 +7,36 @@ import * as FirstNodeWorkerEventType from '../FirstNodeWorkerEventType/FirstNode
  * @returns
  */
 export const getFirstUtilityProcessEvent = async (utilityProcess) => {
-  const { type, event, stdout, stderr } = await new Promise((resolve, reject) => {
-    let stdout = ''
-    let stderr = ''
-    const cleanup = (value) => {
-      // @ts-ignore
-      utilityProcess.stderr.off('data', handleStdErrData)
-      // @ts-ignore
-      utilityProcess.stdout.off('data', handleStdoutData)
-      utilityProcess.off('message', handleMessage)
-      utilityProcess.off('exit', handleExit)
-      resolve(value)
-    }
-    const handleStdErrData = (data) => {
-      stderr += data
-    }
-    const handleStdoutData = (data) => {
-      stdout += data
-    }
-    const handleMessage = (event) => {
-      cleanup({ type: FirstNodeWorkerEventType.Message, event, stdout, stderr })
-    }
-    const handleExit = (event) => {
-      cleanup({ type: FirstNodeWorkerEventType.Exit, event, stdout, stderr })
-    }
+  const { resolve, promise } = Promises.withResolvers()
+  let stdout = ''
+  let stderr = ''
+  const cleanup = (value) => {
     // @ts-ignore
-    utilityProcess.stderr.on('data', handleStdErrData)
+    utilityProcess.stderr.off('data', handleStdErrData)
     // @ts-ignore
-    utilityProcess.stdout.on('data', handleStdoutData)
-    utilityProcess.on('message', handleMessage)
-    utilityProcess.on('exit', handleExit)
-  })
+    utilityProcess.stdout.off('data', handleStdoutData)
+    utilityProcess.off('message', handleMessage)
+    utilityProcess.off('exit', handleExit)
+    resolve(value)
+  }
+  const handleStdErrData = (data) => {
+    stderr += data
+  }
+  const handleStdoutData = (data) => {
+    stdout += data
+  }
+  const handleMessage = (event) => {
+    cleanup({ type: FirstNodeWorkerEventType.Message, event, stdout, stderr })
+  }
+  const handleExit = (event) => {
+    cleanup({ type: FirstNodeWorkerEventType.Exit, event, stdout, stderr })
+  }
+  // @ts-ignore
+  utilityProcess.stderr.on('data', handleStdErrData)
+  // @ts-ignore
+  utilityProcess.stdout.on('data', handleStdoutData)
+  utilityProcess.on('message', handleMessage)
+  utilityProcess.on('exit', handleExit)
+  const { type, event } = await promise
   return { type, event, stdout, stderr }
 }
