bb2372613fcab1a45053eea080b7db37dfd8d10a
Le Vivilet <levivlet@pm.me>
2023-10-31 08:37:18 +0100

    refactor: simplify getFirstEvent promise code

--- packages/shared-process/src/parts/GetFirstEvent/GetFirstEvent.js
+++ packages/shared-process/src/parts/GetFirstEvent/GetFirstEvent.js
@@ -1,22 +1,23 @@
-export const getFirstEvent = async (eventEmitter, eventMap) => {
-  const { type, event } = await new Promise((resolve, reject) => {
-    const listenerMap = Object.create(null)
-    const cleanup = (value) => {
-      for (const event of Object.keys(eventMap)) {
-        eventEmitter.off(event, listenerMap[event])
-      }
-      resolve(value)
+import * as Promises from '../Promises/Promises.js'
+
+export const getFirstEvent = (eventEmitter, eventMap) => {
+  const { resolve, promise } = Promises.withResolvers()
+  const listenerMap = Object.create(null)
+  const cleanup = (value) => {
+    for (const event of Object.keys(eventMap)) {
+      eventEmitter.off(event, listenerMap[event])
     }
-    for (const [event, type] of Object.entries(eventMap)) {
-      const listener = (event) => {
-        cleanup({
-          type,
-          event,
-        })
-      }
-      eventEmitter.on(event, listener)
-      listenerMap[event] = listener
+    resolve(value)
+  }
+  for (const [event, type] of Object.entries(eventMap)) {
+    const listener = (event) => {
+      cleanup({
+        type,
+        event,
+      })
     }
-  })
-  return { type, event }
+    eventEmitter.on(event, listener)
+    listenerMap[event] = listener
+  }
+  return promise
 }
