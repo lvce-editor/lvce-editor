5db570f6460265eec29feac221dc66afeebdd4c4
Le Vivilet <levivlet@pm.me>
2023-10-31 09:14:57 +0100

    feature: align no extensions found message with vscode

--- packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsStrings.js
+++ packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsStrings.js
@@ -4,7 +4,7 @@ import * as I18nString from '../I18NString/I18NString.js'
  * @enum {string}
  */
 const UiStrings = {
-  NoExtensionsFound: 'No extensions found',
+  NoExtensionsFound: 'No extensions found.',
 }
 
 export const noExtensionsFound = () => {
