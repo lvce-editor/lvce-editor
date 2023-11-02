b2e506d6e0b906cb2626474710141b52509e4741
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-11-02 18:05:24 +0100

    feature: remove unused shared process sourcemap files during build (#4124)

--- build/src/parts/RemoveSourceMaps/RemoveSourceMaps.js
+++ build/src/parts/RemoveSourceMaps/RemoveSourceMaps.js
@@ -2,6 +2,7 @@ import { readdir } from 'fs/promises'
 import * as Path from '../Path/Path.js'
 import * as Remove from '../Remove/Remove.js'
 import * as Replace from '../Replace/Replace.js'
+import { existsSync } from 'fs'
 
 export const removeSourceMaps = async (absolutePath) => {
   const dirents = await readdir(absolutePath, { recursive: true })
@@ -9,6 +10,9 @@ export const removeSourceMaps = async (absolutePath) => {
     const absoluteDirentPath = Path.join(absolutePath, dirent)
     if (dirent.endsWith('.js')) {
       const sourceMapPath = absoluteDirentPath + '.map'
+      if (!existsSync(sourceMapPath)) {
+        continue
+      }
       await Remove.remove(sourceMapPath)
       const baseName = Path.baseName(sourceMapPath)
       await Replace.replace({
