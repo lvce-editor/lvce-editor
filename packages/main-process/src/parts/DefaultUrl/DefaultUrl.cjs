667f3cb61c8c938b8eebc4c70cff21f198782729
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-11-04 09:25:31 +0100

    refactor: move authority variable into separate file (#4126)

--- packages/main-process/src/parts/DefaultUrl/DefaultUrl.cjs
+++ packages/main-process/src/parts/DefaultUrl/DefaultUrl.cjs
@@ -1,3 +1,4 @@
 const Platform = require('../Platform/Platform.cjs')
+const Authority = require('../Authority/Authority.cjs')
 
-exports.defaultUrl = `${Platform.scheme}://-/`
+exports.defaultUrl = `${Platform.scheme}://${Authority.authority}/`
