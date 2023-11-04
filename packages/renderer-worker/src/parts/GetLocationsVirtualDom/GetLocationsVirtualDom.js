f5af6ba84d4a2645be0c59f7a7ae07a8139b4b08
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-11-04 18:06:50 +0100

    feature: add function to render locations virtual dom (#4153)

---
+++ packages/renderer-worker/src/parts/GetLocationsVirtualDom/GetLocationsVirtualDom.js
@@ -0,0 +1,68 @@
+import * as AriaRoles from '../AriaRoles/AriaRoles.js'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
+
+const getLocationVirtualDom = (location) => {
+  const { type, icon, lineText, index, name } = location
+  switch (type) {
+    case 'leaf':
+      return [
+        {
+          type: VirtualDomElements.Div,
+          className: 'TreeItem',
+          id: `Reference-${index}`,
+          childCount: 1,
+        },
+        text(lineText || '(empty line)'),
+      ]
+    case 'collapsed':
+      return [
+        {
+          type: VirtualDomElements.Div,
+          className: 'TreeItem',
+          ariaExpanded: false,
+          id: `Reference-${index}`,
+          childCount: 1,
+        },
+        text(name),
+      ]
+    case 'expanded':
+      return [
+        {
+          type: VirtualDomElements.Div,
+          className: 'TreeItem',
+          ariaExpanded: true,
+          id: `Reference-${index}`,
+          childCount: 1,
+        },
+        text(name),
+      ]
+    default:
+      return []
+  }
+}
+
+export const getLocationsVirtualDom = (locations, message) => {
+  const dom = []
+  dom.push(
+    {
+      type: VirtualDomElements.Div,
+      className: 'LocationsMessage',
+      id: 'LocationsMessage',
+      role: AriaRoles.Status,
+      childCount: 1,
+    },
+    text(message),
+    {
+      type: VirtualDomElements.Div,
+      className: 'LocationList',
+      role: AriaRoles.Tree,
+      ariaLabel: 'Locations',
+      tabIndex: 0,
+      ariaDescribedBy: 'LocationsMessage',
+      childCount: locations.length,
+    },
+    ...locations.flatMap(getLocationVirtualDom),
+  )
+  return dom
+}
