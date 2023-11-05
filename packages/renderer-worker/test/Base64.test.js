9ad76481a18f566b272a0a2afb0f7843017ce912
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-11-05 16:44:48 +0100

    feature: lazyload js-base64 (#4177)

--- packages/renderer-worker/test/Base64.test.js
+++ packages/renderer-worker/test/Base64.test.js
@@ -1,5 +1,27 @@
-import * as Base64 from '../src/parts/Base64/Base64.js'
+import { jest } from '@jest/globals'
 
-test('decode', () => {
-  expect(Base64.decode('YWJj')).toBe('abc')
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule('../src/parts/LoadJsBase64/LoadJsBase64.js', () => {
+  const decode = jest.fn()
+  return {
+    loadJsBase64() {
+      return {
+        decode,
+      }
+    },
+  }
+})
+
+const Base64 = await import('../src/parts/Base64/Base64.js')
+const LoadJsBase64 = await import('../src/parts/LoadJsBase64/LoadJsBase64.js')
+
+test('decode', async () => {
+  const jsBase64 = await LoadJsBase64.loadJsBase64()
+  jsBase64.decode.mockImplementation(() => 'abc')
+  expect(await Base64.decode('YWJj')).toBe('abc')
+  expect(jsBase64.decode).toHaveBeenCalledTimes(1)
+  expect(jsBase64.decode).toHaveBeenCalledWith('YWJj')
 })
