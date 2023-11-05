04b51276b977e2e51b93be279b8196828c9e2bb2
Le Vivilet <levivlet@pm.me>
2023-11-05 08:35:44 +0100

    remove unused code

--- packages/renderer-process/test/ViewletLocations.test.js
+++ packages/renderer-process/test/ViewletLocations.test.js
@@ -1,8 +1,6 @@
 /**
  * @jest-environment jsdom
  */
-import { jest } from '@jest/globals'
-
 const ViewletLocations = await import('../src/parts/ViewletLocations/ViewletLocations.js')
 
 test('create', () => {
