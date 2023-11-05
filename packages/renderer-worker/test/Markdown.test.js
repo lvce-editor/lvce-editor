4db19b4682867a79873ea882ef3c29536e9604ab
Le Vivilet <levivlet@pm.me>
2023-11-05 16:03:45 +0100

    fix test

--- packages/renderer-worker/test/Markdown.test.js
+++ packages/renderer-worker/test/Markdown.test.js
@@ -1,6 +1,27 @@
-import * as Markdown from '../src/parts/Markdown/Markdown.js'
+import { jest } from '@jest/globals'
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule('../src/parts/LoadMarked/LoadMarked.js', () => {
+  const marked = jest.fn()
+  return {
+    loadMarked() {
+      return marked
+    },
+  }
+})
+
+const Markdown = await import('../src/parts/Markdown/Markdown.js')
+const LoadMarked = await import('../src/parts/LoadMarked/LoadMarked.js')
 
 test('toHtml', async () => {
-  expect(await Markdown.toHtml('# test')).toBe(`<h1 id=\"test\">test</h1>
-`)
+  const marked = await LoadMarked.loadMarked()
+  marked.mockImplementation(() => {
+    return `<h1 id=\"test\">test</h1>\n`
+  })
+  expect(await Markdown.toHtml('# test')).toBe(`<h1 id=\"test\">test</h1>\n`)
+  expect(marked).toHaveBeenCalledTimes(1)
+  expect(marked).toHaveBeenCalledWith('# test', { baseUrl: '' })
 })
