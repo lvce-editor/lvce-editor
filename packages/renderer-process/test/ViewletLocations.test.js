a1ea6ff64b7857a2693fcce33b24e4ea52921655
Le Vivilet <levivlet@pm.me>
2023-11-05 08:34:45 +0100

    remove unused code

--- packages/renderer-process/test/ViewletLocations.test.js
+++ packages/renderer-process/test/ViewletLocations.test.js
@@ -3,20 +3,6 @@
  */
 import { jest } from '@jest/globals'
 
-beforeEach(() => {
-  jest.resetAllMocks()
-})
-
-jest.unstable_mockModule('../src/parts/RendererWorker/RendererWorker.js', () => {
-  return {
-    send: jest.fn(() => {
-      throw new Error('not implemented')
-    }),
-  }
-})
-
-const RendererWorker = await import('../src/parts/RendererWorker/RendererWorker.js')
-
 const ViewletLocations = await import('../src/parts/ViewletLocations/ViewletLocations.js')
 
 test('create', () => {
