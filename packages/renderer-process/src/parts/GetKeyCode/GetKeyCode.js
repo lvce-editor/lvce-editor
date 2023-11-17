5c8ac2b1fc934bad8b948a42a8bd9e55008210ff
Le Vivilet <levivlet@pm.me>
2023-11-17 22:46:33 +0100

    add more key mappings

--- packages/renderer-process/src/parts/GetKeyCode/GetKeyCode.js
+++ packages/renderer-process/src/parts/GetKeyCode/GetKeyCode.js
@@ -21,6 +21,40 @@ export const getKeyCode = (key) => {
       return KeyCode.KeyH
     case Key.KeyI:
       return KeyCode.KeyI
+    case Key.KeyJ:
+      return KeyCode.KeyJ
+    case Key.KeyK:
+      return KeyCode.KeyK
+    case Key.KeyL:
+      return KeyCode.KeyL
+    case Key.KeyM:
+      return KeyCode.KeyM
+    case Key.KeyN:
+      return KeyCode.KeyN
+    case Key.KeyO:
+      return KeyCode.KeyO
+    case Key.KeyP:
+      return KeyCode.KeyP
+    case Key.KeyQ:
+      return KeyCode.KeyQ
+    case Key.KeyR:
+      return KeyCode.KeyR
+    case Key.KeyS:
+      return KeyCode.KeyS
+    case Key.KeyT:
+      return KeyCode.KeyT
+    case Key.KeyU:
+      return KeyCode.KeyU
+    case Key.KeyV:
+      return KeyCode.KeyV
+    case Key.KeyW:
+      return KeyCode.KeyW
+    case Key.KeyX:
+      return KeyCode.KeyX
+    case Key.KeyY:
+      return KeyCode.KeyY
+    case Key.KeyZ:
+      return KeyCode.KeyZ
     default:
       return KeyCode.Unknown
   }
--- packages/renderer-process/test/GetKeyCode.test.js
+++ packages/renderer-process/test/GetKeyCode.test.js
@@ -37,3 +37,71 @@ test('KeyH', () => {
 test('KeyI', () => {
   expect(GetKeyCode.getKeyCode(Key.KeyI)).toBe(KeyCode.KeyI)
 })
+
+test('KeyJ', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyJ)).toBe(KeyCode.KeyJ)
+})
+
+test('KeyK', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyK)).toBe(KeyCode.KeyK)
+})
+
+test('KeyL', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyL)).toBe(KeyCode.KeyL)
+})
+
+test('KeyM', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyM)).toBe(KeyCode.KeyM)
+})
+
+test('KeyN', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyN)).toBe(KeyCode.KeyN)
+})
+
+test('KeyO', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyO)).toBe(KeyCode.KeyO)
+})
+
+test('KeyP', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyP)).toBe(KeyCode.KeyP)
+})
+
+test('KeyQ', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyQ)).toBe(KeyCode.KeyQ)
+})
+
+test('KeyR', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyR)).toBe(KeyCode.KeyR)
+})
+
+test('KeyS', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyS)).toBe(KeyCode.KeyS)
+})
+
+test('KeyT', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyT)).toBe(KeyCode.KeyT)
+})
+
+test('KeyU', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyU)).toBe(KeyCode.KeyU)
+})
+
+test('KeyV', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyV)).toBe(KeyCode.KeyV)
+})
+
+test('KeyW', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyW)).toBe(KeyCode.KeyW)
+})
+
+test('KeyX', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyX)).toBe(KeyCode.KeyX)
+})
+
+test('KeyY', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyY)).toBe(KeyCode.KeyY)
+})
+
+test('KeyZ', () => {
+  expect(GetKeyCode.getKeyCode(Key.KeyZ)).toBe(KeyCode.KeyZ)
+})
