9decd80deceaeac10314361469e3f783dcf30ab1
Le Vivilet <levivlet@pm.me>
2023-11-18 16:35:58 +0100

    fix build

--- build/src/parts/BundleExtensionSubHostWorker/BundleExtensionHostSubWorker.js
+++ build/src/parts/BundleExtensionSubHostWorker/BundleExtensionHostSubWorker.js
@@ -1,6 +1,7 @@
 import * as BundleJs from '../BundleJsRollup/BundleJsRollup.js'
 import * as Copy from '../Copy/Copy.js'
 import * as Path from '../Path/Path.js'
+import * as Replace from '../Replace/Replace.js'
 
 export const bundleExtensionHostSubWorker = async ({ cachePath, commitHash, platform, assetDir }) => {
   await Copy.copy({
@@ -10,6 +11,11 @@ export const bundleExtensionHostSubWorker = async ({ cachePath, commitHash, plat
   await Copy.copy({
     from: 'static/js',
     to: Path.join(cachePath, 'static', 'js'),
+  })
+  await Replace.replace({
+    path: `${cachePath}/src/parts/AssetDir/AssetDir.js`,
+    occurrence: `ASSET_DIR`,
+    replacement: `'${assetDir}'`,
   })
   await BundleJs.bundleJs({
     cwd: cachePath,
