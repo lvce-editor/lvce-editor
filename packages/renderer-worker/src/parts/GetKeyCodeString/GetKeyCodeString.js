19dbb5caaa5a433f93627a091cdb503679ed4616
Le Vivilet <levivlet@pm.me>
2023-11-18 15:00:12 +0100

    fix: add more keys to getKeyCodeString function

--- packages/renderer-worker/src/parts/GetKeyCodeString/GetKeyCodeString.js
+++ packages/renderer-worker/src/parts/GetKeyCodeString/GetKeyCodeString.js
@@ -33,6 +33,24 @@ export const getKeyCodeString = (keyCode) => {
       return KeyCodeString.Delete
     case KeyCode.Digit0:
       return KeyCodeString.Digit0
+    case KeyCode.Digit1:
+      return KeyCodeString.Digit1
+    case KeyCode.Digit2:
+      return KeyCodeString.Digit2
+    case KeyCode.Digit3:
+      return KeyCodeString.Digit3
+    case KeyCode.Digit4:
+      return KeyCodeString.Digit4
+    case KeyCode.Digit5:
+      return KeyCodeString.Digit5
+    case KeyCode.Digit6:
+      return KeyCodeString.Digit6
+    case KeyCode.Digit7:
+      return KeyCodeString.Digit7
+    case KeyCode.Digit8:
+      return KeyCodeString.Digit8
+    case KeyCode.Digit9:
+      return KeyCodeString.Digit9
     case KeyCode.KeyA:
       return KeyCodeString.KeyA
     case KeyCode.KeyB:
@@ -51,6 +69,40 @@ export const getKeyCodeString = (keyCode) => {
       return KeyCodeString.KeyH
     case KeyCode.KeyI:
       return KeyCodeString.KeyI
+    case KeyCode.KeyJ:
+      return KeyCodeString.KeyJ
+    case KeyCode.KeyK:
+      return KeyCodeString.KeyK
+    case KeyCode.KeyL:
+      return KeyCodeString.KeyL
+    case KeyCode.KeyM:
+      return KeyCodeString.KeyM
+    case KeyCode.KeyN:
+      return KeyCodeString.KeyN
+    case KeyCode.KeyO:
+      return KeyCodeString.KeyO
+    case KeyCode.KeyP:
+      return KeyCodeString.KeyP
+    case KeyCode.KeyQ:
+      return KeyCodeString.KeyQ
+    case KeyCode.KeyR:
+      return KeyCodeString.KeyR
+    case KeyCode.KeyS:
+      return KeyCodeString.KeyS
+    case KeyCode.KeyT:
+      return KeyCodeString.KeyT
+    case KeyCode.KeyU:
+      return KeyCodeString.KeyU
+    case KeyCode.KeyV:
+      return KeyCodeString.KeyV
+    case KeyCode.KeyW:
+      return KeyCodeString.KeyW
+    case KeyCode.KeyX:
+      return KeyCodeString.KeyX
+    case KeyCode.KeyY:
+      return KeyCodeString.KeyY
+    case KeyCode.KeyZ:
+      return KeyCodeString.KeyZ
     default:
       return KeyCodeString.Unknown
   }
