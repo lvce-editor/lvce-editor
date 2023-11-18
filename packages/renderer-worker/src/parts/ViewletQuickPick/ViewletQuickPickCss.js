9980be2a2099b00ac7c4a53351934a786e43bc06
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-11-18 18:39:00 +0100

    feature: align quickpick spacing with vscode (#4222)

--- packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickCss.js
+++ packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickCss.js
@@ -1,3 +1,3 @@
-export const Css = ['/css/parts/ViewletQuickPick.css', '/css/parts/InputBox.css', '/css/parts/FileIcon.css']
+export const Css = ['/css/parts/ViewletQuickPick.css', '/css/parts/InputBox.css', '/css/parts/FileIcon.css', '/css/parts/Progress.css']
 
 export const Variables = ['QuickPickBackground', 'ListForeground', 'ListHoverBackground', 'ListHoverForeground', 'ListActiveSelectionBackground']
