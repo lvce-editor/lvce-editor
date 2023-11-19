9501e28394d75c9f1b89e5182bc27282b4150706
Le Vivilet <levivlet@pm.me>
2023-11-19 09:27:28 +0100

    fix: potential endless loop when setting diagnostics

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandSetDecorations.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandSetDecorations.js
@@ -1,4 +1,7 @@
 export const setDecorations = (editor, decorations) => {
+  if (editor.decorations.length === 0 && decorations.length === 0) {
+    return editor
+  }
   return {
     ...editor,
     decorations,
