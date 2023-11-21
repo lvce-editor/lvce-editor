a8f866c668c61c570c9c3572b8d081673d4cedf5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-11-21 19:58:07 +0100

    fix: font loading in webworker in chrome 120 (#4240)

--- packages/renderer-worker/src/parts/Font/Font.js
+++ packages/renderer-worker/src/parts/Font/Font.js
@@ -1,19 +1,10 @@
 import * as Assert from '../Assert/Assert.js'
-import * as IsFirefox from '../IsFirefox/IsFirefox.js'
+import * as FontState from '../FontState/FontState.js'
 import { VError } from '../VError/VError.js'
 
 const getFonts = () => {
   // @ts-ignore
   return globalThis.fonts || document.fonts
-}
-
-export const has = (fontName, fontSize) => {
-  // workaround for firefox mistakenly reporting fonts
-  if (IsFirefox.isFirefox) {
-    return false
-  }
-  const fonts = getFonts()
-  return fonts.check(`${fontSize}px ${fontName}`)
 }
 
 export const load = async (fontName, fontUrl) => {
@@ -31,4 +22,17 @@ export const load = async (fontName, fontUrl) => {
   } catch (error) {
     throw new VError(error, `Failed to load font ${fontName}`)
   }
+}
+
+export const ensure = async (fontName, fontUrl) => {
+  if (FontState.isLoaded(fontName)) {
+    return
+  }
+  if (FontState.hasPending(fontName)) {
+    return FontState.getPending(fontName)
+  }
+  const promise = load(fontName, fontUrl)
+  FontState.setPending(fontName, promise)
+  await promise
+  FontState.removePending(fontName)
 }
