09cca21f6e764e7454442e10cd477977608c79a8
Le Vivilet <levivlet@pm.me>
2023-11-21 20:18:27 +0100

    fix: static build

--- build/src/parts/Static/Static.js
+++ build/src/parts/Static/Static.js
@@ -265,6 +265,10 @@ const copyStaticFiles = async ({ pathPrefix, ignoreIconTheme, commitHash }) => {
   })
   await Copy.copy({
     from: 'static/js',
+    to: `build/.tmp/dist/${commitHash}/static/js`,
+  })
+  await Copy.copy({
+    from: 'static/js',
     to: `build/.tmp/dist/${commitHash}/js`,
   })
   await Copy.copyFile({
