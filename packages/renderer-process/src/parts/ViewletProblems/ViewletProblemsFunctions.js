cd8382dcebefdbe8f39560bc0a71a0097f860f92
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-11-22 21:15:18 +0100

    feature: send context menu events to problems view (#4251)

--- packages/renderer-process/src/parts/ViewletProblems/ViewletProblemsFunctions.js
+++ packages/renderer-process/src/parts/ViewletProblems/ViewletProblemsFunctions.js
@@ -1,3 +1,4 @@
 import * as ForwardViewletCommand from '../ForwardViewletCommand/ForwardViewletCommand.js'
 
 export const focusIndex = ForwardViewletCommand.forwardViewletCommand('focusIndex')
+export const handleContextMenu = ForwardViewletCommand.forwardViewletCommand('handleContextMenu')
