a3094d8c8e2b0a919fbc419ad635bb566b1885b1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-03 16:40:40 +0100

    refactor: move logging out of jsonrpc function to allow moving handle… (#4284)

--- packages/shared-process/src/parts/HandleJsonRpcMessage/HandleJsonRpcMessage.js
+++ packages/shared-process/src/parts/HandleJsonRpcMessage/HandleJsonRpcMessage.js
@@ -2,14 +2,14 @@ import * as GetErrorResponse from '../GetErrorResponse/GetErrorResponse.js'
 import * as GetResponse from '../GetResponse/GetResponse.js'
 import { JsonRpcError } from '../JsonRpcError/JsonRpcError.js'
 
-export const handleJsonRpcMessage = async (ipc, message, execute, resolve) => {
+export const handleJsonRpcMessage = async (ipc, message, execute, resolve, preparePrettyError, logError) => {
   if ('id' in message) {
     if ('method' in message) {
-      const response = await GetResponse.getResponse(message, ipc, execute)
+      const response = await GetResponse.getResponse(message, ipc, execute, preparePrettyError, logError)
       try {
         ipc.send(response)
       } catch (error) {
-        const errorResponse = GetErrorResponse.getErrorResponse(message, error)
+        const errorResponse = GetErrorResponse.getErrorResponse(message, error, ipc, preparePrettyError, logError)
         ipc.send(errorResponse)
       }
       return
@@ -18,7 +18,7 @@ export const handleJsonRpcMessage = async (ipc, message, execute, resolve) => {
     return
   }
   if ('method' in message) {
-    await GetResponse.getResponse(message, ipc, execute)
+    await GetResponse.getResponse(message, ipc, execute, preparePrettyError, logError)
     return
   }
   throw new JsonRpcError('unexpected message')
