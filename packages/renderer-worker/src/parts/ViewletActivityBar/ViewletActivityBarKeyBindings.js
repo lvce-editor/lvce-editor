15e50486d088cae09c15cc95930bb762df117f74
Le Vivilet <levivlet@pm.me>
2023-12-03 18:49:13 +0100

    feature: use whenExpression enum in activity bar

--- packages/renderer-worker/src/parts/ViewletActivityBar/ViewletActivityBarKeyBindings.js
+++ packages/renderer-worker/src/parts/ViewletActivityBar/ViewletActivityBarKeyBindings.js
@@ -1,56 +1,57 @@
 import * as KeyCode from '../KeyCode/KeyCode.js'
+import * as WhenExpression from '../WhenExpression/WhenExpression.js'
 
 export const getKeyBindings = () => {
   return [
     {
       key: KeyCode.DownArrow,
       command: 'ActivityBar.focusNext',
-      when: 'focus.activityBar',
+      when: WhenExpression.FocusActivityBar,
     },
     {
       key: KeyCode.UpArrow,
       command: 'ActivityBar.focusPrevious',
-      when: 'focus.activityBar',
+      when: WhenExpression.FocusActivityBar,
     },
     {
       key: KeyCode.Home,
       command: 'ActivityBar.focusFirst',
-      when: 'focus.activityBar',
+      when: WhenExpression.FocusActivityBar,
     },
     {
       key: KeyCode.PageUp,
       command: 'ActivityBar.focusFirst',
-      when: 'focus.activityBar',
+      when: WhenExpression.FocusActivityBar,
     },
     {
       key: KeyCode.PageDown,
       command: 'ActivityBar.focusLast',
-      when: 'focus.activityBar',
+      when: WhenExpression.FocusActivityBar,
     },
     {
       key: KeyCode.End,
       command: 'ActivityBar.focusLast',
-      when: 'focus.activityBar',
+      when: WhenExpression.FocusActivityBar,
     },
     {
       key: KeyCode.Space,
       command: 'ActivityBar.selectCurrent',
-      when: 'focus.activityBar',
+      when: WhenExpression.FocusActivityBar,
     },
     {
       key: KeyCode.Enter,
       command: 'ActivityBar.selectCurrent',
-      when: 'focus.activityBar',
+      when: WhenExpression.FocusActivityBar,
     },
     {
       key: KeyCode.Home,
       command: 'ActivityBar.focusFirst',
-      when: 'focus.activityBar',
+      when: WhenExpression.FocusActivityBar,
     },
     {
       key: KeyCode.End,
       command: 'ActivityBar.focusLast',
-      when: 'focus.activityBar',
+      when: WhenExpression.FocusActivityBar,
     },
   ]
 }
