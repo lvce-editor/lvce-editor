b09f8aa4246b5b4b77fac0df90b2017d3d2f4de1
Le Vivilet <levivlet@pm.me>
2023-12-04 22:48:12 +0100

    refactor: move dom element prop into separate file

--- packages/renderer-process/src/parts/VirtualDomElementProps/VirtualDomElementProps.js
+++ packages/renderer-process/src/parts/VirtualDomElementProps/VirtualDomElementProps.js
@@ -1,44 +1,7 @@
-export const setProp = ($Element, key, value) => {
-  switch (key) {
-    case 'maskImage':
-      $Element.style.maskImage = `url('${value}')`
-      $Element.style.webkitMaskImage = `url('${value}')`
-      break
-    case 'paddingLeft':
-    case 'top':
-    case 'left':
-    case 'marginTop':
-      if (typeof value === 'number') {
-        $Element.style[key] = `${value}px`
-      } else {
-        $Element.style[key] = value
-      }
-      break
-    case 'translate':
-      $Element.style[key] = value
-      break
-    case 'width':
-    case 'height':
-      if ($Element instanceof HTMLImageElement) {
-        $Element[key] = value
-      } else if (typeof value === 'number') {
-        $Element.style[key] = `${value}px`
-      } else {
-        $Element.style[key] = value
-      }
-      break
-    case 'style':
-      throw new Error('style property is not supported')
-    case 'childCount':
-    case 'type':
-      break
-    default:
-      $Element[key] = value
-  }
-}
+import * as VirtualDomElementProp from '../VirtualDomElementProp/VirtualDomElementProp.js'
 
 export const setProps = ($Element, props) => {
   for (const key in props) {
-    setProp($Element, key, props[key])
+    VirtualDomElementProp.setProp($Element, key, props[key])
   }
 }
