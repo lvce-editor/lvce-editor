0330ca0764124a63fdff53adefca7d3d67ffefe4
Le Vivilet <levivlet@pm.me>
2023-12-06 22:36:54 +0100

    fix command

--- packages/renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponentCommand.js
+++ packages/renderer-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponentCommand.js
@@ -1,5 +1,9 @@
+import * as Command from '../Command/Command.js'
 import * as ExtensionHostCommand from '../ExtensionHost/ExtensionHostCommands.js'
 
 export const execute = async (id, ...args) => {
+  if (id === 'PointerCapture.mock' || id === 'PointerCapture.unmock') {
+    return Command.execute(id)
+  }
   await ExtensionHostCommand.executeCommand(id)
 }
