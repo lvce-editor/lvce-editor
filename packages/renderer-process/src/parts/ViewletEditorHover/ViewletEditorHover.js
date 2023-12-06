5e4d1f0bc9ac9f36144ee8bb82c5ac12ed08c0f7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-06 22:45:59 +0100

    refactor: remove unused code (#4327)

--- packages/renderer-process/src/parts/ViewletEditorHover/ViewletEditorHover.js
+++ packages/renderer-process/src/parts/ViewletEditorHover/ViewletEditorHover.js
@@ -1,6 +1,4 @@
-import * as AriaRoles from '../AriaRoles/AriaRoles.js'
 import * as SetBounds from '../SetBounds/SetBounds.js'
-import * as SetInnerHtml from '../SetInnerHtml/SetInnerHtml.js'
 import * as VirtualDom from '../VirtualDom/VirtualDom.js'
 
 export const create = () => {
@@ -9,22 +7,6 @@ export const create = () => {
   return {
     $Viewlet,
   }
-}
-
-export const attachEvents = (state) => {
-  const { $Viewlet } = state
-}
-
-export const setHover = (state, sanitzedHtml, documentation) => {
-  const { $Viewlet } = state
-  const $DisplayString = document.createElement('div')
-  $DisplayString.className = 'HoverDisplayString'
-  $DisplayString.role = AriaRoles.Code
-  SetInnerHtml.setInnerHtml($DisplayString, sanitzedHtml)
-  const $Documentation = document.createElement('div')
-  $Documentation.className = 'HoverDocumentation'
-  $Documentation.textContent = documentation
-  $Viewlet.append($DisplayString, $Documentation)
 }
 
 export const setDom = (state, dom) => {
