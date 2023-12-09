c5ff2594211209fa867a9f66178e934aa0c8ecf2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-09 13:09:38 +0100

    feature: improve statusbar test (#4348)

--- packages/extension-host-worker-tests/src/sample.source-control-provider-status-bar-item.js
+++ packages/extension-host-worker-tests/src/sample.source-control-provider-status-bar-item.js
@@ -1,17 +1,17 @@
-export const skip = true
-
 export const name = 'sample.source-control-provider-status-bar-item'
 
-export const test = async ({ FileSystem, Workspace, Extension, StatusBar }) => {
+export const test = async ({ FileSystem, Workspace, Extension, Locator, expect, StatusBar }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await Workspace.setPath(tmpDir)
   await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
 
   // act
+  await StatusBar.update()
 
   // assert
-  const statusBarItems = await StatusBar.getStatusBarItems()
-  console.log({ statusBarItems })
+  const statusBarItems = Locator('.StatusBarItemsLeft')
+  await expect(statusBarItems).toBeVisible()
+
   // TODO statusBarItem should be visible here
 }
