a66dbe13a40124a0c4cf8876c1aecad5c8c769fd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-10 16:42:19 +0100

    feature: add link decoration functions (#4356)

---
+++ packages/renderer-worker/src/parts/GetPossibleLinkDecoration/GetPossibleLinkDecoration.js
@@ -0,0 +1,20 @@
+export const getPossibleLinkDecoration = (editor, position) => {
+  const { decorations, lines } = editor
+  let rowIndex = 0
+  let offset = 0
+  let lineIndex = 0
+  for (let i = 0; i < decorations.length; i += 4) {
+    const decorationOffset = decorations[i]
+    if (decorationOffset >= offset && rowIndex === position.rowIndex) {
+      return {
+        url: 'https://example.com',
+      }
+    }
+    while (offset < decorationOffset && lineIndex < lines.length) {
+      lineIndex++
+      const line = lines[i]
+      offset += line.length
+    }
+  }
+  return undefined
+}
