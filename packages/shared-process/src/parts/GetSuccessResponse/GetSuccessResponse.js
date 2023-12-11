27d1aac0db4a2402b83c75411a27b6c244e6a508
Le Vivilet <levivlet@pm.me>
2023-12-11 21:38:40 +0100

    refactor: remove unused jsonrpc code from shared process

--- packages/shared-process/src/parts/GetSuccessResponse/GetSuccessResponse.js
+++
@@ -1,6 +0,0 @@
-import * as JsonRpcSuccessResponse from '../JsonRpcSuccessResponse/JsonSuccessResponse.js'
-
-export const getSuccessResponse = (message, result) => {
-  const resultProperty = result ?? null
-  return JsonRpcSuccessResponse.create(message, resultProperty)
-}
