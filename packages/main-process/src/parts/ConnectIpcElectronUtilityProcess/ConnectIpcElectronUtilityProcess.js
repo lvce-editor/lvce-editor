a21781dcf581b264c1af860a99001060bd11e0be
Le Vivilet <levivlet@pm.me>
2023-12-11 22:59:15 +0100

    feature: create application window from shared process

--- packages/main-process/src/parts/ConnectIpcElectronUtilityProcess/ConnectIpcElectronUtilityProcess.js
+++ packages/main-process/src/parts/ConnectIpcElectronUtilityProcess/ConnectIpcElectronUtilityProcess.js
@@ -1,8 +1,5 @@
 import * as TransferMessagePortMain from '../TransferMessagePortMain/TransferMessagePortMain.js'
-import * as Performance from '../Performance/Performance.js'
-import * as PerformanceMarkerType from '../PerformanceMarkerType/PerformanceMarkerType.js'
 
 export const connectIpc = async (ipc, browserWindowPort, ...params) => {
   await TransferMessagePortMain.transferMessagePortMain(ipc, browserWindowPort, ...params)
-  Performance.mark(PerformanceMarkerType.DidStartSharedProcess)
 }
--- packages/main-process/src/parts/HandleMessagePortForSharedProcess/HandleMessagePortForSharedProcess.js
+++ packages/main-process/src/parts/HandleMessagePortForSharedProcess/HandleMessagePortForSharedProcess.js
@@ -3,8 +3,6 @@ import * as AppWindowStates from '../AppWindowStates/AppWindowStates.js'
 import * as ConnectIpc from '../ConnectIpc/ConnectIpc.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as Logger from '../Logger/Logger.js'
-import * as Performance from '../Performance/Performance.js'
-import * as PerformanceMarkerType from '../PerformanceMarkerType/PerformanceMarkerType.js'
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
 
 // TODO maybe handle critical (first render) request via ipcMain
@@ -38,7 +36,6 @@ export const handlePort = async (event, browserWindowPort, type, name) => {
     return
   }
   const folder = getFolder(config.parsedArgs)
-  Performance.mark(PerformanceMarkerType.WillStartSharedProcess)
   const method = IpcParentType.ElectronUtilityProcess
   const sharedProcess = await SharedProcess.hydrate({
     method,
