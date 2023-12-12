05e9a92b26ef6b56168e243c375346897deb7b1e
Le Vivilet <levivlet@pm.me>
2023-12-12 19:42:50 +0100

    feature: remove setPriority function call from shared process to avoid potential side effects

--- packages/shared-process/src/parts/Main/Main.js
+++ packages/shared-process/src/parts/Main/Main.js
@@ -1,4 +1,3 @@
-import { setPriority } from 'node:os'
 import * as Command from '../Command/Command.js'
 import * as Module from '../Module/Module.js'
 import * as ParentIpc from '../ParentIpc/ParentIpc.js'
@@ -11,5 +10,4 @@ export const main = async () => {
   process.on(Signal.SIGTERM, ProcessListeners.handleSigTerm)
   process.on('uncaughtExceptionMonitor', ProcessListeners.handleUncaughtExceptionMonitor)
   await ParentIpc.listen()
-  setPriority(process.pid, 19)
 }
