1055dea3324d16160962734aef609bfb3ebdd88f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-12 19:57:28 +0100

    refactor: move handleUncaughtExceptionMonitor function into separate … (#4397)

--- packages/shared-process/src/parts/Main/Main.js
+++ packages/shared-process/src/parts/Main/Main.js
@@ -1,5 +1,6 @@
 import { setPriority } from 'node:os'
 import * as Command from '../Command/Command.js'
+import * as HandleUncaughtExceptionMonitor from '../HandleUncaughtExceptionMonitor/HandleUncaughtExceptionMonitor.js'
 import * as Module from '../Module/Module.js'
 import * as ParentIpc from '../ParentIpc/ParentIpc.js'
 import * as ProcessListeners from '../ProcessListeners/ProcessListeners.js'
@@ -9,7 +10,7 @@ export const main = async () => {
   Command.setLoad(Module.load)
   process.on('disconnect', ProcessListeners.handleDisconnect)
   process.on(Signal.SIGTERM, ProcessListeners.handleSigTerm)
-  process.on('uncaughtExceptionMonitor', ProcessListeners.handleUncaughtExceptionMonitor)
+  process.on('uncaughtExceptionMonitor', HandleUncaughtExceptionMonitor.handleUncaughtExceptionMonitor)
   await ParentIpc.listen()
   setPriority(process.pid, 19)
 }
