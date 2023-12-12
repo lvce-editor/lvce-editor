c5296a40a4d0cac3f7f57f18f4b543e69f4e0202
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-12 19:56:02 +0100

    refactor: simplify electron window code, move electron window code to… (#4394)

--- packages/main-process/src/parts/ElectronWindow/ElectronWindow.ipc.js
+++ packages/main-process/src/parts/ElectronWindow/ElectronWindow.ipc.js
@@ -3,14 +3,9 @@ import * as ElectronWindow from './ElectronWindow.js'
 export const name = 'ElectronWindow'
 
 export const Commands = {
-  close: ElectronWindow.wrapWindowCommand(ElectronWindow.close),
-  focus: ElectronWindow.wrapWindowCommand(ElectronWindow.focus),
-  maximize: ElectronWindow.wrapWindowCommand(ElectronWindow.maximize),
-  minimize: ElectronWindow.wrapWindowCommand(ElectronWindow.minimize),
-  reload: ElectronWindow.wrapWindowCommand(ElectronWindow.reload),
-  toggleDevtools: ElectronWindow.wrapWindowCommand(ElectronWindow.toggleDevtools),
-  unmaximize: ElectronWindow.wrapWindowCommand(ElectronWindow.unmaximize),
   zoomIn: ElectronWindow.wrapWindowCommand(ElectronWindow.zoomIn),
   zoomOut: ElectronWindow.wrapWindowCommand(ElectronWindow.zoomOut),
   zoomReset: ElectronWindow.wrapWindowCommand(ElectronWindow.zoomReset),
+  executeWindowFunction: ElectronWindow.executeWindowFunction,
+  executeWebContentsFunction: ElectronWindow.executeWebContentsFunction,
 }
