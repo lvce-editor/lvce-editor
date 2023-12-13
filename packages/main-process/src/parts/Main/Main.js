9f09d7441ad743c90cd1044da1b534ce39582357
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-13 19:45:06 +0100

    refactor: remove unused import (#4401)

--- packages/main-process/src/parts/Main/Main.js
+++ packages/main-process/src/parts/Main/Main.js
@@ -1,12 +1,11 @@
 // @ts-ignore
 performance.mark('code/start')
-import * as Command from '../Command/Command.js'
-import * as Module from '../Module/Module.js'
-import * as ErrorHandling from '../ErrorHandling/ErrorHandling.js'
 import * as App from '../App/App.js'
-import * as SetStackTraceLimit from '../SetStackTraceLimit/SetStackTraceLimit.js'
-import * as SharedProcess from '../SharedProcess/SharedProcess.js'
+import * as Command from '../Command/Command.js'
+import * as ErrorHandling from '../ErrorHandling/ErrorHandling.js'
+import * as Module from '../Module/Module.js'
 import * as Process from '../Process/Process.js'
+import * as SetStackTraceLimit from '../SetStackTraceLimit/SetStackTraceLimit.js'
 
 export const main = async () => {
   SetStackTraceLimit.setStackTraceLimit(20)
