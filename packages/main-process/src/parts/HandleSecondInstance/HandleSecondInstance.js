a9e0e7160100dfa5a3a7dff2e8da2c0b9bdbb144
Le Vivilet <levivlet@pm.me>
2023-12-13 19:31:21 +0100

    refactor: move handleSecondInstance function into separate file

---
+++ packages/main-process/src/parts/HandleSecondInstance/HandleSecondInstance.js
@@ -0,0 +1,20 @@
+import * as Cli from '../Cli/Cli.js'
+import * as Debug from '../Debug/Debug.js'
+import * as HandleElectronReady from '../HandleElectronReady/HandleElectronReady.js'
+import * as ParseCliArgs from '../ParseCliArgs/ParseCliArgs.js'
+
+export const handleSecondInstance = async (
+  event,
+  commandLine,
+  workingDirectory,
+  additionalData, // additionalData is the actual process.argv https://github.com/electron/electron/pull/30891
+) => {
+  Debug.debug('[info] second instance')
+  const parsedArgs = ParseCliArgs.parseCliArgs(additionalData)
+  Debug.debug('[info] second instance args', additionalData, parsedArgs)
+  const handled = Cli.handleFastCliArgsMaybe(parsedArgs) // TODO don't like the side effect here
+  if (handled) {
+    return
+  }
+  await HandleElectronReady.handleReady(parsedArgs, workingDirectory)
+}
