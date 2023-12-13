73d5c3b4664b29195e574588984dacb23b0c3005
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-13 20:01:14 +0100

    refactor: remove unused appwindow code (#4406)

--- packages/main-process/src/parts/AppWindow/AppWindow.ipc.js
+++ packages/main-process/src/parts/AppWindow/AppWindow.ipc.js
@@ -3,7 +3,5 @@ import * as AppWindow from './AppWindow.js'
 export const name = 'AppWindow'
 
 export const Commands = {
-  createAppWindow: AppWindow.createAppWindow,
   createAppWindow2: AppWindow.createAppWindow2,
-  openNew: AppWindow.openNew,
 }
