d941fdcdf5e9c5659e8b314729c3a505f89069ea
Le Vivilet <levivlet@pm.me>
2023-12-13 19:36:25 +0100

    refactor: simplify handleWindowAllClosed function

--- packages/main-process/src/parts/HandleWindowAllClosed/HandleWindowAllClosed.js
+++ packages/main-process/src/parts/HandleWindowAllClosed/HandleWindowAllClosed.js
@@ -1,12 +1,5 @@
-import * as IpcParentType from '../IpcParentType/IpcParentType.js'
-import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
 
 export const handleWindowAllClosed = async () => {
-  const method = IpcParentType.ElectronUtilityProcess
-  const sharedProcess = await SharedProcess.hydrate({
-    method,
-    env: {},
-  })
-  JsonRpc.send(sharedProcess, 'HandleWindowAllClosed.handleWindowAllClosed')
+  SharedProcess.send('HandleWindowAllClosed.handleWindowAllClosed')
 }
