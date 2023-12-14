77b3c796fd820aa145bb97e7b03772363e25ebfe
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-14 19:02:55 +0100

    refactor: remove console log (#4427)

--- packages/renderer-worker/src/parts/ViewletDebugConsole/ViewletDebugConsoleRender.js
+++ packages/renderer-worker/src/parts/ViewletDebugConsole/ViewletDebugConsoleRender.js
@@ -2,7 +2,6 @@ export const hasFunctionalRender = true
 
 const renderText = {
   isEqual(oldState, newState) {
-    console.log('ren')
     return oldState.text === newState.text
   },
   apply(oldState, newState) {
