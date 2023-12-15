1d4e78f8d09860c91ee2929b650378ca36433129
Le Vivilet <levivlet@pm.me>
2023-12-15 19:20:05 +0100

    fix: error message on uncaught exception being printed twice

--- packages/shared-process/src/parts/HandleUncaughtExceptionMonitor/HandleUncaughtExceptionMonitor.js
+++ packages/shared-process/src/parts/HandleUncaughtExceptionMonitor/HandleUncaughtExceptionMonitor.js
@@ -1,5 +1,15 @@
 import * as ErrorHandling from '../ErrorHandling/ErrorHandling.js'
 
-export const handleUncaughtExceptionMonitor = (error) => {
+const handleUncaughtExceptionMonitor = (error) => {
   ErrorHandling.handleUncaughtExceptionMonitor(error)
+}
+
+export const handleUncaughtException = (error) => {
+  handleUncaughtExceptionMonitor(error)
+  process.exit(1)
+}
+
+export const handleUnhandledRejection = (error) => {
+  handleUncaughtExceptionMonitor(error)
+  process.exit(1)
 }
