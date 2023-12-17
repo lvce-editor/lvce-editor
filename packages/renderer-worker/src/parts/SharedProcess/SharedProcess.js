e7c2583e1c7949f24aab9af1fb159d1ab0d5d749
Le Vivilet <levivlet@pm.me>
2023-12-17 16:33:58 +0100

    refactor: simplify shared process ipc

--- packages/renderer-worker/src/parts/SharedProcess/SharedProcess.js
+++ packages/renderer-worker/src/parts/SharedProcess/SharedProcess.js
@@ -1,41 +1,17 @@
 /* istanbul ignore file */
-import * as Callback from '../Callback/Callback.js'
-import * as Command from '../Command/Command.js'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
-import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
-import * as Logger from '../Logger/Logger.js'
 import * as SharedProcessIpc from '../SharedProcessIpc/SharedProcessIpc.js'
 import * as SharedProcessState from '../SharedProcessState/SharedProcessState.js'
 
-export const handleMessageFromSharedProcess = async (message) => {
-  if (message.method) {
-    const result = await Command.execute(message.method, ...message.params)
-    if (message.id) {
-      SharedProcessState.state.ipc.send({
-        jsonrpc: JsonRpcVersion.Two,
-        id: message.id,
-        result,
-      })
-    }
-  } else if (message.id) {
-    Callback.resolve(message.id, message)
-  } else {
-    Logger.warn('unexpected message from shared process', message)
-  }
-}
-
 export const listen = async () => {
   const ipc = await SharedProcessIpc.listen(IpcParentType.Node)
-  ipc.onmessage = handleMessageFromSharedProcess
+  HandleIpc.handleIpc(ipc)
   SharedProcessState.state.ipc = ipc
 }
 
 export const invoke = async (method, ...params) => {
-  // if (platform === 'web') {
-  //   console.warn('SharedProcess is not available on web')
-  //   return
-  // }
   const result = await JsonRpc.invoke(SharedProcessState.state.ipc, method, ...params)
   return result
 }
