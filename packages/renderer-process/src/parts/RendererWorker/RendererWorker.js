483087f816a739a49cbebcb2f36af4fb503dba33
Le Vivilet <levivlet@pm.me>
2023-12-17 16:29:44 +0100

    refactor: move launchRendererWorker function into separate file

--- packages/renderer-process/src/parts/RendererWorker/RendererWorker.js
+++ packages/renderer-process/src/parts/RendererWorker/RendererWorker.js
@@ -1,10 +1,7 @@
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
-import * as IpcParent from '../IpcParent/IpcParent.js'
-import * as IpcParentType from '../IpcParentType/IpcParentType.js'
-import * as IsElectron from '../IsElectron/IsElectron.js'
 import * as JsonRpcEvent from '../JsonRpcEvent/JsonRpcEvent.js'
 import * as JsonRpcRequest from '../JsonRpcRequest/JsonRpcRequest.js'
-import * as RendererWorkerUrl from '../RendererWorkerUrl/RendererWorkerUrl.js'
+import * as LaunchRendererWorker from '../LaunchRendererWorker/LaunchRendererWorker.js'
 import * as UnwrapJsonRpcResult from '../UnwrapJsonRpcResult/UnwrapJsonRpcResult.js'
 
 export const state = {
@@ -14,32 +11,8 @@ export const state = {
   ipc: undefined,
 }
 
-const getIpc = async () => {
-  const isElectron = IsElectron.isElectron()
-  const name = isElectron ? 'Renderer Worker (Electron)' : 'Renderer Worker'
-  const rendererWorker = await IpcParent.create({
-    method: IpcParentType.Auto,
-    url: RendererWorkerUrl.rendererWorkerUrl,
-    name,
-  })
-  return {
-    send(message) {
-      rendererWorker.postMessage(message)
-    },
-    sendAndTransfer(message, transferables) {
-      rendererWorker.postMessage(message, transferables)
-    },
-    get onmessage() {
-      return rendererWorker.onmessage
-    },
-    set onmessage(listener) {
-      rendererWorker.onmessage = listener
-    },
-  }
-}
-
-export const hydrate = async (config) => {
-  const ipc = await getIpc()
+export const hydrate = async () => {
+  const ipc = await LaunchRendererWorker.launchRendererWorker()
   HandleIpc.handleIpc(ipc)
   state.ipc = ipc
 }
