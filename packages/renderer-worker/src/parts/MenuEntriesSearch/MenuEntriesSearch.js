bd81d53a8fa1f6827dac86417ae97db2b87057d8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-18 22:09:12 +0100

    refactor: move search strings into separate file (#4489)

--- packages/renderer-worker/src/parts/MenuEntriesSearch/MenuEntriesSearch.js
+++ packages/renderer-worker/src/parts/MenuEntriesSearch/MenuEntriesSearch.js
@@ -1,25 +1,17 @@
-import * as I18nString from '../I18NString/I18NString.js'
 import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
-
-/**
- * @enum {string}
- */
-export const UiStrings = {
-  CopyPath: 'Copy Path',
-  Dismiss: 'Dismiss',
-}
+import * as ViewletSearchStrings from '../ViewletSearch/ViewletSearchStrings.js'
 
 export const getMenuEntries = () => {
   return [
     {
       id: 'dismiss',
-      label: I18nString.i18nString(UiStrings.Dismiss),
+      label: ViewletSearchStrings.dismiss(),
       flags: MenuItemFlags.None,
       command: /* TODO */ -1,
     },
     {
       id: 'copyPath',
-      label: I18nString.i18nString(UiStrings.CopyPath),
+      label: ViewletSearchStrings.copyPath(),
       flags: MenuItemFlags.None,
       command: /* TODO */ -1,
     },
