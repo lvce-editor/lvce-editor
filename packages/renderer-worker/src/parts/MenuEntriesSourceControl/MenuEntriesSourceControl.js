1aca20fd52835f21fc5cbb5aaa037c3e001811a8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-18 22:00:48 +0100

    refactor: move source control strings into separate file (#4487)

--- packages/renderer-worker/src/parts/MenuEntriesSourceControl/MenuEntriesSourceControl.js
+++ packages/renderer-worker/src/parts/MenuEntriesSourceControl/MenuEntriesSourceControl.js
@@ -1,59 +1,45 @@
-import * as I18nString from '../I18NString/I18NString.js'
 import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
-
-/**
- * @enum {string}
- */
-export const UiStrings = {
-  OpenChanges: 'Open Changes',
-  OpenFile: 'Open File',
-  OpenFileHead: 'Open File (HEAD)',
-  DiscardChanges: 'Discard Changes',
-  StageChanges: 'Stage Changes',
-  AddToGitignore: 'Add to gitignore',
-  RevealInExplorerView: 'Reveal in Explorer View',
-  OpenContainingFolder: 'Open Containing Folder',
-}
+import * as ViewletSourceControlStrings from '../ViewletSourceControl/ViewletSourceControlStrings.js'
 
 export const getMenuEntries = () => {
   return [
     {
-      label: I18nString.i18nString(UiStrings.OpenChanges),
+      label: ViewletSourceControlStrings.openChanges(),
       flags: MenuItemFlags.None,
       command: /* TODO */ -1,
     },
     {
-      label: I18nString.i18nString(UiStrings.OpenFile),
+      label: ViewletSourceControlStrings.openFile(),
       flags: MenuItemFlags.None,
       command: /* TODO */ -1,
     },
     {
-      label: I18nString.i18nString(UiStrings.OpenFileHead),
+      label: ViewletSourceControlStrings.openFileHead(),
       flags: MenuItemFlags.None,
       command: /* TODO */ -1,
     },
     {
-      label: I18nString.i18nString(UiStrings.DiscardChanges),
+      label: ViewletSourceControlStrings.discardChanges(),
       flags: MenuItemFlags.None,
       command: /* TODO */ -1,
     },
     {
-      label: I18nString.i18nString(UiStrings.StageChanges),
+      label: ViewletSourceControlStrings.stageChanges(),
       flags: MenuItemFlags.None,
       command: /* TODO */ -1,
     },
     {
-      label: I18nString.i18nString(UiStrings.AddToGitignore),
+      label: ViewletSourceControlStrings.addToGitignore(),
       flags: MenuItemFlags.None,
       command: /* TODO */ -1,
     },
     {
-      label: I18nString.i18nString(UiStrings.RevealInExplorerView),
+      label: ViewletSourceControlStrings.revealInExplorerView(),
       flags: MenuItemFlags.None,
       command: /* TODO */ -1,
     },
     {
-      label: I18nString.i18nString(UiStrings.OpenContainingFolder),
+      label: ViewletSourceControlStrings.openContainingFolder(),
       flags: MenuItemFlags.None,
       command: /* TODO */ -1,
     },
