6ff6c370a6157e19aba67e68db3db34f48b4ef34
Le Vivilet <levivlet@pm.me>
2023-12-18 22:11:49 +0100

    refactor: move handleBeforeInstallPrompt function into separate file

--- packages/renderer-process/src/parts/Main/Main.js
+++ packages/renderer-process/src/parts/Main/Main.js
@@ -1,15 +1,11 @@
 import * as Command from '../Command/Command.js'
-import * as Event from '../Event/Event.js'
+import * as HandleBeforeInstallPrompt from '../HandleBeforeInstallPrompt/HandleBeforeInstallPrompt.js'
 import * as HandleContentSecurityPolicyViolation from '../HandleContentSecurityPolicyViolation/HandleContentSecurityPolicyViolation.js'
 import * as Module from '../Module/Module.js'
 import * as Platform from '../Platform/Platform.js'
 import * as PlatformType from '../PlatformType/PlatformType.js'
 import * as RendererWorker from '../RendererWorker/RendererWorker.js'
 import * as UnhandledErrorHandling from '../UnhandledErrorHandling/UnhandledErrorHandling.js'
-
-const handleBeforeInstallPrompt = (event) => {
-  Event.preventDefault(event)
-}
 
 export const main = async () => {
   onerror = UnhandledErrorHandling.handleUnhandledError
@@ -21,7 +17,7 @@ export const main = async () => {
   if (Platform.platform === PlatformType.Web) {
     // disable prompt to download app as pwa
     // @ts-ignore
-    window.onbeforeinstallprompt = handleBeforeInstallPrompt
+    window.onbeforeinstallprompt = HandleBeforeInstallPrompt.handleBeforeInstallPrompt
   }
   // TODO this is discovered very late
   await RendererWorker.hydrate()
