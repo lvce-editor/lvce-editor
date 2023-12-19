c8df574885ed94511dc10180733e96fcdafb1d70
Le Vivilet <levivlet@pm.me>
2023-12-19 22:23:42 +0100

    feature: add menu entries for splitting editors

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMainStrings.js
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainStrings.js
@@ -5,8 +5,28 @@ import * as I18nString from '../I18NString/I18NString.js'
  */
 export const UiStrings = {
   OpenFile: 'Open File',
+  SplitUp: 'Split Up',
+  SplitDown: 'Split Down',
+  SplitLeft: 'Split Left',
+  SplitRight: 'Split Right',
 }
 
 export const openFile = () => {
   return I18nString.i18nString(UiStrings.OpenFile)
+}
+
+export const splitUp = () => {
+  return I18nString.i18nString(UiStrings.SplitUp)
+}
+
+export const splitDown = () => {
+  return I18nString.i18nString(UiStrings.SplitDown)
+}
+
+export const splitLeft = () => {
+  return I18nString.i18nString(UiStrings.SplitLeft)
+}
+
+export const splitRight = () => {
+  return I18nString.i18nString(UiStrings.SplitRight)
 }
