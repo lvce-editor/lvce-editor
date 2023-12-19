c2e81594d4560a6f14d56883ec3504ebd88389b4
Le Vivilet <levivlet@pm.me>
2023-12-19 22:53:30 +0100

    refactor: move loadfont function into separate file

--- packages/renderer-worker/src/parts/Font/Font.js
+++ packages/renderer-worker/src/parts/Font/Font.js
@@ -1,27 +1,8 @@
-import * as Assert from '../Assert/Assert.js'
 import * as FontState from '../FontState/FontState.js'
-import { VError } from '../VError/VError.js'
-
-const getFonts = () => {
-  // @ts-ignore
-  return globalThis.fonts || document.fonts
-}
+import * as LoadFont from '../LoadFont/LoadFont.js'
 
 export const load = async (fontName, fontUrl) => {
-  try {
-    Assert.string(fontName)
-    Assert.string(fontUrl)
-    if (fontName.startsWith('\'')) {
-      throw new Error('font name is not allowed start with quotes')
-    }
-    const fontFace = new FontFace(fontName, fontUrl, {})
-    await fontFace.load()
-    const fonts = getFonts()
-    // @ts-ignore
-    fonts.add(fontFace)
-  } catch (error) {
-    throw new VError(error, `Failed to load font ${fontName}`)
-  }
+  return LoadFont.loadFont(fontName, fontUrl)
 }
 
 export const ensure = async (fontName, fontUrl) => {
