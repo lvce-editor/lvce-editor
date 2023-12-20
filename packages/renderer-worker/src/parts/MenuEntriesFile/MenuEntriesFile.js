575c082598e2939fa8d0cf33f53e0e2feee83db6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-20 10:30:18 +0100

    refactor: move file strings into separate file (#4508)

--- packages/renderer-worker/src/parts/MenuEntriesFile/MenuEntriesFile.js
+++ packages/renderer-worker/src/parts/MenuEntriesFile/MenuEntriesFile.js
@@ -1,72 +1,58 @@
-import * as I18nString from '../I18NString/I18NString.js'
+import * as FileStrings from '../FileStrings/FileStrings.js'
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.js'
 import * as MenuEntrySeparator from '../MenuEntrySeparator/MenuEntrySeparator.js'
 import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
-
-/**
- * @enum {string}
- */
-export const UiStrings = {
-  NewFile: 'New File',
-  NewWindow: 'New Window',
-  OpenFile: 'Open File',
-  OpenFolder: 'Open Folder',
-  OpenRecent: 'Open Recent',
-  Exit: 'Exit',
-  Save: 'Save',
-  SaveAll: 'Save All',
-}
 
 export const getMenuEntries = () => {
   return [
     {
       id: 'newFile',
-      label: I18nString.i18nString(UiStrings.NewFile),
+      label: FileStrings.newFile(),
       flags: MenuItemFlags.None,
       command: -1,
     },
     {
       id: 'newWindow',
-      label: I18nString.i18nString(UiStrings.NewWindow),
+      label: FileStrings.newWindow(),
       flags: MenuItemFlags.None,
       command: /* Window.openNew */ 'Window.openNew',
     },
     MenuEntrySeparator.menuEntrySeparator,
     {
       id: 'openFile',
-      label: I18nString.i18nString(UiStrings.OpenFile),
+      label: FileStrings.openFile(),
       flags: MenuItemFlags.None,
       command: 'Dialog.openFile',
     },
     {
       id: 'openFolder',
-      label: I18nString.i18nString(UiStrings.OpenFolder),
+      label: FileStrings.openFolder(),
       flags: MenuItemFlags.RestoreFocus,
       command: 'Dialog.openFolder',
     },
     {
       id: MenuEntryId.OpenRecent,
-      label: I18nString.i18nString(UiStrings.OpenRecent),
+      label: FileStrings.openRecent(),
       flags: MenuItemFlags.SubMenu,
       command: '',
     },
     MenuEntrySeparator.menuEntrySeparator,
     {
       id: 'save',
-      label: I18nString.i18nString(UiStrings.Save),
+      label: FileStrings.save(),
       flags: MenuItemFlags.None,
       command: 'Main.save',
     },
     {
       id: 'saveAll',
-      label: I18nString.i18nString(UiStrings.SaveAll),
+      label: FileStrings.saveAll(),
       flags: MenuItemFlags.None,
       command: 'Main.saveAll',
     },
     MenuEntrySeparator.menuEntrySeparator,
     {
       id: 'exit',
-      label: I18nString.i18nString(UiStrings.Exit),
+      label: FileStrings.exit(),
       flags: MenuItemFlags.Ignore,
       command: 'Chrome.exit',
     },
