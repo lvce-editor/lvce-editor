d365927bb464db1dea7f02d4b3d738faab6b5566
Le Vivilet <levivlet@pm.me>
2023-12-22 14:45:53 +0100

    refactor: move createElectronSession function into separate file

--- packages/main-process/src/parts/ElectronSession/ElectronSession.js
+++ packages/main-process/src/parts/ElectronSession/ElectronSession.js
@@ -1,22 +1,4 @@
-import * as Electron from 'electron'
-import * as HandleHeadersReceived from '../HandleHeadersReceived/HandleHeadersReceived.js'
-import * as HandlePermission from '../HandlePermission/HandlePermission.js'
-import * as HandleRequest from '../HandleRequest/HandleRequest.js'
-import * as IsSessionCacheEnabled from '../IsSessionCacheEnabled/IsSessionCacheEnabled.js'
-import * as Platform from '../Platform/Platform.js'
-import * as Protocol from '../Protocol/Protocol.js'
-
-const createSession = () => {
-  const sessionId = Platform.getSessionId()
-  const session = Electron.session.fromPartition(sessionId, {
-    cache: IsSessionCacheEnabled.isSessionCacheEnabled,
-  })
-  session.webRequest.onHeadersReceived(HandleHeadersReceived.handleHeadersReceived)
-  session.setPermissionRequestHandler(HandlePermission.handlePermissionRequest)
-  session.setPermissionCheckHandler(HandlePermission.handlePermissionCheck)
-  Protocol.handle(session.protocol, Platform.scheme, HandleRequest.handleRequest)
-  return session
-}
+import * as CreateElectronSession from '../CreateElectronSession/CreateElectronSession.js'
 
 export const state = {
   /**
@@ -27,7 +9,7 @@ export const state = {
 
 export const get = () => {
   if (!state.session) {
-    state.session = createSession()
+    state.session = CreateElectronSession.createElectronSession()
   }
   return state.session
 }
