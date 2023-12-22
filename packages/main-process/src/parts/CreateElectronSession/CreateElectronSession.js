3f8a234b48300e333a95f19f34ded8b8ce4e8f3d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-22 15:03:06 +0100

    refactor: move createElectronSession function into separate file (#4536)

---
+++ packages/main-process/src/parts/CreateElectronSession/CreateElectronSession.js
@@ -0,0 +1,19 @@
+import * as Electron from 'electron'
+import * as HandleHeadersReceived from '../HandleHeadersReceived/HandleHeadersReceived.js'
+import * as HandlePermission from '../HandlePermission/HandlePermission.js'
+import * as HandleRequest from '../HandleRequest/HandleRequest.js'
+import * as IsSessionCacheEnabled from '../IsSessionCacheEnabled/IsSessionCacheEnabled.js'
+import * as Platform from '../Platform/Platform.js'
+import * as Protocol from '../Protocol/Protocol.js'
+
+export const createElectronSession = () => {
+  const sessionId = Platform.getSessionId()
+  const session = Electron.session.fromPartition(sessionId, {
+    cache: IsSessionCacheEnabled.isSessionCacheEnabled,
+  })
+  session.webRequest.onHeadersReceived(HandleHeadersReceived.handleHeadersReceived)
+  session.setPermissionRequestHandler(HandlePermission.handlePermissionRequest)
+  session.setPermissionCheckHandler(HandlePermission.handlePermissionCheck)
+  Protocol.handle(session.protocol, Platform.scheme, HandleRequest.handleRequest)
+  return session
+}
