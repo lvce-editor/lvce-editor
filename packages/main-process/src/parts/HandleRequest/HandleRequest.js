740da9e05d67f4e3af7358361d140e1b75bda966
Le Vivilet <levivlet@pm.me>
2023-12-22 13:09:25 +0100

    refactor: move getElectronFileResponse function into separate file

--- packages/main-process/src/parts/HandleRequest/HandleRequest.js
+++ packages/main-process/src/parts/HandleRequest/HandleRequest.js
@@ -1,23 +1,10 @@
-import * as ElectronSessionGetAbsolutePath from '../ElectronSessionGetAbsolutePath/ElectronSessionGetAbsolutePath.js'
-import * as GetFileResponse from '../GetFileResponse/GetFileResponse.js'
-import * as GetRelativePath from '../GetRelativePath/GetRelativePath.js'
-import * as Platform from '../Platform/Platform.js'
+import * as GetElectronFileResponse from '../GetElectronFileResponse/GetElectronFileResponse.js'
 
 /**
  *
  * @param {GlobalRequest} request
  */
 
-export const handleRequest = async (request) => {
-  const pathName = GetRelativePath.getRelativePath(request.url)
-  const path = ElectronSessionGetAbsolutePath.getAbsolutePath(pathName)
-  const response = await GetFileResponse.getFileResponse(path)
-  if (!Platform.isProduction && request.url === `${Platform.scheme}://-/`) {
-    const text = await response.text()
-    const modifiedText = text.replace('    <link rel="manifest" href="/manifest.json" crossorigin="use-credentials" />\n', '')
-    // @ts-ignore
-    const modifiedResponse = new Response(modifiedText, response)
-    return modifiedResponse
-  }
-  return response
+export const handleRequest = (request) => {
+  return GetElectronFileResponse.getElectronFileResponse(request.url)
 }
