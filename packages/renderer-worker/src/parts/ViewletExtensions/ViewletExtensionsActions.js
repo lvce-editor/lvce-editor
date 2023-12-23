a2ebba6082c9f773533783a6265f78ddc46ca745
Le Vivilet <levivlet@pm.me>
2023-12-23 09:45:50 +0100

    refactor: move extension strings into separate file

--- packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsActions.js
+++ packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsActions.js
@@ -1,30 +1,22 @@
 import * as ActionType from '../ActionType/ActionType.js'
 import * as MaskIcon from '../MaskIcon/MaskIcon.js'
-
-/**
- * @enum {string}
- */
-const UiStrings = {
-  Filter: 'Filter',
-  Refresh: 'Refresh',
-  ClearExtensionSearchResults: 'Clear extension search results',
-}
+import * as ViewletExtensionStrings from '../ViewletExtensions/ViewletExtensionsStrings.js'
 
 export const getActions = () => {
   return [
     {
       type: ActionType.Button,
-      id: UiStrings.Filter,
+      id: ViewletExtensionStrings.filter(),
       icon: MaskIcon.Filter,
     },
     {
       type: ActionType.Button,
-      id: UiStrings.Refresh,
+      id: ViewletExtensionStrings.refresh(),
       icon: MaskIcon.Refresh,
     },
     {
       type: ActionType.Button,
-      id: UiStrings.ClearExtensionSearchResults,
+      id: ViewletExtensionStrings.clearExtensionSearchResults(),
       icon: MaskIcon.ClearAll,
       command: 'Extensions.clearSearchResults',
     },
--- packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsStrings.js
+++ packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsStrings.js
@@ -5,8 +5,23 @@ import * as I18nString from '../I18NString/I18NString.js'
  */
 const UiStrings = {
   NoExtensionsFound: 'No extensions found.',
+  Filter: 'Filter',
+  Refresh: 'Refresh',
+  ClearExtensionSearchResults: 'Clear extension search results',
 }
 
 export const noExtensionsFound = () => {
   return I18nString.i18nString(UiStrings.NoExtensionsFound)
+}
+
+export const filter = () => {
+  return I18nString.i18nString(UiStrings.Filter)
+}
+
+export const refresh = () => {
+  return I18nString.i18nString(UiStrings.Refresh)
+}
+
+export const clearExtensionSearchResults = () => {
+  return I18nString.i18nString(UiStrings.ClearExtensionSearchResults)
 }
