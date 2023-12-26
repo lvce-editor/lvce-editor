8fede1caf9e4be926563e199e19c0dee35dc1a38
Le Vivilet <levivlet@pm.me>
2023-12-26 21:39:03 +0100

    refactor: simplify isElectron variable

--- packages/renderer-process/src/parts/IsElectron/IsElectron.js
+++ packages/renderer-process/src/parts/IsElectron/IsElectron.js
@@ -1,3 +1,4 @@
-export const isElectron = () => {
-  return Boolean(globalThis.isElectron)
-}
+import * as Platform from '../Platform/Platform.js'
+import * as PlatformType from '../PlatformType/PlatformType.js'
+
+export const isElectron = Platform.platform === PlatformType.Electron
--- packages/renderer-process/src/parts/LaunchRendererWorker/LaunchRendererWorker.js
+++ packages/renderer-process/src/parts/LaunchRendererWorker/LaunchRendererWorker.js
@@ -4,8 +4,7 @@ import * as IsElectron from '../IsElectron/IsElectron.js'
 import * as RendererWorkerUrl from '../RendererWorkerUrl/RendererWorkerUrl.js'
 
 export const launchRendererWorker = async () => {
-  const isElectron = IsElectron.isElectron()
-  const name = isElectron ? 'Renderer Worker (Electron)' : 'Renderer Worker'
+  const name = IsElectron.isElectron ? 'Renderer Worker (Electron)' : 'Renderer Worker'
   const rendererWorker = await IpcParent.create({
     method: IpcParentType.Auto,
     url: RendererWorkerUrl.rendererWorkerUrl,
