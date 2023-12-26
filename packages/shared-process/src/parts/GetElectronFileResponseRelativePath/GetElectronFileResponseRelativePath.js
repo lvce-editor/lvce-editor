8288d1c6041fc63bbbe1aead2f5a00f88b4fef93
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-26 08:47:38 +0100

    refactor: simplify getResponseRelativePath function (#4593)

--- packages/shared-process/src/parts/GetElectronFileResponseRelativePath/GetElectronFileResponseRelativePath.js
+++ packages/shared-process/src/parts/GetElectronFileResponseRelativePath/GetElectronFileResponseRelativePath.js
@@ -1,8 +1,14 @@
 import * as Platform from '../Platform/Platform.js'
 
+const { scheme } = Platform
+const prefix = `${scheme}://-`
+const prefixLength = prefix.length
+
 export const getElectronFileResponseRelativePath = (requestUrl) => {
+  if (requestUrl.startsWith('/')) {
+    return requestUrl
+  }
   const decoded = decodeURI(requestUrl)
-  const { scheme } = Platform
-  const pathName = decoded.slice(`${scheme}://-`.length)
+  const pathName = decoded.slice(prefixLength)
   return pathName
 }
