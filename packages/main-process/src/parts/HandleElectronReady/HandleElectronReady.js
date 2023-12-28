9ac2bd746ca810b3cff0987afe63e31daa84165e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-28 13:42:34 +0100

    refactor: simplify electron ready code (#4612)

--- packages/main-process/src/parts/HandleElectronReady/HandleElectronReady.js
+++ packages/main-process/src/parts/HandleElectronReady/HandleElectronReady.js
@@ -1,13 +1,5 @@
-import { JsonRpcEvent } from '../JsonRpc/JsonRpc.js'
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
-import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 
 export const handleReady = async (parsedArgs, workingDirectory) => {
-  // TODO move preferences loading and window creation to shared process
-  const method = IpcParentType.ElectronUtilityProcess
-  const sharedProcess = await SharedProcess.hydrate({
-    method,
-  })
-  const message = JsonRpcEvent.create('HandleElectronReady.handleElectronReady', [parsedArgs, workingDirectory])
-  sharedProcess.send(message)
+  await SharedProcess.send('HandleElectronReady.handleElectronReady', parsedArgs, workingDirectory)
 }
