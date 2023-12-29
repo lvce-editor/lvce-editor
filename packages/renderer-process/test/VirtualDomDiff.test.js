c711a334b86d228aaffd9ab37ff56999cdb00ee0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-30 00:10:43 +0100

    test: add more diff tests (#4667)

--- packages/renderer-process/test/VirtualDomDiff.test.js
+++ packages/renderer-process/test/VirtualDomDiff.test.js
@@ -20,6 +20,76 @@ test('renderDiff - update class name', () => {
   expect($Element.innerHTML).toBe(`<div class="b"></div>`)
 })
 
+test('renderDiff - remove a text node', () => {
+  const oldHtml = `a`
+  const diff = [
+    {
+      type: 'remove',
+      nodes: [0],
+    },
+  ]
+  const $Element = document.createElement('div')
+  $Element.innerHTML = oldHtml
+  VirtualDomDiff.renderDiff($Element, diff)
+  expect($Element.innerHTML).toBe(``)
+})
+
+test('renderDiff - sub node attribute modified', () => {
+  const oldHtml = `<div class="List"><div class="ListItems"></div><div class="ScrollBar"><div class="ScrollBarThumb"></div></div>`
+  const diff = [
+    {
+      type: 'updateProp',
+      index: 3,
+      key: 'height',
+      value: 20,
+    },
+  ]
+  const $Element = document.createElement('div')
+  $Element.innerHTML = oldHtml
+  VirtualDomDiff.renderDiff($Element, diff)
+  expect($Element.innerHTML).toBe(
+    '<div class="List"><div class="ListItems"></div><div class="ScrollBar"><div class="ScrollBarThumb" style="height: 20px;"></div></div></div>',
+  )
+})
+
+test('renderDiff - sub node removed at end', () => {
+  const oldHtml = `<div class="List"><div class="ListItems"></div><div class="ScrollBar"><div class="ScrollBarThumb"></div></div>`
+  const diff = [
+    {
+      type: 'remove',
+      nodes: [2],
+    },
+  ]
+  const $Element = document.createElement('div')
+  $Element.innerHTML = oldHtml
+  VirtualDomDiff.renderDiff($Element, diff)
+  expect($Element.innerHTML).toBe('<div class="List"><div class="ListItems"></div></div>')
+})
+
+test('renderDiff - sub node removed at start', () => {
+  const oldHtml = `<div class="List"><div class="ScrollBar"><div class="ScrollBarThumb"></div><div class="ListItems"></div></div>`
+  const diff = [
+    {
+      index: 1,
+      key: 'className',
+      type: 'updateProp',
+      value: 'ListItems',
+    },
+    {
+      type: 'remove',
+      nodes: [2],
+    },
+    {
+      type: 'remove',
+      nodes: [3],
+    },
+  ]
+  const $Element = document.createElement('div')
+  $Element.innerHTML = oldHtml
+  VirtualDomDiff.renderDiff($Element, diff)
+  expect($Element.innerHTML).toBe('<div class="List"><div class="ListItems"></div></div>')
+})
+
 test.skip('renderDiff - insert node - start', () => {
   const oldHtml = `<div><div class="b"></div></div>`
   const diff = [
@@ -90,4 +160,29 @@ test.skip('renderDiff - toggle replace - collapse', () => {
   $Element.innerHTML = oldHtml
   VirtualDomDiff.renderDiff($Element, diff)
   expect($Element.innerHTML).toBe(``)
+})
+
+test('renderDiff - remove and add nodes', () => {
+  const oldHtml = `<div class="a"><i class="b"></i></div><div class="a"></div>`
+  const diff = [
+    {
+      type: 'remove',
+      nodes: [1],
+    },
+    {
+      index: 2,
+      nodes: [
+        {
+          type: VirtualDomElements.I,
+          className: 'b',
+          childCount: 0,
+        },
+      ],
+      type: 'insert',
+    },
+  ]
+  const $Element = document.createElement('div')
+  $Element.innerHTML = oldHtml
+  VirtualDomDiff.renderDiff($Element, diff)
+  expect($Element.innerHTML).toBe('<div class="a"></div><div class="a"><i class="b"></i></div>')
 })
