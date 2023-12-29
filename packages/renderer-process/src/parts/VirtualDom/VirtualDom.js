cf3a8d4f821cc71a5eadd869fc7162de477ae157
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-29 23:33:19 +0100

    refactor: remove virtual dom diff render function into separate file (#4666)

--- packages/renderer-process/src/parts/VirtualDom/VirtualDom.js
+++ packages/renderer-process/src/parts/VirtualDom/VirtualDom.js
@@ -1,7 +1,6 @@
 import * as Assert from '../Assert/Assert.js'
 import * as ClearNode from '../ClearNode/ClearNode.js'
 import * as VirtualDomElement from '../VirtualDomElement/VirtualDomElement.js'
-import * as VirtualDomElementProp from '../VirtualDomElementProp/VirtualDomElementProp.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 /**
  *
@@ -79,28 +78,4 @@ export const render = (elements) => {
 
 const insert = ($Node, diffItem) => {
   renderInternal($Node, diffItem.nodes)
-}
-
-export const renderDiff = ($Root, diff) => {
-  const iter = document.createNodeIterator($Root, NodeFilter.SHOW_ALL)
-  let i = 0
-  let $Node = iter.nextNode()
-  console.log({ diff })
-  for (let diffIndex = 0; diffIndex < diff.length; diffIndex++) {
-    const diffItem = diff[diffIndex]
-    while (i <= diffItem.index) {
-      $Node = iter.nextNode()
-      i++
-    }
-    switch (diffItem.type) {
-      case 'updateProp':
-        VirtualDomElementProp.setProp($Node, diffItem.key, diffItem.value)
-        break
-      case 'insert':
-        insert($Node, diffItem)
-        break
-      default:
-        break
-    }
-  }
 }
