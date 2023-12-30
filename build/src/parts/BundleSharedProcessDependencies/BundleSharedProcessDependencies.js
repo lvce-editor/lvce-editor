c3ab9996b04faa2ecfccc072f87f8f5828f10ee7
Le Vivilet <levivlet@pm.me>
2023-12-30 17:01:11 +0100

    feature: remove unused files from shared process dependencies

--- build/src/parts/BundleSharedProcessDependencies/BundleSharedProcessDependencies.js
+++ build/src/parts/BundleSharedProcessDependencies/BundleSharedProcessDependencies.js
@@ -51,6 +51,8 @@ export const bundleSharedProcessDependencies = async ({ to, arch, electronVersio
   await Remove.remove(`${to}/node_modules/b4a/browser.js`)
   await Remove.remove(`${to}/node_modules/tail/.nyc_output`)
   await Remove.remove(`${to}/node_modules/which/bin`)
+  await Remove.remove(`${to}/node_modules/symlink-dir/dist/cli.js`)
+  await Remove.remove(`${to}/node_modules/extract-zip/cli.js`)
   await Remove.remove(`${to}/node_modules/@lvce-editor/ripgrep/src/downloadRipGrep.js`)
   await Remove.remove(`${to}/node_modules/@lvce-editor/ripgrep/src/postinstall.js`)
   await RemoveSourceMaps.removeSourceMaps(`${to}/node_modules/cacheable-request`)
