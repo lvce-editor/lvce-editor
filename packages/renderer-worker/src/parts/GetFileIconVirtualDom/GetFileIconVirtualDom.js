6f420fc8a6f87e19e159d2847146513d24834545
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-30 11:39:23 +0100

    refactor: use classnames enum for file icon (#4679)

--- packages/renderer-worker/src/parts/GetFileIconVirtualDom/GetFileIconVirtualDom.js
+++ packages/renderer-worker/src/parts/GetFileIconVirtualDom/GetFileIconVirtualDom.js
@@ -1,9 +1,10 @@
+import * as ClassNames from '../ClassNames/ClassNames.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 
 export const getFileIconVirtualDom = (icon) => {
   return {
     type: VirtualDomElements.Img,
-    className: 'FileIcon',
+    className: ClassNames.FileIcon,
     src: icon,
     childCount: 0,
   }
