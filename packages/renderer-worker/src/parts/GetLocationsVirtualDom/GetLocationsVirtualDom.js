39fee2280d5aac00a0ea322690e7f50d0bcc59b1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-30 11:59:17 +0100

    refactor: use enum for location type (#4682)

--- packages/renderer-worker/src/parts/GetLocationsVirtualDom/GetLocationsVirtualDom.js
+++ packages/renderer-worker/src/parts/GetLocationsVirtualDom/GetLocationsVirtualDom.js
@@ -1,12 +1,13 @@
 import * as AriaRoles from '../AriaRoles/AriaRoles.js'
 import * as ClassNames from '../ClassNames/ClassNames.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+import * as LocationType from '../LocationType/LocationType.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
 
 const getLocationVirtualDom = (location) => {
   const { type, icon, lineText, index, name } = location
   switch (type) {
-    case 'leaf':
+    case LocationType.Leaf:
       return [
         {
           type: VirtualDomElements.Div,
@@ -16,7 +17,7 @@ const getLocationVirtualDom = (location) => {
         },
         text(lineText || '(empty line)'),
       ]
-    case 'collapsed':
+    case LocationType.Collapsed:
       return [
         {
           type: VirtualDomElements.Div,
@@ -27,7 +28,7 @@ const getLocationVirtualDom = (location) => {
         },
         text(name),
       ]
-    case 'expanded':
+    case LocationType.Expanded:
       return [
         {
           type: VirtualDomElements.Div,
