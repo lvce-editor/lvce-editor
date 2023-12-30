ea7b9ee334c270fddc5ec28bc856d2db637d38cc
Le Vivilet <levivlet@pm.me>
2023-12-30 11:05:58 +0100

    refactor: simplify getImageVirtualDom function

--- packages/renderer-worker/src/parts/GetImageVirtualDom/GetImageVirtualDom.js
+++ packages/renderer-worker/src/parts/GetImageVirtualDom/GetImageVirtualDom.js
@@ -2,14 +2,16 @@ import * as ClassNames from '../ClassNames/ClassNames.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
 
+const imageContent = {
+  type: VirtualDomElements.Div,
+  className: ClassNames.ImageContent,
+  childCount: 1,
+}
+
 export const getImageVirtualDom = (src, errorMessage) => {
   if (errorMessage) {
     return [
-      {
-        type: VirtualDomElements.Div,
-        className: ClassNames.ImageContent,
-        childCount: 1,
-      },
+      imageContent,
       {
         type: VirtualDomElements.Div,
         className: ClassNames.ImageErrorMessage,
@@ -19,11 +21,7 @@ export const getImageVirtualDom = (src, errorMessage) => {
     ]
   }
   const dom = [
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.ImageContent,
-      childCount: 1,
-    },
+    imageContent,
     {
       type: VirtualDomElements.Img,
       className: ClassNames.ImageElement,
