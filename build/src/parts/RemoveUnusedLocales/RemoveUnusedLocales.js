141bedb0d1ae52a2630b8b8ee489e712548d6374
Le Vivilet <levivlet@pm.me>
2024-01-01 00:48:19 +0100

    simplify code

--- build/src/parts/RemoveUnusedLocales/RemoveUnusedLocales.js
+++ build/src/parts/RemoveUnusedLocales/RemoveUnusedLocales.js
@@ -13,16 +13,14 @@ const shouldLocaleBeRemovedMacos = (dirent) => {
 
 const removeUnusedLocalesMacos = async ({ arch, product }) => {
   const localesPath1 = `build/.tmp/electron-bundle/${arch}/${product.applicationName}.app/Contents/Resources`
-  const dirents1 = await ReadDir.readDir(localesPath1)
-  const toRemove1 = dirents1.filter(shouldLocaleBeRemovedMacos)
-  for (const dirent of toRemove1) {
-    await Remove.remove(`${localesPath1}/${dirent}`)
-  }
   const localesPath2 = `build/.tmp/electron-bundle/${arch}/${product.applicationName}.app/Contents/Frameworks/Electron.Framework.framework/Versions/A/Resources`
-  const dirents2 = await ReadDir.readDir(localesPath2)
-  const toRemove2 = dirents2.filter(shouldLocaleBeRemovedMacos)
-  for (const dirent of toRemove2) {
-    await Remove.remove(`${localesPath2}/${dirent}`)
+  const localesPaths = [localesPath1, localesPath2]
+  for (const localePath of localesPaths) {
+    const dirents = await ReadDir.readDir(localePath)
+    const toRemove = dirents.filter(shouldLocaleBeRemovedMacos)
+    for (const dirent of toRemove) {
+      await Remove.remove(`${localePath}/${dirent}`)
+    }
   }
 }
 
