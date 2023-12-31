1367ed53239ee091f47edb5f5789af2a9c88c489
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-31 09:14:48 +0100

    fix: don't fail build if source maps can't be removed (#4690)

--- build/src/parts/RemoveSourceMaps/RemoveSourceMaps.js
+++ build/src/parts/RemoveSourceMaps/RemoveSourceMaps.js
@@ -1,25 +1,34 @@
+import { VError } from '@lvce-editor/verror'
+import { existsSync } from 'fs'
 import { readdir } from 'fs/promises'
+import * as IsEnoentError from '../IsEnoentError/IsEnoentError.js'
 import * as Path from '../Path/Path.js'
 import * as Remove from '../Remove/Remove.js'
 import * as Replace from '../Replace/Replace.js'
-import { existsSync } from 'fs'
 
 export const removeSourceMaps = async (absolutePath) => {
-  const dirents = await readdir(absolutePath, { recursive: true })
-  for (const dirent of dirents) {
-    const absoluteDirentPath = Path.join(absolutePath, dirent)
-    if (dirent.endsWith('.js')) {
-      const sourceMapPath = absoluteDirentPath + '.map'
-      if (!existsSync(sourceMapPath)) {
-        continue
+  try {
+    const dirents = await readdir(absolutePath, { recursive: true })
+    for (const dirent of dirents) {
+      const absoluteDirentPath = Path.join(absolutePath, dirent)
+      if (dirent.endsWith('.js')) {
+        const sourceMapPath = absoluteDirentPath + '.map'
+        if (!existsSync(sourceMapPath)) {
+          continue
+        }
+        await Remove.remove(sourceMapPath)
+        const baseName = Path.baseName(sourceMapPath)
+        await Replace.replace({
+          path: absoluteDirentPath,
+          occurrence: `//# sourceMappingURL=${baseName}`,
+          replacement: '',
+        })
       }
-      await Remove.remove(sourceMapPath)
-      const baseName = Path.baseName(sourceMapPath)
-      await Replace.replace({
-        path: absoluteDirentPath,
-        occurrence: `//# sourceMappingURL=${baseName}`,
-        replacement: '',
-      })
     }
+  } catch (error) {
+    if (IsEnoentError.isEnoentError(error)) {
+      return
+    }
+    throw new VError(error, `failed to remove source maps`)
   }
 }
