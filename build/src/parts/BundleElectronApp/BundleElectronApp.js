0ec9b403ac4e921877e2221a62e76a88f84ad43d
Le Vivilet <levivlet@pm.me>
2023-12-31 13:56:18 +0100

    feature: add separate function for removing locales on macos

--- build/src/parts/BundleElectronApp/BundleElectronApp.js
+++ build/src/parts/BundleElectronApp/BundleElectronApp.js
@@ -103,17 +103,28 @@ const copyElectron = async ({ arch, electronVersion, useInstalledElectronVersion
   }
 }
 
-const shouldLocaleBeRemoved = (locale) => {
+const removeUnusedLocalesMacos = async ({ arch }) => {
+  //  TODO
+}
+
+const shouldLocaleBeRemovedOther = (locale) => {
   return locale !== 'en-US.pak'
 }
 
-const removeUnusedLocales = async ({ arch, isMacos }) => {
+const removeUnusedLocalesOther = async ({ arch }) => {
   const localesPath = `build/.tmp/electron-bundle/${arch}/locales`
   const dirents = await ReadDir.readDir(localesPath)
-  const toRemove = dirents.filter(shouldLocaleBeRemoved)
+  const toRemove = dirents.filter(shouldLocaleBeRemovedOther)
   for (const dirent of toRemove) {
     await Remove.remove(`build/.tmp/electron-bundle/${arch}/locales/${dirent}`)
   }
+}
+
+const removeUnusedLocales = async ({ arch, isMacos }) => {
+  if (isMacos) {
+    return removeUnusedLocalesMacos({ arch })
+  }
+  return removeUnusedLocalesOther({ arch })
 }
 
 const copyDependencies = async ({ cachePath, resourcesPath }) => {
