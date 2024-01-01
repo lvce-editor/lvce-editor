428e0917a7ba81cd0c82172d75cc9e1defa018c1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-01 15:46:41 +0100

    refactor: simplify getPreloadUrl function (#4712)

--- packages/shared-process/src/parts/GetProcessExplorerUrl/GetProcessExplorerUrl.js
+++ packages/shared-process/src/parts/GetProcessExplorerUrl/GetProcessExplorerUrl.js
@@ -1,11 +1,5 @@
-import * as Path from '../Path/Path.js'
 import * as Platform from '../Platform/Platform.js'
-import * as Root from '../Root/Root.js'
 
 export const getProcessExplorerUrl = () => {
   return `${Platform.scheme}://-/packages/main-process/pages/process-explorer/process-explorer.html`
-}
-
-export const getProcessExplorerPreloadUrl = () => {
-  return Path.join(Root.root, 'packages', 'main-process', 'src', 'preload.js')
 }
