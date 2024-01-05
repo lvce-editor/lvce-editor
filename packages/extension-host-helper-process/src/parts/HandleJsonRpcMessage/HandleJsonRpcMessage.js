e0c078e20c9f2bb8dad5282811263bfbb6f613a5
Le Vivilet <levivlet@pm.me>
2024-01-05 17:45:25 +0100

    remove unused code

--- packages/extension-host-helper-process/src/parts/HandleJsonRpcMessage/HandleJsonRpcMessage.js
+++ packages/extension-host-helper-process/src/parts/HandleJsonRpcMessage/HandleJsonRpcMessage.js
@@ -1,6 +1,7 @@
 import * as GetResponse from '../GetResponse/GetResponse.js'
 
 export const handleJsonRpcMessage = async (ipc, message, execute, resolve) => {
+  console.log({ ipc, message })
   if ('result' in message || 'error' in message) {
     resolve(message.id, message)
     return
