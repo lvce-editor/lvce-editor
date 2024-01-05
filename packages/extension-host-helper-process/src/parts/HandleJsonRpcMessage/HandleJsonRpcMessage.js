72da0c6d49ce738ef53547e5af010fccc5f9c7ca
Le Vivilet <levivlet@pm.me>
2024-01-06 00:05:05 +0100

    remove log

--- packages/extension-host-helper-process/src/parts/HandleJsonRpcMessage/HandleJsonRpcMessage.js
+++ packages/extension-host-helper-process/src/parts/HandleJsonRpcMessage/HandleJsonRpcMessage.js
@@ -1,7 +1,6 @@
 import * as GetResponse from '../GetResponse/GetResponse.js'
 
 export const handleJsonRpcMessage = async (ipc, message, execute, resolve) => {
-  console.log({ ipc, message })
   if ('result' in message || 'error' in message) {
     resolve(message.id, message)
     return
