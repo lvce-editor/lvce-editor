1bc6698b86a0cb0d9de8fac4a3ad4b97402f18f5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-06 11:59:54 +0100

    fix: race condition when launching two extension host helper processe… (#4739)

--- packages/shared-process/src/parts/ExtensionHostHelperProcessIpc/ExtensionHostHelperProcessIpc.js
+++ packages/shared-process/src/parts/ExtensionHostHelperProcessIpc/ExtensionHostHelperProcessIpc.js
@@ -1,13 +1,15 @@
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as PlatformPaths from '../PlatformPaths/PlatformPaths.js'
+import * as Id from '../Id/Id.js'
 
 export const create = async ({ method }) => {
   const extensionHostHelperProcessPath = await PlatformPaths.getExtensionHostHelperProcessPath()
+  const id = Id.create()
   const ipc = await IpcParent.create({
     method,
     path: extensionHostHelperProcessPath,
     execArgv: ['--max-old-space-size=60', '--enable-source-maps'],
-    name: 'Extension Host Helper Process',
+    name: `Extension Host Helper Process ${id}`,
   })
   return ipc
 }
