0eddc794d915e7e9b9651e0c8bf010892b57a020
Le Vivilet <levivlet@pm.me>
2024-01-06 12:17:21 +0100

    add todo

--- packages/shared-process/src/parts/HandleMessagePortForExtensionHostHelperProcess/HandleMessagePortForExtensionHostHelperProcess.js
+++ packages/shared-process/src/parts/HandleMessagePortForExtensionHostHelperProcess/HandleMessagePortForExtensionHostHelperProcess.js
@@ -4,6 +4,11 @@ import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 
+// TODO connect this ipc to the renderer worker ipc
+// when renderer worker ipc closes, dispose all
+// matching extension host helper processes
+// unless the app is exiting, in which case the child processes
+// would get cleaned up automatically
 export const handleMessagePortForExtensionHostHelperProcess = async (port) => {
   Assert.object(port)
   const ipc = await ExtensionHostHelperProcessIpc.create({
