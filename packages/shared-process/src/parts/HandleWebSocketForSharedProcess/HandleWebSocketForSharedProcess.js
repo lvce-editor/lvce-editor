f9ec699a6639b627373e9007e717cc5380b6ae91
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-06 18:56:25 +0100

    refactor: simplify websocket code in shared process (#4756)

--- packages/shared-process/src/parts/HandleWebSocketForSharedProcess/HandleWebSocketForSharedProcess.js
+++ packages/shared-process/src/parts/HandleWebSocketForSharedProcess/HandleWebSocketForSharedProcess.js
@@ -1,15 +1,17 @@
+import * as Assert from '../Assert/Assert.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as HandleSocketError from '../HandleSocketError/HandleSocketError.js'
 import * as IpcChild from '../IpcChild/IpcChild.js'
 import * as IpcChildType from '../IpcChildType/IpcChildType.js'
-import * as WebSocketServer from '../WebSocketServer/WebSocketServer.js'
 
-export const handleWebSocket = async (message, handle) => {
+export const handleWebSocket = async (request, handle) => {
+  Assert.object(request)
+  Assert.object(handle)
   handle.on('error', HandleSocketError.handleSocketError)
-  const webSocket = await WebSocketServer.handleUpgrade(message, handle)
   const ipc = await IpcChild.listen({
     method: IpcChildType.WebSocket,
-    webSocket,
+    request,
+    handle,
   })
   HandleIpc.handleIpc(ipc)
 }
