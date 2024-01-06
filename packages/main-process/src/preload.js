d5ced5c9542e018234e9b58b07f90d6d7a4286bf
Le Vivilet <levivlet@pm.me>
2024-01-06 12:01:48 +0100

    feature: only handle first window post message in preload script, then removing the event listener

--- packages/main-process/src/preload.js
+++ packages/main-process/src/preload.js
@@ -2,39 +2,30 @@ const { ipcRenderer, contextBridge } = require('electron')
 
 const channelName = 'port'
 
-const ipcConnect = (message, transfer) => {
-  if (typeof message !== 'object') {
-    throw new TypeError('[preload] message must be of type object')
-  }
-  ipcRenderer.postMessage(channelName, message, transfer)
-}
-
 const handlePort = (event, message) => {
   // @ts-ignore
   const { origin } = location
-  if (event.ports.length === 1) {
-    const port = event.ports[0]
+  const { ports } = event
+  if (ports.length === 1) {
+    const port = ports[0]
     // @ts-ignore
     window.postMessage({ ...message, result: port }, origin, [port])
   } else {
     // @ts-ignore
     window.postMessage(message, origin)
   }
+  ipcRenderer.off(channelName, handlePort)
 }
 
 const handleWindowMessage = (event) => {
   const { data, ports } = event
-  if ('result' in data || 'error' in data) {
-    return
-  }
-  ipcConnect(data, ports)
+  ipcRenderer.postMessage(channelName, data, ports)
 }
 
 const main = () => {
   ipcRenderer.on(channelName, handlePort)
   // @ts-ignore
-  window.addEventListener('message', handleWindowMessage)
-
+  window.addEventListener('message', handleWindowMessage, { once: true })
   contextBridge.exposeInMainWorld('isElectron', true)
 }
 
