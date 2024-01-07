c674a747d8797f25bdb50cd2b62cb90e5e7cc61e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-08 00:43:27 +0100

    feature: add about view info icon (#4797)

--- packages/renderer-worker/src/parts/AboutStrings/AboutStrings.js
+++ packages/renderer-worker/src/parts/AboutStrings/AboutStrings.js
@@ -1,15 +1,37 @@
+import * as I18nString from '../I18NString/I18NString.js'
+
 /**
  * @enum {string}
  */
 const UiStrings = {
   Ok: 'Ok',
   Copy: 'Copy',
+  Version: 'Version',
+  Commit: 'Commit',
+  Date: 'Date',
+  Browser: 'Browser',
 }
 
 export const ok = () => {
-  return UiStrings.Ok
+  return I18nString.i18nString(UiStrings.Ok)
 }
 
 export const copy = () => {
-  return UiStrings.Copy
+  return I18nString.i18nString(UiStrings.Copy)
+}
+
+export const version = () => {
+  return I18nString.i18nString(UiStrings.Version)
+}
+
+export const commit = () => {
+  return I18nString.i18nString(UiStrings.Commit)
+}
+
+export const date = () => {
+  return I18nString.i18nString(UiStrings.Date)
+}
+
+export const browser = () => {
+  return I18nString.i18nString(UiStrings.Browser)
 }
