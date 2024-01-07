df367a9ca127de05706ecc2e7e2ed938c95f41bd
Le Vivilet <levivlet@pm.me>
2024-01-07 22:39:25 +0100

    refactor: move about electron function into separate file

--- packages/renderer-worker/src/parts/About/About.js
+++ packages/renderer-worker/src/parts/About/About.js
@@ -1,43 +1,16 @@
-import * as AboutStrings from '../AboutStrings/AboutStrings.js'
-import * as ClipBoard from '../ClipBoard/ClipBoard.js'
-import * as ElectronDialog from '../ElectronDialog/ElectronDialog.js'
-import * as ElectronMessageBoxType from '../ElectronMessageBoxType/ElectronMessageBoxType.js'
-import * as GetAboutDetailString from '../GetAboutDetailString/GetAboutDetailString.js'
-import * as GetWindowId from '../GetWindowId/GetWindowId.js'
+import * as AboutElectron from '../AboutElectron/AboutElectron.js'
 import * as Logger from '../Logger/Logger.js'
 import * as Platform from '../Platform/Platform.js'
 import * as PlatformType from '../PlatformType/PlatformType.js'
-import * as Product from '../Product/Product.js'
 
 const showAboutDefault = async () => {
   Logger.warn('show about - not implemented')
 }
 
-const showAboutElectron = async () => {
-  const windowId = await GetWindowId.getWindowId()
-  const detail = await GetAboutDetailString.getDetailString()
-  const productNameLong = await Product.getProductNameLong()
-  const options = {
-    windowId,
-    message: productNameLong,
-    buttons: [AboutStrings.copy(), AboutStrings.ok()],
-    type: ElectronMessageBoxType.Info,
-    detail,
-  }
-  const index = await ElectronDialog.showMessageBox(options)
-  switch (index) {
-    case 0:
-      await ClipBoard.writeText(detail)
-      break
-    default:
-      break
-  }
-}
-
 const getFn = () => {
   switch (Platform.platform) {
     case PlatformType.Electron:
-      return showAboutElectron
+      return AboutElectron.showAboutElectron
     default:
       return showAboutDefault
   }
