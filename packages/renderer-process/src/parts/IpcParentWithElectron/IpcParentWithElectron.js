c9c82f5cc82a2450b86753493696f1c63b1aa818
Le Vivilet <levivlet@pm.me>
2024-01-07 12:01:43 +0100

    refactor: simplify electron ipc

--- packages/renderer-process/src/parts/IpcParentWithElectron/IpcParentWithElectron.js
+++ packages/renderer-process/src/parts/IpcParentWithElectron/IpcParentWithElectron.js
@@ -1,29 +1,15 @@
-import * as Callback from '../Callback/Callback.js'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcChildWithWindow from '../IpcChildWithWindow/IpcChildWithWindow.js'
 import * as IsElectron from '../IsElectron/IsElectron.js'
-import * as JsonRpcRequest from '../JsonRpcRequest/JsonRpcRequest.js'
-import * as UnwrapJsonRpcResult from '../UnwrapJsonRpcResult/UnwrapJsonRpcResult.js'
-
-const handleMessageFromWindow = (event) => {
-  const { origin, data } = event
-  if (origin !== location.origin) {
-    return
-  }
-  if ('method' in data) {
-    return
-  }
-  Callback.resolve(data.id, data)
-}
-
-// @ts-ignore
-window.addEventListener('message', handleMessageFromWindow)
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 
 export const create = async ({ type, name, port }) => {
-  const { message, promise } = JsonRpcRequest.create('CreateMessagePort.createMessagePort', [type, name])
   if (!IsElectron.isElectron) {
     throw new Error('Electron api was requested but is not available')
   }
-  window.postMessage(message, '*', [port])
-  const responseMessage = await promise
-  UnwrapJsonRpcResult.unwrapJsonRpcResult(responseMessage)
+  const windowIpc = IpcChildWithWindow.wrap(window)
+  HandleIpc.handleIpc(windowIpc)
+  await JsonRpc.invokeAndTransfer(windowIpc, [port], 'CreateMessagePort.createMessagePort', type, name)
+  windowIpc.dispose()
   return undefined
 }
