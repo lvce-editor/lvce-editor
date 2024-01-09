c7393323898f00b0fde50ce6f0f4372deb5382a2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-09 20:34:06 +0100

    refactor: move parseKeyBinding function into separate file (#4834)

--- packages/renderer-worker/src/parts/ParseKeyBindings/ParseKeyBindings.js
+++ packages/renderer-worker/src/parts/ParseKeyBindings/ParseKeyBindings.js
@@ -1,28 +1,5 @@
-import * as Assert from '../Assert/Assert.js'
-import * as GetKeyCodeString from '../GetKeyCodeString/GetKeyCodeString.js'
-import * as KeyModifier from '../KeyModifier/KeyModifier.js'
-
-const parseKey = (rawKey) => {
-  Assert.number(rawKey)
-  const isCtrl = Boolean(rawKey & KeyModifier.CtrlCmd)
-  const isShift = Boolean(rawKey & KeyModifier.Shift)
-  const keyCode = rawKey & 0x000000FF
-  const key = GetKeyCodeString.getKeyCodeString(keyCode)
-  return {
-    key,
-    isCtrl,
-    isShift,
-  }
-}
-
-const parseKeyBinding = (keyBinding) => {
-  return {
-    ...keyBinding,
-    rawKey: keyBinding.key,
-    ...parseKey(keyBinding.key),
-  }
-}
+import * as ParseKeyBinding from '../ParseKeyBinding/ParseKeyBinding.js'
 
 export const parseKeyBindings = (keyBindings) => {
-  return keyBindings.map(parseKeyBinding)
+  return keyBindings.map(ParseKeyBinding.parseKeyBinding)
 }
