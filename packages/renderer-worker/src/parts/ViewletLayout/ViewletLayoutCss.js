223dc291c7bd05f9fd3506e95da9c9e9ca44ef4e
Le Vivilet <levivlet@pm.me>
2024-01-12 22:48:10 +0100

    refactor: move global css into viewletlayout css

--- packages/renderer-worker/src/parts/ViewletLayout/ViewletLayoutCss.js
+++ packages/renderer-worker/src/parts/ViewletLayout/ViewletLayoutCss.js
@@ -1,1 +1,15 @@
-export const Css = ['/css/parts/ViewletLayout.css', '/css/parts/ViewletSash.css']
+export const Css = [
+  '/css/parts/ViewletLayout.css',
+  '/css/parts/ViewletSash.css',
+  '/css/parts/Viewlet.css',
+
+  // TODO remove these
+  '/css/parts/AriaAlert.css',
+  '/css/parts/Menu.css',
+  '/css/parts/Notification.css',
+  '/css/parts/ContextMenu.css',
+  '/css/parts/Widgets.css',
+  '/css/parts/Mobile.css',
+  '/css/parts/BackDrop.css',
+  '/css/parts/Focus.css',
+]
