48c005953ae4111c78c7e4c8337c57a2cbdc35ea
Le Vivilet <levivlet@pm.me>
2024-01-13 14:50:20 +0100

    fix: wrong keyboard shortcut displayed in menu entries

--- packages/renderer-worker/src/parts/GetCommandKeyBinding/GetCommandKeyBinding.js
+++ packages/renderer-worker/src/parts/GetCommandKeyBinding/GetCommandKeyBinding.js
@@ -1,6 +1,25 @@
-export const getCommandKeyBinding = (keyBindings, command) => {
+const matchesArgs = (a, b) => {
+  console.log({ a, b })
+  if (!a && !b) {
+    return true
+  }
+  if ((!a && b) || (!b && a)) {
+    return false
+  }
+  if (a.length !== b.length) {
+    return false
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+export const getCommandKeyBinding = (keyBindings, command, args) => {
   for (const keyBinding of keyBindings) {
-    if (keyBinding.command === command) {
+    if (keyBinding.command === command && matchesArgs(keyBinding.args, args)) {
       return keyBinding
     }
   }
