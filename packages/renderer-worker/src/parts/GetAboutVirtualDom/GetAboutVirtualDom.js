da958b6118db5841c5130d41f635270fd7b74731
Le Vivilet <levivlet@pm.me>
2024-01-13 13:47:34 +0100

    refactor: simplify about virtual dom

--- packages/renderer-worker/src/parts/GetAboutVirtualDom/GetAboutVirtualDom.js
+++ packages/renderer-worker/src/parts/GetAboutVirtualDom/GetAboutVirtualDom.js
@@ -1,7 +1,5 @@
-import * as AboutStrings from '../AboutStrings/AboutStrings.js'
+import * as GetDialogVirtualDom from '../GetDialogVirtualDom/GetDialogVirtualDom.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
-import * as TabIndex from '../TabIndex/TabIndex.js'
-import * as AriaRoles from '../AriaRoles/AriaRoles.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
 
 const br = {
@@ -17,84 +15,13 @@ const renderLine = (line, index) => {
 }
 
 export const getAboutVirtualDom = (productName, lines, closeMessage, okMessage, copyMessage, infoMessage) => {
-  const dom = [
-    {
-      type: VirtualDomElements.Div,
-      className: 'DialogContent',
-      tabIndex: TabIndex.Focusable,
-      role: AriaRoles.Dialog,
-      ariaModal: 'true',
-      ariaLabelledBy: 'DialogIcon DialogHeading',
-      onFocusIn: 'handleFocusIn',
-      childCount: 3,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'DialogToolBar',
-      childCount: 1,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'DialogClose',
-      ariaLabel: closeMessage,
-      role: AriaRoles.Button,
-      onClick: 'handleClickClose',
-      childCount: 1,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'MaskIcon MaskIconClose',
-      childCount: 0,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'DialogContentWrapper',
-      childCount: 2,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'DialogIcon DialogInfoIcon MaskIcon MaskIconInfo',
-      id: 'DialogIcon',
-      ariaLabel: infoMessage,
-      childCount: 0,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'DialogContentRight',
-      childCount: 2,
-    },
-    {
-      type: VirtualDomElements.Div,
-      id: 'DialogHeading',
-      className: 'DialogHeading',
-      childCount: 1,
-    },
-    text(productName),
+  const content = [
     {
       type: VirtualDomElements.Div,
       className: 'DialogMessage',
       childCount: lines.length * 2 - 1,
     },
     ...lines.flatMap(renderLine),
-    {
-      type: VirtualDomElements.Div,
-      className: 'DialogButtons',
-      childCount: 2,
-    },
-    {
-      type: VirtualDomElements.Button,
-      className: 'Button ButtonSecondary',
-      onClick: 'handleClickOk',
-      childCount: 1,
-    },
-    text(okMessage),
-    {
-      type: VirtualDomElements.Button,
-      className: 'Button ButtonPrimary',
-      onClick: 'handleClickCopy',
-      childCount: 1,
-    },
-    text(copyMessage),
   ]
-  return dom
+  return GetDialogVirtualDom.getDialogVirtualDom(content, closeMessage, infoMessage, okMessage, copyMessage, productName)
 }
