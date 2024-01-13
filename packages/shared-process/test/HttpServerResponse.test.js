0d4d27554a2e795eba5dd5b122fb353cf8235cb6
Le Vivilet <levivlet@pm.me>
2024-01-13 16:40:35 +0100

    add test

--- packages/shared-process/test/HttpServerResponse.test.js
+++ packages/shared-process/test/HttpServerResponse.test.js
@@ -1,3 +1,22 @@
+import { Writable } from 'node:stream'
+import * as HttpServerResponse from '../src/parts/HttpServerResponse/HttpServerResponse.js'
+
 test('send', () => {
-  // TODO
+  const request = {}
+  let output = ''
+  const socket = new Writable({
+    write(chunk, encoding, callback) {
+      output += chunk
+      callback()
+    },
+  })
+  const result = {
+    init: {
+      status: 200,
+      headers: {},
+    },
+  }
+  HttpServerResponse.send(request, socket, result)
+  expect(output).toContain(`HTTP/1.1 200 OK`)
+  expect(output).toContain('Connection: close')
 })
