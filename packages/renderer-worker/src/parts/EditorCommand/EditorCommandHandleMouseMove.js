848df3139655648e4d7d438fca3199877ae49f76
Le Vivilet <levivlet@pm.me>
2024-01-13 20:27:31 +0100

    feature: add option to show editor hover on mouseover

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleMouseMove.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandHandleMouseMove.js
@@ -1,57 +1,34 @@
-import * as Hover from '../Hover/Hover.js'
-import * as RendererProcess from '../RendererProcess/RendererProcess.js'
-import * as TextDocument from '../TextDocument/TextDocument.js'
+import * as EditorHoverState from '../EditorHoverState/EditorHoverState.js'
+import * as Viewlet from '../Viewlet/Viewlet.js'
+import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
 import * as EditorPosition from './EditorCommandPosition.js'
 
-const getHover = async (editor, rowIndex, columnIndex) => {
-  const offset = TextDocument.offsetAt(editor, rowIndex, columnIndex)
-  const hover = await Hover.getHover(editor, offset)
-  return hover
-}
-
 const showHover = async (editor, position) => {
-  const hover = await getHover(editor, position.rowIndex, position.columnIndex)
-  if (!hover) {
-    console.log('no hover')
-    return
-  }
-
-  RendererProcess.invoke(
-    /* EditorHover.show */ 6216,
-    /* x */ editor.left + position.columnIndex * editor.columnWidth,
-    /* y */ editor.top + position.rowIndex * editor.rowHeight,
-    /* hover */ hover,
-  )
-  console.log({ hover })
-}
-
-const hoverState = {
-  timeout: -1,
-  x: 0,
-  y: 0,
+  await Viewlet.openWidget(ViewletModuleId.EditorHover, position)
 }
 
 // TODO several things can happen:
 // 1. highlight link when alt key is pressed
 // 2. show hover info
 // 3. selection moves
+// 4. highlight go to definition
 const onHoverIdle = () => {
-  // console.log('hover idle')
-  // const { x, y, editor } = hoverState
-  // console.log({ x, y })
-  // const position = EditorPosition.at(editor, x, y)
-  // console.log({ position })
-  // showHover(editor, position)
+  const { x, y, editor } = EditorHoverState.get()
+  const position = EditorPosition.at(editor, x, y)
+  showHover(editor, position)
 }
 
+const hoverDelay = 1000
+
 export const handleMouseMove = (editor, x, y) => {
-  if (hoverState.timeout !== -1) {
-    clearTimeout(hoverState.timeout)
+  if (!editor.hoverEnabled) {
+    return
   }
-  // @ts-ignore
-  hoverState.timeout = setTimeout(onHoverIdle, 1000)
-  hoverState.x = x
-  hoverState.y = y
-  // hoverState.editor = editor
+  const oldState = EditorHoverState.get()
+  if (oldState.timeout !== -1) {
+    clearTimeout(oldState.timeout)
+  }
+  const timeout = setTimeout(onHoverIdle, hoverDelay)
+  EditorHoverState.set(editor, timeout, x, y)
   return editor
 }
