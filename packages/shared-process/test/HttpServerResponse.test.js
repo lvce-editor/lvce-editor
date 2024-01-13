717e571b97493e2b588a61f31480f0c272fe9baf
Le Vivilet <levivlet@pm.me>
2024-01-13 16:41:17 +0100

    add test

--- packages/shared-process/test/HttpServerResponse.test.js
+++ packages/shared-process/test/HttpServerResponse.test.js
@@ -20,3 +20,23 @@ test('send', () => {
   expect(output).toContain(`HTTP/1.1 200 OK`)
   expect(output).toContain('Connection: close')
 })
+
+test('send - error response', () => {
+  const request = {}
+  let output = ''
+  const socket = new Writable({
+    write(chunk, encoding, callback) {
+      output += chunk
+      callback()
+    },
+  })
+  const result = {
+    init: {
+      status: 404,
+      headers: {},
+    },
+  }
+  HttpServerResponse.send(request, socket, result)
+  expect(output).toContain(`HTTP/1.1 404 Not Found`)
+  expect(output).toContain('Connection: close')
+})
