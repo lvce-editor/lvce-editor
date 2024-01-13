b6633e18ab532eb61e609efeb484a3c11cbbd149
Le Vivilet <levivlet@pm.me>
2024-01-13 22:38:12 +0100

    add organize imports command

---
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandOrganizeImports.js
@@ -0,0 +1,7 @@
+export const organizeImports = (editor) => {
+  console.log('TODO: organize imports')
+  // 1. ask extension host for import edits
+  // 2. if edits are empty, ignore
+  // 3. else, apply import edits and return new editor
+  return editor
+}
