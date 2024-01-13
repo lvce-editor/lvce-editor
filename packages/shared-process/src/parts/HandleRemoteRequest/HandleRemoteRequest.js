5c984c26bd659052a409d79610c8abb2e40b8101
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-13 16:58:30 +0100

    refactor: move http server response function into separate file (#4884)

--- packages/shared-process/src/parts/HandleRemoteRequest/HandleRemoteRequest.js
+++ packages/shared-process/src/parts/HandleRemoteRequest/HandleRemoteRequest.js
@@ -1,18 +1,10 @@
-import { ServerResponse } from 'node:http'
 import * as Assert from '../Assert/Assert.js'
 import * as GetElectronFileResponse from '../GetElectronFileResponse/GetElectronFileResponse.js'
-import * as SetHeaders from '../SetHeaders/SetHeaders.js'
+import * as HttpServerResponse from '../HttpServerResponse/HttpServerResponse.js'
 
 export const handleRemoteRequest = async (request, socket) => {
   Assert.object(request)
   Assert.object(socket)
-  const response = new ServerResponse(request)
-  response.assignSocket(socket)
   const result = await GetElectronFileResponse.getElectronFileResponse(request.url, request)
-  response.statusCode = result.init.status
-  SetHeaders.setHeaders(response, {
-    ...result.init.headers,
-    Connection: 'close',
-  })
-  response.end(result.body)
+  HttpServerResponse.send(request, socket, result)
 }
