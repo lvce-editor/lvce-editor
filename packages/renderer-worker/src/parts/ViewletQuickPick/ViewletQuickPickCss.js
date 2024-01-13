53fa08265423a5665b26bddb956efab792dca35d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-13 16:39:43 +0100

    refactor: don't use important keyword for quickpick hover (#4882)

--- packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickCss.js
+++ packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickCss.js
@@ -1,3 +1,9 @@
-export const Css = ['/css/parts/ViewletQuickPick.css', '/css/parts/InputBox.css', '/css/parts/FileIcon.css', '/css/parts/Progress.css']
+export const Css = [
+  '/css/parts/FileIcon.css',
+  '/css/parts/InputBox.css',
+  '/css/parts/Label.css',
+  '/css/parts/Progress.css',
+  '/css/parts/ViewletQuickPick.css',
+]
 
 export const Variables = ['QuickPickBackground', 'ListForeground', 'ListHoverBackground', 'ListHoverForeground', 'ListActiveSelectionBackground']
