be3116f833d0b9cccc8b50cdaeb8a012081c7182
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-13 10:15:41 +0100

    fix: missing line-height variable in diff editor (#4872)

--- packages/renderer-worker/src/parts/ViewletDiffEditor/ViewletDiffEditorCss.js
+++ packages/renderer-worker/src/parts/ViewletDiffEditor/ViewletDiffEditorCss.js
@@ -1,4 +1,3 @@
-export const Css = [
-  '/css/parts/ViewletDiffEditor.css',
-  '/css/parts/ViewletEditor.css',
-]
+export const Css = ['/css/parts/ViewletDiffEditor.css', '/css/parts/ViewletEditor.css']
+
+export { getDynamicCss } from '../ViewletEditorText/ViewletEditorTextCss.js'
