ea5d13a5bdf65a84834354a54d50e45449ae4ed5
Le Vivilet <levivlet@pm.me>
2024-01-13 13:26:07 +0100

    feature: use image element for title bar icon instead of background image

--- packages/renderer-process/src/parts/ViewletTitleBarIcon/ViewletTitleBarIcon.js
+++ packages/renderer-process/src/parts/ViewletTitleBarIcon/ViewletTitleBarIcon.js
@@ -1,7 +1,14 @@
+import * as VirtualDom from '../VirtualDom/VirtualDom.js'
+
 export const create = () => {
   const $Viewlet = document.createElement('div')
   $Viewlet.className = 'Viewlet TitleBarIcon'
   return {
     $Viewlet,
   }
+}
+
+export const setDom = (state, dom) => {
+  const { $Viewlet } = state
+  VirtualDom.renderInto($Viewlet, dom)
 }
