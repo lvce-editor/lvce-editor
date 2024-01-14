f65be9770f4800bae903d6fc1ba86c47441b952e
Le Vivilet <levivlet@pm.me>
2024-01-14 11:47:33 +0100

    feature: improve source action styles

--- packages/renderer-worker/src/parts/GetSourceActiomsVirtualDom/GetSourceActionsVirtualDom.js
+++ packages/renderer-worker/src/parts/GetSourceActiomsVirtualDom/GetSourceActionsVirtualDom.js
@@ -5,8 +5,18 @@ const getSourceActionVirtualDom = (sourceAction) => {
   return [
     {
       type: VirtualDomElements.Div,
-      className: 'SourceActionItem',
+      className: 'SourceActionHeading',
       childCount: 1,
+    },
+    text('Source Action'),
+    {
+      type: VirtualDomElements.Div,
+      className: 'SourceActionItem',
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: 'SourceActionIcon MaskIcon MaskIconSymbolFile',
     },
     text(sourceAction.name),
   ]
