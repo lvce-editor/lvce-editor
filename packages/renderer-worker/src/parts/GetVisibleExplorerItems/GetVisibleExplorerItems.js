d0682b2fd282b85f6500015b8781b77511ffa453
Le Vivilet <levivlet@pm.me>
2024-01-14 19:25:27 +0100

    fix: undefined

--- packages/renderer-worker/src/parts/GetVisibleExplorerItems/GetVisibleExplorerItems.js
+++ packages/renderer-worker/src/parts/GetVisibleExplorerItems/GetVisibleExplorerItems.js
@@ -1,6 +1,6 @@
 export const getVisibleExplorerItems = (items, minLineY, maxLineY, focusedIndex) => {
   const visible = []
-  for (let i = minLineY; i < maxLineY; i++) {
+  for (let i = minLineY; i < Math.min(maxLineY, items.length); i++) {
     const item = items[i]
     visible.push({
       ...item,
