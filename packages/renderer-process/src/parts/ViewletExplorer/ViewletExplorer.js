4e07dc5a8edb8c6d9ed0fa43dd8e35e1711e2ee0
Le Vivilet <levivlet@pm.me>
2024-01-14 21:02:36 +0100

    refactor: remove unused explorer code

--- packages/renderer-process/src/parts/ViewletExplorer/ViewletExplorer.js
+++ packages/renderer-process/src/parts/ViewletExplorer/ViewletExplorer.js
@@ -1,17 +1,13 @@
-import * as AriaBoolean from '../AriaBoolean/AriaBoolean.js'
 import * as AriaRoles from '../AriaRoles/AriaRoles.js'
 import * as Assert from '../Assert/Assert.js'
 import * as AttachEvents from '../AttachEvents/AttachEvents.js'
-import * as DirentType from '../DirentType/DirentType.js'
 import * as DomAttributeType from '../DomAttributeType/DomAttributeType.js'
 import * as DomEventOptions from '../DomEventOptions/DomEventOptions.js'
 import * as DomEventType from '../DomEventType/DomEventType.js'
-import * as FileIcon from '../FileIcon/FileIcon.js'
 import * as InputBox from '../InputBox/InputBox.js'
-import * as Label from '../Label/Label.js'
 import * as RendererWorker from '../RendererWorker/RendererWorker.js'
-import * as WhenExpression from '../WhenExpression/WhenExpression.js'
 import * as VirtualDom from '../VirtualDom/VirtualDom.js'
+import * as WhenExpression from '../WhenExpression/WhenExpression.js'
 import * as ViewletExplorerEvents from './ViewletExplorerEvents.js'
 
 const activeId = 'TreeItemActive'
@@ -47,102 +43,11 @@ export const attachEvents = (state) => {
   })
 }
 
-const create$Row = () => {
-  const $Row = document.createElement('div')
-  $Row.role = AriaRoles.TreeItem
-  $Row.className = 'TreeItem'
-  $Row.draggable = true
-  const $Label = Label.create('')
-  const $Icon = FileIcon.create('')
-  $Row.append($Icon, $Label)
-  return $Row
-}
-
-// TODO rename to renderDirent
-const render$Row = ($Row, rowInfo) => {
-  const $Icon = $Row.childNodes[0]
-  const $LabelText = $Row.childNodes[1].childNodes[0]
-  FileIcon.setIcon($Icon, rowInfo.icon)
-  $LabelText.data = rowInfo.name
-  $Row.title = rowInfo.path
-  $Row.ariaSetSize = `${rowInfo.setSize}`
-  // TODO bug with windows narrator
-  // windows narrator reads heading level 1
-  $Row.ariaLevel = `${rowInfo.depth}`
-  $Row.ariaPosInSet = `${rowInfo.posInSet}`
-  $Row.style.paddingLeft = `${rowInfo.depth * defaultIndent}rem`
-  $Row.ariaLabel = rowInfo.name
-  $Row.ariaDescription = ''
-  switch (rowInfo.type) {
-    // TODO decide on directory vs folder
-    case DirentType.Directory:
-      $Row.ariaExpanded = AriaBoolean.False
-      break
-    case DirentType.DirectoryExpanding:
-      $Row.ariaExpanded = AriaBoolean.True // TODO tree should be aria-busy then
-      break
-    case DirentType.DirectoryExpanded:
-      $Row.ariaExpanded = AriaBoolean.True
-      break
-    case DirentType.File:
-      $Row.ariaExpanded = undefined
-      break
-    default:
-      break
-  }
-}
-
-const render$RowsLess = ($Rows, rowInfos) => {
-  for (let i = 0; i < $Rows.children.length; i++) {
-    render$Row($Rows.children[i], rowInfos[i])
-  }
-  const fragment = document.createDocumentFragment()
-  for (let i = $Rows.children.length; i < rowInfos.length; i++) {
-    const $Row = create$Row()
-    render$Row($Row, rowInfos[i])
-    fragment.append($Row)
-  }
-  $Rows.append(fragment)
-}
-
-const render$RowsEqual = ($Rows, rowInfos) => {
-  for (let i = 0; i < rowInfos.length; i++) {
-    render$Row($Rows.children[i], rowInfos[i])
-  }
-}
-
-const render$RowsMore = ($Rows, rowInfos) => {
-  for (let i = 0; i < rowInfos.length; i++) {
-    render$Row($Rows.children[i], rowInfos[i])
-  }
-  const diff = $Rows.children.length - rowInfos.length
-  for (let i = 0; i < diff; i++) {
-    $Rows.lastChild.remove()
-  }
-}
-
-const render$Rows = ($Rows, rowInfos) => {
-  if ($Rows.children.length < rowInfos.length) {
-    render$RowsLess($Rows, rowInfos)
-  } else if ($Rows.children.length === rowInfos.length) {
-    render$RowsEqual($Rows, rowInfos)
-  } else {
-    render$RowsMore($Rows, rowInfos)
-  }
-}
-
 export const handleError = (state, message) => {
   Assert.object(state)
   Assert.string(message)
   const { $Viewlet } = state
   $Viewlet.textContent = message
-}
-
-export const updateDirents = (state, dirents) => {
-  Assert.object(state)
-  Assert.array(dirents)
-  const { $Viewlet } = state
-  render$Rows($Viewlet, dirents)
 }
 
 export const setFocusedIndex = (state, oldIndex, newIndex, focused) => {
@@ -199,18 +104,6 @@ export const setFocusedIndex = (state, oldIndex, newIndex, focused) => {
 
 export const dispose = (state) => {}
 
-export const hoverIndex = (state, oldIndex, newIndex) => {
-  Assert.object(state)
-  Assert.number(oldIndex)
-  Assert.number(newIndex)
-  if (oldIndex !== -1) {
-    const $OldItem = state.$Viewlet.children[oldIndex]
-    $OldItem.classList.remove('Hover')
-  }
-  const $NewItem = state.$Viewlet.children[newIndex]
-  $NewItem.classList.add('Hover')
-}
-
 export const replaceWithEditBox = (state, index, value) => {
   const { $Viewlet } = state
   const $InputBox = InputBox.create()
@@ -232,6 +125,7 @@ export const replaceWithEditBox = (state, index, value) => {
 }
 
 export const insertEditBox = (state, index, value) => {
+  // TODO use virtual dom for this
   const { $Viewlet } = state
   const $InputBox = InputBox.create()
   $InputBox.value = value
@@ -284,15 +178,11 @@ export const replaceEditBox = (state, index, dirent) => {
   Assert.object(state)
   Assert.number(index)
   const { $Viewlet } = state
-  const $OldRow = $Viewlet.children[index]
-  const $Dirent = create$Row()
-  $Dirent.id = activeId
-  render$Row($Dirent, dirent)
-  $OldRow.replaceWith($Dirent)
   $Viewlet.focus()
 }
 
 export const setDropTargets = (state, oldDropTargets, newDropTargets) => {
+  // TODO use virtual dom for this
   const { $Viewlet } = state
   for (const oldIndex of oldDropTargets) {
     if (oldIndex === -1) {
