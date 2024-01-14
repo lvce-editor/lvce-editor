ad951fe331915d8c5f5f63f06af12efc9bcfc1ec
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-14 22:10:37 +0100

    Discard changes to packages/renderer-worker/src/parts/GetExplorerVirtualDom/GetExplorerVirtualDom.js

--- packages/renderer-worker/src/parts/GetExplorerVirtualDom/GetExplorerVirtualDom.js
+++ packages/renderer-worker/src/parts/GetExplorerVirtualDom/GetExplorerVirtualDom.js
@@ -1,16 +1,16 @@
 import * as AriaRoles from '../AriaRoles/AriaRoles.js'
 import * as ClassNames from '../ClassNames/ClassNames.js'
 import * as DirentType from '../DirentType/DirentType.js'
+import * as GetChevronVirtualDom from '../GetChevronVirtualDom/GetChevronVirtualDom.js'
 import * as GetFileIconVirtualDom from '../GetFileIconVirtualDom/GetFileIconVirtualDom.js'
 import * as GetTreeItemIndent from '../GetTreeItemIndent/GetTreeItemIndent.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
 
-const getItemVirtualDom = (item) => {
-  const { posInSet, setSize, icon, name, path, depth, type, isFocused, isInput } = item
-  if (isInput) {
-    return []
-  }
+const useChevrons = false
+
+const getItemVirtualDomFile = (item) => {
+  const { posInSet, setSize, icon, name, path, depth, isFocused } = item
   const dom = [
     {
       type: VirtualDomElements.Div,
@@ -37,23 +37,80 @@ const getItemVirtualDom = (item) => {
   if (isFocused) {
     dom[0].id = 'TreeItemActive'
   }
+  return dom
+}
+
+const getItemVirtualDomFolder = (item) => {
+  const { posInSet, setSize, icon, name, path, depth, type, isFocused } = item
+  let ariaExpanded = ''
+  let chevron
   switch (type) {
     // TODO decide on directory vs folder
     case DirentType.Directory:
-      dom[0].ariaExpanded = 'false'
+      ariaExpanded = 'false'
+      chevron = GetChevronVirtualDom.getChevronRightVirtualDom()
       break
     case DirentType.DirectoryExpanding:
-      dom[0].ariaExpanded = 'true' // TODO tree should be aria-busy then
+      ariaExpanded = 'true' // TODO tree should be aria-busy then
+      chevron = GetChevronVirtualDom.getChevronRightVirtualDom()
       break
     case DirentType.DirectoryExpanded:
-      dom[0].ariaExpanded = 'true'
+      ariaExpanded = 'true'
+      chevron = GetChevronVirtualDom.getChevronDownVirtualDom()
       break
     case DirentType.File:
       break
     default:
       break
   }
+  const dom = []
+
+  dom.push({
+    type: VirtualDomElements.Div,
+    role: AriaRoles.TreeItem,
+    className: ClassNames.TreeItem,
+    draggable: true,
+    title: path,
+    ariaPosInSet: posInSet,
+    ariaSetSize: setSize,
+    ariaLevel: depth,
+    childCount: 2,
+    paddingLeft: GetTreeItemIndent.getTreeItemIndent(depth),
+    ariaLabel: name,
+    ariaExpanded,
+    ariaDescription: '',
+  })
+
+  if (useChevrons) {
+    dom[0].childCount++
+    dom.push(chevron)
+  }
+  dom.push(
+    GetFileIconVirtualDom.getFileIconVirtualDom(icon),
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Label,
+      childCount: 1,
+    },
+    text(name),
+  )
+  if (isFocused) {
+    // @ts-ignore
+    dom[0].id = 'TreeItemActive'
+  }
   return dom
+}
+
+const getItemVirtualDom = (item) => {
+  const { type } = item
+  switch (type) {
+    case DirentType.Directory:
+    case DirentType.DirectoryExpanding:
+    case DirentType.DirectoryExpanded:
+      return getItemVirtualDomFolder(item)
+    default:
+      return getItemVirtualDomFile(item)
+  }
 }
 
 export const getExplorerVirtualDom = (visibleItems) => {
