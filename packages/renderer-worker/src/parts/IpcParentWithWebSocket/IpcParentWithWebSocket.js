a8fb4ab739fadb31699013c3712b74c5c0260410
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-14 20:38:36 +0100

    feature: reconnect shared process websocket connection on close (#4910)

--- packages/renderer-worker/src/parts/IpcParentWithWebSocket/IpcParentWithWebSocket.js
+++ packages/renderer-worker/src/parts/IpcParentWithWebSocket/IpcParentWithWebSocket.js
@@ -3,13 +3,14 @@ import * as FirstWebSocketEventType from '../FirstWebSocketEventType/FirstWebSoc
 import * as GetWsUrl from '../GetWsUrl/GetWsUrl.js'
 import { IpcError } from '../IpcError/IpcError.js'
 import * as Json from '../Json/Json.js'
+import * as ReconnectingWebSocket from '../ReconnectingWebSocket/ReconnectingWebSocket.js'
 import * as WaitForWebSocketToBeOpen from '../WaitForWebSocketToBeOpen/WaitForWebSocketToBeOpen.js'
 
 export const create = async ({ protocol }) => {
   Assert.string(protocol)
   // TODO replace this during build
   const wsUrl = GetWsUrl.getWsUrl()
-  const webSocket = new WebSocket(wsUrl, [protocol])
+  const webSocket = ReconnectingWebSocket.create(wsUrl, [protocol])
   const { type, event } = await WaitForWebSocketToBeOpen.waitForWebSocketToBeOpen(webSocket)
   if (type === FirstWebSocketEventType.Close) {
     throw new IpcError('Websocket connection was immediately closed')
---
+++ packages/renderer-worker/src/parts/ReconnectingWebSocket/ReconnectingWebSocket.js
@@ -0,0 +1,36 @@
+export const create = (url, args) => {
+  const webSocket = new WebSocket(url, args)
+
+  const reconnect = () => {
+    const originalOnMessage = context.webSocket.onmessage
+    context.webSocket = new WebSocket(url, args)
+    context.webSocket.onmessage = originalOnMessage
+    context.webSocket.onclose = handleClose
+  }
+
+  const handleClose = (event) => {
+    setTimeout(reconnect, 500)
+  }
+
+  const context = {
+    webSocket,
+    get onmessage() {
+      return this.webSocket.onmessage
+    },
+    set onmessage(value) {
+      this.webSocket.onmessage = value
+    },
+    send(message) {
+      this.webSocket.send(message)
+    },
+    addEventListener(type, listener) {
+      this.webSocket.addEventListener(type, listener)
+    },
+    removeEventListener(type, listener) {
+      this.webSocket.removeEventListener(type, listener)
+    },
+  }
+
+  webSocket.onclose = handleClose
+  return context
+}
