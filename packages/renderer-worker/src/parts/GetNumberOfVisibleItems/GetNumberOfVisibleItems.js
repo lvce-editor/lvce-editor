b533b21842fdf064d4f58ce53f070e36744e1988
Le Vivilet <levivlet@pm.me>
2024-01-19 23:13:51 +0100

    fix: extension virtual list item being hidden to early sometimes

--- packages/renderer-worker/src/parts/GetNumberOfVisibleItems/GetNumberOfVisibleItems.js
+++ packages/renderer-worker/src/parts/GetNumberOfVisibleItems/GetNumberOfVisibleItems.js
@@ -1,3 +1,6 @@
+// TODO optimize this function to return the minimum number
+// of visible items needed, e.g. when not scrolled 5 items with
+// 20px fill 100px but when scrolled 6 items are needed
 export const getNumberOfVisibleItems = (listHeight, itemHeight) => {
-  return Math.ceil(listHeight / itemHeight)
+  return Math.ceil(listHeight / itemHeight) + 1
 }
