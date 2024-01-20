a502b2113ce747425ff8a7fbb1b393489bb552ad
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-20 23:18:46 +0100

    feature: add classname for debug symbol values (#4976)

---
+++ packages/renderer-worker/src/parts/GetDebugValueClassName/GetDebugValueClassName.js
@@ -0,0 +1,15 @@
+import * as ClassNames from '../ClassNames/ClassNames.js'
+import * as DebugValueType from '../DebugValueType/DebugValueType.js'
+
+export const getDebugValueClassName = (valueType) => {
+  switch (valueType) {
+    case DebugValueType.Undefined:
+      return ClassNames.DebugValueUndefined
+    case DebugValueType.Number:
+      return ClassNames.DebugValueNumber
+    case DebugValueType.Symbol:
+      return ClassNames.DebugValueSymbol
+    default:
+      return ''
+  }
+}
