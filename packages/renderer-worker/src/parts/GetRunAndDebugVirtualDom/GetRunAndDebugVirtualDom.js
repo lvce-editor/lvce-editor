3488b763646681abc277e71cdc9bff408a282070
Le Vivilet <levivlet@pm.me>
2024-01-20 21:51:17 +0100

    refactor: move debug buttons dom into separate file

--- packages/renderer-worker/src/parts/GetRunAndDebugVirtualDom/GetRunAndDebugVirtualDom.js
+++ packages/renderer-worker/src/parts/GetRunAndDebugVirtualDom/GetRunAndDebugVirtualDom.js
@@ -1,12 +1,10 @@
 import * as ClassNames from '../ClassNames/ClassNames.js'
 import * as DebugScopeChainType from '../DebugScopeChainType/DebugScopeChainType.js'
-import * as DebugState from '../DebugState/DebugState.js'
 import * as DebugValueType from '../DebugValueType/DebugValueType.js'
+import * as GetRunAndDebugButtonsVirtualDom from '../GetRunAndDebugButtonsVirtualDom/GetRunAndDebugButtonsVirtualDom.js'
 import * as ViewletRunAndDebugStrings from '../ViewletRunAndDebug/ViewletRunAndDebugStrings.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
-
-const DebugButton = ClassNames.IconButton + ' ' + ClassNames.DebugButton
 
 /**
  * @enum {string}
@@ -14,90 +12,6 @@ const DebugButton = ClassNames.IconButton + ' ' + ClassNames.DebugButton
 const Roles = {
   TreeItem: 'treeitem',
   None: 'none',
-}
-
-const buttonResume = {
-  type: VirtualDomElements.Button,
-  className: DebugButton,
-  title: ViewletRunAndDebugStrings.resume(),
-  childCount: 1,
-}
-
-const iconContinue = {
-  type: VirtualDomElements.Div,
-  className: `MaskIcon MaskIconDebugContinue DebugContinue`,
-  childCount: 0,
-}
-
-const buttonPause = {
-  type: VirtualDomElements.Button,
-  className: DebugButton,
-  title: ViewletRunAndDebugStrings.pause(),
-  childCount: 1,
-}
-
-const iconPause = {
-  type: VirtualDomElements.Div,
-  className: 'MaskIcon MaskIconDebugPause DebugPause',
-  childCount: 0,
-}
-
-const buttonStepOver = {
-  type: VirtualDomElements.Button,
-  className: DebugButton,
-  title: ViewletRunAndDebugStrings.stepOver(),
-  childCount: 1,
-}
-
-const iconStepOver = {
-  type: VirtualDomElements.Div,
-  className: 'MaskIcon MaskIconDebugStepOver DebugStepOver',
-  childCount: 0,
-}
-
-const buttonStepInto = {
-  type: VirtualDomElements.Button,
-  className: DebugButton,
-  title: ViewletRunAndDebugStrings.stepInto(),
-  childCount: 1,
-}
-
-const iconStepInto = {
-  type: VirtualDomElements.Div,
-  className: 'MaskIcon MaskIconDebugStepInto DebugStepInto',
-  childCount: 0,
-}
-
-const buttonStepOut = {
-  type: VirtualDomElements.Button,
-  className: DebugButton,
-  title: ViewletRunAndDebugStrings.stepOut(),
-  childCount: 1,
-}
-
-const iconStepOut = {
-  type: VirtualDomElements.Div,
-  className: 'MaskIcon MaskIconDebugStepOut DebugStepOut',
-  childCount: 0,
-}
-
-const buttons = {
-  type: VirtualDomElements.Div,
-  className: ClassNames.DebugButtons,
-  childCount: 4,
-}
-
-const renderButtons = (state) => {
-  const { debugState } = state
-  const elements = []
-  elements.push(buttons)
-  if (debugState === DebugState.Paused) {
-    elements.push(buttonResume, iconContinue)
-  } else {
-    elements.push(buttonPause, iconPause)
-  }
-  elements.push(buttonStepOver, iconStepOver, buttonStepInto, iconStepInto, buttonStepOut, iconStepOut)
-  return elements
 }
 
 const watchHeader = {
@@ -313,7 +227,7 @@ const renderCallStack = (state) => {
 
 export const getRunAndDebugVirtualDom = (state) => {
   const dom = []
-  dom.push(...renderButtons(state))
+  dom.push(...GetRunAndDebugButtonsVirtualDom.getRunAndDebugButtonsVirtualDom(state.debugState))
   dom.push(...renderWatch(state))
   dom.push(...renderBreakPoints(state))
   dom.push(...renderScope(state))
