11e96b8bd8269e67a9c7f4ec8ed89544eb603d28
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-21 13:18:37 +0100

    refactor: move debug value css into separate file (#4983)

--- packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugCss.js
+++ packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugCss.js
@@ -1,3 +1,9 @@
-export const Css = ['/css/parts/MaskIcon.css', '/css/parts/IconButton.css', '/css/parts/ViewletRunAndDebug.css']
+export const Css = [
+  '/css/parts/DebugButtons.css',
+  '/css/parts/DebugValue.css',
+  '/css/parts/IconButton.css',
+  '/css/parts/MaskIcon.css',
+  '/css/parts/ViewletRunAndDebug.css',
+]
 
 export const Variables = ['TitleBarForeground', 'DebugSectionHeaderBackground', 'SideBarBackground']
