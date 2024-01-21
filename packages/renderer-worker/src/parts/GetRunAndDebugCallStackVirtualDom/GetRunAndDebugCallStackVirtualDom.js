7174f2edffcc80d482b8b6a26291bb46f18313a0
Le Vivilet <levivlet@pm.me>
2024-01-21 20:30:40 +0100

    feature: move debug callstack virtual dom function into separate file

---
+++ packages/renderer-worker/src/parts/GetRunAndDebugCallStackVirtualDom/GetRunAndDebugCallStackVirtualDom.js
@@ -0,0 +1,63 @@
+import * as ClassNames from '../ClassNames/ClassNames.js'
+import * as ViewletRunAndDebugStrings from '../ViewletRunAndDebug/ViewletRunAndDebugStrings.js'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
+
+const iconTriangleRight = {
+  type: VirtualDomElements.Div,
+  className: ClassNames.DebugMaskIcon + ' MaskIconTriangleRight',
+  childCount: 0,
+}
+
+const iconTriangleDown = {
+  type: VirtualDomElements.Div,
+  className: ClassNames.DebugMaskIcon + ' MaskIconTriangleDown',
+  childCount: 0,
+}
+
+const debugRow1 = {
+  type: VirtualDomElements.Div,
+  className: ClassNames.DebugRow,
+  childCount: 1,
+}
+
+const debugPausedMessage = {
+  type: VirtualDomElements.Div,
+  className: ClassNames.DebugPausedMessage,
+  childCount: 1,
+}
+const textNotPaused = text(ViewletRunAndDebugStrings.notPaused())
+
+const headerCallStack = {
+  type: VirtualDomElements.Div,
+  className: ClassNames.DebugSectionHeader,
+  ariaExpanded: false,
+  childCount: 2,
+  onPointerDown: 'handleClickSectionCallstack',
+}
+const headerCallStackExpanded = {
+  type: VirtualDomElements.Div,
+  className: ClassNames.DebugSectionHeader,
+  ariaExpanded: true,
+  childCount: 2,
+  onPointerDown: 'handleClickSectionCallstack',
+}
+const textCallStack = text(ViewletRunAndDebugStrings.callStack())
+
+export const getRunAndDebugCallStackVirtualDom = (state) => {
+  const { callStack, callStackExpanded } = state
+  const elements = []
+  if (callStackExpanded) {
+    elements.push(headerCallStackExpanded, iconTriangleDown, textCallStack)
+    if (callStack.length === 0) {
+      elements.push(debugPausedMessage, textNotPaused)
+    } else {
+      for (const item of callStack) {
+        elements.push(debugRow1, text(item.functionName))
+      }
+    }
+  } else {
+    elements.push(headerCallStack, iconTriangleRight, textCallStack)
+  }
+  return elements
+}
--- packages/renderer-worker/src/parts/GetRunAndDebugVirtualDom/GetRunAndDebugVirtualDom.js
+++ packages/renderer-worker/src/parts/GetRunAndDebugVirtualDom/GetRunAndDebugVirtualDom.js
@@ -1,5 +1,6 @@
 import * as ClassNames from '../ClassNames/ClassNames.js'
 import * as GetRunAndDebugButtonsVirtualDom from '../GetRunAndDebugButtonsVirtualDom/GetRunAndDebugButtonsVirtualDom.js'
+import * as GetRunAndDebugCallStackVirtualDom from '../GetRunAndDebugCallStackVirtualDom/GetRunAndDebugCallStackVirtualDom.js'
 import * as GetRunAndDebugScopeVirtualDom from '../GetRunAndDebugScopeVirtualDom/GetRunAndDebugScopeVirtualDom.js'
 import * as ViewletRunAndDebugStrings from '../ViewletRunAndDebug/ViewletRunAndDebugStrings.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
@@ -14,12 +15,6 @@ const watchHeader = {
 const iconTriangleRight = {
   type: VirtualDomElements.Div,
   className: ClassNames.DebugMaskIcon + ' MaskIconTriangleRight',
-  childCount: 0,
-}
-
-const iconTriangleDown = {
-  type: VirtualDomElements.Div,
-  className: ClassNames.DebugMaskIcon + ' MaskIconTriangleDown',
   childCount: 0,
 }
 
@@ -38,55 +33,8 @@ const breakPointsHeader = {
 
 const textBreakPoints = text(ViewletRunAndDebugStrings.breakPoints())
 
-const debugRow1 = {
-  type: VirtualDomElements.Div,
-  className: ClassNames.DebugRow,
-  childCount: 1,
-}
-
 const renderBreakPoints = (state) => {
   return [breakPointsHeader, iconTriangleRight, textBreakPoints]
-}
-
-const debugPausedMessage = {
-  type: VirtualDomElements.Div,
-  className: ClassNames.DebugPausedMessage,
-  childCount: 1,
-}
-const textNotPaused = text(ViewletRunAndDebugStrings.notPaused())
-
-const headerCallStack = {
-  type: VirtualDomElements.Div,
-  className: ClassNames.DebugSectionHeader,
-  ariaExpanded: false,
-  childCount: 2,
-  onPointerDown: 'handleClickSectionCallstack',
-}
-const headerCallStackExpanded = {
-  type: VirtualDomElements.Div,
-  className: ClassNames.DebugSectionHeader,
-  ariaExpanded: true,
-  childCount: 2,
-  onPointerDown: 'handleClickSectionCallstack',
-}
-const textCallStack = text(ViewletRunAndDebugStrings.callStack())
-
-const renderCallStack = (state) => {
-  const { callStack, callStackExpanded } = state
-  const elements = []
-  if (callStackExpanded) {
-    elements.push(headerCallStackExpanded, iconTriangleDown, textCallStack)
-    if (callStack.length === 0) {
-      elements.push(debugPausedMessage, textNotPaused)
-    } else {
-      for (const item of callStack) {
-        elements.push(debugRow1, text(item.functionName))
-      }
-    }
-  } else {
-    elements.push(headerCallStack, iconTriangleRight, textCallStack)
-  }
-  return elements
 }
 
 export const getRunAndDebugVirtualDom = (state) => {
@@ -95,6 +43,6 @@ export const getRunAndDebugVirtualDom = (state) => {
   dom.push(...renderWatch(state))
   dom.push(...renderBreakPoints(state))
   dom.push(...GetRunAndDebugScopeVirtualDom.getRunAndDebugScopeVirtualDom(state))
-  dom.push(...renderCallStack(state))
+  dom.push(...GetRunAndDebugCallStackVirtualDom.getRunAndDebugCallStackVirtualDom(state))
   return dom
 }
