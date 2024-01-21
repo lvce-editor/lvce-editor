d998a20bb39d60af1b32b569d6f3969b447ca75c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-21 21:08:19 +0100

    feature: add debug object value preview (#5000)

--- packages/renderer-worker/src/parts/GetDebugPropertyValueLabel/GetDebugPropertyValueLabel.js
+++ packages/renderer-worker/src/parts/GetDebugPropertyValueLabel/GetDebugPropertyValueLabel.js
@@ -1,10 +1,26 @@
+const getInnerPreview = (inner) => {
+  if (inner.type === 'string') {
+    return `${inner.name}:'${inner.value}'`
+  }
+  return `${inner.name}:${inner.value}`
+}
+
+const getDebugPropertyValueLabelObject = (property) => {
+  if (property.preview) {
+    const inner = property.preview.properties.map(getInnerPreview).join(',')
+    return `{${inner}}`
+  }
+  return property.description
+}
+
 export const getDebugPropertyValueLabel = (property) => {
   switch (property.type) {
     case 'number':
     case 'symbol':
     case 'function':
-    case 'object':
       return property.description
+    case 'object':
+      return getDebugPropertyValueLabelObject(property)
     case 'undefined':
       return `undefined`
     case 'string':
