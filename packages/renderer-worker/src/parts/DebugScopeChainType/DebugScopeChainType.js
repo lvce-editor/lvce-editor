d930163c72e9d6708699d1ff848f63729530d488
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-23 20:13:12 +0100

    feature: use numeric enum for debug scope chain type (#5013)

--- packages/renderer-worker/src/parts/DebugScopeChainType/DebugScopeChainType.js
+++ packages/renderer-worker/src/parts/DebugScopeChainType/DebugScopeChainType.js
@@ -1,4 +1,4 @@
-export const Scope = 'scope'
-export const This = 'this'
-export const Property = 'property'
-export const Exception = 'exception'
+export const Scope = 1
+export const This = 2
+export const Property = 3
+export const Exception = 4
