a9c4b86db95c455ad8af85bb9ab5b27420b49b17
Le Vivilet <levivlet@pm.me>
2024-01-23 22:23:41 +0100

    feature: use virtual dom for debug console

--- packages/renderer-process/src/parts/ViewletDebugConsole/ViewletDebugConsole.js
+++ packages/renderer-process/src/parts/ViewletDebugConsole/ViewletDebugConsole.js
@@ -1,31 +1,24 @@
-import * as InputBox from '../InputBox/InputBox.js'
+import * as VirtualDom from '../VirtualDom/VirtualDom.js'
 
 export const create = () => {
-  const $DebugConsoleTop = document.createElement('div')
-  $DebugConsoleTop.className = 'DebugConsoleTop'
-
-  const $Input = InputBox.create()
-
-  const $DebugConsoleBottom = document.createElement('div')
-  $DebugConsoleBottom.className = 'DebugConsoleBottom'
-  $DebugConsoleBottom.append($Input)
-
   const $Viewlet = document.createElement('div')
   $Viewlet.className = 'Viewlet DebugConsole'
-  $Viewlet.append($DebugConsoleTop, $DebugConsoleBottom)
   return {
     $Viewlet,
-    $DebugConsoleTop,
-    $Input,
   }
 }
 
+export const setDom = (state, dom) => {
+  const { $Viewlet } = state
+  VirtualDom.renderInto($Viewlet, dom)
+}
+
 export const focus = (state) => {
-  const { $Input } = state
-  $Input.focus()
+  // const { $Input } = state
+  // $Input.focus()
 }
 
 export const setText = (state, text) => {
-  const { $DebugConsoleTop } = state
-  $DebugConsoleTop.textContent = text
+  // const { $DebugConsoleTop } = state
+  // $DebugConsoleTop.textContent = text
 }
