8e6c953636e9c8515d07906227cff9ff50e627f1
Le Vivilet <levivlet@pm.me>
2024-01-23 20:12:18 +0100

    feature: make scopechain collapsible

--- packages/renderer-worker/src/parts/Arrays/Arrays.js
+++ packages/renderer-worker/src/parts/Arrays/Arrays.js
@@ -78,3 +78,11 @@ export const remove = (array, index, deleteCount) => {
 export const toSorted = (array, compare) => {
   return [...array].sort(compare)
 }
+
+export const removeElement = (array, element) => {
+  const index = array.indexOf(element)
+  if (index === -1) {
+    return array
+  }
+  return [...array.slice(0, index), ...array.slice(index + 1)]
+}
