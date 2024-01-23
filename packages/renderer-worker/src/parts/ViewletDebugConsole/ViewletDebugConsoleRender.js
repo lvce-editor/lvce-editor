acaf962de2906183e6c0b53d721d374c79b59165
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-23 22:24:35 +0100

    Discard changes to packages/renderer-worker/src/parts/ViewletDebugConsole/ViewletDebugConsoleRender.js

--- packages/renderer-worker/src/parts/ViewletDebugConsole/ViewletDebugConsoleRender.js
+++ packages/renderer-worker/src/parts/ViewletDebugConsole/ViewletDebugConsoleRender.js
@@ -1,14 +1,11 @@
-import * as GetDebugConsoleVirtualDom from '../GetDebugConsoleVirtualDom/GetDebugConsoleVirtualDom.js'
-
 export const hasFunctionalRender = true
 
 const renderText = {
   isEqual(oldState, newState) {
-    return oldState === newState
+    return oldState.text === newState.text
   },
   apply(oldState, newState) {
-    const dom = GetDebugConsoleVirtualDom.getVirtualDom()
-    return ['setDom', dom]
+    return ['setText', newState.text]
   },
 }
 
