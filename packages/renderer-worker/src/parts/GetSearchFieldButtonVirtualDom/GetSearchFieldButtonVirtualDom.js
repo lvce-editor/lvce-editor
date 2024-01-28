4c7cb664dd1a2919a18612b703217c494df4fb2f
Le Vivilet <levivlet@pm.me>
2024-01-28 17:05:41 +0100

    feature: add getSearchFieldVirtualDom function

---
+++ packages/renderer-worker/src/parts/GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.js
@@ -0,0 +1,20 @@
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+
+export const getSearchFieldButtonVirtualDom = (button) => {
+  const { icon, checked, title } = button
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: `SearchFieldButton ${checked ? 'SearchFieldButtonChecked' : ''}`,
+      title,
+      role: 'checkbox', // TODO use aria roles enum
+      childCount: 1,
+      ariaChecked: checked,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: `MaskIcon ${icon}`,
+      childCount: 0,
+    },
+  ]
+}
