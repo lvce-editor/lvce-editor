33e15806368bcab5f65d150cbb6ad34bbe16db1d
Le Vivilet <levivlet@pm.me>
2024-01-28 18:49:19 +0100

    refactor; simplify search header virtual dom

--- packages/renderer-worker/src/parts/GetSearchHeaderVirtualDom/GetSearchHeaderVirtualDom.js
+++ packages/renderer-worker/src/parts/GetSearchHeaderVirtualDom/GetSearchHeaderVirtualDom.js
@@ -1,4 +1,5 @@
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+import * as GetSearchFieldVirtualDom from '../GetSearchFieldVirtualDom/GetSearchFieldVirtualDom.js'
 
 export const getSearchHeaderVirtualDom = (replaceExpanded, matchCase, matchWholeWord, useRegularExpression) => {
   const headerTopRight = {
@@ -6,10 +7,6 @@ export const getSearchHeaderVirtualDom = (replaceExpanded, matchCase, matchWhole
     className: 'SearchHeaderTopRight',
     childCount: 1,
   }
-
-  /**
-   * @type {any[]}
-   */
   const dom = [
     {
       type: VirtualDomElements.Div,
@@ -37,97 +34,34 @@ export const getSearchHeaderVirtualDom = (replaceExpanded, matchCase, matchWhole
       childCount: 0,
     },
     headerTopRight,
-    {
-      type: VirtualDomElements.Div,
-      className: 'SearchField',
-      childCount: 4,
-    },
-    {
-      type: VirtualDomElements.TextArea,
-      className: 'MultilineInputBox',
-      spellcheck: false,
-      autocapitalize: 'off',
-      autocorrect: 'off',
-      placeholder: 'Search',
-      name: 'search-value',
-      onInput: 'handleInput',
-      childCount: 0,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: `SearchFieldButton ${matchCase ? 'SearchFieldButtonChecked' : ''}`,
-      title: 'Match Case',
-      role: 'checkbox',
-      childCount: 1,
-      ariaChecked: matchCase,
-      'data-command': 'toggleMatchCase',
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'MaskIcon MaskIconCaseSensitive',
-      childCount: 0,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: `SearchFieldButton ${matchWholeWord ? 'SearchFieldButtonChecked' : ''}`,
-      title: 'Match Whole Word',
-      role: 'checkbox',
-      childCount: 1,
-      ariaChecked: matchWholeWord,
-      'data-command': 'toggleMatchWholeWord',
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'MaskIcon MaskIconWholeWord',
-      childCount: 0,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: `SearchFieldButton ${useRegularExpression ? 'SearchFieldButtonChecked' : ''}`,
-      title: 'Use Regular Expression',
-      role: 'checkbox',
-      childCount: 1,
-      ariaChecked: useRegularExpression,
-      'data-command': 'toggleUseRegularExpression',
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'MaskIcon MaskIconRegex',
-      childCount: 0,
-    },
+    ...GetSearchFieldVirtualDom.getSearchFieldVirtualDom('search-value', 'Search', 'handleInput', [
+      {
+        icon: 'MaskIconCaseSensitive',
+        checked: matchCase,
+        title: 'Match Case',
+      },
+      {
+        icon: 'MaskIconWholeWord',
+        checked: matchWholeWord,
+        title: 'Match Whole Word',
+      },
+      {
+        icon: 'MaskIconRegex',
+        checked: useRegularExpression,
+        title: 'Use Regular Expression',
+      },
+    ]),
   ]
   if (replaceExpanded) {
     headerTopRight.childCount++
     dom.push(
-      {
-        type: VirtualDomElements.Div,
-        className: 'SearchField SearchFieldReplace',
-        childCount: 2,
-      },
-      {
-        type: VirtualDomElements.Input,
-        className: 'SearchFieldInput',
-        spellcheck: false,
-        autocapitalize: 'off',
-        inputType: 'text',
-        autocorrect: 'off',
-        placeholder: 'Replace',
-        name: 'search-replace-value',
-        onInput: 'handleReplaceInput',
-        childCount: 0,
-      },
-      {
-        type: VirtualDomElements.Button,
-        className: 'SearchFieldButton',
-        title: 'Replace All',
-        childCount: 1,
-      },
-      {
-        type: VirtualDomElements.Div,
-        className: 'MaskIcon MaskIconReplaceAll',
-        role: 'none',
-        childCount: 0,
-      },
+      ...GetSearchFieldVirtualDom.getSearchFieldVirtualDom('search-replace-value', 'Replace', 'handleReplaceInput', [
+        {
+          icon: 'MaskIconReplaceAll',
+          checked: false,
+          title: 'Replace All',
+        },
+      ]),
     )
   }
   return dom
