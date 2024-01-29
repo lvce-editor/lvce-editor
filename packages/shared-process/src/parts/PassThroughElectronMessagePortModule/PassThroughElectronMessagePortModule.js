332ca70fc0dc5611978d0a9db07979ee27223c07
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-30 00:09:19 +0100

    refactor: remove unused shared process files (#5054)

--- packages/shared-process/src/parts/PassThroughElectronMessagePortModule/PassThroughElectronMessagePortModule.js
+++
@@ -1,8 +0,0 @@
-export const getModule = (type) => {
-  switch (type) {
-    case 'terminal-process':
-      return import('../PassThroughElectronMessageForTerminalProcess/PassThroughElectronMessageForTerminalProcess.js')
-    default:
-      return undefined
-  }
-}
