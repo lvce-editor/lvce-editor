b51f50254d66e11f4b12e8e8beb58c822b2a4c6e
Le Vivilet <levivlet@pm.me>
2024-01-30 19:12:58 +0100

    refactor: simplify pty host ipc

--- packages/pty-host/src/parts/IpcChild/IpcChild.js
+++ packages/pty-host/src/parts/IpcChild/IpcChild.js
@@ -4,6 +4,9 @@ export const listen = async ({ method, ...params }) => {
   const module = await IpcChildModule.getModule(method)
   // @ts-ignore
   const rawIpc = await module.listen(params)
+  if (module.signal) {
+    module.signal(rawIpc)
+  }
   const ipc = module.wrap(rawIpc)
   return ipc
 }
