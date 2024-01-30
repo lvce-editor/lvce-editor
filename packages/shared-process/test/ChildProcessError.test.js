2640fd0a6ecbcecd3e826a12024f9d7a6760046c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-30 21:27:36 +0100

    refactor: remove unused shared process files (#5066)

--- packages/shared-process/test/ChildProcessError.test.js
+++
@@ -1,9 +0,0 @@
-import { ChildProcessError } from '../src/parts/ChildProcessError/ChildProcessError.js'
-
-test('ChildProcessError', () => {
-  const stderr = ``
-  const error = new ChildProcessError(stderr)
-  expect(error).toBeInstanceOf(Error)
-  expect(error.name).toBe('ChildProcessError')
-  expect(error.message).toBe(`child process error`)
-})
