d40494eb79c645bb37921c69d4fbbcb634f40865
Le Vivilet <levivlet@pm.me>
2024-01-31 19:39:21 +0100

    feature: render debug buttons next to the side bar are title

--- packages/renderer-worker/src/parts/GetRunAndDebugVirtualDom/GetRunAndDebugVirtualDom.js
+++ packages/renderer-worker/src/parts/GetRunAndDebugVirtualDom/GetRunAndDebugVirtualDom.js
@@ -1,12 +1,10 @@
 import * as GetRunAndDebugBreakPointsVirtualDom from '../GetRunAndDebugBreakPointsVirtualDom/GetRunAndDebugBreakPointsVirtualDom.js'
-import * as GetRunAndDebugButtonsVirtualDom from '../GetRunAndDebugButtonsVirtualDom/GetRunAndDebugButtonsVirtualDom.js'
 import * as GetRunAndDebugCallStackVirtualDom from '../GetRunAndDebugCallStackVirtualDom/GetRunAndDebugCallStackVirtualDom.js'
 import * as GetRunAndDebugScopeVirtualDom from '../GetRunAndDebugScopeVirtualDom/GetRunAndDebugScopeVirtualDom.js'
 import * as GetRunAndDebugWatchVirtualDom from '../GetRunAndDebugWatchVirtualDom/GetRunAndDebugWatchVirtualDom.js'
 
 export const getRunAndDebugVirtualDom = (state) => {
   const dom = []
-  dom.push(...GetRunAndDebugButtonsVirtualDom.getRunAndDebugButtonsVirtualDom(state.debugState))
   dom.push(...GetRunAndDebugWatchVirtualDom.renderWatch(state))
   dom.push(...GetRunAndDebugBreakPointsVirtualDom.renderBreakPoints(state))
   dom.push(...GetRunAndDebugScopeVirtualDom.getRunAndDebugScopeVirtualDom(state))
