e78ecb86b6c2ef390459f19fcbc223dbd483427e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-31 19:46:42 +0100

    feature: improve problems input box css (#5076)

--- packages/renderer-worker/src/parts/ViewletProblems/ViewletProblemsCss.js
+++ packages/renderer-worker/src/parts/ViewletProblems/ViewletProblemsCss.js
@@ -1,1 +1,1 @@
-export const Css = ['/css/parts/ViewletProblems.css', '/css/parts/Badge.css']
+export const Css = ['/css/parts/ViewletProblems.css', '/css/parts/Badge.css', '/css/parts/InputBox.css']
