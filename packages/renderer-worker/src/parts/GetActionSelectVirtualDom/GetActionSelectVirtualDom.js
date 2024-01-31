2e6f33f7bd7ba7e4e5139625cfb86abd43636a61
Le Vivilet <levivlet@pm.me>
2024-01-31 19:17:44 +0100

    feature: render select options

--- packages/renderer-worker/src/parts/GetActionSelectVirtualDom/GetActionSelectVirtualDom.js
+++ packages/renderer-worker/src/parts/GetActionSelectVirtualDom/GetActionSelectVirtualDom.js
@@ -1,16 +1,25 @@
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
 
+const getOptionVirtualDom = (option) => {
+  return [
+    {
+      type: VirtualDomElements.Option,
+      className: 'Option',
+      childCount: 1,
+    },
+    text(option),
+  ]
+}
+
 export const getActionSelectVirtualDom = (action) => {
+  const { id, options } = action
   return [
     {
       type: VirtualDomElements.Select,
-      childCount: 1,
+      className: 'Select',
+      childCount: options.length,
     },
-    {
-      type: VirtualDomElements.Option,
-      childCount: 1,
-    },
-    text('test'),
+    ...options.flatMap(getOptionVirtualDom),
   ]
 }
