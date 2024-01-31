c747e3ddd88821bfc13e840c426cf1b960248f1a
Le Vivilet <levivlet@pm.me>
2024-01-31 18:56:29 +0100

    refactor: simplify extension host helper process ipc

--- packages/extension-host-helper-process/src/parts/IpcChild/IpcChild.js
+++ packages/extension-host-helper-process/src/parts/IpcChild/IpcChild.js
@@ -3,6 +3,11 @@ import * as IpcChildModule from '../IpcChildModule/IpcChildModule.js'
 export const listen = async ({ method, ...params }) => {
   const module = await IpcChildModule.getModule(method)
   const rawIpc = await module.listen(params)
+  // @ts-ignore
+  if (module.signal) {
+    // @ts-ignore
+    module.signal(rawIpc)
+  }
   const ipc = module.wrap(rawIpc)
   return ipc
 }
