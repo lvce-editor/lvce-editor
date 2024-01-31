f9f620f4910dbeec7d65187cdf1a0c11a69c91f9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-31 19:59:18 +0100

    feature: add function to render problems actions virtual dom (#5082)

---
+++ packages/renderer-worker/src/parts/ViewletProblems/ViewletProblemsRenderActions.js
@@ -0,0 +1,13 @@
+import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.js'
+import * as ViewletProblemsActions from './ViewletProblemsActions.js'
+
+export const renderActions = {
+  isEqual(oldState, newState) {
+    return oldState === newState
+  },
+  apply(oldState, newState) {
+    const actions = ViewletProblemsActions.getActions()
+    const dom = GetActionsVirtualDom.getActionsVirtualDom(actions)
+    return dom
+  },
+}
