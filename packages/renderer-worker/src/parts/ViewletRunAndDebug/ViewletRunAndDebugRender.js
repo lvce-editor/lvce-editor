c25a55acb187db8ddd5986d302808c3389978d60
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-01 20:16:57 +0100

    feature: improve formatting of debug array value (#5096)

--- packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugRender.js
+++ packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugRender.js
@@ -8,10 +8,8 @@ const renderDebug = {
     return false
   },
   apply(oldState, newState) {
-    console.log(newState.scopeChain)
     const oldDom = GetRunAndDebugVirtualDom.getRunAndDebugVirtualDom(oldState)
     const newDom = GetRunAndDebugVirtualDom.getRunAndDebugVirtualDom(newState)
-    // console.log({ oldDom, newDom })
     const diff = DiffDom.diffDom(oldDom, newDom)
     // console.log({ diff })
     // if (first) {
---
+++ packages/renderer-worker/test/GetDebugPropertyValueLabelArray.test.js
@@ -0,0 +1,27 @@
+import * as GetDebugPropertyValueLabelArray from '../src/parts/GetDebugPropertyValueLabelArray/GetDebugPropertyValueLabelArray.js'
+
+test('empty array', () => {
+  const value = {
+    description: 'Array(0)',
+    preview: {
+      properties: [],
+    },
+  }
+  expect(GetDebugPropertyValueLabelArray.getDebugPropertyValueLabelArray(value)).toBe(`(0) []`)
+})
+
+test('array with string value', () => {
+  const value = {
+    description: 'Array(1)',
+    preview: {
+      properties: [
+        {
+          name: '0',
+          type: 'string',
+          value: 'Host',
+        },
+      ],
+    },
+  }
+  expect(GetDebugPropertyValueLabelArray.getDebugPropertyValueLabelArray(value)).toBe(`(1) ["Host"]`)
+})
