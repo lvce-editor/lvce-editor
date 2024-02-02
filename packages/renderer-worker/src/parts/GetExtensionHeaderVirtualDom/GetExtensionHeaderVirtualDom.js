66aa73f8f650e778cd810161528eda91225dc360
Le Vivilet <levivlet@pm.me>
2024-02-02 19:38:03 +0100

    feature: render actions inline

--- packages/renderer-worker/src/parts/GetExtensionHeaderVirtualDom/GetExtensionHeaderVirtualDom.js
+++ packages/renderer-worker/src/parts/GetExtensionHeaderVirtualDom/GetExtensionHeaderVirtualDom.js
@@ -1,6 +1,23 @@
 import * as ClassNames from '../ClassNames/ClassNames.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 import * as GetSearchFieldVirtualDom from '../GetSearchFieldVirtualDom/GetSearchFieldVirtualDom.js'
+import * as ActionType from '../ActionType/ActionType.js'
+import * as MaskIcon from '../MaskIcon/MaskIcon.js'
+import * as ViewletExtensionStrings from '../ViewletExtensions/ViewletExtensionsStrings.js'
+
+const actions = [
+  {
+    type: ActionType.Button,
+    id: ViewletExtensionStrings.clearExtensionSearchResults(),
+    icon: `MaskIcon${MaskIcon.ClearAll}`,
+    command: 'Extensions.clearSearchResults',
+  },
+  {
+    type: ActionType.Button,
+    id: ViewletExtensionStrings.filter(),
+    icon: `MaskIcon${MaskIcon.Filter}`,
+  },
+]
 
 export const getExtensionHeaderVirtualDom = (placeholder) => {
   return [
@@ -9,6 +26,6 @@ export const getExtensionHeaderVirtualDom = (placeholder) => {
       className: ClassNames.ExtensionHeader,
       childCount: 1,
     },
-    ...GetSearchFieldVirtualDom.getSearchFieldVirtualDom('extensions', placeholder, 'handleExtensionsInput', [], []),
+    ...GetSearchFieldVirtualDom.getSearchFieldVirtualDom('extensions', placeholder, 'handleExtensionsInput', actions, []),
   ]
 }
