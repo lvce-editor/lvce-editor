b4091a09bc50a94b321cd1e44a883e24729dc685
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-02 20:24:52 +0100

    feature: add main split quickpick commands (#5102)

---
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainMenuEntries.js
@@ -0,0 +1,20 @@
+export const getQuickPickMenuEntries = () => {
+  return [
+    {
+      id: 'Main.splitRight',
+      label: 'Main: Split Right',
+    },
+    {
+      id: 'Main.splitLeft',
+      label: 'Main: Split Left',
+    },
+    {
+      id: 'Main.splitDown',
+      label: 'Main: Split Down',
+    },
+    {
+      id: 'Main.splitUp',
+      label: 'Main: Split Up',
+    },
+  ]
+}
