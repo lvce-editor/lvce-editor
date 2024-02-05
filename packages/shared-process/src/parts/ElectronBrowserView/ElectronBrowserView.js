ef4a017aa8fa7fdf84e7748c8bfc8c10a773bdbd
Le Vivilet <levivlet@pm.me>
2024-02-05 18:28:12 +0100

    feature: add webcontents event listeners

--- packages/shared-process/src/parts/ElectronBrowserView/ElectronBrowserView.js
+++ packages/shared-process/src/parts/ElectronBrowserView/ElectronBrowserView.js
@@ -27,3 +27,27 @@ export const handleBrowserViewDestroyed = (id) => {
   console.log('destroyed', id)
   ElectronBrowserViewState.remove(id)
 }
+
+export const handleKeyBinding = (id, keyBinding) => {
+  console.log('keybinding')
+}
+
+export const handleWindowOpen = (webContentsId, url) => {
+  // TODO
+}
+
+export const handleWillNavigate = (webContentsId, url) => {
+  // TODO
+}
+
+export const handleDidNavigate = (webContentsId, url) => {
+  // TODO
+}
+
+export const handleContextMenu = (webContentsId, params) => {
+  // TODO
+}
+
+export const handlePageTitleUpdated = (webContentsId, title) => {
+  // TODO
+}
