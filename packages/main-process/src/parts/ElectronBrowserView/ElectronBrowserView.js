1173a77f65c0fc3271d8e5ea1163cd762db42df2
Le Vivilet <levivlet@pm.me>
2024-02-05 19:40:30 +0100

    remove unused code

--- packages/main-process/src/parts/ElectronBrowserView/ElectronBrowserView.js
+++ packages/main-process/src/parts/ElectronBrowserView/ElectronBrowserView.js
@@ -7,34 +7,6 @@ import * as ElectronSessionForBrowserView from '../ElectronSessionForBrowserView
 import * as Logger from '../Logger/Logger.js'
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
 
-const normalizeKey = (key) => {
-  if (key === ' ') {
-    return 'Space'
-  }
-  if (key.length === 1) {
-    return key.toLowerCase()
-  }
-  return key
-}
-
-const getIdentifier = (input) => {
-  let identifier = ''
-  if (input.control) {
-    identifier += 'ctrl+'
-  }
-  if (input.shift) {
-    identifier += 'shift+'
-  }
-  if (input.alt) {
-    identifier += 'alt+'
-  }
-  if (input.meta) {
-    identifier += 'meta+'
-  }
-  identifier += normalizeKey(input.key)
-  return identifier
-}
-
 /**
  *
  * @param {number} id
