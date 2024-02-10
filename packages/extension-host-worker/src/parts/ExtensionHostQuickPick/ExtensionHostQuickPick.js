6426999f8fe94f6819f5ff33f5ccf6f21b0b0c71
Le Vivilet <levivlet@pm.me>
2024-02-10 22:22:43 +0100

    fix: quickpickItems.map is not a function

--- packages/extension-host-worker/src/parts/ExtensionHostQuickPick/ExtensionHostQuickPick.js
+++ packages/extension-host-worker/src/parts/ExtensionHostQuickPick/ExtensionHostQuickPick.js
@@ -2,6 +2,7 @@ import * as Rpc from '../Rpc/Rpc.js'
 import * as RendererWorkerCommandType from '../RendererWorkerCommandType/RendererWorkerCommandType.js'
 
 export const showQuickPick = async ({ getPicks, toPick }) => {
-  const picks = getPicks().map(toPick)
+  const rawPicks = await getPicks()
+  const picks = rawPicks.map(toPick)
   return Rpc.invoke(RendererWorkerCommandType.ExtensionHostQuickPickShow, picks)
 }
