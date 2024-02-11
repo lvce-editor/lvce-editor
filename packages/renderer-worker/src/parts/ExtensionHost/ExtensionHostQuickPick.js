0068d0c3d677ee93fc54f90a1196b5e9e82aef94
Le Vivilet <levivlet@pm.me>
2024-02-11 17:33:38 +0100

    feature: select extension quickpick item

--- packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostQuickPick.js
+++ packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostQuickPick.js
@@ -1,5 +1,8 @@
 import * as Command from '../Command/Command.js'
+import * as Promises from '../Promises/Promises.js'
 
-export const show = (picks) => {
-  return Command.execute(`QuickPick.showCustom`, picks)
+export const show = async (picks) => {
+  const { resolve, promise } = Promises.withResolvers()
+  await Command.execute(`QuickPick.showCustom`, picks, resolve)
+  return promise
 }
