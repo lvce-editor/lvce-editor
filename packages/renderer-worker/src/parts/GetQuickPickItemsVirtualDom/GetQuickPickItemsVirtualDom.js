adc4513f75dad7a962cf8ff41b457d3da3c71921
Le Vivilet <levivlet@pm.me>
2024-02-15 22:54:09 +0100

    refactor: move quickpickItemVirtualDom into separate file

--- packages/renderer-worker/src/parts/GetQuickPickItemsVirtualDom/GetQuickPickItemsVirtualDom.js
+++ packages/renderer-worker/src/parts/GetQuickPickItemsVirtualDom/GetQuickPickItemsVirtualDom.js
@@ -1,79 +1,7 @@
-import * as AriaRoles from '../AriaRoles/AriaRoles.js'
 import * as ClassNames from '../ClassNames/ClassNames.js'
-import * as GetFileIconVirtualDom from '../GetFileIconVirtualDom/GetFileIconVirtualDom.js'
+import * as GetQuickPickItemVirtualDom from '../GetQuickPickItemVirtualDom/GetQuickPickItemVirtualDom.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
-
-const quickPickHighlight = {
-  type: VirtualDomElements.Span,
-  className: ClassNames.QuickPickHighlight,
-  childCount: 1,
-}
-
-const addHighlights = (dom, highlights, label) => {
-  const labelDom = {
-    type: VirtualDomElements.Div,
-    className: ClassNames.Label,
-    childCount: 0,
-  }
-  dom.push(labelDom)
-  let position = 0
-  for (let i = 0; i < highlights.length; i += 2) {
-    const highlightStart = highlights[i]
-    const highlightEnd = highlights[i + 1]
-    if (position < highlightStart) {
-      const beforeText = label.slice(position, highlightStart)
-      labelDom.childCount++
-      dom.push(text(beforeText))
-    }
-    const highlightText = label.slice(highlightStart, highlightEnd)
-    labelDom.childCount++
-    dom.push(quickPickHighlight, text(highlightText))
-    position = highlightEnd
-  }
-  if (position < label.length) {
-    const afterText = label.slice(position)
-    labelDom.childCount++
-    dom.push(text(afterText))
-  }
-}
-
-const getQuickPickItemVirtualDom = (visibleItem) => {
-  const { posInSet, label, setSize, isActive, description, icon, matches } = visibleItem
-  const highlights = matches.slice(1)
-  const dom = []
-  dom.push({
-    type: VirtualDomElements.Div,
-    className: ClassNames.QuickPickItem,
-    role: AriaRoles.Option,
-    ariaPosInSet: posInSet,
-    ariaSetSize: setSize,
-    childCount: 1,
-  })
-  const parent = dom[0]
-  if (isActive) {
-    // @ts-ignore
-    parent.id = 'QuickPickItemActive'
-    parent.className += ' ' + ClassNames.QuickPickItemActive
-  }
-  if (icon) {
-    parent.childCount++
-    dom.push(GetFileIconVirtualDom.getFileIconVirtualDom(icon))
-  }
-  addHighlights(dom, highlights, label)
-  if (description) {
-    parent.childCount++
-    dom.push(
-      {
-        type: VirtualDomElements.Div,
-        className: ClassNames.QuickPickItemDescription,
-        childCount: 1,
-      },
-      text(description),
-    )
-  }
-  return dom
-}
 
 export const getQuickPickItemsVirtualDom = (visibleItems) => {
   if (visibleItems.length === 0) {
@@ -91,6 +19,6 @@ export const getQuickPickItemsVirtualDom = (visibleItems) => {
       text('No Results'),
     ]
   }
-  const dom = visibleItems.flatMap(getQuickPickItemVirtualDom)
+  const dom = visibleItems.flatMap(GetQuickPickItemVirtualDom.getQuickPickItemVirtualDom)
   return dom
 }
