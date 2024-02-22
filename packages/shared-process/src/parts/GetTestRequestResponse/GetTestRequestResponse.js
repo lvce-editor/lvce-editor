4cf3958dacbfc774bcb629af43515b15fc358c33
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-22 20:15:51 +0100

    feature: move more request tests code to shared process (#5197)

---
+++ packages/shared-process/src/parts/GetTestRequestResponse/GetTestRequestResponse.js
@@ -0,0 +1,53 @@
+import { isAbsolute, join } from 'path'
+import * as CreateTestOverview from '../CreateTestOverview/CreateTestOverview.js'
+import * as HttpStatusCode from '../HttpStatusCode/HttpStatusCode.js'
+import * as Root from '../Root/Root.js'
+
+const getPathName = (request) => {
+  const { pathname } = new URL(request.url || '', `https://${request.headers.host}`)
+  return pathname
+}
+
+const getTestPath = () => {
+  if (process.env.TEST_PATH) {
+    const testPath = process.env.TEST_PATH
+    if (isAbsolute(testPath)) {
+      return testPath
+    }
+    return join(process.cwd(), testPath)
+  }
+  return join(Root.root, 'packages', 'extension-host-worker-tests')
+}
+
+export const getTestRequestResponse = async (req) => {
+  const pathName = getPathName(req)
+  if (pathName.endsWith('.html')) {
+    return {
+      body: '',
+      init: {
+        status: HttpStatusCode.Ok,
+        headers: {},
+      },
+    }
+  }
+  if (pathName === '/tests/') {
+    const testPath = getTestPath()
+    const testPathSrc = join(testPath, 'src')
+    const body = await CreateTestOverview.createTestOverview(testPathSrc)
+    return {
+      body,
+      init: {
+        status: HttpStatusCode.MultipleChoices,
+        headers: {
+          'Cache-Control': 'public, max-age=0, must-revalidate',
+        },
+      },
+    }
+  }
+  return {
+    body: 'not-found',
+    init: {
+      status: HttpStatusCode.NotFound,
+    },
+  }
+}
--- packages/shared-process/src/parts/HandleRequestTest/HandleRequestTest.js
+++ packages/shared-process/src/parts/HandleRequestTest/HandleRequestTest.js
@@ -1,11 +1,10 @@
 import * as Assert from '../Assert/Assert.js'
-import * as GetElectronFileResponse from '../GetElectronFileResponse/GetElectronFileResponse.js'
+import * as GetTestRequestResponse from '../GetTestRequestResponse/GetTestRequestResponse.js'
 import * as HttpServerResponse from '../HttpServerResponse/HttpServerResponse.js'
 
 export const handleRequestTest = async (request, socket) => {
   Assert.object(request)
   Assert.object(socket)
-  console.log({ request, socket })
-  const result = await GetElectronFileResponse.getElectronFileResponse(request.url, request)
+  const result = await GetTestRequestResponse.getTestRequestResponse(request)
   HttpServerResponse.send(request, socket, result)
 }
