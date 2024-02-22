2bf965fed051bbf72729cab39850aa6a6dcc90db
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-22 19:35:02 +0100

    feature: add function to generate test overview in shared process (#5196)

---
+++ packages/shared-process/src/parts/HandleRequestTest/HandleRequestTest.ipc.js
@@ -0,0 +1,7 @@
+import * as HandleRequestTest from './HandleRequestTest.js'
+
+export const name = 'HandleRequestTest'
+
+export const Commands = {
+  handleRequestTest: HandleRequestTest.handleRequestTest,
+}
