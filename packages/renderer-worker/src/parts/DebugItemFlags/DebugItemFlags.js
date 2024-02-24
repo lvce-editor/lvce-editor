bd136b43d639adb59f9b6ad4f8d9fc6d140bb7f7
Le Vivilet <levivlet@pm.me>
2024-02-24 20:30:29 +0100

    feature: render chevron icon for expandable debug items

---
+++ packages/renderer-worker/src/parts/DebugItemFlags/DebugItemFlags.js
@@ -0,0 +1,4 @@
+export const None = 0
+export const Expanded = 1 << 0
+export const Collapsed = 1 << 1
+export const Focused = 1 << 2
--- packages/renderer-worker/src/parts/GetScopePropertyVirtualDom/GetScopePropertyVirtualDom.js
+++ packages/renderer-worker/src/parts/GetScopePropertyVirtualDom/GetScopePropertyVirtualDom.js
@@ -1,6 +1,8 @@
 import * as ClassNames from '../ClassNames/ClassNames.js'
 import * as GetDebugValueClassName from '../GetDebugValueClassName/GetDebugValueClassName.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+import * as DebugItemFlags from '../DebugItemFlags/DebugItemFlags.js'
+import * as GetChevronVirtualDom from '../GetChevronVirtualDom/GetChevronVirtualDom.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
 
 const separator = text(': ')
@@ -11,16 +13,28 @@ const debugPropertyKey = {
 }
 
 export const getScopePropertyVirtualDom = (scope) => {
-  const { indent, key, value, valueType } = scope
+  const { indent, key, value, valueType, flags } = scope
   const className = GetDebugValueClassName.getDebugValueClassName(valueType)
-  return [
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.DebugRow,
-      paddingLeft: indent,
-      childCount: 3,
-      onPointerDown: 'handleClickScopeValue',
-    },
+  const isExpanded = flags & DebugItemFlags.Expanded
+  const isCollapsed = flags & DebugItemFlags.Collapsed
+  const isFocused = flags & DebugItemFlags.Focused
+  const dom = []
+  console.log({ scope })
+  dom.push({
+    type: VirtualDomElements.Div,
+    className: ClassNames.DebugRow,
+    paddingLeft: indent,
+    childCount: 3,
+    onPointerDown: 'handleClickScopeValue',
+  })
+  if (isExpanded) {
+    dom[0].childCount++
+    dom.push(GetChevronVirtualDom.getChevronDownVirtualDom())
+  } else if (isCollapsed) {
+    dom[0].childCount++
+    dom.push(GetChevronVirtualDom.getChevronRightVirtualDom())
+  }
+  dom.push(
     debugPropertyKey,
     text(key),
     separator,
@@ -30,5 +44,6 @@ export const getScopePropertyVirtualDom = (scope) => {
       childCount: 1,
     },
     text(value),
-  ]
+  )
+  return dom
 }
