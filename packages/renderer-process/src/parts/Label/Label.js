876e49ee23a7b173c427b27f66e74faea1a4fade
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-25 08:24:26 +0100

    refactor: remove unused renderer process code (#5240)

--- packages/renderer-process/src/parts/Label/Label.js
+++
@@ -1,7 +0,0 @@
-export const create = (text) => {
-  const $LabelText = document.createTextNode(text)
-  const $Label = document.createElement('div')
-  $Label.className = 'Label'
-  $Label.append($LabelText)
-  return $Label
-}
--- packages/renderer-process/src/parts/SupportsHtml5Dialog/SupportsHtml5Dialog.js
+++
@@ -1,5 +0,0 @@
-export const supportsHtml5Dialog = () => {
-  const $Dialog = document.createElement('dialog')
-  // @ts-ignore
-  return typeof $Dialog.showModal === 'function'
-}
