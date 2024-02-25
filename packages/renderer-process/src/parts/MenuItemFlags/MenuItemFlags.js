9c9072158a6f59a1ee27531bccded231018dfdd2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-25 08:10:34 +0100

    Discard changes to packages/renderer-process/src/parts/MenuItemFlags/MenuItemFlags.js

---
+++ packages/renderer-process/src/parts/MenuItemFlags/MenuItemFlags.js
@@ -0,0 +1,15 @@
+export const Separator = 1
+
+export const None = 0
+
+export const SubMenu = 4
+
+export const Checked = 2
+
+export const Unchecked = 3
+
+export const Disabled = 5
+
+export const RestoreFocus = 6
+
+export const Ignore = 7
