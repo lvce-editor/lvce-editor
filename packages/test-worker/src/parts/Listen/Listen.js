7b9f8c7815de040ad7af211140077475b7c1d8bd
Le Vivilet <levivlet@pm.me>
2024-03-16 19:29:47 +0100

    fix: rpc creation

--- packages/test-worker/src/parts/Listen/Listen.js
+++ packages/test-worker/src/parts/Listen/Listen.js
@@ -3,16 +3,11 @@ import * as CommandState from '../CommandState/CommandState.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcChild from '../IpcChild/IpcChild.js'
 import * as IpcChildType from '../IpcChildType/IpcChildType.js'
-import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+import * as Rpc from '../Rpc/Rpc.js'
 
 export const listen = async () => {
   CommandState.registerCommands(CommandMap.commandMap)
   const ipc = await IpcChild.listen({ method: IpcChildType.Auto() })
   HandleIpc.handleIpc(ipc)
-  const rpc = {
-    invoke(method, ...params) {
-      return JsonRpc.invoke(ipc, method, ...params)
-    },
-  }
-  globalThis.rpc = rpc
+  Rpc.listen(ipc)
 }
