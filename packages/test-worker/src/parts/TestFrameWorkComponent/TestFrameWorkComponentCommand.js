729a90866bafb1fd1190f0b579d9c14da6a48954
Le Vivilet <levivlet@pm.me>
2024-03-16 10:12:08 +0100

    fix files

--- packages/test-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponentCommand.js
+++ packages/test-worker/src/parts/TestFrameWorkComponent/TestFrameWorkComponentCommand.js
@@ -1,9 +1,5 @@
-import * as Command from '../Command/Command.js'
-import * as ExtensionHostCommand from '../ExtensionHost/ExtensionHostCommands.js'
+import * as Rpc from '../Rpc/Rpc.js'
 
 export const execute = async (id, ...args) => {
-  if (id === 'PointerCapture.mock' || id === 'PointerCapture.unmock' || id === 'About.showAbout') {
-    return Command.execute(id)
-  }
-  await ExtensionHostCommand.executeCommand(id)
+  return Rpc.invoke(id, ...args)
 }
