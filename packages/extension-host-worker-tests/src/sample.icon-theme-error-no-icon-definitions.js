a73c3965fe41cd5fc0dd43f3d5b2276301278771
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-03-16 10:09:28 +0100

    refactor: don't rely on global variables in e2e tests (#5274)

--- packages/extension-host-worker-tests/src/sample.icon-theme-error-no-icon-definitions.js
+++ packages/extension-host-worker-tests/src/sample.icon-theme-error-no-icon-definitions.js
@@ -1,21 +1,16 @@
-test('sample.icon-theme', async () => {
+export const name = 'sample.icon-theme'
+
+export const test = async ({ FileSystem, Workspace, Extension, IconTheme }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/test.xyz`, 'test')
   await FileSystem.mkdir(`${tmpDir}/test-folder`)
   await Workspace.setPath(tmpDir)
-  await Extension.addWebExtension(
-    new URL(
-      '../fixtures/sample.icon-theme-error-no-icon-definitions',
-      import.meta.url
-    ).toString()
-  )
+  await Extension.addWebExtension(new URL('../fixtures/sample.icon-theme-error-no-icon-definitions', import.meta.url).toString())
 
   // act
   await IconTheme.setIconTheme('test-icon-theme')
 
   // assert
   // TODO check that warning message is printed to console
-})
-
-export {}
+}
