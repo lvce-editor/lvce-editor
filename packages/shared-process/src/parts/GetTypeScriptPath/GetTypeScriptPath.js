b13fe20bc5be749d1d4598692619c0493a6b9e95
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-03-16 18:48:55 +0100

    feature: load typescript from node modules when running as server sin… (#5298)

--- packages/shared-process/src/parts/GetTypeScriptPath/GetTypeScriptPath.js
+++ packages/shared-process/src/parts/GetTypeScriptPath/GetTypeScriptPath.js
@@ -1,7 +1,11 @@
 import { join } from 'node:path'
+import * as IsBuiltServer from '../IsBuiltServer/IsBuiltServer.js'
 import * as PlatformPaths from '../PlatformPaths/PlatformPaths.js'
 
 export const getTypeScriptPath = () => {
+  if (IsBuiltServer.isBuiltServer) {
+    return 'typescript'
+  }
   return join(
     PlatformPaths.getBuiltinExtensionsPath(),
     'builtin.language-features-typescript',
