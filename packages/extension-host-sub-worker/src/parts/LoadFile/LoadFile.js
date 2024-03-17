7809b6832e41ee43a18e2c28078f4968a896b4a4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-03-17 11:26:07 +0100

    fix: loadfile error message being displayed twice (#5310)

--- packages/extension-host-sub-worker/src/parts/LoadFile/LoadFile.js
+++ packages/extension-host-sub-worker/src/parts/LoadFile/LoadFile.js
@@ -1,19 +1,14 @@
 import * as Assert from '../Assert/Assert.js'
 import * as CommandState from '../CommandState/CommandState.js'
 import * as ImportScript from '../ImportScript/ImportScript.js'
-import { VError } from '../VError/VError.js'
 
 export const loadFile = async (path) => {
-  try {
-    Assert.string(path)
-    const module = await ImportScript.importScript(path)
-    if (module && module.commandMap) {
-      const commandMap = module.commandMap
-      CommandState.registerCommands(commandMap)
-    } else {
-      throw new Error(`missing commandMap export`)
-    }
-  } catch (error) {
-    throw new VError(error, `Failed to load ${path}`)
+  Assert.string(path)
+  const module = await ImportScript.importScript(path)
+  if (module && module.commandMap) {
+    const commandMap = module.commandMap
+    CommandState.registerCommands(commandMap)
+  } else {
+    throw new Error(`missing commandMap export`)
   }
 }
