498811856cf929aedc158a3d18dd3f3f1b7b58b5
Le Vivilet <levivlet@pm.me>
2024-03-17 06:56:58 +0100

    feature: forward test worker commands to renderer process

---
+++ packages/renderer-worker/src/parts/TestFrameWork/TestFrameWork.ipc.js
@@ -0,0 +1,8 @@
+import * as TestFrameWork from './TestFrameWork.js'
+
+export const name = 'TestFrameWork'
+
+export const Commands = {
+  checkMultiElementCondition: TestFrameWork.checkMultiElementCondition,
+  checkSingleElementCondition: TestFrameWork.checkSingleElementCondition,
+}
