b98edf21f41e9b6e70e3a25542b9ff4229b50de0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-03-24 16:48:00 +0100

    Discard changes to scripts/update-dependencies.sh

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -18,7 +18,7 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x msw -x got`
+  OUTPUT=`ncu -u -x msw -x got -x electron`
   SUB='All dependencies match the latest package versions'
   if [[ "$OUTPUT" == *"$SUB"* ]]; then
     echo "$OUTPUT"
