51d49b8fb0079871f370d55a385bff693259fa70
Le Vivilet <levivlet@pm.me>
2024-03-24 16:26:34 +0100

    feature: move getWebExtensions function into separate file

---
+++ packages/renderer-worker/src/parts/ExtensionMetaState/ExtensionMetaState.js
@@ -0,0 +1,6 @@
+export const state = {
+  /**
+   * @type {any[]}
+   */
+  webExtensions: [],
+}
---
+++ packages/renderer-worker/src/parts/GetWebExtensions/GetWebExtensions.js
@@ -0,0 +1,43 @@
+import * as Command from '../Command/Command.js'
+import * as ExtensionMetaState from '../ExtensionMetaState/ExtensionMetaState.js'
+import * as Platform from '../Platform/Platform.js'
+import * as PlatformType from '../PlatformType/PlatformType.js'
+import * as SharedProcess from '../SharedProcess/SharedProcess.js'
+import * as WebExtensionsUrl from '../WebExtensionsUrl/WebExtensionsUrl.js'
+
+const getSharedProcessExtensions = () => {
+  return SharedProcess.invoke(/* ExtensionManagement.getExtensions */ 'ExtensionManagement.getExtensions')
+}
+
+const getStaticWebExtensions = () => {
+  return Command.execute('Ajax.getJson', WebExtensionsUrl.webExtensionsUrl)
+}
+
+const getWebExtensionsWeb = async () => {
+  const staticWebExtensions = await getStaticWebExtensions()
+  return [...staticWebExtensions, ...ExtensionMetaState.state.webExtensions]
+}
+
+const isWebExtension = (extension) => {
+  return extension && typeof extension.browser === 'string'
+}
+
+const getWebExtensionsDefault = async () => {
+  const staticWebExtensions = await getStaticWebExtensions()
+  const sharedProcessExtensions = await getSharedProcessExtensions()
+  const sharedProcessWebExtensions = sharedProcessExtensions.filter(isWebExtension)
+  return [...staticWebExtensions, sharedProcessWebExtensions, ...ExtensionMetaState.state.webExtensions]
+}
+
+export const getWebExtensions = async () => {
+  try {
+    switch (Platform.platform) {
+      case PlatformType.Web:
+        return getWebExtensionsWeb()
+      default:
+        return getWebExtensionsDefault()
+    }
+  } catch {
+    return ExtensionMetaState.state.webExtensions
+  }
+}
