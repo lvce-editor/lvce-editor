2b6e8ad9da456a8ad970687511333ef612629a11
Le Vivilet <levivlet@pm.me>
2024-03-24 21:55:14 +0100

    feature: remove unused test worker code

--- packages/test-worker/src/parts/Test/Test.ipc.js
+++
@@ -1,9 +0,0 @@
-import * as Test from './Test.js'
-
-export const name = 'Test'
-
-export const Commands = {
-  execute: Test.execute,
-  executeMockExecFunction: Test.executeMockExecFunction,
-  executeMockRpcFunction: Test.executeMockRpcFunction,
-}
