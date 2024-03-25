77ae6c2ce3bb6f3f061786fb3e31cbefe1f8cdc4
Le Vivilet <levivlet@pm.me>
2024-03-25 20:07:29 +0100

    feature: improve pty-host typings

--- packages/pty-host/test/PtyController.test.js
+++ packages/pty-host/test/PtyController.test.js
@@ -5,6 +5,7 @@ import * as PtyController from '../src/parts/PtyController/PtyController.js'
 // })
 
 test.skip('create', () => {
+  // @ts-ignore
   if (Platform.isWindows) {
     // TODO add windows test
     return
@@ -13,18 +14,25 @@ test.skip('create', () => {
 })
 
 test.skip('dispose', () => {
+  // @ts-ignore
   if (Platform.isWindows) {
     // TODO add windows test
     return
   }
   PtyController.create(1)
   PtyController.create(2)
+  // @ts-ignore
   expect(PtyController.state.ptyMap[1]).toBeDefined()
+  // @ts-ignore
   expect(PtyController.state.ptyMap[2]).toBeDefined()
   PtyController.dispose(2)
+  // @ts-ignore
   expect(PtyController.state.ptyMap[1]).toBeDefined()
+  // @ts-ignore
   expect(PtyController.state.ptyMap[2]).toBeUndefined()
   PtyController.dispose(1)
+  // @ts-ignore
   expect(PtyController.state.ptyMap[1]).toBeUndefined()
+  // @ts-ignore
   expect(PtyController.state.ptyMap[2]).toBeUndefined()
 })
