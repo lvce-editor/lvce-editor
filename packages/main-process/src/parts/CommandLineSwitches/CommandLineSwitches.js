f0c9bea0a06b2c8489501695dcf532d3bcbc6bac
Le Vivilet <levivlet@pm.me>
2024-03-25 22:42:08 +0100

    feature: move sandbox into separate file

--- packages/main-process/src/parts/CommandLineSwitches/CommandLineSwitches.js
+++ packages/main-process/src/parts/CommandLineSwitches/CommandLineSwitches.js
@@ -1,10 +1,11 @@
 import * as ElectronApp from '../ElectronApp/ElectronApp.js'
 import * as Platform from '../Platform/Platform.js'
+import * as Sandbox from '../Sandbox/Sandbox.js'
 
 export const enable = (parsedCliArgs) => {
   // command line switches
   if (parsedCliArgs.sandbox) {
-    ElectronApp.enableSandbox()
+    Sandbox.enableSandbox()
   } else {
     // see https://github.com/microsoft/vscode/issues/151187#issuecomment-1221475319
     if (Platform.isLinux) {
