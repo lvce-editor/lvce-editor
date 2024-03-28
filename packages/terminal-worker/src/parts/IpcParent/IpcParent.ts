94a2a6486151a4502e8350dc6528e8387df967e7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-03-28 15:34:17 +0100

    feature: add websocket ipc to terminal worker (#5416)

--- packages/terminal-worker/src/parts/IpcParent/IpcParent.ts
+++ packages/terminal-worker/src/parts/IpcParent/IpcParent.ts
@@ -1,4 +1,4 @@
-import * as IpcParentModule from '../IpcParentModule/IpcParentModule.js'
+import * as IpcParentModule from '../IpcParentModule/IpcParentModule.ts'
 
 export const create = async ({ method, ...options }) => {
   const module = await IpcParentModule.getModule(method)
--- packages/terminal-worker/src/parts/TerminalProcess/TerminalProcess.ts
+++ packages/terminal-worker/src/parts/TerminalProcess/TerminalProcess.ts
@@ -1,8 +1,9 @@
 import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
 import * as TerminalProcessState from '../TerminalProcessState/TerminalProcessState.ts'
+import * as LaunchTerminalProcess from '../LaunchTerminalProcess/LaunchTerminalProcess.ts'
 
 export const listen = async () => {
-  console.log('setup terminal connection')
+  await LaunchTerminalProcess.launchTerminalProcess()
 }
 
 export const invoke = (method, ...params) => {
