84f5c1c6dc76fc0547c2768b73e38367d3953676
Le Vivilet <levivlet@pm.me>
2024-03-28 19:15:53 +0100

    send port

--- packages/terminal-worker/src/parts/SendMessagePortToElectron/SendMessagePortToElectron.ts
+++ packages/terminal-worker/src/parts/SendMessagePortToElectron/SendMessagePortToElectron.ts
@@ -1,5 +1,5 @@
 import * as Rpc from '../Rpc/Rpc.ts'
 
 export const sendMessagePortToElectron = async (port, initialCommand) => {
-  await Rpc.invokeAndTransfer('SendMessagePortToElectron.sendMessagePortToElectron', port, initialCommand)
+  await Rpc.invokeAndTransfer([port], 'SendMessagePortToElectron.sendMessagePortToElectron', port, initialCommand)
 }
