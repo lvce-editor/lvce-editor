5670c11fdf1bf7f7a59339efcfe439cf52844745
Le Vivilet <levivlet@pm.me>
2024-03-28 18:33:56 +0100

    feature: add platform test

---
+++ packages/terminal-worker/test/Platform.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from '@jest/globals'
+import * as Platform from '../src/parts/Platform/Platform.ts'
+
+test('platform', () => {
+  expect(typeof Platform.platform).toBe('string')
+})
