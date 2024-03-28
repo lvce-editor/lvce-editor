5e6fe779999b3725a258c5f28f9a0bd91af9404e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-03-28 19:03:09 +0100

    feature: add terminal worker electron ipc (#5425)

--- packages/renderer-worker/src/parts/LaunchTerminalWorker/LaunchTerminalWorker.js
+++ packages/renderer-worker/src/parts/LaunchTerminalWorker/LaunchTerminalWorker.js
@@ -1,12 +1,15 @@
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as TerminalWorkerUrl from '../TerminalWorkerUrl/TerminalWorkerUrl.js'
+import * as Platform from '../Platform/Platform.js'
+import * as PlatformType from '../PlatformType/PlatformType.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 
 export const launchTerminalWorker = async () => {
+  const name = Platform.platform === PlatformType.Electron ? 'Terminal Worker (Electron)' : 'Terminal Worker'
   const ipc = await IpcParent.create({
     method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
-    name: 'Terminal Worker',
+    name,
     url: TerminalWorkerUrl.terminalWorkerUrl,
   })
   HandleIpc.handleIpc(ipc)
--- packages/terminal-worker/src/parts/IpcParentWithNodeAlternate/IpcParentWithNodeAlternate.ts
+++ packages/terminal-worker/src/parts/IpcParentWithNodeAlternate/IpcParentWithNodeAlternate.ts
@@ -1,14 +1,25 @@
 import * as GetData from '../GetData/GetData.ts'
+import * as Platform from '../Platform/Platform.ts'
+import * as PlatformType from '../PlatformType/PlatformType.ts'
+import * as IpcParentWithElectron from '../IpcParentWithElectron/IpcParentWithElectron.ts'
 
 export const create = async (options) => {
-  const module = await import('../IpcParentWithWebSocket/IpcParentWithWebSocket.ts')
-  const rawIpc = await module.create(options)
-  if (options.raw) {
-    return rawIpc
-  }
-  return {
-    rawIpc,
-    module,
+  switch (Platform.platform) {
+    case PlatformType.Web:
+    case PlatformType.Remote:
+      const module = await import('../IpcParentWithWebSocket/IpcParentWithWebSocket.ts')
+      const rawIpc = await module.create(options)
+      if (options.raw) {
+        return rawIpc
+      }
+      return {
+        rawIpc,
+        module,
+      }
+    case PlatformType.Electron:
+      return IpcParentWithElectron.create()
+    default:
+      throw new Error('unsupported platform')
   }
 }
 
