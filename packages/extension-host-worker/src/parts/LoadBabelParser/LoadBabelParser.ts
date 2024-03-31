7e0cfc9c78b6ee202aa59dcba4212aadc3b711cc
Le Vivilet <levivlet@pm.me>
2024-03-31 10:16:09 +0200

    fix babel parser

--- packages/extension-host-worker/src/parts/LoadBabelParser/LoadBabelParser.ts
+++ packages/extension-host-worker/src/parts/LoadBabelParser/LoadBabelParser.ts
@@ -1,6 +1,6 @@
 import * as AssetDir from '../AssetDir/AssetDir.ts'
 
 export const loadBabelParser = () => {
-  const url = `${AssetDir.assetDir}/js/babel-parser.ts`
+  const url = `${AssetDir.assetDir}/js/babel-parser.js`
   return import(url)
 }
