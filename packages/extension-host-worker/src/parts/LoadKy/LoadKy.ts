0d3a73a362925691d7f38a98e04f6412ce37188d
Le Vivilet <levivlet@pm.me>
2024-03-31 10:16:30 +0200

    fix ky

--- packages/extension-host-worker/src/parts/LoadKy/LoadKy.ts
+++ packages/extension-host-worker/src/parts/LoadKy/LoadKy.ts
@@ -5,7 +5,7 @@ import * as AssetDir from '../AssetDir/AssetDir.ts'
  * @returns {Promise<any>}
  */
 export const loadKy = async () => {
-  const url = `${AssetDir.assetDir}/js/ky.ts`
+  const url = `${AssetDir.assetDir}/js/ky.js`
   const module = await import(url)
   return module
 }
