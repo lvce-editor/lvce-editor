40bf720c7be252cf670bdb9453941c8a6c563737
Le Vivilet <levivlet@pm.me>
2024-04-01 16:34:20 +0200

    update icon

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerActions.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerActions.js
@@ -13,7 +13,7 @@ export const getActions = () => {
     {
       type: ActionType.Button,
       id: ViewletExplorerStrings.newFolder(),
-      icon: MaskIcon.NewFolder,
+      icon: MaskIcon.ListTree,
       command: 'newFolder',
     },
     {
