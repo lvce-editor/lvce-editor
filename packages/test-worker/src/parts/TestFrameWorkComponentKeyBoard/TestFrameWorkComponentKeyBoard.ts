62065ee219eed6937cb48fc7560b307096d3587a
Le Vivilet <levivlet@pm.me>
2024-04-01 14:29:21 +0200

    feature: move getKeyOptions into separate file

--- packages/test-worker/src/parts/TestFrameWorkComponentKeyBoard/TestFrameWorkComponentKeyBoard.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentKeyBoard/TestFrameWorkComponentKeyBoard.ts
@@ -1,38 +1,8 @@
+import * as GetKeyOptions from '../GetKeyOptions/GetKeyOptions.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
 
-const getKeyOptions = (rawKey) => {
-  if (rawKey.includes('+')) {
-    const parts = rawKey.split('+')
-    let ctrlKey = false
-    let altKey = false
-    let key = ''
-    for (const part of parts) {
-      switch (part) {
-        case 'Control':
-          ctrlKey = true
-          break
-        case 'Space':
-          key = ' '
-          break
-        case 'Alt':
-          altKey = true
-          break
-        default:
-          key = part
-          break
-      }
-    }
-    return {
-      key,
-      ctrlKey,
-      altKey,
-    }
-  }
-  return { key: rawKey }
-}
-
 export const press = async (key) => {
-  const keyOptions = getKeyOptions(key)
+  const keyOptions = GetKeyOptions.getKeyOptions(key)
   const options = {
     cancelable: true,
     bubbles: true,
