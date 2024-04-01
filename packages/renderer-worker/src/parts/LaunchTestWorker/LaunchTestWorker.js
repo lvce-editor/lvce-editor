af9d5a2f57a861a9584941b8282b14a68ac24c06
Le Vivilet <levivlet@pm.me>
2024-04-01 13:09:57 +0200

    feature: move launchTestWorker function into separate file

---
+++ packages/renderer-worker/src/parts/LaunchTestWorker/LaunchTestWorker.js
@@ -0,0 +1,14 @@
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcParent from '../IpcParent/IpcParent.js'
+import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as TestWorkerUrl from '../TestWorkerUrl/TestWorkerUrl.js'
+
+export const launchTestWorker = async () => {
+  const ipc = await IpcParent.create({
+    method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
+    url: TestWorkerUrl.testWorkerUrl,
+    name: 'Test Worker',
+  })
+  HandleIpc.handleIpc(ipc, 'test-worker')
+  return ipc
+}
