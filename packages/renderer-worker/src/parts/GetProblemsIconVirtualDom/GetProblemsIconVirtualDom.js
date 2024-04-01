39992fb9ebf2b5559af69bd7f22b79ecde227b91
Le Vivilet <levivlet@pm.me>
2024-04-01 18:21:41 +0200

    move function into separate file

---
+++ packages/renderer-worker/src/parts/GetProblemsIconVirtualDom/GetProblemsIconVirtualDom.js
@@ -0,0 +1,10 @@
+import * as ClassNames from '../ClassNames/ClassNames.js'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+
+export const getProblemsIconVirtualDom = () => {
+  return {
+    type: VirtualDomElements.Div,
+    className: ClassNames.ProblemsErrorIcon,
+    childCount: 0,
+  }
+}
