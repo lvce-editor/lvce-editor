4c60e01e9b62a7ce8990cf8e0042e9609156c088
Le Vivilet <levivlet@pm.me>
2024-04-01 14:06:20 +0200

    feature: use typescript for renderer process

--- packages/renderer-process/src/parts/LoadDomPurify/LoadDomPurify.ts
+++ packages/renderer-process/src/parts/LoadDomPurify/LoadDomPurify.ts
@@ -1,4 +1,4 @@
-import * as AssetDir from '../AssetDir/AssetDir.js'
+import * as AssetDir from '../AssetDir/AssetDir.ts'
 
 export const loadDomPurify = async () => {
   const url = `${AssetDir.assetDir}/js/dompurify.js`
