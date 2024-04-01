dde5027acfd26b10d2964d17b7bb9d95ba592d99
Le Vivilet <levivlet@pm.me>
2024-04-01 11:00:03 +0200

    tsc

--- packages/renderer-process/src/parts/ViewletMainTabs/ViewletMainTabs.js
+++ packages/renderer-process/src/parts/ViewletMainTabs/ViewletMainTabs.js
@@ -1,7 +1,7 @@
-import * as AriaRoles from '../AriaRoles/AriaRoles.js'
-import * as AttachEvents from '../AttachEvents/AttachEvents.js'
-import * as DomEventOptions from '../DomEventOptions/DomEventOptions.js'
-import * as DomEventType from '../DomEventType/DomEventType.js'
+import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as AttachEvents from '../AttachEvents/AttachEvents.ts'
+import * as DomEventOptions from '../DomEventOptions/DomEventOptions.ts'
+import * as DomEventType from '../DomEventType/DomEventType.ts'
 import * as VirtualDom from '../VirtualDom/VirtualDom.js'
 import * as ViewletMainTabEvents from './ViewletMainTabEvents.js'
 
