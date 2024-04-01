925fdf565675b91802deff6696e5abce99f0f795
Le Vivilet <levivlet@pm.me>
2024-04-01 14:47:30 +0200

    add test

--- packages/test-worker/test/SplitLines.test.ts
+++ packages/test-worker/test/SplitLines.test.ts
@@ -1,7 +1,12 @@
 import * as SplitLines from '../src/parts/SplitLines/SplitLines.ts'
 import { test, expect } from '@jest/globals'
 
-test('assetDir', () => {
+test('splitLines', () => {
   const lines = 'a\nb'
   expect(SplitLines.splitLines(lines)).toEqual(['a', 'b'])
+})
+
+test('empty string', () => {
+  const lines = ''
+  expect(SplitLines.splitLines(lines)).toEqual([])
 })
