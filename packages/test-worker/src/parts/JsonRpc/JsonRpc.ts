6645597bc80ba0ab9dbd06eb33be7381e7a24ddc
Le Vivilet <levivlet@pm.me>
2024-04-02 09:25:59 +0200

    feature: simplify test-worker code

--- packages/test-worker/src/parts/JsonRpc/JsonRpc.ts
+++ packages/test-worker/src/parts/JsonRpc/JsonRpc.ts
@@ -1,16 +1,2 @@
-import * as JsonRpcEvent from '../JsonRpcEvent/JsonRpcEvent.ts'
-import * as JsonRpcRequest from '../JsonRpcRequest/JsonRpcRequest.ts'
-import * as UnwrapJsonRpcResult from '../UnwrapJsonRpcResult/UnwrapJsonRpcResult.ts'
-
-export const send = (transport, method, ...params) => {
-  const message = JsonRpcEvent.create(method, params)
-  transport.send(message)
-}
-
-export const invoke = async (ipc, method, ...params) => {
-  const { message, promise } = JsonRpcRequest.create(method, params)
-  ipc.send(message)
-  const responseMessage = await promise
-  const result = UnwrapJsonRpcResult.unwrapJsonRpcResult(responseMessage)
-  return result
-}
+// @ts-ignore
+export * from '../../../../../static/js/lvce-editor-json-rpc.js'
