6a1fea54205154a3f4d182b5dbe861908573550c
Le Vivilet <levivlet@pm.me>
2024-04-02 10:28:32 +0200

    feature: simplify terminal worker rpc

--- packages/terminal-worker/src/parts/HandleIpc/HandleIpc.ts
+++ packages/terminal-worker/src/parts/HandleIpc/HandleIpc.ts
@@ -1,9 +1,29 @@
 import * as Callback from '../Callback/Callback.ts'
 import * as Command from '../Command/Command.ts'
-import * as HandleJsonRpcMessage from '../HandleJsonRpcMessage/HandleJsonRpcMessage.ts'
+import * as HandleJsonRpcMessage from '../JsonRpc/JsonRpc.ts'
+
+const requiresSocket = () => {
+  return false
+}
+
+const preparePrettyError = (error) => {
+  return error
+}
+
+const logError = (error) => {
+  console.error(error)
+}
 
 const handleMessage = (event) => {
-  return HandleJsonRpcMessage.handleJsonRpcMessage(event, Command.execute, Callback.resolve)
+  return HandleJsonRpcMessage.handleJsonRpcMessage(
+    event.target,
+    event.data,
+    Command.execute,
+    Callback.resolve,
+    preparePrettyError,
+    logError,
+    requiresSocket,
+  )
 }
 
 export const handleIpc = (ipc) => {
