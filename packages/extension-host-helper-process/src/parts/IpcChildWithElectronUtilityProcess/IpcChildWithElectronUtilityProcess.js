f1aca50a36c0adf337b5c1065b6f02941dd20807
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-02 17:12:19 +0200

    feature: update ipc version (#5489)

--- packages/extension-host-helper-process/src/parts/IpcChildWithElectronUtilityProcess/IpcChildWithElectronUtilityProcess.js
+++ packages/extension-host-helper-process/src/parts/IpcChildWithElectronUtilityProcess/IpcChildWithElectronUtilityProcess.js
@@ -3,3 +3,6 @@ import { IpcChildWithElectronUtilityProcess } from '@lvce-editor/ipc'
 export const listen = IpcChildWithElectronUtilityProcess.listen
 
 export const wrap = IpcChildWithElectronUtilityProcess.wrap
+
+// @ts-ignore
+export const signal = IpcChildWithElectronUtilityProcess.signal
