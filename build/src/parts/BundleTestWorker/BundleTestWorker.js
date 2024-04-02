bd98fe0e56a73a39b301e4412237cad67885f113
Le Vivilet <levivlet@pm.me>
2024-04-02 09:26:48 +0200

    fix build

--- build/src/parts/BundleTestWorker/BundleTestWorker.js
+++ build/src/parts/BundleTestWorker/BundleTestWorker.js
@@ -1,12 +1,20 @@
 import * as BundleJs from '../BundleJsRollup/BundleJsRollup.js'
 import * as Copy from '../Copy/Copy.js'
 import * as Path from '../Path/Path.js'
+import * as Replace from '../Replace/Replace.js'
 
 export const bundleTestWorker = async ({ cachePath, commitHash, platform, assetDir }) => {
   await Copy.copy({
     from: 'packages/test-worker/src',
     to: Path.join(cachePath, 'src'),
   })
+  for (const file of ['JsonRpc']) {
+    await Replace.replace({
+      path: `${cachePath}/src/parts/${file}/${file}.ts`,
+      occurrence: `../../../../../static/`,
+      replacement: `../../../static/`,
+    })
+  }
   await BundleJs.bundleJs({
     cwd: cachePath,
     from: `./src/testWorkerMain.ts`,
