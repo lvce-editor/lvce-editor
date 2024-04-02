e97e9a3940ade0e8b1cf811fb94e617e86a532af
Le Vivilet <levivlet@pm.me>
2024-04-02 09:30:44 +0200

    remove unused code

--- packages/test-worker/src/parts/HandleIpc/HandleIpc.ts
+++ packages/test-worker/src/parts/HandleIpc/HandleIpc.ts
@@ -1,6 +1,6 @@
 import * as Callback from '../Callback/Callback.ts'
 import * as Command from '../Command/Command.ts'
-import * as HandleJsonRpcMessage from '../HandleJsonRpcMessage/HandleJsonRpcMessage.ts'
+import * as HandleJsonRpcMessage from '../JsonRpc/JsonRpc.ts'
 
 const handleMessage = (event) => {
   return HandleJsonRpcMessage.handleJsonRpcMessage(event, Command.execute, Callback.resolve)
--- packages/test-worker/src/parts/PrintPrettyError/PrintPrettyError.ts
+++
@@ -1,3 +0,0 @@
-export const printPrettyError = (prettyError, prefix = '') => {
-  // Logger.error(`${prefix}${prettyError.type}: ${prettyError.message}\n\n${prettyError.codeFrame}\n\n${prettyError.stack}\n`)
-}
