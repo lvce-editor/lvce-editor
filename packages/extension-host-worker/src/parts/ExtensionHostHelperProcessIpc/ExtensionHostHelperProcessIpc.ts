3afc092bd7b3f6b21c12cd773edc5df4a40a3a9a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-02 12:47:16 +0200

    feature: remove unused extension host worker code (#5484)

--- packages/extension-host-worker/src/parts/ExtensionHostHelperProcessIpc/ExtensionHostHelperProcessIpc.ts
+++
@@ -1,8 +0,0 @@
-import * as IpcParent from '../IpcParent/IpcParent.ts'
-
-export const create = (method) => {
-  return IpcParent.create({
-    method,
-    type: 'extension-host-helper-process',
-  })
-}
--- packages/extension-host-worker/src/parts/ExtensionHostHelperProcessIpcType/ExtensionHostHelperProcessIpcType.ts
+++
@@ -1,9 +0,0 @@
-import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
-import * as IsElectron from '../IsElectron/IsElectron.ts'
-
-export const getIpcType = () => {
-  if (IsElectron.isElectron()) {
-    return IpcParentType.ElectronMessagePort
-  }
-  return IpcParentType.WebSocket
-}
