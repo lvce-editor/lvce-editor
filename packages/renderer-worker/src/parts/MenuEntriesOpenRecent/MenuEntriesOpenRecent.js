b233d5414c83943c00b9dfcebe7fe5169288932c
Le Vivilet <levivlet@pm.me>
2024-04-04 23:08:33 +0200

    feature: use title bar strings for recent menu items

--- packages/renderer-worker/src/parts/MenuEntriesOpenRecent/MenuEntriesOpenRecent.js
+++ packages/renderer-worker/src/parts/MenuEntriesOpenRecent/MenuEntriesOpenRecent.js
@@ -1,17 +1,8 @@
 import * as Command from '../Command/Command.js'
-import * as I18nString from '../I18NString/I18NString.js'
-import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
-import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
-import * as PathDisplay from '../PathDisplay/PathDisplay.js'
 import * as MenuEntrySeparator from '../MenuEntrySeparator/MenuEntrySeparator.js'
-
-/**
- * @enum {string}
- */
-export const UiStrings = {
-  More: 'More ...',
-  ClearRecentlyOpened: 'Clear Recently Opened',
-}
+import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
+import * as PathDisplay from '../PathDisplay/PathDisplay.js'
+import * as TitleBarMenuBarStrings from '../TitleBarMenuBarStrings/TitleBarMenuBarStrings.js'
 
 const MAX_MENU_RECENT_ENTRIES = 10
 
@@ -39,14 +30,14 @@ export const getMenuEntries = async () => {
   items.push(
     {
       id: 'more',
-      label: I18nString.i18nString(UiStrings.More),
+      label: TitleBarMenuBarStrings.moreDot(),
       flags: MenuItemFlags.None,
       command: 'QuickPick.showRecent',
     },
     MenuEntrySeparator.menuEntrySeparator,
     {
       id: 'clearRecentlyOpened',
-      label: I18nString.i18nString(UiStrings.ClearRecentlyOpened),
+      label: TitleBarMenuBarStrings.clearRecentlyOpened(),
       flags: MenuItemFlags.None,
       command: 'RecentlyOpened.clearRecentlyOpened',
     },
--- packages/renderer-worker/src/parts/TitleBarMenuBarStrings/TitleBarMenuBarStrings.js
+++ packages/renderer-worker/src/parts/TitleBarMenuBarStrings/TitleBarMenuBarStrings.js
@@ -5,8 +5,18 @@ import * as I18nString from '../I18NString/I18NString.js'
  */
 const UiStrings = {
   More: 'more',
+  MoreDot: 'More ...',
+  ClearRecentlyOpened: 'Clear Recently Opened',
 }
 
 export const more = () => {
   return I18nString.i18nString(UiStrings.More)
+}
+
+export const moreDot = () => {
+  return I18nString.i18nString(UiStrings.MoreDot)
+}
+
+export const clearRecentlyOpened = () => {
+  return I18nString.i18nString(UiStrings.ClearRecentlyOpened)
 }
