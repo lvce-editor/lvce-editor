d3098b57b1bc404357a54be962d0d493765a1b85
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-04 23:29:34 +0200

    feature: use terminal strings for terminal menu items (#5540)

--- packages/renderer-worker/src/parts/MenuEntriesTerminal/MenuEntriesTerminal.js
+++ packages/renderer-worker/src/parts/MenuEntriesTerminal/MenuEntriesTerminal.js
@@ -1,18 +1,11 @@
-import * as I18nString from '../I18NString/I18NString.js'
 import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
-
-/**
- * @enum {string}
- */
-export const UiStrings = {
-  NewTerminal: 'New Terminal',
-}
+import * as TerminalStrings from '../TerminalStrings/TerminalStrings.js'
 
 export const getMenuEntries = () => {
   return [
     {
       id: 'newTerminal',
-      label: I18nString.i18nString(UiStrings.NewTerminal),
+      label: TerminalStrings.newTerminal(),
       flags: MenuItemFlags.None,
       command: 'Layout.togglePanel',
       args: ['Terminal'],
