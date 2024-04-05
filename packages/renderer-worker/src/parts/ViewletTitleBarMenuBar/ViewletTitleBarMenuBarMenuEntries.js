5abc3a5c113478a84b2b73af64dc940d9a51d9d3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-05 22:34:24 +0200

    feature: move title bar menu entries to titlebar module (#5548)

---
+++ packages/renderer-worker/src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBarMenuEntries.js
@@ -0,0 +1,19 @@
+import * as MenuEntriesEdit from '../MenuEntriesEdit/MenuEntriesEdit.js'
+import * as MenuEntriesFile from '../MenuEntriesFile/MenuEntriesFile.js'
+import * as MenuEntriesGo from '../MenuEntriesGo/MenuEntriesGo.js'
+import * as MenuEntriesHelp from '../MenuEntriesHelp/MenuEntriesHelp.js'
+import * as MenuEntriesRun from '../MenuEntriesRun/MenuEntriesRun.js'
+import * as MenuEntriesSelection from '../MenuEntriesSelection/MenuEntriesSelection.js'
+import * as MenuEntriesTerminal from '../MenuEntriesTerminal/MenuEntriesTerminal.js'
+import * as MenuEntriesView from '../MenuEntriesView/MenuEntriesView.js'
+
+export const menus = [
+  MenuEntriesEdit,
+  MenuEntriesFile,
+  MenuEntriesSelection,
+  MenuEntriesView,
+  MenuEntriesGo,
+  MenuEntriesRun,
+  MenuEntriesTerminal,
+  MenuEntriesHelp,
+]
