26007e7db507d96f59d9c9353f8084d93cf8fb54
Le Vivilet <levivlet@pm.me>
2024-04-05 20:25:18 +0200

    feature: simplify promises withresolvers function

--- packages/renderer-worker/src/parts/Promises/Promises.js
+++ packages/renderer-worker/src/parts/Promises/Promises.js
@@ -1,19 +1,7 @@
 export const withResolvers = () => {
-  /**
-   * @type {any}
-   */
-  let _resolve
-  /**
-   * @type {any}
-   */
-  let _reject
-  const promise = new Promise((resolve, reject) => {
-    _resolve = resolve
-    _reject = reject
-  })
+  const { resolve, promise } = Promise.withResolvers()
   return {
-    resolve: _resolve,
-    reject: _reject,
+    resolve,
     promise,
   }
 }
