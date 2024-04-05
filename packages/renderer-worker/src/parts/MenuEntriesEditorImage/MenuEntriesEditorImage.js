9a28efd571d5fd7477bc75df67761dc5792e491f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-05 17:53:15 +0200

    feature: use editorImageStrings for editor image menu items (#5537)

--- packages/renderer-worker/src/parts/MenuEntriesEditorImage/MenuEntriesEditorImage.js
+++ packages/renderer-worker/src/parts/MenuEntriesEditorImage/MenuEntriesEditorImage.js
@@ -1,14 +1,6 @@
-import * as I18nString from '../I18NString/I18NString.js'
+import * as EditorImageStrings from '../EditorImageStrings/EditorImageStrings.js'
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.js'
 import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
-
-/**
- * @enum {string}
- */
-export const UiStrings = {
-  Copy: 'Copy',
-  CopyPath: 'Copy Path',
-}
 
 export const id = MenuEntryId.EditorImage
 
@@ -16,13 +8,13 @@ export const getMenuEntries = () => {
   return [
     {
       id: 'copy',
-      label: I18nString.i18nString(UiStrings.Copy),
+      label: EditorImageStrings.copy(),
       flags: MenuItemFlags.None,
       command: 'EditorImage.copyImage',
     },
     {
       id: 'copyToClipBoard',
-      label: I18nString.i18nString(UiStrings.CopyPath),
+      label: EditorImageStrings.copyPath(),
       flags: MenuItemFlags.None,
       command: 'EditorImage.copyPath',
     },
