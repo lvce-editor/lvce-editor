d4651b2b071c025f89e4a2b39cdb67c577b75a77
Le Vivilet <levivlet@pm.me>
2024-04-06 11:00:54 +0200

    feature: simplify shared process ipc imports

--- packages/shared-process/src/parts/IpcChildModule/IpcChildModule.js
+++ packages/shared-process/src/parts/IpcChildModule/IpcChildModule.js
@@ -1,4 +1,5 @@
 import * as IpcChildType from '../IpcChildType/IpcChildType.js'
+import { IpcChildWithElectronUtilityProcess, IpcChildWithWebSocket } from '@lvce-editor/ipc'
 
 export const getModule = (method) => {
   switch (method) {
@@ -7,13 +8,13 @@ export const getModule = (method) => {
     case IpcChildType.NodeWorker:
       return import('../IpcChildWithNodeWorker/IpcChildWithNodeWorker.js')
     case IpcChildType.ElectronUtilityProcess:
-      return import('../IpcChildWithElectronUtilityProcess/IpcChildWithElectronUtilityProcess.js')
+      return IpcChildWithElectronUtilityProcess
     case IpcChildType.ElectronMessagePort:
       return import('../IpcChildWithElectronMessagePort/IpcChildWithElectronMessagePort.js')
     case IpcChildType.NodeMessagePort:
       return import('../IpcChildWithNodeMessagePort/IpcChildWithNodeMessagePort.js')
     case IpcChildType.WebSocket:
-      return import('../IpcChildWithWebSocket/IpcChildWithWebSocket.js')
+      return IpcChildWithWebSocket
     default:
       throw new Error('unexpected ipc type')
   }
