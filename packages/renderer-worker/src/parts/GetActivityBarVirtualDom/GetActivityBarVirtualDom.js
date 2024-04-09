42594be0b2750a72397c61d059695b26629d7656
Le Vivilet <levivlet@pm.me>
2024-04-09 23:55:42 +0200

    add virtual dom

--- packages/renderer-worker/src/parts/GetActivityBarVirtualDom/GetActivityBarVirtualDom.js
+++ packages/renderer-worker/src/parts/GetActivityBarVirtualDom/GetActivityBarVirtualDom.js
@@ -1,5 +1,21 @@
 import * as GetActivityBarItemsVirtualDom from '../GetActivityBarItemsVirtualDom/GetActivityBarItemsVirtualDom.js'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 
 export const getActivityBarVirtualDom = (visibleItems) => {
-  return [...GetActivityBarItemsVirtualDom.getVirtualDom(visibleItems)]
+  return [
+    {
+      type: VirtualDomElements.Div,
+      id: 'ActivityBar',
+      className: 'Viewlet ActivityBar',
+      role: 'toolbar',
+      ariaRoleDescription: 'Activity Bar',
+      ariaOrientation: 'vertical',
+      tabIndex: 0,
+      onMouseDown: 'handleMouseDown',
+      onContextMenu: 'handleContextMenu',
+      onFocus: 'handleFocus',
+      onBlur: 'handleBlur',
+    },
+    ...GetActivityBarItemsVirtualDom.getVirtualDom(visibleItems),
+  ]
 }
