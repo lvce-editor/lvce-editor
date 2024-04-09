bc1097c5ff45efc476e7c186f529c67d4731fd40
Le Vivilet <levivlet@pm.me>
2024-04-09 23:19:56 +0200

    feature: move status bar items dom into separate file

---
+++ packages/renderer-worker/src/parts/GetStatusBarItemsVirtualDom/GetStatusBarItemsVirtualDom.js
@@ -0,0 +1,31 @@
+import * as AriaRoles from '../AriaRoles/AriaRoles.js'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
+
+const getStatusBarItemVirtualDom = (statusBarItem) => {
+  const { tooltip, icon } = statusBarItem
+  const dom = []
+  dom.push(
+    {
+      type: VirtualDomElements.Div,
+      className: 'StatusBarItem',
+      role: AriaRoles.Button,
+      tabIndex: -1,
+      title: tooltip,
+      childCount: 1,
+    },
+    text(statusBarItem.text),
+  )
+  return dom
+}
+
+export const getStatusBarItemsVirtualDom = (items, className) => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className,
+      childCount: items.length,
+    },
+    ...items.flatMap(getStatusBarItemVirtualDom),
+  ]
+}
