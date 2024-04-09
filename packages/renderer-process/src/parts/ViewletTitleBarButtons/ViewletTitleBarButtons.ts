1d6d829c70a94b7c848c8226abf66736fdfd293c
Le Vivilet <levivlet@pm.me>
2024-04-09 23:45:35 +0200

    remove unused code

--- packages/renderer-process/src/parts/ViewletTitleBarButtons/ViewletTitleBarButtons.ts
+++ packages/renderer-process/src/parts/ViewletTitleBarButtons/ViewletTitleBarButtons.ts
@@ -1,27 +1,3 @@
-import * as AttachEvents from '../AttachEvents/AttachEvents.ts'
-import * as DomEventType from '../DomEventType/DomEventType.ts'
-import * as VirtualDom from '../VirtualDom/VirtualDom.ts'
 import * as ViewletTitleBarButtonEvents from './ViewletTitleBarButtonsEvents.ts'
-
-// export const create = () => {
-//   const $TitleBarButtons = document.createElement('div')
-//   $TitleBarButtons.className = 'Viewlet TitleBarButtons'
-//   return {
-//     $TitleBarButtons,
-//     $Viewlet: $TitleBarButtons,
-//   }
-// }
-
-// export const attachEvents = (state) => {
-//   const { $Viewlet } = state
-//   AttachEvents.attachEvents($Viewlet, {
-//     [DomEventType.Click]: ViewletTitleBarButtonEvents.handleTitleBarButtonsClick,
-//   })
-// }
-
-export const setDom = (state, dom) => {
-  const { $Viewlet } = state
-  VirtualDom.renderInto($Viewlet, dom)
-}
 
 export const Events = ViewletTitleBarButtonEvents
