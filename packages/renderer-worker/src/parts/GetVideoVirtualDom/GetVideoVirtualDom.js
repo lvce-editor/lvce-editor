b370a593d927f460070c32cf401cb9814b85a562
Le Vivilet <levivlet@pm.me>
2024-04-10 20:38:31 +0200

    feature: use functional root for video viewer

--- packages/renderer-worker/src/parts/GetVideoVirtualDom/GetVideoVirtualDom.js
+++ packages/renderer-worker/src/parts/GetVideoVirtualDom/GetVideoVirtualDom.js
@@ -1,6 +1,16 @@
+// based on the video editor by vscode
+
 import * as GetMediaVirtualDom from '../GetMediaVirtualDom/GetMediaVirtualDom.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 
 export const getVideoVirtualDom = (src, errorMessage) => {
-  return GetMediaVirtualDom.getMediaVirtualDom(VirtualDomElements.Video, src, errorMessage)
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: 'Viewlet Media Video',
+      childCount: 1,
+      onError: 'handleVideoError',
+    },
+    ...GetMediaVirtualDom.getMediaVirtualDom(VirtualDomElements.Video, src, errorMessage),
+  ]
 }
