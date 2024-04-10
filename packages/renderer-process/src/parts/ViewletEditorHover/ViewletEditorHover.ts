c4c5966a23b503eea10c9aecffffba7e42b5d795
Le Vivilet <levivlet@pm.me>
2024-04-10 21:55:46 +0200

    feature: use functional root for hover dom

--- packages/renderer-process/src/parts/ViewletEditorHover/ViewletEditorHover.ts
+++ packages/renderer-process/src/parts/ViewletEditorHover/ViewletEditorHover.ts
@@ -1,18 +1,4 @@
 import * as SetBounds from '../SetBounds/SetBounds.ts'
-import * as VirtualDom from '../VirtualDom/VirtualDom.ts'
-
-export const create = () => {
-  const $Viewlet = document.createElement('div')
-  $Viewlet.className = 'Viewlet EditorHover'
-  return {
-    $Viewlet,
-  }
-}
-
-export const setDom = (state, dom) => {
-  const { $Viewlet } = state
-  VirtualDom.renderInto($Viewlet, dom)
-}
 
 export const setBounds = (state, x, y) => {
   const { $Viewlet } = state
