e8d3a91c0bc08b4d6900d38eab8f500111491b52
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-11 20:29:19 +0200

    Discard changes to packages/renderer-process/src/parts/ViewletKeyBindings/ViewletKeyBindings.ts

--- packages/renderer-process/src/parts/ViewletKeyBindings/ViewletKeyBindings.ts
+++ packages/renderer-process/src/parts/ViewletKeyBindings/ViewletKeyBindings.ts
@@ -1,22 +1,92 @@
+import * as DomEventOptions from '../DomEventOptions/DomEventOptions.ts'
+import * as DomEventType from '../DomEventType/DomEventType.ts'
+import * as InputBox from '../InputBox/InputBox.ts'
+import * as InputType from '../InputType/InputType.ts'
+import * as VirtualDom from '../VirtualDom/VirtualDom.ts'
 import * as ViewletkeyBindingsEvents from './ViewletKeyBindingsEvents.ts'
 
-export const setValue = (state, value) => {
+/**
+ * @enum {string}
+ */
+const UiStrings = {
+  SearchKeyBindings: 'Search Key Bindings', // TODO placeholder string should come from renderer worker
+  ResultsWillUpdateAsYouType: 'Results will update as you type',
+}
+
+export const create = () => {
+  const $InputBox = InputBox.create()
+  $InputBox.type = InputType.Search
+  $InputBox.placeholder = UiStrings.SearchKeyBindings
+  $InputBox.ariaDescription = UiStrings.ResultsWillUpdateAsYouType
+
+  const $KeyBindingsHeader = document.createElement('div')
+  $KeyBindingsHeader.className = 'KeyBindingsHeader'
+  $KeyBindingsHeader.append($InputBox)
+
+  const $KeyBindingsTableWrapper = document.createElement('div')
+  $KeyBindingsTableWrapper.className = 'KeyBindingsTableWrapper'
+
+  const $ScrollBarThumb = document.createElement('div')
+  $ScrollBarThumb.className = 'ScrollBarThumb'
+
+  const $ScrollBar = document.createElement('div')
+  $ScrollBar.className = 'ScrollBar'
+  $ScrollBar.append($ScrollBarThumb)
+
+  const $Resizer1 = document.createElement('div')
+  $Resizer1.className = 'Resizer'
+
+  const $Resizer2 = document.createElement('div')
+  $Resizer2.className = 'Resizer'
+
+  $KeyBindingsTableWrapper.append($Resizer1, $Resizer2)
+
+  const $Viewlet = document.createElement('div')
+  $Viewlet.className = 'Viewlet KeyBindings'
+  $Viewlet.append($KeyBindingsHeader, $KeyBindingsTableWrapper, $ScrollBar)
+
+  return {
+    $Viewlet,
+    $InputBox,
+    $KeyBindingsHeader,
+    $KeyBindingsTableWrapper,
+    $ScrollBarThumb,
+    $ScrollBar,
+    $Resizer1,
+    $Resizer2,
+  }
+}
+
+export const attachEvents = (state) => {
   const { $Viewlet } = state
-  const $InputBox = $Viewlet.querySelector('input')
+  $Viewlet.onpointerdown = ViewletkeyBindingsEvents.handlePointerDown
+  $Viewlet.ondblclick = ViewletkeyBindingsEvents.handleTableDoubleClick
+  $Viewlet.addEventListener(DomEventType.Input, ViewletkeyBindingsEvents.handleInput, { capture: true })
+  $Viewlet.addEventListener(DomEventType.Wheel, ViewletkeyBindingsEvents.handleWheel, DomEventOptions.Passive)
+}
+
+export const setTableDom = (state, dom) => {
+  const { $KeyBindingsTableWrapper } = state
+  const $Root = VirtualDom.render(dom)
+  const $Child = $KeyBindingsTableWrapper.children[0]
+  if ($Child.tagName === 'TABLE') {
+    $Child.replaceWith($Root.firstChild)
+  } else {
+    $KeyBindingsTableWrapper.prepend($Root.firstChild)
+  }
+}
+
+export const setValue = (state, value) => {
+  const { $InputBox } = state
   $InputBox.value = value
 }
 
 export const setColumnWidths = (state, columnWidth1, columnWidth2, columnWidth3) => {
   const paddingLeft = 15
-  const { $Viewlet } = state
-  const $$Resizers = $Viewlet.querySelectorAll('.Resizer')
-  const $Resizer1 = $$Resizers[0]
-  const $Resizer2 = $$Resizers[1]
+  const { $Resizer1, $Resizer2 } = state
   $Resizer1.style.left = `${paddingLeft + columnWidth1}px`
   $Resizer2.style.left = `${paddingLeft + columnWidth1 + columnWidth2}px`
 }
 
 export * from '../ViewletScrollable/ViewletScrollable.ts'
 export * from '../ViewletSizable/ViewletSizable.ts'
-
-export const Events = ViewletkeyBindingsEvents
