9590d8ebe23383e0aa42255619b9e63a26b8f715
Le Vivilet <levivlet@pm.me>
2024-04-13 13:39:30 +0200

    update flags

--- packages/main-process/src/parts/GetSharedProcessArgv/GetSharedProcessArgv.js
+++ packages/main-process/src/parts/GetSharedProcessArgv/GetSharedProcessArgv.js
@@ -4,10 +4,5 @@ export const getSharedProcessArgv = () => {
   if (Platform.isProduction) {
     return ['--enable-source-maps']
   }
-  if (process.versions.node === '18.19.1' || process.versions.node === '18.18.2') {
-    // node<=20.5, deprecated
-    return ['--loader', '@swc-node/register/esm']
-  }
-  // node>=20.6
-  return ['--import', '@swc-node/register/esm-register']
+  return []
 }
