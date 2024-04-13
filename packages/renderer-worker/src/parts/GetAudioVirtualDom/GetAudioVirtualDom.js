d640f18431355d5f846c0afb11c344981edb1b9d
Le Vivilet <levivlet@pm.me>
2024-04-13 09:57:11 +0200

    feature: use functional root for audio view

--- packages/renderer-worker/src/parts/GetAudioVirtualDom/GetAudioVirtualDom.js
+++ packages/renderer-worker/src/parts/GetAudioVirtualDom/GetAudioVirtualDom.js
@@ -2,5 +2,13 @@ import * as GetMediaVirtualDom from '../GetMediaVirtualDom/GetMediaVirtualDom.js
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 
 export const getAudioVirtualDom = (src, errorMessage) => {
-  return GetMediaVirtualDom.getMediaVirtualDom(VirtualDomElements.Audio, src, errorMessage)
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: 'Viewlet Media Audio',
+      childCount: 1,
+      onError: 'handleAudioError',
+    },
+    ...GetMediaVirtualDom.getMediaVirtualDom(VirtualDomElements.Audio, src, errorMessage),
+  ]
 }
