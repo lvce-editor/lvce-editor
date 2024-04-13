74e3093c0f72961bce9a2c78eccf475a38fbd19c
Le Vivilet <levivlet@pm.me>
2024-04-13 18:42:19 +0200

    feature: add icon for keybindings tab

--- packages/renderer-worker/src/parts/PathDisplay/PathDisplay.js
+++ packages/renderer-worker/src/parts/PathDisplay/PathDisplay.js
@@ -1,5 +1,6 @@
 import * as Workspace from '../Workspace/Workspace.js'
 import * as IconTheme from '../IconTheme/IconTheme.js'
+import * as AssetDir from '../AssetDir/AssetDir.js'
 
 export const getTitle = (uri) => {
   if (!uri) {
@@ -18,6 +19,9 @@ export const getLabel = (uri) => {
 }
 
 export const getFileIcon = (uri) => {
+  if (uri === 'app://keybindings') {
+    return `MaskIconRecordKey`
+  }
   const baseName = Workspace.pathBaseName(uri)
   const icon = IconTheme.getFileIcon({ name: baseName })
   return icon
