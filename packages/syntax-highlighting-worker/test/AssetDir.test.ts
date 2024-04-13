39f3c1ee594286e3b3c0cda25252b43e8136dc12
Le Vivilet <levivlet@pm.me>
2024-04-13 23:39:23 +0200

    feature: remove unused syntax highlighting worker code

--- packages/syntax-highlighting-worker/test/AssetDir.test.ts
+++
@@ -1,6 +0,0 @@
-import * as AssetDir from '../src/parts/AssetDir/AssetDir.ts'
-import { test, expect } from '@jest/globals'
-
-test('assetDir', () => {
-  expect(typeof AssetDir.assetDir).toBe('string')
-})
