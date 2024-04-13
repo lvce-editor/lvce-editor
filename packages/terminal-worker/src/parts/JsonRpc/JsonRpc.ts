88e9b4eb2ce745ded1e6284557edaf20e100af1b
Le Vivilet <levivlet@pm.me>
2024-04-13 22:49:01 +0200

    fix build

--- packages/terminal-worker/src/parts/JsonRpc/JsonRpc.ts
+++ packages/terminal-worker/src/parts/JsonRpc/JsonRpc.ts
@@ -1,2 +1,2 @@
 // @ts-ignore
-export * from '/static/js/lvce-editor-json-rpc.js'
+export { send, invoke, invokeAndTransfer, send, resolve, registerPromise, handleJsonRpcMessage } from '/static/js/lvce-editor-json-rpc.js'
