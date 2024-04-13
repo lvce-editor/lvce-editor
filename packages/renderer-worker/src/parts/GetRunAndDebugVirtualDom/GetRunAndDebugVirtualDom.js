731c9d692f792a10fbf16c27a6b0816ab00a6314
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-13 10:33:35 +0200

    feature: use functional root for debug view (#5625)

--- packages/renderer-worker/src/parts/GetRunAndDebugVirtualDom/GetRunAndDebugVirtualDom.js
+++ packages/renderer-worker/src/parts/GetRunAndDebugVirtualDom/GetRunAndDebugVirtualDom.js
@@ -2,9 +2,16 @@ import * as GetRunAndDebugBreakPointsVirtualDom from '../GetRunAndDebugBreakPoin
 import * as GetRunAndDebugCallStackVirtualDom from '../GetRunAndDebugCallStackVirtualDom/GetRunAndDebugCallStackVirtualDom.js'
 import * as GetRunAndDebugScopeVirtualDom from '../GetRunAndDebugScopeVirtualDom/GetRunAndDebugScopeVirtualDom.js'
 import * as GetRunAndDebugWatchVirtualDom from '../GetRunAndDebugWatchVirtualDom/GetRunAndDebugWatchVirtualDom.js'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 
 export const getRunAndDebugVirtualDom = (state) => {
   const dom = []
+  dom.push({
+    type: VirtualDomElements.Div,
+    className: 'Viewlet RunAndDebug',
+    tabIndex: 0,
+    childCount: 4,
+  })
   dom.push(...GetRunAndDebugWatchVirtualDom.renderWatch(state))
   dom.push(...GetRunAndDebugBreakPointsVirtualDom.renderBreakPoints(state))
   dom.push(...GetRunAndDebugScopeVirtualDom.getRunAndDebugScopeVirtualDom(state))
