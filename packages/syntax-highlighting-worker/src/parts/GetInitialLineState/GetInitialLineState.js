0edaf6b9c7b8f995ee24bf7709cb3633f18424be
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-14 01:16:34 +0200

    feature: add option to tokenize codeblock in a worker (#5661)

--- packages/syntax-highlighting-worker/src/parts/GetInitialLineState/GetInitialLineState.js
+++
@@ -1,5 +0,0 @@
-import * as DeepCopy from '../DeepCopy/DeepCopy.js'
-
-export const getInitialLineState = (initialLineState) => {
-  return DeepCopy.deepCopy(initialLineState)
-}
--- packages/syntax-highlighting-worker/src/parts/SplitLines/SplitLines.ts
+++ packages/syntax-highlighting-worker/src/parts/SplitLines/SplitLines.ts
@@ -1,6 +1,8 @@
+import * as Character from '../Character/Character.ts'
+
 export const splitLines = (lines: string | undefined): readonly string[] => {
   if (!lines) {
     return []
   }
-  return lines.split('\n')
+  return lines.split(Character.NewLine)
 }
