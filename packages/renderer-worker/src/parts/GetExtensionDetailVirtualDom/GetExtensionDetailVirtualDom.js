11779ab9fa47a5382c396b1aa3b1e63967797dc2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-13 13:39:14 +0200

    fix: extension details content not visible (#5641)

--- packages/renderer-worker/src/parts/GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.js
+++ packages/renderer-worker/src/parts/GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.js
@@ -1,18 +1,31 @@
+import * as AriaRoles from '../AriaRoles/AriaRoles.js'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.js'
 import * as GetExtensionDetailHeaderVirtualDom from '../GetExtensionDetailHeaderVirtualDom/GetExtensionDetailHeaderVirtualDom.js'
 import * as GetMarkdownVirtualDom from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.js'
-import * as GetTotalChildCount from '../GetTotalChildCount/GetTotalChildCount.js'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
-import * as AriaRoles from '../AriaRoles/AriaRoles.js'
+
+const getMarkdownChildCount = (markdownDom) => {
+  const max = markdownDom.length - 1
+  let stack = []
+  for (let i = max; i >= 0; i--) {
+    const element = markdownDom[i]
+    console.log({ element, markdownDom, i })
+    if (element.childCount > 0) {
+      stack = stack.slice(element.childCount)
+    }
+    stack.unshift(element)
+  }
+  return stack.length
+}
 
 export const getExtensionDetailVirtualDom = (extensionDetail, sanitizedReadmeHtml) => {
   const markdownDom = GetMarkdownVirtualDom.getMarkdownVirtualDom(sanitizedReadmeHtml)
-  const childCount = GetTotalChildCount.getTotalChildCount(markdownDom, 0)
+  const childCount = getMarkdownChildCount(markdownDom)
   const dom = [
     {
       type: VirtualDomElements.Div,
       className: 'Viewlet ExtensionDetail',
-      childCount: 2,
+      childCount: childCount + 1,
     },
 
     ...GetExtensionDetailHeaderVirtualDom.getExtensionDetailHeaderVirtualDom(extensionDetail),
