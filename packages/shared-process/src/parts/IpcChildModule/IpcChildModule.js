401195d2bf493962e290c8fcd7386690965ca273
Le Vivilet <levivlet@pm.me>
2024-04-13 15:31:52 +0200

    simplify ipc

--- packages/shared-process/src/parts/IpcChildModule/IpcChildModule.js
+++ packages/shared-process/src/parts/IpcChildModule/IpcChildModule.js
@@ -1,5 +1,11 @@
 import * as IpcChildType from '../IpcChildType/IpcChildType.js'
-import { IpcChildWithElectronUtilityProcess, IpcChildWithWebSocket, IpcChildWithNodeWorker, IpcChildWithNodeForkedProcess } from '@lvce-editor/ipc'
+import {
+  IpcChildWithElectronUtilityProcess,
+  IpcChildWithWebSocket,
+  IpcChildWithNodeWorker,
+  IpcChildWithNodeForkedProcess,
+  IpcChildWithElectronMessagePort,
+} from '@lvce-editor/ipc'
 
 export const getModule = (method) => {
   switch (method) {
@@ -10,7 +16,7 @@ export const getModule = (method) => {
     case IpcChildType.ElectronUtilityProcess:
       return IpcChildWithElectronUtilityProcess
     case IpcChildType.ElectronMessagePort:
-      return import('../IpcChildWithElectronMessagePort/IpcChildWithElectronMessagePort.js')
+      return IpcChildWithElectronMessagePort
     case IpcChildType.NodeMessagePort:
       return import('../IpcChildWithNodeMessagePort/IpcChildWithNodeMessagePort.js')
     case IpcChildType.WebSocket:
