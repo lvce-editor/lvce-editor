d6d80c9fea8f848634ab2d8b6085c8739be078a9
Le Vivilet <levivlet@pm.me>
2024-04-14 17:03:09 +0200

    fix imports

--- packages/build/src/parts/ReplaceTs/ReplaceTs.js
+++ packages/build/src/parts/ReplaceTs/ReplaceTs.js
@@ -1,3 +1,19 @@
+export const fixImports = (content) => {
+  if (!content) {
+    return content
+  }
+  const newLines = []
+  const lines = content.split('\n')
+  for (const line of lines) {
+    let newLine = line
+    if (newLine.startsWith('import') || (newLine.startsWith('{') && newLine.endsWith(".ts'"))) {
+      newLine = newLine.replace(".ts'", ".js'")
+    }
+    newLines.push(newLine)
+  }
+  return newLines.join('\n')
+}
+
 export const replaceTs = async (content) => {
   if (!content) {
     return content
@@ -9,5 +25,5 @@ export const replaceTs = async (content) => {
       target: 'esnext',
     },
   })
-  return newContent
+  return fixImports(newContent)
 }
