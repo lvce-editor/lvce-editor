ad4342dd9259f917d8f3776d3ee7c997e0f94e83
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-14 08:28:31 +0200

    feature: simplify test worker ipc (#5668)

--- packages/test-worker/src/parts/JsonRpc/JsonRpc.ts
+++ packages/test-worker/src/parts/JsonRpc/JsonRpc.ts
@@ -1,2 +1,2 @@
 // @ts-ignore
-export * from '../../../../../static/js/lvce-editor-json-rpc.js'
+export { handleJsonRpcMessage, invoke, invokeAndTransfer, registerPromise, resolve, send } from '/static/js/lvce-editor-json-rpc.js'
