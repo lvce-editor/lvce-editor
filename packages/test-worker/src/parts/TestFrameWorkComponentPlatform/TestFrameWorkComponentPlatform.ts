b29dedf434c787e89ec350709b560dec1367222f
Le Vivilet <levivlet@pm.me>
2024-04-14 07:38:32 +0200

    feature: lint test worker

--- packages/test-worker/src/parts/TestFrameWorkComponentPlatform/TestFrameWorkComponentPlatform.ts
+++ packages/test-worker/src/parts/TestFrameWorkComponentPlatform/TestFrameWorkComponentPlatform.ts
@@ -1,10 +1,10 @@
 import * as IsFirefox from '../IsFirefox/IsFirefox.ts'
 import * as PlatformPaths from '../PlatformPaths/PlatformPaths.ts'
 
-export const getNodePath = () => {
+export const getNodePath = (): Promise<string> => {
   return PlatformPaths.getNodePath()
 }
 
-export const isFirefox = () => {
+export const isFirefox = (): boolean => {
   return IsFirefox.isFirefox
 }
