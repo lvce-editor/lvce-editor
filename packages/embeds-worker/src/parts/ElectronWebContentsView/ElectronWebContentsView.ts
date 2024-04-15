0cfacef0aba7316203f224eab88a9c3a800d4f07
Le Vivilet <levivlet@pm.me>
2024-04-15 20:26:34 +0200

    feature: send webcontents commands to embeds worker

--- packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
+++ packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
@@ -1,8 +1,19 @@
-export const createWebContentsView = (restoreId, fallThroughKeyBindings) => {
+import * as Rpc from '../Rpc/Rpc.ts'
+
+export const createWebContentsView = async (restoreId, fallThroughKeyBindings) => {
   // TODO send to shared process
   console.log({ restoreId, fallThroughKeyBindings })
+  const port = await Rpc.invoke('IpcParent.create', {
+    method: 8,
+    type: 1,
+    raw: true,
+    initialCommand: 'HandleMessageportForSharedProcess.handleMessagePortForSharedProcess',
+  })
+  console.log({ port })
+  throw new Error('not implemented')
 }
 
 export const disposeWebContentsView = (id) => {
   // TODO send to shared process
+  throw new Error('not implemented')
 }
