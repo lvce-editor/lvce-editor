673408ed2914707c41e5be86e8e64be66b5af2a8
Le Vivilet <levivlet@pm.me>
2024-04-16 18:47:29 +0200

    fix: send handle

--- packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
+++ packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
@@ -3,13 +3,13 @@ import * as Rpc from '../Rpc/Rpc.ts'
 export const createWebContentsView = async (restoreId, fallThroughKeyBindings) => {
   // TODO send to shared process
   console.log({ restoreId, fallThroughKeyBindings })
-  const port = await Rpc.invoke('IpcParent.create', {
+  const { port1, port2 } = new MessageChannel()
+  await Rpc.invokeAndTransfer('IpcParent.create', [port1], {
     method: 8,
     type: 1,
-    raw: true,
-    initialCommand: 'HandleMessageportForSharedProcess.handleMessagePortForSharedProcess',
+    initialCommand: 'HandleNodeMessagePort.handleNodeMessagePort',
   })
-  console.log({ port })
+  console.log({ port2 })
   throw new Error('not implemented')
 }
 
