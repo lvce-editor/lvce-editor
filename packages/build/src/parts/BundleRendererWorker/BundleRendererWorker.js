f8650fe8f2d94c27a289a0a11cc1f17fe8593f5a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-16 21:01:07 +0200

    feature: update embeds worker url during build (#5706)

--- packages/build/src/parts/BundleRendererWorker/BundleRendererWorker.js
+++ packages/build/src/parts/BundleRendererWorker/BundleRendererWorker.js
@@ -93,6 +93,11 @@ export const bundleRendererWorker = async ({ cachePath, platform, commitHash, as
       occurrence: '/packages/terminal-worker/src/terminalWorkerMain.ts',
       replacement: `/packages/terminal-worker/dist/terminalWorkerMain.js`,
     })
+    await Replace.replace({
+      path: `${cachePath}/src/parts/EmbedsWorkerUrl/EmbedsWorkerUrl.js`,
+      occurrence: '/packages/embeds-worker/src/embedsWorkerMain.ts',
+      replacement: `/packages/embeds-worker/dist/embedsWorkerMain.js`,
+    })
 
     await Replace.replace({
       path: `${cachePath}/src/parts/Product/Product.js`,
