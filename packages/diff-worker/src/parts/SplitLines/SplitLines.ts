36b4db2e7efa299e479aad5b4486d14aadad8da9
Le Vivilet <levivlet@pm.me>
2024-04-16 18:28:20 +0200

    remove unused code

--- packages/diff-worker/src/parts/SplitLines/SplitLines.ts
+++
@@ -1,8 +0,0 @@
-import * as Character from '../Character/Character.ts'
-
-export const splitLines = (lines: string | undefined): readonly string[] => {
-  if (!lines) {
-    return []
-  }
-  return lines.split(Character.NewLine)
-}
