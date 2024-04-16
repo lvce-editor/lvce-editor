a9d63fd091f1c5e86b63b4adf0d20a4a4c064c95
Le Vivilet <levivlet@pm.me>
2024-04-16 20:44:38 +0200

    remove log

--- packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
+++ packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
@@ -2,7 +2,6 @@ import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
 
 export const createWebContentsView = async (restoreId, fallThroughKeyBindings) => {
   const id = await SharedProcess.invoke('ElectronWebContentsView.createWebContentsView', restoreId, fallThroughKeyBindings)
-  console.log({ id })
   return id
 }
 
