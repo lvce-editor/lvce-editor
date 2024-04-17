5264ff4be88d9e7ee523124286b8a2161d07ed92
Le Vivilet <levivlet@pm.me>
2024-04-17 19:56:38 +0200

    fix extension host rpc

--- packages/renderer-worker/src/parts/ExtensionHostRpc/ExtensionHostRpc.js
+++ packages/renderer-worker/src/parts/ExtensionHostRpc/ExtensionHostRpc.js
@@ -1,11 +1,11 @@
 import * as Callback from '../Callback/Callback.js'
 import * as Command from '../Command/Command.js'
+import * as GetErrorResponse from '../GetErrorResponse/GetErrorResponse.js'
 import * as GetResponse from '../GetResponse/GetResponse.js'
 import * as GlobalEventBus from '../GlobalEventBus/GlobalEventBus.js'
 import * as HasTransferableResult from '../HasTransferableResult/HasTransferableResult.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 import { JsonRpcError } from '../JsonRpcError/JsonRpcError.js'
-import * as GetErrorResponse from '../GetErrorResponse/GetErrorResponse.js'
 
 // TODO use normal ipc methods
 const isResultMessage = (message) => {
@@ -16,19 +16,21 @@ const isErrorMessage = (message) => {
   return 'error' in message
 }
 
-const handleMessageMethod = async (message, event) => {
+const handleMessageMethod = async (event) => {
+  const { data, target } = event
+  const message = data
   if ('id' in message) {
     if ('method' in message) {
       const response = await GetResponse.getResponse(message, Command.execute)
       if (HasTransferableResult.hasTransferrableResult(message.method) && 'result' in response) {
         try {
-          event.target.send(response, [response.result])
+          target.send(response, [response.result])
         } catch (error) {
           const response = GetErrorResponse.getErrorResponse(message, error)
-          event.target.send(response)
+          target.send(response)
         }
       } else {
-        event.target.send(response)
+        target.send(response)
       }
       return
     }
@@ -38,12 +40,13 @@ const handleMessageMethod = async (message, event) => {
   throw new JsonRpcError('unexpected message from extension host')
 }
 
-const handleMessage = async (message, event) => {
+const handleMessage = async (event) => {
+  const message = event.data
   if (message.id) {
     if (isResultMessage(message) || isErrorMessage(message)) {
       Callback.resolve(message.id, message)
     } else if ('method' in message) {
-      handleMessageMethod(message, event)
+      handleMessageMethod(event)
     } else {
       throw new JsonRpcError('unexpected message type')
     }
