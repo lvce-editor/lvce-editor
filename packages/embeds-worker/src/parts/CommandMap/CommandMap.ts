bed2ce83fdf2649941c87d079cf2d34024bfa7fb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-17 19:39:06 +0200

    feature: add more embeds worker functions (#5709)

--- packages/embeds-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/embeds-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,8 +1,20 @@
 import * as ElectronWebContentsView from '../ElectronWebContentsView/ElectronWebContentsView.ts'
 
 export const commandMap = {
+  'ElectronWebContentsView.backward': ElectronWebContentsView.backward,
+  'ElectronWebContentsView.cancelNavigation': ElectronWebContentsView.cancelNavigation,
+  'ElectronWebContentsView.copyImageAt': ElectronWebContentsView.copyImageAt,
   'ElectronWebContentsView.createWebContentsView': ElectronWebContentsView.createWebContentsView,
   'ElectronWebContentsView.disposeWebContentsView': ElectronWebContentsView.disposeWebContentsView,
+  'ElectronWebContentsView.focus': ElectronWebContentsView.focus,
+  'ElectronWebContentsView.forward': ElectronWebContentsView.forward,
+  'ElectronWebContentsView.getStats': ElectronWebContentsView.getStats,
+  'ElectronWebContentsView.hide': ElectronWebContentsView.hide,
+  'ElectronWebContentsView.inspectElement': ElectronWebContentsView.inspectElement,
+  'ElectronWebContentsView.openDevtools': ElectronWebContentsView.openDevtools,
+  'ElectronWebContentsView.reload': ElectronWebContentsView.reload,
   'ElectronWebContentsView.resizeWebContentsView': ElectronWebContentsView.resizeWebContentsView,
+  'ElectronWebContentsView.setFallthroughKeyBindings': ElectronWebContentsView.setFallthroughKeyBindings,
   'ElectronWebContentsView.setIframeSrc': ElectronWebContentsView.setIframeSrc,
+  'ElectronWebContentsView.show': ElectronWebContentsView.show,
 }
--- packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
+++ packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
@@ -16,3 +16,51 @@ export const resizeWebContentsView = (id, x, y, width, height) => {
 export const setIframeSrc = (id, iframeSrc) => {
   return SharedProcess.invoke('ElectronWebContentsViewFunctions.setIframeSrc', id, iframeSrc)
 }
+
+export const focus = (id) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.focus', id)
+}
+
+export const openDevtools = (id) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.openDevtools', id)
+}
+
+export const reload = (id) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.reload', id)
+}
+
+export const show = (id) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.show', id)
+}
+
+export const hide = (id) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.hide', id)
+}
+
+export const forward = (id) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.forward', id)
+}
+
+export const backward = (id) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.backward', id)
+}
+
+export const cancelNavigation = (id) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.cancelNavigation', id)
+}
+
+export const inspectElement = (id, x, y) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.inspectElement', id, x, y)
+}
+
+export const copyImageAt = (id, x, y) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.copyImageAt', id, x, y)
+}
+
+export const setFallthroughKeyBindings = (id, fallthroughKeybindings) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.setFallthroughKeyBindings', id, fallthroughKeybindings)
+}
+
+export const getStats = (id, fallthroughKeybindings) => {
+  return SharedProcess.invoke('ElectronWebContentsViewFunctions.getStats', id, fallthroughKeybindings)
+}
