5d5ef830d57b3e8ec3b467cd56c4cd0898c841fd
Le Vivilet <levivlet@pm.me>
2024-04-20 00:16:28 +0200

    feature: add functions to bundle embeds process

---
+++ packages/build/src/parts/FilterEmbedsProcessDependencies/FilterEmbedsProcessDependencies.js
@@ -0,0 +1,27 @@
+import * as WalkDependencies from '../WalkDependencies/WalkDependencies.js'
+
+export const filterDependencies = (rawDependencies, exclude = []) => {
+  const dependencyPaths = []
+  const handleDependency = (dependency) => {
+    if (!dependency.path) {
+      return false
+    }
+    if (!dependency.name) {
+      return false
+    }
+
+    if (dependency.name.includes('@types')) {
+      return false
+    }
+    if (dependency.name === 'type-fest') {
+      return false
+    }
+    if (exclude.includes(dependency.name)) {
+      return false
+    }
+    dependencyPaths.push(dependency.path)
+    return true
+  }
+  WalkDependencies.walkDependencies(rawDependencies, handleDependency)
+  return dependencyPaths.slice(1)
+}
