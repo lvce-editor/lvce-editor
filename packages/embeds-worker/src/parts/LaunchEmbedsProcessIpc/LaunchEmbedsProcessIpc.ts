88b29949f345260267f04927593e430c8ff9e164
Le Vivilet <levivlet@pm.me>
2024-04-20 00:27:38 +0200

    simplify ipc

--- packages/embeds-worker/src/parts/LaunchEmbedsProcessIpc/LaunchEmbedsProcessIpc.ts
+++ packages/embeds-worker/src/parts/LaunchEmbedsProcessIpc/LaunchEmbedsProcessIpc.ts
@@ -1,45 +1,10 @@
-import * as Rpc from '../Rpc/Rpc.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
-
-const getPort = async () => {
-  const { port1, port2 } = new MessageChannel()
-  await Rpc.invokeAndTransfer('IpcParent.create', [port1], {
-    method: 8,
-    type: 1,
-    initialCommand: 'HandleElectronMessagePortForEmbedsProcess.handleElectronMessagePortForEmbedsProcess',
-    port: port1,
-    raw: true,
-  })
-
-  return port2
-}
-
-const createIpc = (port) => {
-  const ipc = {
-    port,
-    send(message) {
-      port.postMessage(message)
-    },
-    set onmessage(listener) {
-      const wrappedListener = (event) => {
-        const syntheticEvent = {
-          target: this,
-          data: event.data,
-        }
-        listener(syntheticEvent)
-      }
-      this.port.onmessage = wrappedListener
-    },
-    dispose() {
-      this.port.close
-    },
-  }
-  return ipc
-}
+import * as IpcParent from '../IpcParent/IpcParent.ts'
 
 export const launchEmbedsProcessIpc = async () => {
-  const port = await getPort()
-  const ipc = createIpc(port)
+  const ipc = await IpcParent.create({
+    initialCommand: 'HandleElectronMessagePortForEmbedsProcess.handleElectronMessagePortForEmbedsProcess',
+  })
   HandleIpc.handleIpc(ipc)
   return ipc
 }
