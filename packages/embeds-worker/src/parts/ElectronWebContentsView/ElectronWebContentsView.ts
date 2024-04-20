caddd8c0f9ee4991fea572d95608637b882fbafc
Le Vivilet <levivlet@pm.me>
2024-04-20 13:15:12 +0200

    send commands to embeds process

--- packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
+++ packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
@@ -1,66 +1,66 @@
-import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
+import * as EmbedsProcess from '../EmbedsProcess/EmbedsProcess.ts'
 
 export const createWebContentsView = async (restoreId, fallThroughKeyBindings) => {
-  const id = await SharedProcess.invoke('ElectronWebContentsView.createWebContentsView', restoreId, fallThroughKeyBindings)
+  const id = await EmbedsProcess.invoke('ElectronWebContentsView.createWebContentsView', restoreId, fallThroughKeyBindings)
   return id
 }
 
 export const disposeWebContentsView = (id) => {
-  return SharedProcess.invoke('ElectronWebContentsView.disposeWebContentsView', id)
+  return EmbedsProcess.invoke('ElectronWebContentsView.disposeWebContentsView', id)
 }
 
 export const resizeWebContentsView = (id, x, y, width, height) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.resizeBrowserView', id, x, y, width, height)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.resizeBrowserView', id, x, y, width, height)
 }
 
 export const setIframeSrc = (id, iframeSrc) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.setIframeSrc', id, iframeSrc)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.setIframeSrc', id, iframeSrc)
 }
 
 export const focus = (id) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.focus', id)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.focus', id)
 }
 
 export const openDevtools = (id) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.openDevtools', id)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.openDevtools', id)
 }
 
 export const reload = (id) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.reload', id)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.reload', id)
 }
 
 export const show = (id) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.show', id)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.show', id)
 }
 
 export const hide = (id) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.hide', id)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.hide', id)
 }
 
 export const forward = (id) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.forward', id)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.forward', id)
 }
 
 export const backward = (id) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.backward', id)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.backward', id)
 }
 
 export const cancelNavigation = (id) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.cancelNavigation', id)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.cancelNavigation', id)
 }
 
 export const inspectElement = (id, x, y) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.inspectElement', id, x, y)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.inspectElement', id, x, y)
 }
 
 export const copyImageAt = (id, x, y) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.copyImageAt', id, x, y)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.copyImageAt', id, x, y)
 }
 
 export const setFallthroughKeyBindings = (id, fallthroughKeybindings) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.setFallthroughKeyBindings', id, fallthroughKeybindings)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.setFallthroughKeyBindings', id, fallthroughKeybindings)
 }
 
 export const getStats = (id, fallthroughKeybindings) => {
-  return SharedProcess.invoke('ElectronWebContentsViewFunctions.getStats', id, fallthroughKeybindings)
+  return EmbedsProcess.invoke('ElectronWebContentsViewFunctions.getStats', id, fallthroughKeybindings)
 }
