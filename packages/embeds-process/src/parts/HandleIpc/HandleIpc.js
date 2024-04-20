0b2649190cba1c6bf8113490d1c7520909e973d7
Le Vivilet <levivlet@pm.me>
2024-04-20 14:32:05 +0200

    fix embeds process ipc

--- packages/embeds-process/src/parts/HandleIpc/HandleIpc.js
+++ packages/embeds-process/src/parts/HandleIpc/HandleIpc.js
@@ -2,22 +2,25 @@ import * as Callback from '../Callback/Callback.js'
 import * as Command from '../Command/Command.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 
-const prepare = (error) => {
+const preparePrettyError = (error) => {
   return error
+}
+
+const logError = (error) => {
+  console.error(error)
 }
 
 const requiresSocket = () => {
   return false
 }
 
-const logError = (error, prettyError) => {
-  console.error(error)
-}
-
 const handleMessage = (event) => {
-  return JsonRpc.handleJsonRpcMessage(event.target, event.data, Command.execute, Callback.resolve, prepare, logError, requiresSocket)
+  return JsonRpc.handleJsonRpcMessage(event.target, event.data, Command.execute, Callback.resolve, preparePrettyError, logError, requiresSocket)
 }
 
 export const handleIpc = (ipc) => {
   ipc.on('message', handleMessage)
+  if (ipc.start) {
+    ipc.start()
+  }
 }
