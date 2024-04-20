b927da7bc6d834ebf13208e86bcb25f38964a2ec
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-20 14:22:06 +0200

    feature: remove unused process explorer code from shared process (#5737)

--- packages/shared-process/src/parts/CreatePidMap/CreatePidMap.js
+++
@@ -1,6 +0,0 @@
-import * as ParentIpc from '../ParentIpc/ParentIpc.js'
-
-export const createPidMap = async () => {
-  const pidMap = await ParentIpc.invoke('CreatePidMap.createPidMap')
-  return pidMap
-}
