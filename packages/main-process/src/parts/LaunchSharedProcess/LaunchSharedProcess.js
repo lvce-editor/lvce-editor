3087b47aa4b50a1dc2fbbba6b5b672ff206d4932
Le Vivilet <levivlet@pm.me>
2024-04-20 18:39:58 +0200

    feature: simplify shared process launch

--- packages/main-process/src/parts/LaunchSharedProcess/LaunchSharedProcess.js
+++ packages/main-process/src/parts/LaunchSharedProcess/LaunchSharedProcess.js
@@ -1,45 +1,16 @@
-import * as Callback from '../Callback/Callback.js'
-import * as Command from '../Command/Command.js'
 import * as ExitCode from '../ExitCode/ExitCode.js'
-import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as GetSharedProcessArgv from '../GetSharedProcessArgv/GetSharedProcessArgv.js'
-import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as Logger from '../Logger/Logger.js'
 import * as Performance from '../Performance/Performance.js'
 import * as PerformanceMarkerType from '../PerformanceMarkerType/PerformanceMarkerType.js'
 import * as Platform from '../Platform/Platform.js'
-import * as PrettyError from '../PrettyError/PrettyError.js'
-import * as PrintPrettyError from '../PrintPrettyError/PrintPrettyError.js'
 import * as Process from '../Process/Process.js'
-import * as RequiresSocket from '../RequiresSocket/RequiresSocket.js'
 import * as SharedProcessState from '../SharedProcessState/SharedProcessState.js'
 
 const handleChildError = (error) => {
   Process.exit(ExitCode.Error)
-}
-
-const logError = (error, prettyError) => {
-  PrintPrettyError.printPrettyError(prettyError, '[main-process] ')
-}
-
-const handleChildMessage = async (event) => {
-  const message = event.data
-  if (message === 'ready') {
-    return
-  }
-  if (Array.isArray(message)) {
-    Logger.warn('invalid message', message)
-    return
-  }
-  await JsonRpc.handleJsonRpcMessage(
-    event.target,
-    message,
-    Command.execute,
-    Callback.resolve,
-    PrettyError.prepare,
-    logError,
-    RequiresSocket.requiresSocket,
-  )
 }
 
 const handleChildExit = (event) => {
@@ -72,9 +43,9 @@ export const launchSharedProcess = async ({ method, env = {} }) => {
     name: 'shared-process',
   })
   sharedProcess.on('error', handleChildError)
-  sharedProcess.on('message', handleChildMessage)
   sharedProcess.on('exit', handleChildExit)
   sharedProcess.on('disconnect', handleChildDisconnect)
+  HandleIpc.handleIpc(sharedProcess)
   SharedProcessState.state.sharedProcess = sharedProcess
   Performance.mark(PerformanceMarkerType.DidStartSharedProcess)
   return sharedProcess
