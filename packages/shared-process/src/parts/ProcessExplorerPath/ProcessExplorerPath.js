c8691867034d36c7ac81e0480c2b044595f27612
Le Vivilet <levivlet@pm.me>
2024-04-20 08:16:40 +0200

    feature: add option to launch process explorer child process from shared process

---
+++ packages/shared-process/src/parts/ProcessExplorerPath/ProcessExplorerPath.js
@@ -0,0 +1,4 @@
+import * as Path from '../Path/Path.js'
+import * as Root from '../Root/Root.js'
+
+export const processExplorerPath = Path.join(Root.root, 'packages', 'process-explorer', 'src', 'processExplorerMain.js')
