f453b05545468799dbc14812026fdc83765ebf6d
Le Vivilet <levivlet@pm.me>
2024-04-20 16:57:00 +0200

    feature: add handleMessage function

--- packages/shared-process/src/parts/HandleIpc/HandleIpc.js
+++ packages/shared-process/src/parts/HandleIpc/HandleIpc.js
@@ -1,31 +1,5 @@
-import * as Callback from '../Callback/Callback.js'
-import * as Command from '../Command/Command.js'
-import * as JsonRpc from '../JsonRpc/JsonRpc.js'
-import * as PrettyError from '../PrettyError/PrettyError.js'
-import * as PrintPrettyError from '../PrintPrettyError/PrintPrettyError.js'
-import * as RequiresSocket from '../RequiresSocket/RequiresSocket.js'
-import * as ShouldLogError from '../ShouldLogError/ShouldLogError.js'
-
-const preparePrettyError = PrettyError.prepare
-
-const logError = (error, prettyError) => {
-  if (ShouldLogError.shouldLogError(error)) {
-    PrintPrettyError.printPrettyError(prettyError, `[shared-process] `)
-  }
-}
-
-const handleMessage = (event) => {
-  return JsonRpc.handleJsonRpcMessage(
-    event.target,
-    event.data,
-    Command.execute,
-    Callback.resolve,
-    preparePrettyError,
-    logError,
-    RequiresSocket.requiresSocket,
-  )
-}
+import * as HandleMessage from '../HandleMessage/HandleMessage.js'
 
 export const handleIpc = (ipc) => {
-  ipc.on('message', handleMessage)
+  ipc.on('message', HandleMessage.handleMessage)
 }
