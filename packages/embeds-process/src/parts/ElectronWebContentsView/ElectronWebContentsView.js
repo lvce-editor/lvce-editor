da788e6c40e2434dfe6b5ce25e558ec705486d53
Le Vivilet <levivlet@pm.me>
2024-04-20 17:41:22 +0200

    fix error

--- packages/embeds-process/src/parts/ElectronWebContentsView/ElectronWebContentsView.js
+++ packages/embeds-process/src/parts/ElectronWebContentsView/ElectronWebContentsView.js
@@ -1,13 +1,11 @@
-import * as ElectronBrowserViewIpcState from '../ElectronBrowserViewIpcState/ElectronBrowserViewIpcState.js'
+import * as Assert from '../Assert/Assert.js'
 import * as ElectronWebContents from '../ElectronWebContents/ElectronWebContents.js'
 import * as ParentIpc from '../ParentIpc/ParentIpc.js'
-import * as Assert from '../Assert/Assert.js'
 
 export const createWebContentsView = async (ipc, restoreId, fallthroughKeyBindings) => {
-  Assert.object(ipc)
   Assert.number(restoreId)
   const webContentsId = await ParentIpc.invoke('ElectronWebContentsView.createWebContentsView', restoreId)
-  ElectronBrowserViewIpcState.add(webContentsId, ipc)
+  // ElectronBrowserViewIpcState.add(webContentsId, ipc)
   // TODO get window id from renderer worker
   await ParentIpc.invoke('ElectronWebContentsView.attachEventListeners', webContentsId)
   await ParentIpc.invoke('ElectronWebContentsViewFunctions.setBackgroundColor', webContentsId, 'white')
