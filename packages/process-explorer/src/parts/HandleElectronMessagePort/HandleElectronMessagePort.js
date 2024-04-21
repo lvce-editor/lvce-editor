0aa366127f8e8548e183a01e6c60424ef0f34b82
Le Vivilet <levivlet@pm.me>
2024-04-21 09:14:01 +0200

    fix: missing pid map in process explorer

--- packages/process-explorer/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js
+++ packages/process-explorer/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js
@@ -2,15 +2,16 @@ import * as Assert from '../Assert/Assert.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcChild from '../IpcChild/IpcChild.js'
 import * as IpcChildType from '../IpcChildType/IpcChildType.js'
+import * as ParentIpc from '../ParentIpc/ParentIpc.js'
 
-export const handleElectronMessagePort = async (messagePort, ...params) => {
+export const handleElectronMessagePort = async (messagePort, isElectron) => {
   Assert.object(messagePort)
   const ipc = await IpcChild.listen({
     method: IpcChildType.ElectronMessagePort,
     messagePort,
   })
   HandleIpc.handleIpc(ipc)
-  // TODO find better way to associate configuration with ipc
-  // @ts-ignore
-  ipc.windowId = params[1]
+  if (isElectron) {
+    ParentIpc.state.ipc = ipc
+  }
 }
