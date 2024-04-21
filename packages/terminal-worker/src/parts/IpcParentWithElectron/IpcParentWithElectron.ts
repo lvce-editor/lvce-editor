9f7d422764690df8d1f405116514b11a95cafffb
Le Vivilet <levivlet@pm.me>
2024-04-21 18:28:59 +0200

    add getPortTuple function to terminal worker

--- packages/terminal-worker/src/parts/IpcParentWithElectron/IpcParentWithElectron.ts
+++ packages/terminal-worker/src/parts/IpcParentWithElectron/IpcParentWithElectron.ts
@@ -1,7 +1,8 @@
+import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
 import * as SendMessagePortToElectron from '../SendMessagePortToElectron/SendMessagePortToElectron.ts'
 
 export const create = async (options) => {
-  const { port1, port2 } = new MessageChannel()
+  const { port1, port2 } = GetPortTuple.getPortTuple()
   await SendMessagePortToElectron.sendMessagePortToElectron(port2, options.initialCommand)
   return port1
 }
