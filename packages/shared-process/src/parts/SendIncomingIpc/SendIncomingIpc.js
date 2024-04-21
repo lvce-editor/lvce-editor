323f5e23edd88c7f304e568953fce3f58b1c9391
Le Vivilet <levivlet@pm.me>
2024-04-22 00:04:34 +0200

    add todo

--- packages/shared-process/src/parts/SendIncomingIpc/SendIncomingIpc.js
+++ packages/shared-process/src/parts/SendIncomingIpc/SendIncomingIpc.js
@@ -31,6 +31,13 @@ const supportsPartialIpcHandling = (ipcId) => {
   }
 }
 
+// TODO fork ipc object instead of ref counting
+// duplicated ipc object sends and receives one message
+// original ipc object is never used and can
+// be collected after being out of scope
+// duplicated ipc can also be collected
+// when it is out of scope
+
 export const sendIncomingIpc = async (target, response, ipcId) => {
   if (!hasState(ipcId) && supportsPartialIpcHandling(ipcId)) {
     HandleIpc.handleIpc(target)
