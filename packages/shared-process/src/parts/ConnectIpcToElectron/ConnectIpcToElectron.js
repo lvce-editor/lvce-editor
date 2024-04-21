6372f8b4a8c8cb90d457dc493a990f451d52ceca
Le Vivilet <levivlet@pm.me>
2024-04-21 14:31:34 +0200

    feature: add ipcId property to check what kind of port is being sent

--- packages/shared-process/src/parts/ConnectIpcToElectron/ConnectIpcToElectron.js
+++ packages/shared-process/src/parts/ConnectIpcToElectron/ConnectIpcToElectron.js
@@ -1,9 +1,10 @@
 import * as GetPortTuple from '../GetPortTuple/GetPortTuple.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 import * as ParentIpc from '../ParentIpc/ParentIpc.js'
+import * as IpcId from '../IpcId/IpcId.js'
 
 export const connectIpcToElectron = async (ipc) => {
   const { port1, port2 } = await GetPortTuple.getPortTuple()
-  await JsonRpc.invokeAndTransfer(ipc, [port1], 'HandleElectronMessagePort.handleElectronMessagePort', /* isElectron */ true)
+  await JsonRpc.invokeAndTransfer(ipc, [port1], 'HandleElectronMessagePort.handleElectronMessagePort', IpcId.MainProcess)
   await ParentIpc.invokeAndTransfer('HandleElectronMessagePort.handleElectronMessagePort', [port2])
 }
