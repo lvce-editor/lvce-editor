075b02280fa2d5622cc63fb44835aefe735a50c1
Le Vivilet <levivlet@pm.me>
2024-04-21 16:18:24 +0200

    fix: process explorer ipc

--- packages/process-explorer/src/parts/HandleIpc/HandleIpc.js
+++ packages/process-explorer/src/parts/HandleIpc/HandleIpc.js
@@ -1,5 +1,10 @@
 import * as HandleMessage from '../HandleMessage/HandleMessage.js'
 
 export const handleIpc = (ipc) => {
-  ipc.on('message', HandleMessage.handleMessage)
+  if ('addEventListener' in ipc) {
+    ipc.addEventListener('message', HandleMessage.handleMessage)
+  } else {
+    // deprecated
+    ipc.on('message', HandleMessage.handleMessage)
+  }
 }
