486bb0a01b331a4c5e10cbeeced04645a6ce8359
Le Vivilet <levivlet@pm.me>
2024-04-21 15:49:55 +0200

    add test

--- packages/shared-process/test/HandleIpcModule.test.ts
+++ packages/shared-process/test/HandleIpcModule.test.ts
@@ -1,8 +1,13 @@
 import { expect, test } from '@jest/globals'
 import * as HandleIpcModule from '../src/parts/HandleIpcModule/HandleIpcModule.js'
 import * as HandleIpcSharedProcess from '../src/parts/HandleIpcSharedProcess/HandleIpcSharedProcess.js'
+import * as HandleIpcEmbedsProcess from '../src/parts/HandleIpcEmbedsProcess/HandleIpcEmbedsProcess.js'
 import * as IpcId from '../src/parts/IpcId/IpcId.js'
 
-test('shared process', async () => {
+test('shared process', () => {
   expect(HandleIpcModule.getModule(IpcId.SharedProcess)).toBe(HandleIpcSharedProcess)
+})
+
+test('embeds process', () => {
+  expect(HandleIpcModule.getModule(IpcId.EmbedsProcess)).toBe(HandleIpcEmbedsProcess)
 })
