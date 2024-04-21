574a6ec6497174b4b22e3d5c6c6b812183abda62
Le Vivilet <levivlet@pm.me>
2024-04-21 23:26:03 +0200

    fix error

--- packages/pty-host/src/parts/HandleIpc/HandleIpc.js
+++ packages/pty-host/src/parts/HandleIpc/HandleIpc.js
@@ -8,5 +8,8 @@ export const handleIpc = (ipc) => {
   } else if ('onmessage' in ipc) {
     // deprecated
     ipc.onmessage = HandleMessage.handleMessage
+  } else if ('on' in ipc) {
+    // deprecated
+    ipc.on('message', HandleMessage.handleMessage)
   }
 }
