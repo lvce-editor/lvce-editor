7dd54bcb80591696fc6ae76e1fe16ece4d50f12e
Le Vivilet <levivlet@pm.me>
2024-04-21 16:45:29 +0200

    fix: helper processs ipc

--- packages/extension-host-helper-process/src/parts/HandleIpc/HandleIpc.js
+++ packages/extension-host-helper-process/src/parts/HandleIpc/HandleIpc.js
@@ -1,23 +1,10 @@
-import * as Callback from '../Callback/Callback.js'
-import * as Command from '../Command/Command.js'
-import * as JsonRpc from '../JsonRpc/JsonRpc.js'
-import * as PrettyError from '../PrettyError/PrettyError.js'
-import * as PrintPrettyError from '../PrintPrettyError/PrintPrettyError.js'
-
-const preparePrettyError = PrettyError.prepare
-
-const logError = (error, prettyError) => {
-  PrintPrettyError.printPrettyError(prettyError, `[extension-host-helper-process] `)
-}
-
-const requiresSocket = () => {
-  return false
-}
-
-const handleMessage = (event) => {
-  return JsonRpc.handleJsonRpcMessage(event.target, event.data, Command.execute, Callback.resolve, preparePrettyError, logError, requiresSocket)
-}
+import * as HandleMessage from '../HandleMessage/HandleMessage.js'
 
 export const handleIpc = (ipc) => {
-  ipc.on('message', handleMessage)
+  if ('addEventListener' in ipc) {
+    ipc.addEventListener('message', HandleMessage.handleMessage)
+  } else {
+    // deprecated
+    ipc.on('message', HandleMessage.handleMessage)
+  }
 }
