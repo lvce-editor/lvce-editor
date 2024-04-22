74da38d9ba699329623d62e93abc075e1f53ee44
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-22 18:04:00 +0200

    update dependencies (#5800)

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -18,7 +18,7 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x msw -x electron -x @types/node`
+  OUTPUT=`ncu -u -x msw -x electron -x @types/node -x rollup`
   SUB='All dependencies match the latest package versions'
   if [[ "$OUTPUT" == *"$SUB"* ]]; then
     echo "$OUTPUT"
