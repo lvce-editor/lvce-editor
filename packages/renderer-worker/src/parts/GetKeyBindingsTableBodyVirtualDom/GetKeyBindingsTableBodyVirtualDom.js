cd7152ea45457350c386e6601a963d2adf662a38
Le Vivilet <levivlet@pm.me>
2024-04-22 20:36:19 +0200

    simplofy vdom

---
+++ packages/renderer-worker/src/parts/GetKeyBindingsTableBodyVirtualDom/GetKeyBindingsTableBodyVirtualDom.js
@@ -0,0 +1,130 @@
+import * as ClassNames from '../ClassNames/ClassNames.js'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.js'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
+
+const kbdDom = {
+  type: VirtualDomElements.Kbd,
+  className: ClassNames.Key,
+  childCount: 1,
+}
+
+const highlight = {
+  type: VirtualDomElements.Span,
+  className: ClassNames.SearchHighlight,
+  childCount: 1,
+}
+
+const textCtrl = text('Ctrl')
+const textShift = text('Shift')
+const textPlus = text('+')
+
+const addHighlights = (tableCell, dom, highlights, label) => {
+  dom.push(tableCell)
+  let position = 0
+  for (let i = 0; i < highlights.length; i += 2) {
+    const highlightStart = highlights[i]
+    const highlightEnd = highlights[i + 1]
+    if (position < highlightStart) {
+      const beforeText = label.slice(position, highlightStart)
+      tableCell.childCount++
+      dom.push(text(beforeText))
+    }
+    const highlightText = label.slice(highlightStart, highlightEnd)
+    tableCell.childCount++
+    dom.push(highlight, text(highlightText))
+    position = highlightEnd
+  }
+  if (position < label.length) {
+    const afterText = label.slice(position)
+    tableCell.childCount++
+    dom.push(text(afterText))
+  }
+}
+
+// TODO needing childCount variable everywhere can be error prone
+const getKeyBindingCellChildren = (keyBinding) => {
+  const { isCtrl, isShift, key, keyMatches, commandMatches } = keyBinding
+  const children = []
+  let childCount = 0
+  if (isCtrl) {
+    childCount += 2
+    children.push(kbdDom, textCtrl, textPlus)
+  }
+  if (isShift) {
+    childCount += 2
+    children.push(kbdDom, textShift, textPlus)
+  }
+  childCount++
+  children.push(kbdDom, text(key))
+  return { children, childCount }
+}
+
+const tableCellProps = {
+  className: ClassNames.KeyBindingsTableCell,
+}
+
+const getRowClassName = (isEven, selected) => {
+  return MergeClassNames.mergeClassNames(
+    ClassNames.KeyBindingsTableRow,
+    isEven ? ClassNames.KeyBindingsTableRowEven : ClassNames.KeyBindingsTableRowOdd,
+    selected ? ClassNames.KeyBindingsTableRowSelected : '',
+  )
+}
+
+const getTableRowDom = (keyBinding) => {
+  const { children, childCount } = getKeyBindingCellChildren(keyBinding)
+  const { rowIndex, selected, commandMatches, command } = keyBinding
+  const commandHighlights = commandMatches.slice(1)
+  const isEven = rowIndex % 2 === 0
+  const className = getRowClassName(isEven, selected)
+  const dom = []
+  dom.push({
+    type: VirtualDomElements.Tr,
+    ariaRowIndex: rowIndex,
+    className,
+    key: rowIndex,
+    childCount: 4,
+  })
+  const tableCell = {
+    type: VirtualDomElements.Td,
+    ...tableCellProps,
+    childCount: 0,
+  }
+  dom.push({
+    type: VirtualDomElements.Td,
+    childCount: 0,
+    className: ClassNames.KeyBindingsTableCell,
+  })
+  addHighlights(tableCell, dom, commandHighlights, command)
+  dom.push(
+    {
+      type: VirtualDomElements.Td,
+      className: ClassNames.KeyBindingsTableCell,
+      childCount,
+    },
+    ...children,
+    {
+      type: VirtualDomElements.Td,
+      className: ClassNames.KeyBindingsTableCell,
+      childCount: 1,
+    },
+    text(keyBinding.when || ''),
+  )
+  return dom
+}
+
+const getTableBodyDom = (displayKeyBindings) => {
+  return [
+    {
+      type: VirtualDomElements.TBody,
+      className: ClassNames.KeyBindingsTableBody,
+      childCount: displayKeyBindings.length,
+    },
+    ...displayKeyBindings.flatMap(getTableRowDom),
+  ]
+}
+
+export const getKeyBindingsTableBodyDom = (displayKeyBindings) => {
+  return getTableBodyDom(displayKeyBindings)
+}
