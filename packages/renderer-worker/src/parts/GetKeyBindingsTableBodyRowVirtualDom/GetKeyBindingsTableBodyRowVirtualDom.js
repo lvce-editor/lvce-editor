826a55d20533565b2b7bfe16620d7adc126f9bfb
Le Vivilet <levivlet@pm.me>
2024-04-24 19:34:41 +0200

    simplify dom

--- packages/renderer-worker/src/parts/GetKeyBindingsTableBodyRowVirtualDom/GetKeyBindingsTableBodyRowVirtualDom.js
+++ packages/renderer-worker/src/parts/GetKeyBindingsTableBodyRowVirtualDom/GetKeyBindingsTableBodyRowVirtualDom.js
@@ -1,10 +1,9 @@
 import * as ClassNames from '../ClassNames/ClassNames.js'
 import * as GetKeyBindingsTableBodyRowClassName from '../GetKeyBindingsTableBodyRowClassName/GetKeyBindingsTableBodyRowClassName.js'
+import * as GetKeyBindingsTableCellCommandVirtualDom from '../GetKeyBindingsTableCellCommandVirtualDom/GetKeyBindingsTableCellCommandVirtualDom.js'
+import * as GetKeyBindingsTableCellEditVirtualDom from '../GetKeyBindingsTableCellEditVirtualDom/GetKeyBindingsTableCellEditVirtualDom.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
-import * as GetIconVirtualDom from '../GetIconVirtualDom/GetIconVirtualDom.js'
-import * as GetKeyBindingsTableEditCellVirtualDom from '../GetKeyBindingsTableEditCellVirtualDom/GetKeyBindingsTableEditCellVirtualDom.js'
-import * as Icon from '../Icon/Icon.js'
 
 const kbdDom = {
   type: VirtualDomElements.Kbd,
@@ -12,38 +11,9 @@ const kbdDom = {
   childCount: 1,
 }
 
-const highlight = {
-  type: VirtualDomElements.Span,
-  className: ClassNames.SearchHighlight,
-  childCount: 1,
-}
-
 const textCtrl = text('Ctrl')
 const textShift = text('Shift')
 const textPlus = text('+')
-
-const addHighlights = (tableCell, dom, highlights, label) => {
-  dom.push(tableCell)
-  let position = 0
-  for (let i = 0; i < highlights.length; i += 2) {
-    const highlightStart = highlights[i]
-    const highlightEnd = highlights[i + 1]
-    if (position < highlightStart) {
-      const beforeText = label.slice(position, highlightStart)
-      tableCell.childCount++
-      dom.push(text(beforeText))
-    }
-    const highlightText = label.slice(highlightStart, highlightEnd)
-    tableCell.childCount++
-    dom.push(highlight, text(highlightText))
-    position = highlightEnd
-  }
-  if (position < label.length) {
-    const afterText = label.slice(position)
-    tableCell.childCount++
-    dom.push(text(afterText))
-  }
-}
 
 // TODO needing childCount variable everywhere can be error prone
 const getKeyBindingCellChildren = (keyBinding) => {
@@ -65,8 +35,7 @@ const getKeyBindingCellChildren = (keyBinding) => {
 
 export const getKeyBindingsTableBodyRowDom = (keyBinding) => {
   const { children, childCount } = getKeyBindingCellChildren(keyBinding)
-  const { rowIndex, selected, commandMatches, command } = keyBinding
-  const commandHighlights = commandMatches.slice(1)
+  const { rowIndex, selected } = keyBinding
   const isEven = rowIndex % 2 === 0
   const className = GetKeyBindingsTableBodyRowClassName.getRowClassName(isEven, selected)
   const dom = []
@@ -77,13 +46,8 @@ export const getKeyBindingsTableBodyRowDom = (keyBinding) => {
     key: rowIndex,
     childCount: 4,
   })
-  dom.push(...GetKeyBindingsTableEditCellVirtualDom.getKeyBindingsTableEditCellDom())
-  const tableCell = {
-    type: VirtualDomElements.Td,
-    className: ClassNames.KeyBindingsTableCell,
-    childCount: 0,
-  }
-  addHighlights(tableCell, dom, commandHighlights, command)
+  dom.push(...GetKeyBindingsTableCellEditVirtualDom.getKeyBindingsTableEditCellDom())
+  dom.push(...GetKeyBindingsTableCellCommandVirtualDom.getKeyBindingsTableCellCommandDom(keyBinding))
   dom.push(
     {
       type: VirtualDomElements.Td,
