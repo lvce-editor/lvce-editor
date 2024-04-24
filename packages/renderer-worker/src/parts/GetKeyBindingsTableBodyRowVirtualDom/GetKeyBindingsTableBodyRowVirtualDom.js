2f1f1d41c73aa52264975932c002b4d6d777b18d
Le Vivilet <levivlet@pm.me>
2024-04-24 19:36:19 +0200

    simplify dom

--- packages/renderer-worker/src/parts/GetKeyBindingsTableBodyRowVirtualDom/GetKeyBindingsTableBodyRowVirtualDom.js
+++ packages/renderer-worker/src/parts/GetKeyBindingsTableBodyRowVirtualDom/GetKeyBindingsTableBodyRowVirtualDom.js
@@ -2,39 +2,11 @@ import * as ClassNames from '../ClassNames/ClassNames.js'
 import * as GetKeyBindingsTableBodyRowClassName from '../GetKeyBindingsTableBodyRowClassName/GetKeyBindingsTableBodyRowClassName.js'
 import * as GetKeyBindingsTableCellCommandVirtualDom from '../GetKeyBindingsTableCellCommandVirtualDom/GetKeyBindingsTableCellCommandVirtualDom.js'
 import * as GetKeyBindingsTableCellEditVirtualDom from '../GetKeyBindingsTableCellEditVirtualDom/GetKeyBindingsTableCellEditVirtualDom.js'
+import * as GetKeyBindingsTableCellKeyVirtualDom from '../GetKeyBindingsTableCellKeyVirtualDom/GetKeyBindingsTableCellKeyVirtualDom.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
 
-const kbdDom = {
-  type: VirtualDomElements.Kbd,
-  className: ClassNames.Key,
-  childCount: 1,
-}
-
-const textCtrl = text('Ctrl')
-const textShift = text('Shift')
-const textPlus = text('+')
-
-// TODO needing childCount variable everywhere can be error prone
-const getKeyBindingCellChildren = (keyBinding) => {
-  const { isCtrl, isShift, key, keyMatches, commandMatches } = keyBinding
-  const children = []
-  let childCount = 0
-  if (isCtrl) {
-    childCount += 2
-    children.push(kbdDom, textCtrl, textPlus)
-  }
-  if (isShift) {
-    childCount += 2
-    children.push(kbdDom, textShift, textPlus)
-  }
-  childCount++
-  children.push(kbdDom, text(key))
-  return { children, childCount }
-}
-
 export const getKeyBindingsTableBodyRowDom = (keyBinding) => {
-  const { children, childCount } = getKeyBindingCellChildren(keyBinding)
   const { rowIndex, selected } = keyBinding
   const isEven = rowIndex % 2 === 0
   const className = GetKeyBindingsTableBodyRowClassName.getRowClassName(isEven, selected)
@@ -48,13 +20,8 @@ export const getKeyBindingsTableBodyRowDom = (keyBinding) => {
   })
   dom.push(...GetKeyBindingsTableCellEditVirtualDom.getKeyBindingsTableEditCellDom())
   dom.push(...GetKeyBindingsTableCellCommandVirtualDom.getKeyBindingsTableCellCommandDom(keyBinding))
+  dom.push(...GetKeyBindingsTableCellKeyVirtualDom.getKeyBindingsTableCellKeyDom(keyBinding))
   dom.push(
-    {
-      type: VirtualDomElements.Td,
-      className: ClassNames.KeyBindingsTableCell,
-      childCount,
-    },
-    ...children,
     {
       type: VirtualDomElements.Td,
       className: ClassNames.KeyBindingsTableCell,
