ad925787cddbbab9329a15a99ec9e4e2d4a00e37
Le Vivilet <levivlet@pm.me>
2024-04-24 19:39:26 +0200

    add todo

--- packages/renderer-worker/src/parts/GetKeyBindingsTableBodyRowVirtualDom/GetKeyBindingsTableBodyRowVirtualDom.js
+++ packages/renderer-worker/src/parts/GetKeyBindingsTableBodyRowVirtualDom/GetKeyBindingsTableBodyRowVirtualDom.js
@@ -7,7 +7,7 @@ import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js
 
 export const getKeyBindingsTableBodyRowDom = (keyBinding) => {
   const { rowIndex, selected } = keyBinding
-  const isEven = rowIndex % 2 === 0
+  const isEven = rowIndex % 2 === 0 // TODO compute iseven in getvisible
   const className = GetKeyBindingsTableBodyRowClassName.getRowClassName(isEven, selected)
   const dom = [
     {
