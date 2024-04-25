efdcce568802e7cf71c1adc8bee6fcb703cdec91
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-25 23:18:25 +0200

    feature: send ipcId to process explorer (#5826)

--- packages/process-explorer/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js
+++ packages/process-explorer/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js
@@ -4,9 +4,11 @@ import * as IpcChild from '../IpcChild/IpcChild.js'
 import * as IpcChildType from '../IpcChildType/IpcChildType.js'
 import * as IpcId from '../IpcId/IpcId.js'
 import * as ParentIpc from '../ParentIpc/ParentIpc.js'
+import * as ProcessExplorerFrontendIpc from '../ProcessExplorerFrontendIpc/ProcessExplorerFrontendIpc.js'
 
 export const handleElectronMessagePort = async (messagePort, ipcId) => {
   Assert.object(messagePort)
+  // Assert.number(ipcId)
   const ipc = await IpcChild.listen({
     method: IpcChildType.ElectronMessagePort,
     messagePort,
@@ -14,5 +16,9 @@ export const handleElectronMessagePort = async (messagePort, ipcId) => {
   HandleIpc.handleIpc(ipc)
   if (ipcId === IpcId.MainProcess) {
     ParentIpc.state.ipc = ipc
+  } else if (ipcId === IpcId.ProcessExplorerRenderer) {
+    // TODO how to connect message ports to the correct
+    // renderer process?
+    ProcessExplorerFrontendIpc.state.ipc = ipc
   }
 }
