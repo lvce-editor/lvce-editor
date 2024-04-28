70bbe37eb8db506c76d386c6325fdc3bab4e95e9
Le Vivilet <levivlet@pm.me>
2024-04-28 13:04:33 +0200

    feature: use search process by default

--- packages/renderer-worker/src/parts/SharedProcessCommandType/SharedProcessCommandType.js
+++ packages/renderer-worker/src/parts/SharedProcessCommandType/SharedProcessCommandType.js
@@ -1,4 +1,4 @@
-export const DownloadDownload = 'Download.download'
+export const DownloadDownload = 'Download.download' // TODO move to network process
 
 export const ExtensionHostGetIconThemeJson = 'ExtensionHost.getIconThemeJson'
 
@@ -22,8 +22,8 @@ export const PreferencesGetAll = 'Preferences.getAll'
 
 export const RecentlyOpenedAddPath = 'RecentlyOpened.addPath'
 
-export const SearchFileSearchFile = 'SearchFile.searchFile'
+export const SearchFileSearchFile = 'SearchFile.searchFile' // TODO move to search process
 
-export const TextSearchSearch = 'TextSearch.search'
+export const TextSearchSearch = 'TextSearch.search' // TODO move to search process
 
 export const WorkspaceResolveRoot = 'Workspace.resolveRoot'
