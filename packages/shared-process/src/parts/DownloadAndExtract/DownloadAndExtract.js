304dddda6fffab12dd45dd97dd2df1d79aaf99a6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-28 17:17:55 +0200

    Discard changes to packages/shared-process/src/parts/DownloadAndExtract/DownloadAndExtract.js

--- packages/shared-process/src/parts/DownloadAndExtract/DownloadAndExtract.js
+++ packages/shared-process/src/parts/DownloadAndExtract/DownloadAndExtract.js
@@ -1,5 +1,22 @@
-import * as NetworkProcess from '../NetworkProcess/NetworkProcess.js'
+import { pipeline } from 'node:stream/promises'
+import { createGunzip } from 'node:zlib'
+import got, { RequestError } from 'got'
+import tar from 'tar-fs'
+import { VError } from '../VError/VError.js'
 
 export const downloadAndExtractTarGz = async ({ url, outDir, strip }) => {
-  return NetworkProcess.invoke('Download.downloadAndExtractTarGz', { url, outDir, strip })
+  try {
+    await pipeline(
+      got.stream(url),
+      createGunzip(),
+      tar.extract(outDir, {
+        strip,
+      }),
+    )
+  } catch (error) {
+    if (error && error instanceof RequestError) {
+      throw new VError(`Failed to download ${url}: ${error.message}`)
+    }
+    throw error
+  }
 }
