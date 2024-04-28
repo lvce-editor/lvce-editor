b3ba1a8032e26b62caebf6b88df122ec13b4448d
Le Vivilet <levivlet@pm.me>
2024-04-28 17:16:09 +0200

    fix install

--- packages/network-process/src/parts/CommandMap/CommandMap.js
+++ packages/network-process/src/parts/CommandMap/CommandMap.js
@@ -1,9 +1,11 @@
 import * as Download from '../Download/Download.js'
 import * as DownloadAndExtract from '../DownloadAndExtract/DownloadAndExtract.js'
 import * as GetJson from '../GetJson/GetJson.js'
+import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.js'
 
 export const commandMap = {
   'Download.downloadAndExtractTarGz': DownloadAndExtract.downloadAndExtractTarGz,
   'Download.download': Download.download,
   'Download.getJson': GetJson.getJson,
+  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
 }
