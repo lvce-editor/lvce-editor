103066835a014daa6f38627390c452e57d5e7c1e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-28 15:02:51 +0200

    feature: listen for messages in network process (#5866)

--- packages/network-process/src/parts/Main/Main.js
+++ packages/network-process/src/parts/Main/Main.js
@@ -1,3 +1,8 @@
-export const main = () => {
-  // TODO
+import * as CommandMap from '../CommandMap/CommandMap.js'
+import * as CommandState from '../CommandState/CommandState.js'
+import * as Listen from '../Listen/Listen.js'
+
+export const main = async () => {
+  CommandState.registerCommands(CommandMap.commandMap)
+  await Listen.listen()
 }
