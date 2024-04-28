8e4280a12c16baa291ade5a15c786c7510c898f3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-28 17:20:24 +0200

    Discard changes to packages/shared-process/src/parts/GetPortTuple/GetPortTuple.js

--- packages/shared-process/src/parts/GetPortTuple/GetPortTuple.js
+++ packages/shared-process/src/parts/GetPortTuple/GetPortTuple.js
@@ -1,12 +1,7 @@
-import { VError } from '@lvce-editor/verror'
 import * as TemporaryMessagePort from '../TemporaryMessagePort/TemporaryMessagePort.js'
 
 // workaround for MessageChannelMain not being available in utility process
 export const getPortTuple = async () => {
-  try {
-    const { port1, port2 } = await TemporaryMessagePort.getPortTuple()
-    return { port1, port2 }
-  } catch (error) {
-    throw new VError(error, `Failed to get port tuple`)
-  }
+  const { port1, port2 } = await TemporaryMessagePort.getPortTuple()
+  return { port1, port2 }
 }
