7b2044ca8096827675482985f3c3d8c6dfec74f1
Le Vivilet <levivlet@pm.me>
2024-04-28 13:21:33 +0200

    feature: add launchNetworkProcess function

---
+++ packages/shared-process/src/parts/NetworkProcessPath/NetworkProcessPath.js
@@ -0,0 +1,4 @@
+import * as Path from '../Path/Path.js'
+import * as Root from '../Root/Root.js'
+
+export const networkProcessPath = Path.join(Root.root, 'packages', 'network-process', 'src', 'networkProcessMain.js')
