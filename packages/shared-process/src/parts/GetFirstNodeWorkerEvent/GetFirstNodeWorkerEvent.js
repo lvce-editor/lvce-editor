b2471760c77cb6e41ce64332e58f17204909c85c
Le Vivilet <levivlet@pm.me>
2024-04-28 22:21:22 +0200

    feature: remove unused shared process code

--- packages/shared-process/src/parts/GetFirstNodeWorkerEvent/GetFirstNodeWorkerEvent.js
+++
@@ -1,9 +0,0 @@
-import * as FirstNodeWorkerEventType from '../FirstNodeWorkerEventType/FirstNodeWorkerEventType.js'
-import * as GetFirstEvent from '../GetFirstEvent/GetFirstEvent.js'
-
-export const getFirstNodeWorkerEvent = (worker) => {
-  return GetFirstEvent.getFirstEvent(worker, {
-    exit: FirstNodeWorkerEventType.Exit,
-    error: FirstNodeWorkerEventType.Error,
-  })
-}
