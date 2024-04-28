7473b40a42954fb3945566b80e4fc4242b9976d9
Le Vivilet <levivlet@pm.me>
2024-04-28 12:12:20 +0200

    feature: dispose embeds process when all webcontentViews are disposes

---
+++ packages/shared-process/src/parts/EmbedsProcessState/EmbedsProcessState.js
@@ -0,0 +1,11 @@
+const state = {
+  ref: 0,
+}
+
+export const increment = () => {
+  state.ref++
+}
+
+export const decrement = () => {
+  state.ref--
+}
--- packages/shared-process/src/parts/HandleMessagePortForEmbedsProcess/HandleMessagePortForEmbedsProcess.js
+++ packages/shared-process/src/parts/HandleMessagePortForEmbedsProcess/HandleMessagePortForEmbedsProcess.js
@@ -1,5 +1,6 @@
 import * as HandleIncomingIpc from '../HandleIncomingIpc/HandleIncomingIpc.js'
 import * as IpcId from '../IpcId/IpcId.js'
+import * as EmbedProcessState from '../EmbedProcessState/EmbedProcessState.js'
 
 export const handleMessagePortForEmbedsProcess = (port, ipcId) => {
   return HandleIncomingIpc.handleIncomingIpc(IpcId.EmbedsProcess, port, {
