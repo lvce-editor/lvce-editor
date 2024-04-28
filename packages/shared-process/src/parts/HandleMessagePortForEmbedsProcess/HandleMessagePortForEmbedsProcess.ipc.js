6d904e643b5db039b47f46ec7b6db380d55a0a42
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-28 13:28:00 +0200

    feature: handle embeds process ipc closed function (#5857)

--- packages/shared-process/src/parts/HandleMessagePortForEmbedsProcess/HandleMessagePortForEmbedsProcess.ipc.js
+++ packages/shared-process/src/parts/HandleMessagePortForEmbedsProcess/HandleMessagePortForEmbedsProcess.ipc.js
@@ -4,4 +4,5 @@ export const name = 'HandleMessagePortForEmbedsProcess'
 
 export const Commands = {
   handleMessagePortForEmbedsProcess: HandleMessagePortForEmbedsProcess.handleMessagePortForEmbedsProcess,
+  handleEmbedsProcessIpcClosed: HandleMessagePortForEmbedsProcess.handleEmbedsProcessIpcClosed,
 }
