513338bdd5f9c5275430b16a1c6b8770fa34ad5d
Le Vivilet <levivlet@pm.me>
2024-04-28 13:32:12 +0200

    add network process module

---
+++ packages/shared-process/src/parts/NetworkProcess/NetworkProcess.js
@@ -0,0 +1,21 @@
+import * as LaunchNetworkProcess from '../LaunchNetworkProcess/LaunchNetworkProcess.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+
+export const state = {
+  /**
+   * @type {any}
+   */
+  ipc: undefined,
+}
+
+export const getOrCreate = async () => {
+  if (!state.ipc) {
+    state.ipc = LaunchNetworkProcess.launchNetworkProcess()
+  }
+  return state.ipc
+}
+
+export const invoke = async (method, ...params) => {
+  const ipc = await getOrCreate()
+  return JsonRpc.invoke(ipc, method, ...params)
+}
