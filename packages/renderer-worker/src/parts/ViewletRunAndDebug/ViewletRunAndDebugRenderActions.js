963d42f5941d900231de3760756c5e0f683750ef
Le Vivilet <levivlet@pm.me>
2024-04-30 18:23:44 +0200

    fix: send debug events to debug view

--- packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugRenderActions.js
+++ packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugRenderActions.js
@@ -2,7 +2,7 @@ import * as GetRunAndDebugButtonsVirtualDom from '../GetRunAndDebugButtonsVirtua
 
 export const renderActions = {
   isEqual(oldState, newState) {
-    return false // TODO
+    return false
   },
   apply(oldState, newState) {
     const dom = GetRunAndDebugButtonsVirtualDom.getRunAndDebugButtonsVirtualDom(newState.debugState)
