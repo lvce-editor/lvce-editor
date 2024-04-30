38f5c9874b08fe35b1fa8d9ddaf221549503abc0
Le Vivilet <levivlet@pm.me>
2024-04-30 19:29:41 +0200

    feature: add functions to get debug rows, simplifying virtual dom creation

---
+++ packages/renderer-worker/src/parts/GetRunAndDebugVisibleRows/GetRunAndDebugVisibleRows.js
@@ -0,0 +1,166 @@
+import * as DebugScopeChainType from '../DebugScopeChainType/DebugScopeChainType.js'
+import * as DebugStrings from '../DebugStrings/DebugStrings.js'
+import * as GetChevronVirtualDom from '../GetChevronVirtualDom/GetChevronVirtualDom.js'
+import * as GetScopeExceptionVirtualDom from '../GetScopeExceptionVirtualDom/GetScopeExceptionVirtualDom.js'
+import * as GetScopePropertyVirtualDom from '../GetScopePropertyVirtualDom/GetScopePropertyVirtualDom.js'
+import * as GetScopeScopeVirtualDom from '../GetScopeScopeVirtualDom/GetScopeScopeVirtualDom.js'
+import * as GetScopeThisVirtualDom from '../GetScopeThisVirtualDom/GetScopeThisVirtualDom.js'
+import * as GetVisibleScopeItems from '../GetVisibleScopeItems/GetVisibleScopeItems.js'
+import * as DebugItemFlags from '../DebugItemFlags/DebugItemFlags.js'
+
+const getRunAndDebugVisibleRowsWatch = (state) => {
+  const { watchExpanded } = state
+  return [
+    {
+      type: 'section-heading',
+      text: DebugStrings.watch(),
+      expanded: watchExpanded,
+      key: '',
+      value: '',
+      indent: 0,
+      valueType: '',
+    },
+  ]
+}
+
+const getRunAndDebugVisibleRowsBreakPoints = (state) => {
+  const { breakPointsExpanded } = state
+  return [
+    {
+      type: 'section-heading',
+      text: DebugStrings.breakPoints(),
+      expanded: breakPointsExpanded,
+      key: '',
+      value: '',
+      indent: 0,
+      valueType: '',
+    },
+  ]
+}
+
+const getScopeThisRows = (scope) => {
+  const { indent, key, value, valueType } = scope
+  return [
+    {
+      type: 'value',
+      text: '',
+      expanded: false,
+      key,
+      value,
+      indent,
+      valueType,
+    },
+  ]
+}
+
+const getScopeExceptionRows = (scope) => {
+  const { key, value } = scope
+  return [
+    {
+      type: 'exception',
+      text: '',
+      expanded: false,
+      key,
+      value,
+      indent: 0,
+      valueType: '',
+    },
+  ]
+}
+
+const getScopeScopeRows = (scope) => {
+  const { key, flags } = scope
+  return [
+    {
+      type: 'scope',
+      text: '',
+      expanded: flags & DebugItemFlags.Expanded,
+      key,
+      value: '',
+      indent: 0,
+      valueType: '',
+    },
+  ]
+}
+
+const getScopePropertyRows = (scope) => {
+  const { indent, key, value, valueType, flags } = scope
+  return {
+    type: 'property',
+    text: '',
+    expanded: flags & DebugItemFlags.Expanded,
+    key,
+    value,
+    indent,
+    valueType,
+  }
+}
+
+const getNoopRows = () => {
+  return []
+}
+
+const getScopeRenderer = (type) => {
+  switch (type) {
+    case DebugScopeChainType.This:
+      return getScopeThisRows
+    case DebugScopeChainType.Exception:
+      return getScopeExceptionRows
+    case DebugScopeChainType.Scope:
+      return getScopeScopeRows
+    case DebugScopeChainType.Property:
+      return getScopePropertyRows
+    default:
+      return getNoopRows
+  }
+}
+
+const getRunAndDebugVisibleRowsScope = (state) => {
+  const rows = []
+  const { scopeChain, scopeExpanded, expandedIds, scopeFocusedIndex } = state
+  if (scopeExpanded) {
+    rows.push({
+      type: 'section-heading',
+      text: DebugStrings.scope(),
+      expanded: true,
+      key: '',
+      value: '',
+      indent: 0,
+      valueType: '',
+    })
+    if (scopeChain.length === 0) {
+      rows.push({
+        type: 'message',
+        text: DebugStrings.notPaused(),
+        expanded: false,
+        key: '',
+        value: '',
+        indent: 0,
+        valueType: '',
+      })
+    } else {
+      const visible = GetVisibleScopeItems.getVisibleScopeItems(scopeChain, expandedIds, scopeFocusedIndex)
+      for (const scope of visible) {
+        const renderer = getScopeRenderer(scope.type)
+        rows.push(...renderer(scope))
+      }
+    }
+  } else {
+    rows.push({
+      type: 'section-heading',
+      text: DebugStrings.scope(),
+      expanded: false,
+      key: '',
+      value: '',
+      indent: 0,
+      valueType: '',
+    })
+  }
+  return rows
+}
+
+export const getRunAndDebugVisibleRows = (state) => {
+  const rows = [...getRunAndDebugVisibleRowsWatch(state), ...getRunAndDebugVisibleRowsBreakPoints(state), ...getRunAndDebugVisibleRowsScope(state)]
+
+  return rows
+}
