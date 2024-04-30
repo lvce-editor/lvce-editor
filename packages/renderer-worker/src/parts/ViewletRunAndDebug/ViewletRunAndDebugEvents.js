3defc9765fdb44d976c3b1126a9b160618fd6d4b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-30 18:54:56 +0200

    fix: send debug events to debug view (#5891)

---
+++ packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugEvents.js
@@ -0,0 +1,7 @@
+import * as ViewletRunAndDebug from './ViewletRunAndDebug.js'
+
+export const Events = {
+  'Debug.scriptParsed': ViewletRunAndDebug.handleScriptParsed,
+  'Debug.paused': ViewletRunAndDebug.handlePaused,
+  'Debug.resumed': ViewletRunAndDebug.handleResumed,
+}
