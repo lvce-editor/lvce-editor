db94b6492276c8727aa26ddfb6e9010c3036f957
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-01 23:18:40 +0200

    feature: use search process by default (#5860)

--- packages/search-process/src/parts/HandleIpc/HandleIpc.js
+++ packages/search-process/src/parts/HandleIpc/HandleIpc.js
@@ -1,5 +1,10 @@
 import * as HandleMessage from '../HandleMessage/HandleMessage.js'
 
 export const handleIpc = (ipc) => {
-  ipc.addEventListener('message', HandleMessage.handleMessage)
+  if ('addEventListener' in ipc) {
+    ipc.addEventListener('message', HandleMessage.handleMessage)
+  } else if ('on' in ipc) {
+    // deprecated
+    ipc.on('message', HandleMessage.handleMessage)
+  }
 }
