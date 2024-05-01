2e66c3ab75dce6819fe646896417cc9a4bf3e01f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-01 13:44:12 +0200

    feature: use destroy socket helper function (#5902)

--- packages/shared-process/src/parts/HandleWebSocketForUnknown/HandleWebSocketForUnknown.js
+++ packages/shared-process/src/parts/HandleWebSocketForUnknown/HandleWebSocketForUnknown.js
@@ -1,10 +1,7 @@
 import * as Logger from '../Logger/Logger.js'
+import * as DestroySocket from '../DestroySocket/DestroySocket.js'
 
 export const handleWebSocket = (message, handle, protocol) => {
-  Logger.warn(`[shared-process] unsupported sec-websocket-procotol ${protocol}`)
-  try {
-    handle.destroy()
-  } catch {
-    // ignore
-  }
+  Logger.warn(`[shared-process] unsupported sec-websocket-protocol ${protocol}`)
+  DestroySocket.destroySocket(handle)
 }
