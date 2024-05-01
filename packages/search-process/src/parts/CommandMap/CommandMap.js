2604fec3ede36052511a677f9c19069fb872b595
Le Vivilet <levivlet@pm.me>
2024-05-01 21:56:53 +0200

    add missing command

--- packages/search-process/src/parts/CommandMap/CommandMap.js
+++ packages/search-process/src/parts/CommandMap/CommandMap.js
@@ -1,9 +1,11 @@
 import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.js'
+import * as HandleWebSocket from '../HandleWebSocket/HandleWebSocket.js'
 import * as SearchFile from '../SearchFile/SearchFile.js'
 import * as TextSearch from '../TextSearch/TextSearch.js'
 
 export const commandMap = {
   'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
   'SearchFile.searchFile': SearchFile.searchFile,
+  'HandleWebSocket.handleWebSocket': HandleWebSocket.handleWebSocket,
   'TextSearch.search': TextSearch.search,
 }
