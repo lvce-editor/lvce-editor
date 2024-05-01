46cb4e95af08fba827f34c86dcca0a40e1437b4c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-01 16:00:22 +0200

    feature: add function to send websocket to search process (#5910)

--- packages/shared-process/src/parts/ProtocolType/ProtocolType.js
+++ packages/shared-process/src/parts/ProtocolType/ProtocolType.js
@@ -1,4 +1,5 @@
 export const ExtensionHost = 'extension-host'
 export const ExtensionHostHelperProcess = 'extension-host-helper-process'
+export const SearchProcess = 'search-process'
 export const SharedProcess = 'shared-process'
 export const TerminalProcess = 'terminal-process'
