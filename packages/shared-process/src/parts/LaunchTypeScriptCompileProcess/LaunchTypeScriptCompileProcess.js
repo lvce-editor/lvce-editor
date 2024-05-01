ffb63ff61a7073fea1e4562eacd4f7e4be4e5246
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-01 15:56:56 +0200

    feature: add function to launch typescript compile process (#5909)

---
+++ packages/shared-process/src/parts/LaunchTypeScriptCompileProcess/LaunchTypeScriptCompileProcess.js
@@ -0,0 +1,18 @@
+import * as TypeScriptCompileProcessPath from '../TypeScriptCompileProcessPath/TypeScriptCompileProcessPath.js'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcParent from '../IpcParent/IpcParent.js'
+import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as IsElectron from '../IsElectron/IsElectron.js'
+
+export const launchTypeScriptCompileProcess = async () => {
+  const method = IsElectron.isElectron() ? IpcParentType.ElectronUtilityProcess : IpcParentType.NodeForkedProcess
+  const typescriptCompileProcess = await IpcParent.create({
+    method,
+    path: TypeScriptCompileProcessPath.typescriptCompileProcessPath,
+    argv: [],
+    stdio: 'inherit',
+    name: 'TypeScript Compile Process',
+  })
+  HandleIpc.handleIpc(typescriptCompileProcess)
+  return typescriptCompileProcess
+}
---
+++ packages/shared-process/src/parts/TypeScriptCompileProcessPath/TypeScriptCompileProcessPath.js
@@ -0,0 +1,4 @@
+import * as Path from '../Path/Path.js'
+import * as Root from '../Root/Root.js'
+
+export const typescriptCompileProcessPath = Path.join(Root.root, 'packages', 'typescript-compile-process', 'src', 'typescriptCompileProcessMain.js')
