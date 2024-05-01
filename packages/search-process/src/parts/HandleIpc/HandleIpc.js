098084001c7f9b043ee4ef1df04a91ff0a290f4f
Le Vivilet <levivlet@pm.me>
2024-05-01 21:54:51 +0200

    fix error

--- packages/search-process/src/parts/HandleIpc/HandleIpc.js
+++ packages/search-process/src/parts/HandleIpc/HandleIpc.js
@@ -1,5 +1,11 @@
 import * as HandleMessage from '../HandleMessage/HandleMessage.js'
 
 export const handleIpc = (ipc) => {
-  ipc.addEventListener('message', HandleMessage.handleMessage)
+  console.log({ ipc })
+  if ('addEventListener' in ipc) {
+    ipc.addEventListener('message', HandleMessage.handleMessage)
+  } else if ('on' in ipc) {
+    // deprecated
+    ipc.on('message', HandleMessage.handleMessage)
+  }
 }
