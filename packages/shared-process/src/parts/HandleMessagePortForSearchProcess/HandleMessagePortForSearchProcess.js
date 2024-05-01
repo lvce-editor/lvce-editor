0fa6d6f3bf823910a9ea3a8ed15ccce01bb93754
Le Vivilet <levivlet@pm.me>
2024-05-01 15:32:43 +0200

    feature: add function to send websocket to search process

--- packages/shared-process/src/parts/HandleMessagePortForSearchProcess/HandleMessagePortForSearchProcess.js
+++ packages/shared-process/src/parts/HandleMessagePortForSearchProcess/HandleMessagePortForSearchProcess.js
@@ -2,6 +2,7 @@ import * as Assert from '../Assert/Assert.js'
 import * as SearchProcess from '../SearchProcess/SearchProcess.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 
+// TODO use handleIncomingIpc function
 export const handleMessagePortForSearchProcess = async (port) => {
   Assert.object(port)
   const ipc = await SearchProcess.getOrCreate()
