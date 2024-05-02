cb98ba5c4a23526e25d78f1647c00f45d82757a8
Le Vivilet <levivlet@pm.me>
2024-05-03 00:09:48 +0200

    feature: don't export command state variable

--- packages/diff-worker/src/parts/CommandState/CommandState.ts
+++ packages/diff-worker/src/parts/CommandState/CommandState.ts
@@ -1,17 +1,15 @@
-export const state = {
-  commands: Object.create(null),
+const commands = Object.create(null)
+
+export const registerCommand = (key: string, fn: any) => {
+  commands[key] = fn
 }
 
-export const registerCommand = (key, fn) => {
-  state.commands[key] = fn
-}
-
-export const registerCommands = (commandMap) => {
+export const registerCommands = (commandMap: any) => {
   for (const [key, value] of Object.entries(commandMap)) {
     registerCommand(key, value)
   }
 }
 
-export const getCommand = (key) => {
-  return state.commands[key]
+export const getCommand = (key: string) => {
+  return commands[key]
 }
