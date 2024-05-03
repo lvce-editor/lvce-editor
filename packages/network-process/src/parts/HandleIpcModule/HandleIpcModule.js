b581485f013ed00fd29e0ad5d23cbb38d6374397
Le Vivilet <levivlet@pm.me>
2024-05-03 22:06:02 +0200

    tsc

--- packages/network-process/src/parts/HandleIpcModule/HandleIpcModule.js
+++ packages/network-process/src/parts/HandleIpcModule/HandleIpcModule.js
@@ -7,16 +7,6 @@ export const getModule = (ipcId) => {
   switch (ipcId) {
     case IpcId.SharedProcess:
       return HandleIpcSharedProcess
-    case IpcId.EmbedsProcess:
-      return HandleIpcEmbedsProcess
-    case IpcId.TerminalProcess:
-      return HandleIpcTerminalProcess
-    case IpcId.ExtensionHostHelperProcess:
-      return HandleIpcExtensionHostHelperProcess
-    case IpcId.ProcessExplorer:
-      return HandleIpcProcessExplorer
-    case IpcId.SearchProcess:
-      return HandleIpcSearchProcess
     default:
       throw new Error(`unexpected incoming ipc`)
   }
