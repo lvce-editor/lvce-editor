5b3e0b1517bc3102af99835e4af94cd8d12e549d
Le Vivilet <levivlet@pm.me>
2024-05-03 17:56:02 +0200

    feature: move trash function to network process to reduce shared process size

--- packages/network-process/src/parts/CommandMap/CommandMap.js
+++ packages/network-process/src/parts/CommandMap/CommandMap.js
@@ -4,14 +4,16 @@ import * as DownloadAndExtract from '../DownloadAndExtract/DownloadAndExtract.js
 import * as Extract from '../Extract/Extract.js'
 import * as GetUrl from '../GetUrl/GetUrl.js'
 import * as SymLink from '../SymLink/SymLink.js'
+import * as TrashNode from '../TrashNode/TrashNode.js'
 
 export const commandMap = {
+  'ClipBoard.readFiles': ClipBoard.readFiles,
+  'ClipBoard.writeFiles': ClipBoard.writeFiles,
   'Download.download': Download.download,
   'Download.downloadAndExtractTarGz': DownloadAndExtract.downloadAndExtractTarGz,
   'Download.getUrl': GetUrl.getUrl,
   'Extract.extractTarBr': Extract.extractTarBr,
   'Extract.extractTarGz': Extract.extractTarGz,
   'Symlink.createSymLink': SymLink.createSymLink,
-  'ClipBoard.readFiles': ClipBoard.readFiles,
-  'ClipBoard.writeFiles': ClipBoard.writeFiles,
+  'TrashNode.trash': TrashNode.trash,
 }
