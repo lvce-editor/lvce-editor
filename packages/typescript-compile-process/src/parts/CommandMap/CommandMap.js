112142a7749ec2719d55dd1b70806a7112f4441c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-03 22:48:26 +0200

    feature: ask transpile typescript process to transpile typescript (#5958)

--- packages/typescript-compile-process/src/parts/CommandMap/CommandMap.js
+++ packages/typescript-compile-process/src/parts/CommandMap/CommandMap.js
@@ -1,5 +1,7 @@
+import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.js'
 import * as TranspileTypeScript from '../TranspileTypeScript/TranspileTypeScript.js'
 
 export const commandMap = {
   'TranspileTypeScript.transpileTypeScript': TranspileTypeScript.transpileTypeScript,
+  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
 }
