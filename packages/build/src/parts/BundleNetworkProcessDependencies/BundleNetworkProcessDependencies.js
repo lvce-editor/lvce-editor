570bbc7366d07b2f30b2cf8a446025ff90f84ba5
Le Vivilet <levivlet@pm.me>
2024-05-03 22:43:19 +0200

    feature: remove unused barefs prebuild files

--- packages/build/src/parts/BundleNetworkProcessDependencies/BundleNetworkProcessDependencies.js
+++ packages/build/src/parts/BundleNetworkProcessDependencies/BundleNetworkProcessDependencies.js
@@ -3,8 +3,45 @@ import * as FilterNetworkProcessDependencies from '../FilterNetworkProcessDepend
 import * as JsonFile from '../JsonFile/JsonFile.js'
 import * as NpmDependencies from '../NpmDependencies/NpmDependencies.js'
 import * as Path from '../Path/Path.js'
+import * as ReadDir from '../ReadDir/ReadDir.js'
 import * as Remove from '../Remove/Remove.js'
 import * as RemoveSourceMaps from '../RemoveSourceMaps/RemoveSourceMaps.js'
+
+const getPrebuildsToKeep = (platform, arch) => {
+  if (platform === 'linux' && arch === 'x64') {
+    return 'linux-x64'
+  }
+  if (platform === 'linux' && arch === 'arm') {
+    return 'linux-arm64'
+  }
+  if (platform === 'darwin' && arch === 'x64') {
+    return 'darwin-x64'
+  }
+  if (platform === 'darwin' && arch === 'arm') {
+    return 'darwin-arm64'
+  }
+  if (platform === 'win32') {
+    return 'win32-x64'
+  }
+  return '*'
+}
+
+const removeBarePrebuilds = async (to, platform, arch) => {
+  const toKeep = getPrebuildsToKeep(platform, arch)
+  if (toKeep === '*') {
+    return
+  }
+  const modules = ['bare-fs', 'bare-os']
+  for (const module of modules) {
+    const dirents = await ReadDir.readDir(`${to}/node_modules/${module}/prebuilds`)
+    if (!dirents.includes(toKeep)) {
+      throw new Error('missing files to keep')
+    }
+    for (const dirent of dirents) {
+      await Remove.remove(`${to}/node_modules/${module}/prebuilds/${dirent}`)
+    }
+  }
+}
 
 export const bundleNetworkProcessDependencies = async ({ to, exclude = [], arch, platform }) => {
   const projectPath = Path.absolute('packages/network-process')
@@ -51,11 +88,7 @@ export const bundleNetworkProcessDependencies = async ({ to, exclude = [], arch,
   await RemoveSourceMaps.removeSourceMaps(`${to}/node_modules/cacheable-request`)
   await RemoveSourceMaps.removeSourceMaps(`${to}/node_modules/symlink-dir`)
   await Remove.removeMatching(`${to}/node_modules`, '**/*.d.ts')
-  if (arch === 'x64') {
-    await Remove.remove(`${to}/node_modules/bare-fs/prebuilds/linux-arm64`)
-    await Remove.remove(`${to}/node_modules/bare-os/prebuilds/linux-arm64`)
-  } else {
-    await Remove.remove(`${to}/node_modules/bare-fs/prebuilds/linux-x64`)
-    await Remove.remove(`${to}/node_modules/bare-os/prebuilds/linux-x64`)
-  }
+  await Remove.remove(`${to}/node_modules/bare-os/binding.c`)
+  await Remove.remove(`${to}/node_modules/bare-os/CMakeLists.txt`)
+  await removeBarePrebuilds(to, platform, arch)
 }
