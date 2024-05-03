d2c4a0386c9dcff4ee8bcf222e389c00ef263d91
Le Vivilet <levivlet@pm.me>
2024-05-03 19:35:56 +0200

    feature: ask transpile typescript process to transpile typescript

--- packages/shared-process/src/parts/TranspileTypeScript/TranspileTypeScript.js
+++ packages/shared-process/src/parts/TranspileTypeScript/TranspileTypeScript.js
@@ -1,20 +1,5 @@
-import { VError } from '@lvce-editor/verror'
-import * as Assert from '../Assert/Assert.js'
-import * as GetTypeScriptPath from '../GetTypeScriptPath/GetTypeScriptPath.js'
-import * as LoadTypeScript from '../LoadTypeScript/LoadTypeScript.js'
+import * as TypeScriptCompileProcess from '../TypeScriptCompileProcess/TypeScriptCompileProcess.js'
 
-export const transpileTypeScript = async (code) => {
-  try {
-    Assert.string(code)
-    const typescriptUri = GetTypeScriptPath.getTypeScriptUri()
-    const typescript = await LoadTypeScript.loadTypeScript(typescriptUri)
-    const newContent = await typescript.transpileModule(code, {
-      compilerOptions: {
-        target: 'esnext',
-      },
-    })
-    return newContent
-  } catch (error) {
-    throw new VError(error, `Failed to transpile typescript`)
-  }
+export const transpileTypeScript = (code) => {
+  return TypeScriptCompileProcess.invoke('TranspileTypeScript.transpileTypeScript', code)
 }
