7a3ee38c75cbe5cfc85cf47883378401dabdeddf
Le Vivilet <levivlet@pm.me>
2024-05-04 23:09:44 +0200

    use functional events for title bar

--- packages/renderer-process/src/parts/ViewletTitleBarButtons/ViewletTitleBarButtonsEvents.ts
+++ packages/renderer-process/src/parts/ViewletTitleBarButtons/ViewletTitleBarButtonsEvents.ts
@@ -1,12 +1,10 @@
-import * as ComponentUid from '../ComponentUid/ComponentUid.ts'
-import * as ViewletTitleBarButtonsFunctions from './ViewletTitleBarButtonsFunctions.ts'
-
 /**
  *
  * @param {MouseEvent} event
  */
 export const handleTitleBarButtonsClick = (event) => {
   const { target } = event
-  const uid = ComponentUid.fromEvent(event)
-  ViewletTitleBarButtonsFunctions.handleClick(uid, target.className)
+  return ['handleClick', target.className]
 }
+
+export const returnValue = true
