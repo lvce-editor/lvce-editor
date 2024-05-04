1bbd8037864d04ebec82211ef80b6703dbbf67d1
Le Vivilet <levivlet@pm.me>
2024-05-04 10:15:40 +0200

    feature: add function to compile typescript cached

---
+++ packages/shared-process/src/parts/TypeScriptCompileCachePath/TypeScriptCompileCachePath.js
@@ -0,0 +1,4 @@
+import * as Path from '../Path/Path.js'
+import * as Root from '../Root/Root.js'
+
+export const typescriptCompileCachePath = Path.join(Root.root, 'packages', 'build', '.tmp', 'typescript-compile-cache')
