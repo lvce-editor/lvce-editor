b3cf4607ad2b2db2f53d9a8b415de9014e70896e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-04 18:44:02 +0200

    feature: add functions to bundle search worker (#6001)

---
+++ packages/build/src/parts/BundleSearchWorker/BundleSearchWorker.js
@@ -0,0 +1,28 @@
+import * as BundleJs from '../BundleJsRollup/BundleJsRollup.js'
+import * as Copy from '../Copy/Copy.js'
+import * as Path from '../Path/Path.js'
+import * as Replace from '../Replace/Replace.js'
+
+export const bundleSearchWorker = async ({ cachePath }) => {
+  await Copy.copy({
+    from: 'packages/search-worker/src',
+    to: Path.join(cachePath, 'src'),
+  })
+  await Copy.copy({
+    from: 'static/js',
+    to: Path.join(cachePath, 'static', 'js'),
+  })
+  for (const file of ['JsonRpc', 'IpcChildModule']) {
+    await Replace.replace({
+      path: `${cachePath}/src/parts/${file}/${file}.ts`,
+      occurrence: `/static/`,
+      replacement: `../../../static/`,
+    })
+  }
+  await BundleJs.bundleJs({
+    cwd: cachePath,
+    from: `./src/searchWorkerMain.ts`,
+    platform: 'webworker',
+    allowCyclicDependencies: false,
+  })
+}
