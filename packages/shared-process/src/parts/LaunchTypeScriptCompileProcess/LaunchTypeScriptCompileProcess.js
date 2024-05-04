abb1c93a80b01cdd14389b06284d7e4d63d80f29
Le Vivilet <levivlet@pm.me>
2024-05-04 09:55:22 +0200

    feature: lazyload typescript compile process dependency

--- packages/shared-process/src/parts/LaunchTypeScriptCompileProcess/LaunchTypeScriptCompileProcess.js
+++ packages/shared-process/src/parts/LaunchTypeScriptCompileProcess/LaunchTypeScriptCompileProcess.js
@@ -2,10 +2,10 @@ import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as IsElectron from '../IsElectron/IsElectron.js'
-import * as TypeScriptCompileProcessPath from '../TypeScriptCompileProcessPath/TypeScriptCompileProcessPath.js'
 
 export const launchTypeScriptCompileProcess = async () => {
   const method = IsElectron.isElectron ? IpcParentType.ElectronUtilityProcess : IpcParentType.NodeForkedProcess
+  const TypeScriptCompileProcessPath = await import('../TypeScriptCompileProcessPath/TypeScriptCompileProcessPath.js')
   const typescriptCompileProcess = await IpcParent.create({
     method,
     path: TypeScriptCompileProcessPath.typescriptCompileProcessPath,
