1b15c97daeef0d4aefe09d6cd057b5ea8fd49c6f
Le Vivilet <levivlet@pm.me>
2024-05-04 18:24:13 +0200

    feature: add function to launch search worker

---
+++ packages/renderer-worker/src/parts/LaunchSearchWorker/LaunchSearchWorker.js
@@ -0,0 +1,17 @@
+import * as IpcParent from '../IpcParent/IpcParent.js'
+import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as SearchWorkerUrl from '../SearchWorkerUrl/SearchWorkerUrl.js'
+import * as Platform from '../Platform/Platform.js'
+import * as PlatformType from '../PlatformType/PlatformType.js'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+
+export const launchSearchWorker = async () => {
+  const name = Platform.platform === PlatformType.Electron ? 'Search Worker (Electron)' : 'Search Worker'
+  const ipc = await IpcParent.create({
+    method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
+    name,
+    url: SearchWorkerUrl.searchWorkerUrl,
+  })
+  HandleIpc.handleIpc(ipc)
+  return ipc
+}
