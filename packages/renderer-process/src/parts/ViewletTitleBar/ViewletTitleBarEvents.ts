97625f42f3b89d17d2a8d4e7277e1e8c801525fd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-05 00:13:48 +0200

    feature: use functional events for title bar (#6011)

--- packages/renderer-process/src/parts/ViewletTitleBar/ViewletTitleBarEvents.ts
+++ packages/renderer-process/src/parts/ViewletTitleBar/ViewletTitleBarEvents.ts
@@ -1,10 +1,9 @@
-import * as ComponentUid from '../ComponentUid/ComponentUid.ts'
 import * as Event from '../Event/Event.ts'
-import * as ForwardCommand from '../ForwardCommand/ForwardCommand.ts'
 
 export const handleContextMenu = (event) => {
   Event.preventDefault(event)
   const { button, clientX, clientY } = event
-  const uid = ComponentUid.fromEvent(event)
-  ForwardCommand.handleContextMenu(uid, button, clientX, clientY)
+  return ['handleContextMenu', button, clientX, clientY]
 }
+
+export const returnValue = true
