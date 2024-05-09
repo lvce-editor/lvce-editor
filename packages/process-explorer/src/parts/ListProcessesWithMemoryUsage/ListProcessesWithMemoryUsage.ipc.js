f6a94a8aa4ae0353f6c4786e9eea37cf2dcb1a3b
Le Vivilet <levivlet@pm.me>
2024-05-09 18:56:37 +0200

    feature: remove unused process explorer code

--- packages/process-explorer/src/parts/ListProcessesWithMemoryUsage/ListProcessesWithMemoryUsage.ipc.js
+++
@@ -1,7 +0,0 @@
-import * as ListProcessesWithMemoryUsage from './ListProcessesWithMemoryUsage.js'
-
-export const name = 'ListProcessesWithMemoryUsage'
-
-export const Commands = {
-  listProcessesWithMemoryUsage: ListProcessesWithMemoryUsage.listProcessesWithMemoryUsage,
-}
--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -35,7 +35,6 @@ cd packages/extension-host-helper-process           && updateDependencies && cd 
 cd packages/extension-host-worker-tests             && updateDependencies && cd ../.. &&
 cd packages/main-process                            && updateDependencies && cd ../.. &&
 cd packages/network-process                         && updateDependencies && cd ../.. &&
-cd packages/process-explorer                        && updateDependencies && cd ../.. &&
 cd packages/pty-host                                && updateDependencies && cd ../.. &&
 cd packages/renderer-process                        && updateDependencies && cd ../.. &&
 cd packages/renderer-worker                         && updateDependencies && cd ../.. &&
