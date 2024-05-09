85e9ac97a5a2181c5644639ccffa357f9fcd1eb4
Le Vivilet <levivlet@pm.me>
2024-05-09 18:47:11 +0200

    feature: use process explorer npm package to reduce size of main repository

--- packages/shared-process/src/parts/ProcessExplorerPath/ProcessExplorerPath.js
+++ packages/shared-process/src/parts/ProcessExplorerPath/ProcessExplorerPath.js
@@ -1,4 +1,1 @@
-import * as Path from '../Path/Path.js'
-import * as Root from '../Root/Root.js'
-
-export const processExplorerPath = Path.join(Root.root, 'packages', 'process-explorer', 'src', 'processExplorerMain.js')
+export const processExplorerPath = '@lvce-editor/process-explorer'
