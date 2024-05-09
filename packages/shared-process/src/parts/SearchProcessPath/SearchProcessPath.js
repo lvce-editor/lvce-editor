c6f02355af4369033231b3064c7bacc738f1551a
Le Vivilet <levivlet@pm.me>
2024-05-09 21:35:12 +0200

    feature: use npm package for search process to reduce main repository size

--- packages/shared-process/src/parts/SearchProcessPath/SearchProcessPath.js
+++ packages/shared-process/src/parts/SearchProcessPath/SearchProcessPath.js
@@ -1,4 +1,13 @@
 import * as Path from '../Path/Path.js'
 import * as Root from '../Root/Root.js'
 
-export const searchProcessPath = Path.join(Root.root, 'packages', 'search-process', 'src', 'searchProcessMain.js')
+export const searchProcessPath = Path.join(
+  Root.root,
+  'packages',
+  'shared-process',
+  'node_modules',
+  '@lvce-editor',
+  'search-process',
+  'src',
+  'searchProcessMain.js',
+)
