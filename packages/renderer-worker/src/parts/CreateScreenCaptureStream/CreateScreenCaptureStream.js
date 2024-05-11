8229f5b7a1253dfefd62a49f92ccd6f2e2d3899d
Le Vivilet <levivlet@pm.me>
2024-05-11 22:11:27 +0200

    feature: simplify screen capture code

---
+++ packages/renderer-worker/src/parts/CreateScreenCaptureStream/CreateScreenCaptureStream.js
@@ -0,0 +1,32 @@
+import * as DesktopCapturer from '../DesktopCapturer/DesktopCapturer.js'
+import * as MediaDevices from '../MediaDevices/MediaDevices.js'
+import * as Platform from '../Platform/Platform.js'
+import * as PlatformType from '../PlatformType/PlatformType.js'
+
+export const createScreenCaptureStream = async (captureId) => {
+  if (Platform.platform === PlatformType.Electron) {
+    const sources = await DesktopCapturer.getSources({
+      types: ['screen'],
+    })
+    const source = sources[0]
+    const sourceId = source.id
+    await MediaDevices.getUserMedia(captureId, {
+      audio: false,
+      video: {
+        mandatory: {
+          chromeMediaSource: 'desktop',
+          chromeMediaSourceId: sourceId,
+          minWidth: 1280,
+          maxWidth: 1280,
+          minHeight: 720,
+          maxHeight: 720,
+        },
+      },
+    })
+    return
+  }
+  await MediaDevices.getUserMedia(captureId, {
+    audio: false,
+    video: true,
+  })
+}
--- packages/renderer-worker/src/parts/ViewletScreenCapture/ViewletScreenCapture.js
+++ packages/renderer-worker/src/parts/ViewletScreenCapture/ViewletScreenCapture.js
@@ -1,8 +1,6 @@
-import * as DesktopCapturer from '../DesktopCapturer/DesktopCapturer.js'
+import * as CreateScreenCaptureStream from '../CreateScreenCaptureStream/CreateScreenCaptureStream.js'
 import * as Id from '../Id/Id.js'
 import * as MediaDevices from '../MediaDevices/MediaDevices.js'
-import * as Platform from '../Platform/Platform.js'
-import * as PlatformType from '../PlatformType/PlatformType.js'
 
 export const create = (id, uri) => {
   return {
@@ -15,31 +13,7 @@ export const create = (id, uri) => {
 
 export const loadContent = async (state) => {
   const captureId = Id.create()
-  if (Platform.platform === PlatformType.Electron) {
-    const sources = await DesktopCapturer.getSources({
-      types: ['screen'],
-    })
-    const source = sources[0]
-    const sourceId = source.id
-    await MediaDevices.getUserMedia(captureId, {
-      audio: false,
-      video: {
-        mandatory: {
-          chromeMediaSource: 'desktop',
-          chromeMediaSourceId: sourceId,
-          minWidth: 1280,
-          maxWidth: 1280,
-          minHeight: 720,
-          maxHeight: 720,
-        },
-      },
-    })
-  } else {
-    await MediaDevices.getUserMedia(captureId, {
-      audio: false,
-      video: true,
-    })
-  }
+  await CreateScreenCaptureStream.createScreenCaptureStream(captureId)
   return {
     ...state,
     captureId,
