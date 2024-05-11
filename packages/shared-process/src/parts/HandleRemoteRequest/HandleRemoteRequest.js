8f65cef0cb507c57aa17ca074206ea96f6d8b92d
Le Vivilet <levivlet@pm.me>
2024-05-11 21:48:53 +0200

    fix error

--- packages/shared-process/src/parts/HandleRemoteRequest/HandleRemoteRequest.js
+++ packages/shared-process/src/parts/HandleRemoteRequest/HandleRemoteRequest.js
@@ -2,9 +2,9 @@ import * as Assert from '../Assert/Assert.js'
 import * as GetElectronFileResponse from '../GetElectronFileResponse/GetElectronFileResponse.js'
 import * as HttpServerResponse from '../HttpServerResponse/HttpServerResponse.js'
 
-export const handleRemoteRequest = async (request, socket) => {
-  Assert.object(request)
+export const handleRemoteRequest = async (socket, request) => {
   Assert.object(socket)
+  Assert.object(request)
   const result = await GetElectronFileResponse.getElectronFileResponse(request.url, request)
   HttpServerResponse.send(request, socket, result)
 }
