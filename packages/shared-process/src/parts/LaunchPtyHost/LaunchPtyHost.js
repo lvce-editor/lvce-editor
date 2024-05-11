47367c354b85f0a1786053d66abffaa74675ecaa
Le Vivilet <levivlet@pm.me>
2024-05-11 22:49:25 +0200

    feature: don't dispose pty host on shared process exit: pty host disposes itself on disconnect

--- packages/shared-process/src/parts/LaunchPtyHost/LaunchPtyHost.js
+++ packages/shared-process/src/parts/LaunchPtyHost/LaunchPtyHost.js
@@ -1,23 +1,8 @@
-import exitHook from 'exit-hook'
-import * as Debug from '../Debug/Debug.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as PtyHostPath from '../PtyHostPath/PtyHostPath.js'
 import * as PtyHostState from '../PtyHostState/PtyHostState.js'
 
-const cleanUpAll = () => {
-  if (PtyHostState.state.ipc) {
-    PtyHostState.state.ipc.dispose()
-    PtyHostState.state.ipc = undefined
-  }
-}
-
-const handleProcessExit = () => {
-  Debug.debug('shared process exit, clean terminals')
-  cleanUpAll()
-}
-
 export const launchPtyHost = async (method) => {
-  exitHook(handleProcessExit)
   const ptyHostPath = await PtyHostPath.ptyHostPath
   const ptyHost = await IpcParent.create({
     method,
