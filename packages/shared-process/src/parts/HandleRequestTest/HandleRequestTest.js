086a75bb212994e7849e74edbdac69147a03caf0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-11 22:26:55 +0200

    feature: update ipc to v9 (#6043)

--- packages/shared-process/src/parts/HandleRequestTest/HandleRequestTest.js
+++ packages/shared-process/src/parts/HandleRequestTest/HandleRequestTest.js
@@ -2,10 +2,10 @@ import * as Assert from '../Assert/Assert.js'
 import * as GetTestRequestResponse from '../GetTestRequestResponse/GetTestRequestResponse.js'
 import * as HttpServerResponse from '../HttpServerResponse/HttpServerResponse.js'
 
-export const handleRequestTest = async (request, indexHtmlPath, socket) => {
+export const handleRequestTest = async (socket, request, indexHtmlPath) => {
+  Assert.object(socket)
   Assert.object(request)
   Assert.string(indexHtmlPath)
-  Assert.object(socket)
   const result = await GetTestRequestResponse.getTestRequestResponse(request, indexHtmlPath)
   HttpServerResponse.send(request, socket, result)
 }
