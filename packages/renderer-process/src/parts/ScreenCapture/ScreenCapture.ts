7d1ba643dece069a018bd7b3f6cbe4b610932ec9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-11 22:17:31 +0200

    feature: move screen capture state into separate file (#6044)

--- packages/renderer-process/src/parts/ScreenCapture/ScreenCapture.ts
+++ packages/renderer-process/src/parts/ScreenCapture/ScreenCapture.ts
@@ -1,13 +1,10 @@
-import { VError } from '../VError/VError.ts'
 import * as Assert from '../Assert/Assert.ts'
-
-export const state = {
-  screenCaptures: Object.create(null),
-}
+import * as ScreenCaptureState from '../ScreenCaptureState/ScreenCaptureState.ts'
+import { VError } from '../VError/VError.ts'
 
 export const get = (id) => {
   Assert.number(id)
-  return state.screenCaptures[id]
+  return ScreenCaptureState.get(id)
 }
 
 export const start = async (id, options) => {
@@ -15,7 +12,7 @@ export const start = async (id, options) => {
     Assert.number(id)
     Assert.object(options)
     const captureStream = await navigator.mediaDevices.getUserMedia(options)
-    state.screenCaptures[id] = captureStream
+    ScreenCaptureState.set(id, captureStream)
   } catch (error) {
     throw new VError(error, `Failed to start screen capture`)
   }
@@ -24,11 +21,11 @@ export const start = async (id, options) => {
 export const dispose = async (id, options) => {
   try {
     Assert.number(id)
-    const captureStream = await state.screenCaptures[id]
+    const captureStream = ScreenCaptureState.get(id)
     for (const track of captureStream.getTracks()) {
       track.stop()
     }
-    delete state.screenCaptures[id]
+    ScreenCaptureState.remove(id)
   } catch (error) {
     throw new VError(error, `Failed to start screen capture`)
   }
