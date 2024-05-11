16a30d5b4a76c2cd7efd4ef442712ddaef5c675c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-11 23:30:05 +0200

    feature: move handleDisconnect function into separate file (#6052)

--- packages/shared-process/src/parts/Main/Main.js
+++ packages/shared-process/src/parts/Main/Main.js
@@ -1,4 +1,5 @@
 import * as Command from '../Command/Command.js'
+import * as HandleDisconnect from '../HandleDisconnect/HandleDisconnect.js'
 import * as HandleUncaughtExceptionMonitor from '../HandleUncaughtExceptionMonitor/HandleUncaughtExceptionMonitor.js'
 import * as Module from '../Module/Module.js'
 import * as ParentIpc from '../ParentIpc/ParentIpc.js'
@@ -7,7 +8,7 @@ import * as Signal from '../Signal/Signal.js'
 
 export const main = async () => {
   Command.setLoad(Module.load)
-  process.on('disconnect', ProcessListeners.handleDisconnect)
+  process.on('disconnect', HandleDisconnect.handleDisconnect)
   process.on(Signal.SIGTERM, ProcessListeners.handleSigTerm)
   process.on('uncaughtException', HandleUncaughtExceptionMonitor.handleUncaughtException)
   process.on('unhandledRejection', HandleUncaughtExceptionMonitor.handleUnhandledRejection)
--- packages/shared-process/src/parts/ProcessListeners/ProcessListeners.js
+++ packages/shared-process/src/parts/ProcessListeners/ProcessListeners.js
@@ -2,11 +2,6 @@ import * as ErrorHandling from '../ErrorHandling/ErrorHandling.js'
 import * as ExitCode from '../ExitCode/ExitCode.js'
 import * as Process from '../Process/Process.js'
 
-export const handleDisconnect = () => {
-  console.info('[shared process] disconnected')
-  Process.exit(ExitCode.Success)
-}
-
 export const handleSigTerm = () => {
   console.info('[shared-process] sigterm')
   Process.exit(ExitCode.Success)
