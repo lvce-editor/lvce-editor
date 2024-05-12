8e787d84e12f6e077ba478ef3a5ad872bd22a89b
Le Vivilet <levivlet@pm.me>
2024-05-12 10:25:10 +0200

    feature: copy tests

--- packages/build/src/parts/TestStaticExport/TestStaticExport.js
+++ packages/build/src/parts/TestStaticExport/TestStaticExport.js
@@ -4,6 +4,7 @@ import * as Copy from '../Copy/Copy.js'
 import * as Mkdir from '../Mkdir/Mkdir.js'
 import * as Path from '../Path/Path.js'
 import * as Remove from '../Remove/Remove.js'
+import * as ReadDir from '../ReadDir/ReadDir.js'
 import * as WriteFile from '../WriteFile/WriteFile.js'
 
 const main = async () => {
@@ -31,17 +32,33 @@ const main = async () => {
     from: `packages/shared-process/node_modules/@lvce-editor/verror`,
     to: `packages/build/.tmp/server/shared-process/node_modules/@lvce-editor/verror`,
   })
+  let commitHash = ''
   try {
-    await module.exportStatic({
+    const result = await module.exportStatic({
       extensionPath,
       testPath,
       root: tmpDir,
       pathPrefix: '/test',
     })
+    commitHash = result.commitHash
   } catch (error) {
     throw new VError(error, `static export failed`)
   }
   await Remove.remove(`packages/build/.tmp/server/shared_process/node_modules`)
+
+  const testFiles = await ReadDir.readDir('packages/extension-host-worker-tests/src')
+  const filteredDirents = testFiles.filter((dirent) => !dirent.startsWith('_'))
+  for (const dirent of filteredDirents) {
+    const name = dirent.slice(0, -3)
+    await Copy.copyFile({
+      from: `packages/build/.tmp/export-test/dist/index.html`,
+      to: `packages/build/.tmp/export-test/dist/tests/${name}.html`,
+    })
+    await Copy.copyFile({
+      from: `packages/extension-host-worker-tests/src/${name}.js`,
+      to: `packages/build/.tmp/export-test/dist/${commitHash}/tests/${name}.js`,
+    })
+  }
 }
 
 main()
