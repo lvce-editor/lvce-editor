b74009ccd493a0b01ee7dbdd99e2756e89b8cdaf
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-18 11:51:30 +0200

    Discard changes to packages/renderer-worker/src/parts/EditorCommand/EditorCommandCursorCharacterRight.js

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandCursorCharacterRight.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandCursorCharacterRight.js
@@ -1,7 +1,9 @@
-import * as EditorWorker from '../EditorWorker/EditorWorker.js'
+import * as EditorCursorHorizontalRight from './EditorCommandCursorHorizontalRight.js'
+import * as EditorDelta from './EditorCommandDelta.js'
+import * as EditorDeltaId from '../EditorDeltaId/EditorDeltaId.js'
 
 export const cursorCharacterRight = (editor) => {
-  return EditorWorker.invoke('Editor.cursorCharacterRight', editor)
+  return EditorCursorHorizontalRight.editorCursorHorizontalRight(editor, EditorDeltaId.CharacterRight)
 }
 
 export const cursorRight = cursorCharacterRight
