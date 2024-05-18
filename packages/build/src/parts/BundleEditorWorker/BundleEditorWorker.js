c836cfc03d4ad0867cee4a0bfa26f73cdb7d9f64
Le Vivilet <levivlet@pm.me>
2024-05-18 10:10:04 +0200

    feature: bundle editor worker

---
+++ packages/build/src/parts/BundleEditorWorker/BundleEditorWorker.js
@@ -0,0 +1,21 @@
+import * as BundleJs from '../BundleJsRollup/BundleJsRollup.js'
+import * as Copy from '../Copy/Copy.js'
+import * as Path from '../Path/Path.js'
+import * as Replace from '../Replace/Replace.js'
+
+export const bundleEditorWorker = async ({ cachePath, commitHash, platform, assetDir }) => {
+  await Copy.copy({
+    from: 'static/js',
+    to: Path.join(cachePath, 'static', 'js'),
+  })
+  await Copy.copy({
+    from: 'packages/editor-worker/src',
+    to: Path.join(cachePath, 'src'),
+  })
+  await BundleJs.bundleJs({
+    cwd: cachePath,
+    from: `./src/editorWorkerMain.ts`,
+    platform: 'webworker',
+    allowCyclicDependencies: false,
+  })
+}
---
+++ packages/build/src/parts/BundleEditorWorkerCached/BundleEditorWorkerCached.js
@@ -0,0 +1,24 @@
+import { existsSync } from 'node:fs'
+import * as CachePaths from '../CachePaths/CachePaths.js'
+import * as Logger from '../Logger/Logger.js'
+import * as Path from '../Path/Path.js'
+import * as Remove from '../Remove/Remove.js'
+
+export const bundleEditorWorkerCached = async ({ commitHash, platform, assetDir }) => {
+  const cachePath = await CachePaths.getEditorWorkerCachePath([platform])
+  if (existsSync(cachePath)) {
+    Logger.info('[build step skipped] bundleEditorWorker')
+  } else {
+    console.time('bundleEditorWorker')
+    await Remove.remove(Path.absolute('packages/build/.tmp/cachedSources/embeds-worker'))
+    const BundleEditorWorker = await import('../BundleEditorWorker/BundleEditorWorker.js')
+    await BundleEditorWorker.bundleEditorWorker({
+      cachePath,
+      commitHash,
+      platform,
+      assetDir,
+    })
+    console.timeEnd('bundleEditorWorker')
+  }
+  return cachePath
+}
