baf46294c68ab0f7a122c69d465c561dcd32d9e5
Le Vivilet <levivlet@pm.me>
2024-05-18 09:45:00 +0200

    add horizontal function

--- packages/editor-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/editor-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
 import * as DeleteCharacterLeft from '../EditorCommand/EditorCommandDeleteCharacterLeft.js'
+import * as DeleteHorizontalRight from '../EditorCommand/EditorCommandDeleteHorizontalRight.js'
 
 export const commandMap = {
-  'Editor.deleteCharacterLeft': DeleteCharacterLeft.deleteCharacterLeft
+  'Editor.deleteCharacterLeft': DeleteCharacterLeft.deleteCharacterLeft,
+  'Editor.deleteHorizontalRight': DeleteHorizontalRight.editorDeleteHorizontalRight
 }
