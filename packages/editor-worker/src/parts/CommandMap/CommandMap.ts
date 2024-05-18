18dc2c0fa101a2bd31fdde7b244ebade2b9d8f53
Le Vivilet <levivlet@pm.me>
2024-05-18 09:40:29 +0200

    fix: editor worker imports

--- packages/editor-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/editor-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,1 +1,5 @@
-export const commandMap = {}
+import * as DeleteCharacterLeft from '../EditorCommand/EditorCommandDeleteCharacterLeft.js'
+
+export const commandMap = {
+  'Editor.deleteCharacterLeft': DeleteCharacterLeft.deleteCharacterLeft
+}
