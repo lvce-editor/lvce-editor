cd100ed8274c8ab4926ab27a95197451d840648c
Le Vivilet <levivlet@pm.me>
2024-05-18 09:54:14 +0200

    add helper function

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteCharacterLeft.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteCharacterLeft.js
@@ -2,6 +2,7 @@ import * as EditorFunctionType from '../EditorFunctionType/EditorFunctionType.js
 import * as EditorDeleteHorizontalLeft from './EditorCommandDeleteHorizontalLeft.js'
 import * as EditorDelta from './EditorCommandDelta.js'
 import * as RunEditorWidgetFunctions from './RunEditorWidgetFunctions.js'
+import * as EditorDeltaId from '../EditorDeltaId/EditorDeltaId.js'
 
 export const deleteCharacterLeft = (editor) => {
   const newEditor = EditorDeleteHorizontalLeft.editorDeleteHorizontalLeft(editor, EditorDelta.characterLeft)
