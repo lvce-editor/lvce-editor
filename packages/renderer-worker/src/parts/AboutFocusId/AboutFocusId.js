51c0c4a2b1c66aeb6fce82b4126d6d04c149d8bf
Le Vivilet <levivlet@pm.me>
2024-05-18 22:51:41 +0200

    feature: trap focus in about dialog

---
+++ packages/renderer-worker/src/parts/AboutFocusId/AboutFocusId.js
@@ -0,0 +1,3 @@
+export const None = 0
+export const Ok = 1
+export const Copy = 2
--- packages/renderer-worker/src/parts/ViewletAbout/ViewletAboutKeyBindings.js
+++ packages/renderer-worker/src/parts/ViewletAbout/ViewletAboutKeyBindings.js
@@ -1,11 +1,22 @@
 import * as KeyCode from '../KeyCode/KeyCode.js'
 import * as WhenExpression from '../WhenExpression/WhenExpression.js'
+import * as KeyModifier from '../KeyModifier/KeyModifier.js'
 
 export const getKeyBindings = () => {
   return [
     {
       key: KeyCode.Escape,
       command: 'About.handleClickClose',
+      when: WhenExpression.FocusAbout,
+    },
+    {
+      key: KeyCode.Tab,
+      command: 'About.focusNext',
+      when: WhenExpression.FocusAbout,
+    },
+    {
+      key: KeyCode.Tab | KeyModifier.Shift,
+      command: 'About.focusPrevious',
       when: WhenExpression.FocusAbout,
     },
   ]
