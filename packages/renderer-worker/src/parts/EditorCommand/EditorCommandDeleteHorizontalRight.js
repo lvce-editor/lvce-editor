317329c7bd1fe7b5b31e97259da9094991a6a1b8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-18 11:51:35 +0200

    Discard changes to packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteHorizontalRight.js

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteHorizontalRight.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteHorizontalRight.js
@@ -1,6 +1,43 @@
-import * as EditorWorker from '../EditorWorker/EditorWorker.js'
+import * as EditOrigin from '../EditOrigin/EditOrigin.js'
+import * as Editor from '../Editor/Editor.js'
+import * as GetEditorDeltaFunction from '../GetEditorDeltaFunction/GetEditorDeltaFunction.js'
+import * as EditorSelection from '../EditorSelection/EditorSelection.js'
+import * as GetSelectionPairs from '../GetSelectionPairs/GetSelectionPairs.js'
+import * as TextDocument from '../TextDocument/TextDocument.js'
+import * as EditorGetPositionRight from './EditorCommandGetPositionRight.js'
+import { editorReplaceSelections } from './EditorCommandReplaceSelection.js'
 
-export const editorDeleteHorizontalRight = async (editor, deltaId) => {
-  const result = await EditorWorker.invoke('Editor.deleteHorizontalRight', editor, deltaId)
-  return result
+const getChanges = (editor, getDelta) => {
+  const selections = editor.selections
+  if (EditorSelection.isEverySelectionEmpty(selections)) {
+    const changes = []
+    const lines = editor.lines
+    for (let i = 0; i < selections.length; i += 4) {
+      const [selectionStartRow, selectionStartColumn, selectionEndRow, selectionEndColumn] = GetSelectionPairs.getSelectionPairs(selections, i)
+      const start = {
+        rowIndex: selectionStartRow,
+        columnIndex: selectionStartColumn,
+      }
+      const positionRight = EditorGetPositionRight.editorGetPositionRight(start, lines, getDelta)
+      changes.push({
+        start: start,
+        end: positionRight,
+        inserted: [''],
+        deleted: TextDocument.getSelectionText(editor, {
+          start: start,
+          end: positionRight,
+        }),
+        origin: EditOrigin.DeleteHorizontalRight,
+      })
+    }
+    return changes
+  }
+  const changes = editorReplaceSelections(editor, [''], EditOrigin.DeleteHorizontalRight)
+  return changes
+}
+
+export const editorDeleteHorizontalRight = (editor, deltaId) => {
+  const fn = GetEditorDeltaFunction.getEditorDeltaFunction(deltaId)
+  const changes = getChanges(editor, fn)
+  return Editor.scheduleDocumentAndCursorsSelections(editor, changes)
 }
