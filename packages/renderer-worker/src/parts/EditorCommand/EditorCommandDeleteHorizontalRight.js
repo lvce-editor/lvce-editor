24ddd36f9838a3e9185368b7b35433231c5e9bb7
Le Vivilet <levivlet@pm.me>
2024-05-18 11:15:22 +0200

    fix fn

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteHorizontalRight.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandDeleteHorizontalRight.js
@@ -1,19 +1,6 @@
-import * as EditOrigin from '../EditOrigin/EditOrigin.js'
-import * as Editor from '../Editor/Editor.js'
-import * as GetEditorDeltaFunction from '../GetEditorDeltaFunction/GetEditorDeltaFunction.js'
-import * as EditorSelection from '../EditorSelection/EditorSelection.js'
-import * as GetSelectionPairs from '../GetSelectionPairs/GetSelectionPairs.js'
-import * as TextDocument from '../TextDocument/TextDocument.js'
-import * as EditorGetPositionRight from './EditorCommandGetPositionRight.js'
-import { editorReplaceSelections } from './EditorCommandReplaceSelection.js'
+import * as EditorWorker from '../EditorWorker/EditorWorker.js'
 
 export const editorDeleteHorizontalRight = async (editor, deltaId) => {
-  const { tokenizer, ...rest } = editor // TODO new tokenizer api
-  const newState = await EditorWorker.invoke('Editor.deleteHorizontalRight', rest, deltaId)
-  const newEditor = {
-    ...newState,
-    tokenizer,
-  }
-  const changes = editorReplaceSelections(editor, [''], EditOrigin.DeleteHorizontalRight)
-  return changes
+  const result = await EditorWorker.invoke('Editor.deleteHorizontalRight', editor, deltaId)
+  return result
 }
