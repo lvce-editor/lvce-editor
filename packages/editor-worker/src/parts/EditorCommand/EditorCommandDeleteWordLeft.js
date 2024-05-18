248081174c2b5b6b028981cb288b2b948b6e957d
Le Vivilet <levivlet@pm.me>
2024-05-18 07:35:54 +0200

    fix test

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandDeleteWordLeft.js
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandDeleteWordLeft.js
@@ -1,13 +1,10 @@
-// @ts-ignore
-import * as EditorFunctionType from '../EditorFunctionType/EditorFunctionType.js'
 import * as EditorDeleteHorizontalLeft from './EditorCommandDeleteHorizontalLeft.js'
 import * as EditorDelta from './EditorCommandDelta.js'
-import * as RunEditorWidgetFunctions from './RunEditorWidgetFunctions.js'
 
 export const deleteWordLeft = (editor) => {
   const newEditor = EditorDeleteHorizontalLeft.editorDeleteHorizontalLeft(editor, EditorDelta.wordLeft)
   return {
     newState: newEditor,
-    commands: RunEditorWidgetFunctions.runEditorWidgetFunctions(newEditor, EditorFunctionType.HandleEditorDeleteLeft),
+    commands: [],
   }
 }
