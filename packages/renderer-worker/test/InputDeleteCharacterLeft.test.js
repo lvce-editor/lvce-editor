f4e0a0beba15355774ec9507718a96b60c8382b0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-18 14:30:56 +0200

    feature: remove unused editor tests (#6087)

--- packages/renderer-worker/test/InputDeleteCharacterLeft.test.js
+++ packages/renderer-worker/test/InputDeleteCharacterLeft.test.js
@@ -1,4 +1,4 @@
-import { beforeAll, afterAll, test, expect, beforeEach, afterEach } from '@jest/globals'
+import { expect, test } from '@jest/globals'
 import * as InputDeleteCharacterLeft from '../src/parts/InputDeleteCharacterLeft/InputDeleteCharacterLeft.js'
 
 test('deleteCharacterLeft - empty string', () => {
