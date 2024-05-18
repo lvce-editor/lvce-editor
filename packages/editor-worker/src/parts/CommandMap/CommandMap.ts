5bcac7249a9c8cacbc21193e8060f79977082438
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-18 13:47:19 +0200

    feature: add more editor worker commands (#6086)

--- packages/editor-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/editor-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,15 @@
+import * as CursorCharacterLeft from '../EditorCommand/EditorCommandCursorCharacterLeft.js'
+import * as CursorCharacterRight from '../EditorCommand/EditorCommandCursorCharacterRight.js'
 import * as DeleteCharacterLeft from '../EditorCommand/EditorCommandDeleteCharacterLeft.js'
 import * as DeleteHorizontalRight from '../EditorCommand/EditorCommandDeleteHorizontalRight.js'
+import * as DeleteWordLeft from '../EditorCommand/EditorCommandDeleteWordLeft.js'
+import * as DeleteWordPartLeft from '../EditorCommand/EditorCommandDeleteWordPartLeft.js'
 
 export const commandMap = {
+  'Editor.cursorCharacterLeft': CursorCharacterLeft.cursorCharacterLeft,
+  'Editor.cursorCharacterRight': CursorCharacterRight.cursorCharacterRight,
   'Editor.deleteCharacterLeft': DeleteCharacterLeft.deleteCharacterLeft,
-  'Editor.deleteHorizontalRight': DeleteHorizontalRight.editorDeleteHorizontalRight
+  'Editor.deleteHorizontalRight': DeleteHorizontalRight.editorDeleteHorizontalRight,
+  'Editor.deleteWordLeft': DeleteWordLeft.deleteWordLeft,
+  'Editor.deleteWordPartLeft': DeleteWordPartLeft.deleteWordPartLeft,
 }
