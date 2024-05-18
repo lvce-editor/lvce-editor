04bff7bcc201917d2a05857b2000601dfafb36d7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-18 11:49:04 +0200

    Discard changes to packages/renderer-worker/test/EditorCursorEnd.test.js

---
+++ packages/renderer-worker/test/EditorCursorEnd.test.js
@@ -0,0 +1,28 @@
+import { beforeAll, afterAll, test, expect, beforeEach, afterEach } from '@jest/globals'
+import * as EditorCursorEnd from '../src/parts/EditorCommand/EditorCommandCursorEnd.js'
+import * as EditorSelection from '../src/parts/EditorSelection/EditorSelection.js'
+
+test('editorCursorEnd', () => {
+  const editor = {
+    lines: ['aaaaa'],
+    primarySelectionIndex: 0,
+    selections: EditorSelection.fromRange(0, 4, 0, 4),
+  }
+  expect(EditorCursorEnd.cursorEnd(editor)).toMatchObject({
+    selections: EditorSelection.fromRange(0, 5, 0, 5),
+  })
+})
+
+test('editorCursorEnd - with selection', () => {
+  const editor = {
+    lines: ['aaaaa'],
+    cursor: {
+      rowIndex: 0,
+      columnIndex: 4,
+    },
+    selections: EditorSelection.fromRange(0, 0, 0, 4),
+  }
+  expect(EditorCursorEnd.cursorEnd(editor)).toMatchObject({
+    selections: EditorSelection.fromRange(0, 4, 0, 4),
+  })
+})
