dd421a88d5261a04d3d189675f164cd5ca7f552a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-18 11:51:04 +0200

    Discard changes to packages/editor-worker/src/parts/CommandMap/CommandMap.ts

--- packages/editor-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/editor-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,15 +1,7 @@
-import * as CursorCharacterLeft from '../EditorCommand/EditorCommandCursorCharacterLeft.js'
-import * as CursorCharacterRight from '../EditorCommand/EditorCommandCursorCharacterRight.js'
 import * as DeleteCharacterLeft from '../EditorCommand/EditorCommandDeleteCharacterLeft.js'
 import * as DeleteHorizontalRight from '../EditorCommand/EditorCommandDeleteHorizontalRight.js'
-import * as DeleteWordLeft from '../EditorCommand/EditorCommandDeleteWordLeft.js'
-import * as DeleteWordPartLeft from '../EditorCommand/EditorCommandDeleteWordPartLeft.js'
 
 export const commandMap = {
-  'Editor.cursorCharacterLeft': CursorCharacterLeft.cursorCharacterLeft,
-  'Editor.cursorCharacterRight': CursorCharacterRight.cursorCharacterRight,
   'Editor.deleteCharacterLeft': DeleteCharacterLeft.deleteCharacterLeft,
-  'Editor.deleteHorizontalRight': DeleteHorizontalRight.editorDeleteHorizontalRight,
-  'Editor.deleteWordLeft': DeleteWordLeft.deleteWordLeft,
-  'Editor.deleteWordPartLeft': DeleteWordPartLeft.deleteWordPartLeft,
+  'Editor.deleteHorizontalRight': DeleteHorizontalRight.editorDeleteHorizontalRight
 }
