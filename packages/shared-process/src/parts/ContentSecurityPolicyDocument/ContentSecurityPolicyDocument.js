5ed08acd5355e2e38da2a54d1f68cea5e5ffd950
Le Vivilet <levivlet@pm.me>
2024-05-18 22:16:49 +0200

    feature: disallow iframe in csp

--- packages/shared-process/src/parts/ContentSecurityPolicyDocument/ContentSecurityPolicyDocument.js
+++ packages/shared-process/src/parts/ContentSecurityPolicyDocument/ContentSecurityPolicyDocument.js
@@ -3,7 +3,6 @@ import * as GetContentSecurityPolicy from '../GetContentSecurityPolicy/GetConten
 export const value = GetContentSecurityPolicy.getContentSecurityPolicy([
   `default-src 'none'`,
   `font-src 'self'`,
-  `frame-src *`,
   `img-src 'self' https: data:`,
   `script-src 'self'`,
   `media-src 'self'`,
