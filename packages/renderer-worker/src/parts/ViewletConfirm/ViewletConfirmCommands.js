4e4da289863ffb6ec799cb4a2dc3c6a985f73945
Le Vivilet <levivlet@pm.me>
2024-05-18 14:01:25 +0200

    tsc

--- packages/renderer-worker/src/parts/ViewletConfirm/ViewletConfirmCommands.js
+++ packages/renderer-worker/src/parts/ViewletConfirm/ViewletConfirmCommands.js
@@ -2,7 +2,6 @@ import * as ViewletAbout from './ViewletConfirm.js'
 
 export const Commands = {
   handleClickClose: ViewletAbout.handleClickClose,
-  handleClickCopy: ViewletAbout.handleClickCopy,
   handleClickOk: ViewletAbout.handleClickOk,
   handleFocusIn: ViewletAbout.handleFocusIn,
 }
