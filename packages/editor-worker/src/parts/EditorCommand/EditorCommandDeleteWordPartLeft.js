413eab16778bde9e699880839c57cda8470573a2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-18 11:51:11 +0200

    Discard changes to packages/editor-worker/src/parts/EditorCommand/EditorCommandDeleteWordPartLeft.js

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandDeleteWordPartLeft.js
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandDeleteWordPartLeft.js
@@ -1,10 +1,13 @@
+// @ts-ignore
+import * as EditorFunctionType from '../EditorFunctionType/EditorFunctionType.js'
 import * as EditorDeleteHorizontalLeft from './EditorCommandDeleteHorizontalLeft.js'
 import * as EditorDelta from './EditorCommandDelta.js'
+import * as RunEditorWidgetFunctions from './RunEditorWidgetFunctions.js'
 
 export const deleteWordPartLeft = (editor) => {
   const newEditor = EditorDeleteHorizontalLeft.editorDeleteHorizontalLeft(editor, EditorDelta.wordPartLeft)
   return {
     newState: newEditor,
-    commands: [],
+    commands: RunEditorWidgetFunctions.runEditorWidgetFunctions(newEditor, EditorFunctionType.HandleEditorDeleteLeft),
   }
 }
