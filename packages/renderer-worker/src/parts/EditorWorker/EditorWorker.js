ce3a844de49d7b3b6d1fd583c2152839aa641786
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-18 07:20:46 +0200

    feature: add function to launch editor worker (#6075)

--- packages/renderer-worker/src/parts/EditorWorker/EditorWorker.js
+++ packages/renderer-worker/src/parts/EditorWorker/EditorWorker.js
@@ -1,1 +1,6 @@
-export {}
+import * as GetOrCreateWorker from '../GetOrCreateWorker/GetOrCreateWorker.js'
+import * as LaunchEditorWorker from '../LaunchEditorWorker/LaunchEditorWorker.js'
+
+const { invoke, invokeAndTransfer } = GetOrCreateWorker.getOrCreateWorker(LaunchEditorWorker.launchEditorWorker)
+
+export { invoke, invokeAndTransfer }
