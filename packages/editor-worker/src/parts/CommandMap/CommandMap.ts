d7d3499d48568d7333fb9258db74b59ff5b79fb1
Le Vivilet <levivlet@pm.me>
2024-05-18 11:23:25 +0200

    add command

--- packages/editor-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/editor-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
 import * as DeleteCharacterLeft from '../EditorCommand/EditorCommandDeleteCharacterLeft.js'
 import * as DeleteHorizontalRight from '../EditorCommand/EditorCommandDeleteHorizontalRight.js'
+import * as CursorCharacterLeft from '../EditorCommand/EditorCommandCursorCharacterLeft.js'
 
 export const commandMap = {
   'Editor.deleteCharacterLeft': DeleteCharacterLeft.deleteCharacterLeft,
-  'Editor.deleteHorizontalRight': DeleteHorizontalRight.editorDeleteHorizontalRight
+  'Editor.deleteHorizontalRight': DeleteHorizontalRight.editorDeleteHorizontalRight,
+  'Editor.cursorCharacterLeft': CursorCharacterLeft.cursorCharacterLeft
 }
