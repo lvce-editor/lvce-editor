60cf6d88a6dbb616ebf81a40269a7b835f35af24
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-18 11:51:21 +0200

    Discard changes to packages/editor-worker/src/parts/EditorCommand/RunEditorWidgetFunctions.js

---
+++ packages/editor-worker/src/parts/EditorCommand/RunEditorWidgetFunctions.js
@@ -0,0 +1,27 @@
+// @ts-ignore
+import * as Viewlet from '../Viewlet/Viewlet.js'
+// @ts-ignore
+import * as ViewletStates from '../ViewletStates/ViewletStates.js'
+
+export const runEditorWidgetFunctions = (editor, fnName, ...args) => {
+  const allCommands = []
+  const { widgets } = editor
+  if (widgets) {
+    for (const widget of widgets) {
+      const instance = ViewletStates.getInstance(widget)
+      if (!instance) {
+        return []
+      }
+      const { state, factory } = instance
+      const newState = factory[fnName](state, editor, ...args)
+      if (newState.disposed) {
+        const index = editor.widgets.indexOf(widget)
+        editor.widgets.splice(index, 1)
+        allCommands.push(...Viewlet.disposeFunctional(widget))
+      } else {
+        allCommands.push(...Viewlet.setStateFunctional(widget, newState))
+      }
+    }
+  }
+  return allCommands
+}
