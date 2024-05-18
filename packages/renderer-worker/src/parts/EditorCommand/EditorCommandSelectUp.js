1c539d5ed5ddc2cbe14f2810dbe17aae08f1405a
Le Vivilet <levivlet@pm.me>
2024-05-18 16:54:05 +0200

    add function

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandSelectUp.js
+++ packages/renderer-worker/src/parts/EditorCommand/EditorCommandSelectUp.js
@@ -1,20 +1,5 @@
-import * as Editor from '../Editor/Editor.js'
-import * as GetSelectionPairs from '../GetSelectionPairs/GetSelectionPairs.js'
-
-const getSelectUpChanges = (lines, selections) => {
-  const newSelections = new Uint32Array(selections.length)
-  for (let i = 0; i < selections.length; i += 4) {
-    const [selectionStartRow, selectionStartColumn, selectionEndRow, selectionEndColumn] = GetSelectionPairs.getSelectionPairs(selections, i)
-    newSelections[i] = Math.max(selectionStartRow - 1, 0)
-    newSelections[i + 1] = selectionStartColumn
-    newSelections[i + 2] = selectionEndRow
-    newSelections[i + 3] = selectionEndColumn
-  }
-  return newSelections
-}
+import * as EditorWorker from '../EditorWorker/EditorWorker.js'
 
 export const selectUp = (editor) => {
-  const { lines, selections } = editor
-  const newSelections = getSelectUpChanges(lines, selections)
-  return Editor.scheduleSelections(editor, newSelections)
+  return EditorWorker.invoke('Editor.selectUp', editor)
 }
