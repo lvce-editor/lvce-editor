976c7a7f160d67eaf3380b6e27cad68947d01a53
Le Vivilet <levivlet@pm.me>
2024-05-19 10:13:59 +0200

    feature: remove old browser view code

--- packages/main-process/src/parts/ElectronBrowserViewSuggestions/ElectronBrowserViewSuggestions.ipc.js
+++
@@ -1,9 +0,0 @@
-import * as ElectronBrowserViewSuggestions from './ElectronBrowserViewSuggestions.js'
-
-export const name = 'ElectronBrowserViewSuggestions'
-
-export const Commands = {
-  createBrowserView: ElectronBrowserViewSuggestions.createBrowserView,
-  disposeBrowserView: ElectronBrowserViewSuggestions.disposeBrowserView,
-  setSuggestions: ElectronBrowserViewSuggestions.setSuggestions,
-}
