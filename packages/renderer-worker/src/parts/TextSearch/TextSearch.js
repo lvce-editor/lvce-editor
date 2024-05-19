cc68d10ab0162f5fe30161b68ae7d363dc8480a7
Le Vivilet <levivlet@pm.me>
2024-05-19 18:16:05 +0200

    feature: use search worker for text search

--- packages/renderer-worker/src/parts/TextSearch/TextSearch.js
+++ packages/renderer-worker/src/parts/TextSearch/TextSearch.js
@@ -1,8 +1,10 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as GetProtocol from '../GetProtocol/GetProtocol.js'
 import * as TextSearchProvider from '../TextSearchProvider/TextSearchProvider.js'
+import * as SearchWorker from '../SearchWorker/SearchWorker.js'
 
 export const textSearch = async (root, query, options) => {
+  return SearchWorker.invoke('TextSearch.textSearch', root, query, options)
   Assert.string(root)
   Assert.string(query)
   const scheme = GetProtocol.getProtocol(root)
