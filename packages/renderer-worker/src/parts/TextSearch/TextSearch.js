210c58fb0a2c24e177aedaefe8aba64c7426cd93
Le Vivilet <levivlet@pm.me>
2024-05-19 18:16:47 +0200

    remove unused code

--- packages/renderer-worker/src/parts/TextSearch/TextSearch.js
+++ packages/renderer-worker/src/parts/TextSearch/TextSearch.js
@@ -1,16 +1,7 @@
-import * as Assert from '../Assert/Assert.ts'
-import * as GetProtocol from '../GetProtocol/GetProtocol.js'
-import * as TextSearchProvider from '../TextSearchProvider/TextSearchProvider.js'
 import * as SearchWorker from '../SearchWorker/SearchWorker.js'
 
 export const textSearch = async (root, query, options) => {
   return SearchWorker.invoke('TextSearch.textSearch', root, query, options)
-  Assert.string(root)
-  Assert.string(query)
-  const scheme = GetProtocol.getProtocol(root)
-  const provider = await TextSearchProvider.getProvider(scheme)
-  const results = await provider.textSearch(scheme, root, query, options)
-  return results
 }
 
 export const cancel = async (searchId) => {
