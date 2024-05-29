5fec39cca85596b0c10334053f35b284efbad51c
Le Vivilet <levivlet@pm.me>
2024-05-29 22:30:13 +0200

    feature: simplify inline diff editor line dom

--- packages/renderer-worker/src/parts/GetInlineDiffEditorVirtualDom/GetInlineDiffEditorVirtualDom.js
+++ packages/renderer-worker/src/parts/GetInlineDiffEditorVirtualDom/GetInlineDiffEditorVirtualDom.js
@@ -1,79 +1,7 @@
 import * as ClassNames from '../ClassNames/ClassNames.js'
-import * as DiffType from '../DiffType/DiffType.js'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.js'
-import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
-
-const deletion = {
-  type: VirtualDomElements.Div,
-  className: `${ClassNames.EditorRow} ${ClassNames.Deletion}`,
-  childCount: 1,
-}
-
-const insertion = {
-  type: VirtualDomElements.Div,
-  className: `${ClassNames.EditorRow} ${ClassNames.Insertion}`,
-  childCount: 1,
-}
-
-const normal = {
-  type: VirtualDomElements.Div,
-  className: ClassNames.EditorRow,
-  childCount: 1,
-}
-
-const getPrefix = (type) => {
-  switch (type) {
-    case DiffType.Insertion:
-      return insertion
-    case DiffType.Deletion:
-      return deletion
-    case DiffType.None:
-      return normal
-  }
-}
-
-const getClassName = (type) => {
-  switch (type) {
-    case DiffType.Deletion:
-      return ClassNames.Deletion
-    case DiffType.Insertion:
-      return ClassNames.Insertion
-    case DiffType.None:
-      return ''
-  }
-}
-
-const renderLine = (value) => {
-  if (Math) {
-    const { type } = value
-    const prefix = getPrefix(type)
-    return [prefix, text(value.text)]
-  }
-  const { line, lineInfo, type } = value
-  if (lineInfo) {
-    const dom = []
-    dom.push({
-      type: VirtualDomElements.Div,
-      className: `${ClassNames.EditorRow} ${getClassName(type)}`,
-      childCount: lineInfo.length / 2,
-    })
-    for (let i = 0; i < lineInfo.length; i += 2) {
-      const tokenText = lineInfo[i]
-      const className = lineInfo[i + 1]
-      dom.push(
-        {
-          type: VirtualDomElements.Span,
-          className,
-          childCount: 1,
-        },
-        text(tokenText),
-      )
-    }
-    return dom
-  }
-  return [getPrefix(type), text(line)]
-}
+import * as GetInlineDiffEditorLineVirtualDom from '../GetInlineDiffEditorLineVirtualDom/GetInlineDiffEditorLineVirtualDom.js'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 
 const getLinesVirtualDom = (lines, className) => {
   return [
@@ -92,7 +20,7 @@ const getLinesVirtualDom = (lines, className) => {
       className: ClassNames.EditorRows,
       childCount: lines.length,
     },
-    ...lines.flatMap(renderLine),
+    ...lines.flatMap(GetInlineDiffEditorLineVirtualDom.renderLine),
   ]
 }
 
