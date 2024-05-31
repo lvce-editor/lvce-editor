82b498384f68f6fcc4ff638735fa412c597e048e
Le Vivilet <levivlet@pm.me>
2024-05-31 17:41:31 +0200

    add fn

---
+++ packages/renderer-process/src/parts/GetFilePathElectron/GetFilePathElectron.ipc.ts
@@ -0,0 +1,7 @@
+import * as GetFilePathElectron from './GetFilePathElectron.ts'
+
+export const name = 'GetFilePathElectron'
+
+export const Commands = {
+  getFilePathElectron: GetFilePathElectron.getFilePathElectron,
+}
