4c60a7f04a6976019636d63d83a0ec9a8ffb477f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-31 18:43:08 +0200

    fix: icon path (#6169)

--- packages/shared-process/src/parts/GetIcon/GetIcon.js
+++ packages/shared-process/src/parts/GetIcon/GetIcon.js
@@ -1,13 +1,15 @@
+import * as IsLinux from '../IsLinux/IsLinux.js'
+import * as IsWindows from '../IsWindows/IsWindows.js'
 import * as Path from '../Path/Path.js'
 import * as Platform from '../Platform/Platform.js'
 import * as Root from '../Root/Root.js'
 
 export const getIcon = () => {
-  if (!Platform.isProduction && Platform.isLinux) {
-    return Path.join(Root.root, 'build', 'files', 'icon.png')
+  if (!Platform.isProduction && IsLinux.isLinux) {
+    return Path.join(Root.root, 'packages', 'build', 'files', 'icon.png')
   }
-  if (!Platform.isProduction && Platform.isWindows) {
-    return Path.join(Root.root, 'build', 'files', 'icon.png')
+  if (!Platform.isProduction && IsWindows.isWindows) {
+    return Path.join(Root.root, 'packages', 'build', 'files', 'icon.png')
   }
   if (Platform.isProduction && Platform.isArchLinux) {
     return Path.join(Root.root, 'static', 'icons', 'icon.png')
