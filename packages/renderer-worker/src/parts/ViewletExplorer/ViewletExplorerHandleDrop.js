0507adee55fdcf0d1c495b40bb674beb24775302
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-31 17:30:42 +0200

    Discard changes to packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDrop.js

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDrop.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDrop.js
@@ -5,7 +5,6 @@ import { getIndexFromPosition } from './ViewletExplorerShared.js'
 
 export const handleDrop = async (state, x, y, files) => {
   try {
-    console.log({ x, y, files })
     const index = getIndexFromPosition(state, x, y)
     switch (index) {
       case -1:
