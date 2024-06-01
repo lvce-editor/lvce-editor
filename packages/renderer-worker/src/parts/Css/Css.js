a34b3200aedec434d1f7bf80ac37c785b387f93c
Le Vivilet <levivlet@pm.me>
2024-06-01 22:18:55 +0200

    fix test

--- packages/renderer-worker/src/parts/Css/Css.js
+++ packages/renderer-worker/src/parts/Css/Css.js
@@ -2,20 +2,15 @@ import * as Character from '../Character/Character.js'
 import * as CssState from '../CssState/CssState.js'
 import * as GetCss from '../GetCss/GetCss.js'
 import * as RendererProcess from '../RendererProcess/RendererProcess.js'
-import { VError } from '../VError/VError.js'
 
 const getId = (path) => {
   return 'Css' + path.replace('/css/parts/', '').replaceAll(Character.Slash, Character.Dash).replace('.css', Character.EmptyString)
 }
 
 const actuallyLoadCssStyleSheet = async (css) => {
-  try {
-    const text = await GetCss.getCss(css)
-    const id = getId(css)
-    await addCssStyleSheet(id, text)
-  } catch (error) {
-    throw new VError(error, `Failed to load css "${css}"`)
-  }
+  const text = await GetCss.getCss(css)
+  const id = getId(css)
+  await addCssStyleSheet(id, text)
 }
 
 export const loadCssStyleSheet = (id) => {
