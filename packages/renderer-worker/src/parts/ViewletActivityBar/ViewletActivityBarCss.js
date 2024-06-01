36a02ebd3602b6149310bd5b07579f138231f96d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-06-01 22:52:55 +0200

    feature: move activity bar item css into separate file (#6189)

--- packages/renderer-worker/src/parts/ViewletActivityBar/ViewletActivityBarCss.js
+++ packages/renderer-worker/src/parts/ViewletActivityBar/ViewletActivityBarCss.js
@@ -1,3 +1,3 @@
-export const Css = ['/css/parts/ViewletActivityBar.css', '/css/parts/MaskIcon.css', '/css/parts/Badge.css']
+export const Css = ['/css/parts/ViewletActivityBar.css', '/css/parts/MaskIcon.css', '/css/parts/Badge.css', '/css/parts/ActivityBarItem.css']
 
 export const Variables = ['ActivityBarForeground', 'ActivityBarActiveForeground', 'ActvityBarActiveBackground', 'BadgeBackground', 'BadgeForeground']
