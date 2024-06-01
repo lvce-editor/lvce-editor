c7045041615e59f55d80f22b7073bd7c2fae8d01
Le Vivilet <levivlet@pm.me>
2024-06-01 14:39:30 +0200

    feature: use npm package for typescript compile process

--- packages/shared-process/src/parts/TypeScriptCompileProcessPath/TypeScriptCompileProcessPath.js
+++ packages/shared-process/src/parts/TypeScriptCompileProcessPath/TypeScriptCompileProcessPath.js
@@ -1,4 +1,13 @@
 import * as Path from '../Path/Path.js'
 import * as Root from '../Root/Root.js'
 
-export const typescriptCompileProcessPath = Path.join(Root.root, 'packages', 'typescript-compile-process', 'src', 'typescriptCompileProcessMain.js')
+export const typescriptCompileProcessPath = Path.join(
+  Root.root,
+  'packages',
+  'shared-process',
+  'node_modules',
+  '@lvce-editor',
+  'typescript-compile-process',
+  'src',
+  'typescriptCompileProcessMain.js',
+)
