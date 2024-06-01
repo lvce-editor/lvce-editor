ffd4cb7c1df54411c210b597d2645ffc209efa70
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-06-01 22:16:33 +0200

    feature: improve file watcher (#6185)

--- packages/renderer-worker/src/parts/DevelopFileWatcher/DevelopFileWatcher.js
+++ packages/renderer-worker/src/parts/DevelopFileWatcher/DevelopFileWatcher.js
@@ -1,7 +1,9 @@
-import * as Preferences from '../Preferences/Preferences.js'
-import * as SharedProcess from '../SharedProcess/SharedProcess.js'
-import * as IsProduction from '../IsProduction/IsProduction.js'
 import * as FileWatcher from '../FileWatcher/FileWatcher.js'
+import * as IsElectron from '../IsElectron/IsElectron.js'
+import * as IsProduction from '../IsProduction/IsProduction.js'
+import * as Preferences from '../Preferences/Preferences.js'
+import * as Reload from '../Reload/Reload.js'
+import * as SharedProcess from '../SharedProcess/SharedProcess.js'
 
 export const hydrate = async () => {
   if (IsProduction.isProduction) {
@@ -12,10 +14,19 @@ export const hydrate = async () => {
     return
   }
   const root = await SharedProcess.invoke('Platform.getRoot')
-  await FileWatcher.watch({
-    root,
+  const staticPath = `${root}/static`
+  const watcher = await FileWatcher.watch({
+    root: staticPath,
     exclude: ['node_modules', 'dist', '.tmp'],
   })
-  console.log({ root })
+  const handleEvent = async (event) => {
+    const { detail } = event
+    if (detail && detail.eventType === 'change') {
+      await Reload.reload()
+    }
+    // console.log(detail)
+  }
+  // TODO use async iterator
+  watcher.addEventListener('watcher-event', handleEvent)
   // const watcher =
 }
--- packages/shared-process/src/parts/FileWatcher/FileWatcher.js
+++ packages/shared-process/src/parts/FileWatcher/FileWatcher.js
@@ -1,10 +1,11 @@
 import * as fs from 'node:fs/promises'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 
-const handleEvents = async (watcher) => {
+const handleEvents = async (watcher, id, ipc) => {
   try {
-    console.log(watcher)
     for await (const event of watcher) {
-      console.log(event)
+      JsonRpc.send(ipc, 'FileWatcher.handleEvent', id, event)
+      // ipc.send('FileWatcher.handleEvent', id, event)
     }
   } catch (error) {
     console.log('event error', error)
@@ -12,15 +13,13 @@ const handleEvents = async (watcher) => {
 }
 
 // TODO  run file watcher in a separate process to not crash application when file watcher crashes
-export const watch = async ({ root, exclude }) => {
+export const watch = async (ipc, id, { root, exclude }) => {
   try {
-    console.log('start watching', root)
     const watcher = fs.watch(root, {
       recursive: true,
     })
-    handleEvents(watcher)
+    handleEvents(watcher, id, ipc)
   } catch (error) {
     console.error(error)
   }
-  console.log('finish watching')
 }
