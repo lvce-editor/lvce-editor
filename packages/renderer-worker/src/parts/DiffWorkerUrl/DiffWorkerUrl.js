17378433774a58c1a7eea2293737e6c0039e12ae
Le Vivilet <levivlet@pm.me>
2024-06-08 09:53:46 +0200

    feature: use diff worker npm package

--- packages/renderer-worker/src/parts/DiffWorkerUrl/DiffWorkerUrl.js
+++ packages/renderer-worker/src/parts/DiffWorkerUrl/DiffWorkerUrl.js
@@ -1,3 +1,3 @@
 import * as AssetDir from '../AssetDir/AssetDir.js'
 
-export const diffWorkerUrl = `${AssetDir.assetDir}/packages/diff-worker/src/diffWorkerMain.ts`
+export const diffWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/diff-worker/dist/diffWorkerMain.js`
