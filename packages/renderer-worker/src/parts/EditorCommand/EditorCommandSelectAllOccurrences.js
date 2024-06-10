4175b3886427d15f1ca423c31d6b7db2c4274424
Le Vivilet <levivlet@pm.me>
2024-06-10 10:23:22 +0200

    feature: remove unused editor commands

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandSelectAllOccurrences.js
+++
@@ -1,5 +0,0 @@
-import * as EditorWorker from '../EditorWorker/EditorWorker.js'
-
-export const selectAllOccurrences = (editor) => {
-  return EditorWorker.invoke('Editor.selectAllOccurrences', editor)
-}
