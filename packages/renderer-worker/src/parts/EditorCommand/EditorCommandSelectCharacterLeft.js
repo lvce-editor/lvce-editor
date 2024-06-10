ca08300a6d4b94d8b0e70d9294f3255e294ed709
Le Vivilet <levivlet@pm.me>
2024-06-10 10:23:51 +0200

    feature: remove unused editor commands

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandSelectCharacterLeft.js
+++
@@ -1,5 +0,0 @@
-import * as EditorWorker from '../EditorWorker/EditorWorker.js'
-
-export const selectCharacterLeft = (editor) => {
-  return EditorWorker.invoke('Editor.selectCharacterLeft', editor)
-}
--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandSelectCharacterRight.js
+++
@@ -1,5 +0,0 @@
-import * as EditorWorker from '../EditorWorker/EditorWorker.js'
-
-export const selectCharacterRight = (editor) => {
-  return EditorWorker.invoke('Editor.selectCharacterRight', editor)
-}
