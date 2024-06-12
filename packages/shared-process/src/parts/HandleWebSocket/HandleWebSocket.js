48e9195fd86280b14e24e90a08027406ad8e0034
Le Vivilet <levivlet@pm.me>
2024-06-12 19:45:13 +0200

    try to fix econnreset error in shared process

--- packages/shared-process/src/parts/HandleWebSocket/HandleWebSocket.js
+++ packages/shared-process/src/parts/HandleWebSocket/HandleWebSocket.js
@@ -11,8 +11,8 @@ export const handleWebSocket = async (handle, message) => {
     const { url } = message
     const type = GetTypeFromUrl.getTypeFromUrl(url)
     handle.pause()
-    const module = await HandleWebSocketModule.load(type)
-    await module.handleWebSocket(message, handle, type)
+    const module = HandleWebSocketModule.load(type)
+    module.handleWebSocket(message, handle, type)
   } catch (error) {
     DestroyWebSocket.destroySocket(handle)
     throw new VError(error, `Failed to connect to websocket`)
--- packages/shared-process/src/parts/HandleWebSocketModule/HandleWebSocketModule.js
+++ packages/shared-process/src/parts/HandleWebSocketModule/HandleWebSocketModule.js
@@ -1,3 +1,8 @@
+import * as HandleWebSocketForExtensionHostHelperProcess from '../HandleWebSocketForExtensionHostHelperProcess/HandleWebSocketForExtensionHostHelperProcess.js'
+import * as HandleWebSocketForSearchProcess from '../HandleWebSocketForSearchProcess/HandleWebSocketForSearchProcess.js'
+import * as HandleWebSocketForSharedProcess from '../HandleWebSocketForSharedProcess/HandleWebSocketForSharedProcess.js'
+import * as HandleWebSocketForTerminalProcess from '../HandleWebSocketForTerminalProcess/HandleWebSocketForTerminalProcess.js'
+import * as HandleWebSocketForUnknown from '../HandleWebSocketForUnknown/HandleWebSocketForUnknown.js'
 import * as ProtocolType from '../ProtocolType/ProtocolType.js'
 import { VError } from '../VError/VError.js'
 
@@ -7,14 +12,14 @@ export const load = (protocol) => {
   }
   switch (protocol) {
     case ProtocolType.SharedProcess:
-      return import('../HandleWebSocketForSharedProcess/HandleWebSocketForSharedProcess.js')
+      return HandleWebSocketForSharedProcess
     case ProtocolType.ExtensionHostHelperProcess:
-      return import('../HandleWebSocketForExtensionHostHelperProcess/HandleWebSocketForExtensionHostHelperProcess.js')
+      return HandleWebSocketForExtensionHostHelperProcess
     case ProtocolType.TerminalProcess:
-      return import('../HandleWebSocketForTerminalProcess/HandleWebSocketForTerminalProcess.js')
+      return HandleWebSocketForTerminalProcess
     case ProtocolType.SearchProcess:
-      return import('../HandleWebSocketForSearchProcess/HandleWebSocketForSearchProcess.js')
+      return HandleWebSocketForSearchProcess
     default:
-      return import('../HandleWebSocketForUnknown/HandleWebSocketForUnknown.js')
+      return HandleWebSocketForUnknown
   }
 }
