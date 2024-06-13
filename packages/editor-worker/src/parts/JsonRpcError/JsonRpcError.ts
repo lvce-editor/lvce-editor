68315a30fa348ec88506c5fd188863602cffd594
Le Vivilet <levivlet@pm.me>
2024-06-13 20:02:57 +0200

    fix types

--- packages/editor-worker/src/parts/JsonRpcError/JsonRpcError.ts
+++ packages/editor-worker/src/parts/JsonRpcError/JsonRpcError.ts
@@ -1,5 +1,5 @@
 export class JsonRpcError extends Error {
-  constructor(message) {
+  constructor(message: string) {
     super(message)
     this.name = 'JsonRpcError'
   }
--- packages/editor-worker/src/parts/MeasureLongestLineWidth/MeasureLongestLineWidth.ts
+++ packages/editor-worker/src/parts/MeasureLongestLineWidth/MeasureLongestLineWidth.ts
@@ -1,6 +1,14 @@
 import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.ts'
 
-export const measureLongestLineWidth = (lines, fontWeight, fontSize, fontFamily, letterSpacing, isMonoSpaceFont, charWidth) => {
+export const measureLongestLineWidth = (
+  lines: any[],
+  fontWeight: number,
+  fontSize: number,
+  fontFamily: string,
+  letterSpacing: number,
+  isMonoSpaceFont: boolean,
+  charWidth: number,
+) => {
   let longest = 0
   for (const line of lines) {
     const lineWidth = MeasureTextWidth.measureTextWidth(line, fontWeight, fontSize, fontFamily, letterSpacing, isMonoSpaceFont, charWidth)
