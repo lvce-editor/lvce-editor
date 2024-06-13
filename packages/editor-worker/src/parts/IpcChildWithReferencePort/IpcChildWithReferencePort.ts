320dca306695a739b50b4a44e28765395dd590e6
Le Vivilet <levivlet@pm.me>
2024-06-13 20:06:21 +0200

    fix types

--- packages/editor-worker/src/parts/IpcChildWithReferencePort/IpcChildWithReferencePort.ts
+++ packages/editor-worker/src/parts/IpcChildWithReferencePort/IpcChildWithReferencePort.ts
@@ -1,20 +1,21 @@
 export const listen = () => {
   const referencePort = {
-    onmessage(message, transferables) {},
+    onmessage(message: any, transferables: any) {},
     postMessage() {},
   }
   // TODO get rid of extra data wrapper
+  // @ts-ignore
   globalThis.acceptReferencePort(referencePort)
   return referencePort
 }
 
-export const wrap = (referencePort) => {
+export const wrap = (referencePort: any) => {
   return {
     referencePort,
-    send(message) {
+    send(message: any) {
       this.referencePort.onmessage({ data: message })
     },
-    sendAndTransfer({ data: message }, transferables) {
+    sendAndTransfer({ data: message }: any, transferables: any) {
       this.referencePort.onmessage(message, transferables)
     },
     get onmessage() {
