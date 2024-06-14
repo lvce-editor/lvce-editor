7ec732bfe0d79fabd2a94078e1a6bbd98f83f150
Le Vivilet <levivlet@pm.me>
2024-06-14 23:43:10 +0200

    feature: add setting to configure renderer process path

--- packages/shared-process/src/parts/GetElectronFileResponseContent/GetElectronFileResponseContent.js
+++ packages/shared-process/src/parts/GetElectronFileResponseContent/GetElectronFileResponseContent.js
@@ -2,6 +2,8 @@ import { readFile } from 'node:fs/promises'
 import * as Platform from '../Platform/Platform.js'
 import * as ShouldTranspileTypescript from '../ShouldTranspileTypescript/ShouldTranspileTypescript.js'
 import * as TranspileTypeScript from '../TranspileTypeScript/TranspileTypeScript.js'
+import * as Preferences from '../Preferences/Preferences.js'
+import { pathToFileURL } from 'node:url'
 
 export const getElectronFileResponseContent = async (request, absolutePath, url) => {
   if (ShouldTranspileTypescript.shouldTranspileTypescript(request, url)) {
@@ -13,6 +15,19 @@ export const getElectronFileResponseContent = async (request, absolutePath, url)
   if (!Platform.isProduction && url === `${Platform.scheme}://-/`) {
     // @ts-ignore
     content = content.toString().replace('    <link rel="manifest" href="/manifest.json" crossorigin="use-credentials" />\n', '')
+  }
+  if (!Platform.isProduction && url === '/') {
+    const preferences = await Preferences.getUserPreferences()
+    if (preferences['develop.rendererProcessPath']) {
+      console.log('custom renderer process path')
+      // @ts-ignore
+      content = content
+        .toString()
+        .replace(
+          '/packages/renderer-worker/node_modules/@lvce-editor/renderer-process/dist/rendererProcessMain.js',
+          '/remote' + pathToFileURL(preferences['develop.rendererProcessPath']).toString().slice(7),
+        )
+    }
   }
   return content
 }
