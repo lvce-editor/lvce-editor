dc47e0722bf20d0008681214708935753442916d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-06-15 22:02:28 +0200

    feature: move selectNextOccurrence command to editor worker (#6250)

--- packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextCommands.js
+++ packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextCommands.js
@@ -126,7 +126,6 @@ export const LazyCommands = {
   openFind: () => import('../EditorCommand/EditorCommandOpenFind.js'),
   selectionGrow: () => import('../EditorCommand/EditorCommandSelectionGrow.js'),
   selectInsideString: () => import('../EditorCommand/EditorCommandSelectInsideString.js'),
-  selectNextOccurrence: () => import('../EditorCommand/EditorCommandSelectNextOccurrence.js'),
   setDecorations: () => import('../EditorCommand/EditorCommandSetDecorations.js'),
   setDeltaY: () => import('../EditorCommand/EditorCommandSetDelta.js'),
   setDelta: () => import('../EditorCommand/EditorCommandSetDelta.js'),
