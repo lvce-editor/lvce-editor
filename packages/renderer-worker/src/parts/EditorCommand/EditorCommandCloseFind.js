15e4adf1348ee2cac2f382960aa351ffc0084ae1
Le Vivilet <levivlet@pm.me>
2024-06-15 21:57:08 +0200

    feature: remove unused editor worker commands

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandCloseFind.js
+++
@@ -1,7 +0,0 @@
-import * as Viewlet from '../Viewlet/Viewlet.js'
-import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
-
-export const closeFind = async (state) => {
-  await Viewlet.closeWidget(ViewletModuleId.FindWidget)
-  return state
-}
--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandCopyLineDown.js
+++
@@ -1,6 +0,0 @@
-import * as EditorWorker from '../EditorWorker/EditorWorker.js'
-
-export const copyLineDown = async (editor) => {
-  const newEditor = await EditorWorker.invoke('Editor.copyLineDown', editor)
-  return newEditor
-}
