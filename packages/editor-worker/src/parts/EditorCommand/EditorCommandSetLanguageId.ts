2c1d0330a314dfab671a20457153b15d1199c0e5
Le Vivilet <levivlet@pm.me>
2024-06-16 00:00:18 +0200

    tsc

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandSetLanguageId.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandSetLanguageId.ts
@@ -1,27 +1,24 @@
-// @ts-ignore
-import * as Tokenizer from '../Tokenizer/Tokenizer.ts'
-// @ts-ignore
-import * as ViewletState from '../ViewletStates/ViewletStates.ts'
+// import * as Tokenizer from '../Tokenizer/Tokenizer.ts'
+// import * as ViewletState from '../ViewletStates/ViewletStates.ts'
 
-// @ts-ignore
-export const setLanguageId = async (editor, languageId) => {
-  const { uid } = editor
+export const setLanguageId = async (editor: any, languageId: string) => {
+  // const { uid } = editor
   // TODO only load tokenizer if not already loaded
   // if already loaded just set tokenizer and rerender text
-  await Tokenizer.loadTokenizer(languageId)
-  const tokenizer = Tokenizer.getTokenizer(languageId)
-  if (tokenizer === editor.tokenizer) {
-    return editor
-  }
+  // await Tokenizer.loadTokenizer(languageId)
+  // const tokenizer = Tokenizer.getTokenizer(languageId)
+  // if (tokenizer === editor.tokenizer) {
+  // return editor
+  // }
   // TODO update syntax highlighting
   // TODO get edits
 
-  const latestEditor = ViewletState.getState(uid)
+  // const latestEditor = ViewletState.getState(uid)
 
   return {
-    ...latestEditor,
+    ...editor,
     languageId,
     invalidStartIndex: 0,
-    tokenizer,
+    // tokenizer,
   }
 }
