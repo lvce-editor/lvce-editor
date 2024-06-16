c93fdf3e1f45ae1a9e1ef887a5a948ed165e7dab
Le Vivilet <levivlet@pm.me>
2024-06-16 22:52:15 +0200

    feature: send messageport from editor worker to renderer process

--- packages/editor-worker/src/parts/HandleIpc/HandleIpc.ts
+++ packages/editor-worker/src/parts/HandleIpc/HandleIpc.ts
@@ -1,11 +1,10 @@
-// @ts-nocheck
-
 import * as Callback from '../Callback/Callback.ts'
 import * as Command from '../Command/Command.ts'
 import * as HandleJsonRpcMessage from '../HandleJsonRpcMessage/HandleJsonRpcMessage.ts'
 
-export const handleIpc = (ipc, source = 'process') => {
-  const handleMessage = (message) => {
+export const handleIpc = (ipc: any, source = 'process') => {
+  const handleMessage = (message: any) => {
+    // @ts-ignore
     return HandleJsonRpcMessage.handleJsonRpcMessage(ipc, message, Command.execute, Callback.resolve, source)
   }
   ipc.onmessage = handleMessage
