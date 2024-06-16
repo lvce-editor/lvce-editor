b578231b51aacd3ef508edef60f5b1fb784126dd
Le Vivilet <levivlet@pm.me>
2024-06-16 22:43:31 +0200

    feature: prepare to send messageport from editor worker to renderer process for direct communication

---
+++ packages/renderer-worker/src/parts/SendMessagePortToRendererProcess/SendMessagePortToRendererProcess.ipc.js
@@ -0,0 +1,7 @@
+import * as SendMessagePortToRendererProcess from './SendMessagePortToRendererProcess.js'
+
+export const name = 'SendMessagePortToRendererProcess'
+
+export const Commands = {
+  sendMessagePortToRendererProcess: SendMessagePortToRendererProcess.sendMessagePortToRendererProcess,
+}
