fde39ab4901562c4aa1c21ba6a97935113eb9ac4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-06-16 22:43:55 +0200

    feature: prepare to send messageport from editor worker to renderer p… (#6295)

--- packages/editor-worker/src/parts/IpcParentModule/IpcParentModule.ts
+++ packages/editor-worker/src/parts/IpcParentModule/IpcParentModule.ts
@@ -4,6 +4,8 @@ export const getModule = (method: any) => {
   switch (method) {
     case IpcParentType.NodeAlternate:
       return import('../IpcParentWithNodeAlternate/IpcParentWithNodeAlternate.ts')
+    case IpcParentType.RendererProcess:
+      return import('../IpcParentWithRendererProcess/IpcParentWithRendererProcess.ts')
     default:
       throw new Error('unexpected ipc type')
   }
--- packages/renderer-worker/src/parts/LaunchEditorWorker/LaunchEditorWorker.js
+++ packages/renderer-worker/src/parts/LaunchEditorWorker/LaunchEditorWorker.js
@@ -1,7 +1,7 @@
+import * as EditorWorkerUrl from '../EditorWorkerUrl/EditorWorkerUrl.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
-import * as EditorWorkerUrl from '../EditorWorkerUrl/EditorWorkerUrl.js'
 
 export const launchEditorWorker = async () => {
   const ipc = await IpcParent.create({
