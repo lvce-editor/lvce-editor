cbd1efe713292294adc8ad15809c93e3a3482977
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-06-17 21:49:24 +0200

    feature: add beforeinput command (#6301)

--- packages/editor-worker/src/parts/EditorCommand/EditorCommandSelectCharacterRight.ts
+++ packages/editor-worker/src/parts/EditorCommand/EditorCommandSelectCharacterRight.ts
@@ -1,7 +1,6 @@
 import * as EditorSelectHorizontalRight from './EditorCommandSelectHorizontalRight.ts'
 import * as EditorDelta from './EditorCommandDelta.ts'
 
-// @ts-ignore
-export const selectCharacterRight = (editor) => {
+export const selectCharacterRight = (editor: any) => {
   return EditorSelectHorizontalRight.editorSelectHorizontalRight(editor, EditorDelta.characterRight)
 }
