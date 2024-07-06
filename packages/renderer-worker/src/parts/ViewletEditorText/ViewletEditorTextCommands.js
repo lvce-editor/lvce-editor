3cef9b7ac1f34a68979bd2a4ccb83db4bd410113
Le Vivilet <levivlet@pm.me>
2024-07-06 22:11:06 +0200

    fix: typing in editor

--- packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextCommands.js
+++ packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextCommands.js
@@ -59,6 +59,7 @@ const ids = [
   'goToDefinition',
   'goToTypeDefinition',
   'handleBeforeInputFromContentEditable',
+  'handleBeforeInput',
   'handleContextMenu',
   'handleDoubleClick',
   'handleMouseDown',
