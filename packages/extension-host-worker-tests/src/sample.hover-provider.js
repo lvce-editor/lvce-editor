470c1ebb0b168fffd0153a76e26d27bd130575ff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-06 23:07:01 +0200

    feature: add hover e2e test (#6313)

---
+++ packages/extension-host-worker-tests/src/sample.hover-provider.js
@@ -0,0 +1,21 @@
+export const name = 'sample.hover-provider'
+
+export const test = async ({ FileSystem, Workspace, Extension, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, ['line', ''].join('\n'))
+
+  await Workspace.setPath(tmpDir)
+  await Extension.addWebExtension(new URL('../fixtures/sample.hover-provider', import.meta.url).toString())
+
+  // act
+  await Main.openUri(`${tmpDir}/test.xyz`)
+  await Editor.setCursor(0, 0)
+  await Editor.openHover()
+
+  // assert
+  const hover = Locator('.EditorHover')
+  await expect(hover).toBeVisible()
+  await expect(hover).toHaveText('abc')
+  // await expect(hover).toHaveCSS('translate', '0px -528px')
+}
