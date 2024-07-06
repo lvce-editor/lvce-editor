c23c156a0704628f9903aed993d9eed1a32fb3eb
Le Vivilet <levivlet@pm.me>
2024-07-06 23:49:22 +0200

    feature: prepapre to make hover widget a fixed height and position it at the top or bottom

--- packages/renderer-worker/src/parts/MeasureTextHeight/MeasureTextHeight.js
+++ packages/renderer-worker/src/parts/MeasureTextHeight/MeasureTextHeight.js
@@ -3,3 +3,7 @@ import * as RendererProcess from '../RendererProcess/RendererProcess.js'
 export const measureTextHeight = (text, fontFamily, fontSize) => {
   return RendererProcess.invoke('MeasureTextHeight.measureTextHeight', text, fontFamily, fontSize)
 }
+
+export const measureTextBlockHeight = (text, fontFamily, fontSize, lineHeight, width) => {
+  return RendererProcess.invoke('MeasureTextBlockHeight.measureTextBlockHeight', text, fontSize, fontFamily, lineHeight, width)
+}
