99b9762f5d380ff1d8cda43dbdc33c200f77f757
Le Vivilet <levivlet@pm.me>
2024-07-06 22:51:34 +0200

    e2e

---
+++ packages/extension-host-worker-tests/src/sample.hover-provider.js
@@ -0,0 +1,15 @@
+export const name = 'sample.hover-provider'
+
+export const test = async ({ FileSystem, Workspace, Extension, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, ['   line   ', '   line   ', '   line   '].join('\n'))
+
+  await Workspace.setPath(tmpDir)
+  await Extension.addWebExtension(new URL('../fixtures/sample.hover-provider', import.meta.url).toString())
+
+  // act
+  await Main.openUri(`${tmpDir}/test.xyz`)
+  await Editor.setCursor(0, 0)
+  await Editor.openCompletion()
+}
