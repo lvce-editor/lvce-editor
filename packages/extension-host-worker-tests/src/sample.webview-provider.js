599d6ef851a42d8786b04f54945ce31766129d5f
Le Vivilet <levivlet@pm.me>
2024-07-07 22:50:26 +0200

    add e2e test

---
+++ packages/extension-host-worker-tests/src/sample.webview-provider.js
@@ -0,0 +1,8 @@
+export const skip = true
+
+export const name = 'sample.webview-provider'
+
+export const test = async ({ Extension, QuickPick, Locator, expect }) => {
+  // TODO open sample webview
+  // TODO verify that expected content is displayed
+}
