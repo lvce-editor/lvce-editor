7794bca62b4fd76de75d192e90428dabc6e0964e
Le Vivilet <levivlet@pm.me>
2024-07-07 22:50:43 +0200

    add fn

--- packages/extension-host-worker-tests/src/sample.webview-provider.js
+++ packages/extension-host-worker-tests/src/sample.webview-provider.js
@@ -5,4 +5,5 @@ export const name = 'sample.webview-provider'
 export const test = async ({ Extension, QuickPick, Locator, expect }) => {
   // TODO open sample webview
   // TODO verify that expected content is displayed
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
 }
