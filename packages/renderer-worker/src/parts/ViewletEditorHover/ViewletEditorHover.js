874ad57e6ef02b81604a8812dd70b0f4485c59bb
Le Vivilet <levivlet@pm.me>
2024-07-07 10:05:26 +0200

    feature: add hover padding

--- packages/renderer-worker/src/parts/ViewletEditorHover/ViewletEditorHover.js
+++ packages/renderer-worker/src/parts/ViewletEditorHover/ViewletEditorHover.js
@@ -49,7 +49,12 @@ const fallbackDisplayStringLanguageId = 'typescript' // TODO remove this
 const hoverDocumentationFontSize = 15
 const hoverDocumentationFontFamily = 'Fira Code'
 const hoverDocumentationLineHeight = '1.33333'
-const hoverDocumentationWidth = 398 // 400px minus 1px border
+const hoverBorderLeft = 1
+const hoverBorderRight = 1
+const hoverPaddingLeft = 10
+const hoverPaddingRight = 10
+const hovverFullWidth = 400
+const hoverDocumentationWidth = hovverFullWidth - hoverPaddingLeft - hoverPaddingRight - hoverBorderLeft - hoverBorderRight
 
 const getHoverPositionXy = (editor, rowIndex, wordStart, documentationHeight) => {
   const x = EditorPosition.x(editor, rowIndex, wordStart)
