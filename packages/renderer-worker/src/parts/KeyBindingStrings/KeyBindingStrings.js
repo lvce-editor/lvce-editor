53edc747e2d7ee87cc6cf8b3f9c9339383b8e23d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-07 15:56:30 +0200

    feature: add more keybindingstable context menu items (#6323)

--- packages/renderer-worker/src/parts/KeyBindingStrings/KeyBindingStrings.js
+++ packages/renderer-worker/src/parts/KeyBindingStrings/KeyBindingStrings.js
@@ -12,7 +12,15 @@ const UiStrings = {
   TypeToSearchKeyBindings: 'Type to search in keybindings',
   PressDesiredKeyCombinationThenPressEnter: 'Press Desired Key Combination, Then Press Enter',
   ResultsWillUpdateAsYouType: 'Results will update as you type',
-  Copy: 'copy',
+  Copy: 'Copy',
+  CopyCommandId: 'Copy Command ID',
+  CopyCommandTitle: 'Copy Command Title',
+  ChangeKeyBinding: 'Change Keybinding...',
+  AddKeyBinding: 'Add Keybinding...',
+  RemoveKeyBinding: 'Remove Keybinding...',
+  ResetKeyBinding: 'Reset Keybinding',
+  ChangeWhenExpression: 'Change When Expression',
+  ShowSameKeyBindings: 'Show Same Keybindings',
 }
 
 export const keyBindings = () => {
@@ -45,4 +53,36 @@ export const resultsWillUpdateAsYouType = () => {
 
 export const copy = () => {
   return I18nString.i18nString(UiStrings.Copy)
+}
+
+export const copyCommandId = () => {
+  return I18nString.i18nString(UiStrings.CopyCommandId)
+}
+
+export const copyCommandTitle = () => {
+  return I18nString.i18nString(UiStrings.CopyCommandTitle)
+}
+
+export const changeKeyBinding = () => {
+  return I18nString.i18nString(UiStrings.ChangeKeyBinding)
+}
+
+export const addKeyBinding = () => {
+  return I18nString.i18nString(UiStrings.AddKeyBinding)
+}
+
+export const removeKeyBinding = () => {
+  return I18nString.i18nString(UiStrings.RemoveKeyBinding)
+}
+
+export const resetKeyBinding = () => {
+  return I18nString.i18nString(UiStrings.ResetKeyBinding)
+}
+
+export const changeWhenExpression = () => {
+  return I18nString.i18nString(UiStrings.ChangeWhenExpression)
+}
+
+export const showSameKeyBindings = () => {
+  return I18nString.i18nString(UiStrings.ShowSameKeyBindings)
 }
--- packages/renderer-worker/src/parts/MenuEntriesKeyBindingsTable/MenuEntriesKeyBindingsTable.js
+++ packages/renderer-worker/src/parts/MenuEntriesKeyBindingsTable/MenuEntriesKeyBindingsTable.js
@@ -12,5 +12,53 @@ export const getMenuEntries = () => {
       flags: MenuItemFlags.None,
       command: 'KeyBindings.copy',
     },
+    {
+      id: 'copyCommandId',
+      label: KeyBindingStrings.copyCommandId(),
+      flags: MenuItemFlags.None,
+      command: 'KeyBindings.copyCommandId',
+    },
+    {
+      id: 'copyCommandTitle',
+      label: KeyBindingStrings.copyCommandTitle(),
+      flags: MenuItemFlags.None,
+      command: 'KeyBindings.copyCommandTitle',
+    },
+    {
+      id: 'changeKeyBinding',
+      label: KeyBindingStrings.changeKeyBinding(),
+      flags: MenuItemFlags.None,
+      command: 'KeyBindings.changeKeyBinding',
+    },
+    {
+      id: 'addKeyBinding',
+      label: KeyBindingStrings.addKeyBinding(),
+      flags: MenuItemFlags.None,
+      command: 'KeyBindings.addKeyBinding',
+    },
+    {
+      id: 'removeKeyBinding',
+      label: KeyBindingStrings.removeKeyBinding(),
+      flags: MenuItemFlags.None,
+      command: 'KeyBindings.removeKeyBinding',
+    },
+    {
+      id: 'resetKeyBinding',
+      label: KeyBindingStrings.resetKeyBinding(),
+      flags: MenuItemFlags.None,
+      command: 'KeyBindings.resetKeyBinding',
+    },
+    {
+      id: 'changeWhenExpression',
+      label: KeyBindingStrings.changeWhenExpression(),
+      flags: MenuItemFlags.None,
+      command: 'KeyBindings.changeWhenExpression',
+    },
+    {
+      id: 'showSameKeyBindings',
+      label: KeyBindingStrings.showSameKeyBindings(),
+      flags: MenuItemFlags.None,
+      command: 'KeyBindings.showSameKeyBindings',
+    },
   ]
 }
