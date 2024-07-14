d21cb8917b08867854a939d4110bc0e0198e67f3
Le Vivilet <levivlet@pm.me>
2024-07-14 09:53:11 +0200

    remove unused

--- packages/renderer-worker/src/parts/EditorCommand/EditorCommandMoveRectangleSelectionPx.js
+++
@@ -1,7 +0,0 @@
-import * as EditorMoveRectangleSelection from './EditorCommandMoveRectangleSelection.js'
-import * as EditorPosition from './EditorCommandPosition.js'
-
-export const moveRectangleSelectionPx = (editor, x, y) => {
-  const position = EditorPosition.at(editor, x, y)
-  EditorMoveRectangleSelection.moveRectangleSelection(editor, position)
-}
