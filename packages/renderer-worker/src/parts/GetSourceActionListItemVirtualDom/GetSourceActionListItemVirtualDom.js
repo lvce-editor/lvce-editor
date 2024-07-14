a1b01df516bbbc16cba96e5668850d3dd3084da2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-14 09:41:53 +0200

    feature: add source actions click and focus events (#6336)

--- packages/renderer-worker/src/parts/GetSourceActionListItemVirtualDom/GetSourceActionListItemVirtualDom.js
+++ packages/renderer-worker/src/parts/GetSourceActionListItemVirtualDom/GetSourceActionListItemVirtualDom.js
@@ -1,18 +1,28 @@
 import * as ClassNames from '../ClassNames/ClassNames.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
 
+const getActionClassName = (isFocused) => {
+  if (isFocused) {
+    return MergeClassNames.mergeClassNames(ClassNames.SourceActionItem, ClassNames.SourceActionItemFocused)
+  }
+  return ClassNames.SourceActionItem
+}
+
 export const getSourceActionListItemVirtualDom = (sourceAction) => {
+  const { name, isFocused } = sourceAction
+  const actionClassName = getActionClassName(isFocused)
   return [
     {
       type: VirtualDomElements.Div,
-      className: ClassNames.SourceActionItem,
+      className: actionClassName,
       childCount: 2,
     },
     {
       type: VirtualDomElements.Div,
       className: 'SourceActionIcon MaskIcon MaskIconSymbolFile',
     },
-    text(sourceAction.name),
+    text(name),
   ]
 }
