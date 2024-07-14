2ee6d6ef84186362d5dad47bed11c265bf251a71
Le Vivilet <levivlet@pm.me>
2024-07-14 09:19:21 +0200

    add keybindings

--- packages/renderer-worker/src/parts/ViewletEditorSourceActions/ViewletEditorSourceActionsKeyBindings.js
+++ packages/renderer-worker/src/parts/ViewletEditorSourceActions/ViewletEditorSourceActionsKeyBindings.js
@@ -8,5 +8,25 @@ export const getKeyBindings = () => {
       command: 'SourceActions.close',
       when: WhenExpression.FocusSourceActions,
     },
+    {
+      key: KeyCode.DownArrow,
+      command: 'SourceActions.focusNext',
+      when: WhenExpression.FocusSourceActions,
+    },
+    {
+      key: KeyCode.UpArrow,
+      command: 'SourceActions.focusPrevious',
+      when: WhenExpression.FocusSourceActions,
+    },
+    {
+      key: KeyCode.Home,
+      command: 'SourceActions.focusFirst',
+      when: WhenExpression.FocusSourceActions,
+    },
+    {
+      key: KeyCode.End,
+      command: 'SourceActions.focusLast',
+      when: WhenExpression.FocusSourceActions,
+    },
   ]
 }
