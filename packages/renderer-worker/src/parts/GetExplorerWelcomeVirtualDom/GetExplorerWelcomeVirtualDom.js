c08880b6c674b2ca0f0c9821a04373ced87ca3d6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-18 22:01:51 +0200

    feature: add explorer openFolder command (#6358)

--- packages/renderer-worker/src/parts/GetExplorerWelcomeVirtualDom/GetExplorerWelcomeVirtualDom.js
+++ packages/renderer-worker/src/parts/GetExplorerWelcomeVirtualDom/GetExplorerWelcomeVirtualDom.js
@@ -1,4 +1,5 @@
 import * as ClassNames from '../ClassNames/ClassNames.js'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.js'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.js'
 import * as ExplorerStrings from '../ViewletExplorer/ViewletExplorerStrings.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
@@ -27,6 +28,7 @@ export const getExplorerWelcomeVirtualDom = () => {
       type: VirtualDomElements.Button,
       className: MergeClassNames.mergeClassNames(ClassNames.Button, ClassNames.ButtonPrimary),
       childCount: 1,
+      onClick: DomEventListenerFunctions.handleClickOpenFolder,
     },
     text(ExplorerStrings.openFolder()),
   ]
