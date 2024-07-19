40822852de4ee2f2d0340a1fce33e87515f9d5d4
Le Vivilet <levivlet@pm.me>
2024-07-19 23:39:50 +0200

    unused imports

--- packages/renderer-worker/test/ExtensionHostReference.test.js
+++ packages/renderer-worker/test/ExtensionHostReference.test.js
@@ -1,4 +1,4 @@
-import { jest, beforeAll, afterAll, test, expect, beforeEach, afterEach } from '@jest/globals'
+import { beforeEach, expect, jest, test } from '@jest/globals'
 import * as ExtensionHostActivationEvent from '../src/parts/ExtensionHostActivationEvent/ExtensionHostActivationEvent.js'
 import * as ExtensionHostCommandType from '../src/parts/ExtensionHostCommandType/ExtensionHostCommandType.js'
 
