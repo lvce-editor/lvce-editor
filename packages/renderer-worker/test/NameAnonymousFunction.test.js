316ecf8183ef442a76986e3ab0db2b68744b1338
Le Vivilet <levivlet@pm.me>
2024-07-19 23:33:39 +0200

    unused imports

--- packages/renderer-worker/test/NameAnonymousFunction.test.js
+++ packages/renderer-worker/test/NameAnonymousFunction.test.js
@@ -1,4 +1,4 @@
-import { beforeAll, afterAll, test, expect, beforeEach, afterEach } from '@jest/globals'
+import { expect, test } from '@jest/globals'
 import * as NameAnonymousFunction from '../src/parts/NameAnonymousFunction/NameAnonymousFunction.js'
 
 test('nameAnonymousFunction', () => {
--- packages/renderer-worker/test/OffscreenCanvas.test.js
+++ packages/renderer-worker/test/OffscreenCanvas.test.js
@@ -1,4 +1,4 @@
-import { jest, beforeAll, afterAll, test, expect, beforeEach, afterEach } from '@jest/globals'
+import { beforeEach, expect, jest, test } from '@jest/globals'
 import * as Callback from '../src/parts/Callback/Callback.js'
 
 beforeEach(() => {
