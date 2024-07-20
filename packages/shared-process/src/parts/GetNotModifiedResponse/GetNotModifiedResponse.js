93384f987b6d100aecebe04fe7cfa8a05fac6bac
Le Vivilet <levivlet@pm.me>
2024-07-20 12:34:17 +0200

    update

--- packages/shared-process/src/parts/GetNotModifiedResponse/GetNotModifiedResponse.js
+++ packages/shared-process/src/parts/GetNotModifiedResponse/GetNotModifiedResponse.js
@@ -1,11 +1,11 @@
 import * as HttpStatusCode from '../HttpStatusCode/HttpStatusCode.js'
 
-export const getNotModifiedResponse = async () => {
+export const getNotModifiedResponse = async (headers = {}) => {
   return {
     body: '',
     init: {
       status: HttpStatusCode.NotModifed,
-      headers: {},
+      headers,
     },
   }
 }
