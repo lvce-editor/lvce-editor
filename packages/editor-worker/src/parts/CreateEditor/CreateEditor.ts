3bcc13f1a52c62e41591c1b0276ab6e7df997ef9
Le Vivilet <levivlet@pm.me>
2024-07-20 14:32:42 +0200

    remove log

--- packages/editor-worker/src/parts/CreateEditor/CreateEditor.ts
+++ packages/editor-worker/src/parts/CreateEditor/CreateEditor.ts
@@ -1,7 +1,6 @@
 import * as EditorState from '../Editors/Editors.ts'
 
 export const createEditor = (id: number, content: string) => {
-  console.log({ content })
   const editor = {
     content,
   }
