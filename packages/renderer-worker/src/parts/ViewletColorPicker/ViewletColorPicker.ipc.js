bba0f0d9418a522520d3596a73181e56953c1392
Le Vivilet <levivlet@pm.me>
2024-07-20 18:25:13 +0200

    feature: use typescript for color picker

--- packages/renderer-worker/src/parts/ViewletColorPicker/ViewletColorPicker.ipc.js
+++
@@ -1,5 +0,0 @@
-export * from './ViewletColorPicker.js'
-export * from './ViewletColorPickerCommands.js'
-export * from './ViewletColorPickerCss.js'
-export * from './ViewletColorPickerName.js'
-export * from './ViewletColorPickerRender.js'
