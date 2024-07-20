c0aa0c8e3e78a87929466dd0c92a1e49a60833d7
Le Vivilet <levivlet@pm.me>
2024-07-20 14:31:51 +0200

    tsc

--- packages/editor-worker/src/parts/LoadTokenizers/LoadTokenizers.ts
+++ packages/editor-worker/src/parts/LoadTokenizers/LoadTokenizers.ts
@@ -2,6 +2,7 @@ import * as Tokenizer from '../Tokenizer/Tokenizer.ts'
 
 export const loadTokenizers = async (languageIds: any) => {
   for (const languageId of languageIds) {
+    // @ts-ignore
     await Tokenizer.loadTokenizer(languageId)
   }
 }
