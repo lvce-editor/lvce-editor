73cc702183771b6f6257daa29891d99911ecd1d6
Le Vivilet <levivlet@pm.me>
2024-07-20 14:21:19 +0200

    tsc

---
+++ packages/editor-worker/src/parts/GetEditorRowsVirtualDom/GetEditorRowsVirtualDom.ts
@@ -0,0 +1,31 @@
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+import * as ClassNames from '../ClassNames/ClassNames.js'
+import * as Px from '../Px/Px.js'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
+
+export const getEditorRowsVirtualDom = (textInfos: any, differences: any, lineNumbers = true) => {
+  const dom = []
+  for (let i = 0; i < textInfos.length; i++) {
+    const textInfo = textInfos[i]
+    const difference = differences[i]
+    dom.push({
+      type: VirtualDomElements.Div,
+      className: ClassNames.EditorRow,
+      translate: Px.px(difference),
+      childCount: textInfo.length / 2,
+    })
+    for (let j = 0; j < textInfo.length; j += 2) {
+      const tokenText = textInfo[j]
+      const className = textInfo[j + 1]
+      dom.push(
+        {
+          type: VirtualDomElements.Span,
+          className,
+          childCount: 1,
+        },
+        text(tokenText),
+      )
+    }
+  }
+  return dom
+}
