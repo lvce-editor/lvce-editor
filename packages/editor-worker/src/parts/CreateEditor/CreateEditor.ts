308b37a71df6891bd7a30dcbe26d6525c1d6f370
Le Vivilet <levivlet@pm.me>
2024-07-20 14:39:24 +0200

    use lines

--- packages/editor-worker/src/parts/CreateEditor/CreateEditor.ts
+++ packages/editor-worker/src/parts/CreateEditor/CreateEditor.ts
@@ -1,8 +1,13 @@
 import * as EditorState from '../Editors/Editors.ts'
+import * as Assert from '../Assert/Assert.ts'
+import * as SplitLines from '../SplitLines/SplitLines.ts'
 
 export const createEditor = (id: number, content: string) => {
+  Assert.number(id)
+  Assert.string(content)
+  const lines = SplitLines.splitLines(content)
   const editor = {
-    content,
+    lines,
   }
   EditorState.set(id, editor)
 }
