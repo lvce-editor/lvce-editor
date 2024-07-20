85c3cd17120486c22290c10755bc56ce7e5ca0b5
Le Vivilet <levivlet@pm.me>
2024-07-20 18:53:15 +0200

    feature: remove unused editor files

--- packages/editor-worker/src/parts/GetFontUrl/GetFontUrl.ts
+++
@@ -1,7 +0,0 @@
-import * as AssetDir from '../AssetDir/AssetDir.ts'
-
-// @ts-ignore
-export const getFontUrl = (relativePath) => {
-  const fontUrl = `url('${AssetDir.assetDir}${relativePath}')`
-  return fontUrl
-}
