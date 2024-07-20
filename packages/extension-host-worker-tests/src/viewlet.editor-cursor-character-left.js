9dc867a5c93ae4ed95b4e93cfe0871c47a2fc361
Le Vivilet <levivlet@pm.me>
2024-07-20 12:14:37 +0200

    skip

--- packages/extension-host-worker-tests/src/viewlet.editor-cursor-character-left.js
+++ packages/extension-host-worker-tests/src/viewlet.editor-cursor-character-left.js
@@ -1,5 +1,7 @@
 export const name = 'viewlet.editor-cursor-character-left'
 
+export const skip = true
+
 export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
