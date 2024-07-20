08e1d2eef8fb83a3a3e7dce92819b8fdd22d7ff4
Le Vivilet <levivlet@pm.me>
2024-07-20 19:08:56 +0200

    feature: move color picker functions to editor worker

--- packages/renderer-worker/src/parts/ViewletColorPicker/ViewletColorPicker.ts
+++ packages/renderer-worker/src/parts/ViewletColorPicker/ViewletColorPicker.ts
@@ -1,4 +1,4 @@
-import * as Clamp from '../Clamp/Clamp.js'
+import * as EditorWorker from '../EditorWorker/EditorWorker.js'
 
 export const create = () => {
   return {
@@ -9,43 +9,14 @@ export const create = () => {
   }
 }
 
-const getNewColor = (x, max) => {
-  const percent = x / max
-  const hue = percent * 360
-  const newColor = `hsl(${hue}, 100%, 50%)`
-  return newColor
-}
-
 export const loadContent = (state) => {
-  const max = 300
-  const x = 20
-  const color = getNewColor(x, max)
-  return {
-    ...state,
-    offsetX: x,
-    color,
-    max,
-  }
+  return EditorWorker.invoke('ColorPicker.loadContent', state)
 }
 
 export const handleSliderPointerDown = (state, x, y) => {
-  const { min, max } = state
-  const newX = Clamp.clamp(x, min, max)
-  const newColor = getNewColor(newX, max)
-  return {
-    ...state,
-    color: newColor,
-    offsetX: newX,
-  }
+  return EditorWorker.invoke('ColorPicker.handleSliderPointerDown', state, x, y)
 }
 
 export const handleSliderPointerMove = (state, x, y) => {
-  const { min, max } = state
-  const newX = Clamp.clamp(x, min, max)
-  const newColor = getNewColor(newX, max)
-  return {
-    ...state,
-    color: newColor,
-    offsetX: newX,
-  }
+  return EditorWorker.invoke('ColorPicker.handleSliderPointerMove', state, x, y)
 }
