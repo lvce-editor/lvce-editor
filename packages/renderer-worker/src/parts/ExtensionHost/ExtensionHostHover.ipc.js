0784c3434de50be8a0c45819deb520a6187d8c9c
Le Vivilet <levivlet@pm.me>
2024-07-20 22:12:21 +0200

    feature: add more hover commands to editor worker

---
+++ packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostHover.ipc.js
@@ -0,0 +1,7 @@
+import * as ExtensionHostHover from './ExtensionHostHover.js'
+
+export const name = 'ExtensionHostHover'
+
+export const Commands = {
+  executeHoverProvider: ExtensionHostHover.executeHoverProvider,
+}
