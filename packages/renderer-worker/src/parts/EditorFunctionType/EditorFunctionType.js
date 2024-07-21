c20132c290529ff8a88d3a2567c7a7c508e4c8b3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-21 10:41:50 +0200

    feature: remove unused renderer worker files (#6413)

--- packages/renderer-worker/src/parts/EditorFunctionType/EditorFunctionType.js
+++
@@ -1,4 +0,0 @@
-export const HandleEditorDeleteLeft = 'handleEditorDeleteLeft'
-export const HandleEditorType = 'handleEditorType'
-export const HandleEditorBlur = 'handleEditorBlur'
-export const HandleEditorClick = 'handleEditorClick'
