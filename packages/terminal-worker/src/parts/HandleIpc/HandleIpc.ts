15d38acf7770864c2de66f35301e959ba30917a0
Le Vivilet <levivlet@pm.me>
2024-07-21 13:07:50 +0200

    fix debug

--- packages/terminal-worker/src/parts/HandleIpc/HandleIpc.ts
+++ packages/terminal-worker/src/parts/HandleIpc/HandleIpc.ts
@@ -1,5 +1,9 @@
 import * as HandleMessage from '../HandleMessage/HandleMessage.ts'
 
 export const handleIpc = (ipc) => {
-  ipc.onmessage = HandleMessage.handleMessage
+  if ('addEventListener' in ipc) {
+    ipc.addEventListener('message', HandleMessage.handleMessage)
+  } else {
+    ipc.onmessage = HandleMessage.handleMessage
+  }
 }
