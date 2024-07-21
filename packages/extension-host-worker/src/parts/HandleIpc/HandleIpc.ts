3dfb3eb6c8f7a2a0a12f8ff755a042bfe7e90208
Le Vivilet <levivlet@pm.me>
2024-07-21 13:15:48 +0200

    fix rror

--- packages/extension-host-worker/src/parts/HandleIpc/HandleIpc.ts
+++ packages/extension-host-worker/src/parts/HandleIpc/HandleIpc.ts
@@ -1,6 +1,7 @@
 import * as HandleMessage from '../HandleMessage/HandleMessage.ts'
 
-export const handleIpc = (ipc) => {
+export const handleIpc = (ipc: any) => {
+  console.log({ ipc })
   if ('addEventListener' in ipc) {
     ipc.addEventListener('message', HandleMessage.handleMessage)
   } else {
