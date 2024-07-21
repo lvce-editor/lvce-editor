e3beea1afca4919b74fb50ee6743067ea8e7674e
Le Vivilet <levivlet@pm.me>
2024-07-21 08:44:29 +0200

    feature: remove unused renderer worker files

--- packages/renderer-worker/src/parts/MeasureCharacterWidth/MeasureCharacterWidth.js
+++
@@ -1,5 +0,0 @@
-import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.js'
-
-export const measureCharacterWidth = (fontWeight, fontSize, fontFamily, letterSpacing) => {
-  return MeasureTextWidth.measureTextWidth('a', fontWeight, fontSize, fontFamily, letterSpacing, false, 0)
-}
