70003082ce9604c0785f1ff5d211ea333017fc7f
Le Vivilet <levivlet@pm.me>
2024-07-21 17:39:13 +0200

    remove log

--- packages/extension-host-worker/src/parts/HandleIpc/HandleIpc.ts
+++ packages/extension-host-worker/src/parts/HandleIpc/HandleIpc.ts
@@ -1,7 +1,6 @@
 import * as HandleMessage from '../HandleMessage/HandleMessage.ts'
 
 export const handleIpc = (ipc: any) => {
-  console.log({ ipc })
   if ('addEventListener' in ipc) {
     ipc.addEventListener('message', HandleMessage.handleMessage)
   } else {
