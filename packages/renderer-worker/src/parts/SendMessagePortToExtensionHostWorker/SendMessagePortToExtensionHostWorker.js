709ebd323c83f1ddbf8b221a6538a23b6a20796c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-21 13:07:43 +0200

    feature: connect editor worker to extension host worker (#6418)

--- packages/renderer-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.js
+++ packages/renderer-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.js
@@ -1,10 +1,8 @@
 import * as Assert from '../Assert/Assert.ts'
-// import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.js'
+import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.js'
 
 export const sendMessagePortToExtensionHostWorker = async (port, initialCommand) => {
   Assert.object(port)
   Assert.string(initialCommand)
-  console.log({ port })
-  // TODO
-  // await ExtensionHostWorker.invokeAndTransfer(initialCommand, [port], port)
+  await ExtensionHostWorker.invokeAndTransfer([port], initialCommand, port)
 }
