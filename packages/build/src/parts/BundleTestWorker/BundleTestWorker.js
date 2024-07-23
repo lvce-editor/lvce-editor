7d2ec05b9ebf13ca6fc37c54465796c7a6d80e4f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-23 23:26:27 +0200

    Discard changes to packages/build/src/parts/BundleTestWorker/BundleTestWorker.js

--- packages/build/src/parts/BundleTestWorker/BundleTestWorker.js
+++ packages/build/src/parts/BundleTestWorker/BundleTestWorker.js
@@ -1,9 +1,28 @@
+import * as BundleJs from '../BundleJsRollup/BundleJsRollup.js'
 import * as Copy from '../Copy/Copy.js'
 import * as Path from '../Path/Path.js'
+import * as Replace from '../Replace/Replace.js'
 
-export const bundleTestWorker = async ({ cachePath }) => {
-  await Copy.copyFile({
-    from: 'packages/renderer-worker/node_modules/@lvce-editor/test-worker/dist/index.js',
-    to: Path.join(cachePath, 'dist', 'testWorkerMain.js'),
+export const bundleTestWorker = async ({ cachePath, commitHash, platform, assetDir }) => {
+  await Copy.copy({
+    from: 'static/js',
+    to: Path.join(cachePath, 'static', 'js'),
+  })
+  await Copy.copy({
+    from: 'packages/test-worker/src',
+    to: Path.join(cachePath, 'src'),
+  })
+  for (const file of ['JsonRpc', 'IpcChildModule', 'VError']) {
+    await Replace.replace({
+      path: `${cachePath}/src/parts/${file}/${file}.ts`,
+      occurrence: `/static/`,
+      replacement: `../../../static/`,
+    })
+  }
+  await BundleJs.bundleJs({
+    cwd: cachePath,
+    from: `./src/testWorkerMain.ts`,
+    platform: 'webworker',
+    allowCyclicDependencies: false,
   })
 }
