512957132bfadc16f21b96a1ba2e1b8f23e97dd5
Le Vivilet <levivlet@pm.me>
2024-07-24 00:15:50 +0200

    render test names

--- packages/renderer-worker/src/parts/ViewletE2eTests/ViewletE2eTests.ts
+++ packages/renderer-worker/src/parts/ViewletE2eTests/ViewletE2eTests.ts
@@ -1,4 +1,6 @@
 import type { E2eState } from './ViewletE2eTestsTypes.ts'
+import * as SharedProcess from '../SharedProcess/SharedProcess.js'
+import * as FileSystem from '../FileSystem/FileSystem.js'
 
 export const create = (): E2eState => {
   return {
@@ -6,8 +8,19 @@ export const create = (): E2eState => {
   }
 }
 
+const getTests = async () => {
+  const root = await SharedProcess.invoke('Platform.getRoot')
+  const testPath = await SharedProcess.invoke('Platform.getTestPath')
+  const absolutePath = `${root}/${testPath}/src`
+  const dirents = await FileSystem.readDirWithFileTypes(absolutePath)
+  const filteredDirents = dirents.slice(1)
+  console.log({ filteredDirents })
+  const tests = filteredDirents.map((dirent) => dirent.name)
+  return tests
+}
+
 export const loadContent = async (state: E2eState): Promise<E2eState> => {
-  const tests = ['1', '2', '3']
+  const tests = await getTests()
   return {
     ...state,
     tests,
