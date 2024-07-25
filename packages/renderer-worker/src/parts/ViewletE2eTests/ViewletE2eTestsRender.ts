8654ee658fb7737d84514b09c1b1568309a1e9c0
Le Vivilet <levivlet@pm.me>
2024-07-25 22:29:21 +0200

    feature: allow clicking e2e test item and marking it as active

--- packages/renderer-worker/src/parts/ViewletE2eTests/ViewletE2eTestsRender.ts
+++ packages/renderer-worker/src/parts/ViewletE2eTests/ViewletE2eTestsRender.ts
@@ -1,4 +1,5 @@
 import * as GetE2eTestsVirtualDom from '../GetE2eTestsVirtualDom/GetE2eTestsVirtualDom.ts'
+import * as GetVisibleE2eTests from '../GetVisibleE2eTests/GetVisibleE2eTests.ts'
 import type { E2eState } from './ViewletE2eTestsTypes.ts'
 
 export const hasFunctionalRender = true
@@ -7,10 +8,11 @@ export const hasFunctionalRootRender = true
 
 const renderTests = {
   isEqual(oldState: E2eState, newState: E2eState) {
-    return oldState.tests === newState.tests
+    return oldState.tests === newState.tests && oldState.index === newState.index
   },
   apply(oldState: E2eState, newState: E2eState) {
-    const dom = GetE2eTestsVirtualDom.getE2eTestsVirtualDom(newState.tests)
+    const visible = GetVisibleE2eTests.getVisibleE2eTests(newState.tests, newState.index)
+    const dom = GetE2eTestsVirtualDom.getE2eTestsVirtualDom(visible)
     return ['Viewlet.setDom2', dom]
   },
 }
