65721fbde6239308c190765cfc874c90a124d39f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-26 22:46:17 +0200

    feature: use types for color picker state (#6455)

--- packages/renderer-worker/src/parts/ViewletColorPicker/ViewletColorPickerRender.ts
+++ packages/renderer-worker/src/parts/ViewletColorPicker/ViewletColorPickerRender.ts
@@ -1,33 +1,34 @@
 import * as GetColorPickerVirtualDom from '../GetColorPickerVirtualDom/GetColorPickerVirtualDom.js'
 import * as RenderMethod from '../RenderMethod/RenderMethod.js'
+import type { ColorPickerState } from './ViewletColorPickerTypes.ts'
 
 export const hasFunctionalRender = true
 
 export const hasFunctionalRootRender = true
 
 const renderColor = {
-  isEqual(oldState, newState) {
+  isEqual(oldState: ColorPickerState, newState: ColorPickerState) {
     return oldState.color === newState.color
   },
-  apply(oldState, newState) {
+  apply(oldState: ColorPickerState, newState: ColorPickerState) {
     return [/* method */ RenderMethod.SetColor, /* color */ newState.color]
   },
 }
 
 const renderOffsetX = {
-  isEqual(oldState, newState) {
+  isEqual(oldState: ColorPickerState, newState: ColorPickerState) {
     return oldState.offsetX === newState.offsetX
   },
-  apply(oldState, newState) {
+  apply(oldState: ColorPickerState, newState: ColorPickerState) {
     return [/* method */ RenderMethod.SetOffsetX, /* offsetX */ newState.offsetX]
   },
 }
 
 const renderColorPicker = {
-  isEqual(oldState, newState) {
+  isEqual(oldState: ColorPickerState, newState: ColorPickerState) {
     return oldState.min === newState.min && oldState.max === newState.max
   },
-  apply(oldState, newState) {
+  apply(oldState: ColorPickerState, newState: ColorPickerState) {
     const dom = GetColorPickerVirtualDom.getColorPickerVirtualDom()
     return ['Viewlet.setDom2', dom]
   },
---
+++ packages/renderer-worker/src/parts/ViewletColorPicker/ViewletColorPickerTypes.ts
@@ -0,0 +1,6 @@
+export interface ColorPickerState {
+  readonly color: string
+  readonly offsetX: number
+  readonly min: number
+  readonly max: number
+}
