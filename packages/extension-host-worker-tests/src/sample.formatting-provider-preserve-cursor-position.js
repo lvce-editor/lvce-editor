5a44b447aca662bd613ad4c21fd3c957fe83488c
Le Vivilet <levivlet@pm.me>
2024-07-26 19:05:04 +0200

    skip

--- packages/extension-host-worker-tests/src/sample.formatting-provider-preserve-cursor-position.js
+++ packages/extension-host-worker-tests/src/sample.formatting-provider-preserve-cursor-position.js
@@ -1,5 +1,7 @@
 export const name = 'sample.formatting-provider-preserve-cursor-position'
 
+export const skip = true
+
 export const test = async ({ FileSystem, Workspace, Extension, Main, Editor, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
