8f2954a0af8fc31db5aa119661ec1adc1efdf066
Le Vivilet <levivlet@pm.me>
2024-07-27 20:00:51 +0200

    improve layout

--- packages/renderer-worker/src/parts/ViewletE2eTest/ViewletE2eTest.ts
+++ packages/renderer-worker/src/parts/ViewletE2eTest/ViewletE2eTest.ts
@@ -26,8 +26,9 @@ export const create = (id, uri, x, y, width, height): E2eTestState => {
 const getPreviewTransform = (width: number, height: number) => {
   const wantedWidth = 1000
   const wantedHeight = 600
-  const widthRatio = width / wantedWidth
-  const availableHeight = height / 2
+  const availableWidth = width / 2
+  const availableHeight = height
+  const widthRatio = availableWidth / wantedWidth
   const heightRatio = availableHeight / wantedHeight
   const min = Math.min(widthRatio, heightRatio)
   const scale = min
