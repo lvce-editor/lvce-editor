3782561197b8a799192c530a8b72e0363fa10495
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-27 10:55:29 +0200

    feature: use typescript for terminal view (#6467)

---
+++ packages/renderer-worker/src/parts/ViewletTerminal/ViewletTerminal.ipc.ts
@@ -0,0 +1,7 @@
+export const name = 'Terminal'
+
+export * from './ViewletTerminal.ts'
+export * from './ViewletTerminalActions.ts'
+export * from './ViewletTerminalCommands.ts'
+export * from './ViewletTerminalRender.ts'
+export * from './ViewletTermincalCss.ts'
