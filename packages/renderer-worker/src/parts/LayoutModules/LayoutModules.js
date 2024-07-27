31f353e0308cccfa02e34a8700c6a929f537476e
Le Vivilet <levivlet@pm.me>
2024-07-27 21:27:06 +0200

    feature: move layout keys into separate file, making it easier to add additional keys

---
+++ packages/renderer-worker/src/parts/LayoutModules/LayoutModules.js
@@ -0,0 +1,56 @@
+import * as LayoutKeys from '../LayoutKeys/LayoutKeys.js'
+import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
+
+export const Main = {
+  moduleId: ViewletModuleId.Main,
+  kVisible: LayoutKeys.MainVisible,
+  kTop: LayoutKeys.MainTop,
+  kLeft: LayoutKeys.MainLeft,
+  kWidth: LayoutKeys.MainWidth,
+  kHeight: LayoutKeys.MainHeight,
+}
+
+export const ActivityBar = {
+  moduleId: ViewletModuleId.ActivityBar,
+  kVisible: LayoutKeys.ActivityBarVisible,
+  kTop: LayoutKeys.ActivityBarTop,
+  kLeft: LayoutKeys.ActivityBarLeft,
+  kWidth: LayoutKeys.ActivityBarWidth,
+  kHeight: LayoutKeys.ActivityBarHeight,
+}
+
+export const SideBar = {
+  moduleId: ViewletModuleId.SideBar,
+  kVisible: LayoutKeys.SideBarVisible,
+  kTop: LayoutKeys.SideBarTop,
+  kLeft: LayoutKeys.SideBarLeft,
+  kWidth: LayoutKeys.SideBarWidth,
+  kHeight: LayoutKeys.SideBarHeight,
+}
+
+export const TitleBar = {
+  moduleId: ViewletModuleId.TitleBar,
+  kVisible: LayoutKeys.TitleBarVisible,
+  kTop: LayoutKeys.TitleBarTop,
+  kLeft: LayoutKeys.TitleBarTop,
+  kWidth: LayoutKeys.TitleBarWidth,
+  kHeight: LayoutKeys.TitleBarHeight,
+}
+
+export const StatusBar = {
+  moduleId: ViewletModuleId.StatusBar,
+  kVisible: LayoutKeys.StatusBarVisible,
+  kTop: LayoutKeys.StatusBarTop,
+  kLeft: LayoutKeys.StatusBarLeft,
+  kWidth: LayoutKeys.StatusBarWidth,
+  kHeight: LayoutKeys.StatusBarHeight,
+}
+
+export const Panel = {
+  moduleId: ViewletModuleId.Panel,
+  kVisible: LayoutKeys.PanelVisible,
+  kTop: LayoutKeys.panelTop,
+  kLeft: LayoutKeys.PanelLeft,
+  kWidth: LayoutKeys.PanelWidth,
+  kHeight: LayoutKeys.PanelHeight,
+}
