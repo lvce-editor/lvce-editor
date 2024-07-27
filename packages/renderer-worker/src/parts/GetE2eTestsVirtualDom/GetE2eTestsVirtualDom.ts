4e84fc1657fc0410a635d1c074d826ae64211f8d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-27 14:49:27 +0200

    feature: add e2e test icons (#6469)

--- packages/renderer-worker/src/parts/GetE2eTestsVirtualDom/GetE2eTestsVirtualDom.ts
+++ packages/renderer-worker/src/parts/GetE2eTestsVirtualDom/GetE2eTestsVirtualDom.ts
@@ -1,36 +1,61 @@
+import * as ClassNames from '../ClassNames/ClassNames.js'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.js'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
 
-const getTreeItemClassName = (isActive) => {
+const getTreeItemClassName = (isActive: boolean) => {
   if (isActive) {
-    return 'TreeItem TestActive'
+    return MergeClassNames.mergeClassNames(ClassNames.TreeItem, ClassNames.TestActive)
   }
-  return 'TreeItem'
+  return ClassNames.TreeItem
 }
 
-const getTestDom = (test) => {
-  return [
+const getIconClass = (isActive) => {
+  if (isActive) {
+    return ClassNames.MaskIconDebugPause
+  }
+  return ClassNames.MaskIconPlay
+}
+
+const getTestDom = (test: any) => {
+  const { name, isActive } = test
+  const dom: any[] = []
+  const iconClass = getIconClass(isActive)
+  dom.push(
     {
       type: VirtualDomElements.Div,
+      childCount: 2,
+      className: getTreeItemClassName(isActive),
+    },
+    {
+      type: VirtualDomElements.Button,
       childCount: 1,
-      className: getTreeItemClassName(test.isActive),
+      className: 'InlineButton',
+      ariaLabel: 'Pause',
+    },
+    {
+      type: VirtualDomElements.I,
+      className: MergeClassNames.mergeClassNames(ClassNames.MaskIcon, iconClass),
+      childCount: 0,
     },
     {
       type: VirtualDomElements.Span,
-      className: 'Label',
+      className: ClassNames.Label,
       childCount: 1,
     },
-    text(test.name),
-  ]
+    text(name),
+  )
+  return dom
 }
 
-export const getE2eTestsVirtualDom = (tests) => {
+export const getE2eTestsVirtualDom = (tests: any[]) => {
   return [
     {
       type: VirtualDomElements.Div,
       className: 'Viewlet E2eTests Tree',
       childCount: tests.length,
-      onClick: 'handleClickAt',
+      onClick: DomEventListenerFunctions.HandleClickAt,
     },
     ...tests.flatMap(getTestDom),
   ]
