0cc088f9369a9a385820e8b5f8895a6646ad1cf4
Le Vivilet <levivlet@pm.me>
2024-07-27 15:20:26 +0200

    feature: wait for iframe to be loaded before sending message port

--- packages/renderer-worker/src/parts/ViewletE2eTests/ViewletE2eTests.ts
+++ packages/renderer-worker/src/parts/ViewletE2eTests/ViewletE2eTests.ts
@@ -46,6 +46,15 @@ export const executeTest = async (state: E2eState, index: number): Promise<E2eSt
   const { tests } = state
   const test = tests[index]
   const htmlFileName = test.replace('.js', '.html')
+  const iframeSrc = `http://localhost:3001/tests/${htmlFileName}`
+  return {
+    ...state,
+    index,
+    iframeSrc,
+  }
+}
+
+export const handleLoad = async (state: E2eState): Promise<E2eState> => {
   const messagePortId = Id.create()
   const { port1, port2 } = new MessageChannel()
   await Transferrable.transferToRendererProcess(messagePortId, port1)
@@ -57,10 +66,8 @@ export const executeTest = async (state: E2eState, index: number): Promise<E2eSt
   const iframeOrigin = 'http://localhost:3001'
   return {
     ...state,
-    index,
-    iframeSrc,
-    iframeOrigin,
     portId: messagePortId,
+    iframeOrigin,
   }
 }
 
--- packages/renderer-worker/src/parts/ViewletE2eTests/ViewletE2eTestsCommands.ts
+++ packages/renderer-worker/src/parts/ViewletE2eTests/ViewletE2eTestsCommands.ts
@@ -2,5 +2,6 @@ import * as ViewletE2eTests from './ViewletE2eTests.ts'
 
 export const Commands = {
   handleClickAt: ViewletE2eTests.handleClickAt,
+  handleLoad: ViewletE2eTests.handleLoad,
   runAll: ViewletE2eTests.runAll,
 }
