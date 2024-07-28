0fe5ef3a00c96f72e48ddcf5588ff4b3627ef720
Le Vivilet <levivlet@pm.me>
2024-07-28 17:11:04 +0200

    feature: use npm package for syntax highlighting worker

--- packages/renderer-worker/src/parts/SyntaxHighlightingWorkerUrl/SyntaxHighlightingWorkerUrl.js
+++ packages/renderer-worker/src/parts/SyntaxHighlightingWorkerUrl/SyntaxHighlightingWorkerUrl.js
@@ -1,3 +1,3 @@
 import * as AssetDir from '../AssetDir/AssetDir.js'
 
-export const syntaxHighlightingWorkerUrl = `${AssetDir.assetDir}/packages/syntax-highlighting-worker/src/syntaxHighlightingWorkerMain.ts`
+export const syntaxHighlightingWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/syntax-highlighting-worker/dist/syntaxHighlightingWorkerMain.js`
