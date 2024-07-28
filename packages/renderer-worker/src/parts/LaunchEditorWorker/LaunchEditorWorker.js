f56658bed9643a9de24ad21f2a90cfec64538d2e
Le Vivilet <levivlet@pm.me>
2024-07-28 20:01:58 +0200

    feature: use syntax highlighting worker by default

--- packages/renderer-worker/src/parts/LaunchEditorWorker/LaunchEditorWorker.js
+++ packages/renderer-worker/src/parts/LaunchEditorWorker/LaunchEditorWorker.js
@@ -2,22 +2,31 @@ import * as EditorWorkerUrl from '../EditorWorkerUrl/EditorWorkerUrl.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as IsProduction from '../IsProduction/IsProduction.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 import * as Preferences from '../Preferences/Preferences.js'
 
-export const launchEditorWorker = async () => {
+const getConfiguredWorkerUrl = () => {
   let configuredWorkerUrl = Preferences.get('developer.editorWorkerPath') || ''
   if (configuredWorkerUrl) {
     configuredWorkerUrl = '/remote' + configuredWorkerUrl
   }
   configuredWorkerUrl = configuredWorkerUrl || EditorWorkerUrl.editorWorkerUrl
+  if (IsProduction.isProduction) {
+    configuredWorkerUrl = EditorWorkerUrl.editorWorkerUrl
+  }
+  return configuredWorkerUrl
+}
+
+export const launchEditorWorker = async () => {
+  const configuredWorkerUrl = getConfiguredWorkerUrl()
   const ipc = await IpcParent.create({
     method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
     url: configuredWorkerUrl,
     name: 'Editor Worker',
   })
   HandleIpc.handleIpc(ipc)
-  const syntaxHighlightingWorker = Preferences.get('developer.syntaxHighlightingWorker') || false
+  const syntaxHighlightingWorker = true
   const syncIncremental = true
   await JsonRpc.invoke(ipc, 'Initialize.initialize', syntaxHighlightingWorker, syncIncremental)
   return ipc
--- packages/renderer-worker/src/parts/LaunchSyntaxHighlightingWorker/LaunchSyntaxHighlightingWorker.js
+++ packages/renderer-worker/src/parts/LaunchSyntaxHighlightingWorker/LaunchSyntaxHighlightingWorker.js
@@ -1,15 +1,24 @@
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
-import * as SyntaxHighlightingWorkerUrl from '../SyntaxHighlightingWorkerUrl/SyntaxHighlightingWorkerUrl.js'
+import * as IsProduction from '../IsProduction/IsProduction.js'
 import * as Preferences from '../Preferences/Preferences.js'
+import * as SyntaxHighlightingWorkerUrl from '../SyntaxHighlightingWorkerUrl/SyntaxHighlightingWorkerUrl.js'
 
-export const launchSyntaxHighlightingWorker = async () => {
+const getConfiguredWorkerUrl = () => {
   let configuredWorkerUrl = Preferences.get('developer.syntaxHighlightingWorkerPath') || ''
   if (configuredWorkerUrl) {
     configuredWorkerUrl = '/remote' + configuredWorkerUrl
   }
   configuredWorkerUrl = configuredWorkerUrl || SyntaxHighlightingWorkerUrl.syntaxHighlightingWorkerUrl
+  if (IsProduction.isProduction) {
+    configuredWorkerUrl = SyntaxHighlightingWorkerUrl.syntaxHighlightingWorkerUrl
+  }
+  return configuredWorkerUrl
+}
+
+export const launchSyntaxHighlightingWorker = async () => {
+  const configuredWorkerUrl = getConfiguredWorkerUrl()
   const name = 'Syntax Highlighting Worker'
   const ipc = await IpcParent.create({
     method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
