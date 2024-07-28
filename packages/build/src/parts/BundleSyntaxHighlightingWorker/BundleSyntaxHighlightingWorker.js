4ceb4dc1ae140f1c4b73d882eae04dd9fb0c7c23
Le Vivilet <levivlet@pm.me>
2024-07-28 17:13:59 +0200

    build

--- packages/build/src/parts/BundleSyntaxHighlightingWorker/BundleSyntaxHighlightingWorker.js
+++ packages/build/src/parts/BundleSyntaxHighlightingWorker/BundleSyntaxHighlightingWorker.js
@@ -1,28 +1,9 @@
-import * as BundleJs from '../BundleJsRollup/BundleJsRollup.js'
 import * as Copy from '../Copy/Copy.js'
 import * as Path from '../Path/Path.js'
-import * as Replace from '../Replace/Replace.js'
 
 export const bundleSyntaxHighlightingWorker = async ({ cachePath, commitHash, platform, assetDir }) => {
-  await Copy.copy({
-    from: 'packages/syntax-highlighting-worker/src',
-    to: Path.join(cachePath, 'src'),
-  })
-  await Copy.copy({
-    from: 'static/js',
-    to: Path.join(cachePath, 'static', 'js'),
-  })
-  for (const file of ['JsonRpc', 'IpcChildModule']) {
-    await Replace.replace({
-      path: `${cachePath}/src/parts/${file}/${file}.ts`,
-      occurrence: `/static/`,
-      replacement: `../../../static/`,
-    })
-  }
-  await BundleJs.bundleJs({
-    cwd: cachePath,
-    from: `./src/syntaxHighlightingWorkerMain.ts`,
-    platform: 'webworker',
-    allowCyclicDependencies: false,
+  await Copy.copyFile({
+    from: 'packages/renderer-worker/node_modules/@lvce-editor/syntax-highlighting-worker/dist/syntaxHighlightingWorkerMain.js',
+    to: Path.join(cachePath, 'dist', 'syntaxHighlightingWorkerMain.js'),
   })
 }
