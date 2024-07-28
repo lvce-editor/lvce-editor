958a2d36de49de1ef44da6048d866592d3fccbde
Le Vivilet <levivlet@pm.me>
2024-07-28 17:25:55 +0200

    feature: allow configuring syntax highlighting worker url

--- packages/renderer-worker/src/parts/LaunchSyntaxHighlightingWorker/LaunchSyntaxHighlightingWorker.js
+++ packages/renderer-worker/src/parts/LaunchSyntaxHighlightingWorker/LaunchSyntaxHighlightingWorker.js
@@ -2,13 +2,19 @@ import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as SyntaxHighlightingWorkerUrl from '../SyntaxHighlightingWorkerUrl/SyntaxHighlightingWorkerUrl.js'
+import * as Preferences from '../Preferences/Preferences.js'
 
 export const launchSyntaxHighlightingWorker = async () => {
-  const name =  'Syntax Highlighting Worker'
+  let configuredWorkerUrl = Preferences.get('developer.syntaxHighlightingWorkerPath') || ''
+  if (configuredWorkerUrl) {
+    configuredWorkerUrl = '/remote' + configuredWorkerUrl
+  }
+  configuredWorkerUrl = configuredWorkerUrl || SyntaxHighlightingWorkerUrl.syntaxHighlightingWorkerUrl
+  const name = 'Syntax Highlighting Worker'
   const ipc = await IpcParent.create({
     method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
     name,
-    url: SyntaxHighlightingWorkerUrl.syntaxHighlightingWorkerUrl,
+    url: configuredWorkerUrl,
   })
   HandleIpc.handleIpc(ipc)
   return ipc
