d57c1562eb1b9e0daf8af58bd3b7c4d72c768b0b
Le Vivilet <levivlet@pm.me>
2024-07-31 19:28:38 +0200

    fix completion

--- packages/renderer-worker/src/parts/ViewletEditorCompletion/ViewletEditorCompletionSelectCurrent.ts
+++ packages/renderer-worker/src/parts/ViewletEditorCompletion/ViewletEditorCompletionSelectCurrent.ts
@@ -1,6 +1,6 @@
-import * as ViewletEditorCompletionSelectIndex from './ViewletEditorCompletionSelectIndex.ts'
+import * as EditorWorker from '../EditorWorker/EditorWorker.js'
 
 export const selectCurrent = (state) => {
-  const { focusedIndex } = state
-  return ViewletEditorCompletionSelectIndex.selectIndex(state, focusedIndex)
+  const { editorUid } = state
+  return EditorWorker.invoke('EditorCompletion.selectCurrent', editorUid, state)
 }
