155cfaf1e35fde2ee6cba4a6a44b036508a6e0e3
Le Vivilet <levivlet@pm.me>
2024-08-17 21:55:20 +0200

    move out function

--- packages/shared-process/src/parts/CreateWebViewServer/CreateWebViewServer.js
+++ packages/shared-process/src/parts/CreateWebViewServer/CreateWebViewServer.js
@@ -7,14 +7,15 @@ import { VError } from '@lvce-editor/verror'
 export const createWebViewServer = async (port, frameAncestors) => {
   try {
     const csp = GetContentSecurityPolicy.getContentSecurityPolicy([`default-src 'none'`, `script-src 'self'`, `frame-ancestors ${frameAncestors}`])
-    const server = createServer((req, res) => {
+    const handleRequest = (req, res) => {
       SetHeaders.setHeaders(res, {
         'Cross-Origin-Resource-Policy': 'cross-origin',
         'Cross-Origin-Embedder-Policy': 'require-corp',
         'Content-Security-Policy': csp,
       })
       res.end('<button>ok</button>')
-    })
+    }
+    const server = createServer(handleRequest)
     const { resolve, promise } = Promises.withResolvers()
     server.listen(port, resolve)
     await promise
