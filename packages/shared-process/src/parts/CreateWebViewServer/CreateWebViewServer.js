0afe8e32611c254568fc4b5175d5ee7febb6151d
Le Vivilet <levivlet@pm.me>
2024-08-17 21:52:39 +0200

    add frame ancestors

--- packages/shared-process/src/parts/CreateWebViewServer/CreateWebViewServer.js
+++ packages/shared-process/src/parts/CreateWebViewServer/CreateWebViewServer.js
@@ -1,16 +1,23 @@
 import { createServer } from 'node:http'
 import * as Promises from '../Promises/Promises.js'
 import * as SetHeaders from '../SetHeaders/SetHeaders.js'
+import * as GetContentSecurityPolicy from '../GetContentSecurityPolicy/GetContentSecurityPolicy.js'
 import { VError } from '@lvce-editor/verror'
 
 export const createWebViewServer = async (port) => {
   try {
     const server = createServer((req, res) => {
+      const csp = GetContentSecurityPolicy.getContentSecurityPolicy([
+        `default-src 'none'`,
+        `script-src 'self'`,
+        `frame-ancestors http://localhost:3000`,
+      ])
       SetHeaders.setHeaders(res, {
         'Cross-Origin-Resource-Policy': 'cross-origin',
         'Cross-Origin-Embedder-Policy': 'require-corp',
+        'Content-Security-Policy': csp,
       })
-      res.end('ok')
+      res.end('<button>ok</button>')
     })
     const { resolve, promise } = Promises.withResolvers()
     server.listen(port, resolve)
