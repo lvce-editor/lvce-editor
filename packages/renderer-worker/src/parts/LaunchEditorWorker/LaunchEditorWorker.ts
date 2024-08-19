25c1e3817de7d283738b912505fb1654bc7a799f
Le Vivilet <levivlet@pm.me>
2024-08-19 23:04:41 +0200

    feature: move getConfiguredWorkerUrl into separate file

--- packages/renderer-worker/src/parts/LaunchEditorWorker/LaunchEditorWorker.ts
+++ packages/renderer-worker/src/parts/LaunchEditorWorker/LaunchEditorWorker.ts
@@ -1,25 +1,12 @@
 import * as EditorWorkerUrl from '../EditorWorkerUrl/EditorWorkerUrl.js'
+import * as GetConfiguredWorkerUrl from '../GetConfiguredWorkerUrl/GetConfiguredWorkerUrl.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
-import * as IsProduction from '../IsProduction/IsProduction.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
-import * as Preferences from '../Preferences/Preferences.js'
-
-const getConfiguredWorkerUrl = () => {
-  let configuredWorkerUrl = Preferences.get('developer.editorWorkerPath') || ''
-  if (configuredWorkerUrl) {
-    configuredWorkerUrl = '/remote' + configuredWorkerUrl
-  }
-  configuredWorkerUrl = configuredWorkerUrl || EditorWorkerUrl.editorWorkerUrl
-  if (IsProduction.isProduction) {
-    configuredWorkerUrl = EditorWorkerUrl.editorWorkerUrl
-  }
-  return configuredWorkerUrl
-}
 
 export const launchEditorWorker = async () => {
-  const configuredWorkerUrl = getConfiguredWorkerUrl()
+  const configuredWorkerUrl = GetConfiguredWorkerUrl.getConfiguredWorkerUrl('developer.editorWorkerPath', EditorWorkerUrl.editorWorkerUrl)
   const ipc = await IpcParent.create({
     method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
     url: configuredWorkerUrl,
