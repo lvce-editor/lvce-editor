5b8fab33c120d81576feab302585162a82b4cba5
Le Vivilet <levivlet@pm.me>
2024-08-19 23:06:31 +0200

    feature: make test worker url configurable

--- packages/renderer-worker/src/parts/LaunchTestWorker/LaunchTestWorker.ts
+++ packages/renderer-worker/src/parts/LaunchTestWorker/LaunchTestWorker.ts
@@ -1,12 +1,14 @@
+import * as GetConfiguredWorkerUrl from '../GetConfiguredWorkerUrl/GetConfiguredWorkerUrl.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as TestWorkerUrl from '../TestWorkerUrl/TestWorkerUrl.js'
 
 export const launchTestWorker = async () => {
+  const configuredWorkerUrl = GetConfiguredWorkerUrl.getConfiguredWorkerUrl('developer.testWorkerPath', TestWorkerUrl.testWorkerUrl)
   const ipc = await IpcParent.create({
     method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
-    url: TestWorkerUrl.testWorkerUrl,
+    url: configuredWorkerUrl,
     name: 'Test Worker',
   })
   HandleIpc.handleIpc(ipc)
