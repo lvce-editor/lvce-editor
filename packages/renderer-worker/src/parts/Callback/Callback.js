614cc937084e96621d7ae316ec8e74d1cac95a42
Le Vivilet <levivlet@pm.me>
2024-08-24 11:48:14 +0200

    remove unused code

--- packages/renderer-worker/src/parts/Callback/Callback.js
+++ packages/renderer-worker/src/parts/Callback/Callback.js
@@ -1,16 +1,3 @@
-import * as Id from '../Id/Id.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
-import * as Promises from '../Promises/Promises.js'
-
-export const state = {
-  callbacks: Object.create(null),
-}
-
-export const registerPromise = () => {
-  const id = Id.create()
-  const { resolve, promise } = Promises.withResolvers()
-  state.callbacks[id] = resolve
-  return { id, promise }
-}
 
 export const resolve = JsonRpc.resolve
