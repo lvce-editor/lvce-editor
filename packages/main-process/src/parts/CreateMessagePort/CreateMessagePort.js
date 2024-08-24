baaa7d46a3dc3fd4a4414fe5f9df928dedabb6c9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-24 14:36:51 +0200

    fix: electron messageport (#6606)

--- packages/main-process/src/parts/CreateMessagePort/CreateMessagePort.js
+++ packages/main-process/src/parts/CreateMessagePort/CreateMessagePort.js
@@ -2,7 +2,7 @@ import * as Assert from '../Assert/Assert.js'
 import * as HandleMessagePortForSharedProcess from '../HandleMessagePortForSharedProcess/HandleMessagePortForSharedProcess.js'
 
 // TODO reverse order of parameters: make ports first
-export const createMessagePort = async (ipcId, port, webContentsId) => {
+export const createMessagePort = async (ipcId, webContentsId, port) => {
   Assert.number(ipcId)
   Assert.object(port)
   await HandleMessagePortForSharedProcess.handlePort(port, ipcId)
