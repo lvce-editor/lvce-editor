7788fe185af3801b79b6ccde622b1d20e42455c3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-24 19:23:14 +0200

    Discard changes to packages/shared-process/src/parts/GetWindowId/GetWindowId.js

--- packages/shared-process/src/parts/GetWindowId/GetWindowId.js
+++ packages/shared-process/src/parts/GetWindowId/GetWindowId.js
@@ -1,7 +1,7 @@
 import * as Assert from '../Assert/Assert.js'
 import * as ParentIpc from '../ParentIpc/ParentIpc.js'
 
-export const getWindowId = (webContentsId = 1) => {
+export const getWindowId = (webContentsId) => {
   Assert.number(webContentsId)
   return ParentIpc.invoke('GetWindowId.getWindowId', webContentsId)
 }
