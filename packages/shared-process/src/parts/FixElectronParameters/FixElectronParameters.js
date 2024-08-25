04b652b338a32fe3e731fb251d2bf9a4d439229b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-25 15:59:40 +0200

    fix: try to fix electron ipc (#6627)

---
+++ packages/shared-process/src/parts/FixElectronParameters/FixElectronParameters.js
@@ -0,0 +1,14 @@
+import * as GetTransferrables from '../GetTransferrables/GetTransferrables.js'
+import * as RemoveValues from '../RemoveValues/RemoveValues.js'
+
+// workaround for electron not supporting transferrable objects
+// as parameters. If the transferrable object is a parameter, in electron
+// only an empty objected is received in the main process
+export const fixElectronParameters = (value) => {
+  const transfer = GetTransferrables.getTransferrables(value)
+  const newValue = RemoveValues.removeValues(value, transfer)
+  return {
+    newValue,
+    transfer,
+  }
+}
---
+++ packages/shared-process/src/parts/Transferrables/Transferrables.js
@@ -0,0 +1,5 @@
+import * as IsMessagePort from '../IsMessagePort/IsMessagePort.js'
+import * as IsMessagePortMain from '../IsMessagePortMain/IsMessagePortMain.js'
+import * as IsSocket from '../IsSocket/IsSocket.js'
+
+export const transferrables = [IsMessagePort.isMessagePort, IsMessagePortMain.isMessagePortMain, IsSocket.isSocket]
