fa60d5777e5a46d36d9e794dbab93e865a68c582
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-25 18:12:04 +0200

    fix: remove console log (#6631)

--- packages/shared-process/src/parts/HandleIncomingIpc/HandleIncomingIpc.js
+++ packages/shared-process/src/parts/HandleIncomingIpc/HandleIncomingIpc.js
@@ -20,6 +20,5 @@ export const handleIncomingIpc = async (ipcId, handle, message) => {
   Assert.number(ipcId)
   const module = HandleIpcModule.getModule(ipcId)
   const { target, response } = await getIpcAndResponse(module, handle, message)
-  console.log({ target, response })
   await ApplyIncomingIpcResponse.applyIncomingIpcResponse(target, response, ipcId)
 }
