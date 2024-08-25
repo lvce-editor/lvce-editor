8ca24c7473ab6351ba5f79911d7be73c3acdaab7
Le Vivilet <levivlet@pm.me>
2024-08-25 10:48:13 +0200

    debug

--- packages/shared-process/src/parts/HandleWebSocket/HandleWebSocket.js
+++ packages/shared-process/src/parts/HandleWebSocket/HandleWebSocket.js
@@ -6,7 +6,7 @@ import { VError } from '../VError/VError.js'
 
 export const handleWebSocket = async (handle, message) => {
   try {
-    Assert.object(handle)
+    Assert.object(handle
     Assert.object(message)
     const { url } = message
     const type = GetTypeFromUrl.getTypeFromUrl(url)
