511c74bc0a68e281b112072c1a34ca51e5ca816e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-25 20:33:01 +0200

    Discard changes to packages/renderer-worker/src/parts/Transferrable/Transferrable.js

--- packages/renderer-worker/src/parts/Transferrable/Transferrable.js
+++ packages/renderer-worker/src/parts/Transferrable/Transferrable.js
@@ -4,7 +4,7 @@ import * as RendererProcess from '../RendererProcess/RendererProcess.js'
 const objects = Object.create(null)
 
 export const transferToRendererProcess = async (objectId, transferable) => {
-  await RendererProcess.invokeAndTransfer('Transferrable.transfer', transferable, objectId)
+  await RendererProcess.invokeAndTransfer('Transferrable.transfer', [transferable], objectId, transferable)
 }
 
 export const transfer = (objectId, transferable) => {
