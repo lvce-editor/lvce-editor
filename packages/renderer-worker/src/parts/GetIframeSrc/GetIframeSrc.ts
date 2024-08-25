e4effabff87bd6bd0b76034bb5639bfe416d4851
Le Vivilet <levivlet@pm.me>
2024-08-25 20:41:27 +0200

    add fn

--- packages/renderer-worker/src/parts/GetIframeSrc/GetIframeSrc.ts
+++ packages/renderer-worker/src/parts/GetIframeSrc/GetIframeSrc.ts
@@ -30,20 +30,23 @@ export const getIframeSrc = (webViews, webViewId, webViewPort, root, isGitpod, l
     if (!webViewUri) {
       return undefined
     }
+    console.log({ webViewUri })
     let iframeSrc = webViewUri
     let webViewRoot = webViewUri
     if (Platform.platform === PlatformType.Remote) {
       const relativePath = new URL(webViewUri).pathname.replace('/index.html', '')
       console.log({ relativePath })
-      webViewRoot = root + relativePath
+      if (webViewUri.startsWith('file://')) {
+        // ignore
+        webViewRoot = webViewUri.slice('file://'.length)
+      } else {
+        webViewRoot = root + relativePath
+      }
       if (isGitpod) {
         iframeSrc = CreateUrl.createUrl(locationProtocol, locationHost.replace('3000', webViewPort))
       } else {
         iframeSrc = `http://localhost:${webViewPort}`
       }
-    }
-    if (isGitpod) {
-      iframeSrc = iframeSrc
     }
     return {
       iframeSrc,
