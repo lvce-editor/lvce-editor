055b432b31413db854afe83c32c3b559d834e6ac
Le Vivilet <levivlet@pm.me>
2024-08-25 13:35:08 +0200

    fix: electron ipc

--- packages/shared-process/src/parts/HandleIncomingIpc/HandleIncomingIpc.js
+++ packages/shared-process/src/parts/HandleIncomingIpc/HandleIncomingIpc.js
@@ -4,16 +4,21 @@ import * as HandleIncomingIpcMessagePort from '../HandleIncomingIpcMessagePort/H
 import * as HandleIncomingIpcWebSocket from '../HandleIncomingIpcWebSocket/HandleIncomingIpcWebSocket.js'
 import * as HandleIpcModule from '../HandleIpcModule/HandleIpcModule.js'
 import * as IsMessagePortMain from '../IsMessagePortMain/IsMessagePortMain.js'
+import * as IsSocket from '../IsSocket/IsSocket.js'
 
 const getIpcAndResponse = (module, handle, message) => {
   if (IsMessagePortMain.isMessagePortMain(handle)) {
     return HandleIncomingIpcMessagePort.handleIncomingIpcMessagePort(module, handle, message)
   }
-  return HandleIncomingIpcWebSocket.handleIncomingIpcWebSocket(module, handle, message)
+  if (IsSocket.isSocket(handle)) {
+    return HandleIncomingIpcWebSocket.handleIncomingIpcWebSocket(module, handle, message)
+  }
+  throw new Error(`Unexpected ipc handle`)
 }
 
 export const handleIncomingIpc = async (ipcId, handle, message) => {
   Assert.number(ipcId)
+  console.log({ ipcId, handle, message })
   const module = HandleIpcModule.getModule(ipcId)
   const { target, response } = await getIpcAndResponse(module, handle, message)
   await ApplyIncomingIpcResponse.applyIncomingIpcResponse(target, response, ipcId)
