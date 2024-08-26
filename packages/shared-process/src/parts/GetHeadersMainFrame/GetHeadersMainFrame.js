83a73e9a248f77f524756c6b66e71a8efc4a08c0
Le Vivilet <levivlet@pm.me>
2024-08-26 21:46:08 +0200

    feature: add cross origin resource policy header

--- packages/shared-process/src/parts/GetHeadersMainFrame/GetHeadersMainFrame.js
+++ packages/shared-process/src/parts/GetHeadersMainFrame/GetHeadersMainFrame.js
@@ -1,12 +1,14 @@
 import * as ContentSecurityPolicyDocument from '../ContentSecurityPolicyDocument/ContentSecurityPolicyDocument.js'
 import * as CrossOriginEmbedderPolicy from '../CrossOriginEmbedderPolicy/CrossOriginEmbedderPolicy.js'
 import * as CrossOriginOpenerPolicy from '../CrossOriginOpenerPolicy/CrossOriginOpenerPolicy.js'
+import * as CrossOriginResourcePolicy from '../CrossOriginResourcePolicy/CrossOriginResourcePolicy.js'
 import * as HttpHeader from '../HttpHeader/HttpHeader.js'
 
 export const getHeadersMainFrame = () => {
   return {
     [HttpHeader.ContentSecurityPolicy]: ContentSecurityPolicyDocument.value,
-    [HttpHeader.CrossOriginOpenerPolicy]: CrossOriginOpenerPolicy.value,
     [HttpHeader.CrossOriginEmbedderPolicy]: CrossOriginEmbedderPolicy.value,
+    [HttpHeader.CrossOriginOpenerPolicy]: CrossOriginOpenerPolicy.value,
+    [HttpHeader.CrossOriginResourcePolicy]: CrossOriginResourcePolicy.value,
   }
 }
