db245d466437fd766b93036a55e8ae44af11307d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-27 21:24:44 +0200

    Discard changes to packages/shared-process/src/parts/LaunchSearchProcess/LaunchSearchProcess.js

--- packages/shared-process/src/parts/LaunchSearchProcess/LaunchSearchProcess.js
+++ packages/shared-process/src/parts/LaunchSearchProcess/LaunchSearchProcess.js
@@ -1,17 +1,18 @@
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
-import * as PreviewProcessPath from '../PreviewProcessPath/PreviewProcessPath.js'
+import * as IsElectron from '../IsElectron/IsElectron.js'
+import * as SearchProcessPath from '../SearchProcessPath/SearchProcessPath.js'
 
-export const launchPreviewProcess = async () => {
-  const method = IpcParentType.NodeForkedProcess
-  const previewProcess = await IpcParent.create({
+export const launchSearchProcess = async () => {
+  const method = IsElectron.isElectron ? IpcParentType.ElectronUtilityProcess : IpcParentType.NodeForkedProcess
+  const searchProcess = await IpcParent.create({
     method,
-    path: PreviewProcessPath.previewProcessPath,
+    path: SearchProcessPath.searchProcessPath,
     argv: [],
     stdio: 'inherit',
-    name: 'Preview Process',
+    name: 'Search Process',
   })
-  HandleIpc.handleIpc(previewProcess)
-  return previewProcess
+  HandleIpc.handleIpc(searchProcess)
+  return searchProcess
 }
