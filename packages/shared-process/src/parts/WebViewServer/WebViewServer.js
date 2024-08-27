bf816e5c19b22a597ae16b529fe81d4b51fde1cd
Le Vivilet <levivlet@pm.me>
2024-08-27 21:22:41 +0200

    use process

--- packages/shared-process/src/parts/WebViewServer/WebViewServer.js
+++ packages/shared-process/src/parts/WebViewServer/WebViewServer.js
@@ -1,25 +1,9 @@
-import * as CreateWebViewServer from '../CreateWebViewServer/CreateWebViewServer.js'
-import * as CreateWebViewServerHandler from '../CreateWebViewServerHandler/CreateWebViewServerHandler.js'
-
-const state = {
-  /**
-   * @type {any }
-   */
-  promise: undefined,
-}
-
-// TODO move webview preview
-// server into separate process
+import * as PreviewProcess from '../PreviewProcess/PreviewProcess.js'
 
 export const start = async (port) => {
-  if (!state.promise) {
-    state.promise = CreateWebViewServer.createWebViewServer(port)
-  }
-  return state.promise
+  await PreviewProcess.invoke('WebViewServer.start', port)
 }
 
 export const setHandler = async (frameAncestors, webViewRoot) => {
-  const server = await state.promise
-  const handler = CreateWebViewServerHandler.createHandler(frameAncestors, webViewRoot)
-  server.setHandler(handler)
+  await PreviewProcess.invoke('WebViewServer.setHandler', frameAncestors, webViewRoot)
 }
