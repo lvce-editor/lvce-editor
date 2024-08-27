315ae75a0133a4f98f4374f79020fae8d6074e99
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-27 21:40:38 +0200

    feature: use preview process (#6639)

---
+++ packages/shared-process/src/parts/LaunchPreviewProcess/LaunchPreviewProcess.js
@@ -0,0 +1,17 @@
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcParent from '../IpcParent/IpcParent.js'
+import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as PreviewProcessPath from '../PreviewProcessPath/PreviewProcessPath.js'
+
+export const launchPreviewProcess = async () => {
+  const method = IpcParentType.NodeForkedProcess
+  const previewProcess = await IpcParent.create({
+    method,
+    path: PreviewProcessPath.previewProcessPath,
+    argv: [],
+    stdio: 'inherit',
+    name: 'Preview Process',
+  })
+  HandleIpc.handleIpc(previewProcess)
+  return previewProcess
+}
