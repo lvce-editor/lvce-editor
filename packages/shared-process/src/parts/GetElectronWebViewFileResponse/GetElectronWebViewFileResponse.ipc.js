5929591397851a7398e8edb836e3c231ea950d66
Le Vivilet <levivlet@pm.me>
2024-08-29 23:44:22 +0200

    remove unused

--- packages/shared-process/src/parts/GetElectronWebViewFileResponse/GetElectronWebViewFileResponse.ipc.js
+++
@@ -1,7 +0,0 @@
-import * as GetElectronFileResponse from './GetElectronWebViewFileResponse.js'
-
-export const name = 'GetElectronFileResponse'
-
-export const Commands = {
-  getElectronFileResponse: GetElectronFileResponse.getElectronFileResponse,
-}
