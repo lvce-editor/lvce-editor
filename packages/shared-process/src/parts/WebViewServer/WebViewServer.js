b7b6f8eb1a97ad3b1bc29f14cf13b4c40ceae649
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-31 23:31:47 +0200

    feature: improve iframe preview in electron (#6664)

--- packages/shared-process/src/parts/WebViewServer/WebViewServer.js
+++ packages/shared-process/src/parts/WebViewServer/WebViewServer.js
@@ -1,11 +1,20 @@
 import * as PreviewProcess from '../PreviewProcess/PreviewProcess.js'
 import * as ParentIpc from '../ParentIpc/ParentIpc.js'
+import * as GetPortTuple from '../GetPortTuple/GetPortTuple.js'
 
 export const registerProtocol = async () => {
   // TODO only do this once
   // TODO send messageport to proview process
   // TODO send other messageport to parent process
-  await ParentIpc.invoke('ElectronSession.registerWebviewProtocol')
+  console.time('get-ports')
+  const { port1, port2 } = await GetPortTuple.getPortTuple()
+  console.timeEnd('get-ports')
+  console.time('register-protocol')
+  await ParentIpc.invokeAndTransfer('ElectronSession.registerWebviewProtocol', port1)
+  console.timeEnd('register-protocol')
+  console.time('handle-port')
+  await PreviewProcess.invokeAndTransfer('HandleElectronMessagePort.handleElectronMessagePort', port2)
+  console.timeEnd('handle-port')
 }
 
 export const create = async (previewId) => {
