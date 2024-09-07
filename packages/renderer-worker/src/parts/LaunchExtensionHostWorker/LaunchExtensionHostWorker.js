ac3d61e05ae3960a0d651452eeef3a92d4058894
Le Vivilet <levivlet@pm.me>
2024-09-07 11:17:32 +0200

    fix: sending workspace path to extension host worker

--- packages/renderer-worker/src/parts/LaunchExtensionHostWorker/LaunchExtensionHostWorker.js
+++ packages/renderer-worker/src/parts/LaunchExtensionHostWorker/LaunchExtensionHostWorker.js
@@ -2,8 +2,10 @@ import * as ExtensionHostWorkerUrl from '../ExtensionHostWorkerUrl/ExtensionHost
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 import * as Platform from '../Platform/Platform.js'
 import * as PlatformType from '../PlatformType/PlatformType.js'
+import * as Workspace from '../Workspace/Workspace.js'
 
 export const launchExtensionHostWorker = async () => {
   const name = Platform.platform === PlatformType.Electron ? 'Extension Host (Electron)' : 'Extension Host'
@@ -13,5 +15,6 @@ export const launchExtensionHostWorker = async () => {
     url: ExtensionHostWorkerUrl.extensionHostWorkerUrl,
   })
   HandleIpc.handleIpc(ipc)
+  await JsonRpc.invoke('Workspace.setWorkspacePath', Workspace.state.workspacePath)
   return ipc
 }
