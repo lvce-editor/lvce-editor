ebdc792878ad72eae3f0fb18c77f3b17708c0e5a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-07 11:21:06 +0200

    Discard changes to packages/extension-host-worker/src/parts/ErrorCodes/ErrorCodes.ts

--- packages/extension-host-worker/src/parts/ErrorCodes/ErrorCodes.ts
+++ packages/extension-host-worker/src/parts/ErrorCodes/ErrorCodes.ts
@@ -1,3 +1,13 @@
+export const ENOENT = 'ENOENT'
+
+export const EXDEV = 'EXDEV'
+
+export const EEXIST = 'EEXIST'
+
+export const ENOTDIR = 'ENOTDIR'
+
+export const E_MANIFEST_NOT_FOUND = 'E_MANIFEST_NOT_FOUND'
+
 export const BABEL_PARSER_SYNTAX_ERROR = 'BABEL_PARSER_SYNTAX_ERROR'
 
 export const E_COMMAND_NOT_FOUND = 'E_COMMAND_NOT_FOUND'
