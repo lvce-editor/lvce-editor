fd1830533372a027145c49dd0502e59f135371ab
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-07 11:35:18 +0200

    fix: remove console log (#6696)

--- packages/extension-host-helper-process/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js
+++ packages/extension-host-helper-process/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js
@@ -9,7 +9,6 @@ const handleClose = () => {
 }
 
 export const handleElectronMessagePort = async (messagePort, ipcId) => {
-  console.log({ messagePort, ipcId })
   Assert.object(messagePort)
   const ipc = await IpcChild.listen({
     method: IpcChildType.ElectronMessagePort,
