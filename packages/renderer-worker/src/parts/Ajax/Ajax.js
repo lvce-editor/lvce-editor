be2ee52f7060334a10c164f1d74877f2eda739d6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-08 16:24:02 +0200

    feature: use fetch for loading json instead of ky due to error (#6715)

--- packages/renderer-worker/src/parts/Ajax/Ajax.js
+++ packages/renderer-worker/src/parts/Ajax/Ajax.js
@@ -3,29 +3,17 @@ import * as LoadKy from '../LoadKy/LoadKy.js'
 import { VError } from '../VError/VError.js'
 
 export const getJson = async (url, options = {}) => {
-  const { default: ky, HTTPError } = await LoadKy.loadKy()
   try {
     Assert.string(url)
-    // cannot use ky(url).json() because it sets "accept: application/json"
-    // which doesn't work with preload, see
-    // https://stackoverflow.com/questions/41655955/why-are-preload-link-not-working-for-json-requests#answer-48645748
-    const response = await ky(url, options)
-    return response.json()
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+    const result = await response.json()
+    return result
   } catch (error) {
     if (error && error instanceof TypeError && error.message === 'Failed to fetch') {
       throw new VError(`Failed to request json from "${url}". Make sure that the server is running and has CORS enabled`)
-    }
-    if (error && error instanceof HTTPError) {
-      let json
-      try {
-        // @ts-ignore
-        json = await error.response.json()
-      } catch {
-        throw error
-      }
-      if (json && json.message) {
-        throw new VError(`Failed to request json from "${url}": ${json.message}`)
-      }
     }
     // @ts-ignore
     error.message = `Failed to request json from "${url}": ${error.message}`
