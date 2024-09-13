7b74eabdedc26ebe59862ec1964f4d43a64f2555
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-13 23:40:47 +0200

    fix: invalid script tag (#6739)

--- packages/renderer-worker/src/parts/GetWebViewHtml/GetWebViewHtml.ts
+++ packages/renderer-worker/src/parts/GetWebViewHtml/GetWebViewHtml.ts
@@ -33,7 +33,7 @@ export const getWebViewHtml = (webView: any): string => {
     if (element.type === 'title') {
       middle.push(`<title>${element.value}</title>`)
     } else if (element.type === 'script') {
-      middle.push(`<script type="module" src="${AssetDir.assetDir}/preview-injected.js">`)
+      middle.push(`<script type="module" src="${AssetDir.assetDir}/preview-injected.js"></script>`)
       middle.push(`<script type="module" src="${baseUrl}/${element.path}"></script>`)
     } else if (element.type === 'css') {
       middle.push(`<link rel="stylesheet" href="${baseUrl}/${element.path}" />`)
