b47cabc0416245f9aa0ca6e8e9d90e057250da69
Le Vivilet <levivlet@pm.me>
2024-09-13 20:17:27 +0200

    feature: prepapre to use csp for github pages

---
+++ packages/build/src/parts/StaticContentSecurityPolicy/StaticContentSecurityPolicy.js
@@ -0,0 +1,10 @@
+import * as GetContentSecurityPolicy from '../GetContentSecurityPolicy/GetContentSecurityPolicy.js'
+
+export const staticContentSecurityPolicy = GetContentSecurityPolicy.getContentSecurityPolicy([
+  `default-src 'none'`,
+  `font-src 'self'`,
+  `img-src 'self' https: data: blob:`, // TODO maybe disallow https and data images
+  `media-src 'self'`,
+  `script-src 'self'`,
+  `style-src 'self'`,
+])
