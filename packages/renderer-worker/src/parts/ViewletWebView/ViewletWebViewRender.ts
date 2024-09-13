c75a9bd493732d8105e49ca8a37a98ae7359e949
Le Vivilet <levivlet@pm.me>
2024-09-13 20:24:48 +0200

    feature: add webviewState types

--- packages/renderer-worker/src/parts/ViewletWebView/ViewletWebViewRender.ts
+++ packages/renderer-worker/src/parts/ViewletWebView/ViewletWebViewRender.ts
@@ -1,34 +1,35 @@
 import * as GetWebViewVirtualDom from '../GetWebViewVirtualDom/GetWebViewVirtualDom.ts'
+import type { ViewletWebViewState } from './ViewletWebViewState.ts'
 
 export const hasFunctionalRender = true
 
 export const hasFunctionalRootRender = true
 
 const renderWebView = {
-  isEqual(oldState, newState) {
+  isEqual(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
     return oldState.iframeSrc === newState.iframeSrc
   },
-  apply(oldState, newState) {
+  apply(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
     const dom = GetWebViewVirtualDom.getWebViewVirtualDom()
     return ['Viewlet.setDom2', dom]
   },
 }
 
 const renderIframe = {
-  isEqual(oldState, newState) {
+  isEqual(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
     return oldState.iframeSrc === newState.iframeSrc && oldState.sandbox === newState.sandbox
   },
-  apply(oldState, newState) {
+  apply(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
     // TODO support CSP
     return ['setIframe', newState.iframeSrc, newState.sandbox]
   },
 }
 
 const renderPort = {
-  isEqual(oldState, newState) {
+  isEqual(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
     return oldState.portId === newState.portId && oldState.origin === newState.origin
   },
-  apply(oldState, newState) {
+  apply(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
     return ['setPort', newState.portId, newState.origin]
   },
 }
