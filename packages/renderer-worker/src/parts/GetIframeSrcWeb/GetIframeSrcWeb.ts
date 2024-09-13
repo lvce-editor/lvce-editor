62225194dae25adc424f6800c27ea4bb08f18d98
Le Vivilet <levivlet@pm.me>
2024-09-13 20:56:42 +0200

    update url for web

--- packages/renderer-worker/src/parts/GetIframeSrcWeb/GetIframeSrcWeb.ts
+++ packages/renderer-worker/src/parts/GetIframeSrcWeb/GetIframeSrcWeb.ts
@@ -1,7 +1,7 @@
 import * as GetWebViewHtml from '../GetWebViewHtml/GetWebViewHtml.ts'
 
-export const getIframeSrc = async (webView: any, webViewPort: number) => {
-  const srcHtml = GetWebViewHtml.getWebViewHtml(webView, webViewPort)
+export const getIframeSrc = async (webView: any) => {
+  const srcHtml = GetWebViewHtml.getWebViewHtml(webView)
   if (srcHtml) {
     const blob = new Blob([srcHtml], {
       type: 'text/html',
