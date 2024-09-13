1b85972e32849ab8e307aad5096c92e83d2973e0
Le Vivilet <levivlet@pm.me>
2024-09-13 20:53:51 +0200

    use blob src

--- packages/renderer-worker/src/parts/GetIframeSrcWeb/GetIframeSrcWeb.ts
+++ packages/renderer-worker/src/parts/GetIframeSrcWeb/GetIframeSrcWeb.ts
@@ -1,14 +1,15 @@
-import * as Base64 from '../Base64/Base64.js'
 import * as GetWebViewHtml from '../GetWebViewHtml/GetWebViewHtml.ts'
 
 export const getIframeSrc = async (webView: any, webViewPort: number) => {
   const srcHtml = GetWebViewHtml.getWebViewHtml(webView, webViewPort)
   if (srcHtml) {
-    const base64 = await Base64.encode(srcHtml)
-    const dataUrl = `data:text/html;base64,${base64}`
+    const blob = new Blob([srcHtml], {
+      type: 'text/html',
+    })
+    const url = URL.createObjectURL(blob) // TODO dispose
     return {
       srcDoc: '',
-      iframeSrc: dataUrl,
+      iframeSrc: url,
       webViewRoot: '',
     }
   }
