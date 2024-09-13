2b2b13e6cc710ff441efdf1d34740ab2ad35ef41
Le Vivilet <levivlet@pm.me>
2024-09-13 20:44:15 +0200

    use fn

--- packages/renderer-worker/src/parts/GetIframeSrc/GetIframeSrc.ts
+++ packages/renderer-worker/src/parts/GetIframeSrc/GetIframeSrc.ts
@@ -1,21 +1,13 @@
-import * as GetIframeSrcRemote from '../GetIframeSrcRemote/GetIframeSrcRemote.js'
-import * as GetIframeSrcWeb from '../GetIframeSrcWeb/GetIframeSrcWeb.js'
+import * as GetIframeSrcRemote from '../GetIframeSrcRemote/GetIframeSrcRemote.ts'
+import * as GetIframeSrcWeb from '../GetIframeSrcWeb/GetIframeSrcWeb.ts'
+import * as GetWebView from '../GetWebView/GetWebView.ts'
 import * as Platform from '../Platform/Platform.js'
 import * as PlatformType from '../PlatformType/PlatformType.js'
 import { VError } from '../VError/VError.js'
 
-const getWebView = (webViews, webViewId) => {
-  for (const webView of webViews) {
-    if (webView.id === webViewId) {
-      return webView
-    }
-  }
-  return undefined
-}
-
 export const getIframeSrc = async (webViews, webViewId, webViewPort, root, isGitpod, locationProtocol, locationHost) => {
   try {
-    const webView = getWebView(webViews, webViewId)
+    const webView = GetWebView.getWebView(webViews, webViewId)
     if (Platform.platform === PlatformType.Web) {
       return GetIframeSrcWeb.getIframeSrc(webView, webViewPort)
     }
