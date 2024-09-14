f385da7bbb8f5c01de8f9c817b69c7f7bdb57ca2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-14 10:13:01 +0200

    fix: promise could not be cloned error (#6746)

--- packages/renderer-worker/src/parts/GetWebViewSandBox/GetWebViewSandBox.ts
+++ packages/renderer-worker/src/parts/GetWebViewSandBox/GetWebViewSandBox.ts
@@ -2,7 +2,7 @@ import * as Sandbox from '../Sandbox/Sandbox.ts'
 import * as Platform from '../Platform/Platform.js'
 import * as PlatformType from '../PlatformType/PlatformType.js'
 
-export const getIframeSandbox = () => {
+export const getIframeSandbox = (): readonly string[] => {
   if (Platform.platform === PlatformType.Remote) {
     return [Sandbox.AllowScripts, Sandbox.AllowSameOrigin]
   }
