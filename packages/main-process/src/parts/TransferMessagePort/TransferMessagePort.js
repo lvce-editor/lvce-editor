20a6972e0cb251d0e4996016a93a67321bfe0319
Le Vivilet <levivlet@pm.me>
2024-09-15 14:19:12 +0200

    tsc

--- packages/main-process/src/parts/TransferMessagePort/TransferMessagePort.js
+++ packages/main-process/src/parts/TransferMessagePort/TransferMessagePort.js
@@ -3,7 +3,7 @@ import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 
 export const transferMessagePort = async (ipc, port, ...params) => {
   try {
-    await JsonRpc.invokeAndTransfer(ipc, [port], 'HandleNodeMessagePort.handleNodeMessagePort', port, ...params)
+    await JsonRpc.invokeAndTransfer(ipc, 'HandleNodeMessagePort.handleNodeMessagePort', port, ...params)
   } catch (error) {
     throw new VError(error, `Failed to send message port to worker thread`)
   }
