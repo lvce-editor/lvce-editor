2e16975049c2424018f88d6b6476b039a3fd3731
Le Vivilet <levivlet@pm.me>
2024-09-15 09:15:12 +0200

    update url

--- packages/build/src/parts/BundleRendererProcess/BundleRendererProcess.js
+++ packages/build/src/parts/BundleRendererProcess/BundleRendererProcess.js
@@ -36,6 +36,11 @@ export const bundleRendererProcess = async ({ cachePath, commitHash, platform, a
       occurrence: `const syntaxHighlightingWorkerUrl = \`\${assetDir}/packages/renderer-worker/node_modules/@lvce-editor/syntax-highlighting-worker/dist/syntaxHighlightingWorkerMain.js\`;`,
       replacement: `const syntaxHighlightingWorkerUrl = \`\${assetDir}/packages/syntax-highlighting-worker/dist/syntaxHighlightingWorkerMain.js\`;`,
     })
+    await Replace.replace({
+      path: `${cachePath}/dist/rendererProcessMain.js`,
+      occurrence: `const editorWorkerUrl = \`\${assetDir}/packages/renderer-worker/node_modules/@lvce-editor/editor-worker/dist/editorWorkerMain.js\`;`,
+      replacement: `const editorWorkerUrl = \`\${assetDir}/packages/editor-worker/dist/editorWorkerMain.js\`;`,
+    })
     const platformCode = getPlatformCode(platform)
     await Replace.replace({
       path: `${cachePath}/dist/rendererProcessMain.js`,
