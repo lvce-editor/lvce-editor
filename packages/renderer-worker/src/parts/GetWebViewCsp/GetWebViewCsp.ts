0e699499bc748f3ca62741b2d3db4ff25c6645a5
Le Vivilet <levivlet@pm.me>
2024-09-15 16:11:24 +0200

    feature: set stricter webview csp

--- packages/renderer-worker/src/parts/GetWebViewCsp/GetWebViewCsp.ts
+++ packages/renderer-worker/src/parts/GetWebViewCsp/GetWebViewCsp.ts
@@ -1,4 +1,12 @@
+import * as GetContentSecurityPolicy from '../GetContentSecurityPolicy/GetContentSecurityPolicy.js'
+
 export const getWebViewCsp = () => {
-  const csp = `default-src *; script-src *; style-src *; script-src-elem *; style-src-elem *`
+  const csp = GetContentSecurityPolicy.getContentSecurityPolicy([
+    `default-src 'none'`,
+    `script-src 'self`,
+    `style-src 'self`,
+    `img-src 'self`,
+    `media-src 'self'`,
+  ])
   return csp
 }
