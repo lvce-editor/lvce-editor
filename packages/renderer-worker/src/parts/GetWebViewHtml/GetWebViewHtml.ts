f825539d2d364b28a0ae1db179065104bea18e6c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-16 22:51:43 +0200

    feature: move baseUrl into separate file (#6786)

--- packages/renderer-worker/src/parts/GetWebViewHtml/GetWebViewHtml.ts
+++ packages/renderer-worker/src/parts/GetWebViewHtml/GetWebViewHtml.ts
@@ -1,30 +1,6 @@
 import * as AssetDir from '../AssetDir/AssetDir.js'
 
-const getDefaultBaseUrl = (webView: any) => {
-  const { remotePath, path } = webView
-  if (remotePath) {
-    if (remotePath.endsWith('/index.html')) {
-      return remotePath.slice(0, -'/index.html'.length)
-    }
-    return remotePath
-  }
-  if (path) {
-    if (path.endsWith('/index.html')) {
-      return path.slice(0, -'/index.html'.length)
-    }
-    return path
-  }
-  return ''
-}
-
-const getBaseUrl = (webView: any) => {
-  const defaultBaseUrl = getDefaultBaseUrl(webView)
-  return defaultBaseUrl
-}
-
-export const getWebViewHtml = (webView: any, locationOrigin: string): string => {
-  const { elements } = webView
-  const baseUrl = getBaseUrl(webView)
+export const getWebViewHtml = (baseUrl: string, locationOrigin: string, elements: any[]): string => {
   const middle: string[] = []
   middle.push('<meta charset="utf-8">')
   for (const element of elements) {
