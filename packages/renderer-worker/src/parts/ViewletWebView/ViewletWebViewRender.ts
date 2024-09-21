d51f1cd7f0e25dbb827aa13477e6287db31eee75
Le Vivilet <levivlet@pm.me>
2024-09-21 19:27:09 +0200

    remove unused code

--- packages/renderer-worker/src/parts/ViewletWebView/ViewletWebViewRender.ts
+++ packages/renderer-worker/src/parts/ViewletWebView/ViewletWebViewRender.ts
@@ -1,44 +1,8 @@
-import * as GetWebViewVirtualDom from '../GetWebViewVirtualDom/GetWebViewVirtualDom.ts'
 import type { ViewletWebViewState } from './ViewletWebViewState.ts'
 
 export const hasFunctionalRender = true
 
 export const hasFunctionalRootRender = true
-
-// const renderWebView = {
-//   isEqual(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
-//     return oldState.iframeSrc === newState.iframeSrc && oldState.srcDoc === newState.srcDoc && oldState.csp === newState.csp
-//   },
-//   apply(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
-//     const dom = GetWebViewVirtualDom.getWebViewVirtualDom()
-//     return ['Viewlet.setDom2', dom]
-//   },
-// }
-
-// const renderIframe = {
-//   isEqual(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
-//     return (
-//       oldState.iframeSrc === newState.iframeSrc &&
-//       oldState.sandbox === newState.sandbox &&
-//       oldState.srcDoc === newState.srcDoc &&
-//       oldState.csp === newState.csp &&
-//       oldState.credentialless === newState.credentialless
-//     )
-//   },
-//   apply(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
-//     // TODO support CSP also in web
-//     return ['setIframe', newState.iframeSrc, newState.sandbox, newState.srcDoc, newState.csp]
-//   },
-// }
-
-// const renderPort = {
-//   isEqual(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
-//     return oldState.portId === newState.portId && oldState.origin === newState.origin
-//   },
-//   apply(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
-//     return ['setPort', newState.portId, newState.origin]
-//   },
-// }
 
 const renderIframe = {
   isEqual(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
@@ -56,8 +20,4 @@ const renderIframe = {
   },
 }
 
-export const render = [
-  renderIframe,
-  // renderWebView, renderIframe,
-  // renderPort,
-]
+export const render = [renderIframe]
