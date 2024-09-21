fb43e7a9a434c59243f173a45da99d5bacd8b43d
Le Vivilet <levivlet@pm.me>
2024-09-21 16:09:12 +0200

    feature: improve webview test

--- packages/extension-host-worker-tests/src/sample.webview-provider.js
+++ packages/extension-host-worker-tests/src/sample.webview-provider.js
@@ -1,9 +1,17 @@
-export const skip = true
-
 export const name = 'sample.webview-provider'
 
-export const test = async ({ Extension, QuickPick, Locator, expect }) => {
+export const skip = true
+
+export const test = async ({ Extension, Main, FileSystem }) => {
+  // arrange
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, `a`)
+
+  // act
+  await Main.openUri(`${tmpDir}/test.xyz`)
+
+  // assert
   // TODO open sample webview
   // TODO verify that expected content is displayed
-  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
 }
