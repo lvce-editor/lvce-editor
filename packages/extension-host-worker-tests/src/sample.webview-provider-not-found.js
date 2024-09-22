e8c3b6cc51a95e34e68582155805232b67c29c59
Le Vivilet <levivlet@pm.me>
2024-09-22 08:56:26 +0200

    feature: add webview not found test

---
+++ packages/extension-host-worker-tests/src/sample.webview-provider-not-found.js
@@ -0,0 +1,20 @@
+export const name = 'sample.webview-provider-not-found'
+
+export const skip = false
+
+export const test = async ({ Extension, Main, FileSystem, ...rest }) => {
+  // arrange
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, `a`)
+
+  console.log({ rest })
+  // act
+  await Main.openUri(`${tmpDir}/test.xyz`)
+
+  // assert
+  await Main.shouldHaveError('Error: webview provider xyz not found')
+  const errorMessage = Locator('Viewlet Error')
+  // TODO open sample webview
+  // TODO verify that expected content is displayed
+}
