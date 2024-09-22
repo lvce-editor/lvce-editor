3f33484a6e9fb2f02455504e6370424384b9dfee
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-22 11:52:56 +0200

    feature: add webview provider create error test (#6808)

---
+++ packages/extension-host-worker-tests/src/sample.webview-provider-missing-create-function.js
@@ -0,0 +1,19 @@
+export const name = 'sample.webview-provider-missing-create-function'
+
+export const skip = true
+
+export const test = async ({ Extension, Main, FileSystem, Locator, expect }) => {
+  // arrange
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, `a`)
+
+  // act
+  await Main.openUri(`${tmpDir}/test.xyz`)
+
+  // assert
+  // TODO improve error message
+  const error = Locator('.Error')
+  await expect(error).toBeVisible()
+  await expect(error).toHaveText('Error: provider.create is not a function')
+}
