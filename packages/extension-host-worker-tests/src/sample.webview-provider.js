5f4fdf74f2fb42539b5a7980143cb251d608fcd7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-22 11:23:35 +0200

    Discard changes to packages/extension-host-worker-tests/src/sample.webview-provider.js

--- packages/extension-host-worker-tests/src/sample.webview-provider.js
+++ packages/extension-host-worker-tests/src/sample.webview-provider.js
@@ -1,5 +1,7 @@
 export const name = 'sample.webview-provider'
 
+export const skip = true
+
 export const test = async ({ Extension, Main, FileSystem }) => {
   // arrange
   await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
@@ -10,5 +12,6 @@ export const test = async ({ Extension, Main, FileSystem }) => {
   await Main.openUri(`${tmpDir}/test.xyz`)
 
   // assert
+  // TODO open sample webview
   // TODO verify that expected content is displayed
 }
