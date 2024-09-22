458ee78b1d575e76e520b08d5f1b1da7587f72b6
Le Vivilet <levivlet@pm.me>
2024-09-22 11:52:18 +0200

    improve test

--- packages/extension-host-worker-tests/src/sample.webview-provider.js
+++ packages/extension-host-worker-tests/src/sample.webview-provider.js
@@ -1,6 +1,6 @@
 export const name = 'sample.webview-provider'
 
-export const test = async ({ Extension, Main, FileSystem }) => {
+export const test = async ({ Extension, Main, FileSystem, WebView }) => {
   // arrange
   await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
   const tmpDir = await FileSystem.getTmpDir()
@@ -10,6 +10,12 @@ export const test = async ({ Extension, Main, FileSystem }) => {
   await Main.openUri(`${tmpDir}/test.xyz`)
 
   // assert
+  const webView = await WebView.fromId('xyz')
+  console.log({ webview: webView })
+  const heading = webView.locator('h1')
+  // TODO allow using normal expect function for webview also
+  await webView.expect(heading).toBeVisible()
+  await webView.expect(heading).toHaveText('Hello world')
   // TODO open sample webview
   // TODO verify that expected content is displayed
 }
