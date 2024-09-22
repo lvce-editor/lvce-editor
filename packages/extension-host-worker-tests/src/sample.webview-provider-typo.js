22db37cd22078fbb32136eb435687e6f07c02a0b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-22 11:34:09 +0200

    feature: add webview typo test (#6805)

---
+++ packages/extension-host-worker-tests/src/sample.webview-provider-typo.js
@@ -0,0 +1,17 @@
+export const name = 'sample.webview-provider-typo'
+
+export const test = async ({ Extension, Main, FileSystem, Locator, expect }) => {
+  // arrange
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, `a`)
+
+  // act
+  await Main.openUri(`${tmpDir}/test.xyz`)
+
+  // assert
+  // TODO improve error message
+  const error = Locator('.Error')
+  await expect(error).toBeVisible()
+  await expect(error).toHaveText('Error: webview provider xyz not found')
+}
