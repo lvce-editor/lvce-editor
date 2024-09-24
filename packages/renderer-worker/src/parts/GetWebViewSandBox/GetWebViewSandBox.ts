1976f71450c23f2ee31d98525d3223d8061048b3
Le Vivilet <levivlet@pm.me>
2024-09-24 23:13:11 +0200

    feature: allow extension to set iframe sandbox attribute

--- packages/renderer-worker/src/parts/GetWebViewSandBox/GetWebViewSandBox.ts
+++ packages/renderer-worker/src/parts/GetWebViewSandBox/GetWebViewSandBox.ts
@@ -2,13 +2,14 @@ import * as Sandbox from '../Sandbox/Sandbox.ts'
 import * as Platform from '../Platform/Platform.js'
 import * as PlatformType from '../PlatformType/PlatformType.js'
 
-export const getIframeSandbox = (): readonly string[] => {
+export const getIframeSandbox = (webView: any): readonly string[] => {
+  const extensionSandbox = webView.sandbox || []
   if (Platform.platform === PlatformType.Remote) {
-    return [Sandbox.AllowScripts, Sandbox.AllowSameOrigin] // TODO maybe disallow same origin
+    return [Sandbox.AllowScripts, Sandbox.AllowSameOrigin, ...extensionSandbox] // TODO maybe disallow same origin
   }
   if (Platform.platform === PlatformType.Web) {
-    return [Sandbox.AllowScripts]
+    return [Sandbox.AllowScripts, ...extensionSandbox]
   }
   // TODO set something for electron
-  return []
+  return [...extensionSandbox]
 }
