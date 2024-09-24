81badef07cd28764045b027b0a789bfea2be10c7
Le Vivilet <levivlet@pm.me>
2024-09-24 21:51:48 +0200

    feature: add split e2e test

---
+++ packages/extension-host-worker-tests/src/viewlet.main-split-right.js
@@ -0,0 +1,21 @@
+export const name = 'viewlet.main-split-right'
+
+export const test = async ({ Extension, QuickPick, Locator, expect, FileSystem, Workspace, Main }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file1.txt`,
+    `content 1
+content 2`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await QuickPick.open()
+  await QuickPick.setValue('>Split Right')
+
+  // act
+  await QuickPick.selectItem('Main: Split Right')
+
+  // assert
+  // TODO check that two editors are open now
+}
