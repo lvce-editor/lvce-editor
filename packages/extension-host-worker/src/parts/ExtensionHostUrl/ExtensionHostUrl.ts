9212df483225cff0a26327f76a9b544397bc729f
Le Vivilet <levivlet@pm.me>
2024-09-26 23:09:22 +0200

    feature: support remote url for html file system

--- packages/extension-host-worker/src/parts/ExtensionHostUrl/ExtensionHostUrl.ts
+++ packages/extension-host-worker/src/parts/ExtensionHostUrl/ExtensionHostUrl.ts
@@ -1,5 +1,6 @@
 import * as Platform from '../Platform/Platform.ts'
 import * as PlatformType from '../PlatformType/PlatformType.ts'
+import * as Rpc from '../Rpc/Rpc.ts'
 
 // TODO names this method
 // - getRemoteUrl
@@ -9,7 +10,11 @@ import * as PlatformType from '../PlatformType/PlatformType.ts'
 
 // TODO when returning an objectUrl in web, provide a way to dispose the object url
 
-export const getRemoteUrl = (uri: string) => {
+export const getRemoteUrl = async (uri: string) => {
+  if (uri.startsWith('html://')) {
+    const url = await Rpc.invoke('Blob.getSrc', uri)
+    return url
+  }
   if (Platform.platform === PlatformType.Remote) {
     // TODO support custom file system protocols
     return `/remote/${uri}`
