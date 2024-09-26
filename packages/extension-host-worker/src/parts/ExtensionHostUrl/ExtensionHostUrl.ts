4b669db2880185c5146b05931387126be70f344f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-26 23:06:03 +0200

    feature: implement getRemoteUrl function differently based on platform (#6824)

--- packages/extension-host-worker/src/parts/ExtensionHostUrl/ExtensionHostUrl.ts
+++ packages/extension-host-worker/src/parts/ExtensionHostUrl/ExtensionHostUrl.ts
@@ -1,3 +1,6 @@
+import * as Platform from '../Platform/Platform.ts'
+import * as PlatformType from '../PlatformType/PlatformType.ts'
+
 // TODO names this method
 // - getRemoteUrl
 // - getBlobUrl
@@ -7,5 +10,13 @@
 // TODO when returning an objectUrl in web, provide a way to dispose the object url
 
 export const getRemoteUrl = (uri: string) => {
-  return `/remote/${uri}`
+  if (Platform.platform === PlatformType.Remote) {
+    // TODO support custom file system protocols
+    return `/remote/${uri}`
+  }
+  if (Platform.platform === PlatformType.Electron) {
+    // TODO
+    return `/remote/${uri}`
+  }
+  throw new Error(`unsupported platform`)
 }
--- packages/extension-host-worker/src/parts/PlatformType/PlatformType.ts
+++ packages/extension-host-worker/src/parts/PlatformType/PlatformType.ts
@@ -1,3 +1,7 @@
 export const Electron = 'electron'
 
 export const Remote = 'remote'
+
+export const Web = 'web'
+
+export const Test = 'test'
