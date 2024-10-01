5d97a5a2df0a2baee47aa9d76b371fdc69987a05
Le Vivilet <levivlet@pm.me>
2024-10-01 17:37:56 +0200

    build

--- packages/build/src/parts/BundleElectronApp/BundleElectronApp.js
+++ packages/build/src/parts/BundleElectronApp/BundleElectronApp.js
@@ -36,7 +36,6 @@ const getDependencyCacheHash = async ({ electronVersion, arch, supportsAutoUpdat
   const files = [
     'packages/main-process/package-lock.json',
     'packages/shared-process/package-lock.json',
-    'packages/extension-host-worker/package-lock.json',
     'packages/extension-host-sub-worker/package-lock.json',
     'packages/extension-host-helper-process/package-lock.json',
     'packages/build/src/parts/BundleElectronApp/BundleElectronApp.js',
