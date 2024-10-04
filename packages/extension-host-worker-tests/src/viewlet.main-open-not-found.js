9a8173541bd5e0526a40cdb8eac1dd6c010a87c9
Le Vivilet <levivlet@pm.me>
2024-10-04 12:06:45 +0200

    feature: add not-found e2e test

---
+++ packages/extension-host-worker-tests/src/viewlet.main-open-not-found.js
@@ -0,0 +1,16 @@
+export const skip = true
+
+export const name = 'viewlet.main-open-not-found'
+
+export const test = async ({ FileSystem, Workspace, Main, Locator, expect, SideBar }) => {
+  // arrange
+  await SideBar.hide()
+  const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
+
+  // act
+  await Main.openUri(`${tmpDir}/not-found.txt`)
+
+  // assert
+  // TODO check that not-found message is displayed
+}
