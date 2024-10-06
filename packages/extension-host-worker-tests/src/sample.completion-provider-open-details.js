79d6277634c5c0256e00f4c3a2a5643a09e961c2
Le Vivilet <levivlet@pm.me>
2024-10-06 14:45:23 +0200

    feature: add completion toggle details e2e test

---
+++ packages/extension-host-worker-tests/src/sample.completion-provider-open-details.js
@@ -0,0 +1,25 @@
+export const name = 'sample.completion-provider-open-details'
+
+export const test = async ({ FileSystem, Workspace, Extension, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, ['   line   ', '   line   ', '   line   '].join('\n'))
+
+  await Workspace.setPath(tmpDir)
+  await Extension.addWebExtension(new URL('../fixtures/sample.completion-provider-open-details', import.meta.url).toString())
+
+  // act
+  await Main.openUri(`${tmpDir}/test.xyz`)
+  await Editor.setCursor(0, 0)
+  await Editor.openCompletion()
+
+  // assert
+  const completions = Locator('#Completions')
+  await expect(completions).toBeVisible()
+
+  // act
+  await Editor.openCompletionDetails()
+  // assert
+  const completionDetails = Locator('.CompletionDetails')
+  await expect(completionDetails).toBeVisible()
+}
