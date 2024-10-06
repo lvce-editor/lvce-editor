f35fec8b0d711b56531ffdfdffa104d1e53ee9b4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-06 09:47:02 +0200

    feature: use terminal worker dependency instead of custom package (#6902)

--- packages/renderer-worker/src/parts/TerminalWorkerUrl/TerminalWorkerUrl.js
+++ packages/renderer-worker/src/parts/TerminalWorkerUrl/TerminalWorkerUrl.js
@@ -1,3 +1,3 @@
 import * as AssetDir from '../AssetDir/AssetDir.js'
 
-export const terminalWorkerUrl = `${AssetDir.assetDir}/packages/terminal-worker/src/terminalWorkerMain.ts`
+export const terminalWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/terminal-worker/dist/terminalWorkerMain.js`
