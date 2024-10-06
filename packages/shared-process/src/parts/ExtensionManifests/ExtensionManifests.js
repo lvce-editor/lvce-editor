6ac87865ea83919a371beb974a07114ff2f53686
Le Vivilet <levivlet@pm.me>
2024-10-06 19:43:35 +0200

    feature: move deduplicate function into separate file

--- packages/shared-process/src/parts/ExtensionManifests/ExtensionManifests.js
+++ packages/shared-process/src/parts/ExtensionManifests/ExtensionManifests.js
@@ -1,3 +1,4 @@
+import * as DeduplicateExtensions from '../DeduplicateExtensions/DeduplicateExtensions.js'
 import * as ExtensionManifestInputType from '../ExtensionManifestInputType/ExtensionManifestInputType.js'
 import * as ExtensionManifestsGetFromFolder from './ExtensionManifestsFromFolder.js'
 import * as ExtensionManifestsGetFromLinkedExtensionsFolder from './ExtensionManifestsFromLinkedExtensionsFolder.js'
@@ -21,25 +22,9 @@ const get = (input) => {
   return module.getExtensionManifests(input.path)
 }
 
-const deduplicateExtensions = (extensions, builtinExtensionsPath) => {
-  const seen = Object.create(null)
-  const uniqueExtensions = []
-  for (const extension of extensions) {
-    if (extension.id in seen) {
-      continue
-    }
-    seen[extension.id] = true
-    if (extension.path.startsWith(builtinExtensionsPath)) {
-      extension.builtin = true
-    }
-    uniqueExtensions.push(extension)
-  }
-  return uniqueExtensions
-}
-
 export const getAll = async (inputs, builtinExtensionsPath) => {
   const manifests = await Promise.all(inputs.map(get))
   const flatManifests = manifests.flat(1)
-  const uniqueExtensions = deduplicateExtensions(flatManifests, builtinExtensionsPath)
+  const uniqueExtensions = DeduplicateExtensions.deduplicateExtensions(flatManifests, builtinExtensionsPath)
   return uniqueExtensions
 }
