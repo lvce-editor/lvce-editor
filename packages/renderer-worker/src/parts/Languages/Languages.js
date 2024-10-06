7e829aaae1767c3de2da1a8694b6b4e9894041cd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-06 19:40:30 +0200

    Discard changes to packages/renderer-worker/src/parts/Languages/Languages.js

--- packages/renderer-worker/src/parts/Languages/Languages.js
+++ packages/renderer-worker/src/parts/Languages/Languages.js
@@ -9,7 +9,6 @@ import * as LanguagesState from '../LanguagesState/LanguagesState.js'
 import * as Logger from '../Logger/Logger.js'
 import * as Preferences from '../Preferences/Preferences.js'
 import * as SplitLines from '../SplitLines/SplitLines.js'
-import * as GetLanguages from '../GetLanguages/GetLanguages.js'
 
 export const getLanguageId = (fileName) => {
   Assert.string(fileName)
@@ -52,8 +51,7 @@ export const hydrate = async () => {
   LanguagesState.setHydrating(true)
   // TODO handle error
   // TODO main parts should have nothing todo with shared process -> only sub components
-  const languages = await GetLanguages.getLanguages()
-  console.log({ languages })
+  const languages = await ExtensionHostLanguages.getLanguages()
   // TODO avoid side effect here, but how?
   await addLanguages(languages)
   const useJsx = Preferences.get('languages.jsFilesAsJsx')
