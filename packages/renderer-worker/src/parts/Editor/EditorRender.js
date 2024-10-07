6043b5e2dbdb24b365acf85ac4671eab9fdc8231
Le Vivilet <levivlet@pm.me>
2024-10-08 00:28:25 +0200

    feature: improve completion details close button

--- packages/renderer-worker/src/parts/Editor/EditorRender.js
+++ packages/renderer-worker/src/parts/Editor/EditorRender.js
@@ -9,11 +9,18 @@ const renderAll = {
       return []
     }
     const adjustedCommands = commands.map((command) => {
-      if (command[0] === 'Viewlet.create' || command[0] === 'Viewlet.send') {
+      if (
+        command[0] === 'Viewlet.create' ||
+        command[0] === 'Viewlet.send' ||
+        command[0] === 'Viewlet.createFunctionalRoot' ||
+        command[0] === 'Viewlet.setDom2'
+      ) {
         return command
       }
+      console.log({ command })
       return ['Viewlet.send', newState.uid, ...command]
     })
+    console.log({ adjustedCommands })
     return adjustedCommands
   },
   multiple: true,
