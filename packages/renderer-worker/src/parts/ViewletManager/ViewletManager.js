67b2d08cebd27f7e90abc2ae13cb42def9e6ff14
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-08 00:52:43 +0200

    feature: improve completion details close button (#6930)

--- packages/renderer-worker/src/parts/ViewletManager/ViewletManager.js
+++ packages/renderer-worker/src/parts/ViewletManager/ViewletManager.js
@@ -207,7 +207,7 @@ const getRenderCommands = (module, oldState, newState, uid = newState.uid || mod
           command.splice(1, 0, uid)
         } else if (command[0] === 'Viewlet.setDom') {
           command.splice(1, 0, uid)
-        } else if (command[0] !== 'Viewlet.send' && command[0] !== 'Viewlet.ariaAnnounce') {
+        } else if (command[0] !== 'Viewlet.send' && command[0] !== 'Viewlet.ariaAnnounce' && command[0] !== 'Viewlet.createFunctionalRoot') {
           command.unshift('Viewlet.send', uid)
         }
         commands.push(command)
