91f36bbcd874ee7d1d185c0b4e0b646f312e976d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-12 09:24:45 +0200

    feature: move getDirectoryHandle into separate file (#6960)

--- packages/renderer-worker/src/parts/SearchFileWithHtml/SearchFileWithHtml.js
+++ packages/renderer-worker/src/parts/SearchFileWithHtml/SearchFileWithHtml.js
@@ -1,20 +1,7 @@
 import * as FileHandleType from '../FileHandleType/FileHandleType.js'
 import * as FileSystemDirectoryHandle from '../FileSystemDirectoryHandle/FileSystemDirectoryHandle.js'
-import * as Path from '../Path/Path.js'
-import * as PersistentFileHandle from '../PersistentFileHandle/PersistentFileHandle.js'
+import * as GetDirectoryHandle from '../GetDirectoryHandle/GetDirectoryHandle.js'
 import { VError } from '../VError/VError.js'
-
-const getDirectoryHandle = async (uri) => {
-  const handle = await PersistentFileHandle.getHandle(uri)
-  if (handle) {
-    return handle
-  }
-  const dirname = Path.dirname('/', uri)
-  if (uri === dirname) {
-    return undefined
-  }
-  return getDirectoryHandle(dirname)
-}
 
 const searchFilesRecursively = async (all, parent, handle) => {
   const childHandles = await FileSystemDirectoryHandle.getChildHandles(handle)
@@ -37,7 +24,7 @@ const searchFilesRecursively = async (all, parent, handle) => {
 
 export const searchFile = async (uri) => {
   const path = uri.slice('html://'.length)
-  const handle = await getDirectoryHandle(path)
+  const handle = await GetDirectoryHandle.getDirectoryHandle(path)
   if (!handle) {
     throw new VError(`Folder not found ${uri}`)
   }
