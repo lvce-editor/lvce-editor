2c0fc7cbe5337bba5c0ed3cf9cb48fae191323a0
Le Vivilet <levivlet@pm.me>
2024-10-12 09:36:53 +0200

    fix code

--- packages/renderer-worker/src/parts/SearchFileWithRipGrep/SearchFileWithRipGrep.js
+++ packages/renderer-worker/src/parts/SearchFileWithRipGrep/SearchFileWithRipGrep.js
@@ -1,14 +1,6 @@
 import * as FileSearchWorker from '../FileSearchWorker/FileSearchWorker.js'
 
 export const searchFile = async (path, value) => {
-  // const ripGrepArgs = GetFileSearchRipGrepArgs.getFileSearchRipGrepArgs()
-  // const options = {
-  //   ripGrepArgs,
-  //   searchPath: path,
-  //   limit: 9999999,
-  // }
-  // const stdout = await SearchProcess.invoke(SharedProcessCommandType.SearchFileSearchFile, options)
-  // const lines = SplitLines.splitLines(stdout)
   const lines = await FileSearchWorker.invoke('SearchFile.searchFileWithRipGrep', path, value)
   return lines
 }
