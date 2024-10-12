611e88fc4887cd4e765f2eb5146ffd95e6749faf
Le Vivilet <levivlet@pm.me>
2024-10-12 08:22:35 +0200

    feature: prepare to move file search to web worker

---
+++ packages/renderer-worker/src/parts/FileSearchWorkerUrl/FileSearchWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const fileSearchWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/terminal-worker/dist/fileSearchWorkerMain.js`
