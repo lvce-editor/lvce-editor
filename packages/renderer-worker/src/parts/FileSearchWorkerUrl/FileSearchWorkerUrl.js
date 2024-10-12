a4f7cee875ee38f789938fcf7c9826064e7eee68
Le Vivilet <levivlet@pm.me>
2024-10-12 08:31:12 +0200

    fix url

--- packages/renderer-worker/src/parts/FileSearchWorkerUrl/FileSearchWorkerUrl.js
+++ packages/renderer-worker/src/parts/FileSearchWorkerUrl/FileSearchWorkerUrl.js
@@ -1,3 +1,3 @@
 import * as AssetDir from '../AssetDir/AssetDir.js'
 
-export const fileSearchWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/terminal-worker/dist/fileSearchWorkerMain.js`
+export const fileSearchWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/file-search-worker/dist/fileSearchWorkerMain.js`
