d308698a2b66c5831a4d928bf5b10f83afafcd04
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-12 11:56:25 +0200

    feature: move html file search to file search worker (#6970)

--- packages/renderer-worker/src/parts/SearchFileWithHtml/SearchFileWithHtml.js
+++ packages/renderer-worker/src/parts/SearchFileWithHtml/SearchFileWithHtml.js
@@ -1,34 +1,6 @@
-import * as FileHandleType from '../FileHandleType/FileHandleType.js'
-import * as FileSystemDirectoryHandle from '../FileSystemDirectoryHandle/FileSystemDirectoryHandle.js'
-import * as GetDirectoryHandle from '../GetDirectoryHandle/GetDirectoryHandle.js'
-import { VError } from '../VError/VError.js'
-
-const searchFilesRecursively = async (all, parent, handle) => {
-  const childHandles = await FileSystemDirectoryHandle.getChildHandles(handle)
-  const promises = []
-  for (const childHandle of childHandles) {
-    const absolutePath = parent + '/' + childHandle.name
-    switch (childHandle.kind) {
-      case FileHandleType.Directory:
-        promises.push(searchFilesRecursively(all, absolutePath, childHandle))
-        break
-      case FileHandleType.File:
-        all.push(absolutePath)
-        break
-      default:
-        break
-    }
-  }
-  await Promise.all(promises)
-}
+import * as FileSearchWorker from '../FileSearchWorker/FileSearchWorker.js'
 
 export const searchFile = async (uri) => {
-  const path = uri.slice('html://'.length)
-  const handle = await GetDirectoryHandle.getDirectoryHandle(path)
-  if (!handle) {
-    throw new VError(`Folder not found ${uri}`)
-  }
-  const all = []
-  await searchFilesRecursively(all, '', handle)
-  return all
+  const files = await FileSearchWorker.invoke('SearchFile.searchFileWithHtml', uri)
+  return files
 }
