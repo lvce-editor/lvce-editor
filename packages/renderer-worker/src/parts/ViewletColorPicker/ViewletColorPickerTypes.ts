e61d1cc26568ee5e62295c2499d544fc223cb753
Le Vivilet <levivlet@pm.me>
2024-10-15 21:13:52 +0200

    feature: remove unused color picker code

--- packages/renderer-worker/src/parts/ViewletColorPicker/ViewletColorPickerTypes.ts
+++
@@ -1,8 +0,0 @@
-export interface ColorPickerState {
-  readonly color: string
-  readonly offsetX: number
-  readonly min: number
-  readonly max: number
-  commands: any[]
-  readonly uid: number
-}
