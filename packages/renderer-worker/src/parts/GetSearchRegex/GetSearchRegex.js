2b7549b1a4c156988399c1cceab42b5e45419fd0
Le Vivilet <levivlet@pm.me>
2024-10-16 20:15:57 +0200

    feature: remove unused code

--- packages/renderer-worker/src/parts/GetSearchRegex/GetSearchRegex.js
+++
@@ -1,8 +0,0 @@
-import * as EscapeRegex from '../EscapeRegex/EscapeRegex.js'
-
-const regexFlags = 'gi'
-
-export const getSearchRegex = (searchString) => {
-  const escaped = EscapeRegex.escapeRegExpCharacters(searchString)
-  return new RegExp(escaped, regexFlags)
-}
