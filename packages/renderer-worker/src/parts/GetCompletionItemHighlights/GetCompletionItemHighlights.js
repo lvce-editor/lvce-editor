1a8e620bf61b0807ce7da9ceaedcb7176cc4473e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-16 20:25:37 +0200

    feature: remove unused code (#7009)

--- packages/renderer-worker/src/parts/GetCompletionItemHighlights/GetCompletionItemHighlights.js
+++
@@ -1,4 +0,0 @@
-export const getHighlights = (item, leadingWord) => {
-  const { matches } = item
-  return matches.slice(1)
-}
--- packages/renderer-worker/src/parts/GoToTypeDefinitionStrings/GoToTypeDefinitionStrings.js
+++
@@ -1,8 +0,0 @@
-import * as EditorStrings from '../EditorStrings/EditorStrings.js'
-
-export const getNoLocationFoundMessage = (info) => {
-  if (info.word) {
-    return EditorStrings.noTypeDefinitionFoundFor(info.word)
-  }
-  return EditorStrings.noTypeDefinitionFound()
-}
