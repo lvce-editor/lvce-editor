8712bcac04692f03f8f349f310b37d6f190567d8
Le Vivilet <levivlet@pm.me>
2024-10-17 23:34:30 +0200

    feature: add functions to query source actions in renderer worker

--- packages/renderer-worker/src/parts/GetEditorSourceActions/GetEditorSourceActions.js
+++ packages/renderer-worker/src/parts/GetEditorSourceActions/GetEditorSourceActions.js
@@ -1,6 +1,18 @@
 import * as EditorStrings from '../EditorStrings/EditorStrings.js'
+import * as GetExtensions from '../GetExtensions/GetExtensions.js'
+
+const getCodeActionsFromExtension = (extension) => {
+  // TODO
+  if (!extension) {
+    return []
+  }
+  return extension.codeActions
+}
 
 export const getEditorSourceActions = async () => {
+  const extensions = await GetExtensions.getExtensions()
+  const codeActions = extensions.flatMap(getCodeActionsFromExtension)
+  console.log({ codeActions })
   const sourceActions = [
     {
       name: EditorStrings.organizeImports(),
