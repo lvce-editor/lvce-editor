3af61c0fc64ecc7ad69fbca56539c3f5dc8d72bf
Le Vivilet <levivlet@pm.me>
2024-10-18 00:06:30 +0200

    feature: update source actions keybindings

--- packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextKeyBindings.js
+++ packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextKeyBindings.js
@@ -6,7 +6,7 @@ export const getKeyBindings = () => {
   return [
     {
       key: KeyCode.Escape,
-      command: 'EditorSourceActions.close',
+      command: 'Editor.closeSourceAction',
       when: WhenExpression.FocusSourceActions,
     },
     {
