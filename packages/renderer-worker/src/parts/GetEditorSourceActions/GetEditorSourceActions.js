12e9bb0d0c8585fa1f1f4d1c1bf62f2cbe7e32a1
Le Vivilet <levivlet@pm.me>
2024-10-17 23:42:36 +0200

    use extension source actions

--- packages/renderer-worker/src/parts/GetEditorSourceActions/GetEditorSourceActions.js
+++ packages/renderer-worker/src/parts/GetEditorSourceActions/GetEditorSourceActions.js
@@ -1,4 +1,3 @@
-import * as EditorStrings from '../EditorStrings/EditorStrings.js'
 import * as GetExtensions from '../GetExtensions/GetExtensions.js'
 
 const getCodeActionsFromExtension = (extension) => {
@@ -15,16 +14,5 @@ const getCodeActionsFromExtension = (extension) => {
 export const getEditorSourceActions = async () => {
   const extensions = await GetExtensions.getExtensions()
   const codeActions = extensions.flatMap(getCodeActionsFromExtension)
-  console.log({ codeActions })
-  const sourceActions = [
-    {
-      name: EditorStrings.organizeImports(),
-      command: 'Editor.organizeImports',
-    },
-    {
-      name: EditorStrings.sortImports(),
-      command: 'Editor.sortImports',
-    },
-  ]
-  return sourceActions
+  return codeActions
 }
