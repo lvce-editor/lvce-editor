83d6b3bec6e1853e5370082a7780aabb7264f219
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-18 21:54:14 +0200

    feature: move in memory file system to file search worker (#7031)

--- packages/renderer-worker/src/parts/FileSystem/FileSystemMemory.js
+++ packages/renderer-worker/src/parts/FileSystem/FileSystemMemory.js
@@ -1,62 +1,18 @@
-import * as DirentType from '../DirentType/DirentType.js'
+import * as FileSearchWorker from '../FileSearchWorker/FileSearchWorker.js'
 import * as PathSeparatorType from '../PathSeparatorType/PathSeparatorType.js'
 
 export const name = 'Memory'
 
-export const state = {
-  files: Object.create(null),
-}
-
-const getDirent = (uri) => {
-  return state.files[uri]
-}
-
 export const readFile = (uri) => {
-  const dirent = getDirent(uri)
-  if (!dirent) {
-    throw new Error(`File not found: ${uri}`)
-  }
-  if (dirent.type !== DirentType.File) {
-    throw new Error('file is a directory')
-  }
-  return dirent.content
-}
-
-const ensureParentDir = (uri) => {
-  const startIndex = 0
-  let endIndex = uri.indexOf(PathSeparatorType.Slash)
-  while (endIndex >= 0) {
-    const part = uri.slice(startIndex, endIndex + 1)
-    state.files[part] = {
-      type: DirentType.Directory,
-      content: '',
-    }
-    endIndex = uri.indexOf(PathSeparatorType.Slash, endIndex + 1)
-  }
+  return FileSearchWorker.invoke('FileSystemMemory.readFile', uri)
 }
 
 export const writeFile = (uri, content) => {
-  const dirent = getDirent(uri)
-  if (dirent) {
-    dirent.content = content
-  } else {
-    ensureParentDir(uri)
-    state.files[uri] = {
-      type: DirentType.File,
-      content,
-    }
-  }
+  return FileSearchWorker.invoke('FileSystemMemory.writeFile', uri, content)
 }
 
 export const mkdir = (uri) => {
-  if (!uri.endsWith(PathSeparatorType.Slash)) {
-    uri += PathSeparatorType.Slash
-  }
-  ensureParentDir(uri)
-  state.files[uri] = {
-    type: DirentType.Directory,
-    content: '',
-  }
+  return FileSearchWorker.invoke('FileSystemMemory.mkdir', uri)
 }
 
 export const getPathSeparator = () => {
@@ -64,62 +20,27 @@ export const getPathSeparator = () => {
 }
 
 export const remove = (uri) => {
-  const toDelete = []
-  for (const key of Object.keys(state.files)) {
-    if (key.startsWith(uri)) {
-      toDelete.push(key)
-    }
-  }
-  for (const key of toDelete) {
-    delete state.files[key]
-  }
+  return FileSearchWorker.invoke('FileSystemMemory.remove', uri)
 }
 
 export const readDirWithFileTypes = (uri) => {
-  if (!uri.endsWith(PathSeparatorType.Slash)) {
-    uri += PathSeparatorType.Slash
-  }
-  const dirents = []
-  for (const [key, value] of Object.entries(state.files)) {
-    if (key.startsWith(uri)) {
-      switch (value.type) {
-        case DirentType.Directory:
-          if (!key.slice(0, -1).includes(PathSeparatorType.Slash, uri.length) && key !== `${uri}/` && key !== uri) {
-            dirents.push({
-              type: value.type,
-              name: key.slice(uri.length, -1),
-            })
-          }
-          break
-        case DirentType.File:
-          if (!key.includes(PathSeparatorType.Slash, uri.length + 1)) {
-            dirents.push({
-              type: value.type,
-              name: key.slice(uri.length),
-            })
-          }
-          break
-        default:
-          break
-      }
-    }
-  }
-  return dirents
+  return FileSearchWorker.invoke('FileSystemMemory.readDirWithFileTypes', uri)
 }
 
 export const getBlobUrl = (uri) => {
-  const content = readFile(uri)
-  const blob = new Blob([content])
-  const url = URL.createObjectURL(blob)
-  return url
+  return FileSearchWorker.invoke('FileSystemMemory.getBlobUrl', uri)
 }
 
-export const getBlob = (uri) => {
-  const content = readFile(uri)
+export const getBlob = async (uri) => {
+  const content = await readFile(uri)
   const blob = new Blob([content])
   return blob
 }
 
 export const chmod = (path, permissions) => {
-  throw new Error('[memfs] chmod not implemented')
+  return FileSearchWorker.invoke('FileSystemMemory.chmod', path, permissions)
+}
+
+export const getFiles = () => {
+  return FileSearchWorker.invoke('FileSystemMemory.getFiles')
 }
