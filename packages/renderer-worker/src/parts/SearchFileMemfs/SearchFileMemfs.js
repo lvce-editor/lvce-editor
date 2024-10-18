dd72b6a4d856fa4ccd75e4eb885b42905e16e065
Le Vivilet <levivlet@pm.me>
2024-10-18 20:30:03 +0200

    feature: move in memory file system to file search worker

--- packages/renderer-worker/src/parts/SearchFileMemfs/SearchFileMemfs.js
+++ packages/renderer-worker/src/parts/SearchFileMemfs/SearchFileMemfs.js
@@ -1,7 +1,7 @@
 import * as FileSystemMemory from '../FileSystem/FileSystemMemory.js'
 
 export const searchFile = async (path, value) => {
-  const files = FileSystemMemory.state.files
+  const files = await FileSystemMemory.getFiles()
   const keys = Object.keys(files)
   return keys
 }
