1eed07270e71099ff7c8ca025ff6c326c9ce8e16
Le Vivilet <levivlet@pm.me>
2024-10-18 21:46:09 +0200

    feature: remove unused file search code

--- packages/renderer-worker/src/parts/SearchFileMemfs/SearchFileMemfs.js
+++
@@ -1,7 +0,0 @@
-import * as FileSystemMemory from '../FileSystem/FileSystemMemory.js'
-
-export const searchFile = async (path, value) => {
-  const files = FileSystemMemory.state.files
-  const keys = Object.keys(files)
-  return keys
-}
