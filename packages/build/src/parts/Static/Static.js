39bae32cdc6618bb0d0919c655c41c2a07a00cb9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-19 17:01:55 +0200

    Discard changes to packages/build/src/parts/Static/Static.js

--- packages/build/src/parts/Static/Static.js
+++ packages/build/src/parts/Static/Static.js
@@ -13,7 +13,6 @@ import * as BundleIframeWorkerCached from '../BundleIframeWorkerCached/BundleIfr
 import * as BundleFileSearchWorkerCached from '../BundleFileSearchWorkerCached/BundleFileSearchWorkerCached.js'
 import * as BundleTerminalWorkerCached from '../BundleTerminalWorkerCached/BundleTerminalWorkerCached.js'
 import * as BundleTestWorkerCached from '../BundleTestWorkerCached/BundleTestWorkerCached.js'
-import * as BundleWorkers from '../BundleWorkers/BundleWorkers.js'
 import * as CommitHash from '../CommitHash/CommitHash.js'
 import * as Console from '../Console/Console.js'
 import * as Copy from '../Copy/Copy.js'
@@ -419,13 +418,110 @@ const copyIconThemes = async ({ commitHash }) => {
 }
 
 const bundleJs = async ({ commitHash, platform, assetDir, version, date, product }) => {
-  await BundleWorkers.bundleWorkers({
+  const rendererProcessCachePath = await BundleRendererProcessCached.bundleRendererProcessCached({
     commitHash,
     platform,
     assetDir,
-    product,
+  })
+  await Copy.copy({
+    from: rendererProcessCachePath,
+    to: `packages/build/.tmp/dist/${commitHash}/packages/renderer-process`,
+    ignore: ['static'],
+  })
+  const rendererWorkerCachePath = await BundleRendererWorkerCached.bundleRendererWorkerCached({
+    commitHash,
+    platform,
+    assetDir,
     version,
     date,
+    product,
+  })
+  await Copy.copy({
+    from: rendererWorkerCachePath,
+    to: `packages/build/.tmp/dist/${commitHash}/packages/renderer-worker`,
+    ignore: ['static'],
+  })
+  const extensionHostWorkerCachePath = await BundleExtensionHostWorkerCached.bundleExtensionHostWorkerCached({
+    commitHash,
+    platform,
+    assetDir,
+  })
+  await Copy.copy({
+    from: extensionHostWorkerCachePath,
+    to: `packages/build/.tmp/dist/${commitHash}/packages/extension-host-worker`,
+  })
+  await BundleJs.bundleJs({
+    cwd: Path.absolute(`packages/build/.tmp/dist/${commitHash}/packages/extension-host-sub-worker`),
+    from: 'src/extensionHostSubWorkerMain.js',
+    platform: 'webworker',
+    codeSplitting: false,
+  })
+  const terminalWorkerCachePath = await BundleTerminalWorkerCached.bundleTerminalWorkerCached({
+    assetDir,
+    platform,
+    commitHash,
+  })
+  await Copy.copy({
+    from: terminalWorkerCachePath,
+    to: `packages/build/.tmp/dist/${commitHash}/packages/terminal-worker`,
+  })
+  const editorWorkerCachePath = await BundleEditorWorkerCached.bundleEditorWorkerCached({
+    assetDir,
+    platform,
+    commitHash,
+  })
+  await Copy.copy({
+    from: editorWorkerCachePath,
+    to: `packages/build/.tmp/dist/${commitHash}/packages/editor-worker`,
+  })
+  const testWorkerCachePath = await BundleTestWorkerCached.bundleTestWorkerCached({
+    assetDir,
+    commitHash,
+    platform,
+  })
+  await Copy.copy({
+    from: testWorkerCachePath,
+    to: `packages/build/.tmp/dist/${commitHash}/packages/test-worker`,
+  })
+  const diffWorkerCachePath = await BundleDiffWorkerCached.bundleDiffWorkerCached({
+    assetDir,
+    commitHash,
+    platform,
+  })
+  await Copy.copy({
+    from: diffWorkerCachePath,
+    to: `packages/build/.tmp/dist/${commitHash}/packages/diff-worker`,
+  })
+  const syntaxHighlightingWorkerCachePath = await BundleSyntaxHighlightingWorkerCached.bundleSyntaxHighlightingWorkerCached({
+    assetDir,
+    commitHash,
+    platform,
+  })
+  await Copy.copy({
+    from: syntaxHighlightingWorkerCachePath,
+    to: `packages/build/.tmp/dist/${commitHash}/packages/syntax-highlighting-worker`,
+  })
+  const iframeWorkerCachePath = await BundleIframeWorkerCached.bundleIframeWorkerCached({
+    assetDir,
+    commitHash,
+    platform,
+  })
+  await Copy.copy({
+    from: iframeWorkerCachePath,
+    to: `packages/build/.tmp/dist/${commitHash}/packages/iframe-worker`,
+  })
+  const fileSearchWorkerCachePath = await BundleFileSearchWorkerCached.bundleFileSearchWorkerCached({
+    assetDir,
+    commitHash,
+    platform,
+  })
+  await Copy.copy({
+    from: fileSearchWorkerCachePath,
+    to: `packages/build/.tmp/dist/${commitHash}/packages/file-search-worker`,
+  })
+  await Copy.copy({
+    from: 'packages/shared-process/node_modules/@lvce-editor/preview-process/files/previewInjectedCode.js',
+    to: `packages/build/.tmp/dist/${commitHash}/js/preview-injected.js`,
   })
 }
 
