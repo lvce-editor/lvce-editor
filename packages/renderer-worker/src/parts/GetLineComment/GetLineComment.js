31a60976bef95ae26749c46e6991f4c9b52b633f
Le Vivilet <levivlet@pm.me>
2024-10-19 19:33:37 +0200

    remove unused code

--- packages/renderer-worker/src/parts/GetLineComment/GetLineComment.js
+++
@@ -1,9 +0,0 @@
-import * as Languages from '../Languages/Languages.js'
-
-export const getLineComment = async (editor) => {
-  const languageConfiguration = await Languages.getLanguageConfiguration(editor)
-  if (!languageConfiguration || !languageConfiguration.comments || !languageConfiguration.comments.lineComment) {
-    return undefined
-  }
-  return languageConfiguration.comments.lineComment
-}
