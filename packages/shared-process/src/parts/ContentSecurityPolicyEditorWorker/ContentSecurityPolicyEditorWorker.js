3832c1b5a1acb68e8f4a4e826e6de9de22a71fda
Le Vivilet <levivlet@pm.me>
2024-10-19 08:50:39 +0200

    make csp stricter

--- packages/shared-process/src/parts/ContentSecurityPolicyEditorWorker/ContentSecurityPolicyEditorWorker.js
+++ packages/shared-process/src/parts/ContentSecurityPolicyEditorWorker/ContentSecurityPolicyEditorWorker.js
@@ -1,7 +1,3 @@
 import * as GetContentSecurityPolicy from '../GetContentSecurityPolicy/GetContentSecurityPolicy.js'
 
-export const value = GetContentSecurityPolicy.getContentSecurityPolicy([
-  `default-src 'none'`,
-  `font-src 'self'`,
-  `sandbox allow-scripts allow-same-origin`,
-])
+export const value = GetContentSecurityPolicy.getContentSecurityPolicy([`default-src 'none'`, `font-src 'self'`, `sandbox allow-same-origin`])
