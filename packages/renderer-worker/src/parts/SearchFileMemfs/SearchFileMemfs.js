88a29b952fb4725901228acb9c639dd765b8c1e1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-19 08:56:08 +0200

    feature: remove unused file search code (#7036)

--- packages/renderer-worker/src/parts/SearchFileMemfs/SearchFileMemfs.js
+++
@@ -1,7 +0,0 @@
-import * as FileSystemMemory from '../FileSystem/FileSystemMemory.js'
-
-export const searchFile = async (path, value) => {
-  const files = await FileSystemMemory.getFiles()
-  const keys = Object.keys(files)
-  return keys
-}
--- packages/renderer-worker/src/parts/SearchFileWithRipGrep/SearchFileWithRipGrep.js
+++
@@ -1,6 +0,0 @@
-import * as FileSearchWorker from '../FileSearchWorker/FileSearchWorker.js'
-
-export const searchFile = async (path, value, prepare) => {
-  const lines = await FileSearchWorker.invoke('SearchFile.searchFileWithRipGrep', path, value, prepare)
-  return lines
-}
