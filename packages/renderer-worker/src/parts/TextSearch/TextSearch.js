b36a03a3f9e21c480ea0f3bcdefb1bed81ef1957
Le Vivilet <levivlet@pm.me>
2024-10-19 22:36:09 +0200

    pass assetDir

--- packages/renderer-worker/src/parts/TextSearch/TextSearch.js
+++ packages/renderer-worker/src/parts/TextSearch/TextSearch.js
@@ -1,10 +1,11 @@
 import * as Assert from '../Assert/Assert.ts'
+import * as AssetDir from '../AssetDir/AssetDir.js'
 import * as TextSearchWorker from '../TextSearchWorker/TextSearchWorker.js'
 
 export const textSearch = async (root, query, options) => {
   Assert.string(root)
   Assert.string(query)
-  const results = await TextSearchWorker.invoke('TextSearch.textSearch', root, query, options)
+  const results = await TextSearchWorker.invoke('TextSearch.textSearch', root, query, options, AssetDir.assetDir)
   return results
 }
 
