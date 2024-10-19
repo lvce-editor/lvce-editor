99433427f59c911db03fbba3a29cc21d82c2ea99
Le Vivilet <levivlet@pm.me>
2024-10-19 19:35:27 +0200

    remove unused code

--- packages/renderer-worker/src/parts/SymbolName/SymbolName.js
+++
@@ -1,8 +0,0 @@
-export const SymbolProperty = 'SymbolProperty'
-export const SymbolValue = 'SymbolValue'
-export const SymbolFunction = 'SymbolFunction'
-export const SymbolVariable = 'SymbolVariable'
-export const SymbolKeyword = 'SymbolKeyword'
-export const SymbolDefault = 'SymbolDefault'
-export const SymbolField = 'SymbolField'
-export const SymbolNone = ''
