25a119b75b0815f142e99480a8f8c8e40520d4cc
Le Vivilet <levivlet@pm.me>
2024-10-19 08:46:04 +0200

    feature: add sandbox for syntax highlighting worker

--- packages/shared-process/src/parts/ContentSecurityPolicySyntaxHighlightingWorker/ContentSecurityPolicySyntaxHighlightingWorker.js
+++ packages/shared-process/src/parts/ContentSecurityPolicySyntaxHighlightingWorker/ContentSecurityPolicySyntaxHighlightingWorker.js
@@ -1,3 +1,7 @@
 import * as GetContentSecurityPolicy from '../GetContentSecurityPolicy/GetContentSecurityPolicy.js'
 
-export const value = GetContentSecurityPolicy.getContentSecurityPolicy([`default-src 'none'`, `script-src 'self'`])
+export const value = GetContentSecurityPolicy.getContentSecurityPolicy([
+  `default-src 'none'`,
+  `script-src 'self'`,
+  `sandbox allow-scripts allow-same-origin`,
+])
