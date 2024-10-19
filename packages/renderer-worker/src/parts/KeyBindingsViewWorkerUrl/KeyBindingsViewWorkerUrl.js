8aa06193698a7f24e1ecca775f02b61ee06da8c4
Le Vivilet <levivlet@pm.me>
2024-10-19 16:16:06 +0200

    feature: add function to launch keybindings view worker

---
+++ packages/renderer-worker/src/parts/KeyBindingsViewWorkerUrl/KeyBindingsViewWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const keyBindingsViewWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/keybindings-view/dist/keyBindingsViewWorkerMain.js`
