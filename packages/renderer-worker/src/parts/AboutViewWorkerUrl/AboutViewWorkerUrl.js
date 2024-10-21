8065e5d0024567efc84228d0b9e3548d038b7324
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-21 21:13:48 +0200

    feature: add function to launch about view worker (#7079)

---
+++ packages/renderer-worker/src/parts/AboutViewWorkerUrl/AboutViewWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const aboutViewWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/about-view/dist/aboutWorkerMain.js`
