1039876ac98eadae36c4efc83a412cb7833ba003
Le Vivilet <levivlet@pm.me>
2024-10-21 23:05:17 +0200

    fix

--- packages/renderer-worker/src/parts/ViewletConfirm/ViewletConfirmRender.js
+++ packages/renderer-worker/src/parts/ViewletConfirm/ViewletConfirmRender.js
@@ -1,6 +1,3 @@
-import * as GetAboutVirtualDom from '../GetAboutVirtualDom/GetAboutVirtualDom.js'
-import * as AboutStrings from '../AboutStrings/AboutStrings.js'
-
 export const hasFunctionalRender = true
 
 export const hasFunctionalRootRender = true
@@ -10,11 +7,7 @@ export const renderDialog = {
     return oldState.productName === newState.productName && oldState.lines === newState.lines
   },
   apply(oldState, newState) {
-    const okMessage = AboutStrings.ok()
-    const copyMessage = AboutStrings.copy()
-    const closeMessage = AboutStrings.closeDialog()
-    const infoMessage = AboutStrings.info()
-    const dom = GetAboutVirtualDom.getAboutVirtualDom(newState.productName, newState.lines, closeMessage, okMessage, copyMessage, infoMessage)
+    const dom = []
     return ['Viewlet.setDom2', dom]
   },
 }
