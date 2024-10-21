22f5389a7786bd13e7a5d8eb4af5b46f46357a11
Le Vivilet <levivlet@pm.me>
2024-10-21 21:04:24 +0200

    feature: add function to launch about view worker

---
+++ packages/renderer-worker/src/parts/LaunchAboutViewWorker/LaunchAboutViewWorker.js
@@ -0,0 +1,29 @@
+import * as AboutViewWorkerUrl from '../AboutViewWorkerUrl/AboutViewWorkerUrl.js'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcParent from '../IpcParent/IpcParent.js'
+import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as IsProduction from '../IsProduction/IsProduction.js'
+import * as Preferences from '../Preferences/Preferences.js'
+
+const getConfiguredWorkerUrl = () => {
+  let configuredWorkerUrl = Preferences.get('develop.aboutViewWorkerPath') || ''
+  if (configuredWorkerUrl) {
+    configuredWorkerUrl = '/remote' + configuredWorkerUrl
+  }
+  configuredWorkerUrl = configuredWorkerUrl || AboutViewWorkerUrl.aboutViewWorkerUrl
+  if (IsProduction.isProduction) {
+    configuredWorkerUrl = AboutViewWorkerUrl.aboutViewWorkerUrl
+  }
+  return configuredWorkerUrl
+}
+
+export const launchKeyBindingsViewWorker = async () => {
+  const name = 'About View Worker'
+  const ipc = await IpcParent.create({
+    method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
+    name,
+    url: getConfiguredWorkerUrl(),
+  })
+  HandleIpc.handleIpc(ipc)
+  return ipc
+}
