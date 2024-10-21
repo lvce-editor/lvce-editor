745f4b30d2de9db6b87d0ab9942bbf2f72982977
Le Vivilet <levivlet@pm.me>
2024-10-21 21:38:33 +0200

    build

--- packages/build/src/parts/BundleAboutViewWorker/BundleAboutViewWorker.js
+++ packages/build/src/parts/BundleAboutViewWorker/BundleAboutViewWorker.js
@@ -1,9 +1,15 @@
 import * as Copy from '../Copy/Copy.js'
 import * as Path from '../Path/Path.js'
+import * as Replace from '../Replace/Replace.js'
 
 export const bundleAboutViewWorker = async ({ cachePath, commitHash, platform, assetDir }) => {
   await Copy.copyFile({
     from: 'packages/renderer-worker/node_modules/@lvce-editor/about-view/dist/aboutWorkerMain.js',
     to: Path.join(cachePath, 'dist', 'aboutWorkerMain.js'),
+  })
+  await Replace.replace({
+    path: Path.join(cachePath, 'dist', 'aboutWorkerMain.js'),
+    occurrence: `const commit = ''`,
+    replacement: `const commit = '${commitHash}'`,
   })
 }
