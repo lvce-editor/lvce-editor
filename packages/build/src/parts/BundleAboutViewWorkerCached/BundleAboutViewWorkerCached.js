5372b4921082c82aebadd0f4dc623d4b2edf6971
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-22 19:18:36 +0200

    feature: improve about worker build (#7086)

--- packages/build/src/parts/BundleAboutViewWorkerCached/BundleAboutViewWorkerCached.js
+++ packages/build/src/parts/BundleAboutViewWorkerCached/BundleAboutViewWorkerCached.js
@@ -5,12 +5,12 @@ import * as Path from '../Path/Path.js'
 import * as Remove from '../Remove/Remove.js'
 
 export const bundleAboutViewWorkerCached = async ({ commitHash, platform, assetDir, date, product, version }) => {
-  const cachePath = await CachePaths.getAboutViewWorkerCachePath([platform])
+  const cachePath = await CachePaths.getAboutViewWorkerCachePath([platform, version, product, date])
   if (existsSync(cachePath)) {
     Logger.info('[build step skipped] bundleAboutViewWorker')
   } else {
     console.time('bundleAboutViewWorker')
-    await Remove.remove(Path.absolute('packages/build/.tmp/cachedSources/terminal-worker'))
+    await Remove.remove(Path.absolute('packages/build/.tmp/cachedSources/about-view-worker'))
     const BundleAboutViewWorker = await import('../BundleAboutViewWorker/BundleAboutViewWorker.js')
     await BundleAboutViewWorker.bundleAboutViewWorker({
       cachePath,
