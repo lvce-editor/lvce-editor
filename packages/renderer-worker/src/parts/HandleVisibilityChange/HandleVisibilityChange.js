9f63ab37eede13ef4c2f6072c9c6f66d3110a349
Le Vivilet <levivlet@pm.me>
2024-10-24 22:35:35 +0200

    feature: prepare to save extension state before reload

--- packages/renderer-worker/src/parts/HandleVisibilityChange/HandleVisibilityChange.js
+++ packages/renderer-worker/src/parts/HandleVisibilityChange/HandleVisibilityChange.js
@@ -2,18 +2,42 @@ import * as GetStateToSave from '../GetStateToSave/GetStateToSave.js'
 import * as InstanceStorage from '../InstanceStorage/InstanceStorage.js'
 import * as SessionStorage from '../SessionStorage/SessionStorage.js'
 import * as Workspace from '../Workspace/Workspace.js'
+import * as Timestamp from '../Timestamp/Timestamp.js'
+
+const getExtensionStateToSave = async () => {
+  const items = {
+    x: 1,
+  }
+  return items
+}
+
+const saveExtensionState = async () => {
+  const state = await getExtensionStateToSave()
+  await InstanceStorage.setJson('ExtensionState', state)
+}
+
+const saveBuiltinState = async () => {
+  const stateToSave = GetStateToSave.getStateToSave()
+  await Promise.all([
+    InstanceStorage.setJson('workspace', stateToSave.workspace),
+    InstanceStorage.setJsonObjects(stateToSave.instances),
+    SessionStorage.setJson('workspace', stateToSave.workspace),
+  ])
+}
 
 export const handleVisibilityChange = async (visibilityState) => {
   if (Workspace.isTest()) {
     return
   }
   if (visibilityState === 'hidden') {
-    const stateToSave = GetStateToSave.getStateToSave()
-    await Promise.all([
-      InstanceStorage.setJson('workspace', stateToSave.workspace),
-      InstanceStorage.setJsonObjects(stateToSave.instances),
-      SessionStorage.setJson('workspace', stateToSave.workspace),
-    ])
-    // console.log('[renderer worker] state was saved')
+    const builtinSaveStart = Timestamp.now()
+    await saveBuiltinState()
+    const builtinSaveEnd = Timestamp.now()
+    await saveExtensionState()
+    const extensionSaveEnd = Timestamp.now()
+    await InstanceStorage.setJson('Timings', {
+      builtinSaveTime: builtinSaveEnd - builtinSaveStart,
+      extensionSaveTime: extensionSaveEnd - builtinSaveEnd,
+    })
   }
 }
