91c074e670f90f9eb4266228ff24b2dc94eb76cb
Le Vivilet <levivlet@pm.me>
2024-10-25 20:21:36 +0200

    feature: use enum for extension storage key

--- packages/renderer-worker/src/parts/ExtensionStateStorage/ExtensionStateStorage.js
+++ packages/renderer-worker/src/parts/ExtensionStateStorage/ExtensionStateStorage.js
@@ -1,9 +1,10 @@
 import * as LocalStorage from '../LocalStorage/LocalStorage.js'
+import * as StorageKey from '../StorageKey/StorageKey.ts'
 
 export const getJson = () => {
-  return LocalStorage.getJson('ExtensionStateStorage')
+  return LocalStorage.getJson(StorageKey.ExtensionStateStorage)
 }
 
 export const setJson = (value) => {
-  return LocalStorage.setJson('ExtensionStateStorage', value)
+  return LocalStorage.setJson(StorageKey.ExtensionStateStorage, value)
 }
