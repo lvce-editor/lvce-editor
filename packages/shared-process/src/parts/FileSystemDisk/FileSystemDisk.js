9086e8353974bfdd631822613f51b0cd91825ab8
Le Vivilet <levivlet@pm.me>
2024-10-26 15:48:27 +0200

    feature: use file urls for disk file system

--- packages/shared-process/src/parts/FileSystemDisk/FileSystemDisk.js
+++ packages/shared-process/src/parts/FileSystemDisk/FileSystemDisk.js
@@ -7,59 +7,72 @@ import * as ErrorCodes from '../ErrorCodes/ErrorCodes.js'
 import { FileNotFoundError } from '../FileNotFoundError/FileNotFoundError.js'
 import * as GetDirentType from '../GetDirentType/GetDirentType.js'
 import * as IsEnoentError from '../IsEnoentError/IsEnoentError.js'
-import * as Path from '../Path/Path.js'
-import * as Platform from '../Platform/Platform.js'
 import * as Trash from '../Trash/Trash.js'
 import { VError } from '../VError/VError.js'
+import { fileURLToPath } from 'node:url'
 
-export const copy = async (source, target) => {
+const assertUri = (uri) => {
+  if (!uri.startsWith('file://')) {
+    throw new Error(`path must be a valid file uri`)
+  }
+}
+
+export const copy = async (sourceUri, targetUri) => {
   try {
+    assertUri(sourceUri)
+    assertUri(targetUri)
+    const source = fileURLToPath(sourceUri)
+    const target = fileURLToPath(targetUri)
     await fs.cp(source, target, { recursive: true })
   } catch (error) {
     if (error && error.message && error.message.startsWith('Invalid src or dest: cp returned EINVAL (src and dest cannot be the same)')) {
-      throw new VError(`Failed to copy "${source}" to "${target}": src and dest cannot be the same`)
+      throw new VError(`Failed to copy "${sourceUri}" to "${targetUri}": src and dest cannot be the same`)
     }
-    throw new VError(error, `Failed to copy "${source}" to "${target}"`)
+    throw new VError(error, `Failed to copy "${sourceUri}" to "${targetUri}"`)
   }
 }
 
 /**
  *
- * @param {string} path
+ * @param {string} uri
  * @param {BufferEncoding} encoding
  * @returns
  */
-export const readFile = async (path, encoding = EncodingType.Utf8) => {
+export const readFile = async (uri, encoding = EncodingType.Utf8) => {
   try {
-    Assert.string(path)
+    Assert.string(uri)
+    assertUri(uri)
+    const path = fileURLToPath(uri)
     const content = await fs.readFile(path, encoding)
     return content
   } catch (error) {
     if (IsEnoentError.isEnoentError(error)) {
-      throw new FileNotFoundError(path)
+      throw new FileNotFoundError(uri)
     }
-    throw new VError(error, `Failed to read file "${path}"`)
+    throw new VError(error, `Failed to read file "${uri}"`)
   }
 }
 
 /**
  *
- * @param {string} path
+ * @param {string} uri
  * @param {string} content
  * @param {BufferEncoding} encoding
  */
-export const writeFile = async (path, content, encoding = EncodingType.Utf8) => {
+export const writeFile = async (uri, content, encoding = EncodingType.Utf8) => {
   try {
-    Assert.string(path)
+    assertUri(uri)
+    Assert.string(uri)
     Assert.string(content)
+    const path = fileURLToPath(uri)
     // queue would be more correct for concurrent writes but also slower
     // Queue.add(`writeFile/${path}`, () =>
     await fs.writeFile(path, content, encoding)
   } catch (error) {
     if (IsEnoentError.isEnoentError(error)) {
-      throw new FileNotFoundError(path)
+      throw new FileNotFoundError(uri)
     }
-    throw new VError(error, `Failed to write to file "${path}"`)
+    throw new VError(error, `Failed to write to file "${uri}"`)
   }
 }
 
@@ -76,7 +89,9 @@ const isOkayToRemove = (path) => {
   return true
 }
 
-export const remove = async (path) => {
+export const remove = async (uri) => {
+  assertUri(uri)
+  const path = fileURLToPath(uri)
   if (!isOkayToRemove(path)) {
     console.warn('not removing path')
     return
@@ -87,11 +102,13 @@ export const remove = async (path) => {
   try {
     await Trash.trash(path)
   } catch (error) {
-    throw new VError(error, `Failed to remove "${path}"`)
+    throw new VError(error, `Failed to remove "${uri}"`)
   }
 }
 
-export const forceRemove = async (path) => {
+export const forceRemove = async (uri) => {
+  assertUri(uri)
+  const path = fileURLToPath(uri)
   if (!isOkayToRemove(path)) {
     console.warn('not removing path')
     return
@@ -99,13 +116,15 @@ export const forceRemove = async (path) => {
   try {
     await fs.rm(path, { force: true, recursive: true })
   } catch (error) {
-    throw new VError(error, `Failed to remove "${path}"`)
+    throw new VError(error, `Failed to remove "${uri}"`)
   }
 }
 
-export const exists = async (path) => {
+export const exists = async (uri) => {
   try {
-    await fs.access(path)
+    assertUri(uri)
+    const path = fileURLToPath(uri)
+    await fs.access(uri)
     return true
   } catch {
     return false
@@ -122,53 +141,62 @@ const toPrettyDirent = (dirent) => {
   }
 }
 
-export const readDirWithFileTypes = async (path) => {
+export const readDirWithFileTypes = async (uri) => {
   try {
+    assertUri(uri)
+    const path = fileURLToPath(uri)
     const dirents = await fs.readdir(path, { withFileTypes: true })
     const prettyDirents = dirents.map(toPrettyDirent)
     return prettyDirents
   } catch (error) {
-    throw new VError(error, `Failed to read directory "${path}"`)
+    throw new VError(error, `Failed to read directory "${uri}"`)
   }
 }
 
-export const readDir = async (path) => {
+export const readDir = async (uri) => {
   try {
+    const path = fileURLToPath(uri)
     const dirents = await fs.readdir(path)
     return dirents
   } catch (error) {
     if (IsEnoentError.isEnoentError(error)) {
-      throw new FileNotFoundError(path)
+      throw new FileNotFoundError(uri)
     }
-    throw new VError(error, `Failed to read directory "${path}"`)
+    throw new VError(error, `Failed to read directory "${uri}"`)
   }
 }
 
-export const mkdir = async (path) => {
+export const mkdir = async (uri) => {
   try {
+    assertUri(uri)
+    const path = fileURLToPath(uri)
     await fs.mkdir(path, { recursive: true })
   } catch (error) {
-    throw new VError(error, `Failed to create directory "${path}"`)
+    throw new VError(error, `Failed to create directory "${uri}"`)
   }
 }
 
-const fallbackRename = async (oldPath, newPath) => {
+const fallbackRename = async (oldUri, newUri) => {
   try {
+    const oldPath = fileURLToPath(oldUri)
+    const newPath = fileURLToPath(newUri)
     await fs.cp(oldPath, newPath, { recursive: true })
     await fs.rm(oldPath, { recursive: true })
   } catch (error) {
-    throw new VError(error, `Failed to rename "${oldPath}" to "${newPath}"`)
+    throw new VError(error, `Failed to rename "${oldUri}" to "${newUri}"`)
   }
 }
 
-export const rename = async (oldPath, newPath) => {
+export const rename = async (oldUri, newUri) => {
   try {
+    const oldPath = fileURLToPath(oldUri)
+    const newPath = fileURLToPath(newUri)
     await fs.rename(oldPath, newPath)
   } catch (error) {
     if (error && error.code === ErrorCodes.EXDEV) {
-      return fallbackRename(oldPath, newPath)
+      return fallbackRename(oldUri, newUri)
     }
-    throw new VError(error, `Failed to rename "${oldPath}" to "${newPath}"`)
+    throw new VError(error, `Failed to rename "${oldUri}" to "${newUri}"`)
   }
 }
 
@@ -177,28 +205,34 @@ export const getPathSeparator = () => {
 }
 
 // TODO handle error
-export const stat = async (path) => {
+export const stat = async (uri) => {
+  const path = fileURLToPath(uri)
   const stats = await fs.stat(path)
   const type = GetDirentType.getDirentType(stats)
   return type
 }
 
-export const chmod = async (path, permissions) => {
+export const chmod = async (uri, permissions) => {
+  const path = fileURLToPath(uri)
   await fs.chmod(path, permissions)
 }
 
-export const copyFile = async (from, to) => {
+export const copyFile = async (fromUri, toUri) => {
   try {
-    await fs.copyFile(from, to)
+    const fromPath = fileURLToPath(fromUri)
+    const toPath = fileURLToPath(toUri)
+    await fs.copyFile(fromPath, toPath)
   } catch (error) {
-    throw new VError(error, `Failed to copy file from ${from} to ${to}`)
+    throw new VError(error, `Failed to copy file from ${fromUri} to ${toUri}`)
   }
 }
 
-export const cp = async (from, to) => {
+export const cp = async (fromUri, toUri) => {
   try {
-    await fs.cp(from, to, { recursive: true })
+    const fromPath = fileURLToPath(fromUri)
+    const toPath = fileURLToPath(toUri)
+    await fs.cp(fromPath, toPath, { recursive: true })
   } catch (error) {
-    throw new VError(error, `Failed to copy folder from ${from} to ${to}`)
+    throw new VError(error, `Failed to copy folder from ${fromUri} to ${toUri}`)
   }
 }
