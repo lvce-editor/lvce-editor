c3707654eb6822196fb22b5639c41637e38d43fe
Le Vivilet <levivlet@pm.me>
2024-10-27 14:06:50 +0100

    todo

--- packages/renderer-worker/src/parts/GetTextEditorContent/GetTextEditorContent.js
+++ packages/renderer-worker/src/parts/GetTextEditorContent/GetTextEditorContent.js
@@ -2,6 +2,7 @@ import * as FileSystem from '../FileSystem/FileSystem.js'
 
 export const getTextEditorContent = async (uri) => {
   const content = await FileSystem.readFile(uri)
+  // TODO if content is not of type string, throw error
   const actualContent = content ?? 'content could not be loaded'
   return actualContent
 }
