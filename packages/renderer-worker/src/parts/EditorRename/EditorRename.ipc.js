a84dc2476ab347bfd091dffc5358d993d87dcc20
Le Vivilet <levivlet@pm.me>
2024-10-27 20:58:40 +0100

    feature: remove unused rename code

--- packages/renderer-worker/src/parts/EditorRename/EditorRename.ipc.js
+++
@@ -1,9 +0,0 @@
-import * as EditorRename from './EditorRename.js'
-
-export const name = 'EditorRename'
-
-export const Commands = {
-  abort: EditorRename.abort,
-  finish: EditorRename.finish,
-  open: EditorRename.open,
-}
