3bce636c7533aad6baa33a1009b1355341ddfa5e
Le Vivilet <levivlet@pm.me>
2024-10-27 13:26:32 +0100

    remove todo

--- packages/renderer-worker/src/parts/RenderTextEditorError/RenderTextEditorError.js
+++ packages/renderer-worker/src/parts/RenderTextEditorError/RenderTextEditorError.js
@@ -2,10 +2,6 @@ import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
 
 export const renderTextEditorError = (error) => {
-  // TODO
-  // 1. render icon
-  // 2. render error message
-  // 3. render error actions
   const errorString = `${error}`
   const createFile = `Create File`
   return [
