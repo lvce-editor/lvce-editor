6332319b7f792215636567bd27a38cc1f36c9c28
Le Vivilet <levivlet@pm.me>
2024-10-27 14:06:34 +0100

    fix missing return

--- packages/renderer-worker/src/parts/GetTextEditorContent/GetTextEditorContent.js
+++ packages/renderer-worker/src/parts/GetTextEditorContent/GetTextEditorContent.js
@@ -3,5 +3,5 @@ import * as FileSystem from '../FileSystem/FileSystem.js'
 export const getTextEditorContent = async (uri) => {
   const content = await FileSystem.readFile(uri)
   const actualContent = content ?? 'content could not be loaded'
-  actualContent
+  return actualContent
 }
