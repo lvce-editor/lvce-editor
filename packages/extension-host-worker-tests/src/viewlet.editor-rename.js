05ee904ae10eda27e54e23f7259364554a25ddc1
Le Vivilet <levivlet@pm.me>
2024-10-27 20:59:46 +0100

    feature: improve rename e2e test

--- packages/extension-host-worker-tests/src/viewlet.editor-rename.js
+++ packages/extension-host-worker-tests/src/viewlet.editor-rename.js
@@ -1,7 +1,5 @@
 export const name = 'viewlet.editor-rename'
 
-export const skip = true
-
 export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
@@ -18,29 +16,11 @@ export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expec
   await Editor.openRename()
 
   // assert
-  const findWidgetInput = Locator('.FindWidget .MultilineInputBox')
-  await expect(findWidgetInput).toBeVisible()
-  await expect(findWidgetInput).toHaveValue('content')
-  const findWidgetMatchCount = Locator(`.FindWidgetMatchCount`)
-  await expect(findWidgetMatchCount).toBeVisible()
-  await expect(findWidgetMatchCount).toHaveText('1 of 2')
+  const renameWidget = Locator('.EditorRename')
+  await expect(renameWidget).toBeVisible()
+  const renameInput = Locator('.RenameInputBox')
+  await expect(renameInput).toBeVisible()
 
-  // act
-  await FindWidget.focusNext()
-
-  // assert
-  await expect(findWidgetMatchCount).toHaveText('2 of 2')
-  const editorSelection = Locator('.EditorSelection')
-  await expect(editorSelection).toHaveCSS('top', '20px')
-  await expect(editorSelection).toHaveCSS('width', /^(65|66|67|68|69).*?px$/)
-
-  // act
-  await FindWidget.setValue('content-not-found')
-
-  // assert
-  await expect(findWidgetMatchCount).toHaveText('No Results')
-  const buttonPreviousMatch = Locator('[title="Previous Match"]')
-  await expect(buttonPreviousMatch).toHaveAttribute('disabled', '')
-  const buttonNextMatch = Locator('[title="Next Match"]')
-  await expect(buttonNextMatch).toHaveAttribute('disabled', '')
+  // TODO
+  // await expect(renameInput).toBeFocused()
 }
