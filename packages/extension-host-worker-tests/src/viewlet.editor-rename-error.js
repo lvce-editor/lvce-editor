a3260c8cad545a96660d89fff8471ef429d8a1a2
Le Vivilet <levivlet@pm.me>
2024-10-28 20:43:06 +0100

    feature: add closeRename keybindig

---
+++ packages/extension-host-worker-tests/src/viewlet.editor-rename-error.js
@@ -0,0 +1,29 @@
+export const name = 'viewlet.editor-rename-error'
+
+export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file.js`,
+    `let x = 1
+`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file.js`)
+  await Editor.setCursor(0, 5)
+
+  await Editor.openRename()
+  const renameWidget = Locator('.EditorRename')
+  await expect(renameWidget).toBeVisible()
+  const renameInput = Locator('.RenameInputBox')
+  await expect(renameInput).toBeVisible()
+
+  // act
+
+  // TODO
+  // 1. type a value
+  // 2. press enter
+  // 3. verify an error message occurs
+
+  // assert
+}
