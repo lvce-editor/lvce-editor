70edfe2d927377931cf9de7a79bd431e3c24bcf0
Le Vivilet <levivlet@pm.me>
2024-10-28 20:07:39 +0100

    feature: enable source action e2e test

--- packages/extension-host-worker-tests/src/viewlet.editor-source-actions.js
+++ packages/extension-host-worker-tests/src/viewlet.editor-source-actions.js
@@ -1,5 +1,3 @@
-export const skip = true
-
 export const name = 'viewlet.editor-source-actions'
 
 export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, Extension }) => {
