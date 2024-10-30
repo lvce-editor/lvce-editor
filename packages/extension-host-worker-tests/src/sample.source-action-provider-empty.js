f3696cc5a8c379398db79837d737f462b9df1544
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-30 19:47:06 +0100

    feature: add source actions empty e2e test (#7176)

---
+++ packages/extension-host-worker-tests/src/sample.source-action-provider-empty.js
@@ -0,0 +1,25 @@
+export const name = 'sample.source-action-provider-empty'
+
+export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, Extension }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file.txt`,
+    `hello world
+`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+  await Main.openUri(`${tmpDir}/file.txt`)
+
+  // act
+  await Editor.setSelections(new Uint32Array([0, 0, 0, 0]))
+  await Editor.openSourceActions()
+
+  // assert
+  const sourceActionsView = Locator('.EditorSourceActions')
+  await expect(sourceActionsView).toBeVisible()
+
+  // TODO
+  // await expect(sourceActionsView).toHaveText('No code Actions available')
+}
