1da7b9d2c8698c59da5e64e90f88a394c5fc9a1f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-03 16:07:46 +0100

    feature: remove unused text search code (#7220)

--- packages/renderer-worker/src/parts/TextSearch/TextSearchExtension.js
+++
@@ -1,9 +0,0 @@
-import * as Assert from '../Assert/Assert.ts'
-import * as ExtensionHostTextSearch from '../ExtensionHost/ExtensionHostTextSearch.js'
-
-export const textSearch = async (scheme, root, query) => {
-  Assert.string(scheme)
-  Assert.string(query)
-  const results = await ExtensionHostTextSearch.executeTextSearchProvider(scheme, query)
-  return results
-}
