2762e4153307715e216375beaf036b645e57854e
Le Vivilet <levivlet@pm.me>
2024-11-05 23:01:03 +0100

    feature: remove unused formatting code

--- packages/renderer-worker/src/parts/OrganizeImports/OrganizeImports.js
+++
@@ -1,7 +0,0 @@
-import * as Assert from '../Assert/Assert.ts'
-import * as ExtensionHostOrganizeImports from '../ExtensionHost/ExtensionHostOrganizeImports.js'
-
-export const organizeImports = (editor) => {
-  Assert.object(editor)
-  return ExtensionHostOrganizeImports.executeOrganizeImports(editor)
-}
