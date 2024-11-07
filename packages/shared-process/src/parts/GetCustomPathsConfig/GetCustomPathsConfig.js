098b118112e619f11d1c73bd20329ce66647f0b5
Le Vivilet <levivlet@pm.me>
2024-11-07 18:36:03 +0100

    simplify fn

--- packages/shared-process/src/parts/GetCustomPathsConfig/GetCustomPathsConfig.js
+++ packages/shared-process/src/parts/GetCustomPathsConfig/GetCustomPathsConfig.js
@@ -1,19 +1,15 @@
-import { pathToFileURL } from 'node:url'
-
-const getRemoteUrl = (path) => {
-  return '/remote' + pathToFileURL(path).toString().slice(7)
-}
+import * as GetRemoteUrl from '../GetRemoteUrl/GetRemoteUrl.js'
 
 export const getCustomPathsConfig = (preferences) => {
   const config = Object.create(null)
   if (preferences['develop.rendererProcessPath']) {
-    config.rendererProcessPath = getRemoteUrl(preferences['develop.rendererProcessPath'])
+    config.rendererProcessPath = GetRemoteUrl.getRemoteUrl(preferences['develop.rendererProcessPath'])
   }
   if (preferences['develop.extensionHostWorkerPath']) {
-    config.extensionHostWorkerUrl = getRemoteUrl(preferences['develop.extensionHostWorkerPath'])
+    config.extensionHostWorkerUrl = GetRemoteUrl.getRemoteUrl(preferences['develop.extensionHostWorkerPath'])
   }
   if (preferences['develop.editorWorkerPath']) {
-    config.editorWorkerUrl = getRemoteUrl(preferences['develop.editorWorkerPath'])
+    config.editorWorkerUrl = GetRemoteUrl.getRemoteUrl(preferences['develop.editorWorkerPath'])
   }
   return config
 }
