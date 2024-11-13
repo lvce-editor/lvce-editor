3a8e71b2286fae4ad44235e9cdd4ffdf80b31ba6
Le Vivilet <levivlet@pm.me>
2024-11-13 21:42:17 +0100

    fix args

--- packages/renderer-worker/src/parts/ViewletSearch/WrapTextSearchCommand.ts
+++ packages/renderer-worker/src/parts/ViewletSearch/WrapTextSearchCommand.ts
@@ -2,7 +2,7 @@ import * as TextSearchWorker from '../TextSearchWorker/TextSearchWorker.js'
 
 export const wrapTextSearchCommand = (key) => {
   const fn = async (state, ...args) => {
-    await TextSearchWorker.invoke(`TextSearch.${key}`, state.uid, args)
+    await TextSearchWorker.invoke(`TextSearch.${key}`, state.uid, ...args)
     const commands = await TextSearchWorker.invoke('TextSearch.render', state.uid)
     return {
       ...state,
