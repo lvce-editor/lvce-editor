a04836fd2ba23b51d41f2bbd348c21362d218a21
Le Vivilet <levivlet@pm.me>
2024-11-14 18:14:21 +0100

    feature: remove unused text search code

--- packages/renderer-worker/src/parts/TextSearchReplaceAll/TextSearchReplaceAll.js
+++
@@ -1,7 +0,0 @@
-import * as Command from '../Command/Command.js'
-import * as GetBulkReplacementEdits from '../GetBulkReplacementEdits/GetBulkReplacementEdits.js'
-
-export const replaceAll = async (workspacePath, items, replacement) => {
-  const { files, ranges } = GetBulkReplacementEdits.getBulkReplacementEdits(workspacePath, items)
-  await Command.execute('BulkReplacement.applyBulkReplacement', files, ranges, replacement)
-}
