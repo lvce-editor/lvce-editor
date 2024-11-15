97b2883fa5b37da8f14bebc32207b9b496fe125d
Le Vivilet <levivlet@pm.me>
2024-11-15 23:34:54 +0100

    convert uri to path

--- packages/shared-process/src/parts/StaticPath/StaticPath.js
+++ packages/shared-process/src/parts/StaticPath/StaticPath.js
@@ -1,11 +1,13 @@
 import { dirname, join } from 'node:path'
 import * as IsBuiltServer from '../IsBuiltServer/IsBuiltServer.js'
 import * as Root from '../Root/Root.js'
+import { fileURLToPath } from 'node:url'
 
 export const getStaticPath = () => {
   if (IsBuiltServer.isBuiltServer) {
     // TODO maybe move static files to separate package
-    const serverIndexPath = import.meta.resolve('@lvce-editor/server')
+    const serverIndexUri = import.meta.resolve('@lvce-editor/server')
+    const serverIndexPath = fileURLToPath(serverIndexUri)
     const serverPath = dirname(serverIndexPath)
     const staticPath = join(serverPath, 'static')
     return staticPath
