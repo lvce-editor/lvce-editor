ac8d224cd472aed7ef08b8ba39e41f9ea2818bbd
Le Vivilet <levivlet@pm.me>
2024-11-16 17:57:52 +0100

    feature: return status 500 response when an error occurs on tests page

--- packages/shared-process/src/parts/GetTestRequestResponse/GetTestRequestResponse.js
+++ packages/shared-process/src/parts/GetTestRequestResponse/GetTestRequestResponse.js
@@ -8,47 +8,58 @@ import * as CrossOriginResourcePolicy from '../CrossOriginResourcePolicy/CrossOr
 import * as ContentSecurityPolicyDocument from '../ContentSecurityPolicyDocument/ContentSecurityPolicyDocument.js'
 import * as GetPathName from '../GetPathName/GetPathName.js'
 import * as GetTestPath from '../GetTestPath/GetTestPath.js'
+import * as Logger from '../Logger/Logger.js'
 import * as HttpHeader from '../HttpHeader/HttpHeader.js'
 import * as HttpStatusCode from '../HttpStatusCode/HttpStatusCode.js'
 
 export const getTestRequestResponse = async (request, indexHtmlPath) => {
-  const pathName = GetPathName.getPathName(request)
-  if (pathName.endsWith('.html')) {
-    const body = await readFile(indexHtmlPath, 'utf8')
-    const content = await AddCustomPathsToIndexHtml.addCustomPathsToIndexHtml(body)
-    return {
-      body: content,
-      init: {
-        status: HttpStatusCode.Ok,
-        headers: {
-          [HttpHeader.CrossOriginEmbedderPolicy]: CrossOriginEmbedderPolicy.value,
-          [HttpHeader.CrossOriginResourcePolicy]: CrossOriginResourcePolicy.value,
-          [HttpHeader.ContentSecurityPolicy]: ContentSecurityPolicyDocument.value,
+  try {
+    const pathName = GetPathName.getPathName(request)
+    if (pathName.endsWith('.html')) {
+      const body = await readFile(indexHtmlPath, 'utf8')
+      const content = await AddCustomPathsToIndexHtml.addCustomPathsToIndexHtml(body)
+      return {
+        body: content,
+        init: {
+          status: HttpStatusCode.Ok,
+          headers: {
+            [HttpHeader.CrossOriginEmbedderPolicy]: CrossOriginEmbedderPolicy.value,
+            [HttpHeader.CrossOriginResourcePolicy]: CrossOriginResourcePolicy.value,
+            [HttpHeader.ContentSecurityPolicy]: ContentSecurityPolicyDocument.value,
+          },
         },
+      }
+    }
+    if (pathName === '/tests/') {
+      const testPath = GetTestPath.getTestPath()
+      const testPathSrc = join(testPath, 'src')
+      const body = await CreateTestOverview.createTestOverview(testPathSrc)
+      return {
+        body,
+        init: {
+          status: HttpStatusCode.MultipleChoices,
+          headers: {
+            [HttpHeader.CacheControl]: 'public, max-age=0, must-revalidate',
+            [HttpHeader.CrossOriginEmbedderPolicy]: CrossOriginEmbedderPolicy.value,
+            [HttpHeader.CrossOriginOpenerPolicy]: CrossOriginOpenerPolicy.value,
+            [HttpHeader.ContentSecurityPolicy]: "default-src 'none'",
+          },
+        },
+      }
+    }
+    return {
+      body: 'not-found',
+      init: {
+        status: HttpStatusCode.NotFound,
       },
     }
-  }
-  if (pathName === '/tests/') {
-    const testPath = GetTestPath.getTestPath()
-    const testPathSrc = join(testPath, 'src')
-    const body = await CreateTestOverview.createTestOverview(testPathSrc)
+  } catch (error) {
+    Logger.error(error)
     return {
-      body,
+      body: 'Internal server error',
       init: {
-        status: HttpStatusCode.MultipleChoices,
-        headers: {
-          [HttpHeader.CacheControl]: 'public, max-age=0, must-revalidate',
-          [HttpHeader.CrossOriginEmbedderPolicy]: CrossOriginEmbedderPolicy.value,
-          [HttpHeader.CrossOriginOpenerPolicy]: CrossOriginOpenerPolicy.value,
-          [HttpHeader.ContentSecurityPolicy]: "default-src 'none'",
-        },
+        status: HttpStatusCode.ServerError,
       },
     }
-  }
-  return {
-    body: 'not-found',
-    init: {
-      status: HttpStatusCode.NotFound,
-    },
   }
 }
