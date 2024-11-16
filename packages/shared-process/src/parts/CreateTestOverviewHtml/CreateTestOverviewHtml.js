adf1a16e98f179b6eeb100c40e1d525587a73494
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-16 18:17:24 +0100

    feature: move createTestOverviewHtml into separate file (#7279)

---
+++ packages/shared-process/src/parts/CreateTestOverviewHtml/CreateTestOverviewHtml.js
@@ -0,0 +1,29 @@
+export const createTestOverviewHtml = (dirents) => {
+  const pre = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tests</title>
+  </head>
+  <body>
+    <h1>Tests</h1>
+    <p>Available Tests</p>
+    <ul>
+`
+  let middle = ``
+  // TODO properly escape name
+  for (const dirent of dirents) {
+    if (dirent.endsWith('.js') && !dirent.startsWith('_')) {
+      const name = dirent.slice(0, -'.js'.length)
+      middle += `      <li><a href="./${name}.html">${name}</a></li>
+`
+    }
+  }
+
+  const post = `    </ul>
+  </body>
+</html>
+`
+  return pre + middle + post
+}
