09c3baafd1e0fae09d5fc448d38c4a3229991f38
Le Vivilet <levivlet@pm.me>
2024-11-19 18:46:51 +0100

    feature: make saveState function async to allow workers to send their state

--- packages/renderer-worker/src/parts/GetStateToSave/GetStateToSave.js
+++ packages/renderer-worker/src/parts/GetStateToSave/GetStateToSave.js
@@ -2,9 +2,9 @@ import * as SerializeViewlet from '../SerializeViewlet/SerializeViewlet.js'
 import * as ViewletStates from '../ViewletStates/ViewletStates.js'
 import * as Workspace from '../Workspace/Workspace.js'
 
-export const getStateToSave = () => {
+export const getStateToSave = async () => {
   const instances = ViewletStates.getAllInstances()
-  const savedInstances = SerializeViewlet.serializeInstances(instances)
+  const savedInstances = await SerializeViewlet.serializeInstances(instances)
   return {
     instances: savedInstances,
     mainEditors: [],
