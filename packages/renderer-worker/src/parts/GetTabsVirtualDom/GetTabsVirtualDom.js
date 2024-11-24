04bd136ff03b5f420c4b05f2113d7e5d1098e393
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-24 17:18:59 +0100

    feature: prepare to add drag and drop event listeners for tabs (#7350)

--- packages/renderer-worker/src/parts/GetTabsVirtualDom/GetTabsVirtualDom.js
+++ packages/renderer-worker/src/parts/GetTabsVirtualDom/GetTabsVirtualDom.js
@@ -1,86 +1,4 @@
-import * as AriaRoles from '../AriaRoles/AriaRoles.js'
-import * as ClassNames from '../ClassNames/ClassNames.js'
-import * as GetFileIconVirtualDom from '../GetFileIconVirtualDom/GetFileIconVirtualDom.js'
-import * as TabFlags from '../TabFlags/TabFlags.js'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
-import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
-
-const getIconDom = (icon) => {
-  if (icon.startsWith('MaskIcon')) {
-    return {
-      type: VirtualDomElements.Div,
-      className: `TabIcon ${icon}`,
-      childCount: 0,
-    }
-  }
-  return GetFileIconVirtualDom.getFileIconVirtualDom(icon)
-}
-
-const getTabDom = (tab, isActive, fixedWidth) => {
-  const { icon, tabWidth, uri, flags } = tab
-  let tabClassName = ClassNames.MainTab
-  if (isActive) {
-    tabClassName += ' ' + ClassNames.MainTabSelected
-  }
-  const isDirty = flags & TabFlags.Dirty
-  // @ts-ignore
-  const isHovered = flags & TabFlags.Hovered
-  const actualTabWidth = fixedWidth || tabWidth
-  const tabElement = {
-    type: VirtualDomElements.Div,
-    className: tabClassName,
-    role: AriaRoles.Tab,
-    draggable: true,
-    width: actualTabWidth,
-    ariaSelected: isActive,
-    title: uri,
-    childCount: 2,
-  }
-  const dom = []
-
-  dom.push(
-    tabElement,
-    getIconDom(icon),
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.TabLabel,
-      childCount: 1,
-    },
-    text(tab.label),
-  )
-
-  if (isDirty) {
-    tabElement.childCount++
-    dom.push(
-      {
-        type: VirtualDomElements.Div,
-        className: ClassNames.EditorTabCloseButton,
-        childCount: 1,
-      },
-      {
-        type: VirtualDomElements.Div,
-        className: 'MaskIcon MaskIconCircleFilled',
-        childCount: 0,
-      },
-    )
-  } else {
-    tabElement.childCount++
-    dom.push(
-      {
-        type: VirtualDomElements.Button,
-        className: ClassNames.EditorTabCloseButton,
-        title: 'Close',
-        childCount: 1,
-      },
-      {
-        type: VirtualDomElements.Div,
-        className: 'MaskIcon MaskIconClose',
-        childCount: 0,
-      },
-    )
-  }
-  return dom
-}
+import * as GetTabDom from '../GetTabDom/GetTabDom.js'
 
 const getTotalTabWidth = (tabs) => {
   let total = 0
@@ -105,7 +23,7 @@ export const getTabsDom = (tabs, width, activeIndex, deltaX) => {
   for (let i = 0; i < tabs.length; i++) {
     const isActive = i === activeIndex
     const tab = tabs[i]
-    tabsDom.push(...getTabDom(tab, isActive, fixedWidth))
+    tabsDom.push(...GetTabDom.getTabDom(tab, isActive, fixedWidth))
   }
   return tabsDom
 }
