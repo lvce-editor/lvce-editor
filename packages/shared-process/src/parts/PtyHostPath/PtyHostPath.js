1f743e3ad5ac83092f3b3e8ec67b790482dd4d9f
Le Vivilet <levivlet@pm.me>
2024-11-26 22:57:21 +0100

    fix: pty host path

--- packages/shared-process/src/parts/PtyHostPath/PtyHostPath.js
+++ packages/shared-process/src/parts/PtyHostPath/PtyHostPath.js
@@ -1,4 +1,4 @@
 import * as Path from '../Path/Path.js'
 import * as Root from '../Root/Root.js'
 
-export const ptyHostPath = Path.join(Root.root, 'packages', 'shared-process', 'node_modules', '@lvce-editor', 'pty-host', 'src', 'ptyHostMain.js')
+export const ptyHostPath = Path.join(Root.root, 'packages', 'shared-process', 'node_modules', '@lvce-editor', 'pty-host', 'dist', 'ptyHostMain.js')
