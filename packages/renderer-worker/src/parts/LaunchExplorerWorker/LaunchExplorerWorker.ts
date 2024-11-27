2cc9f3bbe79e72e68f861a95a0720f94fc31daff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-27 23:45:41 +0100

    feature: add function to launch explorer worker (#7377)

---
+++ packages/renderer-worker/src/parts/LaunchExplorerWorker/LaunchExplorerWorker.ts
@@ -0,0 +1,16 @@
+import * as GetConfiguredWorkerUrl from '../GetConfiguredWorkerUrl/GetConfiguredWorkerUrl.ts'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcParent from '../IpcParent/IpcParent.js'
+import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as ExplorerWorkerUrl from '../ExplorerWorkerUrl/ExplorerWorkerUrl.js'
+
+export const launchExplorerWorker = async () => {
+  const configuredWorkerUrl = GetConfiguredWorkerUrl.getConfiguredWorkerUrl('develop.explorerWorkerPath', ExplorerWorkerUrl.explorerWorkerUrl)
+  const ipc = await IpcParent.create({
+    method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
+    url: configuredWorkerUrl,
+    name: 'Explorer Worker',
+  })
+  HandleIpc.handleIpc(ipc)
+  return ipc
+}
