8f02fcc1f8975d37bc72a287abd40602a746cb6d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-30 09:23:34 +0100

    feature: remove unused video view (#7409)

--- packages/renderer-worker/src/parts/ViewletVideo/ViewletVideo.ipc.ts
+++
@@ -1,6 +0,0 @@
-export const name = 'Video'
-
-export * from './ViewletVideo.ts'
-export * from './ViewletVideoCommands.ts'
-export * from './ViewletVideoCss.ts'
-export * from './ViewletVideoRender.ts'
