55b2afa1eba5527193127c28a52f654c29b31f7a
Le Vivilet <levivlet@pm.me>
2024-12-04 21:25:08 +0100

    feature: allow calling ExtensionManagement.getExtensions externally

---
+++ packages/renderer-worker/src/parts/ExtensionManagement/ExtensionManagement.ipc.js
@@ -0,0 +1,7 @@
+import * as ExtensionManagement from './ExtensionManagement.js'
+
+export const name = 'ExtensionManagement'
+
+export const Commands = {
+  getAllExtensions: ExtensionManagement.getAllExtensions,
+}
