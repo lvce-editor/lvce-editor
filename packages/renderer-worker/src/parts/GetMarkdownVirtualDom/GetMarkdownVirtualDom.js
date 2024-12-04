a633aa5a76cc32762b834cbb17e8fb0ff7e8e866
Le Vivilet <levivlet@pm.me>
2024-12-04 21:32:09 +0100

    remove unused

--- packages/renderer-worker/src/parts/GetMarkdownVirtualDom/GetMarkdownVirtualDom.js
+++
@@ -1,9 +0,0 @@
-import * as AllowedMarkdownAttributes from '../AllowedMarkdownAttributes/AllowedMarkdownAttributes.js'
-import * as Assert from '../Assert/Assert.ts'
-import * as ParseHtml from '../ParseHtml/ParseHtml.js'
-
-export const getMarkdownVirtualDom = (html) => {
-  Assert.string(html)
-  const dom = ParseHtml.parseHtml(html, AllowedMarkdownAttributes.allowedMarkdownAttributes)
-  return dom
-}
