1370d87b2771f65df344b51ab535cc6647e2107f
Le Vivilet <levivlet@pm.me>
2024-12-04 21:34:02 +0100

    remove unused

--- packages/renderer-worker/src/parts/LoadMarked/LoadMarked.js
+++
@@ -1,7 +0,0 @@
-import * as AssetDir from '../AssetDir/AssetDir.js'
-
-export const loadMarked = async () => {
-  const url = `${AssetDir.assetDir}/js/marked.js`
-  const module = await import(url)
-  return module.marked
-}
