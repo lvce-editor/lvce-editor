464f89cdbd43da11c9f395d1f547705d6c3deca2
Le Vivilet <levivlet@pm.me>
2024-12-04 22:32:23 +0100

    remove unused

--- packages/renderer-worker/src/parts/Markdown/Markdown.js
+++
@@ -1,9 +0,0 @@
-import * as LoadMarked from '../LoadMarked/LoadMarked.js'
-
-export const toHtml = async (markdown, { baseUrl = '' } = {}) => {
-  const marked = await LoadMarked.loadMarked()
-  const html = marked(markdown, {
-    baseUrl,
-  })
-  return html
-}
