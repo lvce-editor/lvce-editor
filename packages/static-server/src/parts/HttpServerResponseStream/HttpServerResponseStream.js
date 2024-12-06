3799b3a065874c694120f0e49ab1e95d5ff1164a
Le Vivilet <levivlet@pm.me>
2024-12-07 00:40:01 +0100

    fix ttf

--- packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
+++ packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
@@ -3,6 +3,7 @@ import { ServerResponse } from 'node:http'
 import { pipeline } from 'node:stream/promises'
 import * as Assert from '../Assert/Assert.js'
 import * as GetHeaders from '../GetHeaders/GetHeaders.js'
+import { readFile } from 'node:fs/promises'
 
 export const send = async (request, socket, filePath) => {
   const response = new ServerResponse(request)
@@ -13,6 +14,11 @@ export const send = async (request, socket, filePath) => {
     response.assignSocket(socket)
     const headers = GetHeaders.getHeaders(filePath)
     response.writeHead(200, headers)
+    if (filePath.endsWith('.ttf')) {
+      const content = await readFile(filePath)
+      response.end(content)
+      return
+    }
     const stream = createReadStream(filePath)
     await pipeline(stream, response)
   } catch (error) {
