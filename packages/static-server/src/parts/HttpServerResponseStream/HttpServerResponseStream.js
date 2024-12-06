183827cec6c2bc6a75256d71007167f3342ae492
Le Vivilet <levivlet@pm.me>
2024-12-07 00:56:33 +0100

    debug font

--- packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
+++ packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
@@ -1,14 +1,14 @@
 import { createReadStream } from 'node:fs'
 import { readFile } from 'node:fs/promises'
 import { ServerResponse } from 'node:http'
-import { pipeline } from 'node:stream/promises'
+import { pipeline, finished } from 'node:stream/promises'
 import * as Assert from '../Assert/Assert.js'
 import * as GetHeaders from '../GetHeaders/GetHeaders.js'
 
 const supportsStream = (filePath) => {
-  if (filePath.endsWith('.ttf')) {
-    return false
-  }
+  // if (filePath.endsWith('.ttf')) {
+  //   return false
+  // }
   return true
 }
 
@@ -27,7 +27,35 @@ export const send = async (request, socket, filePath) => {
       return
     }
     const stream = createReadStream(filePath)
-    await pipeline(stream, response)
+    if (filePath.endsWith('.ttf')) {
+      console.log('before font stream')
+      stream.on('close', () => {
+        console.log('read close')
+      })
+      stream.on('end', () => {
+        console.log('stream end')
+      })
+      stream.on('data', (x) => {
+        console.log(x)
+        console.log('stream data')
+      })
+      response.on('finish', () => {
+        console.log('response finish')
+      })
+      response.on('close', () => {
+        console.log('response closed')
+      })
+      // await finished(stream)
+      // console.log('did finish')
+    }
+    await pipeline(stream, response, { end: false })
+    response.end()
+    if (filePath.endsWith('.ttf')) {
+      await new Promise((r) => {
+        setTimeout(r, 2000)
+      })
+      console.log('after font stream')
+    }
   } catch (error) {
     console.error(`[response error] ${request.url} ${error}`)
     response.statusCode = 500
