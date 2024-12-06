1c932d48b356fa3d6527e335a32d3965721af407
Le Vivilet <levivlet@pm.me>
2024-12-06 22:56:12 +0100

    feature: add getPathEtag helper function

---
+++ packages/shared-process/src/parts/GetPathEtag/GetPathEtag.js
@@ -0,0 +1,12 @@
+import { stat } from 'node:fs/promises'
+import * as GetEtag from '../GetEtag/GetEtag.js'
+
+export const getPathEtag = async (absolutePath) => {
+  let stats = await stat(absolutePath)
+  if (stats.isDirectory()) {
+    absolutePath += '/index.html'
+    stats = await stat(absolutePath)
+  }
+  const etag = GetEtag.getEtag(stats)
+  return etag
+}
