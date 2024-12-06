ca654188b36f15e5979492356b9802f6bd775c07
Le Vivilet <levivlet@pm.me>
2024-12-07 00:39:04 +0100

    simplify code

--- packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
+++ packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
@@ -3,7 +3,6 @@ import { ServerResponse } from 'node:http'
 import { pipeline } from 'node:stream/promises'
 import * as Assert from '../Assert/Assert.js'
 import * as GetHeaders from '../GetHeaders/GetHeaders.js'
-import * as SetHeaders from '../SetHeaders/SetHeaders.js'
 
 export const send = async (request, socket, filePath) => {
   const response = new ServerResponse(request)
@@ -12,10 +11,8 @@ export const send = async (request, socket, filePath) => {
     Assert.object(socket)
     Assert.string(filePath)
     response.assignSocket(socket)
-    // TODO etag caching
-    response.statusCode = 200
     const headers = GetHeaders.getHeaders(filePath)
-    SetHeaders.setHeaders(response, headers)
+    response.writeHead(200, headers)
     const stream = createReadStream(filePath)
     await pipeline(stream, response)
   } catch (error) {
