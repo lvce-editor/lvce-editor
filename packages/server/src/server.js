ccdc78a7954d0681dbdc0a6736597a24ca98cc51
Le Vivilet <levivlet@pm.me>
2024-12-07 00:12:44 +0100

    remove unused code

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -413,12 +413,12 @@ app.use('/packages', servePackages, serve404())
 app.use('/', servePackages, serve404())
 
 // serve static files using static server
-app.use('/css', serveCss, serve404())
-app.use('/fonts', serveCss, serve404())
-app.use('/themes', serveCss, serve404())
-app.use('/sounds', serveCss, serve404())
-app.use('/favicon.ico', serveCss, serve404())
-app.use('/manifest.json', serveCss, serve404())
+app.use('/css', serveCss)
+app.use('/fonts', serveCss)
+app.use('/themes', serveCss)
+app.use('/sounds', serveCss)
+app.use('/favicon.ico', serveCss)
+app.use('/manifest.json', serveCss)
 
 app.use('*', serveStatic(ROOT), serveStatic(STATIC), serve404())
 
