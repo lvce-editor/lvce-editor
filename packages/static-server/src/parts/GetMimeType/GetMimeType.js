7a5d317c1f9e041288c70e02ffe9e452bcbae2c2
Le Vivilet <levivlet@pm.me>
2024-12-07 00:28:03 +0100

    add headers and mime type

---
+++ packages/static-server/src/parts/GetMimeType/GetMimeType.js
@@ -0,0 +1,30 @@
+import * as MimeType from '../MimeType/MimeType.js'
+
+export const getMimeType = (fileExtension) => {
+  switch (fileExtension) {
+    case '.html':
+      return MimeType.TextHtml
+    case '.css':
+      return MimeType.TextCss
+    case '.ttf':
+      return MimeType.FontTtf
+    case '.js':
+    case '.mjs':
+    case '.ts':
+      return MimeType.TextJavaScript
+    case '.svg':
+      return MimeType.ImageSvgXml
+    case '.png':
+      return MimeType.ImagePng
+    case '.json':
+    case '.map':
+      return MimeType.ApplicationJson
+    case '.mp3':
+      return MimeType.AudioMpeg
+    case '.webm':
+      return MimeType.VideoWebm
+    default:
+      console.warn(`[shared-process] unsupported file extension: ${fileExtension}`)
+      return ''
+  }
+}
