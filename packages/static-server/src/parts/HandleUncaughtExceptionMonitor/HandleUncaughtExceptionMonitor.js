5936a09b2326c9263ef66c3aa52e2982ec6a0a5d
Le Vivilet <levivlet@pm.me>
2024-12-07 00:18:55 +0100

    monitor uncaught exception

---
+++ packages/static-server/src/parts/HandleUncaughtExceptionMonitor/HandleUncaughtExceptionMonitor.js
@@ -0,0 +1,3 @@
+export const handleUncaughtExceptionMonitor = (error) => {
+  console.error(`[static-server] uncaught exception ${error}`)
+}
--- packages/static-server/src/parts/Main/Main.js
+++ packages/static-server/src/parts/Main/Main.js
@@ -1,6 +1,8 @@
 import * as HandleMessageFromParent from '../HandleMessageFromParent/HandleMessageFromParent.js'
+import * as HandleUncaughtExceptionMonitor from '../HandleUncaughtExceptionMonitor/HandleUncaughtExceptionMonitor.js'
 
 export const main = () => {
+  process.on('uncaughtExceptionMonitor', HandleUncaughtExceptionMonitor.handleUncaughtExceptionMonitor)
   process.on('message', HandleMessageFromParent.handleMessageFromParent)
   if (process.send) {
     process.send('ready')
