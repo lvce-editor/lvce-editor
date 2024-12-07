296f4025814dc2f731031f0279743418d1c51e7b
Le Vivilet <levivlet@pm.me>
2024-12-07 22:20:03 +0100

    fix sending file

--- packages/shared-process/src/parts/HandleRequest/HandleRequest.js
+++ packages/shared-process/src/parts/HandleRequest/HandleRequest.js
@@ -1,15 +1,11 @@
 import * as Assert from '../Assert/Assert.js'
 import * as HttpServerResponse from '../HttpServerResponse/HttpServerResponse.js'
+import * as GetElectronFileResponse from '../GetElectronFileResponse/GetElectronFileResponse.js'
 
 export const handleRequest = async (socket, request, indexHtmlPath) => {
   Assert.object(socket)
   Assert.object(request)
   Assert.string(indexHtmlPath)
-  HttpServerResponse.send(request, socket, {
-    body: 'okay',
-    init: {
-      status: 200,
-      headers: {},
-    },
-  })
+  const fileResponse = await GetElectronFileResponse.getElectronFileResponse(request.url, request)
+  HttpServerResponse.send(request, socket, fileResponse)
 }
