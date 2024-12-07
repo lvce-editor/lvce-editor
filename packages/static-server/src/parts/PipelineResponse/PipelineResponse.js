ae58a0299b519ecfc6b26660a69d93c3c4e3086b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-07 01:15:06 +0100

    Delete packages/static-server/src/parts/PipelineResponse/PipelineResponse.js

--- packages/static-server/src/parts/PipelineResponse/PipelineResponse.js
+++
@@ -1,9 +0,0 @@
-export const pipelineResponse = async (response, stream) => {
-  const { resolve, promise } = Promise.withResolvers()
-  stream.on('end', resolve)
-  stream.on('data', (x) => {
-    response.write(x)
-  })
-  await promise
-  response.end()
-}
