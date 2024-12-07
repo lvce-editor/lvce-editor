e6db512946b5e441ac1a35baa1f596724e08d55c
Le Vivilet <levivlet@pm.me>
2024-12-07 11:45:36 +0100

    use enum

--- packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
+++ packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
@@ -2,6 +2,7 @@ import { createReadStream } from 'node:fs'
 import * as Assert from '../Assert/Assert.js'
 import { CompatServerResponse } from '../CompatServerResponse/CompatServerResponse.js'
 import * as GetHeaders from '../GetHeaders/GetHeaders.js'
+import * as HttpStatusCode from '../HttpStatusCode/HttpStatusCode.js'
 import * as PipelineResponse from '../PipelineResponse/PipelineResponse.js'
 
 export const send = async (request, socket, filePath) => {
@@ -12,12 +13,12 @@ export const send = async (request, socket, filePath) => {
     Assert.string(filePath)
     response.assignSocket(socket)
     const headers = GetHeaders.getHeaders(filePath)
-    response.writeHead(200, headers)
+    response.writeHead(HttpStatusCode.Ok, headers)
     const stream = createReadStream(filePath)
     await PipelineResponse.pipelineResponse(response, stream)
   } catch (error) {
     console.error(`[response error] ${request.url} ${error}`)
-    response.statusCode = 500
+    response.statusCode = HttpStatusCode.ServerError
     response.end('server error')
   }
 }
