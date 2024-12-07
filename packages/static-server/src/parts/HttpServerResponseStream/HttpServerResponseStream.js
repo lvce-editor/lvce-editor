3727850caf8b0a7aee1eec30a8d17046224a7b3a
Le Vivilet <levivlet@pm.me>
2024-12-07 11:44:34 +0100

    fix: error with server response drain event

--- packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
+++ packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
@@ -1,20 +1,11 @@
 import { createReadStream } from 'node:fs'
-import { readFile } from 'node:fs/promises'
-import { ServerResponse } from 'node:http'
-import { pipeline, finished } from 'node:stream/promises'
 import * as Assert from '../Assert/Assert.js'
+import { CompatServerResponse } from '../CompatServerResponse/CompatServerResponse.js'
 import * as GetHeaders from '../GetHeaders/GetHeaders.js'
 import * as PipelineResponse from '../PipelineResponse/PipelineResponse.js'
 
-const supportsStream = (filePath) => {
-  // if (filePath.endsWith('.ttf')) {
-  //   return false
-  // }
-  return true
-}
-
 export const send = async (request, socket, filePath) => {
-  const response = new ServerResponse(request)
+  const response = new CompatServerResponse(request, socket)
   try {
     Assert.object(request)
     Assert.object(socket)
