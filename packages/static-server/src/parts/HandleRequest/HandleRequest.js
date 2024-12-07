12c0902aa78fda779214df3f7e1cb594df4daa9d
Le Vivilet <levivlet@pm.me>
2024-12-07 23:36:21 +0100

    feature: add getResponseInfo helper function

--- packages/static-server/src/parts/HandleRequest/HandleRequest.js
+++ packages/static-server/src/parts/HandleRequest/HandleRequest.js
@@ -1,9 +1,7 @@
-import { join } from 'node:path'
+import * as GetResponseInfo from '../GetResponseInfo/GetResponseInfo.js'
 import * as HttpServerResponseStream from '../HttpServerResponseStream/HttpServerResponseStream.js'
-import { STATIC } from '../Static/Static.js'
 
 export const handleRequest = async (request, socket) => {
-  const pathname = request.url
-  const filePath = join(STATIC, pathname)
-  await HttpServerResponseStream.send(request, socket, filePath)
+  const { absolutePath, status, headers } = await GetResponseInfo.getResponseInfo(request)
+  await HttpServerResponseStream.send(request, socket, status, headers, absolutePath)
 }
