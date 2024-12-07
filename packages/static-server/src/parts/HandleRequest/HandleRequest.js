ea3803c198e186560611c8a70dbde1cc377d470e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-08 00:23:48 +0100

    feature: set csp header in static server (#7465)
    * feature: set csp header in static server
    
    * feature: set caching headers if immutable is set

--- packages/static-server/src/parts/HandleRequest/HandleRequest.js
+++ packages/static-server/src/parts/HandleRequest/HandleRequest.js
@@ -1,7 +1,8 @@
 import * as GetResponseInfo from '../GetResponseInfo/GetResponseInfo.js'
 import * as HttpServerResponseStream from '../HttpServerResponseStream/HttpServerResponseStream.js'
+import * as IsImmutable from '../IsImmutable/IsImmutable.js'
 
 export const handleRequest = async (request, socket) => {
-  const { absolutePath, status, headers } = await GetResponseInfo.getResponseInfo(request)
+  const { absolutePath, status, headers } = await GetResponseInfo.getResponseInfo(request, IsImmutable.isImmutable)
   await HttpServerResponseStream.send(request, socket, status, headers, absolutePath)
 }
