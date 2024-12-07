1f815f8d27c394ff46f85a878703fe1afd5a2436
Le Vivilet <levivlet@pm.me>
2024-12-07 01:09:44 +0100

    improve response sending

--- packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
+++ packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
@@ -4,6 +4,7 @@ import { ServerResponse } from 'node:http'
 import { pipeline, finished } from 'node:stream/promises'
 import * as Assert from '../Assert/Assert.js'
 import * as GetHeaders from '../GetHeaders/GetHeaders.js'
+import * as PipelineResponse from '../PipelineResponse/PipelineResponse.js'
 
 const supportsStream = (filePath) => {
   // if (filePath.endsWith('.ttf')) {
@@ -21,44 +22,8 @@ export const send = async (request, socket, filePath) => {
     response.assignSocket(socket)
     const headers = GetHeaders.getHeaders(filePath)
     response.writeHead(200, headers)
-    if (!supportsStream(filePath)) {
-      const content = await readFile(filePath)
-      response.end(content)
-      return
-    }
     const stream = createReadStream(filePath)
-    if (filePath.endsWith('.ttf')) {
-      console.log('before font stream')
-      stream.on('close', () => {
-        console.log('read close')
-      })
-      stream.on('end', () => {
-        console.log('stream end')
-        response.end()
-      })
-      stream.on('data', (x) => {
-        console.log('write chunk')
-        response.write(x)
-      })
-      response.on('finish', () => {
-        console.log('response finish')
-      })
-      response.on('close', () => {
-        console.log('response closed')
-      })
-      // stream.pipe(response)
-      // await new Promise((r) => {
-      //   setTimeout(r, 1000)
-      // })
-      return
-      // await finished(stream)
-      // console.log('did finish')
-    }
-    await pipeline(stream, response, { end: false })
-    response.end()
-    if (filePath.endsWith('.ttf')) {
-      console.log('after font stream')
-    }
+    await PipelineResponse.pipelineResponse(response, stream)
   } catch (error) {
     console.error(`[response error] ${request.url} ${error}`)
     response.statusCode = 500
