2aeff13b7cb6ad1757296dc701730d1d0f2a42d0
Le Vivilet <levivlet@pm.me>
2024-12-07 01:10:08 +0100

    remove log

--- packages/static-server/src/parts/PipelineResponse/PipelineResponse.js
+++ packages/static-server/src/parts/PipelineResponse/PipelineResponse.js
@@ -1,25 +1,10 @@
 export const pipelineResponse = async (response, stream) => {
   console.log('before font stream')
   const { resolve, promise } = Promise.withResolvers()
-
-  stream.on('close', () => {
-    console.log('read close')
-  })
-  stream.on('end', () => {
-    resolve(undefined)
-  })
+  stream.on('end', resolve)
   stream.on('data', (x) => {
-    console.log('write chunk')
     response.write(x)
   })
-  response.on('finish', () => {
-    console.log('response finish')
-  })
-  response.on('close', () => {
-    console.log('response closed')
-  })
-
   await promise
-
   response.end()
 }
