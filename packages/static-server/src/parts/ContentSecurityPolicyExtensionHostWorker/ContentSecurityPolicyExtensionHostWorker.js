979a0a174a191319c4ec7429c48dd296878ea093
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-08 15:03:42 +0100

    feature: add csp for workers in static server (#7471)
    * feature: add csp for workers in static server
    
    * add renderer worker csp
    
    * remove one file

---
+++ packages/static-server/src/parts/ContentSecurityPolicyExtensionHostWorker/ContentSecurityPolicyExtensionHostWorker.js
@@ -0,0 +1,8 @@
+import * as GetContentSecurityPolicy from '../GetContentSecurityPolicy/GetContentSecurityPolicy.js'
+
+export const value = GetContentSecurityPolicy.getContentSecurityPolicy([
+  `default-src 'none'`,
+  `connect-src 'self'`,
+  `script-src 'self'`,
+  `font-src 'self'`,
+])
