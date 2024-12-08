0908d607d49ff2a2f2a59b644504646aac8d39d9
Le Vivilet <levivlet@pm.me>
2024-12-08 21:01:39 +0100

    feature: handle test request

--- packages/shared-process/src/parts/HandleRequest/HandleRequest.js
+++ packages/shared-process/src/parts/HandleRequest/HandleRequest.js
@@ -1,10 +1,18 @@
+import { join } from 'path'
 import * as Assert from '../Assert/Assert.js'
-import * as HttpServerResponse from '../HttpServerResponse/HttpServerResponse.js'
 import * as GetElectronFileResponse from '../GetElectronFileResponse/GetElectronFileResponse.js'
+import * as HandleRequestTest from '../HandleRequestTest/HandleRequestTest.js'
+import * as HttpServerResponse from '../HttpServerResponse/HttpServerResponse.js'
+import * as StaticPath from '../StaticPath/StaticPath.js'
 
 export const handleRequest = async (socket, request) => {
   Assert.object(socket)
   Assert.object(request)
+  if (request.url === '/tests' || request.url === '/tests/') {
+    const staticPath = StaticPath.getStaticPath()
+    const indexHtmlPath = join(staticPath, 'index.html')
+    return HandleRequestTest.handleRequestTest(socket, request)
+  }
   const fileResponse = await GetElectronFileResponse.getElectronFileResponse(request.url, request)
   HttpServerResponse.send(request, socket, fileResponse)
 }
