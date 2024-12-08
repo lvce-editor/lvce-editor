c119dfb9f350b00cf087abe324964aab470174fc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-08 13:08:55 +0100

    fix: don't crash static server when file is not found (#7468)

--- packages/static-server/src/parts/GetPathEtag/GetPathEtag.js
+++ packages/static-server/src/parts/GetPathEtag/GetPathEtag.js
@@ -2,11 +2,20 @@ import { stat } from 'node:fs/promises'
 import * as GetEtag from '../GetEtag/GetEtag.js'
 
 export const getPathEtag = async (absolutePath) => {
-  let stats = await stat(absolutePath)
-  if (stats.isDirectory()) {
-    absolutePath += '/index.html'
-    stats = await stat(absolutePath)
+  try {
+    let stats = await stat(absolutePath)
+    if (stats.isDirectory()) {
+      absolutePath += '/index.html'
+      stats = await stat(absolutePath)
+    }
+    const etag = GetEtag.getEtag(stats)
+    return etag
+  } catch (error) {
+    // @ts-ignore
+    if (error && error.code === 'ENOENT') {
+      return ''
+    }
+    console.error(`[static-server] etag error ${error}`)
+    return ''
   }
-  const etag = GetEtag.getEtag(stats)
-  return etag
 }
