9627879858f2e17f1f127b16255f2a2c146ef432
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-08 14:52:11 +0100

    feature: handle socket error in static server (#7470)

--- packages/static-server/src/parts/HandleRequest/HandleRequest.js
+++ packages/static-server/src/parts/HandleRequest/HandleRequest.js
@@ -1,8 +1,10 @@
 import * as GetResponseInfo from '../GetResponseInfo/GetResponseInfo.js'
+import * as HandleSocketError from '../HandleSocketError/HandleSocketError.js'
 import * as HttpServerResponseStream from '../HttpServerResponseStream/HttpServerResponseStream.js'
 import * as IsImmutable from '../IsImmutable/IsImmutable.js'
 
 export const handleRequest = async (request, socket) => {
+  socket.on('error', HandleSocketError.handleSocketError)
   const { absolutePath, status, headers } = await GetResponseInfo.getResponseInfo(request, IsImmutable.isImmutable)
   await HttpServerResponseStream.send(request, socket, status, headers, absolutePath)
 }
