a94e669667498d0b5cc5dbdf175271f470b95c4d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-09 18:25:52 +0100

    feature: make title bar buttons functional (#7500)

--- packages/renderer-worker/src/parts/GetTitleBarButtonVirtualDom/GetTitleBarButtonVirtualDom.js
+++ packages/renderer-worker/src/parts/GetTitleBarButtonVirtualDom/GetTitleBarButtonVirtualDom.js
@@ -2,13 +2,14 @@ import * as GetIconVirtualDom from '../GetIconVirtualDom/GetIconVirtualDom.js'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
 
 export const createTitleBarButton = (button) => {
-  const { id, icon, label } = button
+  const { id, icon, label, onClick } = button
   const dom = [
     {
       type: VirtualDomElements.Button,
       className: `TitleBarButton TitleBarButton${id}`,
       ariaLabel: label,
       childCount: 1,
+      onClick,
     },
     GetIconVirtualDom.getIconVirtualDom(icon, VirtualDomElements.I),
   ]
