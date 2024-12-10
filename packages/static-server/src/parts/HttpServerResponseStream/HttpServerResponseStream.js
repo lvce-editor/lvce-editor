520bbcdef39559f0f872e4b73d599aed602210d6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-10 22:02:32 +0100

    feature: ignore ERR_STREAM_PREMATURE_CLOSE error (#7508)

--- packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
+++ packages/static-server/src/parts/HttpServerResponseStream/HttpServerResponseStream.js
@@ -2,6 +2,7 @@ import { createReadStream } from 'node:fs'
 import { CompatServerResponse } from '../CompatServerResponse/CompatServerResponse.js'
 import * as HttpStatusCode from '../HttpStatusCode/HttpStatusCode.js'
 import * as PipelineResponse from '../PipelineResponse/PipelineResponse.js'
+import * as ErrorCodes from '../ErrorCodes/ErrorCodes.js'
 
 export const send = async (request, socket, status, headers, filePath) => {
   const response = new CompatServerResponse(request, socket, status, headers)
@@ -13,8 +14,12 @@ export const send = async (request, socket, status, headers, filePath) => {
     const stream = createReadStream(filePath)
     await PipelineResponse.pipelineResponse(response, stream)
   } catch (error) {
+    // @ts-ignore
+    if (error && error.code === ErrorCodes.ERR_STREAM_PREMATURE_CLOSE) {
+      return
+    }
     // TODO only do this if headers have not already been sent
-    console.error(`[response error] ${request.url} ${error}`)
+    console.error(`[static server] response error at ${request.url} ${error}`)
     response.statusCode = HttpStatusCode.ServerError
     response.end('server error')
   }
