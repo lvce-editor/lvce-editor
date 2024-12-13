4e5491cc78841955561f7e756fc9108eec51a148
Le Vivilet <levivlet@pm.me>
2024-12-13 23:33:01 +0100

    fix error

--- packages/renderer-worker/src/parts/IndexedDb/IndexedDb.js
+++ packages/renderer-worker/src/parts/IndexedDb/IndexedDb.js
@@ -3,11 +3,11 @@
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.js'
 
 export const saveValue = async (storeId, value) => {
-  await ExtensionHostWorker.invoke('IndexedDb.saveValue', value)
+  await ExtensionHostWorker.invoke('IndexedDb.saveValue', storeId, value)
 }
 
 export const getValues = async (storeId) => {
-  return ExtensionHostWorker.invoke('IndexedDb.getValues')
+  return ExtensionHostWorker.invoke('IndexedDb.getValues', storeId)
 }
 
 export const getValuesByIndexName = async (storeId, indexName, only) => {
