79eea3d40a3fda39f3d55183b65fcb22c26425fb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-13 22:46:07 +0100

    feature: allow command registration to be async to let worker register commands (#7521)

--- packages/renderer-worker/src/parts/ViewletAbout/ViewletAboutCommands.ts
+++ packages/renderer-worker/src/parts/ViewletAbout/ViewletAboutCommands.ts
@@ -1,10 +1,12 @@
 import * as ViewletAbout from './ViewletAbout.ts'
 
-export const Commands = {
-  handleClickClose: ViewletAbout.handleClickClose,
-  handleClickCopy: ViewletAbout.handleClickCopy,
-  handleClickOk: ViewletAbout.handleClickOk,
-  handleFocusIn: ViewletAbout.handleFocusIn,
-  focusNext: ViewletAbout.focusNext,
-  focusPrevious: ViewletAbout.focusPrevious,
+export const getCommands = () => {
+  return {
+    handleClickClose: ViewletAbout.handleClickClose,
+    handleClickCopy: ViewletAbout.handleClickCopy,
+    handleClickOk: ViewletAbout.handleClickOk,
+    handleFocusIn: ViewletAbout.handleFocusIn,
+    focusNext: ViewletAbout.focusNext,
+    focusPrevious: ViewletAbout.focusPrevious,
+  }
 }
