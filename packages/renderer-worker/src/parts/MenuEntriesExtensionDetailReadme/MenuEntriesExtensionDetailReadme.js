dbef79fab3087cd3ccf8c9fc2944435291c25781
Le Vivilet <levivlet@pm.me>
2024-12-14 22:45:38 +0100

    feature: move extension detail menu entries to worker

--- packages/renderer-worker/src/parts/MenuEntriesExtensionDetailReadme/MenuEntriesExtensionDetailReadme.js
+++ packages/renderer-worker/src/parts/MenuEntriesExtensionDetailReadme/MenuEntriesExtensionDetailReadme.js
@@ -1,42 +1,9 @@
+import * as ExtensionDetailViewWorker from '../ExtensionDetailViewWorker/ExtensionDetailViewWorker.js'
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.js'
-import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
-import * as ViewletExtensionDetailStrings from '../ViewletExtensionDetail/ViewletExtensionDetailStrings.ts'
 
 export const id = MenuEntryId.ExtensionDetailReadme
 
-export const getMenuEntries = (props) => {
-  const menuEntries = []
-  if (props.isLink) {
-    menuEntries.push({
-      id: 'openInNewTab',
-      label: ViewletExtensionDetailStrings.openImageInNewTab(),
-      flags: MenuItemFlags.None,
-      command: 'Open.openUrl',
-      args: [props.url],
-    })
-  } else if (props.isImage) {
-    menuEntries.push(
-      {
-        id: 'openImageInNewTab',
-        label: ViewletExtensionDetailStrings.openImageInNewTab(),
-        flags: MenuItemFlags.None,
-        command: 'Open.openUrl',
-        args: [props.url],
-      },
-      {
-        id: 'saveImageAs',
-        label: ViewletExtensionDetailStrings.saveImageAs(),
-        flags: MenuItemFlags.None,
-        command: 'SaveFileAs.saveFileAs',
-        args: ['image.png', props.url],
-      },
-    )
-  }
-  menuEntries.push({
-    id: 'copy',
-    label: ViewletExtensionDetailStrings.copy(),
-    flags: MenuItemFlags.None,
-    command: 'ClipBoard.execCopy',
-  })
+export const getMenuEntries = async (props) => {
+  const menuEntries = await ExtensionDetailViewWorker.invoke('ExtensionDetail.getMenuEntries', props)
   return menuEntries
 }
