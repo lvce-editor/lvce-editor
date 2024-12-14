139334aa6b6a96be481255a755426601b3a5d12d
Le Vivilet <levivlet@pm.me>
2024-12-14 22:23:54 +0100

    feature: ask file search worker for quickpick keybindings

--- packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickKeyBindings.js
+++ packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickKeyBindings.js
@@ -1,38 +1,5 @@
-import * as KeyCode from '../KeyCode/KeyCode.js'
-import * as WhenExpression from '../WhenExpression/WhenExpression.js'
+import * as FileSearchWorker from '../FileSearchWorker/FileSearchWorker.js'
 
 export const getKeyBindings = () => {
-  return [
-    {
-      key: KeyCode.Escape,
-      command: 'Viewlet.closeWidget',
-      args: ['QuickPick'],
-      when: WhenExpression.FocusQuickPickInput,
-    },
-    {
-      key: KeyCode.UpArrow,
-      command: 'QuickPick.focusPrevious',
-      when: WhenExpression.FocusQuickPickInput,
-    },
-    {
-      key: KeyCode.DownArrow,
-      command: 'QuickPick.focusNext',
-      when: WhenExpression.FocusQuickPickInput,
-    },
-    {
-      key: KeyCode.PageUp,
-      command: 'QuickPick.focusFirst',
-      when: WhenExpression.FocusQuickPickInput,
-    },
-    {
-      key: KeyCode.PageDown,
-      command: 'QuickPick.focusLast',
-      when: WhenExpression.FocusQuickPickInput,
-    },
-    {
-      key: KeyCode.Enter,
-      command: 'QuickPick.selectCurrentIndex',
-      when: WhenExpression.FocusQuickPickInput,
-    },
-  ]
+  return FileSearchWorker.invoke('QuickPick.getKeyBindings')
 }
