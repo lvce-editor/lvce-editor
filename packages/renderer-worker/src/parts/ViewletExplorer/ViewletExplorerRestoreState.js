a99f5ce15adc79f3a425a2c9b603cd36a2d3c0b2
Le Vivilet <levivlet@pm.me>
2024-12-15 17:25:58 +0100

    feature: use worker for restoreState

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerRestoreState.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerRestoreState.js
@@ -1,29 +1,5 @@
-import * as Workspace from '../Workspace/Workspace.js'
-
-const getSavedRoot = (savedState, workspacePath) => {
-  return workspacePath
-}
+import * as ExplorerViewWorker from '../ExplorerViewWorker/ExplorerViewWorker.js'
 
 export const restoreState = (savedState) => {
-  if (!savedState) {
-    return {
-      minLineY: 0,
-      deltaY: 0,
-    }
-  }
-  const root = getSavedRoot(savedState, Workspace.state.workspacePath)
-  let minLineY = 0
-  if (savedState && typeof savedState.minLineY === 'number') {
-    minLineY = savedState.minLineY
-  }
-  let deltaY = 0
-  if (savedState && typeof savedState.deltaY === 'number') {
-    deltaY = savedState.deltaY
-  }
-  return {
-    ...savedState,
-    root,
-    minLineY,
-    deltaY,
-  }
+  return ExplorerViewWorker.invoke('Explorer.restoreState', savedState)
 }
