961c32ab745c5a162b7dec3531404b61ed293c2f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-15 23:48:37 +0100

    feature: remove unused explorer code (#7556)

--- packages/renderer-worker/src/parts/SortExplorerItems/SortExplorerItems.js
+++ packages/renderer-worker/src/parts/SortExplorerItems/SortExplorerItems.js
@@ -1,18 +1,6 @@
 import * as Compare from '../Compare/Compare.js'
 import * as DirentType from '../DirentType/DirentType.js'
 
-export const getIndexFromPosition = (state, eventX, eventY) => {
-  const { y, itemHeight, items } = state
-  const index = Math.floor((eventY - y) / itemHeight)
-  if (index < 0) {
-    return 0
-  }
-  if (index >= items.length) {
-    return -1
-  }
-  return index
-}
-
 const priorityMapFoldersFirst = {
   [DirentType.Directory]: 1,
   [DirentType.SymLinkFolder]: 1,
