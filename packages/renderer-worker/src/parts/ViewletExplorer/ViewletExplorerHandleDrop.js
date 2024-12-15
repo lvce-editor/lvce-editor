12fffba73884a8ac5d7875130a67f0490ff09ef1
Le Vivilet <levivlet@pm.me>
2024-12-15 12:44:33 +0100

    move to worker

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDrop.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerHandleDrop.js
@@ -1,18 +1,5 @@
-import { VError } from '../VError/VError.js'
-import { handleDropIndex } from './ViewletExplorerHandleDropIndex.js'
-import { handleDropRoot } from './ViewletExplorerHandleDropRoot.js'
-import { getIndexFromPosition } from './ViewletExplorerShared.js'
+import * as ExplorerViewWorker from '../ExplorerViewWorker/ExplorerViewWorker.js'
 
 export const handleDrop = async (state, x, y, files) => {
-  try {
-    const index = getIndexFromPosition(state, x, y)
-    switch (index) {
-      case -1:
-        return await handleDropRoot(state, files)
-      default:
-        return await handleDropIndex(state, index, files)
-    }
-  } catch (error) {
-    throw new VError(error, 'Failed to drop files')
-  }
+  return ExplorerViewWorker.invoke('Explorer.handleDrop', state, x, y, files)
 }
