d8f46b8259744b0e14d0e087843ca8efdfd65045
Le Vivilet <levivlet@pm.me>
2024-12-15 12:15:46 +0100

    use worker more frequently

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.js
@@ -148,23 +148,7 @@ export const handleIconThemeChange = (state) => {
 
 // TODO rename dirents to items, then can use virtual list component directly
 export const setDeltaY = (state, deltaY) => {
-  const { itemHeight, height, items } = state
-  if (deltaY < 0) {
-    deltaY = 0
-  } else if (deltaY > items.length * itemHeight - height) {
-    deltaY = Math.max(items.length * itemHeight - height, 0)
-  }
-  if (state.deltaY === deltaY) {
-    return state
-  }
-  const minLineY = Math.round(deltaY / itemHeight)
-  const maxLineY = minLineY + Math.round(height / itemHeight)
-  return {
-    ...state,
-    deltaY,
-    minLineY,
-    maxLineY,
-  }
+  return ExplorerViewWorker.invoke('Explorer.setDeltaY', state, deltaY)
 }
 
 export const handleWheel = (state, deltaMode, deltaY) => {
