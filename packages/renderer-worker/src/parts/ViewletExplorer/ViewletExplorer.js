1333fa845a56a721e43d007a09f2de2b6ecd9df5
Le Vivilet <levivlet@pm.me>
2024-12-15 11:52:41 +0100

    feature: use explorer worker to load explorer content

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.js
@@ -1,9 +1,9 @@
 import * as Assert from '../Assert/Assert.ts'
-import * as Character from '../Character/Character.js'
 import * as Command from '../Command/Command.js'
 import * as DirentType from '../DirentType/DirentType.js'
 import * as ErrorHandling from '../ErrorHandling/ErrorHandling.js'
 import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.js'
+import * as ExplorerViewWorker from '../ExplorerViewWorker/ExplorerViewWorker.js'
 import * as FileSystem from '../FileSystem/FileSystem.js'
 import * as Focus from '../Focus/Focus.js'
 import * as FocusKey from '../FocusKey/FocusKey.js'
@@ -14,14 +14,12 @@ import * as IconTheme from '../IconTheme/IconTheme.js'
 import * as MouseEventType from '../MouseEventType/MouseEventType.js'
 import * as OpenFolder from '../OpenFolder/OpenFolder.js'
 import * as PathSeparatorType from '../PathSeparatorType/PathSeparatorType.js'
-import * as Preferences from '../Preferences/Preferences.js'
-import * as PromiseStatus from '../PromiseStatus/PromiseStatus.js'
 import * as RendererWorkerCommandType from '../RendererWorkerCommandType/RendererWorkerCommandType.js'
 import * as SortExplorerItems from '../SortExplorerItems/SortExplorerItems.js'
 import * as Viewlet from '../Viewlet/Viewlet.js' // TODO should not import viewlet manager -> avoid cyclic dependency
 import * as Workspace from '../Workspace/Workspace.js'
 import { focusIndex } from './ViewletExplorerFocusIndex.js'
-import { getChildDirents, getChildDirentsRaw, getIndexFromPosition, getParentEndIndex, getParentStartIndex } from './ViewletExplorerShared.js'
+import { getChildDirents, getIndexFromPosition, getParentEndIndex, getParentStartIndex } from './ViewletExplorerShared.js'
 // TODO viewlet should only have create and refresh functions
 // every thing else can be in a separate module <viewlet>.lazy.js
 // and  <viewlet>.ipc.js
@@ -58,10 +56,6 @@ export const create = (id, uri, x, y, width, height, args, parentUid) => {
     editingType: ExplorerEditingType.None,
     editingIcon: '',
   }
-}
-
-const getPathSeparator = (root) => {
-  return FileSystem.getPathSeparator(root)
 }
 
 // @ts-ignore
@@ -120,90 +114,9 @@ const getSavedChildDirents = (map, path, depth, excluded, pathSeparator) => {
   return dirents
 }
 
-const createDirents = (root, expandedDirentPaths, expandedDirentChildren, excluded, pathSeparator) => {
-  const dirents = []
-  const map = Object.create(null)
-  for (let i = 0; i < expandedDirentPaths.length; i++) {
-    const path = expandedDirentPaths[i]
-    const children = expandedDirentChildren[i]
-    if (children.status === PromiseStatus.Fulfilled) {
-      map[path] = children.value
-    }
-  }
-  dirents.push(...getSavedChildDirents(map, root, 1, excluded, pathSeparator))
-  return dirents
-}
-
-const getSavedExpandedPaths = (savedState, root) => {
-  if (savedState && savedState.root !== root) {
-    return []
-  }
-  if (savedState && savedState.expandedPaths && Array.isArray(savedState.expandedPaths)) {
-    return savedState.expandedPaths
-  }
-  return []
-}
-
-const restoreExpandedState = async (savedState, root, pathSeparator, excluded) => {
-  // TODO read all opened folders in parallel
-  // ignore ENOENT errors
-  // ignore ENOTDIR errors
-  // merge all dirents
-  // restore scroll location
-  const expandedPaths = getSavedExpandedPaths(savedState, root)
-  if (root === Character.EmptyString) {
-    return []
-  }
-  const expandedDirentPaths = [root, ...expandedPaths]
-  const expandedDirentChildren = await Promise.allSettled(expandedDirentPaths.map(getChildDirentsRaw))
-  if (expandedDirentChildren[0].status === PromiseStatus.Rejected) {
-    throw expandedDirentChildren[0].reason
-  }
-  const dirents = createDirents(root, expandedDirentPaths, expandedDirentChildren, excluded, pathSeparator)
-  return dirents
-}
-
-const getExcluded = () => {
-  const excludedObject = Preferences.get('files.exclude') || {}
-  const excluded = []
-  for (const [key, value] of Object.entries(excludedObject)) {
-    if (value) {
-      excluded.push(key)
-    }
-  }
-  return excluded
-}
-
-const getSavedRoot = (savedState, workspacePath) => {
-  return workspacePath
-}
-
 export const loadContent = async (state, savedState) => {
-  const root = getSavedRoot(savedState, Workspace.state.workspacePath)
-  // TODO path separator could be restored from saved state
-  const pathSeparator = await getPathSeparator(root) // TODO only load path separator once
-  const excluded = getExcluded()
-  const restoredDirents = await restoreExpandedState(savedState, root, pathSeparator, excluded)
-  const { itemHeight, height } = state
-  let minLineY = 0
-  if (savedState && typeof savedState.minLineY === 'number') {
-    minLineY = savedState.minLineY
-  }
-  let deltaY = 0
-  if (savedState && typeof savedState.deltaY === 'number') {
-    deltaY = savedState.deltaY
-  }
-  const maxLineY = GetExplorerMaxLineY.getExplorerMaxLineY(minLineY, height, itemHeight, restoredDirents.length)
-  return {
-    ...state,
-    root,
-    items: restoredDirents,
-    minLineY,
-    deltaY,
-    maxLineY,
-    pathSeparator,
-    excluded,
-  }
+  const newState = await ExplorerViewWorker.invoke('Explorer.loadContent', state, savedState)
+  return newState
 }
 
 const updateIcon = (dirent) => {
