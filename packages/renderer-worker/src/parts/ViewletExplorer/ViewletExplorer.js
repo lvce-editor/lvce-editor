a1c5443a994065778f8ade032299804adc92e5fd
Le Vivilet <levivlet@pm.me>
2024-12-15 17:20:13 +0100

    simplify code

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorer.js
@@ -76,10 +76,7 @@ export const handleLanguagesChanged = (state) => {
 }
 
 export const handleWorkspaceChange = async (state) => {
-  const newRoot = Workspace.state.workspacePath
-  const state1 = { ...state, root: newRoot }
-  const newState = await loadContent(state1)
-  return newState
+  return ExplorerViewWorker.invoke('Explorer.handleWorkspaceChange')
 }
 
 export const handleIconThemeChange = (state) => {
@@ -157,135 +154,12 @@ export const newFolder = (state) => {
   return ExplorerViewWorker.invoke('Explorer.newFolder', state)
 }
 
-const handleClickFile = async (state, dirent, index, keepFocus = false) => {
-  await Command.execute(/* Main.openAbsolutePath */ 'Main.openUri', /* absolutePath */ dirent.path, /* focus */ !keepFocus)
-  return {
-    ...state,
-    focusedIndex: index,
-    focused: keepFocus,
-  }
-}
-
-const handleClickDirectory = async (state, dirent, index, keepFocus) => {
-  dirent.type = DirentType.DirectoryExpanding
-  // TODO handle error
-  const dirents = await getChildDirents(state.pathSeparator, dirent)
-  const state2 = Viewlet.getState('Explorer')
-  if (!state2) {
-    return state
-  }
-  // TODO use Viewlet.getState here and check if it exists
-  const newIndex = state2.items.indexOf(dirent)
-  // TODO if viewlet is disposed or root has changed, return
-  if (newIndex === -1) {
-    return state
-  }
-  const newDirents = [...state2.items]
-  newDirents.splice(newIndex + 1, 0, ...dirents)
-  dirent.type = DirentType.DirectoryExpanded
-  dirent.icon = IconTheme.getIcon(dirent)
-  const { height, itemHeight, minLineY } = state2
-  // TODO when focused index has changed while expanding, don't update it
-  const maxLineY = GetExplorerMaxLineY.getExplorerMaxLineY(minLineY, height, itemHeight, newDirents.length)
-  return {
-    ...state,
-    items: newDirents,
-    focusedIndex: newIndex,
-    focused: keepFocus,
-    maxLineY,
-  }
-}
-
-const handleClickDirectoryExpanding = (state, dirent, index, keepFocus) => {
-  dirent.type = DirentType.Directory
-  dirent.icon = IconTheme.getIcon(dirent)
-  return {
-    ...state,
-    focusedIndex: index,
-    focused: keepFocus,
-  }
-}
-
-const handleClickDirectoryExpanded = (state, dirent, index, keepFocus) => {
-  const { minLineY, maxLineY, itemHeight } = state
-  dirent.type = DirentType.Directory
-  dirent.icon = IconTheme.getIcon(dirent)
-  const endIndex = getParentEndIndex(state.items, index)
-  const removeCount = endIndex - index - 1
-  // TODO race conditions and side effects are everywhere
-  const newDirents = [...state.items]
-  newDirents.splice(index + 1, removeCount)
-  const newTotal = newDirents.length
-  if (newTotal < maxLineY) {
-    const visibleItems = Math.min(maxLineY - minLineY, newTotal)
-    const newMaxLineY = Math.min(maxLineY, newTotal)
-    const newMinLineY = newMaxLineY - visibleItems
-    const deltaY = newMinLineY * itemHeight
-    return {
-      ...state,
-      items: newDirents,
-      focusedIndex: index,
-      focused: keepFocus,
-      minLineY: newMinLineY,
-      maxLineY: newMaxLineY,
-      deltaY,
-    }
-  }
-  return {
-    ...state,
-    items: newDirents,
-    focusedIndex: index,
-    focused: keepFocus,
-  }
-}
-
-const getClickFn = (direntType) => {
-  switch (direntType) {
-    case DirentType.File:
-    case DirentType.SymLinkFile:
-      return handleClickFile
-    case DirentType.Directory:
-    case DirentType.SymLinkFolder:
-      return handleClickDirectory
-    case DirentType.DirectoryExpanding:
-      return handleClickDirectoryExpanding
-    case DirentType.DirectoryExpanded:
-      return handleClickDirectoryExpanded
-    case DirentType.Symlink:
-      return handleClickSymLink
-    case DirentType.CharacterDevice:
-      throw new Error('Cannot open character device files')
-    case DirentType.BlockDevice:
-      throw new Error('Cannot open block device files')
-    case DirentType.Socket:
-      throw new Error('Cannot open socket files')
-    default:
-      throw new Error(`unsupported dirent type ${direntType}`)
-  }
-}
-
 export const handleClick = (state, index, keepFocus = false) => {
-  const { items, minLineY } = state
-  if (index === -1) {
-    return focusIndex(state, -1)
-  }
-  const actualIndex = index + minLineY
-  const dirent = items[actualIndex]
-  if (!dirent) {
-    console.warn(`[explorer] dirent at index ${actualIndex} not found`, state)
-    return state
-  }
-  const clickFn = getClickFn(dirent.type)
-  return clickFn(state, dirent, actualIndex, keepFocus)
+  return ExplorerViewWorker.invoke('Explorer.handleClick', state, index, keepFocus)
 }
 
 export const handleClickAt = (state, button, x, y) => {
-  if (button !== MouseEventType.LeftClick) {
-    return state
-  }
-
-  const index = getIndexFromPosition(state, x, y)
-  return handleClick(state, index)
+  return ExplorerViewWorker.invoke('Explorer.handleClickAt', state, button, x, y)
 }
 
 export const handleClickCurrent = (state) => {
@@ -300,17 +174,6 @@ export const scrollUp = () => {}
 
 export const scrollDown = () => {}
 // export const handleBlur=()=>{}
-
-const handleClickSymLink = async (state, dirent, index) => {
-  const realPath = await FileSystem.getRealPath(dirent.path)
-  const type = await FileSystem.stat(realPath)
-  switch (type) {
-    case DirentType.File:
-      return handleClickFile(state, dirent, index)
-    default:
-      throw new Error(`unsupported file type ${type}`)
-  }
-}
 
 export const handleArrowRight = async (state) => {
   return ExplorerViewWorker.invoke('Explorer.handleArrowRight', state)
