d12107c35fff5676846529a158ec56b07940fe29
Le Vivilet <levivlet@pm.me>
2024-12-16 00:07:16 +0100

    update rendering

--- packages/renderer-worker/src/parts/GetVisibleExplorerItems/GetVisibleExplorerItems.js
+++ packages/renderer-worker/src/parts/GetVisibleExplorerItems/GetVisibleExplorerItems.js
@@ -1,23 +1,27 @@
 import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.js'
-import * as IconTheme from '../IconTheme/IconTheme.js'
 
-export const getVisibleExplorerItems = (items, minLineY, maxLineY, focusedIndex, editingIndex, editingType, editingValue) => {
+// TODO rename this to displayResults
+// - items = normal dirents
+// - visible items = normal dirents that are visible
+// - display results = displayed elements (almost dom nodes)
+export const getVisibleExplorerItems = (items, minLineY, maxLineY, focusedIndex, editingIndex, editingType, editingValue, icons) => {
   const visible = []
+  let iconIndex = 0
   for (let i = minLineY; i < Math.min(maxLineY, items.length); i++) {
     const item = items[i]
+    const icon = icons[iconIndex++]
     if (i === editingIndex) {
       visible.push({
         ...item,
         isFocused: i === focusedIndex,
         isEditing: true,
-        icon: IconTheme.getFileIcon({
-          name: editingValue,
-        }),
+        icon,
       })
     } else {
       visible.push({
         ...item,
         isFocused: i === focusedIndex,
+        icon,
       })
     }
   }
