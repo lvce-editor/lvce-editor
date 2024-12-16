804a07ce216b2e4cf13cceaf6d8128d4535fd574
Le Vivilet <levivlet@pm.me>
2024-12-16 22:27:26 +0100

    feature: move explorer root update to worker

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerUpdateRoot.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerUpdateRoot.js
@@ -1,23 +1,6 @@
-import * as Viewlet from '../Viewlet/Viewlet.js'
-import { getTopLevelDirents, mergeDirents } from './ViewletExplorerShared.js'
+import * as ExplorerViewWorker from '../ExplorerViewWorker/ExplorerViewWorker.js'
 
 // TODO add lots of tests for this
-export const updateRoot = async () => {
-  const state1 = Viewlet.getState('Explorer')
-  if (state1.disposed) {
-    return state1
-  }
-  // const file = nativeFiles.files[0]
-  const topLevelDirents = await getTopLevelDirents(state1.root, state1.pathSeparator)
-  const state2 = Viewlet.getState('Explorer')
-  // TODO what if root changes while reading directories?
-  if (state2.disposed || state2.root !== state1.root) {
-    return state2
-  }
-  const newDirents = mergeDirents(state2.items, topLevelDirents)
-  const state3 = {
-    ...state2,
-    items: newDirents,
-  }
-  return state3
+export const updateRoot = async (state) => {
+  return ExplorerViewWorker.invoke('Explorer.updateRoot', state)
 }
