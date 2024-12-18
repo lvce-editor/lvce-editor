de619e818c8ba4d3515a4faed61a27e2fe52c90e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-18 22:13:19 +0100

    feature: remove unused explorer code (#7578)
    * feature: remove unused explorer code
    
    * fix render

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerRender.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerRender.js
@@ -1,5 +1,4 @@
-import * as GetExplorerVirtualDom from '../GetExplorerVirtualDom/GetExplorerVirtualDom.js'
-import * as GetVisibleExplorerItems from '../GetVisibleExplorerItems/GetVisibleExplorerItems.js'
+import * as AdjustCommands from '../AdjustCommands/AdjustCommands.js'
 
 export const hasFunctionalRender = true
 
@@ -7,63 +6,10 @@ export const hasFunctionalRootRender = true
 
 const renderItems = {
   isEqual(oldState, newState) {
-    return (
-      JSON.stringify(oldState.items) === JSON.stringify(newState.items) &&
-      oldState.minLineY === newState.minLineY &&
-      oldState.maxLineY === newState.maxLineY &&
-      oldState.focusedIndex === newState.focusedIndex &&
-      oldState.editingIndex === newState.editingIndex &&
-      oldState.editingType === newState.editingType &&
-      oldState.editingValue === newState.editingValue &&
-      oldState.width === newState.width
-    )
+    return JSON.stringify(oldState.commands) === JSON.stringify(newState.commands)
   },
-  apply(oldState, newState) {
-    const visibleDirents = GetVisibleExplorerItems.getVisibleExplorerItems(
-      newState.items,
-      newState.minLineY,
-      newState.maxLineY,
-      newState.focusedIndex,
-      newState.editingIndex,
-      newState.editingType,
-      newState.editingValue,
-      newState.icons,
-    )
-    const isWide = newState.width > 450
-    const dom = GetExplorerVirtualDom.getExplorerVirtualDom(visibleDirents, newState.focusedIndex, newState.root, isWide)
-    return ['Viewlet.setDom2', dom]
-  },
+  apply: AdjustCommands.apply,
+  multiple: true,
 }
 
-// const renderFocusedIndex = {
-//   isEqual(oldState, newState) {
-//     return oldState.focusedIndex === newState.focusedIndex && oldState.focused === newState.focused && oldState.minLineY === newState.minLineY
-//   },
-//   apply(oldState, newState) {
-//     const oldFocusedIndex = oldState.focusedIndex - oldState.minLineY
-//     const newFocusedIndex = newState.focusedIndex - newState.minLineY
-//     return [/* method */ 'setFocusedIndex', /* oldindex */ oldFocusedIndex, /* newIndex */ newFocusedIndex, /* focused */ newState.focused]
-//   },
-// }
-
-// const renderDropTargets = {
-//   isEqual(oldState, newState) {
-//     return oldState.dropTargets === newState.dropTargets
-//   },
-//   apply(oldState, newState) {
-//     return [/* method */ 'setDropTargets', /* oldDropTargets */ oldState.dropTargets, /* newDropTargets */ newState.dropTargets]
-//   },
-// }
-
-const renderEditingIndex = {
-  isEqual(oldState, newState) {
-    return oldState.editingIndex === newState.editingIndex && oldState.editingType === newState.editingType
-  },
-  apply(oldState, newState) {
-    // @ts-ignore
-    const { editingIndex, editingType, editingValue } = newState
-    return ['focusInput', 'ExplorerInput']
-  },
-}
-
-export const render = [renderItems, renderEditingIndex]
+export const render = [renderItems]
