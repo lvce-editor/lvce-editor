813f9ab1ba0f2917ef52616514a1bdef6378a359
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-19 21:19:48 +0100

    feature: move more keybinding commands to keybindings worker (#7582)

--- packages/renderer-worker/src/parts/ViewletKeyBindings/ViewletKeyBindings.js
+++ packages/renderer-worker/src/parts/ViewletKeyBindings/ViewletKeyBindings.js
@@ -2,14 +2,11 @@
 // see https://github.com/microsoft/vscode/blob/6a5e3aad96929a7d35e09ed8d22e87a72bd16ff6/src/vs/workbench/services/preferences/browser/keybindingsEditorModel.ts
 // see https://github.com/microsoft/vscode/blob/6a5e3aad96929a7d35e09ed8d22e87a72bd16ff6/src/vs/workbench/contrib/preferences/browser/keybindingsEditor.ts
 
-import * as Assert from '../Assert/Assert.ts'
 import * as ContextMenu from '../ContextMenu/ContextMenu.js'
 import * as FileSystem from '../FileSystem/FileSystem.js'
 import * as Focus from '../Focus/Focus.js'
-import * as KeyBindingsInitial from '../KeyBindingsInitial/KeyBindingsInitial.js'
 import * as KeyBindingsViewWorker from '../KeyBindingsViewWorker/KeyBindingsViewWorker.js'
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.js'
-import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.js'
 import * as Viewlet from '../Viewlet/Viewlet.js'
 import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
 import * as WhenExpression from '../WhenExpression/WhenExpression.js'
@@ -48,53 +45,11 @@ export const create = (id, uri, x, y, width, height) => {
 }
 
 export const saveState = (state) => {
-  const { value } = state
-  return {
-    value,
-  }
-}
-
-const getSavedValue = (savedState) => {
-  if (savedState && savedState.value) {
-    return savedState.value
-  }
-  return ''
-}
-
-const getMaxVisibleItems = (height, searchHeaderHeight, tableHeaderHeight, rowHeight) => {
-  return Math.floor((height - searchHeaderHeight - tableHeaderHeight) / rowHeight)
+  return KeyBindingsViewWorker.invoke('KeyBindings.saveState', state)
 }
 
 export const loadContent = async (state, savedState) => {
-  const { height, rowHeight, width, contentPadding, searchHeaderHeight, tableHeaderHeight } = state
-  Assert.number(width)
-  const keyBindings = await KeyBindingsInitial.getKeyBindings()
-  const parsedKeyBindings = await KeyBindingsViewWorker.invoke('ParseKeyBindings.parseKeyBindings', keyBindings)
-  const maxVisibleItems = getMaxVisibleItems(height, searchHeaderHeight, tableHeaderHeight, rowHeight)
-  const savedValue = getSavedValue(savedState)
-  const filteredKeyBindings = await KeyBindingsViewWorker.invoke('FilterKeyBindings.filterKeyBindings', parsedKeyBindings, savedValue)
-  const listHeight = height - searchHeaderHeight - tableHeaderHeight
-  const contentHeight = 2121
-  const scrollBarHeight = ScrollBarFunctions.getScrollBarSize(listHeight, contentHeight, 10)
-  const maxLineY = Math.min(filteredKeyBindings.length, maxVisibleItems)
-  const finalDeltaY = Math.max(contentHeight - listHeight, 0)
-  const contentWidth = width - contentPadding
-  const columnWidth1 = contentWidth / 3
-  const columnWidth2 = contentWidth / 3
-  const columnWidth3 = contentWidth / 3
-  const newState = {
-    ...state,
-    parsedKeyBindings,
-    filteredKeyBindings,
-    maxLineY,
-    maxVisibleItems,
-    value: savedValue,
-    scrollBarHeight,
-    finalDeltaY,
-    columnWidth1,
-    columnWidth2,
-    columnWidth3,
-  }
+  const newState = await KeyBindingsViewWorker.invoke('KeyBindings.loadContent', state, savedState)
   const commands = await KeyBindingsViewWorker.invoke('KeyBindings.render', state, newState)
   newState.commands = commands
   return newState
