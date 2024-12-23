833ac6ec8d0b31ee9283bd63b457971cd720a95f
Le Vivilet <levivlet@pm.me>
2024-12-23 23:48:31 +0100

    feature: support file system blob type

--- packages/renderer-worker/src/parts/FileSystem/FileSystemHtml.js
+++ packages/renderer-worker/src/parts/FileSystem/FileSystemHtml.js
@@ -131,12 +131,13 @@ export const getBlobSrc = async (uri) => {
   return blobUrl
 }
 
-export const getBlob = async (uri) => {
+export const getBlob = async (uri, type) => {
   const handle = await GetFileHandle.getFileHandle(uri)
   if (!handle) {
     throw new Error(`file not found`)
   }
   const file = await handle.getFile()
+  // TODO convert handle to blob mime type?
   return file
 }
 
