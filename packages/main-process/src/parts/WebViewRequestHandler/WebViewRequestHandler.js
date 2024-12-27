9be1ae10e288e3d3738e36fe2b7545ec079dcc28
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-27 11:15:49 +0100

    feature: send headers to preview process (#7662)
    * feature: send headers to preview process
    
    * serialize headers

--- packages/main-process/src/parts/WebViewRequestHandler/WebViewRequestHandler.js
+++ packages/main-process/src/parts/WebViewRequestHandler/WebViewRequestHandler.js
@@ -1,9 +1,11 @@
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+import * as SerializeHeaders from '../SerializeHeaders/SerializeHeaders.js'
 
 export const create = (ipc) => {
   const handleRequest = async (request) => {
-    const { method, url } = request
-    const { body, init } = await JsonRpc.invoke(ipc, 'WebViewProtocol.getResponse', method, url)
+    const { method, url, headers } = request
+    const serializedHeaders = SerializeHeaders.serializeHeaders(headers)
+    const { body, init } = await JsonRpc.invoke(ipc, 'WebViewProtocol.getResponse', method, url, serializedHeaders)
     return new Response(body, init)
   }
   return handleRequest
