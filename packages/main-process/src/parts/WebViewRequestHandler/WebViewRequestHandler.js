b8c16b050359005df7587fb9ba1dbfbb20e7d32f
Le Vivilet <levivlet@pm.me>
2024-12-27 11:03:47 +0100

    feature: send headers to preview process

--- packages/main-process/src/parts/WebViewRequestHandler/WebViewRequestHandler.js
+++ packages/main-process/src/parts/WebViewRequestHandler/WebViewRequestHandler.js
@@ -2,8 +2,8 @@ import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 
 export const create = (ipc) => {
   const handleRequest = async (request) => {
-    const { method, url } = request
-    const { body, init } = await JsonRpc.invoke(ipc, 'WebViewProtocol.getResponse', method, url)
+    const { method, url, headers } = request
+    const { body, init } = await JsonRpc.invoke(ipc, 'WebViewProtocol.getResponse', method, url, headers)
     return new Response(body, init)
   }
   return handleRequest
