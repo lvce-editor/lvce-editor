d29177af787d76cb3f0f1314a15b58a2ea103413
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-29 21:56:44 +0100

    Discard changes to packages/renderer-worker/src/parts/ViewletWebView/ViewletWebViewRender.ts

--- packages/renderer-worker/src/parts/ViewletWebView/ViewletWebViewRender.ts
+++ packages/renderer-worker/src/parts/ViewletWebView/ViewletWebViewRender.ts
@@ -6,7 +6,14 @@ export const hasFunctionalRootRender = true
 
 const renderIframe = {
   isEqual(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
-    return oldState.x === newState.x && oldState.y === newState.y && oldState.width === newState.width && oldState.height === newState.height
+    return (
+      oldState.x === newState.x &&
+      oldState.y === newState.y &&
+      oldState.width === newState.width &&
+      oldState.height === newState.height &&
+      oldState.csp === newState.csp &&
+      oldState.origin === newState.origin
+    )
   },
   apply(oldState: ViewletWebViewState, newState: ViewletWebViewState) {
     return ['setPosition', newState.id, newState.x, newState.y, newState.width, newState.height]
