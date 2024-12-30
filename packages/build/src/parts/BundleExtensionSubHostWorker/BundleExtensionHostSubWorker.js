b41bf2d226f33837064eb1dcaee01d67bcbf3598
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-30 17:06:47 +0100

    feature: use extension host subworker npm package (#7718)

--- packages/build/src/parts/BundleExtensionSubHostWorker/BundleExtensionHostSubWorker.js
+++ packages/build/src/parts/BundleExtensionSubHostWorker/BundleExtensionHostSubWorker.js
@@ -1,26 +1,10 @@
-import * as BundleJs from '../BundleJsRollup/BundleJsRollup.js'
 import * as Copy from '../Copy/Copy.js'
 import * as Path from '../Path/Path.js'
-import * as Replace from '../Replace/Replace.js'
 
 export const bundleExtensionHostSubWorker = async ({ cachePath, commitHash, platform, assetDir }) => {
-  await Copy.copy({
-    from: 'packages/extension-host-sub-worker/src',
-    to: Path.join(cachePath, 'src'),
-  })
-  await Copy.copy({
-    from: 'static/js',
-    to: Path.join(cachePath, 'static', 'js'),
-  })
-  await Replace.replace({
-    path: `${cachePath}/src/parts/AssetDir/AssetDir.js`,
-    occurrence: `ASSET_DIR`,
-    replacement: `'${assetDir}'`,
-  })
-  await BundleJs.bundleJs({
-    cwd: cachePath,
-    from: `./src/extensionHostSubWorkerMain.js`,
-    platform: 'webworker',
-    allowCyclicDependencies: false,
+  const subworkerPath = Path.join(cachePath, 'dist', 'extensionHostSubWorkerMain.js')
+  await Copy.copyFile({
+    from: 'packages/renderer-worker/node_modules/@lvce-editor/extension-host-sub-worker/dist/extensionHostSubWorkerMain.js',
+    to: subworkerPath,
   })
 }
