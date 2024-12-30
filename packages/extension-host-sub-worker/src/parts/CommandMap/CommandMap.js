e87dd258e7bb8ae7b3c8b536296d88f09a5a7bfa
Le Vivilet <levivlet@pm.me>
2024-12-30 13:53:58 +0100

    feature: add setPort command to extension host subworker

--- packages/extension-host-sub-worker/src/parts/CommandMap/CommandMap.js
+++ packages/extension-host-sub-worker/src/parts/CommandMap/CommandMap.js
@@ -1,5 +1,8 @@
 import * as LoadFile from '../LoadFile/LoadFile.js'
+import * as WebView from '../WebView/WebView.js'
 
 export const commandMap = {
   'LoadFile.loadFile': LoadFile.loadFile,
+  '_WebView.create': WebView.create,
+  '_WebView.setPort': WebView.setPort,
 }
