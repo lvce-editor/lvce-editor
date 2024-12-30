ce6c382c43658dd129938afe221a6f9c00b3b12b
Le Vivilet <levivlet@pm.me>
2024-12-30 11:44:43 +0100

    remove unused code

--- packages/renderer-worker/src/parts/IframeWorker/IframeWorker.ts
+++
@@ -1,6 +0,0 @@
-import * as GetOrCreateWorker from '../GetOrCreateWorker/GetOrCreateWorker.js'
-import * as LaunchIframeWorker from '../LaunchIframeWorker/LaunchIframeWorker.js'
-
-const { invoke } = GetOrCreateWorker.getOrCreateWorker(LaunchIframeWorker.launchIframeWorker)
-
-export { invoke }
--- packages/renderer-worker/src/parts/IframeWorkerUrl/IframeWorkerUrl.js
+++
@@ -1,3 +0,0 @@
-import * as AssetDir from '../AssetDir/AssetDir.js'
-
-export const iframeWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/iframe-worker/dist/iframeWorkerMain.js`
