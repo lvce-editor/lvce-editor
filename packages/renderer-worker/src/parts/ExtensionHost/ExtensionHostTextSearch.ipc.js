0f75585fe49a05a755024451e1b48127cc04838d
Le Vivilet <levivlet@pm.me>
2024-12-31 15:01:10 +0100

    feature: add textSearchMemory command

--- packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostTextSearch.ipc.js
+++ packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostTextSearch.ipc.js
@@ -5,5 +5,6 @@ export const name = 'ExtensionHostTextSearch'
 export const Commands = {
   executeTextSearchProvider: ExtensionHostTextSearch.executeTextSearchProvider,
   textSearchFetch: ExtensionHostTextSearch.textSearchFetch,
+  textSearchMemory: ExtensionHostTextSearch.textSearchMemory,
   textSearchHtml: ExtensionHostTextSearch.textSearchHtml,
 }
