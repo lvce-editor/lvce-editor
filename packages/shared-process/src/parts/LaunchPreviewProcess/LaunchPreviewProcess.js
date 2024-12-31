fc316f2aab12b1bf012298fb0f701182f9af4aba
Le Vivilet <levivlet@pm.me>
2024-12-31 08:54:29 +0100

    feature: always use fixed preview process path in production

--- packages/shared-process/src/parts/LaunchPreviewProcess/LaunchPreviewProcess.js
+++ packages/shared-process/src/parts/LaunchPreviewProcess/LaunchPreviewProcess.js
@@ -2,6 +2,7 @@ import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as IsElectron from '../IsElectron/IsElectron.js'
+import * as IsProduction from '../IsProduction/IsProduction.js'
 import * as Preferences from '../Preferences/Preferences.js'
 import * as PreviewProcessPath from '../PreviewProcessPath/PreviewProcessPath.js'
 
@@ -12,10 +13,18 @@ const getMethod = (isElectron) => {
   return IpcParentType.NodeForkedProcess
 }
 
-export const launchPreviewProcess = async () => {
+const getConfiguredUrl = async () => {
   const preferences = await Preferences.getUserPreferences()
   const customPreviewProcessPath = preferences['develop.previewProcessPath']
-  const previewProcessPath = customPreviewProcessPath || PreviewProcessPath.previewProcessPath
+  let previewProcessPath = customPreviewProcessPath || PreviewProcessPath.previewProcessPath
+  if (IsProduction.isProduction) {
+    previewProcessPath = PreviewProcessPath.previewProcessPath
+  }
+  return previewProcessPath
+}
+
+export const launchPreviewProcess = async () => {
+  const previewProcessPath = await getConfiguredUrl()
   const method = getMethod(IsElectron.isElectron)
   const previewProcess = await IpcParent.create({
     method,
