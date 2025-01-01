3676fbc96e9b8591d66c795dcb07677ac4d13de4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-01 13:28:04 +0100

    Discard changes to packages/renderer-worker/src/parts/BulkReplacement/BulkReplacement.js

--- packages/renderer-worker/src/parts/BulkReplacement/BulkReplacement.js
+++ packages/renderer-worker/src/parts/BulkReplacement/BulkReplacement.js
@@ -1,10 +1,27 @@
-import * as LegacyBulkReplacement from '../LegacyBulkReplacement/LegacyBulkReplacement.js'
+import * as Assert from '../Assert/Assert.ts'
+import * as SharedProcess from '../SharedProcess/SharedProcess.js'
+import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.js'
+
+const specialSchemes = ['memfs://', 'html://', 'fetch://']
+
+const isSpecialScheme = (files) => {
+  for (const file of files) {
+    for (const scheme of specialSchemes) {
+      if (file && typeof file === 'string' && file.startsWith(scheme)) {
+        return true
+      }
+    }
+  }
+  return false
+}
 
 export const applyBulkReplacement = async (files, ranges, replacement) => {
-  if (files && ranges && replacement) {
-    return LegacyBulkReplacement.applyLegacyBulkReplacement(files, ranges, replacement)
+  Assert.array(files)
+  Assert.array(ranges)
+  Assert.string(replacement)
+  if (isSpecialScheme(files)) {
+    await ExtensionHostWorker.invoke('BulkReplacement.applyBulkReplacement', files, ranges, replacement)
+    return
   }
-  // new api
-  const edits = files
-  console.log({ edits })
+  await SharedProcess.invoke('BulkReplacement.applyBulkReplacement', files, ranges, replacement)
 }
