f0089aa599d61244d8e0db7ae61c13cece09d114
Le Vivilet <levivlet@pm.me>
2025-01-04 17:27:06 +0100

    feature: add todo

--- packages/static-server/src/parts/ContentSecurityPolicyExtensionHostWorker/ContentSecurityPolicyExtensionHostWorker.js
+++ packages/static-server/src/parts/ContentSecurityPolicyExtensionHostWorker/ContentSecurityPolicyExtensionHostWorker.js
@@ -2,7 +2,7 @@ import * as GetContentSecurityPolicy from '../GetContentSecurityPolicy/GetConten
 
 export const value = GetContentSecurityPolicy.getContentSecurityPolicy([
   `default-src 'none'`,
-  `connect-src 'self'`,
+  `connect-src 'self'`, // TODO should extension host worker be allowed to do network requests?
   `script-src 'self'`,
   `font-src 'self'`,
 ])
