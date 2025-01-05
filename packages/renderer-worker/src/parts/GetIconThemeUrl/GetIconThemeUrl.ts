af531c610531df88419a14adff644d72427f4717
Le Vivilet <levivlet@pm.me>
2025-01-05 13:24:36 +0100

    feature: remove unused code

--- packages/renderer-worker/src/parts/GetIconThemeUrl/GetIconThemeUrl.ts
+++
@@ -1,5 +0,0 @@
-import * as AssetDir from '../AssetDir/AssetDir.js'
-
-export const getIconThemeUrl = (iconThemeId) => {
-  return `${AssetDir.assetDir}/extensions/builtin.${iconThemeId}/icon-theme.json`
-}
