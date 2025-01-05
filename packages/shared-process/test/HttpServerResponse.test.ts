2bf39d937aac79dc225efbf51466b150f0dd11c1
Le Vivilet <levivlet@pm.me>
2025-01-05 09:14:27 +0100

    fix: handle http server response error without crashing process

--- packages/shared-process/test/HttpServerResponse.test.ts
+++ packages/shared-process/test/HttpServerResponse.test.ts
@@ -1,6 +1,7 @@
-import { expect, test } from '@jest/globals'
+import { expect, jest, test } from '@jest/globals'
 import { Writable } from 'node:stream'
 import * as HttpServerResponse from '../src/parts/HttpServerResponse/HttpServerResponse.js'
+import { setTimeout } from 'node:timers/promises'
 
 test('send', () => {
   const request = {}
@@ -40,4 +41,29 @@ test('send - error response', () => {
   HttpServerResponse.send(request, socket, result)
   expect(output).toContain(`HTTP/1.1 404 Not Found`)
   expect(output).toContain('Connection: close')
+})
+
+test('send - epipe error', async () => {
+  const request = {}
+  let output = ''
+  const spy = jest.spyOn(console, 'error').mockImplementation(() => {})
+  const socket = new Writable({
+    write(chunk, encoding, callback) {
+      const error = new Error('write EPIPE')
+      // @ts-ignore
+      error.code = 'EPIPE'
+      callback(error)
+    },
+  })
+  const result = {
+    init: {
+      status: 200,
+      headers: {},
+    },
+  }
+  HttpServerResponse.send(request, socket, result)
+  expect(output).toBe(``)
+  await setTimeout(0)
+  expect(spy).toHaveBeenCalledTimes(1)
+  expect(spy).toHaveBeenCalledWith(`[shared-process] Failed to send response Error: write EPIPE`)
 })
