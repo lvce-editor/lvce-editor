4ce7bef4bfd7137b3d14acc72fc2f5a53db523d6
Le Vivilet <levivlet@pm.me>
2025-01-05 10:27:55 +0100

    build

--- packages/shared-process/src/parts/ExportStatic/ExportStatic.js
+++ packages/shared-process/src/parts/ExportStatic/ExportStatic.js
@@ -127,11 +127,6 @@ const applyOverrides = async ({ root, commitHash, pathPrefix, serverStaticPath }
     `${pathPrefix}/${commitHash}`,
   )
   await replace(
-    Path.join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js'),
-    `return \`\${assetDir}/extensions/builtin.theme-\${colorThemeId}/color-theme.json\``,
-    `return \`\${assetDir}/themes/\${colorThemeId}.json\``,
-  )
-  await replace(
     Path.join(root, 'dist', commitHash, 'packages', 'extension-host-worker', 'dist', 'extensionHostWorkerMain.js'),
     `return \`\${assetDir}/extensions/builtin.theme-\${colorThemeId}/color-theme.json\``,
     `return \`\${assetDir}/themes/\${colorThemeId}.json\``,
