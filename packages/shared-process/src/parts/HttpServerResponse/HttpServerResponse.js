7a1e96ee9736f57397be348099d1afca3ebd7910
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-05 09:26:11 +0100

    fix: handle http server response error without crashing process (#7793)
    * fix: handle http server response error without crashing process
    
    * test

--- packages/shared-process/src/parts/HttpServerResponse/HttpServerResponse.js
+++ packages/shared-process/src/parts/HttpServerResponse/HttpServerResponse.js
@@ -2,7 +2,7 @@ import { ServerResponse } from 'node:http'
 import * as Assert from '../Assert/Assert.js'
 import * as SetHeaders from '../SetHeaders/SetHeaders.js'
 
-export const send = (request, socket, result) => {
+export const send = async (request, socket, result) => {
   Assert.object(request)
   Assert.object(socket)
   Assert.object(result)
@@ -12,6 +12,11 @@ export const send = (request, socket, result) => {
   SetHeaders.setHeaders(response, {
     ...result.init.headers,
     Connection: 'close',
+  })
+
+  // TODO use promises if possible
+  socket.on('error', (error) => {
+    console.error(`[shared-process] Failed to send response ${error}`)
   })
   response.end(result.body)
 }
