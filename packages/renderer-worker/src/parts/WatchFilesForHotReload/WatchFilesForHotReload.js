c7ba35fa9b0dd3ae457cd669c16cd6eb88b931d7
Le Vivilet <levivlet@pm.me>
2025-01-09 23:36:18 +0100

    feature: hot reload explorer when files change

---
+++ packages/renderer-worker/src/parts/WatchFilesForHotReload/WatchFilesForHotReload.js
@@ -0,0 +1,30 @@
+import * as SharedProcess from '../SharedProcess/SharedProcess.js'
+import * as Preferences from '../Preferences/Preferences.js'
+import * as PlatformType from '../PlatformType/PlatformType.js'
+
+const getWatchConfig = () => {
+  const watchConfig = []
+  const explorerPath = Preferences.get('develop.explorerWorkerPath')
+  if (explorerPath) {
+    watchConfig.push({
+      path: explorerPath,
+      command: 'Explorer.hotReload',
+    })
+  }
+  return watchConfig
+}
+
+export const watchFilesForHotReload = async (platform) => {
+  try {
+    if (platform === PlatformType.Web) {
+      return
+    }
+    const config = getWatchConfig()
+    if (config.length === 0) {
+      return
+    }
+    await SharedProcess.invoke('ExtensionHost.watchForHotReload', config)
+  } catch (error) {
+    console.warn(`Failed to watch ${error}`)
+  }
+}
