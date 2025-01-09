df69cdefc1eae56c8d550f7548771fa53aaf378a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-10 00:08:00 +0100

    feature: watch only once per websocket (#7838)

--- packages/shared-process/src/parts/WatchForHotReload/WatchForHotReload.js
+++ packages/shared-process/src/parts/WatchForHotReload/WatchForHotReload.js
@@ -1,7 +1,15 @@
 import * as JsonRpcVersion from '../JsonRpcVersion/JsonRpcVersion.js'
 import * as FileSystemWatch from '../WatchFile/WatchFile.js'
 
+const hasWatcher = new WeakSet()
+
 export const watchForHotReload = async (socket, configs) => {
+  if (hasWatcher.has(socket)) {
+    return
+  }
+  hasWatcher.add(socket)
+  // TODO when socket closes, dispose file watcher?
+  // or keep file watcher and broadcast to all sockets?
   await Promise.all(
     configs.map(async (config) => {
       const callback = () => {
