aa5315d90e0934bf5933953dfc08c49f70576286
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-12 18:26:30 +0100

    Discard changes to packages/renderer-worker/src/parts/ViewletSearch/WrapTextSearchCommand.ts

--- packages/renderer-worker/src/parts/ViewletSearch/WrapTextSearchCommand.ts
+++ packages/renderer-worker/src/parts/ViewletSearch/WrapTextSearchCommand.ts
@@ -4,16 +4,9 @@ export const wrapTextSearchCommand = (key) => {
   const fn = async (state, ...args) => {
     await TextSearchWorker.invoke(`TextSearch.${key}`, state.uid, ...args)
     const commands = await TextSearchWorker.invoke('TextSearch.render', state.uid)
-    const patches = await TextSearchWorker.invoke('TextSearch.renderIncremental', state.uid)
-    console.log({ commands, patches })
-    let actualCommands = commands
-    if (patches.length > 0) {
-      const nonDomCommands = commands.filter((command) => command[0] !== 'Viewlet.setDom2')
-      actualCommands = [['Viewlet.setPatches', state.uid, patches], ...nonDomCommands]
-    }
     return {
       ...state,
-      commands: actualCommands,
+      commands,
     }
   }
   return fn
