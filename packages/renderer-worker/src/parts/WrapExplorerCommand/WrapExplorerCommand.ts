37b2e2251b6017954b5f1a3ca3a878ac4602b9b9
Le Vivilet <levivlet@pm.me>
2025-01-18 12:42:10 +0100

    fix

--- packages/renderer-worker/src/parts/WrapExplorerCommand/WrapExplorerCommand.ts
+++ packages/renderer-worker/src/parts/WrapExplorerCommand/WrapExplorerCommand.ts
@@ -2,11 +2,11 @@ import * as ExplorerViewWorker from '../ExplorerViewWorker/ExplorerViewWorker.js
 
 export const wrapExplorerCommand = (key: string) => {
   const fn = async (state, ...args) => {
-    const newState = await ExplorerViewWorker.invoke(`Explorer.${key}`, state, ...args)
-    if (JSON.stringify(state) === JSON.stringify(newState)) {
+    const newState = await ExplorerViewWorker.invoke(`Explorer.${key}`, state.uid, ...args)
+    const commands = await ExplorerViewWorker.invoke('Explorer.render', state.uid)
+    if (commands.length === 0) {
       return state
     }
-    const commands = await ExplorerViewWorker.invoke('Explorer.render', state, newState)
     return {
       ...newState,
       commands,
