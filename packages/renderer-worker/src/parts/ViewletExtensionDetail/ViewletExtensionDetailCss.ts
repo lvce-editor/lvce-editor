7e2a00779638559ac4b7bcc72d6906ad67946b5e
Le Vivilet <levivlet@pm.me>
2025-01-22 21:27:13 +0100

    feature: make featureContent scrollable

--- packages/renderer-worker/src/parts/ViewletExtensionDetail/ViewletExtensionDetailCss.ts
+++ packages/renderer-worker/src/parts/ViewletExtensionDetail/ViewletExtensionDetailCss.ts
@@ -1,9 +1,10 @@
 export const Css = [
   '/css/parts/AdditionalDetails.css',
+  '/css/parts/Button.css',
+  '/css/parts/FeatureContent.css',
   '/css/parts/Features.css',
   '/css/parts/Markdown.css',
   '/css/parts/ViewletExtensionDetail.css',
   '/css/parts/ViewletExtensionDetailHeader.css',
   '/css/parts/ViewletExtensionDetailTabs.css',
-  '/css/parts/Button.css',
 ]
