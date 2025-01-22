69ae2aba14ca1805ad842d72cd6feee806bc73f6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-22 20:43:15 +0100

    feature: simplify extension detail commands (#7930)
    * feature: simplify extension detail commands
    
    * fix
    
    * fix
    
    * fix
    
    * fix

---
+++ packages/renderer-worker/src/parts/ViewletExtensionDetail/WrapExtensionDetailCommand.ts
@@ -0,0 +1,19 @@
+import * as ExtensionDetailViewWorker from '../ExtensionDetailViewWorker/ExtensionDetailViewWorker.js'
+
+export const wrapExtensionDetailCommand = (key) => {
+  const fn = async (state, ...args) => {
+    const newState = await ExtensionDetailViewWorker.invoke(`ExtensionDetail.${key}`, state, ...args)
+    const dom = await ExtensionDetailViewWorker.invoke(
+      'ExtensionDetail.getVirtualDom',
+      newState,
+      newState.sanitizedReadmeHtml,
+      newState.selectedTab,
+      newState,
+    )
+    return {
+      ...newState,
+      dom,
+    }
+  }
+  return fn
+}
