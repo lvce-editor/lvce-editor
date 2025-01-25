b36185223ef4aadd0e05412426b27c400e9f9113
Le Vivilet <levivlet@pm.me>
2025-01-25 18:50:53 +0100

    fix

--- packages/renderer-worker/src/parts/WrapKeyBindingCommand/WrapKeyBindingCommand.ts
+++ packages/renderer-worker/src/parts/WrapKeyBindingCommand/WrapKeyBindingCommand.ts
@@ -1,9 +1,9 @@
-import * as ExplorerViewWorker from '../ExplorerViewWorker/ExplorerViewWorker.js'
+import * as KeyBindingsViewWorker from '../KeyBindingsViewWorker/KeyBindingsViewWorker.js'
 
 export const wrapKeyBindingCommand = (key: string) => {
   const fn = async (state, ...args) => {
-    await ExplorerViewWorker.invoke(`KeyBindings.${key}`, state.uid, ...args)
-    const commands = await ExplorerViewWorker.invoke('KeyBindings.render2', state.uid)
+    await KeyBindingsViewWorker.invoke(`KeyBindings.${key}`, state.uid, ...args)
+    const commands = await KeyBindingsViewWorker.invoke('KeyBindings.render2', state.uid)
     if (commands.length === 0) {
       return state
     }
