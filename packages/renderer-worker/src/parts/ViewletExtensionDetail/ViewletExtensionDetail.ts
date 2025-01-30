abb4c6b7bee5d691538fbfac3d711431e02ff7d5
Le Vivilet <levivlet@pm.me>
2025-01-30 19:49:01 +0100

    feature: send asset dir to extension detail worker so that the asset dir can be changed more easily

--- packages/renderer-worker/src/parts/ViewletExtensionDetail/ViewletExtensionDetail.ts
+++ packages/renderer-worker/src/parts/ViewletExtensionDetail/ViewletExtensionDetail.ts
@@ -2,6 +2,7 @@ import * as ExtensionDetailViewWorker from '../ExtensionDetailViewWorker/Extensi
 import * as GetViewletSize from '../GetViewletSize/GetViewletSize.js'
 import * as Platform from '../Platform/Platform.js'
 import * as ViewletSize from '../ViewletSize/ViewletSize.js'
+import * as AssetDir from '../AssetDir/AssetDir.js'
 
 export const create = (id: any, uri: string, x: number, y: number, width: number, height: number) => {
   return {
@@ -27,7 +28,17 @@ export const create = (id: any, uri: string, x: number, y: number, width: number
 // TODO when there are multiple extension with the same id,
 // probably need to pass extension location from extensions viewlet
 export const loadContent = async (state, savedState) => {
-  await ExtensionDetailViewWorker.invoke('ExtensionDetail.create', state.uid, state.uri, state.x, state.y, state.width, state.height, null, -1)
+  await ExtensionDetailViewWorker.invoke(
+    'ExtensionDetail.create',
+    state.uid,
+    state.uri,
+    state.x,
+    state.y,
+    state.width,
+    state.height,
+    0,
+    AssetDir.assetDir,
+  )
   await ExtensionDetailViewWorker.invoke('ExtensionDetail.loadContent', state.uid, Platform.platform, savedState)
   const dom = await ExtensionDetailViewWorker.invoke('ExtensionDetail.getVirtualDom2', state.uid)
   return {
@@ -45,7 +56,17 @@ export const hotReload = async (state) => {
   // possible TODO race condition during hot reload
   // there could still be pending promises when the worker is disposed
   await ExtensionDetailViewWorker.restart('ExtensionDetail.terminate')
-  await ExtensionDetailViewWorker.invoke('ExtensionDetail.create', state.uid, state.uri, state.x, state.y, state.width, state.height)
+  await ExtensionDetailViewWorker.invoke(
+    'ExtensionDetail.create',
+    state.uid,
+    state.uri,
+    state.x,
+    state.y,
+    state.width,
+    state.height,
+    0,
+    AssetDir.assetDir,
+  )
   await ExtensionDetailViewWorker.invoke('ExtensionDetail.loadContent', state.uid, {})
   const dom = await ExtensionDetailViewWorker.invoke('ExtensionDetail.getVirtualDom2', state.uid)
   state.isHotReloading = false
