4d38a194181a3d8ccb37aa93b05d33b0a0dd2db2
Le Vivilet <levivlet@pm.me>
2025-02-05 21:35:27 +0100

    fix: about commands

--- packages/renderer-worker/src/parts/ViewletAbout/ViewletAboutCommands.ts
+++ packages/renderer-worker/src/parts/ViewletAbout/ViewletAboutCommands.ts
@@ -1,10 +1,10 @@
-import * as KeyBindingsViewWorker from '../KeyBindingsViewWorker/KeyBindingsViewWorker.js'
+import * as AboutViewWorker from '../AboutViewWorker/AboutViewWorker.js'
 import * as WrapAboutCommand from '../WrapAboutCommand/WrapAboutCommand.ts'
 
 export const Commands = {}
 
 export const getCommands = async () => {
-  const commands = await KeyBindingsViewWorker.invoke('About.getCommandIds')
+  const commands = await AboutViewWorker.invoke('About.getCommandIds')
   for (const command of commands) {
     Commands[command] = WrapAboutCommand.wrapAboutCommand(command)
   }
