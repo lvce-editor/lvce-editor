fca8dadee93e9554a2c0a65f239160416aed3605
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-08 11:26:07 +0100

    feature: add functions to launch markdown worker (#8066)

---
+++ packages/renderer-worker/src/parts/MarkdownWorkerUrl/MarkdownWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const markdownWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/markdown-worker/dist/markdownWorkerMain.js`
