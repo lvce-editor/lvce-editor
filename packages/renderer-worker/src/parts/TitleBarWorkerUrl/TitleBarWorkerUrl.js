ac6a8c43ed95338e40db19d74b3340568a781d30
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-08 23:43:24 +0100

    feature: add functions to launch title bar worker (#8078)

---
+++ packages/renderer-worker/src/parts/TitleBarWorkerUrl/TitleBarWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const titleBarWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/title-bar-worker/dist/titleBarWorkerMain.js`
