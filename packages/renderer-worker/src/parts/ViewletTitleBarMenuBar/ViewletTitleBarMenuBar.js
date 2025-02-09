eebbf116852d19d0c530144cad342c8c76ab1357
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-09 09:53:35 +0100

    feature: use worker for rendering title bar (#8082)
    * feature: use worker for rendering title bar
    
    * fix
    
    * build

--- packages/renderer-worker/src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.js
+++ packages/renderer-worker/src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.js
@@ -1,5 +1,5 @@
-import * as MeasureTitleBarEntryWidth from '../MeasureTitleBarEntryWidth/MeasureTitleBarEntryWidth.js'
 import * as TitleBarMenuBarEntries from '../TitleBarMenuBarEntries/TitleBarMenuBarEntries.js'
+import * as TitleBarWorker from '../TitleBarWorker/TitleBarWorker.js'
 
 export const create = (id, uri, x, y, width, height) => {
   return {
@@ -18,27 +18,41 @@ export const create = (id, uri, x, y, width, height) => {
     y,
     width,
     height,
+    commands: [],
   }
-}
-
-const addWidths = (entries, labelPadding, fontWeight, fontSize, fontFamily, letterSpacing) => {
-  const withWidths = []
-  for (const entry of entries) {
-    const textWidth = MeasureTitleBarEntryWidth.measureTitleBarEntryWidth(entry.label, fontWeight, fontSize, fontFamily, letterSpacing)
-    const width = textWidth + labelPadding * 2
-    withWidths.push({ ...entry, width })
-  }
-  return withWidths
 }
 
 export const loadContent = async (state) => {
-  const { labelFontFamily, labelFontSize, labelFontWeight, labelLetterSpacing, labelPadding } = state
   const titleBarEntries = await TitleBarMenuBarEntries.getEntries()
-  const withWidths = addWidths(titleBarEntries, labelPadding, labelFontWeight, labelFontSize, labelFontFamily, labelLetterSpacing)
-  // const visible = GetVisibleTitleBarEntries.getVisibleTitleBarEntries(withWidths, width)
-  // console.log({ visible })
+  await TitleBarWorker.invoke('TitleBarMenuBar.create', state.uid, state.uri, state.x, state.y, state.width, state.height)
+  await TitleBarWorker.invoke('TitleBarMenuBar.loadContent', state.uid, titleBarEntries)
+  const commands = await TitleBarWorker.invoke('TitleBarMenuBar.render', state.uid)
+  console.log({ commands })
   return {
     ...state,
-    titleBarEntries: withWidths,
+    commands,
+  }
+}
+
+export const hotReload = async (state) => {
+  if (state.isHotReloading) {
+    return state
+  }
+  // TODO avoid mutation
+  state.isHotReloading = true
+  // possible TODO race condition during hot reload
+  // there could still be pending promises when the worker is disposed
+  const savedState = await TitleBarWorker.invoke('TitleBarMenuBar.saveState', state.uid)
+
+  await TitleBarWorker.restart('TitleBarMenuBar.terminate')
+
+  // TODO run create again
+
+  await TitleBarWorker.invoke('TitleBarMenuBar.loadContent', state.uid, savedState)
+  const commands = await TitleBarWorker.invoke('TitleBarMenuBar.render', state.uid)
+  state.commands = commands
+  state.isHotReloading = false
+  return {
+    ...state,
   }
 }
