4bb5393a68a857d9dfd135c6d4c47c4f1251594a
Le Vivilet <levivlet@pm.me>
2025-02-09 09:44:50 +0100

    build

--- packages/build/src/parts/BundleTitleBarWorker/BundleTitleBarWorker.js
+++ packages/build/src/parts/BundleTitleBarWorker/BundleTitleBarWorker.js
@@ -2,9 +2,9 @@ import * as Copy from '../Copy/Copy.js'
 import * as Path from '../Path/Path.js'
 
 export const bundleTitleBarWorker = async ({ cachePath, commitHash, platform, assetDir, version, date, product }) => {
-  const aboutPath = Path.join(cachePath, 'dist', 'aboutWorkerMain.js')
+  const workerPath = Path.join(cachePath, 'dist', 'titleBarWorkerMain.js')
   await Copy.copyFile({
     from: 'packages/renderer-worker/node_modules/@lvce-editor/title-bar-worker/dist/titleBarWorkerMain.js',
-    to: aboutPath,
+    to: workerPath,
   })
 }
