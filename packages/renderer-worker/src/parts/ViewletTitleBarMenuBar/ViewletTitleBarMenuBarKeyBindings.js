a087873f3ef4c34fa258c77817a9d56764bc321b
Le Vivilet <levivlet@pm.me>
2025-02-09 12:00:48 +0100

    feature: ask title bar worker for keybindings

--- packages/renderer-worker/src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBarKeyBindings.js
+++ packages/renderer-worker/src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBarKeyBindings.js
@@ -1,47 +1,6 @@
-import * as KeyCode from '../KeyCode/KeyCode.js'
-import * as WhenExpression from '../WhenExpression/WhenExpression.js'
+import * as TitleBarWorker from '../TitleBarWorker/TitleBarWorker.js'
 
-export const getKeyBindings = () => {
-  return [
-    {
-      key: KeyCode.DownArrow,
-      command: 'TitleBarMenuBar.handleKeyArrowDown',
-      when: WhenExpression.FocusTitleBarMenuBar,
-    },
-    {
-      key: KeyCode.UpArrow,
-      command: 'TitleBarMenuBar.handleKeyArrowUp',
-      when: WhenExpression.FocusTitleBarMenuBar,
-    },
-    {
-      key: KeyCode.RightArrow,
-      command: 'TitleBarMenuBar.handleKeyArrowRight',
-      when: WhenExpression.FocusTitleBarMenuBar,
-    },
-    {
-      key: KeyCode.LeftArrow,
-      command: 'TitleBarMenuBar.handleKeyArrowLeft',
-      when: WhenExpression.FocusTitleBarMenuBar,
-    },
-    {
-      key: KeyCode.Space,
-      command: 'TitleBarMenuBar.handleKeySpace',
-      when: WhenExpression.FocusTitleBarMenuBar,
-    },
-    {
-      key: KeyCode.Home,
-      command: 'TitleBarMenuBar.handleKeyHome',
-      when: WhenExpression.FocusTitleBarMenuBar,
-    },
-    {
-      key: KeyCode.End,
-      command: 'TitleBarMenuBar.handleKeyEnd',
-      when: WhenExpression.FocusTitleBarMenuBar,
-    },
-    {
-      key: KeyCode.Escape,
-      command: 'TitleBarMenuBar.handleKeyEscape',
-      when: WhenExpression.FocusTitleBarMenuBar,
-    },
-  ]
+export const getKeyBindings = async () => {
+  const keyBindings = await TitleBarWorker.invoke('TitleBarMenuBar.getKeyBindings')
+  return keyBindings
 }
