a03fc507bea2be484b4521cbd86bc4be31b0e3a2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-19 22:43:48 +0100

    feature: register extension host commands (#8146)

--- packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostCore.ipc.js
+++ packages/renderer-worker/src/parts/ExtensionHost/ExtensionHostCore.ipc.js
@@ -1,8 +1,10 @@
 import * as ExtensionHostCore from './ExtensionHostCore.js'
+import * as ExtensionHostCommands from './ExtensionHostCommands.js'
 
 export const name = 'ExtensionHost'
 
 // prettier-ignore
 export const Commands = {
   loadWebExtension: ExtensionHostCore.loadWebExtension,
+  getCommands: ExtensionHostCommands.getCommands,
 }
