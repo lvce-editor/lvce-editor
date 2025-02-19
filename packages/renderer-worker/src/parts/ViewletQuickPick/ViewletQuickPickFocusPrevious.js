31fe1cc50a1d651971f925d1d900a4f66ade6a53
Le Vivilet <levivlet@pm.me>
2025-02-19 20:50:54 +0100

    remove unused code

--- packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPickFocusPrevious.js
+++
@@ -1,8 +0,0 @@
-import * as ListIndex from '../ListIndex/ListIndex.js'
-import { focusIndex } from './ViewletQuickPickFocusIndex.js'
-
-export const focusPrevious = (state) => {
-  const { items, focusedIndex } = state
-  const previousIndex = ListIndex.previous(items, focusedIndex)
-  return focusIndex(state, previousIndex)
-}
