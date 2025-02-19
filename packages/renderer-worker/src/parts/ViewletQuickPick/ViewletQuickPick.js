6f067a40dfff4611963895f2cf1e3e68869e369f
Le Vivilet <levivlet@pm.me>
2025-02-19 21:12:44 +0100

    simplify code

--- packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPick.js
+++ packages/renderer-worker/src/parts/ViewletQuickPick/ViewletQuickPick.js
@@ -1,15 +1,10 @@
-import * as Assert from '../Assert/Assert.ts'
+import * as FileSearchWorker from '../FileSearchWorker/FileSearchWorker.js'
 import * as Height from '../Height/Height.js'
 import * as InputSource from '../InputSource/InputSource.js'
 import * as MinimumSliderSize from '../MinimumSliderSize/MinimumSliderSize.js'
-import * as QuickPickEntries from '../QuickPickEntries/QuickPickEntries.js'
-import * as QuickPickEveryThing from '../QuickPickEntriesEverything/QuickPickEntriesEverything.js'
-import * as QuickPickReturnValue from '../QuickPickReturnValue/QuickPickReturnValue.js'
-import * as Viewlet from '../Viewlet/Viewlet.js'
 import * as Platform from '../Platform/Platform.js'
+import * as QuickPickEveryThing from '../QuickPickEntriesEverything/QuickPickEntriesEverything.js'
 import * as VirtualList from '../VirtualList/VirtualList.js'
-import * as FileSearchWorker from '../FileSearchWorker/FileSearchWorker.js'
-import * as ViewletQuickPickGetFilteredItems from './ViewletQuickPickGetFilteredItems.js'
 
 // TODO send open signal to renderer process before items are ready
 // that way user can already type while items are still loading
@@ -57,17 +52,6 @@ export const create = (id, uri, x, y, width, height, args) => {
   }
 }
 
-// TODO naming for provider.getNoResults is a bit weird
-
-const getDefaultValue = (uri) => {
-  switch (uri) {
-    case 'quickPick://everything':
-      return '>'
-    default:
-      return ''
-  }
-}
-
 export const loadContent = async (state) => {
   const { uri, args } = state
   const listItemHeight = 20
@@ -84,114 +68,15 @@ export const loadContent = async (state) => {
     Platform.platform,
     args,
   )
+  await FileSearchWorker.invoke('QuickPick.loadContent', state.uid)
+  const commands = await FileSearchWorker.invoke('QuickPick.render', state.uid)
 
   return {
     ...state,
-    picks: newPicks,
-    items,
-    focusedIndex: 0,
-    state: QuickPickState.Finished,
-    minLineY,
-    maxLineY,
-    value,
-    cursorOffset: value.length,
-    provider,
-    inputSource: InputSource.Script,
-    focused: true,
+    commands,
   }
 }
 
 export const dispose = (state) => {
   return state
 }
-
-export const handleBlur = async (state) => {
-  await Viewlet.closeWidget(state.uid)
-  return state
-}
-
-const getPick = (items, index) => {
-  Assert.array(items)
-  Assert.number(index)
-  // if (index < state.recentPicks.length) {
-  //   return state.recentPicks[index]
-  // }
-  // index -= state.recentPicks.length
-  if (index < items.length) {
-    return items[index].pick
-  }
-  console.warn('no pick matching index', index)
-}
-
-const findLabelIndex = (items, label) => {
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].pick.label === label) {
-      return i
-    }
-  }
-  return -1
-}
-
-export const selectItem = (state, label) => {
-  Assert.string(label)
-  const index = findLabelIndex(state.items, label)
-  if (index === -1) {
-    return state
-  }
-  return selectIndex(state, index)
-}
-
-export const selectIndex = async (state, index, button = /* left */ 0) => {
-  const { minLineY, provider, items } = state
-  const actualIndex = index + minLineY
-  const pick = getPick(items, actualIndex)
-  const selectPickResult = await provider.selectPick(pick, actualIndex, button)
-  Assert.object(selectPickResult)
-  Assert.string(selectPickResult.command)
-  const { command } = selectPickResult
-  switch (command) {
-    case QuickPickReturnValue.Hide:
-      await Viewlet.closeWidget(state.uid)
-      return state
-    default:
-      return state
-  }
-
-  // TODO recent picks should be per provider
-  // if (!state.recentPickIds.has(pick.id)) {
-  //   state.recentPicks.unshift(pick)
-  //   state.recentPickIds.add(pick.id)
-  // }
-  // if (state.recentPicks.length > RECENT_PICKS_MAX_SIZE) {
-  //   const last = state.recentPicks.pop()
-  //   state.recentPickIds.delete(last.id)
-  // }
-}
-
-export const selectCurrentIndex = (state) => {
-  return selectIndex(state, state.focusedIndex)
-}
-
-// TODO use reactive Programming
-// https://angular-2-training-book.rangle.io/http/search_with_switchmap
-
-// searchField.valuesChanges.debounceTime(400).switchMap(term => search(term)).subscribe(result => {
-//   sendRendererProcess(result)
-// })
-// merge(openStream, closeStream, updateStream).switchMap(state => {
-//   switch(state){
-//     case 'default':
-//
-// }
-//
-// })
-
-// TODO merge this with virtual list
-export const handleClickAt = (state, x, y) => {
-  const { top, headerHeight, itemHeight } = state
-  const relativeY = y - top - headerHeight
-  const index = Math.floor(relativeY / itemHeight)
-  return selectIndex(state, index)
-}
-
-export * from '../VirtualList/VirtualList.js'
