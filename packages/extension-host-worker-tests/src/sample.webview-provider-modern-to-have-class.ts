8d81cd0107b51e6019b6311f8984defd69078aaf
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-04 19:33:33 +0100

    feature: enable modern webview class test (#8215)
    * feature: enable modern webview class test
    
    * Discard changes to packages/extension-host-worker-tests/src/sample.webview-provider-message-modern.ts
    
    * skip

---
+++ packages/extension-host-worker-tests/src/sample.webview-provider-modern-to-have-class.ts
@@ -0,0 +1,18 @@
+export const name = 'sample.webview-provider-modern-to-have-class'
+
+export const skip = 1
+
+export const test = async ({ Extension, Main, FileSystem, WebView, expect }) => {
+  // arrange
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, `a`)
+
+  // act
+  await Main.openUri(`${tmpDir}/test.xyz`)
+
+  // assert
+  const webView = await WebView.fromId('xyz')
+  const body = webView.locator('body')
+  await expect(body).toHaveClass('abc')
+}
