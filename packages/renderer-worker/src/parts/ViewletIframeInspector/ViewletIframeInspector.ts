d7f8a5b28d470c71026f4243faedf58e822ffa22
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-10 21:51:27 +0100

    feature: add iframeInspector loadContent function (#8254)

--- packages/renderer-worker/src/parts/ViewletIframeInspector/ViewletIframeInspector.ts
+++ packages/renderer-worker/src/parts/ViewletIframeInspector/ViewletIframeInspector.ts
@@ -1,11 +1,19 @@
 import type { IframeInspectorState } from './ViewletIframeInspectorTypes.ts'
+import * as IframeInspectorWorker from '../IframeInspectorWorker/IframeInspectorWorker.js'
 
 export const create = (): IframeInspectorState => {
   return {
     id: 1,
+    commands: [],
   }
 }
 
 export const loadContent = async (state: IframeInspectorState): Promise<IframeInspectorState> => {
-  return state
+  await IframeInspectorWorker.invoke('IframeInspector.create', state.id)
+  await IframeInspectorWorker.invoke('IframeInspector.loadContent', state.id)
+  const commands = await IframeInspectorWorker.invoke('IframeInspector.render', state.id)
+  return {
+    ...state,
+    commands,
+  }
 }
