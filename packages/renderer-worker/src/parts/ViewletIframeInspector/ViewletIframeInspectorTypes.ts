d807205a64b5354a597ac7a7a40e345cbb8eaa5a
Le Vivilet <levivlet@pm.me>
2025-03-14 19:35:23 +0100

    feature: send x,y,width and height to iframe inspector

--- packages/renderer-worker/src/parts/ViewletIframeInspector/ViewletIframeInspectorTypes.ts
+++ packages/renderer-worker/src/parts/ViewletIframeInspector/ViewletIframeInspectorTypes.ts
@@ -2,4 +2,8 @@ export interface IframeInspectorState {
   readonly id: number
   readonly uid: number
   readonly commands: readonly any[]
+  readonly x: number
+  readonly y: number
+  readonly width: number
+  readonly height: number
 }
