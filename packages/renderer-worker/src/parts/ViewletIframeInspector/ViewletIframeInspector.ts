d93fe40da80672a51cbf5432ec0c0d4a4fc47eae
Le Vivilet <levivlet@pm.me>
2025-03-14 18:22:51 +0100

    rename

--- packages/renderer-worker/src/parts/ViewletIframeInspector/ViewletIframeInspector.ts
+++ packages/renderer-worker/src/parts/ViewletIframeInspector/ViewletIframeInspector.ts
@@ -13,6 +13,7 @@ export const loadContent = async (state: IframeInspectorState): Promise<IframeIn
   await IframeInspectorWorker.invoke('IframeInspector.create', state.id)
   await IframeInspectorWorker.invoke('IframeInspector.loadContent', state.id)
   const commands = await IframeInspectorWorker.invoke('IframeInspector.render', state.id)
+  console.log({ commands })
   return {
     ...state,
     commands,
