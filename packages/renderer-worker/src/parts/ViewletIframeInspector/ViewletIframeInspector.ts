8fb9f7e389fc271cb444418d6d351a45f2aa2c73
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-14 20:14:05 +0100

    feature: send x,y,width and height to iframe inspector (#8265)

--- packages/renderer-worker/src/parts/ViewletIframeInspector/ViewletIframeInspector.ts
+++ packages/renderer-worker/src/parts/ViewletIframeInspector/ViewletIframeInspector.ts
@@ -1,16 +1,20 @@
 import type { IframeInspectorState } from './ViewletIframeInspectorTypes.ts'
 import * as IframeInspectorWorker from '../IframeInspectorWorker/IframeInspectorWorker.js'
 
-export const create = (id: number): IframeInspectorState => {
+export const create = (id: number, uri: string, x: number, y: number, width: number, height: number): IframeInspectorState => {
   return {
     id,
     uid: id,
     commands: [],
+    x,
+    y,
+    width,
+    height,
   }
 }
 
 export const loadContent = async (state: IframeInspectorState): Promise<IframeInspectorState> => {
-  await IframeInspectorWorker.invoke('IframeInspector.create', state.id)
+  await IframeInspectorWorker.invoke('IframeInspector.create', state.id, state.x, state.y, state.width, state.height)
   await IframeInspectorWorker.invoke('IframeInspector.loadContent', state.id)
   const commands = await IframeInspectorWorker.invoke('IframeInspector.render', state.id)
   return {
