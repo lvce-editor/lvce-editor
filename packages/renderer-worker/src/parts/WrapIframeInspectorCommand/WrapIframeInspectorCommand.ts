8abd7e18b9f72c53f440ec475707f71d05a85fbe
Le Vivilet <levivlet@pm.me>
2025-03-14 18:18:19 +0100

    fix

--- packages/renderer-worker/src/parts/WrapIframeInspectorCommand/WrapIframeInspectorCommand.ts
+++ packages/renderer-worker/src/parts/WrapIframeInspectorCommand/WrapIframeInspectorCommand.ts
@@ -1,9 +1,9 @@
-import * as AboutViewWorker from '../AboutViewWorker/AboutViewWorker.js'
+import * as IframeInspectorWorker from '../IframeInspectorWorker/IframeInspectorWorker.js'
 
 export const wrapIframeInspectorCommand = (key: string) => {
   const fn = async (state, ...args) => {
-    const newState = await AboutViewWorker.invoke(`IframeInspector.${key}`, state.uid, ...args)
-    const commands = await AboutViewWorker.invoke('IframeInspector.render', state.uid)
+    const newState = await IframeInspectorWorker.invoke(`IframeInspector.${key}`, state.uid, ...args)
+    const commands = await IframeInspectorWorker.invoke('IframeInspector.render', state.uid)
     if (commands.length === 0) {
       return state
     }
