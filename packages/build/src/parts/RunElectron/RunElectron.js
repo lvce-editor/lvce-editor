5d5bb48e3dab1db890b1a6ee37125138973b01a6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-16 13:42:53 +0100

    feature: add function to run electron app with already bundled main process (#8289)

---
+++ packages/build/src/parts/RunElectron/RunElectron.js
@@ -0,0 +1,30 @@
+import { join } from 'path'
+import { root } from '../Root/Root.js'
+import { spawn } from 'child_process'
+
+let electronPath = ''
+if (process.platform === 'darwin') {
+  electronPath = join(root, 'packages/shared-process/node_modules/electron/dist/Electron.app/Contents/MacOS/Electron')
+} else {
+  electronPath = join(root, 'packages/shared-process/node_modules/electron/dist/electron')
+}
+
+const mainProcessPath = join(root, 'packages', 'shared-process', 'node_modules', '@lvce-editor', 'main-process')
+const sharedProcessPath = join(root, 'packages', 'shared-process', 'src', 'sharedProcessMain.js')
+
+const env = {
+  ...process.env,
+  LVCE_ROOT: root,
+  LVCE_SHARED_PROCESS_PATH: sharedProcessPath,
+}
+
+const main = () => {
+  const child = spawn(electronPath, ['.'], {
+    cwd: mainProcessPath,
+    env,
+  })
+  child.stdout.pipe(process.stdout)
+  child.stderr.pipe(process.stderr)
+}
+
+main()
