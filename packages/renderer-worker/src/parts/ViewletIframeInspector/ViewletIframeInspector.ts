494ee1d317a1bfe6aa6998b45e7953d8b0ca1add
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-19 18:38:58 +0100

    feature: add iframe inspector hot reloading (#8308)

--- packages/renderer-worker/src/parts/ViewletIframeInspector/ViewletIframeInspector.ts
+++ packages/renderer-worker/src/parts/ViewletIframeInspector/ViewletIframeInspector.ts
@@ -22,3 +22,27 @@ export const loadContent = async (state: IframeInspectorState): Promise<IframeIn
     commands,
   }
 }
+
+export const hotReload = async (state) => {
+  if (state.isHotReloading) {
+    return state
+  }
+  // TODO avoid mutation
+  state.isHotReloading = true
+  // possible TODO race condition during hot reload
+  // there could still be pending promises when the worker is disposed
+  const savedState = await IframeInspectorWorker.invoke('IframeInspector.saveState', state.uid)
+  await IframeInspectorWorker.restart('IframeInspector.terminate')
+  const oldState = {
+    ...state,
+    items: [],
+  }
+  await IframeInspectorWorker.invoke('IframeInspector.create', state.id, state.x, state.y, state.width, state.height)
+  await IframeInspectorWorker.invoke('IframeInspector.loadContent', state.id, savedState)
+  const commands = await IframeInspectorWorker.invoke('IframeInspector.render', oldState.id)
+  return {
+    ...oldState,
+    commands,
+    isHotReloading: false,
+  }
+}
