25d77e2b3b221ae9703e39cb89818a2c42795ed7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-24 21:24:52 +0100

    feature: use worker for all extension search commands (#8327)

--- packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsCommands.js
+++ packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsCommands.js
@@ -1,37 +1,12 @@
-import * as VirtualList from '../VirtualList/VirtualList.ipc.js'
+import * as ExtensionSearchViewWorker from '../ExtensionSearchViewWorker/ExtensionSearchViewWorker.js'
 import * as WrapExtensionSearchCommand from '../WrapExtensionSearchCommand/WrapExtensionSearchCommand.ts'
-import * as ViewletExtensions from './ViewletExtensions.js'
 
-// prettier-ignore
-export const Commands = {
-  closeSuggest: ViewletExtensions.closeSuggest,
-  handleInstall: ViewletExtensions.handleInstall,
-  handleUninstall: ViewletExtensions.handleUninstall,
-  openSuggest: ViewletExtensions.openSuggest,
-  toggleSuggest: ViewletExtensions.toggleSuggest,
-}
+export const Commands = {}
 
-// prettier-ignore
-export const LazyCommands = {
-  handleClick: () => import('./ViewletExtensionsHandleClick.js'),
-  handleClickCurrent: () => import('./ViewletExtensionsHandleClickCurrent.js'),
-  handleClickCurrentButKeepFocus: () => import('./ViewletExtensionsHandleClickCurrentButKeepFocus.js'),
-  handleContextMenu: () => import('./ViewletExtensionsHandleContextMenu.js'),
-  clearSearchResults: () => import('./ViewletExtensionsClearSearchResults.js'),
-  handleInput: () => import('./ViewletExtensionsHandleInput.js'),
-  handleFocus: () => import('./ViewletExtensionsHandleFocus.js'),
-  ...VirtualList.LazyCommands
-}
-
-for (const [key, value] of Object.entries(Commands)) {
-  Commands[key] = WrapExtensionSearchCommand.wrapExtensionSearchCommand(value)
-}
-for (const [key, value] of Object.entries(LazyCommands)) {
-  LazyCommands[key] = async () => {
-    const module = await value()
-    const fn = module[key]
-    return {
-      [key]: WrapExtensionSearchCommand.wrapExtensionSearchCommand(fn),
-    }
+export const getCommands = async () => {
+  const commands = await ExtensionSearchViewWorker.invoke('SearchExtensions.getCommandIds')
+  for (const command of commands) {
+    Commands[command] = WrapExtensionSearchCommand.wrapExtensionSearchCommand(command)
   }
+  return Commands
 }
--- packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsResize.js
+++ packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsResize.js
@@ -1,19 +1,15 @@
-import * as GetViewletSize from '../GetViewletSize/GetViewletSize.js'
-import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOfVisibleItems.js'
-import { getListHeight } from './ViewletExtensionsShared.js'
-
 export const hasFunctionalResize = true
 
 export const resize = (state, dimensions) => {
-  const { itemHeight, minLineY, items } = state
-  // TODO should just return new state, render function can take old state and new state and return render commands
-  const listHeight = getListHeight({ ...state, ...dimensions })
-  const maxLineY = Math.min(minLineY + GetNumberOfVisibleItems.getNumberOfVisibleItems(listHeight, itemHeight), items.length)
-  const size = GetViewletSize.getViewletSize(dimensions.width)
-  return {
-    ...state,
-    ...dimensions,
-    maxLineY,
-    size,
-  }
+  return state
+  // // TODO should just return new state, render function can take old state and new state and return render commands
+  // const listHeight = getListHeight({ ...state, ...dimensions })
+  // const maxLineY = Math.min(minLineY + GetNumberOfVisibleItems.getNumberOfVisibleItems(listHeight, itemHeight), items.length)
+  // const size = GetViewletSize.getViewletSize(dimensions.width)
+  // return {
+  //   ...state,
+  //   ...dimensions,
+  //   maxLineY,
+  //   size,
+  // }
 }
