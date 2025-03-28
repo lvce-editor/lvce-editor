46e1b235e1117cbf6201ac25e58bc8fad9a79031
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-28 20:02:03 +0100

    feature: remove unused confirm commands (#8348)
    * feature: remove unused confirm commands
    
    * tsc

--- packages/renderer-worker/src/parts/ViewletConfirm/ViewletConfirmCommands.js
+++ packages/renderer-worker/src/parts/ViewletConfirm/ViewletConfirmCommands.js
@@ -1,7 +1,1 @@
-import * as ViewletAbout from './ViewletConfirm.js'
-
-export const Commands = {
-  handleClickClose: ViewletAbout.handleClickClose,
-  handleClickOk: ViewletAbout.handleClickOk,
-  handleFocusIn: ViewletAbout.handleFocusIn,
-}
+export const Commands = {}
--- packages/renderer-worker/src/parts/ViewletConfirm/ViewletConfirmKeyBindings.js
+++ packages/renderer-worker/src/parts/ViewletConfirm/ViewletConfirmKeyBindings.js
@@ -1,12 +1,3 @@
-import * as KeyCode from '../KeyCode/KeyCode.js'
-import * as WhenExpression from '../WhenExpression/WhenExpression.js'
-
 export const getKeyBindings = () => {
-  return [
-    {
-      key: KeyCode.Escape,
-      command: 'About.handleClickClose',
-      when: WhenExpression.FocusAbout,
-    },
-  ]
+  return []
 }
