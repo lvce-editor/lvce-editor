994a8ff1d7d6dfab68ebadb69263f625e56ca15a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-28 20:04:34 +0100

    feature: remove unused extension code (#8347)
    * feature: remove unused extension code
    
    * test

--- packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsHandleClickCurrent.js
+++
@@ -1,6 +0,0 @@
-import { handleClick } from './ViewletExtensionsHandleClick.js'
-
-export const handleClickCurrent = (state) => {
-  const { focusedIndex } = state
-  return handleClick(state, focusedIndex)
-}
