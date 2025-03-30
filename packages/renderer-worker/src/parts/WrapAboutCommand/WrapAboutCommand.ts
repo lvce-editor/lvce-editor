b7e2ce85685d14c17793fd82c6aa7eacd5a1c1b5
Le Vivilet <levivlet@pm.me>
2025-03-30 16:26:32 +0200

    feature: use new diffing and rendering for about view, keeping about state in worker

--- packages/renderer-worker/src/parts/WrapAboutCommand/WrapAboutCommand.ts
+++ packages/renderer-worker/src/parts/WrapAboutCommand/WrapAboutCommand.ts
@@ -2,13 +2,14 @@ import * as AboutViewWorker from '../AboutViewWorker/AboutViewWorker.js'
 
 export const wrapAboutCommand = (key: string) => {
   const fn = async (state, ...args) => {
-    const newState = await AboutViewWorker.invoke(`About.${key}`, state, ...args)
-    const commands = await AboutViewWorker.invoke('About.render', state, newState)
+    await AboutViewWorker.invoke(`About.${key}`, state.id, ...args)
+    const diffResult = await AboutViewWorker.invoke(`About.diff2`, state.id)
+    const commands = await AboutViewWorker.invoke('About.render2', state.id, diffResult)
     if (commands.length === 0) {
       return state
     }
     return {
-      ...newState,
+      ...state,
       commands,
     }
   }
