a9e8826b2ff03e6c5d1c974ad1883f0a09c53add
Le Vivilet <levivlet@pm.me>
2025-03-30 19:07:53 +0200

    feature: query extension detail commands from worker

--- packages/renderer-worker/src/parts/ViewletExtensionDetail/VielwetExtensionDetailCommands.ts
+++ packages/renderer-worker/src/parts/ViewletExtensionDetail/VielwetExtensionDetailCommands.ts
@@ -1,13 +1,14 @@
+import * as ExtensionDetailViewWorker from '../ExtensionDetailViewWorker/ExtensionDetailViewWorker.js'
 import * as ViewletExtensionDetail from './ViewletExtensionDetail.ts'
 import * as WrapExtensionDetailCommand from './WrapExtensionDetailCommand.ts'
 
-// prettier-ignore
-export const Commands = {
-  handleClickSize: WrapExtensionDetailCommand.wrapExtensionDetailCommand('handleClickSize'),
-  handleFeaturesClick: WrapExtensionDetailCommand.wrapExtensionDetailCommand('handleFeaturesClick'),
-  handleIconError: WrapExtensionDetailCommand.wrapExtensionDetailCommand('handleIconError'),
-  handleReadmeContextMenu: WrapExtensionDetailCommand.wrapExtensionDetailCommand('handleReadmeContextMenu'),
-  handleTabsClick: WrapExtensionDetailCommand.wrapExtensionDetailCommand('handleTabsClick'),
-  hotReload: ViewletExtensionDetail.hotReload,
-  selectTab:  WrapExtensionDetailCommand.wrapExtensionDetailCommand('selectTab'),
+export const Commands = {}
+
+export const getCommands = async () => {
+  const commands = await ExtensionDetailViewWorker.invoke('ExtensionDetail.getCommandIds')
+  for (const command of commands) {
+    Commands[command] = WrapExtensionDetailCommand.wrapExtensionDetailCommand(command)
+  }
+  Commands['hotReload'] = ViewletExtensionDetail.hotReload
+  return Commands
 }
