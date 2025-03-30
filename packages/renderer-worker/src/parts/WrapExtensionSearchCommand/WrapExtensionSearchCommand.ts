6c9b0c447243edfc8edba99d92e11cd2d1ec6568
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-30 17:10:36 +0200

    feature: use diff based renderer for extensions view (#8372)

--- packages/renderer-worker/src/parts/WrapExtensionSearchCommand/WrapExtensionSearchCommand.ts
+++ packages/renderer-worker/src/parts/WrapExtensionSearchCommand/WrapExtensionSearchCommand.ts
@@ -3,7 +3,8 @@ import * as ExtensionSearchViewWorker from '../ExtensionSearchViewWorker/Extensi
 export const wrapExtensionSearchCommand = (key) => {
   const wrapped = async (state, ...args) => {
     await ExtensionSearchViewWorker.invoke(`SearchExtensions.${key}`, state.uid, ...args)
-    const commands = await ExtensionSearchViewWorker.invoke('SearchExtensions.render2', state.uid)
+    const diffResult = await ExtensionSearchViewWorker.invoke(`SearchExtensions.diff2`, state.uid)
+    const commands = await ExtensionSearchViewWorker.invoke('SearchExtensions.render3', state.uid, diffResult)
     return {
       ...state,
       commands,
