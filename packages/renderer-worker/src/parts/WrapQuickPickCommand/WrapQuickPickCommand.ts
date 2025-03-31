ac3f54ac4e5cdff018e8f28951cdc7dcd24c2791
Le Vivilet <levivlet@pm.me>
2025-03-31 23:24:03 +0200

    feature: use new diff based renderer for quickpick

--- packages/renderer-worker/src/parts/WrapQuickPickCommand/WrapQuickPickCommand.ts
+++ packages/renderer-worker/src/parts/WrapQuickPickCommand/WrapQuickPickCommand.ts
@@ -3,7 +3,8 @@ import * as FileSearchWorker from '../FileSearchWorker/FileSearchWorker.js'
 export const wrapQuickPickCommand = (key) => {
   const fn = async (state, ...args) => {
     await FileSearchWorker.invoke(`QuickPick.${key}`, state.uid, ...args)
-    const commands = await FileSearchWorker.invoke('QuickPick.render', state.uid)
+    const diffResult = await FileSearchWorker.invoke('QuickPick.diff2', state.uid)
+    const commands = await FileSearchWorker.invoke('QuickPick.render2', state.uid, diffResult)
     return {
       ...state,
       commands,
