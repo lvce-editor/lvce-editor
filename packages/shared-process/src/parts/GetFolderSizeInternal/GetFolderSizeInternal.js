a198592e47f721e4cea40ec673ca61b04f78eb5f
Le Vivilet <levivlet@pm.me>
2025-04-01 20:01:51 +0200

    fix: don't navigate into symbolic links when querying total size

--- packages/shared-process/src/parts/GetFolderSizeInternal/GetFolderSizeInternal.js
+++ packages/shared-process/src/parts/GetFolderSizeInternal/GetFolderSizeInternal.js
@@ -5,7 +5,7 @@ export const getFolderSizeInternal = async (path) => {
   let total = 0
   const stats = await fs.stat(path)
   total += stats.size
-  if (stats.isDirectory()) {
+  if (stats.isDirectory() && !stats.isSymbolicLink()) {
     const dirents = await fs.readdir(path)
     for (const dirent of dirents) {
       total += await getFolderSizeInternal(join(path, dirent))
