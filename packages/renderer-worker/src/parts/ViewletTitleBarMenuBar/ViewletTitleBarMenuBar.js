c3849d0b358462de4865e1e8bd53449dd2cd3825
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-02 00:58:29 +0200

    feature: use diff based renderer for title bar menu bar (#8400)

--- packages/renderer-worker/src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.js
+++ packages/renderer-worker/src/parts/ViewletTitleBarMenuBar/ViewletTitleBarMenuBar.js
@@ -26,7 +26,8 @@ export const loadContent = async (state) => {
   const titleBarEntries = await TitleBarMenuBarEntries.getEntries()
   await TitleBarWorker.invoke('TitleBarMenuBar.create', state.uid, state.uri, state.x, state.y, state.width, state.height)
   await TitleBarWorker.invoke('TitleBarMenuBar.loadContent', state.uid, titleBarEntries)
-  const commands = await TitleBarWorker.invoke('TitleBarMenuBar.render', state.uid)
+  const diffResult = await TitleBarWorker.invoke('TitleBarMenuBar.diff2', state.uid)
+  const commands = await TitleBarWorker.invoke('TitleBarMenuBar.render2', state.uid, diffResult)
   return {
     ...state,
     commands,
@@ -48,7 +49,8 @@ export const hotReload = async (state) => {
   // TODO run create again
 
   await TitleBarWorker.invoke('TitleBarMenuBar.loadContent', state.uid, savedState)
-  const commands = await TitleBarWorker.invoke('TitleBarMenuBar.render', state.uid)
+  const diffResult = await TitleBarWorker.invoke('TitleBarMenuBar.diff2', state.uid)
+  const commands = await TitleBarWorker.invoke('TitleBarMenuBar.render2', state.uid, diffResult)
   state.commands = commands
   state.isHotReloading = false
   return {
