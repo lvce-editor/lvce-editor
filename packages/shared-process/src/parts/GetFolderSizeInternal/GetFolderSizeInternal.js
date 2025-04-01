1dce8519bc3f7c63b65fa4a89b26e1585371747c
Le Vivilet <levivlet@pm.me>
2025-04-01 19:59:54 +0200

    feature: move folder size into separate file

---
+++ packages/shared-process/src/parts/GetFolderSizeInternal/GetFolderSizeInternal.js
@@ -0,0 +1,15 @@
+import * as fs from 'node:fs/promises'
+import { join } from 'node:path'
+
+export const getFolderSizeInternal = async (path) => {
+  let total = 0
+  const stats = await fs.stat(path)
+  total += stats.size
+  if (stats.isDirectory()) {
+    const dirents = await fs.readdir(path)
+    for (const dirent of dirents) {
+      total += await getFolderSizeInternal(join(path, dirent))
+    }
+  }
+  return total
+}
