cfe9a146592674ffaf38b1cdd0d5768435aa1cfd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-01 19:57:20 +0200

    fix: iframe inspector commands (#8395)

--- packages/renderer-worker/src/parts/WrapIframeInspectorCommand/WrapIframeInspectorCommand.ts
+++ packages/renderer-worker/src/parts/WrapIframeInspectorCommand/WrapIframeInspectorCommand.ts
@@ -3,8 +3,8 @@ import * as IframeInspectorWorker from '../IframeInspectorWorker/IframeInspector
 export const wrapIframeInspectorCommand = (key: string) => {
   const fn = async (state, ...args) => {
     await IframeInspectorWorker.invoke(`IframeInspector.${key}`, state.id, ...args)
-    const diffResult = await IframeInspectorWorker.invoke('IframeInspector.diff', state.id)
-    const commands = await IframeInspectorWorker.invoke('IframeInspector.render', state.id, diffResult)
+    const diffResult = await IframeInspectorWorker.invoke('IframeInspector.diff2', state.id)
+    const commands = await IframeInspectorWorker.invoke('IframeInspector.render2', state.id, diffResult)
     if (commands.length === 0) {
       return state
     }
