bd0196ec5f0ff5a45bf6c68dd60727f78456fde0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-02 22:26:58 +0200

    fix: better handle eloop error when stat fails (#8403)

--- packages/shared-process/src/parts/GetFolderSizeInternal/GetFolderSizeInternal.js
+++ packages/shared-process/src/parts/GetFolderSizeInternal/GetFolderSizeInternal.js
@@ -3,13 +3,17 @@ import { join } from 'node:path'
 
 export const getFolderSizeInternal = async (path) => {
   let total = 0
-  const stats = await fs.stat(path)
-  total += stats.size
-  if (stats.isDirectory() && !stats.isSymbolicLink()) {
-    const dirents = await fs.readdir(path)
-    for (const dirent of dirents) {
-      total += await getFolderSizeInternal(join(path, dirent))
+  try {
+    const stats = await fs.stat(path)
+    total += stats.size
+    if (stats.isDirectory() && !stats.isSymbolicLink()) {
+      const dirents = await fs.readdir(path)
+      for (const dirent of dirents) {
+        total += await getFolderSizeInternal(join(path, dirent))
+      }
     }
+  } catch {
+    return 0
   }
   return total
 }
