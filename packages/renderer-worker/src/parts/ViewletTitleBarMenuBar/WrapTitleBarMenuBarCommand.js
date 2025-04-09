c227cae8ffa3ada305957b418e37596158b14b75
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-09 19:06:58 +0200

    feature: don't rerender title bar if state didn't change (#8446)

--- packages/renderer-worker/src/parts/ViewletTitleBarMenuBar/WrapTitleBarMenuBarCommand.js
+++ packages/renderer-worker/src/parts/ViewletTitleBarMenuBar/WrapTitleBarMenuBarCommand.js
@@ -4,6 +4,9 @@ export const wrapTitleBarMenuBarCommand = (key) => {
   const fn = async (state, ...args) => {
     await TitleBarWorker.invoke(`TitleBarMenuBar.${key}`, state.uid, ...args)
     const diffResult = await TitleBarWorker.invoke('TitleBarMenuBar.diff2', state.uid)
+    if (diffResult.length === 0) {
+      return state
+    }
     const commands = await TitleBarWorker.invoke('TitleBarMenuBar.render2', state.uid, diffResult)
     return {
       ...state,
