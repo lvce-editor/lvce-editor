7a53770987a62ccc044641bf4da09e77487e6f6e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-11 23:08:55 +0200

    feature: don't rerender search view if there are no changes (#8464)

--- packages/renderer-worker/src/parts/ViewletSearch/WrapTextSearchCommand.ts
+++ packages/renderer-worker/src/parts/ViewletSearch/WrapTextSearchCommand.ts
@@ -4,6 +4,9 @@ export const wrapTextSearchCommand = (key) => {
   const fn = async (state, ...args) => {
     await TextSearchWorker.invoke(`TextSearch.${key}`, state.uid, ...args)
     const diffResult = await TextSearchWorker.invoke(`TextSearch.diff2`, state.uid, ...args)
+    if (diffResult.length === 0) {
+      return state
+    }
     const commands = await TextSearchWorker.invoke('TextSearch.render2', state.uid, diffResult)
     return {
       ...state,
