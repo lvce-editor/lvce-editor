dacf095caa68a5b48c0390341c074d45932e8199
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-11 22:18:51 +0200

    feature: ask extension search view worker for menu entries (#8461)

--- packages/renderer-worker/src/parts/MenuEntriesManageExtension/MenuEntriesManageExtension.js
+++ packages/renderer-worker/src/parts/MenuEntriesManageExtension/MenuEntriesManageExtension.js
@@ -1,28 +1,30 @@
-export const getMenuEntries = () => {
-  return [
-    // {
-    //   id: 'enable',
-    //   label: ViewletExtensionsStrings.enable(),
-    //   flags: MenuItemFlags.None,
-    //   command: -1,
-    // },
-    // {
-    //   id: 'disable',
-    //   label: ViewletExtensionsStrings.disable(),
-    //   flags: MenuItemFlags.None,
-    //   command: -1,
-    // },
-    // {
-    //   id: 'uninstall',
-    //   label: ViewletExtensionsStrings.uninstall(),
-    //   flags: MenuItemFlags.None,
-    //   command: 'Extensions.uninstall',
-    // },
-    // {
-    //   id: 'installAnotherVersion',
-    //   label: ViewletExtensionsStrings.installAnotherVersion(),
-    //   flags: MenuItemFlags.None,
-    //   command: -1,
-    // },
-  ]
+import * as ExtensionSearchViewWorker from '../ExtensionSearchViewWorker/ExtensionSearchViewWorker.js'
+
+export const getMenuEntries = async () => {
+  const entries = await ExtensionSearchViewWorker.invoke('SearchExtensions.getMenuEntries')
+  return entries
+  // {
+  //   id: 'enable',
+  //   label: ViewletExtensionsStrings.enable(),
+  //   flags: MenuItemFlags.None,
+  //   command: -1,
+  // },
+  // {
+  //   id: 'disable',
+  //   label: ViewletExtensionsStrings.disable(),
+  //   flags: MenuItemFlags.None,
+  //   command: -1,
+  // },
+  // {
+  //   id: 'uninstall',
+  //   label: ViewletExtensionsStrings.uninstall(),
+  //   flags: MenuItemFlags.None,
+  //   command: 'Extensions.uninstall',
+  // },
+  // {
+  //   id: 'installAnotherVersion',
+  //   label: ViewletExtensionsStrings.installAnotherVersion(),
+  //   flags: MenuItemFlags.None,
+  //   command: -1,
+  // },
 }
