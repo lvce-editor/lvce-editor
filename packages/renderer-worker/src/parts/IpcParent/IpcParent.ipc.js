8fa3fe4a468c7d1bdaaf2502f7905339558d7fd5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-15 23:18:14 +0200

    feature: simplify ipcParent module (#8503)
    * feature: simplify ipcParent module
    
    * feature: simplify ipcParent module

--- packages/renderer-worker/src/parts/IpcParent/IpcParent.ipc.js
+++ packages/renderer-worker/src/parts/IpcParent/IpcParent.ipc.js
@@ -1,7 +1,16 @@
+import * as GetExtensionHostSubWorkerUrl from '../GetExtensionHostSubWorkerUrl/GetExtensionHostSubWorkerUrl.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 
 export const name = 'IpcParent'
 
+const wrappedCreate = (options) => {
+  // TODO
+  if (options && options.url && options.url.endsWith('extensionHostSubWorkerMain.js')) {
+    options.url = GetExtensionHostSubWorkerUrl.getExtensionHostSubWorkerUrl()
+  }
+  return IpcParent.create(options)
+}
+
 export const Commands = {
-  create: IpcParent.create,
+  create: wrappedCreate,
 }
