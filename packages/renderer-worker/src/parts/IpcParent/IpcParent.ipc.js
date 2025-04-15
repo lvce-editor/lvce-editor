417daabf6184197510349b5c0f4497f36a2f18da
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-15 22:57:10 +0200

    Discard changes to packages/renderer-worker/src/parts/IpcParent/IpcParent.ipc.js

--- packages/renderer-worker/src/parts/IpcParent/IpcParent.ipc.js
+++ packages/renderer-worker/src/parts/IpcParent/IpcParent.ipc.js
@@ -1,16 +1,7 @@
-import * as GetExtensionHostSubWorkerUrl from '../GetExtensionHostSubWorkerUrl/GetExtensionHostSubWorkerUrl.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 
 export const name = 'IpcParent'
 
-const wrappedCreate = (options) => {
-  // TODO
-  if (options && options.url && options.url.endsWith('extensionHostSubWorkerMain.js')) {
-    options.url = GetExtensionHostSubWorkerUrl.getExtensionHostSubWorkerUrl()
-  }
-  return IpcParent.create(options)
-}
-
 export const Commands = {
-  create: wrappedCreate,
+  create: IpcParent.create,
 }
