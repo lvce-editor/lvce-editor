5e1736929d7511f64ddd616e235028ae5b687b44
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-15 22:59:28 +0200

    Discard changes to packages/renderer-worker/src/parts/IpcParent/IpcParent.js

--- packages/renderer-worker/src/parts/IpcParent/IpcParent.js
+++ packages/renderer-worker/src/parts/IpcParent/IpcParent.js
@@ -1,6 +1,11 @@
+import * as GetExtensionHostSubWorkerUrl from '../GetExtensionHostSubWorkerUrl/GetExtensionHostSubWorkerUrl.js'
 import * as IpcParentModule from '../IpcParentModule/IpcParentModule.js'
 
 export const create = async ({ method, ...options }) => {
+  // TODO
+  if (options && options.url && options.url.endsWith('extensionHostSubWorkerMain.js')) {
+    options.url = GetExtensionHostSubWorkerUrl.getExtensionHostSubWorkerUrl()
+  }
   const module = await IpcParentModule.getModule(method)
   // @ts-ignore
   const rawIpc = await module.create(options)
