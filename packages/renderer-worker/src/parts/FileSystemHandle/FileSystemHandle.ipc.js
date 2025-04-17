1b76c8b3a8ff921e515d036b268ad0f84ad4bed4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-17 22:21:41 +0200

    feature: add addFileHandle command (#8518)

--- packages/renderer-worker/src/parts/FileSystemHandle/FileSystemHandle.ipc.js
+++ packages/renderer-worker/src/parts/FileSystemHandle/FileSystemHandle.ipc.js
@@ -4,6 +4,7 @@ import * as GetFilePathElectron from '../GetFilePathElectron/GetFilePathElectron
 export const name = 'FileSystemHandle'
 
 export const Commands = {
+  addFilehandle: FileSystemHandle.addFileHandle,
   getFileHandles: FileSystemHandle.getFileHandles,
   getFilePathElectron: GetFilePathElectron.getFilePathElectron,
 }
