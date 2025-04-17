1a48807f80761b411032f74cc9ccbd3c4f298e37
Le Vivilet <levivlet@pm.me>
2025-04-17 22:11:36 +0200

    feature: add addFileHandle command

--- packages/renderer-worker/src/parts/FileSystemHandle/FileSystemHandle.js
+++ packages/renderer-worker/src/parts/FileSystemHandle/FileSystemHandle.js
@@ -3,3 +3,7 @@ import * as RendererProcess from '../RendererProcess/RendererProcess.js'
 export const getFileHandles = (ids) => {
   return RendererProcess.invoke('FileHandles.get', ids)
 }
+
+export const addFileHandle = (fileHandle) => {
+  return RendererProcess.invoke('FileHandles.addFileHandle', fileHandle)
+}
