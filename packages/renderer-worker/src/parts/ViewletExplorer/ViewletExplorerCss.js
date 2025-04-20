1a8e44d76d7af03af31688f3ac485d4a6dad8106
Le Vivilet <levivlet@pm.me>
2025-04-20 08:27:20 +0200

    fix css

--- packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerCss.js
+++ packages/renderer-worker/src/parts/ViewletExplorer/ViewletExplorerCss.js
@@ -5,7 +5,7 @@ export const Css = [
   '/css/parts/FileIcon.css',
   '/css/parts/InputBox.css',
   '/css/parts/Label.css',
-  '/css/parts/ListItems.css',
+  '/css/parts/ViewletList.css',
   '/css/parts/MaskIcon.css',
   '/css/parts/TreeItem.css',
   '/css/parts/ViewletExplorer.css',
