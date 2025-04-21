b3bdb815bf3d43310fc016ff295bd2e622cd868e
Le Vivilet <levivlet@pm.me>
2025-04-21 08:32:44 +0200

    improve get function

--- packages/renderer-worker/src/parts/MouseActions/MouseActions.ts
+++ packages/renderer-worker/src/parts/MouseActions/MouseActions.ts
@@ -4,10 +4,14 @@ export const add = (uid: number, mouseActions: readonly any[]) => {
   registry[uid] = mouseActions
 }
 
+const matchesMouseAction = (item: any, button: number, modifiers: any) => {
+  return item.button === button && Boolean(item.modifiers.ctrl) === modifiers.ctrl && Boolean(item.modifiers.shift) === modifiers.shift
+}
+
 export const get = (uid: number, button: number, modifiers: any) => {
   const items = registry[uid]
   for (const item of items) {
-    if (item.button === button && item.modifiers.ctrl === modifiers.ctrl && item.modifiers.shift === modifiers.shift) {
+    if (matchesMouseAction(item, button, modifiers)) {
       return item.command
     }
   }
