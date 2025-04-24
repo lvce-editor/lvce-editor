a8439d5a4a16590b00699a08f6fedbd4cb504203
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-24 22:25:06 +0200

    fix: don't throw error if passing root folder to getfoldersize (#8568)

--- packages/shared-process/src/parts/GetFolderSizeInternal/GetFolderSizeInternal.js
+++ packages/shared-process/src/parts/GetFolderSizeInternal/GetFolderSizeInternal.js
@@ -2,6 +2,9 @@ import * as fs from 'node:fs/promises'
 import { join } from 'node:path'
 
 export const getFolderSizeInternal = async (path) => {
+  if (path === '/') {
+    throw new Error('Invalid path for folder size')
+  }
   let total = 0
   try {
     const stats = await fs.stat(path)
