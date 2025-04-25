435674bb712b3d6c2bdfd8cc5859233b14f10e3e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-25 22:40:33 +0200

    feature: make file system process url configurable (#8594)

--- packages/shared-process/src/parts/LaunchFileSystemProcess/LaunchFileSystemProcess.js
+++ packages/shared-process/src/parts/LaunchFileSystemProcess/LaunchFileSystemProcess.js
@@ -3,12 +3,25 @@ import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as IsElectron from '../IsElectron/IsElectron.js'
+import * as IsProduction from '../IsProduction/IsProduction.js'
+import * as Preferences from '../Preferences/Preferences.js'
+
+const getConfiguredUrl = async () => {
+  const preferences = await Preferences.getUserPreferences()
+  const customPreviewProcessPath = preferences['develop.fileSystemProcessPath']
+  let previewProcessPath = customPreviewProcessPath || FileSystemProcessPath.fileSystemProcessPath
+  if (IsProduction.isProduction) {
+    previewProcessPath = FileSystemProcessPath.fileSystemProcessPath
+  }
+  return previewProcessPath
+}
 
 export const launchFileSystemProcess = async () => {
   const method = IsElectron.isElectron ? IpcParentType.ElectronUtilityProcess : IpcParentType.NodeForkedProcess
+  const fileSystemProcessPath = await getConfiguredUrl()
   const fileSystemProcess = await IpcParent.create({
     method,
-    path: FileSystemProcessPath.fileSystemProcessPath,
+    path: fileSystemProcessPath,
     argv: [],
     stdio: 'inherit',
     name: 'File System Process',
