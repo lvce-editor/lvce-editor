5331b3b237423bf1a775625b270cf03de92cf738
Le Vivilet <levivlet@pm.me>
2025-04-25 20:01:57 +0200

    launch

---
+++ packages/shared-process/src/parts/FileSystemProcess/FileSystemProcess.js
@@ -0,0 +1,21 @@
+import * as LaunchFileSystemProcess from '../LaunchFileSystemProcess/LaunchFileSystemProcess.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+
+export const state = {
+  /**
+   * @type {any}
+   */
+  ipc: undefined,
+}
+
+export const getOrCreate = async () => {
+  if (!state.ipc) {
+    state.ipc = LaunchFileSystemProcess.launchFileSystemProcess()
+  }
+  return state.ipc
+}
+
+export const invoke = async (method, ...params) => {
+  const ipc = await getOrCreate()
+  return JsonRpc.invoke(ipc, method, ...params)
+}
