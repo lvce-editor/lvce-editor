3391c729ae1bb0ca4958c0fd74bf4f293bf0c184
Le Vivilet <levivlet@pm.me>
2025-04-25 20:01:00 +0200

    feature: add function to launch file system process

---
+++ packages/shared-process/src/parts/FileSystemProcessPath/FileSystemProcessPath.js
@@ -0,0 +1,13 @@
+import * as Path from '../Path/Path.js'
+import * as Root from '../Root/Root.js'
+
+export const fileSystemProcessPath = Path.join(
+  Root.root,
+  'packages',
+  'shared-process',
+  'node_modules',
+  '@lvce-editor',
+  'file-system-process',
+  'src',
+  'fileSystemProcess.js',
+)
