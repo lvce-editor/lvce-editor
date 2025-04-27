b89c3be9fdf6f13b7df54e93f35235c7a7b53937
Le Vivilet <levivlet@pm.me>
2025-04-27 19:59:10 +0200

    feature: add wrapDebugCommand

---
+++ packages/renderer-worker/src/parts/WrapRunAndDebugCommand/WrapRunAndDebugCommand.ts
@@ -0,0 +1,20 @@
+import * as DebugWorker from '../DebugWorker/DebugWorker.js'
+
+export const wrapRunAndDebugCommand = (key: string) => {
+  const fn = async (state, ...args) => {
+    await DebugWorker.invoke(`RunAnDebug.${key}`, state.uid, ...args)
+    const diffResult = await DebugWorker.invoke('RunAndDebug.diff2', state.uid)
+    if (diffResult.length === 0) {
+      return state
+    }
+    const commands = await DebugWorker.invoke('RunAndDebug.render2', state.uid, diffResult)
+    if (commands.length === 0) {
+      return state
+    }
+    return {
+      ...state,
+      commands,
+    }
+  }
+  return fn
+}
