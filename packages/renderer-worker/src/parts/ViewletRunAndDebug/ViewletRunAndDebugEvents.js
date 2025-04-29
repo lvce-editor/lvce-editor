194079bff84196f3ee79835d725c9eefb151abb7
Le Vivilet <levivlet@pm.me>
2025-04-29 19:27:22 +0200

    tsc

--- packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugEvents.js
+++ packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugEvents.js
@@ -2,6 +2,6 @@ import * as ViewletRunAndDebug from './ViewletRunAndDebug.js'
 
 export const Events = {
   'Debug.scriptParsed': ViewletRunAndDebug.handleScriptParsed,
-  'Debug.paused': ViewletRunAndDebug.handlePaused,
+  // 'Debug.paused': ViewletRunAndDebug.handlePaused,
   'Debug.resumed': ViewletRunAndDebug.handleResumed,
 }
