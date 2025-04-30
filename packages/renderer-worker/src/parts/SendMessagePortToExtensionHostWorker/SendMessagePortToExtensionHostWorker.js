3add2060b8ae005f4b48303d71fea0f540e83cf8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-30 19:45:27 +0200

    feature: allow sending rpc id to extension host worker (#8637)

--- packages/renderer-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.js
+++ packages/renderer-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.js
@@ -1,8 +1,8 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.js'
 
-export const sendMessagePortToExtensionHostWorker = async (port, initialCommand) => {
+export const sendMessagePortToExtensionHostWorker = async (port, initialCommand, rpcId) => {
   Assert.object(port)
   Assert.string(initialCommand)
-  await ExtensionHostWorker.invokeAndTransfer(initialCommand, port)
+  await ExtensionHostWorker.invokeAndTransfer(initialCommand, port, rpcId)
 }
