473a161047abb2fc98323525c01eaa2651c04c25
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-30 20:52:21 +0200

    feature: remove unused run and debug code (#8639)
    * feature: remove unused run and debug code
    
    * tsc
    
    * test

--- packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugEvents.js
+++ packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugEvents.js
@@ -1,7 +1,1 @@
-import * as ViewletRunAndDebug from './ViewletRunAndDebug.js'
-
-export const Events = {
-  'Debug.scriptParsed': ViewletRunAndDebug.handleScriptParsed,
-  // 'Debug.paused': ViewletRunAndDebug.handlePaused,
-  'Debug.resumed': ViewletRunAndDebug.handleResumed,
-}
+export const Events = {}
