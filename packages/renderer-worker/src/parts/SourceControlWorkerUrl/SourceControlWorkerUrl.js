6f3fd3602d77c1ff763c61d0239ba79e6f391fd5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-01 13:08:12 +0200

    feature: update source control worker url during build (#8656)
    * feature: update source control worker url during build
    
    * fix url

--- packages/renderer-worker/src/parts/SourceControlWorkerUrl/SourceControlWorkerUrl.js
+++ packages/renderer-worker/src/parts/SourceControlWorkerUrl/SourceControlWorkerUrl.js
@@ -1,3 +1,3 @@
 import * as AssetDir from '../AssetDir/AssetDir.js'
 
-export const sourceControlWorkerUrl = `${AssetDir.assetDir}/packages/source-control-worker/node_modules/@lvce-editor/source-control-worker/dist/sourceControlWorkerMain.js`
+export const sourceControlWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/source-control-worker/dist/sourceControlWorkerMain.js`
