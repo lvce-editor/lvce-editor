ce32575673f3825577b1983d0a170b0b524d3a21
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-01 10:47:23 +0200

    feature: add functions to launch source control worker (#8652)

---
+++ packages/renderer-worker/src/parts/SourceControlWorkerUrl/SourceControlWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const sourceControlWorkerUrl = `${AssetDir.assetDir}/packages/source-control-worker/node_modules/@lvce-editor/source-control-worker/dist/sourceControlWorkerMain.js`
