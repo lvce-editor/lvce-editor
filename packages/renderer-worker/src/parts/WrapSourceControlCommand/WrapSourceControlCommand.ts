117ccdb4dd7abe108677ccf064e9c77b284969f6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-02 18:19:47 +0200

    feature: add wrapSourceControlCommand (#8660)

---
+++ packages/renderer-worker/src/parts/WrapSourceControlCommand/WrapSourceControlCommand.ts
@@ -0,0 +1,20 @@
+import * as SourceControlWorker from '../SourceControlWorker/SourceControlWorker.js'
+
+export const wrapSourceControlCommand = (key: string) => {
+  const fn = async (state, ...args) => {
+    await SourceControlWorker.invoke(`SourceControl.${key}`, state.uid, ...args)
+    const diffResult = await SourceControlWorker.invoke('SourceControl.diff2', state.uid)
+    if (diffResult.length === 0) {
+      return state
+    }
+    const commands = await SourceControlWorker.invoke('SourceControl.render2', state.uid, diffResult)
+    if (commands.length === 0) {
+      return state
+    }
+    return {
+      ...state,
+      commands,
+    }
+  }
+  return fn
+}
