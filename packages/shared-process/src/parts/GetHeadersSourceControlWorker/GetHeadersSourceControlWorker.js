2e1d464170402c30531e6eb96245e33243b8b445
Le Vivilet <levivlet@pm.me>
2025-05-03 13:41:41 +0200

    add headers

---
+++ packages/shared-process/src/parts/GetHeadersSourceControlWorker/GetHeadersSourceControlWorker.js
@@ -0,0 +1,10 @@
+import * as ContentSecurityPolicySourceControlWorker from '../ContentSecurityPolicySourceControlWorker/ContentSecurityPolicySourceControlWorker.js'
+import * as CrossOriginEmbedderPolicy from '../CrossOriginEmbedderPolicy/CrossOriginEmbedderPolicy.js'
+import * as HttpHeader from '../HttpHeader/HttpHeader.js'
+
+export const getHeadersSourceControlWorker = () => {
+  return {
+    [HttpHeader.CrossOriginEmbedderPolicy]: CrossOriginEmbedderPolicy.value,
+    [HttpHeader.ContentSecurityPolicy]: ContentSecurityPolicySourceControlWorker.value,
+  }
+}
