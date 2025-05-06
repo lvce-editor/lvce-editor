1c38ccfdb7c6e244688ed9fbf27bbf7b6c199515
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-06 23:58:33 +0200

    Discard changes to packages/shared-process/src/parts/LaunchPtyHost/LaunchPtyHost.js

--- packages/shared-process/src/parts/LaunchPtyHost/LaunchPtyHost.js
+++ packages/shared-process/src/parts/LaunchPtyHost/LaunchPtyHost.js
@@ -4,12 +4,10 @@ import * as PtyHostState from '../PtyHostState/PtyHostState.js'
 
 export const launchPtyHost = async (method) => {
   const ptyHostPath = PtyHostPath.ptyHostPath
-  console.log({ ptyHostPath })
   const ptyHost = await IpcParent.create({
     method,
     path: ptyHostPath,
     argv: [],
-    execArgv: ['--experimental-strip-types'],
     stdio: 'inherit',
     name: 'Terminal Process',
   })
