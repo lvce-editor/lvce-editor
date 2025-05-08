0a437a1d73aa38d96d967ddbede862079db2fe30
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-08 18:43:01 +0200

    Discard changes to packages/shared-process/src/parts/CreateUtilityProcessRpc/CreateUtilityProcessRpc.js

--- packages/shared-process/src/parts/CreateUtilityProcessRpc/CreateUtilityProcessRpc.js
+++ packages/shared-process/src/parts/CreateUtilityProcessRpc/CreateUtilityProcessRpc.js
@@ -1,12 +1,7 @@
-import { VError } from '@lvce-editor/verror'
 import * as ParentIpc from '../ParentIpc/ParentIpc.js'
 
 export const createUtilityProcessRpc = async (options) => {
-  try {
-    await ParentIpc.invoke('CreateUtilityProcessRpc.createUtilityProcessRpc', {
-      ...options,
-    })
-  } catch (error) {
-    throw new VError(error, `Failed to create electronutility process rpc`)
-  }
+  await ParentIpc.invoke('CreateUtilityProcessRpc.createUtilityProcessRpc', {
+    ...options,
+  })
 }
