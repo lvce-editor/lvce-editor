1fb0cb07b41610777b9564403976de0711a6c4dc
Le Vivilet <levivlet@pm.me>
2025-05-12 00:21:21 +0200

    improve resize

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMainResize.js
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainResize.js
@@ -1,26 +1,42 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as Viewlet from '../Viewlet/Viewlet.js'
 
+const resizeGroups = (groups, dimensions) => {
+  const newGroups = []
+  const width = dimensions.width
+  const groupWidth = width / groups.length
+  let currentX = 0
+  for (const group of groups) {
+    newGroups.push({
+      ...group,
+      x: currentX,
+      y: group.y,
+      width: groupWidth,
+      height: group.height,
+    })
+    currentX += groupWidth
+  }
+  return newGroups
+}
+
 export const resize = async (state, dimensions) => {
   const { groups, tabHeight } = state
-  const x = dimensions.x
-  const contentY = dimensions.y + tabHeight
   const width = dimensions.width
   const contentHeight = dimensions.height - tabHeight
 
   const commands = []
   const groupWidth = width / groups.length
-  let currentX = 0
-  for (const group of groups) {
+  const newGroups = resizeGroups(groups, dimensions)
+  for (const group of newGroups) {
     const { activeIndex, editors, tabsUid } = group
     const editor = editors[activeIndex] || editors[0]
     if (editor) {
       const editorUid = editor.uid
       Assert.number(editorUid)
       const childDimensions = {
-        x: currentX,
-        y: contentY,
-        width: groupWidth,
+        x: group.x,
+        y: group.y,
+        width: group.width,
         height: contentHeight,
       }
       const resizeCommands = await Viewlet.resize(editorUid, childDimensions)
@@ -30,7 +46,6 @@ export const resize = async (state, dimensions) => {
     if (tabsUid !== -1) {
       commands.push(['Viewlet.setBounds', tabsUid, group.x, 0, groupWidth, tabHeight])
     }
-    currentX += groupWidth
   }
   return {
     newState: {
