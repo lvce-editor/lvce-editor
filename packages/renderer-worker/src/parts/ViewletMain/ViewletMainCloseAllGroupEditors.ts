086b3bfd44e1edd84f7522f73d9c2844e0ee1166
Le Vivilet <levivlet@pm.me>
2025-05-11 23:53:09 +0200

    feature: improve editor resizing when closing group

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMainCloseAllGroupEditors.ts
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainCloseAllGroupEditors.ts
@@ -9,14 +9,39 @@ const getEditors = (group) => {
   return group.editors
 }
 
-export const closeGroupAllEditors = (state: MainState, groupIndex: number) => {
+const getResizeCommands = async (groups, width, tabHeight, y): Promise<readonly any[]> => {
+  if (groups.length === 0) {
+    return []
+  }
+  const splitWidth = width / groups.length
+  const allCommands: any[] = []
+  for (const group of groups) {
+    const { editors } = group
+    if (editors.length > 0) {
+      const editor = editors[0]
+      const resizeCommands = await Viewlet.resize(editor.uid, {
+        x: group.x,
+        y: y + group.y + tabHeight,
+        width: splitWidth,
+        height: group.height - tabHeight,
+      })
+      allCommands.push(...resizeCommands)
+    }
+  }
+  return allCommands
+}
+
+export const closeGroupAllEditors = async (state: MainState, groupIndex: number) => {
   // @ts-ignore
-  const { groups, uid } = state
+  const { groups, uid, width, tabHeight, y } = state
   const group = groups[groupIndex]
   const editors = getEditors(group)
   const ids = editors.map(getUid)
-  const commands = [['Viewlet.send', uid, 'dispose'], ...ids.flatMap(Viewlet.disposeFunctional)]
+  const disposeGroupCommands = [['Viewlet.send', uid, 'dispose'], ...ids.flatMap(Viewlet.disposeFunctional)]
   const newGroups = [...groups.slice(0, groupIndex), ...groups.slice(groupIndex + 1)]
+  const resizeOtherGroupsCommands = await getResizeCommands(newGroups, width, tabHeight, y)
+  const commands = [...disposeGroupCommands, ...resizeOtherGroupsCommands]
+
   return {
     newState: {
       ...state,
