4881a7e4cbbbaf6904d772a98cd0239191a5fc08
Le Vivilet <levivlet@pm.me>
2025-05-12 00:16:10 +0200

    feature: improve resizing of editor groups

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMainResize.js
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainResize.js
@@ -7,26 +7,30 @@ export const resize = async (state, dimensions) => {
   const contentY = dimensions.y + tabHeight
   const width = dimensions.width
   const contentHeight = dimensions.height - tabHeight
-  const childDimensions = {
-    x,
-    y: contentY,
-    width,
-    height: contentHeight,
-  }
+
   const commands = []
+  const groupWidth = width / groups.length
+  let currentX = 0
   for (const group of groups) {
     const { activeIndex, editors, tabsUid } = group
-    const editor = editors[activeIndex]
+    const editor = editors[activeIndex] || editors[0]
     if (editor) {
       const editorUid = editor.uid
       Assert.number(editorUid)
+      const childDimensions = {
+        x: currentX,
+        y: contentY,
+        width: groupWidth,
+        height: contentHeight,
+      }
       const resizeCommands = await Viewlet.resize(editorUid, childDimensions)
       commands.push(...resizeCommands)
-      commands.push(['Viewlet.setBounds', editorUid, dimensions.x, tabHeight, dimensions.width, contentHeight])
+      commands.push(['Viewlet.setBounds', editorUid, group.x, tabHeight, groupWidth, contentHeight])
     }
     if (tabsUid !== -1) {
-      commands.push(['Viewlet.setBounds', tabsUid, dimensions.x, 0, dimensions.width, tabHeight])
+      commands.push(['Viewlet.setBounds', tabsUid, group.x, 0, groupWidth, tabHeight])
     }
+    currentX += groupWidth
   }
   return {
     newState: {
