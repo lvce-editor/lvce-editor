c4d64f58ac4b6e7cb993298b309a0b9d1154154c
Le Vivilet <levivlet@pm.me>
2025-05-11 22:47:06 +0200

    feature; improve dimensions when splitting editor right

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMainSplitRight.ts
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainSplitRight.ts
@@ -65,14 +65,17 @@ export const splitRight = async (state) => {
     throw new Error('group not found')
   }
   const { activeIndex, editors, tabsUid } = lastGroup
-  const editor = editors[activeIndex]
-  const dimensions = {
-    x: lastGroup.x,
-    y: lastGroup.y,
-    width: lastGroup.width,
-    height: lastGroup.height,
+  let editor = editors[activeIndex]
+  if (activeIndex === -1 && editors.length > 0) {
+    editor = editors[0]
   }
   const contentHeight = height - tabHeight
+  const dimensions = {
+    x: lastGroup.x,
+    y: y + lastGroup.y + tabHeight,
+    width: lastGroup.width,
+    height: contentHeight,
+  }
   if (editor) {
     const editorUid = editor.uid
     Assert.number(editorUid)
@@ -91,7 +94,18 @@ export const splitRight = async (state) => {
   const realEditor = lastGroup.editors.at(-1)
   const moduleId = await ViewletMap.getModuleId(realEditor.uri)
   const instanceUid = Id.create()
-  const instance = ViewletManager.create(ViewletModule.load, moduleId, state.uid, realEditor.uri, x, y, width, contentHeight)
+  // TODO resize the editor in first group
+  // TODO use half width for editor in second group
+  const instance = ViewletManager.create(
+    ViewletModule.load,
+    moduleId,
+    state.uid,
+    realEditor.uri,
+    x + width / 2,
+    y + tabHeight,
+    width / 2,
+    contentHeight,
+  )
   // @ts-ignore
   instance.show = false
   instance.setBounds = false
