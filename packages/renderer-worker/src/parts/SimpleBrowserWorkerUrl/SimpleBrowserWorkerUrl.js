223166730c66181f6d65826ee9a9796ad99bf603
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-11 22:16:57 +0200

    feature: add function to launch simple browser worker (#8728)

---
+++ packages/renderer-worker/src/parts/SimpleBrowserWorkerUrl/SimpleBrowserWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const simpleBrowserWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/simple-browser-view/dist/simpleBrowserViewWorkerMain.js`
