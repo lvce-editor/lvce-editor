ccef8e78e9bd6f1ff1cf23e2c01ba7d7897534a4
Le Vivilet <levivlet@pm.me>
2025-05-12 20:14:17 +0200

    feature: make embeds process url configurable

--- packages/shared-process/src/parts/LaunchEmbedsProcess/LaunchEmbedsProcess.js
+++ packages/shared-process/src/parts/LaunchEmbedsProcess/LaunchEmbedsProcess.js
@@ -1,14 +1,26 @@
 import * as ConnectIpcToElectron from '../ConnectIpcToElectron/ConnectIpcToElectron.js'
 import * as EmbedsProcessPath from '../EmbedsProcessPath/EmbedsProcessPath.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcId from '../IpcId/IpcId.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
-import * as IpcId from '../IpcId/IpcId.js'
+import * as Platform from '../Platform/Platform.js'
+import * as Preferences from '../Preferences/Preferences.js'
+
+const getConfiguredUrl = async () => {
+  if (Platform.isProduction) {
+    return EmbedsProcessPath.embedsProcessPath
+  }
+  const allPreferences = await Preferences.getAll()
+  const embedsProcessPath = allPreferences['develop.embedsProcessPath'] || EmbedsProcessPath.embedsProcessPath
+  return embedsProcessPath
+}
 
 export const launchEmbedsProcess = async () => {
+  const path = await getConfiguredUrl()
   const ipc = await IpcParent.create({
     method: IpcParentType.ElectronUtilityProcess,
-    path: EmbedsProcessPath.embedsProcessPath,
+    path,
     argv: [],
     stdio: 'inherit',
     name: 'Embeds Process',
