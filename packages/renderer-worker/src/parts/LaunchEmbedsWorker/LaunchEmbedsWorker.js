ce60ace718c8a7c69dde8edff26005301878900a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-12 19:41:26 +0200

    feature: make embeds worker url configurable (#8745)

--- packages/renderer-worker/src/parts/LaunchEmbedsWorker/LaunchEmbedsWorker.js
+++ packages/renderer-worker/src/parts/LaunchEmbedsWorker/LaunchEmbedsWorker.js
@@ -2,11 +2,26 @@ import * as EmbedsWorkerUrl from '../EmbedsWorkerUrl/EmbedsWorkerUrl.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as IsProduction from '../IsProduction/IsProduction.js'
+import * as Preferences from '../Preferences/Preferences.js'
+
+const getConfiguredWorkerUrl = () => {
+  let configuredWorkerUrl = Preferences.get('develop.embedsWorkerPath') || ''
+  if (configuredWorkerUrl) {
+    configuredWorkerUrl = '/remote' + configuredWorkerUrl
+  }
+  configuredWorkerUrl = configuredWorkerUrl || EmbedsWorkerUrl.embedsWorkerUrl
+  if (IsProduction.isProduction) {
+    configuredWorkerUrl = EmbedsWorkerUrl.embedsWorkerUrl
+  }
+  return configuredWorkerUrl
+}
 
 export const launchEmbedsWorker = async () => {
+  const url = getConfiguredWorkerUrl()
   const ipc = await IpcParent.create({
     method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
-    url: EmbedsWorkerUrl.embedsWorkerUrl,
+    url,
     name: 'Embeds Worker',
   })
   HandleIpc.handleIpc(ipc)
