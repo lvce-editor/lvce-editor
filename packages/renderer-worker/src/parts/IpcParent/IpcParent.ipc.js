084b3746147bfb3a936e9a412f64f3ac794cc712
Le Vivilet <levivlet@pm.me>
2025-05-17 23:31:53 +0200

    feature: update rename worker url

--- packages/renderer-worker/src/parts/IpcParent/IpcParent.ipc.js
+++ packages/renderer-worker/src/parts/IpcParent/IpcParent.ipc.js
@@ -1,5 +1,6 @@
 import * as GetColorPickerWorkerUrl from '../GetColorPickerWorkerUrl/GetColorPickerWorkerUrl.js'
 import * as GetExtensionHostSubWorkerUrl from '../GetExtensionHostSubWorkerUrl/GetExtensionHostSubWorkerUrl.js'
+import * as GetRenameWorkerUrl from '../GetRenameWorkerUrl/GetRenameWorkerUrl.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 
 export const name = 'IpcParent'
@@ -12,6 +13,9 @@ const wrappedCreate = (options) => {
   // TODO
   if (options && options.url && options.url.endsWith('colorPickerWorkerMain.js')) {
     options.url = GetColorPickerWorkerUrl.getColorPickerWorkerUrl()
+  }
+  if (options && options.url && options.url.endsWith('renameWorkerMain.js')) {
+    options.url = GetRenameWorkerUrl.getRenameWorkerUrl()
   }
   return IpcParent.create(options)
 }
