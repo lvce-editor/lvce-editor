6e44e7aa7a8d791bbba55d1f27378b7ab94a898c
Le Vivilet <levivlet@pm.me>
2025-05-17 09:30:33 +0200

    feature: add support to launch color picker worker

--- packages/renderer-worker/src/parts/IpcParent/IpcParent.ipc.js
+++ packages/renderer-worker/src/parts/IpcParent/IpcParent.ipc.js
@@ -1,3 +1,4 @@
+import * as GetColorPickerWorkerUrl from '../GetColorPickerWorkerUrl/GetColorPickerWorkerUrl.js'
 import * as GetExtensionHostSubWorkerUrl from '../GetExtensionHostSubWorkerUrl/GetExtensionHostSubWorkerUrl.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 
@@ -7,6 +8,10 @@ const wrappedCreate = (options) => {
   // TODO
   if (options && options.url && options.url.endsWith('extensionHostSubWorkerMain.js')) {
     options.url = GetExtensionHostSubWorkerUrl.getExtensionHostSubWorkerUrl()
+  }
+  // TODO
+  if (options && options.url && options.url.endsWith('colorPickerWorkerMain.js')) {
+    options.url = GetColorPickerWorkerUrl.getColorPickerWorkerUrl()
   }
   return IpcParent.create(options)
 }
