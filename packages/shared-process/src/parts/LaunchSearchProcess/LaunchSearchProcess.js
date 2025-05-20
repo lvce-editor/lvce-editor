ebb7a8be75f261a276a753de0929798653b601a5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-21 00:08:34 +0200

    feature: make search process path configurable (#8796)

--- packages/shared-process/src/parts/LaunchSearchProcess/LaunchSearchProcess.js
+++ packages/shared-process/src/parts/LaunchSearchProcess/LaunchSearchProcess.js
@@ -2,13 +2,25 @@ import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as IsElectron from '../IsElectron/IsElectron.js'
+import * as Platform from '../Platform/Platform.js'
+import * as Preferences from '../Preferences/Preferences.js'
 import * as SearchProcessPath from '../SearchProcessPath/SearchProcessPath.js'
+
+const getConfiguredUrl = async () => {
+  if (Platform.isProduction) {
+    return SearchProcessPath.searchProcessPath
+  }
+  const allPreferences = await Preferences.getAll()
+  const processPath = allPreferences['develop.searchProcessPath'] || SearchProcessPath.searchProcessPath
+  return processPath
+}
 
 export const launchSearchProcess = async () => {
   const method = IsElectron.isElectron ? IpcParentType.ElectronUtilityProcess : IpcParentType.NodeForkedProcess
+  const path = await getConfiguredUrl()
   const searchProcess = await IpcParent.create({
     method,
-    path: SearchProcessPath.searchProcessPath,
+    path,
     argv: [],
     stdio: 'inherit',
     name: 'Search Process',
