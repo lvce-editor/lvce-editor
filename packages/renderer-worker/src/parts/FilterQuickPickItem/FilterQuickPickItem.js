c42d66093acbcd6a3c1fc37ca5396d934639db77
Le Vivilet <levivlet@pm.me>
2025-05-24 14:00:43 +0200

    feature: remove unused code

--- packages/renderer-worker/src/parts/FilterQuickPickItem/FilterQuickPickItem.js
+++
@@ -1,5 +0,0 @@
-import * as FilterCompletionItem from '../FilterCompletionItem/FilterCompletionItem.js'
-
-export const filterQuickPickItem = (pattern, word) => {
-  return FilterCompletionItem.filterCompletionItem(pattern, word)
-}
