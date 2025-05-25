a0461f2272f101093549697dd3528a80642c6bfb
Le Vivilet <levivlet@pm.me>
2025-05-25 19:19:36 +0200

    feature: add completion worker url

---
+++ packages/renderer-worker/src/parts/CompletionWorkerUrl/CompletionWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const completionWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/completion-worker/dist/completionWorkerMain.js`
