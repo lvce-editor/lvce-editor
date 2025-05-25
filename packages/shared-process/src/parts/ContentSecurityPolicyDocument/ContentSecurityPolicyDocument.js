54626dfc9453285c974c61427a7f04d2fbd08e4e
Le Vivilet <levivlet@pm.me>
2025-05-25 20:32:36 +0200

    feature: keep headers in only static server

---
+++ packages/shared-process/src/parts/ContentSecurityPolicyDocument/ContentSecurityPolicyDocument.js
@@ -0,0 +1,52 @@
+import * as GetContentSecurityPolicy from '../GetContentSecurityPolicy/GetContentSecurityPolicy.js'
+import * as GetGitpodPreviewUrl from '../GetGitpodPreviewUrl/GetGitpodPreviewUrl.js'
+import * as IsElectron from '../IsElectron/IsElectron.js'
+import * as IsGitpod from '../IsGitpod/IsGitpod.js'
+import * as Scheme from '../Scheme/Scheme.js'
+
+const getFrameSrc = () => {
+  // TODO make ports configurable
+  if (IsGitpod.isGitpod) {
+    return [`frame-src 'self' ${GetGitpodPreviewUrl.getGitpodPreviewUrl(3001)} ${GetGitpodPreviewUrl.getGitpodPreviewUrl(3002)}`]
+  }
+  if (IsElectron.isElectron) {
+    return [`frame-src ${Scheme.WebView}:`]
+  }
+  return [`frame-src 'self' http://localhost:3001 http://localhost:3002`]
+}
+
+const getManifestSrc = () => {
+  if (IsElectron.isElectron) {
+    return []
+  }
+  return [`manifest-src 'self'`]
+}
+
+const getFrameAncestors = () => {
+  if (IsGitpod.isGitpod) {
+    return [`frame-ancestors *.gitpod.io`]
+  }
+  return [`frame-ancestors 'none'`]
+}
+
+const getSandbox = () => {
+  // disabled because chrome devtools shows a warning
+  // for some reason when sandbox is enabled
+  if (Math) {
+    return []
+  }
+  return [`sandbox allow-scripts allow-same-origin`]
+}
+
+export const value = GetContentSecurityPolicy.getContentSecurityPolicy([
+  `default-src 'none'`,
+  `font-src 'self'`,
+  `img-src 'self' https: data: blob:`, // TODO maybe disallow https and data images
+  `media-src 'self'`,
+  `script-src 'self'`,
+  `style-src 'self'`,
+  ...getFrameAncestors(),
+  ...getFrameSrc(),
+  ...getManifestSrc(),
+  ...getSandbox(),
+])
