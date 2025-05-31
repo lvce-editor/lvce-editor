f2581dbc23e9d53aa902d701f5961dd697be0040
Le Vivilet <levivlet@pm.me>
2025-05-31 11:48:50 +0200

    feature: make removeFocus command available

--- packages/renderer-worker/src/parts/Focus/Focus.ipc.js
+++ packages/renderer-worker/src/parts/Focus/Focus.ipc.js
@@ -5,4 +5,5 @@ export const name = 'Focus'
 export const Commands = {
   setFocus: Focus.setFocus,
   setAdditionalFocus: Focus.setAdditionalFocus,
+  removeAdditionalFocus: Focus.removeAdditionalFocus,
 }
