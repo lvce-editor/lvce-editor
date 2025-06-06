576eae6192aa731e31d31c1bc19bfc3b577ebe05
Le Vivilet <levivlet@pm.me>
2025-06-06 18:30:13 +0200

    feature: add command to send messageport to error worker

--- packages/renderer-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.js
+++ packages/renderer-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.js
@@ -1,6 +1,7 @@
 import * as Assert from '../Assert/Assert.ts'
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.js'
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
+import * as ErrorWorker from '../ErrorWorker/ErrorWorker.js'
 
 export const sendMessagePortToExtensionHostWorker = async (port, initialCommand, rpcId) => {
   Assert.object(port)
@@ -12,4 +13,10 @@ export const sendMessagePortToSharedProcess = async (port, initialCommand, rpcId
   Assert.object(port)
   Assert.string(initialCommand)
   await SharedProcess.invokeAndTransfer(initialCommand, port, rpcId)
+}
+
+export const sendMessagePortToErrorWorker = async (port, initialCommand, rpcId) => {
+  Assert.object(port)
+  Assert.string(initialCommand)
+  await ErrorWorker.invokeAndTransfer(initialCommand, port, rpcId)
 }
