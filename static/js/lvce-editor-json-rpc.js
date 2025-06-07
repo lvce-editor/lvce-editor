1e0f2b3d5a395f52ad97e4aa1692115639e460e2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-07 09:04:39 +0200

    feature: update rpc (#8896)

--- static/js/lvce-editor-json-rpc.js
+++ static/js/lvce-editor-json-rpc.js
@@ -1,383 +1,403 @@
 // @ts-nocheck
-const Two = "2.0";
+const Two = '2.0'
+
 const create$4 = (method, params) => {
   return {
     jsonrpc: Two,
     method,
-    params
-  };
-};
-const JsonRpcEvent = {
-  __proto__: null,
-  create: create$4
-};
-class AssertionError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "AssertionError";
+    params,
   }
 }
-const getType = (value) => {
-  switch (typeof value) {
-    case "number":
-      return "number";
-    case "function":
-      return "function";
-    case "string":
-      return "string";
-    case "object":
-      if (value === null) {
-        return "null";
-      }
-      if (Array.isArray(value)) {
-        return "array";
-      }
-      return "object";
-    case "boolean":
-      return "boolean";
-    default:
-      return "unknown";
-  }
-};
-const number = (value) => {
-  const type = getType(value);
-  if (type !== "number") {
-    throw new AssertionError("expected value to be of type number");
-  }
-};
-const state$1 = {
-  callbacks: Object.create(null)
-};
+
+const JsonRpcEvent = {
+  __proto__: null,
+  create: create$4,
+}
+
+const callbacks = Object.create(null)
 const set = (id, fn) => {
-  state$1.callbacks[id] = fn;
-};
+  callbacks[id] = fn
+}
 const get = (id) => {
-  return state$1.callbacks[id];
-};
+  return callbacks[id]
+}
 const remove = (id) => {
-  delete state$1.callbacks[id];
-};
-const state = {
-  id: 0
-};
+  delete callbacks[id]
+}
+
+let id = 0
 const create$3 = () => {
-  return ++state.id;
-};
-const warn = (...args) => {
-  console.warn(...args);
-};
-const withResolvers = () => {
-  let _resolve;
-  const promise = new Promise((resolve2) => {
-    _resolve = resolve2;
-  });
-  return {
-    resolve: _resolve,
-    promise
-  };
-};
+  return ++id
+}
+
 const registerPromise = () => {
-  const id = create$3();
-  const {
-    resolve: resolve2,
-    promise
-  } = withResolvers();
-  set(id, resolve2);
+  const id = create$3()
+  const { resolve, promise } = Promise.withResolvers()
+  set(id, resolve)
   return {
     id,
-    promise
-  };
-};
-const resolve = (id, args) => {
-  number(id);
-  const fn = get(id);
-  if (!fn) {
-    console.log(args);
-    warn(`callback ${id} may already be disposed`);
-    return;
+    promise,
   }
-  fn(args);
-  remove(id);
-};
+}
+
 const create$2 = (method, params) => {
-  const {
-    id,
-    promise
-  } = registerPromise();
+  const { id, promise } = registerPromise()
   const message = {
     jsonrpc: Two,
     method,
     params,
-    id
-  };
+    id,
+  }
   return {
     message,
-    promise
-  };
-};
-const JsonRpcRequest = {
-  __proto__: null,
-  create: create$2
-};
-class JsonRpcError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "JsonRpcError";
+    promise,
   }
 }
-const NewLine = "\n";
-const DomException = "DOMException";
-const ReferenceError$1 = "ReferenceError";
-const SyntaxError$1 = "SyntaxError";
-const TypeError$1 = "TypeError";
+
+const JsonRpcRequest = {
+  __proto__: null,
+  create: create$2,
+}
+
+class JsonRpcError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = 'JsonRpcError'
+  }
+}
+
+const NewLine = '\n'
+
+const DomException = 'DOMException'
+const ReferenceError$1 = 'ReferenceError'
+const SyntaxError$1 = 'SyntaxError'
+const TypeError$1 = 'TypeError'
+
 const getErrorConstructor = (message, type) => {
   if (type) {
     switch (type) {
       case DomException:
-        return DOMException;
+        return DOMException
       case TypeError$1:
-        return TypeError;
+        return TypeError
       case SyntaxError$1:
-        return SyntaxError;
+        return SyntaxError
       case ReferenceError$1:
-        return ReferenceError;
+        return ReferenceError
       default:
-        return Error;
+        return Error
     }
   }
-  if (message.startsWith("TypeError: ")) {
-    return TypeError;
+  if (message.startsWith('TypeError: ')) {
+    return TypeError
   }
-  if (message.startsWith("SyntaxError: ")) {
-    return SyntaxError;
+  if (message.startsWith('SyntaxError: ')) {
+    return SyntaxError
   }
-  if (message.startsWith("ReferenceError: ")) {
-    return ReferenceError;
+  if (message.startsWith('ReferenceError: ')) {
+    return ReferenceError
   }
-  return Error;
-};
+  return Error
+}
+
 const constructError = (message, type, name) => {
-  const ErrorConstructor = getErrorConstructor(message, type);
+  const ErrorConstructor = getErrorConstructor(message, type)
   if (ErrorConstructor === DOMException && name) {
-    return new ErrorConstructor(message, name);
+    return new ErrorConstructor(message, name)
   }
   if (ErrorConstructor === Error) {
-    const error = new Error(message);
-    if (name && name !== "VError") {
-      error.name = name;
+    const error = new Error(message)
+    if (name && name !== 'VError') {
+      error.name = name
     }
-    return error;
+    return error
   }
-  return new ErrorConstructor(message);
-};
-const getNewLineIndex = (string, startIndex = void 0) => {
-  return string.indexOf(NewLine, startIndex);
-};
-const getParentStack = (error) => {
-  let parentStack = error.stack || error.data || error.message || "";
-  if (parentStack.startsWith("    at")) {
-    parentStack = error.message + NewLine + parentStack;
-  }
-  return parentStack;
-};
+  return new ErrorConstructor(message)
+}
+
 const joinLines = (lines) => {
-  return lines.join(NewLine);
-};
-const MethodNotFound = -32601;
-const Custom = -32001;
+  return lines.join(NewLine)
+}
+
 const splitLines = (lines) => {
-  return lines.split(NewLine);
-};
-const restoreJsonRpcError = (error) => {
-  if (error && error instanceof Error) {
-    return error;
+  return lines.split(NewLine)
+}
+
+const getCurrentStack = () => {
+  const currentStack = joinLines(splitLines(new Error().stack || '').slice(2))
+  return currentStack
+}
+
+const getNewLineIndex = (string, startIndex = undefined) => {
+  return string.indexOf(NewLine, startIndex)
+}
+
+const getParentStack = (error) => {
+  let parentStack = error.stack || error.data || error.message || ''
+  if (parentStack.startsWith('    at')) {
+    parentStack = error.message + NewLine + parentStack
   }
-  const currentStack = joinLines(splitLines(new Error().stack || "").slice(1));
+  return parentStack
+}
+
+const MethodNotFound = -32601
+const Custom = -32001
+
+const restoreJsonRpcError = (error) => {
+  const currentStack = getCurrentStack()
+  if (error && error instanceof Error) {
+    if (typeof error.stack === 'string') {
+      error.stack = error.stack + NewLine + currentStack
+    }
+    return error
+  }
   if (error && error.code && error.code === MethodNotFound) {
-    const restoredError = new JsonRpcError(error.message);
-    const parentStack = getParentStack(error);
-    restoredError.stack = parentStack + NewLine + currentStack;
-    return restoredError;
+    const restoredError = new JsonRpcError(error.message)
+    const parentStack = getParentStack(error)
+    restoredError.stack = parentStack + NewLine + currentStack
+    return restoredError
   }
   if (error && error.message) {
-    const restoredError = constructError(error.message, error.type, error.name);
+    const restoredError = constructError(error.message, error.type, error.name)
     if (error.data) {
       if (error.data.stack && error.data.type && error.message) {
-        restoredError.stack = error.data.type + ": " + error.message + NewLine + error.data.stack + NewLine + currentStack;
+        restoredError.stack = error.data.type + ': ' + error.message + NewLine + error.data.stack + NewLine + currentStack
       } else if (error.data.stack) {
-        restoredError.stack = error.data.stack;
+        restoredError.stack = error.data.stack
       }
       if (error.data.codeFrame) {
-        restoredError.codeFrame = error.data.codeFrame;
+        // @ts-ignore
+        restoredError.codeFrame = error.data.codeFrame
       }
       if (error.data.code) {
-        restoredError.code = error.data.code;
+        // @ts-ignore
+        restoredError.code = error.data.code
       }
       if (error.data.type) {
-        restoredError.name = error.data.type;
+        // @ts-ignore
+        restoredError.name = error.data.type
       }
     } else {
       if (error.stack) {
-        const lowerStack = restoredError.stack || "";
-        const indexNewLine = getNewLineIndex(lowerStack);
-        const parentStack = getParentStack(error);
-        restoredError.stack = parentStack + lowerStack.slice(indexNewLine);
+        const lowerStack = restoredError.stack || ''
+        // @ts-ignore
+        const indexNewLine = getNewLineIndex(lowerStack)
+        const parentStack = getParentStack(error)
+        // @ts-ignore
+        restoredError.stack = parentStack + lowerStack.slice(indexNewLine)
       }
       if (error.codeFrame) {
-        restoredError.codeFrame = error.codeFrame;
+        // @ts-ignore
+        restoredError.codeFrame = error.codeFrame
       }
     }
-    return restoredError;
+    return restoredError
   }
-  if (typeof error === "string") {
-    return new Error(`JsonRpc Error: ${error}`);
+  if (typeof error === 'string') {
+    return new Error(`JsonRpc Error: ${error}`)
   }
-  return new Error(`JsonRpc Error: ${error}`);
-};
+  return new Error(`JsonRpc Error: ${error}`)
+}
+
 const unwrapJsonRpcResult = (responseMessage) => {
-  if ("error" in responseMessage) {
-    const restoredError = restoreJsonRpcError(responseMessage.error);
-    throw restoredError;
+  if ('error' in responseMessage) {
+    const restoredError = restoreJsonRpcError(responseMessage.error)
+    throw restoredError
   }
-  if ("result" in responseMessage) {
-    return responseMessage.result;
+  if ('result' in responseMessage) {
+    return responseMessage.result
   }
-  throw new JsonRpcError("unexpected response message");
-};
-const create$1 = (message, error) => {
-  return {
-    jsonrpc: Two,
-    id: message.id,
-    error
-  };
-};
-const E_COMMAND_NOT_FOUND = "E_COMMAND_NOT_FOUND";
+  throw new JsonRpcError('unexpected response message')
+}
+
+const warn = (...args) => {
+  console.warn(...args)
+}
+
+const resolve = (id, response) => {
+  const fn = get(id)
+  if (!fn) {
+    console.log(response)
+    warn(`callback ${id} may already be disposed`)
+    return
+  }
+  fn(response)
+  remove(id)
+}
+
+const E_COMMAND_NOT_FOUND = 'E_COMMAND_NOT_FOUND'
+
+const getErrorType = (prettyError) => {
+  if (prettyError && prettyError.type) {
+    return prettyError.type
+  }
+  if (prettyError && prettyError.constructor && prettyError.constructor.name) {
+    return prettyError.constructor.name
+  }
+  return undefined
+}
+
+const isAlreadyStack = (line) => {
+  return line.trim().startsWith('at ')
+}
+const getStack = (prettyError) => {
+  const stackString = prettyError.stack || ''
+  const newLineIndex = stackString.indexOf('\n')
+  if (newLineIndex !== -1 && !isAlreadyStack(stackString.slice(0, newLineIndex))) {
+    return stackString.slice(newLineIndex + 1)
+  }
+  return stackString
+}
 const getErrorProperty = (error, prettyError) => {
   if (error && error.code === E_COMMAND_NOT_FOUND) {
     return {
       code: MethodNotFound,
       message: error.message,
-      data: error.stack
-    };
+      data: error.stack,
+    }
   }
   return {
     code: Custom,
     message: prettyError.message,
     data: {
-      stack: prettyError.stack,
+      stack: getStack(prettyError),
       codeFrame: prettyError.codeFrame,
-      type: prettyError.type,
-      code: prettyError.code
-    }
-  };
-};
+      type: getErrorType(prettyError),
+      code: prettyError.code,
+      name: prettyError.name,
+    },
+  }
+}
+
+const create$1 = (message, error) => {
+  return {
+    jsonrpc: Two,
+    id: message.id,
+    error,
+  }
+}
+
 const getErrorResponse = (message, error, preparePrettyError, logError) => {
-  const prettyError = preparePrettyError(error);
-  logError(error, prettyError);
-  const errorProperty = getErrorProperty(error, prettyError);
-  return create$1(message, errorProperty);
-};
+  const prettyError = preparePrettyError(error)
+  logError(error, prettyError)
+  const errorProperty = getErrorProperty(error, prettyError)
+  return create$1(message, errorProperty)
+}
+
 const create = (message, result) => {
   return {
     jsonrpc: Two,
     id: message.id,
-    result: result != null ? result : null
-  };
-};
+    result: result ?? null,
+  }
+}
+
 const getSuccessResponse = (message, result) => {
-  const resultProperty = result != null ? result : null;
-  return create(message, resultProperty);
-};
+  const resultProperty = result ?? null
+  return create(message, resultProperty)
+}
+
 const getResponse = async (message, ipc, execute, preparePrettyError, logError, requiresSocket) => {
   try {
-    const result = requiresSocket(message.method) ? await execute(message.method, ipc, ...message.params) : await execute(message.method, ...message.params);
-    return getSuccessResponse(message, result);
+    const result = requiresSocket(message.method)
+      ? await execute(message.method, ipc, ...message.params)
+      : await execute(message.method, ...message.params)
+    return getSuccessResponse(message, result)
   } catch (error) {
-    return getErrorResponse(message, error, preparePrettyError, logError);
+    return getErrorResponse(message, error, preparePrettyError, logError)
   }
-};
+}
+
 const defaultPreparePrettyError = (error) => {
-  return error;
-};
+  return error
+}
 const defaultLogError = () => {
-};
+  // ignore
+}
 const defaultRequiresSocket = () => {
-  return false;
-};
-const defaultResolve = resolve;
-const handleJsonRpcMessage = async (...args) => {
-  let message;
-  let ipc;
-  let execute;
-  let preparePrettyError;
-  let logError;
-  let resolve2;
-  let requiresSocket;
+  return false
+}
+const defaultResolve = resolve
+
+// TODO maybe remove this in v6 or v7, only accept options object to simplify the code
+const normalizeParams = (args) => {
   if (args.length === 1) {
-    const arg = args[0];
-    message = arg.message;
-    ipc = arg.ipc;
-    execute = arg.execute;
-    preparePrettyError = arg.preparePrettyError || defaultPreparePrettyError;
-    logError = arg.logError || defaultLogError;
-    requiresSocket = arg.requiresSocket || defaultRequiresSocket;
-    resolve2 = arg.resolve || defaultResolve;
-  } else {
-    ipc = args[0];
-    message = args[1];
-    execute = args[2];
-    resolve2 = args[3];
-    preparePrettyError = args[4];
-    logError = args[5];
-    requiresSocket = args[6];
-  }
-  if ("id" in message) {
-    if ("method" in message) {
-      const response = await getResponse(message, ipc, execute, preparePrettyError, logError, requiresSocket);
-      try {
-        ipc.send(response);
-      } catch (error) {
-        const errorResponse = getErrorResponse(message, error, preparePrettyError, logError);
-        ipc.send(errorResponse);
-      }
-      return;
+    const options = args[0]
+    return {
+      ipc: options.ipc,
+      message: options.message,
+      execute: options.execute,
+      resolve: options.resolve || defaultResolve,
+      preparePrettyError: options.preparePrettyError || defaultPreparePrettyError,
+      logError: options.logError || defaultLogError,
+      requiresSocket: options.requiresSocket || defaultRequiresSocket,
     }
-    resolve2(message.id, message);
-    return;
   }
-  if ("method" in message) {
-    await getResponse(message, ipc, execute, preparePrettyError, logError, requiresSocket);
-    return;
+  return {
+    ipc: args[0],
+    message: args[1],
+    execute: args[2],
+    resolve: args[3],
+    preparePrettyError: args[4],
+    logError: args[5],
+    requiresSocket: args[6],
   }
-  throw new JsonRpcError("unexpected message");
-};
+}
+
+const handleJsonRpcMessage = async (...args) => {
+  const options = normalizeParams(args)
+  const { message, ipc, execute, resolve, preparePrettyError, logError, requiresSocket } = options
+  if ('id' in message) {
+    if ('method' in message) {
+      const response = await getResponse(message, ipc, execute, preparePrettyError, logError, requiresSocket)
+      try {
+        ipc.send(response)
+      } catch (error) {
+        const errorResponse = getErrorResponse(message, error, preparePrettyError, logError)
+        ipc.send(errorResponse)
+      }
+      return
+    }
+    resolve(message.id, message)
+    return
+  }
+  if ('method' in message) {
+    await getResponse(message, ipc, execute, preparePrettyError, logError, requiresSocket)
+    return
+  }
+  throw new JsonRpcError('unexpected message')
+}
+
+const invokeHelper = async (ipc, method, params, useSendAndTransfer) => {
+  const { message, promise } = create$2(method, params)
+  if (useSendAndTransfer && ipc.sendAndTransfer) {
+    ipc.sendAndTransfer(message)
+  } else {
+    ipc.send(message)
+  }
+  const responseMessage = await promise
+  return unwrapJsonRpcResult(responseMessage)
+}
 const send = (transport, method, ...params) => {
-  const message = create$4(method, params);
-  transport.send(message);
-};
-const invoke = async (ipc, method, ...params) => {
-  const {
-    message,
-    promise
-  } = create$2(method, params);
-  ipc.send(message);
-  const responseMessage = await promise;
-  const result = unwrapJsonRpcResult(responseMessage);
-  return result;
-};
-const invokeAndTransfer = async (ipc, handle, method, ...params) => {
-  if (typeof handle === "string") {
-    params = [method, ...params];
-    method = handle;
-  }
-  const {
-    message,
-    promise
-  } = create$2(method, params);
-  ipc.sendAndTransfer(message);
-  const responseMessage = await promise;
-  const result = unwrapJsonRpcResult(responseMessage);
-  return result;
-};
-export {JsonRpcEvent, JsonRpcRequest, getErrorResponse, getSuccessResponse, handleJsonRpcMessage, invoke, invokeAndTransfer, registerPromise, resolve, send};
-export default null;
+  const message = create$4(method, params)
+  transport.send(message)
+}
+const invoke = (ipc, method, ...params) => {
+  return invokeHelper(ipc, method, params, false)
+}
+const invokeAndTransfer = (ipc, method, ...params) => {
+  return invokeHelper(ipc, method, params, true)
+}
+
+export {
+  JsonRpcEvent,
+  JsonRpcRequest,
+  getErrorResponse,
+  getSuccessResponse,
+  handleJsonRpcMessage,
+  invoke,
+  invokeAndTransfer,
+  registerPromise,
+  resolve,
+  send,
+}
