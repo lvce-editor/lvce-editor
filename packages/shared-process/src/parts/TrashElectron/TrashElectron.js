82af4006b0e6791e2016ebb3847a73f8d30869b8
Le Vivilet <levivlet@pm.me>
2025-06-12 20:02:03 +0200

    test

--- packages/shared-process/src/parts/TrashElectron/TrashElectron.js
+++ packages/shared-process/src/parts/TrashElectron/TrashElectron.js
@@ -2,7 +2,9 @@ import { existsSync } from 'node:fs'
 import * as MainProcess from '../MainProcess/MainProcess.js'
 
 export const trash = async (path) => {
+  console.log({ existsSync })
   if (!existsSync(path)) {
+    console.log('not exists')
     return
   }
   await MainProcess.invoke('Trash.trash', path)
