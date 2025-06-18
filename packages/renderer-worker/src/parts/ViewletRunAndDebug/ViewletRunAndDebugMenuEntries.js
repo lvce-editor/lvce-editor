ee4f5b7f540ba727c69c34cfb7cd6596e4d068cb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-19 00:03:33 +0200

    fix: worker name (#8983)
    * fix: worker name
    
    * adjust
    
    * log
    
    * return

--- packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugMenuEntries.js
+++ packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugMenuEntries.js
@@ -1,11 +1,19 @@
-import * as TitleBarWorker from '../TitleBarWorker/TitleBarWorker.js'
+import * as DebugWorker from '../DebugWorker/DebugWorker.js'
 
 export const menus = []
 
 export const getMenus = async () => {
   try {
-    const modules = await TitleBarWorker.invoke('RunAndDebug.getMenuEntries')
-    return modules
+    const modules = await DebugWorker.invoke('RunAndDebug.getMenuEntries')
+    const adjusted = modules.map((module) => {
+      return {
+        ...module,
+        async getMenuEntries() {
+          return module.entries
+        },
+      }
+    })
+    return adjusted
   } catch {
     return []
   }
