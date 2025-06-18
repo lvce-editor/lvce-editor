ad57273331df2c7e5dc3199681026cc6af5c8080
Le Vivilet <levivlet@pm.me>
2025-06-18 23:48:57 +0200

    log

--- packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugMenuEntries.js
+++ packages/renderer-worker/src/parts/ViewletRunAndDebug/ViewletRunAndDebugMenuEntries.js
@@ -5,7 +5,6 @@ export const menus = []
 export const getMenus = async () => {
   try {
     const modules = await DebugWorker.invoke('RunAndDebug.getMenuEntries')
-    console.log({ modules })
     const adjusted = modules.map((module) => {
       return {
         ...module,
