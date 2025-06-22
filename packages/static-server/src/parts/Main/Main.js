ad5cc2fb915b0d73a99b660c5d79c764fa2e2ecd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-22 21:56:00 +0200

    feature: remove unused code (#9016)

--- packages/static-server/src/parts/Main/Main.js
+++ packages/static-server/src/parts/Main/Main.js
@@ -1,11 +1,7 @@
 import * as HandleUncaughtExceptionMonitor from '../HandleUncaughtExceptionMonitor/HandleUncaughtExceptionMonitor.js'
 import * as Listen from '../Listen/Listen.js'
 
-export const main = () => {
+export const main = async () => {
   process.on('uncaughtExceptionMonitor', HandleUncaughtExceptionMonitor.handleUncaughtExceptionMonitor)
-  Listen.listen(process.argv)
-  // process.on('message', HandleMessageFromParent.handleMessageFromParent)
-  // if (process.send) {
-  //   process.send('ready')
-  // }
+  await Listen.listen(process.argv)
 }
