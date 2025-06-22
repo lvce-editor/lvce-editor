6aa274aa606217ec1983a6cf6b073064ed63a529
Le Vivilet <levivlet@pm.me>
2025-06-22 20:31:30 +0200

    feature: remove unused connection close header

--- packages/static-server/src/parts/GetHeadersDefault/GetHeadersDefault.js
+++ packages/static-server/src/parts/GetHeadersDefault/GetHeadersDefault.js
@@ -1,11 +1,9 @@
-import * as Connection from '../Connection/Connection.js'
 import * as CrossOriginResourcePolicy from '../CrossOriginResourcePolicy/CrossOriginResourcePolicy.js'
 import * as HttpHeader from '../HttpHeader/HttpHeader.js'
 
 export const getHeadersDefault = (mime, etag, defaultCachingHeader) => {
   return {
     [HttpHeader.CacheControl]: defaultCachingHeader,
-    [HttpHeader.Connection]: Connection.Close,
     [HttpHeader.ContentType]: mime,
     [HttpHeader.CrossOriginResourcePolicy]: CrossOriginResourcePolicy.value,
     [HttpHeader.Etag]: etag,
