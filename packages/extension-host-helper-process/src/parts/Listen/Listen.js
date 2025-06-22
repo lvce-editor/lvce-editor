aa9e6164f5a8e6c59be304148e1dd727216fed6a
Le Vivilet <levivlet@pm.me>
2025-06-22 20:05:13 +0200

    fix: loadFile not working correcly

--- packages/extension-host-helper-process/src/parts/Listen/Listen.js
+++ packages/extension-host-helper-process/src/parts/Listen/Listen.js
@@ -1,10 +1,12 @@
 import * as CommandMap from '../CommandMap/CommandMap.js'
 import * as IpcChild from '../IpcChild/IpcChild.js'
 import * as IpcChildType from '../IpcChildType/IpcChildType.js'
+import * as CommandMapRef from '../CommandMapRef/CommandMapRef.js'
 
 export const listen = async (argv) => {
+  Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
   await IpcChild.listen({
     method: IpcChildType.Auto(argv),
-    commandMap: CommandMap.commandMap,
+    commandMap: CommandMapRef.commandMapRef,
   })
 }
