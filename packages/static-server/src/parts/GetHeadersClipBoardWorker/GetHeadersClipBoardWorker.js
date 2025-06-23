8abb1d4ade91594ab3c7211840e9a7d4a9601714
Le Vivilet <levivlet@pm.me>
2025-06-23 14:45:01 +0200

    feature: add clipboard worker headers

---
+++ packages/static-server/src/parts/GetHeadersClipBoardWorker/GetHeadersClipBoardWorker.js
@@ -0,0 +1,6 @@
+import * as ContentSecurityPolicyClipBoardWorker from '../ContentSecurityPolicyClipBoardWorker/ContentSecurityPolicyClipBoardWorker.js'
+import * as GetHeadersWorker from '../GetHeadersWorker/GetHeadersWorker.js'
+
+export const getHeadersAboutWorker = (mime, etag, defaultCachingHeader) => {
+  return GetHeadersWorker.getHeadersWorker(mime, etag, defaultCachingHeader, ContentSecurityPolicyClipBoardWorker.value)
+}
