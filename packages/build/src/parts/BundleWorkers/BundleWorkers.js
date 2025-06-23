bac396d6ac0be6a75940d71bcf54096111a6bf5c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-23 14:54:44 +0200

    feature: bundle clipboard worker (#9025)

--- packages/build/src/parts/BundleWorkers/BundleWorkers.js
+++ packages/build/src/parts/BundleWorkers/BundleWorkers.js
@@ -127,6 +127,10 @@ export const bundleWorkers = async ({ commitHash, platform, assetDir, version, d
     from: 'packages/renderer-worker/node_modules/@lvce-editor/rename-worker/dist/renameWorkerMain.js',
     to: Path.join(`${toRoot}/packages/rename-worker`, 'dist', 'renameWorkerMain.js'),
   })
+  await Copy.copyFile({
+    from: 'packages/renderer-worker/node_modules/@lvce-editor/clipboard-worker/dist/clipBoardWorkerMain.js',
+    to: Path.join(`${toRoot}/packages/clipboard-worker`, 'dist', 'clipBoardWorkerMain.js'),
+  })
   const sourceControlWorkerCachePath = await BundleSourceControlWorkerCached.bundleSourceControlWorkerCached({
     assetDir,
     commitHash,
