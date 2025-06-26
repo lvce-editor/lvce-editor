eefe4adc2bd3b7c1847a0278e17cb70e6b9e6efb
Le Vivilet <levivlet@pm.me>
2025-06-26 18:44:45 +0200

    feature: use clipboard worker to read and write files to clipboard

--- packages/renderer-worker/src/parts/ClipBoard/ClipBoard.js
+++ packages/renderer-worker/src/parts/ClipBoard/ClipBoard.js
@@ -14,25 +14,11 @@ export const writeText = async (text) => {
 }
 
 export const writeNativeFiles = async (type, files) => {
-  try {
-    if (Platform.platform === PlatformType.Web) {
-      throw new Error('not supported')
-    }
-    await SharedProcess.invoke(/* command */ 'ClipBoard.writeFiles', /* type */ type, /* files */ files)
-  } catch (error) {
-    throw new VError(error, 'Failed to write files to native clipboard')
-  }
+  return await ClipBoardWorker.invoke('ClipBoard.writeNativeFiles', type, files)
 }
 
 export const readNativeFiles = async () => {
-  try {
-    if (Platform.platform === PlatformType.Web) {
-      throw new Error('not supported')
-    }
-    return await SharedProcess.invoke(/* command */ 'ClipBoard.readFiles')
-  } catch (error) {
-    throw new VError(error, 'Failed to read files from native clipboard')
-  }
+  return await ClipBoardWorker.invoke('ClipBoard.readNativeFiles')
 }
 
 export const writeImage = async (blob) => {
