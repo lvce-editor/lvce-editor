e769575cca7a35646ba3a4120f2ddc379731cc3f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-05 21:08:16 +0200

    feature: remove unused code (#9158)

--- packages/renderer-worker/src/parts/ViewletReferences/ViewletReferences.js
+++ packages/renderer-worker/src/parts/ViewletReferences/ViewletReferences.js
@@ -1,17 +1,5 @@
-import * as GetActiveEditor from '../GetActiveEditor/GetActiveEditor.js'
-import * as GetReferencesWithPreview from '../GetReferencesWithPreview/GetReferencesWithPreview.js'
-import * as References from '../References/References.js'
 import * as ViewletLocations from '../ViewletLocations/ViewletLocations.js'
 import * as ViewletStates from '../ViewletStates/ViewletStates.js'
-
-const getReferences = async () => {
-  const editor = GetActiveEditor.getActiveEditor()
-  return GetReferencesWithPreview.getReferencesWithPreview(editor)
-}
-
-const getFileReferences = async (id, languageId) => {
-  return References.getFileReferences(id, languageId)
-}
 
 export const create = ViewletLocations.create
 
@@ -20,16 +8,8 @@ export const saveState = ViewletLocations.saveState
 // TODO speed up this function by 130% by not running activation event (onReferences) again and again
 // e.g. (21ms activation event, 11ms getReferences) => (11ms getReferences)
 export const loadContent = async (state) => {
-  const editor = GetActiveEditor.getActiveEditor()
-  const fn = state.args
-    ? () => {
-        // TODO find out language from somewhere else
-        return getFileReferences(editor.uid, editor.languageId)
-      }
-    : getReferences
-
   // @ts-ignore
-  return ViewletLocations.loadContent(state, fn)
+  return ViewletLocations.loadContent(state)
 }
 
 // TODO side effect is not good here, find a way to call ViewletLocations
