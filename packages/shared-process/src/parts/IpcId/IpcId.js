bc7b4953d5adb750eb60048913b64570340015ae
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-08 00:08:35 +0200

    feature: update ipc ids (#9188)

--- packages/shared-process/src/parts/IpcId/IpcId.js
+++ packages/shared-process/src/parts/IpcId/IpcId.js
@@ -1,15 +1,17 @@
-export const EmbedsProcess = 2
-export const EmbedsWorker = 77
+import { RpcId } from '@lvce-editor/rpc-registry'
+
+export const EmbedsProcess = RpcId.EmbedsProcess
+export const EmbedsWorker = RpcId.EmbedsWorker
 export const ExtensionHostHelperProcess = 3
-export const MainProcess = -5
+export const MainProcess = RpcId.MainProcess
 export const ProcessExplorer = 11
-export const SearchProcess = 13
+export const SearchProcess = RpcId.SearchProcess
 export const SharedProcess = 1
 export const TerminalProcess = 7
 export const Unknown = 0
 export const ProcessExplorerRenderer = 33
-export const ExtensionHostWorker = 218
-export const FileSystemProcess = 219
+export const ExtensionHostWorker = RpcId.ExtensionHostWorker
+export const FileSystemProcess = RpcId.FileSystemProcess
 export const FileWatcherProcess = 220
 export const TypescriptCompileProcess = 221
-export const ClipBoardProcess = 91222
+export const ClipBoardProcess = RpcId.ClipBoardProcess
