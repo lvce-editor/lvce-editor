007be4b92bed2bec5ffc18366cf7483789753464
Le Vivilet <levivlet@pm.me>
2025-07-07 18:47:20 +0200

    feature: initialize file system process

--- packages/shared-process/src/parts/LaunchFileSystemProcess/LaunchFileSystemProcess.js
+++ packages/shared-process/src/parts/LaunchFileSystemProcess/LaunchFileSystemProcess.js
@@ -2,6 +2,7 @@ import * as FileSystemProcessPath from '../FileSystemProcessPath/FileSystemProce
 import * as IpcId from '../IpcId/IpcId.js'
 import * as IsElectron from '../IsElectron/IsElectron.js'
 import * as LaunchProcess from '../LaunchProcess/LaunchProcess.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 
 export const launchFileSystemProcess = async () => {
   const ipc = await LaunchProcess.launchProcess({
@@ -11,5 +12,6 @@ export const launchFileSystemProcess = async () => {
     isElectron: IsElectron.isElectron,
     settingName: 'develop.fileSystemProcessPath',
   })
+  await JsonRpc.invoke(ipc, 'Initialize.initialize')
   return ipc
 }
