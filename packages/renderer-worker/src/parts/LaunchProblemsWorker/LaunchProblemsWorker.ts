38b422a3f17af29a4f41244715a9a67fe6b2d9e1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-16 15:44:58 +0200

    feature: make problems worker url configurable (#9249)
    * feature: make problems worker url configurable
    
    * update name

--- packages/renderer-worker/src/parts/LaunchProblemsWorker/LaunchProblemsWorker.ts
+++ packages/renderer-worker/src/parts/LaunchProblemsWorker/LaunchProblemsWorker.ts
@@ -1,14 +1,31 @@
+import * as ProblemsWorkerUrl from '../ProblemsWorkerUrl/ProblemsWorkerUrl.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
-import * as ProblemsWorkerUrl from '../ProblemsWorkerUrl/ProblemsWorkerUrl.js'
+import * as IsProduction from '../IsProduction/IsProduction.js'
+import * as Preferences from '../Preferences/Preferences.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+
+const getConfiguredWorkerUrl = () => {
+  let configuredWorkerUrl = Preferences.get('develop.problemsWorkerPath') || ''
+  if (configuredWorkerUrl) {
+    configuredWorkerUrl = '/remote' + configuredWorkerUrl
+  }
+  configuredWorkerUrl = configuredWorkerUrl || ProblemsWorkerUrl.problemsViewWorkerUrl
+  if (IsProduction.isProduction) {
+    configuredWorkerUrl = ProblemsWorkerUrl.problemsViewWorkerUrl
+  }
+  return configuredWorkerUrl
+}
 
 export const launchProblemsWorker = async () => {
+  const name = 'Problems Worker'
   const ipc = await IpcParent.create({
     method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
-    url: ProblemsWorkerUrl.problemsViewWorkerUrl,
-    name: 'Problems Worker',
+    name,
+    url: getConfiguredWorkerUrl(),
   })
   HandleIpc.handleIpc(ipc)
+  await JsonRpc.invoke(ipc, 'Problems.initialize')
   return ipc
 }
