b5f33eb60acca4ef4fd5964cb3d4ad1429a83741
Le Vivilet <levivlet@pm.me>
2025-07-16 16:01:59 +0200

    feature: use problems worker for rendering problems

--- packages/renderer-worker/src/parts/ViewletProblems/ViewletProblemsRender.js
+++ packages/renderer-worker/src/parts/ViewletProblems/ViewletProblemsRender.js
@@ -1,38 +1,23 @@
-import * as GetProblemsVirtualDom from '../GetProblemsVirtualDom/GetProblemsVirtualDom.js'
-import * as GetVisibleProblems from '../GetVisibleProblems/GetVisibleProblems.js'
-import * as RenderMethod from '../RenderMethod/RenderMethod.js'
+import * as AdjustCommands from '../AdjustCommands/AdjustCommands.js'
+import * as ProblemsWorker from '../ProblemsWorker/ProblemsWorker.ts'
 
 export const hasFunctionalRender = true
 
 export const hasFunctionalRootRender = true
 
-const renderProblems = {
+export const hasFunctionalEvents = true
+
+const renderItems = {
   isEqual(oldState, newState) {
-    return (
-      oldState.problems === newState.problems &&
-      oldState.focusedIndex === newState.focusedIndex &&
-      oldState.filterValue === newState.filterValue &&
-      oldState.viewMode === newState.viewMode &&
-      oldState.collapsedUris === newState.collapsedUris &&
-      oldState.width === newState.width
-    )
+    return newState.commands.length === 0
   },
-  apply(oldState, newState) {
-    const visible = GetVisibleProblems.getVisibleProblems(newState.problems, newState.collapsedUris, newState.focusedIndex, newState.filterValue)
-    const isSmall = newState.width <= newState.smallWidthBreakPoint
-    const dom = GetProblemsVirtualDom.getProblemsVirtualDom(newState.viewMode, visible, newState.filterValue, isSmall)
-    return ['Viewlet.setDom2', dom]
-  },
+  apply: AdjustCommands.apply,
+  multiple: true,
 }
 
-// TODO set focusoutline classname in renderer worker
-const renderFocusedIndex = {
-  isEqual(oldState, newState) {
-    return oldState.focusedIndex === newState.focusedIndex
-  },
-  apply(oldState, newState) {
-    return [/* method */ RenderMethod.SetFocusedIndex, /* focusedIndex */ newState.focusedIndex]
-  },
-}
+export const render = [renderItems]
 
-export const render = [renderProblems, renderFocusedIndex]
+export const renderEventListeners = async () => {
+  const listeners = await ProblemsWorker.invoke('Problems.renderEventListeners')
+  return listeners
+}
