80a86112481119725f0f96b8caf90c2473770176
Le Vivilet <levivlet@pm.me>
2025-07-17 14:27:09 +0200

    tsc

--- packages/renderer-worker/src/parts/MenuEntriesProblems/MenuEntriesProblems.js
+++ packages/renderer-worker/src/parts/MenuEntriesProblems/MenuEntriesProblems.js
@@ -1,22 +1,21 @@
-import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.js'
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.js'
-import * as ViewletProblemsStrings from '../ViewletProblems/ViewletProblemsStrings.js'
 
 export const id = MenuEntryId.Problems
 
 export const getMenuEntries = () => {
+  // TODO
   return [
-    {
-      id: 'copy',
-      label: ViewletProblemsStrings.copy(),
-      flags: MenuItemFlags.None,
-      command: 'Problems.copy',
-    },
-    {
-      id: 'copyMessage',
-      label: ViewletProblemsStrings.copyMessage(),
-      flags: MenuItemFlags.None,
-      command: 'Problems.copyMessage',
-    },
+    // {
+    //   id: 'copy',
+    //   label: ViewletProblemsStrings.copy(),
+    //   flags: MenuItemFlags.None,
+    //   command: 'Problems.copy',
+    // },
+    // {
+    //   id: 'copyMessage',
+    //   label: ViewletProblemsStrings.copyMessage(),
+    //   flags: MenuItemFlags.None,
+    //   command: 'Problems.copyMessage',
+    // },
   ]
 }
