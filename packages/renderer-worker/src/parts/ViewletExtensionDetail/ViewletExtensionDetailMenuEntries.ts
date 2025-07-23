dc8acf3893fe564e1c8c6bb1b60aca40d3eb29b7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-23 22:16:10 +0200

    feature: query extension detail view menus from worker (#9306)

--- packages/renderer-worker/src/parts/ViewletExtensionDetail/ViewletExtensionDetailMenuEntries.ts
+++ packages/renderer-worker/src/parts/ViewletExtensionDetail/ViewletExtensionDetailMenuEntries.ts
@@ -1,3 +1,20 @@
-import * as MenuEntriesExtensionDetailReadme from '../MenuEntriesExtensionDetailReadme/MenuEntriesExtensionDetailReadme.js'
+import * as ExtensionDetailViewWorker from '../ExtensionDetailViewWorker/ExtensionDetailViewWorker.js'
 
-export const menus = [MenuEntriesExtensionDetailReadme]
+export const menus = []
+
+export const getMenus = async () => {
+  try {
+    const modules = await ExtensionDetailViewWorker.invoke('ExtensionDetail.getMenus')
+    const adjusted = modules.map((module) => {
+      return {
+        ...module,
+        async getMenuEntries() {
+          return module.entries
+        },
+      }
+    })
+    return adjusted
+  } catch {
+    return []
+  }
+}
