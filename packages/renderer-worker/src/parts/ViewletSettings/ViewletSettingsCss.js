999dce8b4d6aba63fbe58785ccee70a05609783f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-27 10:35:20 +0200

    feature: add settings ui css (#9339)

--- packages/renderer-worker/src/parts/ViewletSettings/ViewletSettingsCss.js
+++ packages/renderer-worker/src/parts/ViewletSettings/ViewletSettingsCss.js
@@ -1,12 +1,8 @@
 export const Css = [
-  '/css/parts/Badge.css',
   '/css/parts/Filter.css',
   '/css/parts/Highlight.css',
   '/css/parts/InputBox.css',
   '/css/parts/Label.css',
-  '/css/parts/Message.css',
-  '/css/parts/MessageAction.css',
-  '/css/parts/ProblemsIcon.css',
   '/css/parts/SearchField.css',
-  '/css/parts/ViewletProblems.css',
+  '/css/parts/ViewletSettings.css',
 ]
