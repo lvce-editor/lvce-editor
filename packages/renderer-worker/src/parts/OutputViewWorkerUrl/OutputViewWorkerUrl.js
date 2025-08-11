2161cc41dc7e1b65862a3115600343a7cbf03026
Le Vivilet <levivlet@pm.me>
2025-08-11 16:47:07 +0200

    feature: add function to launch output view worker

---
+++ packages/renderer-worker/src/parts/OutputViewWorkerUrl/OutputViewWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const outputViewWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/output-view/outputViewWorkerMain.js`
