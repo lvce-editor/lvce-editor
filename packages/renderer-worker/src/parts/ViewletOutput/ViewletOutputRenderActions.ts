ad3fa94b9eaa62209e9a30afee3eaeef19967330
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-12 23:50:33 +0200

    feature: render output actions (#9415)

--- packages/renderer-worker/src/parts/ViewletOutput/ViewletOutputRenderActions.ts
+++ packages/renderer-worker/src/parts/ViewletOutput/ViewletOutputRenderActions.ts
@@ -1,13 +1,9 @@
-import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.js'
-import * as ViewletOutputActions from './ViewletOutputActions.ts'
-
 export const renderActions = {
   isEqual(oldState, newState) {
-    return oldState === newState
+    return false
   },
   apply(oldState, newState) {
-    const actions = ViewletOutputActions.getActions(newState)
-    const dom = GetActionsVirtualDom.getActionsVirtualDom(actions)
+    const dom = newState.actionsDom
     return dom
   },
 }
