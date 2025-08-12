07405c4914b2cc914a8c44f8450752a842417fbb
Le Vivilet <levivlet@pm.me>
2025-08-12 23:25:02 +0200

    update url

--- packages/renderer-worker/src/parts/OutputViewWorkerUrl/OutputViewWorkerUrl.js
+++ packages/renderer-worker/src/parts/OutputViewWorkerUrl/OutputViewWorkerUrl.js
@@ -1,3 +1,3 @@
 import * as AssetDir from '../AssetDir/AssetDir.js'
 
-export const outputViewWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/output-view/outputViewWorkerMain.js`
+export const outputViewWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/output-view/dist/outputViewWorkerMain.js`
