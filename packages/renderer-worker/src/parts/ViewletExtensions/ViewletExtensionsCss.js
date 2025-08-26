c4413ca8fbf55c85362ead903638122c15c158bf
Le Vivilet <levivlet@pm.me>
2025-08-26 08:46:03 +0200

    feature: improve table css

--- packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsCss.js
+++ packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsCss.js
@@ -3,6 +3,7 @@ export const Css = [
   '/css/parts/ScrollBar.css',
   '/css/parts/ScrollBarThumb.css',
   '/css/parts/SearchField.css',
+  '/css/parts/Table.css',
   '/css/parts/ViewletExtensions.css',
   '/css/parts/ViewletList.css',
 ]
