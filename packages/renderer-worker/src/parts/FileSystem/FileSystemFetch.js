5d15bb0dd8378261701f8e9feb3a8ad729f33148
Le Vivilet <levivlet@pm.me>
2025-08-30 19:23:50 +0200

    fix json

--- packages/renderer-worker/src/parts/FileSystem/FileSystemFetch.js
+++ packages/renderer-worker/src/parts/FileSystem/FileSystemFetch.js
@@ -20,6 +20,9 @@ export const readFile = async (uri) => {
 }
 
 export const readJson = async (uri) => {
+  if (uri.startsWith('localhost')) {
+    return FileSystemWorker.invoke('FileSystem.readJson', `http://${uri}`)
+  }
   if (uri.startsWith('http://') || uri.startsWith('https://')) {
     return FileSystemWorker.invoke('FileSystem.readJson', uri)
   }
