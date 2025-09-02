a4049ca911df51460b8efa69df01eafab8fff527
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-02 15:09:00 +0200

    feature: add mock confirmprompt (#9572)
    * feature: add mock confirmprompt
    
    * execute mock with arguments
    
    * add args

--- packages/renderer-worker/src/parts/ConfirmPrompt/ConfirmPrompt.js
+++ packages/renderer-worker/src/parts/ConfirmPrompt/ConfirmPrompt.js
@@ -3,11 +3,24 @@ import * as ConfirmPromptWeb from '../ConfirmPromptWeb/ConfirmPromptWeb.js'
 import * as Platform from '../Platform/Platform.js'
 import * as ConfirmPromptStrings from '../ConfirmPromptStrings/ConfirmPromptStrings.js'
 import * as PlatformType from '../PlatformType/PlatformType.js'
+import * as TestWorker from '../TestWorker/TestWorker.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
+
+let _mockId = 0
+
+const showMockConfirmPrompt = async (message, options) => {
+  const ipc = TestWorker.get()
+  const result = await JsonRpc.invoke(ipc, 'Test.executeMock', _mockId, message, options)
+  return result
+}
 
 export const prompt = async (
   message,
   { confirmMessage = ConfirmPromptStrings.ok(), title = '', cancelMessage = ConfirmPromptStrings.cancel() } = {},
 ) => {
+  if (_mockId) {
+    return showMockConfirmPrompt(message, { confirmMessage, title, cancelMessage })
+  }
   // TODO always ask shared process for prompts
   // when running in electron it uses native prompts
   // when running in node or web it uses browser prompts
@@ -15,4 +28,8 @@ export const prompt = async (
     return ConfirmPromptElectron.prompt(message, confirmMessage, title, cancelMessage)
   }
   return ConfirmPromptWeb.prompt(message, confirmMessage, title)
+}
+
+export const mock = (mockId) => {
+  _mockId = mockId
 }
