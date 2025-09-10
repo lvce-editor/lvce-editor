1fce3bee71b2224431b37aa68f530a820364ba28
Le Vivilet <levivilet@pm.me>
2025-09-10 22:22:21 +0200

    feature: load file icons from icon theme worker

--- packages/renderer-worker/src/parts/IconTheme/IconTheme.js
+++ packages/renderer-worker/src/parts/IconTheme/IconTheme.js
@@ -1,13 +1,17 @@
-import * as GetIconThemeJson from '../GetIconThemeJson/GetIconThemeJson.js'
+import { assetDir } from '../AssetDir/AssetDir.js'
+import { getAllExtensions } from '../ExtensionManagement/ExtensionManagement.js'
 import * as HandleIconThemeChange from '../HandleIconThemeChange/HandleIconThemeChange.js'
 import * as IconThemeState from '../IconThemeState/IconThemeState.js'
+import * as IconThemeWorker from '../IconThemeWorker/IconThemeWorker.js'
+import * as Platform from '../Platform/Platform.js'
 import * as Preferences from '../Preferences/Preferences.js'
 import { VError } from '../VError/VError.js'
 import * as Workspace from '../Workspace/Workspace.js'
 
 export const setIconTheme = async (iconThemeId) => {
   try {
-    const iconTheme = await GetIconThemeJson.getIconThemeJson(iconThemeId)
+    const extensions = await getAllExtensions()
+    const iconTheme = await IconThemeWorker.invoke('IconTheme.getIconThemeJson', extensions, iconThemeId, assetDir, Platform.platform)
     if (!iconTheme) {
       return
     }
@@ -23,6 +27,7 @@ export const setIconTheme = async (iconThemeId) => {
 }
 
 export const hydrate = async () => {
+  // TODO do this all in worker
   const iconThemeId = Preferences.get('icon-theme') || 'vscode-icons'
   await setIconTheme(iconThemeId)
 }
