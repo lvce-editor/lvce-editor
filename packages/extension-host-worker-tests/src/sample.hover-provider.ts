b2250aefe63143ad1793fc8ecd1424002d5c8166
Le Vivilet <levivilet@pm.me>
2025-09-12 21:48:12 +0200

    hover

--- packages/extension-host-worker-tests/src/sample.hover-provider.ts
+++ packages/extension-host-worker-tests/src/sample.hover-provider.ts
@@ -1,5 +1,7 @@
 export const name = 'sample.hover-provider'
 
+export const skip = 1
+
 export const test = async ({ FileSystem, Workspace, Extension, Main, Editor, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
