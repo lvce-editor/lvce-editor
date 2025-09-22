4f8ebf89c1f27b50b4b11714e67384cd359599fe
Le Vivilet <levivilet@pm.me>
2025-09-22 23:58:13 +0200

    feature: simplify test function (#9747)

--- packages/renderer-worker/src/parts/ExecuteCurrentTest/ExecuteCurrentTest.js
+++ packages/renderer-worker/src/parts/ExecuteCurrentTest/ExecuteCurrentTest.js
@@ -1,7 +1,7 @@
-import * as Command from '../Command/Command.js'
 import * as GetTestFilePath from '../GetTestFilePath/GetTestFilePath.js'
+import * as Test from '../Test/Test.js'
 
 export const executeCurrentTest = async (initData) => {
   const jsPath = await GetTestFilePath.getTestFilePath(initData.Location.href)
-  await Command.execute('Test.execute', jsPath)
+  await Test.execute(jsPath)
 }
