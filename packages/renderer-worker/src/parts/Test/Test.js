fbf29d0d50376ef59515bd0f9d5b07f2aaf4554a
Le Vivilet <levivilet@pm.me>
2025-09-22 23:48:03 +0200

    feature: pass platform to executeTest

--- packages/renderer-worker/src/parts/Test/Test.js
+++ packages/renderer-worker/src/parts/Test/Test.js
@@ -1,7 +1,8 @@
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 import * as LaunchTestWorker from '../LaunchTestWorker/LaunchTestWorker.ts'
+import * as Platform from '../Platform/Platform.ts'
 
 export const execute = async (href) => {
   const ipc = await LaunchTestWorker.launchTestWorker()
-  await JsonRpc.invoke(ipc, 'Test.execute', href)
+  await JsonRpc.invoke(ipc, 'Test.execute', href, Platform.platform)
 }
