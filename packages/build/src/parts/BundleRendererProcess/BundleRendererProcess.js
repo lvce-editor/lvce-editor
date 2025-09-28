21c3551c2f8c4cea6b3b411f158e58055eb60e49
Le Vivilet <levivilet@pm.me>
2025-09-28 18:40:41 +0200

    build

--- packages/build/src/parts/BundleRendererProcess/BundleRendererProcess.js
+++ packages/build/src/parts/BundleRendererProcess/BundleRendererProcess.js
@@ -34,12 +34,12 @@ export const bundleRendererProcess = async ({ cachePath, commitHash, platform, a
     })
     await Replace.replace({
       path: `${cachePath}/dist/rendererProcessMain.js`,
-      occurrence: `const syntaxHighlightingWorkerUrl = \`\${assetDir}/packages/renderer-worker/node_modules/@lvce-editor/syntax-highlighting-worker/dist/syntaxHighlightingWorkerMain.js\`;`,
+      occurrence: `const syntaxHighlightingWorkerUrl = getConfiguredSyntaxHighlightingWorkerUrl() || \`\${assetDir}/packages/renderer-worker/node_modules/@lvce-editor/syntax-highlighting-worker/dist/syntaxHighlightingWorkerMain.js\`;`,
       replacement: `const syntaxHighlightingWorkerUrl = \`\${assetDir}/packages/syntax-highlighting-worker/dist/syntaxHighlightingWorkerMain.js\`;`,
     })
     await Replace.replace({
       path: `${cachePath}/dist/rendererProcessMain.js`,
-      occurrence: `const extensionHostWorkerUrl = \`\${assetDir}/packages/renderer-worker/node_modules/@lvce-editor/extension-host-worker/dist/extensionHostWorkerMain.js\`;`,
+      occurrence: `const extensionHostWorkerUrl = getConfiguredExtensionHostWorkerUrl() || \`\${assetDir}/packages/renderer-worker/node_modules/@lvce-editor/extension-host-worker/dist/extensionHostWorkerMain.js\`;`,
       replacement: 'const extensionHostWorkerUrl = `${assetDir}/packages/extension-host-worker/dist/extensionHostWorkerMain.js`;',
     })
     await Replace.replace({
