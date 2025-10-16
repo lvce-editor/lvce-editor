be81a0c28ed4d9410da25ef62e9b3c29c6e17125
Le Vivilet <levivilet@pm.me>
2025-10-16 11:24:25 +0200

    update css

--- packages/renderer-worker/src/parts/ViewletTitleBar/ViewletTitleBarCss.ts
+++ packages/renderer-worker/src/parts/ViewletTitleBar/ViewletTitleBarCss.ts
@@ -1,3 +1,7 @@
-export const Css = ['/css/parts/ViewletTitleBar.css']
+export const Css = ['/css/parts/ViewletTitleBar.css',
+  '/css/parts/ViewletTitleBarMenuBar.css', '/css/parts/TitleBarEntry.css',
+  '/css/parts/ViewletTitleBarTitle.css',
+  '/css/parts/ViewletTitleBarButtons.css', '/css/parts/MaskIcon.css'
+]
 
 export const Variables = ['TitleBarForeground', 'TitleBarForegroundActive', 'TitleBarActiveBackground', 'TitleBarBackground']
