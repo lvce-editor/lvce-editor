f1e2cd65ec9a18a6bbde522989684110cb85d353
Le Vivilet <levivilet@pm.me>
2025-10-26 15:22:11 +0100

    feature: add function to launch activity bar worker

---
+++ packages/renderer-worker/src/parts/ActivityBarWorkerUrl/ActivityBarWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const activityBarWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/activity-bar-worker/dist/activityBarWorkerMain.js`
