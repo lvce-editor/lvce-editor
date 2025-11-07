deb55426b70c2d8e716e06a13d6d4bfffcbfaf05
Le Vivilet <levivilet@pm.me>
2025-11-07 20:20:25 +0100

    feature: remove unused debug code (#10030)

--- packages/shared-process/src/parts/HandleWindowAllClosed/HandleWindowAllClosed.js
+++ packages/shared-process/src/parts/HandleWindowAllClosed/HandleWindowAllClosed.js
@@ -1,11 +1,8 @@
-import * as Debug from '../Debug/Debug.js'
 import * as ParentIpc from '../MainProcess/MainProcess.js'
 import * as Platform from '../Platform/Platform.js'
 
 export const handleWindowAllClosed = async () => {
-  Debug.debug('[info] all windows closed')
   if (!Platform.isMacOs) {
-    Debug.debug('[info] quitting')
     await ParentIpc.invoke('Exit.exit')
   }
 }
