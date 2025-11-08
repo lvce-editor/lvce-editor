64608d8c10534770c55a3f1a9e9ecea63fd16a58
Le Vivilet <levivilet@pm.me>
2025-11-09 00:32:00 +0100

    feature: add update worker launch

---
+++ packages/renderer-worker/src/parts/UpdateWorkerUrl/UpdateWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const updateWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/update-worker/dist/updateWorkerMain.js`
