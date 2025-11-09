f23f5247698ffcce124b1100033da1ba97a7d0c6
Le Vivilet <levivilet@pm.me>
2025-11-09 10:50:01 +0100

    feature: use worker for auto update check

--- packages/renderer-worker/src/parts/AutoUpdater/AutoUpdater.js
+++ packages/renderer-worker/src/parts/AutoUpdater/AutoUpdater.js
@@ -1,41 +1,6 @@
-import * as AutoUpdaterModule from '../AutoUpdaterModule/AutoUpdaterModule.js'
-import * as AutoUpdaterStrings from '../AutoUpdaterStrings/AutoUpdaterStrings.js'
-import * as Command from '../Command/Command.js'
-import * as GetAutoUpdateType from '../GetAutoUpdateType/GetAutoUpdateType.js'
-import * as GetLatestVersion from '../GetLatestVersion/GetLatestVersion.js'
-
-const getShouldUpdate = async (updateSetting, version) => {
-  if (updateSetting && updateSetting !== 'none') {
-    return true
-  }
-  const message = AutoUpdaterStrings.promptMessage(version)
-  const shouldUpdate = await Command.execute('ConfirmPrompt.prompt', message)
-  return shouldUpdate
-}
+import * as UpdateWorker from '../UpdateWorker/UpdateWorker.js'
 
 export const checkForUpdates = async (updateSetting) => {
-  const info = await GetLatestVersion.getLatestVersion()
-  if (!info || !info.version) {
-    return
-  }
-  const type = await GetAutoUpdateType.getAutoUpdateType()
-  const shouldUpdate = await getShouldUpdate(updateSetting, info.version)
-  if (!shouldUpdate) {
-    return
-  }
-  const module = await AutoUpdaterModule.getModule(type)
-  if (!module) {
-    return
-  }
-  await Command.execute('Layout.setUpdateState', {
-    state: 'downloading',
-    progress: 0,
-  })
-  const downloadPath = await module.downloadUpdate(info.version)
-  const messageRestart = AutoUpdaterStrings.promptRestart()
-  const shouldRestart = await Command.execute('ConfirmPrompt.prompt', messageRestart)
-  if (!shouldRestart) {
-    return
-  }
-  await module.installAndRestart(downloadPath)
+  const repository = 'lvce-editor/lvce-editor'
+  await UpdateWorker.invoke('Update.checkForUpdates', updateSetting, repository)
 }
