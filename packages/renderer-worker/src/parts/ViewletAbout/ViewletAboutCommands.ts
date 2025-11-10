7defc394b7d71b608ab3b6c439d74ccb6e0bfe82
Le Vivilet <levivilet@pm.me>
2025-11-10 10:02:42 +0100

    fix: about not showing after second time (#10084)

--- packages/renderer-worker/src/parts/ViewletAbout/ViewletAboutCommands.ts
+++ packages/renderer-worker/src/parts/ViewletAbout/ViewletAboutCommands.ts
@@ -6,6 +6,9 @@ export const Commands = {}
 export const getCommands = async () => {
   const commands = await AboutViewWorker.invoke('About.getCommandIds')
   for (const command of commands) {
+    if (command === 'About.showAbout') {
+      continue
+    }
     Commands[command] = WrapAboutCommand.wrapAboutCommand(command)
   }
   return Commands
