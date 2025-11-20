f8101c98c0bb103d9458cb90a0cb35a0c456cfe2
Le Vivilet <levivilet@pm.me>
2025-11-20 13:17:20 +0100

    feature: improve error message when moving item to trash fails

--- packages/shared-process/src/parts/TrashNode/TrashNode.js
+++ packages/shared-process/src/parts/TrashNode/TrashNode.js
@@ -1,5 +1,10 @@
+import { VError } from '@lvce-editor/verror'
 import _trash from 'trash'
 
 export const trash = async (path) => {
-  await _trash(path)
+  try {
+    await _trash(path)
+  } catch (error) {
+    throw new VError(error, 'Failed to move item to trash')
+  }
 }
