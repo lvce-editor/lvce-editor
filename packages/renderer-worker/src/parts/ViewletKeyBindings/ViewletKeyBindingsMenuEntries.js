93838c874c71d3212ad6baaa93b7d648dbe3524c
Le Vivilet <levivilet@pm.me>
2025-11-21 00:55:59 +0100

    feature: query keybindings menus from worker

--- packages/renderer-worker/src/parts/ViewletKeyBindings/ViewletKeyBindingsMenuEntries.js
+++ packages/renderer-worker/src/parts/ViewletKeyBindings/ViewletKeyBindingsMenuEntries.js
@@ -1,3 +1,22 @@
-import * as MenuEntriesKeyBindingsTable from '../MenuEntriesKeyBindingsTable/MenuEntriesKeyBindingsTable.js'
+import * as KeyBindingsViewWorker from '../KeyBindingsViewWorker/KeyBindingsViewWorker.js'
 
-export const menus = [MenuEntriesKeyBindingsTable]
+export const menus = []
+
+export const getMenus = async () => {
+  try {
+    const ids = await KeyBindingsViewWorker.invoke('KeyBindings.getMenuEntryIds')
+    const adjusted = ids.map((id) => {
+      return {
+        id,
+        async getMenuEntries(...args) {
+          // TODO pass menu id also
+          const entries = await KeyBindingsViewWorker.invoke('KeyBindings.getMenuEntries', ...args)
+          return entries
+        },
+      }
+    })
+    return adjusted
+  } catch {
+    return []
+  }
+}
