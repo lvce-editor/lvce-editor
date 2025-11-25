de107d9517be024babf27c54978dbdb128caba25
Le Vivilet <levivilet@pm.me>
2025-11-25 17:44:59 +0100

    feature: query source control menus from source control worker

--- packages/renderer-worker/src/parts/ViewletSourceControl/ViewletSourceControlMenuEntries.js
+++ packages/renderer-worker/src/parts/ViewletSourceControl/ViewletSourceControlMenuEntries.js
@@ -1,3 +1,22 @@
-import * as MenuEntriesSourceControl from '../MenuEntriesSourceControl/MenuEntriesSourceControl.js'
+import * as SourceControlWorker from '../SourceControlWorker/SourceControlWorker.js'
 
-export const menus = [MenuEntriesSourceControl]
+export const menus = []
+
+export const getMenus = async () => {
+  try {
+    const ids = await SourceControlWorker.invoke('SourceControl.getMenuIds')
+    const adjusted = ids.map((id) => {
+      return {
+        id,
+        async getMenuEntries(...args) {
+          // TODO pass menu id also
+          const entries = await SourceControlWorker.invoke('SourceControl.getMenuEntries2', ...args)
+          return entries
+        },
+      }
+    })
+    return adjusted
+  } catch {
+    return []
+  }
+}
