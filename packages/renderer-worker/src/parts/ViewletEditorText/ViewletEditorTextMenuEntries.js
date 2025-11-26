75c3490e678fb00da92cccf78982f6c64b5a8b2e
Le Vivilet <levivilet@pm.me>
2025-11-26 12:46:22 +0100

    feature: query editor menu entries from editor worker

--- packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextMenuEntries.js
+++ packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextMenuEntries.js
@@ -5,3 +5,26 @@ export const getQuickPickMenuEntries = async () => {
   const entries = await EditorWorker.invoke('Editor.getQuickPickMenuEntries')
   return entries
 }
+
+export const menus = []
+
+export const getMenus = async () => {
+  try {
+    // @ts-ignore
+    const ids = await EditorWorker.invoke('Editor.getMenuIds')
+    const adjusted = ids.map((id) => {
+      return {
+        id,
+        async getMenuEntries(...args) {
+          // TODO pass menu id also
+          // @ts-ignore
+          const entries = await EditorWorker.invoke('Editor.getMenuEntries2', ...args)
+          return entries
+        },
+      }
+    })
+    return adjusted
+  } catch {
+    return []
+  }
+}
