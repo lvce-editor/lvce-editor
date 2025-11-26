779f765a7172dcce701d28bbdd839bfd0c466875
Le Vivilet <levivilet@pm.me>
2025-11-26 12:29:41 +0100

    feature: query extension search menus from worker

--- packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsMenuEntries.js
+++ packages/renderer-worker/src/parts/ViewletExtensions/ViewletExtensionsMenuEntries.js
@@ -1,9 +1,22 @@
-import * as MenuEntryId from '../MenuEntryId/MenuEntryId.js'
-import * as MenuEntriesManageExtension from '../MenuEntriesManageExtension/MenuEntriesManageExtension.js'
+import * as ExtensionSearchViewWorker from '../ExtensionSearchViewWorker/ExtensionSearchViewWorker.js'
 
-export const menus = [
-  {
-    id: MenuEntryId.ManageExtension,
-    ...MenuEntriesManageExtension,
-  },
-]
+export const menus = []
+
+export const getMenus = async () => {
+  try {
+    const ids = await ExtensionSearchViewWorker.invoke('SearchExtensions.getMenuIds')
+    const adjusted = ids.map((id) => {
+      return {
+        id,
+        async getMenuEntries(...args) {
+          // TODO pass menu id also
+          const entries = await ExtensionSearchViewWorker.invoke('SearchExtensions.getMenuEntries2', ...args)
+          return entries
+        },
+      }
+    })
+    return adjusted
+  } catch {
+    return []
+  }
+}
