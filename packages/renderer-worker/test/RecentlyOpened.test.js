bd43de03ed0902f172a32e210dcb05cd99f119c4
Le Vivilet <levivilet@pm.me>
2025-12-03 12:50:35 +0100

    fix

--- packages/renderer-worker/test/RecentlyOpened.test.js
+++ packages/renderer-worker/test/RecentlyOpened.test.js
@@ -5,15 +5,20 @@ beforeEach(() => {
   jest.resetAllMocks()
 })
 
-jest.unstable_mockModule('../src/parts/FileSystem/FileSystem.js', () => {
+jest.unstable_mockModule('../src/parts/LocalStorage/LocalStorage.js', () => {
   return {
-    readFile: jest.fn(() => {
+    getJson: jest.fn(() => {
       throw new Error('not implemented')
     }),
-    readJson: jest.fn(() => {
+    getText: jest.fn(() => {
       throw new Error('not implemented')
     }),
-    writeFile: jest.fn(() => {
+  }
+})
+
+jest.unstable_mockModule('../src/parts/Command/Command.js', () => {
+  return {
+    execute: jest.fn(() => {
       throw new Error('not implemented')
     }),
   }
@@ -22,58 +27,69 @@ jest.unstable_mockModule('../src/parts/FileSystem/FileSystem.js', () => {
 jest.unstable_mockModule('../src/parts/Platform/Platform.js', () => {
   return {
     platform: PlatformType.Web,
-    getPlatform: jest.fn(() => {
+    getPlatform: () => {
       return PlatformType.Web
-    }),
+    },
     assetDir: '',
   }
 })
 
-const FileSystem = await import('../src/parts/FileSystem/FileSystem.js')
+jest.unstable_mockModule('../src/parts/PlatformPaths/PlatformPaths.js', () => {
+  return {
+    getRecentlyOpenedPath: jest.fn(() => {
+      return 'app://recently-opened.json'
+    }),
+  }
+})
+
+jest.unstable_mockModule('../src/parts/GetFileSystem/GetFileSystem.js', () => {
+  return {
+    getFileSystem: jest.fn(() => {
+      throw new Error('not implemented')
+    }),
+  }
+})
+
+const LocalStorage = await import('../src/parts/LocalStorage/LocalStorage.js')
+const Command = await import('../src/parts/Command/Command.js')
 const RecentlyOpened = await import('../src/parts/RecentlyOpened/RecentlyOpened.js')
 
 test('addToRecentlyOpened - already in list', async () => {
   // @ts-ignore
-  FileSystem.readJson.mockImplementation(() => {
+  LocalStorage.getJson.mockImplementation(() => {
     return ['/test/folder-1', '/test/folder-2', '/test/folder-3']
   })
   // @ts-ignore
-  FileSystem.writeFile.mockImplementation(() => {})
+  Command.execute.mockImplementation(() => {})
   await RecentlyOpened.addToRecentlyOpened('/test/folder-3')
-  expect(FileSystem.writeFile).toHaveBeenCalledWith(
-    'app://recently-opened.json',
-    `[
+  expect(Command.execute).toHaveBeenCalledWith('LocalStorage.setText', 'app://recently-opened.json', `[
   "/test/folder-3",
   "/test/folder-1",
   "/test/folder-2"
 ]
-`,
-  )
+`)
 })
 
 test('addToRecentlyOpened - already at front of list', async () => {
   // @ts-ignore
-  FileSystem.readJson.mockImplementation(() => {
+  LocalStorage.getJson.mockImplementation(() => {
     return ['/test/folder-3', '/test/folder-1', '/test/folder-2']
   })
   // @ts-ignore
-  FileSystem.writeFile.mockImplementation(() => {})
+  Command.execute.mockImplementation(() => {})
   await RecentlyOpened.addToRecentlyOpened('/test/folder-3')
   // TODO not necessary to write again, because it is already at front of list
-  expect(FileSystem.writeFile).toHaveBeenCalledWith(
-    'app://recently-opened.json',
-    `[
+  expect(Command.execute).toHaveBeenCalledWith('LocalStorage.setText', 'app://recently-opened.json', `[
   "/test/folder-3",
   "/test/folder-1",
   "/test/folder-2"
 ]
-`,
-  )
+`)
 })
 
 test('addToRecentlyOpened - error - recently opened path is of type array', async () => {
   // @ts-ignore
-  FileSystem.readJson.mockImplementation(() => {
+  LocalStorage.getJson.mockImplementation(() => {
     throw new Error('expected value to be of type string')
   })
   const spy = jest.spyOn(console, 'warn').mockImplementation(() => {})
@@ -84,17 +100,19 @@ test('addToRecentlyOpened - error - recently opened path is of type array', asyn
 
 test('addToRecentlyOpened - error - invalid json when reading recently opened', async () => {
   // @ts-ignore
-  FileSystem.readFile.mockImplementation(() => {
+  LocalStorage.getText.mockImplementation(() => {
     return JSON.stringify(['/test/folder-1', '/test/folder-2', '/test/folder-3']) + '##'
   })
   // @ts-ignore
-  FileSystem.writeFile.mockImplementation(() => {})
+  LocalStorage.getJson.mockImplementation(() => {
+    return undefined
+  })
+  // @ts-ignore
+  Command.execute.mockImplementation(() => {})
   await RecentlyOpened.addToRecentlyOpened('/test/folder-3')
-  expect(FileSystem.writeFile).toHaveBeenCalledWith(
-    'app://recently-opened.json',
-    `[
+  expect(Command.execute).toHaveBeenCalledWith('LocalStorage.setText', 'app://recently-opened.json', `[
   "/test/folder-3"
 ]
-`,
-  )
+`)
 })
+
