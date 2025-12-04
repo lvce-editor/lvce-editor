4576ce9b586dff04f3afe67f4a57eccbd6f85caf
Le Vivilet <levivilet@pm.me>
2025-12-04 21:16:24 +0100

    fix

--- packages/build/src/parts/BundleRendererWorker/BundleRendererWorker.js
+++ packages/build/src/parts/BundleRendererWorker/BundleRendererWorker.js
@@ -89,7 +89,7 @@ export const bundleRendererWorker = async ({ cachePath, platform, commitHash, as
       from: 'static/js',
       to: Path.join(cachePath, 'static', 'js'),
     })
-    for (const file of ['PrettyBytes', 'Blob', 'JsonRpc']) {
+    for (const file of ['PrettyBytes', 'JsonRpc']) {
       await Replace.replace({
         path: `${cachePath}/src/parts/${file}/${file}.js`,
         occurrence: `../../../../../static/`,
