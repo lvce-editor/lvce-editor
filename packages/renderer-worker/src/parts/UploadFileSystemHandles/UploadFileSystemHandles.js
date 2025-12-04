fb10175e33a44b32de849557b7e6adb269ed3c30
Le Vivilet <levivilet@pm.me>
2025-12-04 20:48:46 +0100

    feature: use file system worker for uploading handles

--- packages/renderer-worker/src/parts/UploadFileSystemHandles/UploadFileSystemHandles.js
+++ packages/renderer-worker/src/parts/UploadFileSystemHandles/UploadFileSystemHandles.js
@@ -1,23 +1,5 @@
-import * as Command from '../Command/Command.js'
-import * as FileHandleType from '../FileHandleType/FileHandleType.js'
-import * as UploadFileSystemHandle from '../UploadFileSystemHandle/UploadFileSystemHandle.js'
-
-const uploadHandles = async (fileSystemHandles, pathSeparator, root) => {
-  for (const fileSystemHandle of fileSystemHandles) {
-    await UploadFileSystemHandle.uploadHandle(fileSystemHandle, pathSeparator, root, uploadHandles)
-  }
-}
+import * as FileSystemWorker from '../FileSystemWorker/FileSystemWorker.js'
 
 export const uploadFileSystemHandles = async (root, pathSeparator, fileSystemHandles) => {
-  if (fileSystemHandles.length === 1) {
-    const file = fileSystemHandles[0]
-    const { name, kind } = file
-    if (kind === FileHandleType.Directory) {
-      await Command.execute('PersistentFileHandle.addHandle', `/${name}`, file)
-      await Command.execute('Workspace.setPath', `html:///${name}`)
-      return true
-    }
-  }
-  await uploadHandles(fileSystemHandles, pathSeparator, root)
-  return false
+  return FileSystemWorker.invoke('FileSystem.uploadFileSystemHandles', root, pathSeparator, fileSystemHandles)
 }
