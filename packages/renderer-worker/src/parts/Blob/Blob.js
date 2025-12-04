eb102f677e6fc13e8904bd14499445f5a9bca6a3
Le Vivilet <levivilet@pm.me>
2025-12-04 21:26:42 +0100

    feature: use file system worker for blob conversions (#10328)
    * feature: use file system worker for blob conversions
    
    * test
    
    * fix

--- packages/renderer-worker/src/parts/Blob/Blob.js
+++ packages/renderer-worker/src/parts/Blob/Blob.js
@@ -1,31 +1,13 @@
-// @ts-ignore
-import * as BlobUtil from '../../../../../static/js/blob-util.js'
-import * as NormalizeBlobError from '../NormalizeBlobError/NormalizeBlobError.js'
-import { VError } from '../VError/VError.js'
+import * as FileSystemWorker from '../FileSystemWorker/FileSystemWorker.js'
 
 export const base64StringToBlob = (base64String) => {
-  try {
-    return BlobUtil.base64StringToBlob(base64String)
-  } catch (error) {
-    const normalizedError = NormalizeBlobError.normalizeBlobError(error)
-    throw new VError(normalizedError, 'Failed to convert base64 string to blob')
-  }
+  return FileSystemWorker.invoke('Blob.base64StringToBlob', base64String)
 }
 
 export const binaryStringToBlob = async (string, type) => {
-  try {
-    return await BlobUtil.binaryStringToBlob(string, type)
-  } catch (error) {
-    const normalizedError = NormalizeBlobError.normalizeBlobError(error)
-    throw new VError(normalizedError, 'Failed to convert binary string to blob')
-  }
+  return FileSystemWorker.invoke('Blob.binaryStringToBlob', string, type)
 }
 
 export const blobToBinaryString = async (blob) => {
-  try {
-    return await BlobUtil.blobToBinaryString(blob)
-  } catch (error) {
-    const normalizedError = NormalizeBlobError.normalizeBlobError(error)
-    throw new VError(normalizedError, 'Failed to convert blob to binary string')
-  }
+  return FileSystemWorker.invoke('Blob.blobToBinaryString', blob)
 }
