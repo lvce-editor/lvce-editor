01cef3a00594f170ca974f5df32b7da048ae028a
Le Vivilet <levivilet@pm.me>
2025-12-06 10:56:29 +0100

    feature: query source control badge count for any command (#10340)

--- packages/renderer-worker/src/parts/WrapSourceControlCommand/WrapSourceControlCommand.ts
+++ packages/renderer-worker/src/parts/WrapSourceControlCommand/WrapSourceControlCommand.ts
@@ -8,12 +8,14 @@ export const wrapSourceControlCommand = (key: string) => {
       return state
     }
     const commands = await SourceControlWorker.invoke('SourceControl.render2', state.uid, diffResult)
+    const badgeCount = await SourceControlWorker.invoke('SourceControl.getBadgeCount', state.uid, diffResult)
     if (commands.length === 0) {
       return state
     }
     return {
       ...state,
       commands,
+      badgeCount,
     }
   }
   return fn
