b6e608097e59a161682cdf6a1947f2881ba8948d
Le Vivilet <levivilet@pm.me>
2025-12-06 12:30:42 +0100

    feature: make getActiveSideBarView available

--- packages/renderer-worker/src/parts/ViewletManager/ViewletManager.js
+++ packages/renderer-worker/src/parts/ViewletManager/ViewletManager.js
@@ -49,7 +49,7 @@ const runFn = async (instance, id, key, fn, args) => {
     const oldState = instance.state
     const newState = await fn(oldState, ...args)
 
-    if (key === 'getAllQuickPickMenuEntries' || key === 'getBadgeCounts') {
+    if (key === 'getAllQuickPickMenuEntries' || key === 'getBadgeCounts' || key === 'getActiveSideBarView') {
       return newState
     }
     if (!newState) {
