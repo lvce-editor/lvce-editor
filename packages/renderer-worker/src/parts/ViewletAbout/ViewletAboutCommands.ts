31cdd51990108ca5e3661985d52747a5e947b938
Le Vivilet <levivilet@pm.me>
2025-12-08 15:19:26 +0100

    fix: about view cannot show twice

--- packages/renderer-worker/src/parts/ViewletAbout/ViewletAboutCommands.ts
+++ packages/renderer-worker/src/parts/ViewletAbout/ViewletAboutCommands.ts
@@ -6,7 +6,7 @@ export const Commands = {}
 export const getCommands = async () => {
   const commands = await AboutViewWorker.invoke('About.getCommandIds')
   for (const command of commands) {
-    if (command === 'About.showAbout') {
+    if (command === 'showAbout' || command === 'showAboutElectron') {
       continue
     }
     Commands[command] = WrapAboutCommand.wrapAboutCommand(command)
