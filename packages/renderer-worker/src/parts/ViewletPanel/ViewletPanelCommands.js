4a04efd8397d6bef72280e8dd24a3273bd478e0f
Le Vivilet <levivilet@pm.me>
2025-12-09 14:40:47 +0100

    feature: add selectView

--- packages/renderer-worker/src/parts/ViewletPanel/ViewletPanelCommands.js
+++ packages/renderer-worker/src/parts/ViewletPanel/ViewletPanelCommands.js
@@ -2,6 +2,7 @@ import * as ViewletPanel from '../ViewletPanel/ViewletPanel.js'
 
 export const Commands = {
   selectIndex: ViewletPanel.selectIndex,
+  selectView: ViewletPanel.selectView,
   hidePanel: ViewletPanel.hidePanel,
   handleClickAction: ViewletPanel.handleClickAction,
   handleFilterInput: ViewletPanel.handleFilterInput,
