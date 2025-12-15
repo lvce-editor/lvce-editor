3637d531d2e9d80052976af7e13774acdead871e
Le Vivilet <levivilet@pm.me>
2025-12-15 19:08:47 +0100

    pass params

--- packages/renderer-worker/src/parts/GetColorThemeJson/GetColorThemeJson.js
+++ packages/renderer-worker/src/parts/GetColorThemeJson/GetColorThemeJson.js
@@ -1,5 +1,8 @@
 import * as ExtensionManagementWorker from '../ExtensionManagementWorker/ExtensionManagementWorker.js'
+import * as Platform from '../Platform/Platform.js'
+import * as AssetDir from '../AssetDir/AssetDir.js'
 
 export const getColorThemeJson = (colorThemeId) => {
-  return ExtensionManagementWorker.invoke('Extensions.getColorThemeJson', colorThemeId)
+  const platform = Platform.getPlatform()
+  return ExtensionManagementWorker.invoke('Extensions.getColorThemeJson', colorThemeId, platform, AssetDir.assetDir)
 }
