f169fb5c430408bb25a33b381432cb43afcbb694
Le Vivilet <levivilet@pm.me>
2025-12-15 12:53:38 +0100

    feature: add extension management worker csp (#10442)

--- packages/static-server/src/parts/GetHeadersExtensionManagementWorker/GetHeadersExtensionManagementWorker.js
+++ packages/static-server/src/parts/GetHeadersExtensionManagementWorker/GetHeadersExtensionManagementWorker.js
@@ -1,6 +1,6 @@
-import * as ContentSecurityPolicyAboutWorker from '../ContentSecurityPolicyAboutWorker/ContentSecurityPolicyAboutWorker.js'
+import * as ContentSecurityPolicyExtensionManagementWorker from '../ContentSecurityPolicyExtensionManagementWorker/ContentSecurityPolicyExtensionManagementWorker.js'
 import * as GetHeadersWorker from '../GetHeadersWorker/GetHeadersWorker.js'
 
 export const getHeadersExtensionManagementWorker = (mime, etag, defaultCachingHeader) => {
-  return GetHeadersWorker.getHeadersWorker(mime, etag, defaultCachingHeader, ContentSecurityPolicyAboutWorker.value)
+  return GetHeadersWorker.getHeadersWorker(mime, etag, defaultCachingHeader, ContentSecurityPolicyExtensionManagementWorker.value)
 }
