9ef6e6e72def53de9244e3ec6026027638a6a5a2
Le Vivilet <levivilet@pm.me>
2025-12-27 22:39:32 +0100

    feature: simplify passing platform to getColorThemeJson

--- packages/renderer-worker/src/parts/GetColorThemeJson/GetColorThemeJson.js
+++ packages/renderer-worker/src/parts/GetColorThemeJson/GetColorThemeJson.js
@@ -1,8 +1,9 @@
 import * as ExtensionManagementWorker from '../ExtensionManagementWorker/ExtensionManagementWorker.js'
-import * as Platform from '../Platform/Platform.js'
-import * as AssetDir from '../AssetDir/AssetDir.js'
+import * as Assert from '../Assert/Assert.js'
 
-export const getColorThemeJson = (colorThemeId) => {
-  const platform = Platform.getPlatform()
-  return ExtensionManagementWorker.invoke('Extensions.getColorThemeJson', colorThemeId, platform, AssetDir.assetDir)
+export const getColorThemeJson = (colorThemeId, platform, assetDir) => {
+  Assert.string(colorThemeId)
+  Assert.number(platform)
+  Assert.string(assetDir)
+  return ExtensionManagementWorker.invoke('Extensions.getColorThemeJson', colorThemeId, platform, assetDir)
 }
