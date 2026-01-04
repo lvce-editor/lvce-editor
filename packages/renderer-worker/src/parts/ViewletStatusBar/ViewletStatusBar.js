efb265c4fffc23c866792dbe7f5af4acf75639fa
Le Vivilet <levivilet@pm.me>
2026-01-04 14:42:36 +0100

    faeture: pass platform and assetDir to status bar worker

--- packages/renderer-worker/src/parts/ViewletStatusBar/ViewletStatusBar.js
+++ packages/renderer-worker/src/parts/ViewletStatusBar/ViewletStatusBar.js
@@ -1,4 +1,6 @@
 import * as StatusBarWorker from '../StatusBarWorker/StatusBarWorker.js'
+import * as Platform from '../Platform/Platform.js'
+import * as AssetDir from '../AssetDir/AssetDir.js'
 
 export const create = (id, uri, x, y, width, height) => {
   return {
@@ -13,7 +15,17 @@ export const create = (id, uri, x, y, width, height) => {
 
 export const loadContent = async (state) => {
   const savedState = {}
-  await StatusBarWorker.invoke('StatusBar.create', state.uid, '', state.x, state.y, state.width, state.height, null)
+  await StatusBarWorker.invoke(
+    'StatusBar.create',
+    state.uid,
+    '',
+    state.x,
+    state.y,
+    state.width,
+    state.height,
+    Platform.getPlatform(),
+    AssetDir.assetDir,
+  )
   await StatusBarWorker.invoke('StatusBar.loadContent', state.uid, savedState)
   const diffResult = await StatusBarWorker.invoke('StatusBar.diff2', state.uid)
   const commands = await StatusBarWorker.invoke('StatusBar.render2', state.uid, diffResult)
