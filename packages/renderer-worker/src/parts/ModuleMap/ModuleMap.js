f75974b44cd32c7bd76c35b3a84dcf5936d02103
Le Vivilet <levivilet@pm.me>
2026-01-05 18:04:31 +0100

    feature: remove unused code

--- packages/renderer-worker/src/parts/ModuleMap/ModuleMap.js
+++ packages/renderer-worker/src/parts/ModuleMap/ModuleMap.js
@@ -67,8 +67,6 @@ export const getModuleId = (commandId) => {
       return ModuleId.EditorDiagnostics
     case 3900:
       return ModuleId.EditorError
-    case 'ElectronApplicationMenu':
-      return ModuleId.ElectronApplicationMenu
     case 'ElectronBrowserView':
       return ModuleId.ElectronBrowserView
     case 'ElectronClipBoard':
