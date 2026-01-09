6e92c3013651163bd976a2d850bb0fd2784bb78d
Le Vivilet <levivilet@pm.me>
2026-01-09 15:33:11 +0100

    feature: use preloadUrl function (#10622)

--- packages/shared-process/src/parts/PreloadUrl/PreloadUrl.js
+++ packages/shared-process/src/parts/PreloadUrl/PreloadUrl.js
@@ -1,4 +1,6 @@
 import { join } from 'node:path'
 import * as Root from '../Root/Root.js'
 
-export const preloadUrl = join(Root.root, 'packages', 'shared-process', 'node_modules', '@lvce-editor', 'preload', 'src', 'index.js')
+export const getPreloadUrl = () => {
+  return join(Root.root, 'packages', 'shared-process', 'node_modules', '@lvce-editor', 'preload', 'src', 'index.js')
+}
