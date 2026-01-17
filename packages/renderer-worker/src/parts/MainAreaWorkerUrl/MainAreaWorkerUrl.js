195fe1636c229a9c160d189e4456837f89f3a582
Le Vivilet <levivilet@pm.me>
2026-01-17 12:10:07 +0100

    feature: add function to launch main area worker

---
+++ packages/renderer-worker/src/parts/MainAreaWorkerUrl/MainAreaWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const mainAreaWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/main-area-worker/dist/mainAreaWorkerMain.js`
