754da08039598404ee3256ef40034a72d53fe30e
Le Vivilet <levivilet@pm.me>
2026-01-20 20:40:27 +0100

    feature: add layout types (#10743)
    * feature: add layout types
    
    * fix
    
    * fix: saving sidebar view

---
+++ packages/renderer-worker/src/parts/ViewletLayout/LayoutState.ts
@@ -0,0 +1,40 @@
+export interface LayoutState {
+  readonly sideBarView: string
+  readonly commands: readonly any[]
+  readonly assetDir: string
+  readonly commit: string
+  readonly platform: number
+  readonly sashId: any
+  readonly sideBarLocation: number
+  readonly uid: number
+  readonly points: Uint16Array
+  readonly activityBarId: number
+  readonly sideBarSashId: number
+  readonly sideBarId: number
+  readonly panelSashId: number
+  readonly panelId: number
+  readonly mainId: number
+  readonly contentAreaId: number
+  readonly mainContentsId: number
+  readonly statusBarId: number
+  readonly titleBarId: number
+  readonly workbenchId: number
+  readonly activityBarVisible: boolean
+  readonly contentAreaVisible: boolean
+  readonly mainContentsVisible: boolean
+  readonly mainVisible: boolean
+  readonly panelSashVisible: boolean
+  readonly panelVisible: boolean
+  readonly sideBarSashVisible: boolean
+  readonly sideBarVisible: boolean
+  readonly statusBarVisible: boolean
+  readonly titleBarVisible: boolean
+  readonly workbenchVisible: boolean
+  readonly updateState: string
+  readonly updateProgress: number
+}
+
+export interface LayoutStateResult {
+  readonly newState: LayoutState
+  readonly commands: readonly any[]
+}
