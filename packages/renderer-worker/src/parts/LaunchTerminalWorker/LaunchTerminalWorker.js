facaa11a6b09cd4118a864b73db18d5ac09444c5
Le Vivilet <levivilet@pm.me>
2026-01-23 19:16:36 +0100

    feature: make terminal worker url configurable

--- packages/renderer-worker/src/parts/LaunchTerminalWorker/LaunchTerminalWorker.js
+++ packages/renderer-worker/src/parts/LaunchTerminalWorker/LaunchTerminalWorker.js
@@ -4,13 +4,15 @@ import * as TerminalWorkerUrl from '../TerminalWorkerUrl/TerminalWorkerUrl.js'
 import * as Platform from '../Platform/Platform.js'
 import * as PlatformType from '../PlatformType/PlatformType.js'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as GetConfiguredWorkerUrl from '../GetConfiguredWorkerUrl/GetConfiguredWorkerUrl.ts'
 
 export const launchTerminalWorker = async () => {
+  const configuredWorkerUrl = GetConfiguredWorkerUrl.getConfiguredWorkerUrl('develop.terminalWorkerPath', TerminalWorkerUrl.terminalWorkerUrl)
   const name = Platform.getPlatform() === PlatformType.Electron ? 'Terminal Worker (Electron)' : 'Terminal Worker'
   const ipc = await IpcParent.create({
     method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
     name,
-    url: TerminalWorkerUrl.terminalWorkerUrl,
+    url: configuredWorkerUrl,
   })
   HandleIpc.handleIpc(ipc)
   return ipc
