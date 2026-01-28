db5fd5f6b5e4a8e2a9349a4ff8b7f4eac8d91cf0
Le Vivilet <levivilet@pm.me>
2026-01-29 00:57:45 +0100

    feature: adjust csp for production (#10812)

--- packages/static-server/src/parts/ContentSecurityPolicyDocument/ContentSecurityPolicyDocument.js
+++ packages/static-server/src/parts/ContentSecurityPolicyDocument/ContentSecurityPolicyDocument.js
@@ -1,6 +1,9 @@
 import * as GetContentSecurityPolicy from '../GetContentSecurityPolicy/GetContentSecurityPolicy.js'
 
-const getFrameSrc = () => {
+const getFrameSrc = (isForElectronProduction) => {
+  if (isForElectronProduction) {
+    return [`frame-src lvce-webview:`]
+  }
   return [`frame-src 'self' lvce-oss-webview: http://localhost:3001 http://localhost:3002`]
 }
 
@@ -21,15 +24,16 @@ const getSandbox = () => {
   return [`sandbox allow-scripts allow-same-origin`]
 }
 
-export const value = GetContentSecurityPolicy.getContentSecurityPolicy([
-  `default-src 'none'`,
-  `font-src 'self'`,
-  `img-src 'self' https: data: blob:`, // TODO maybe disallow https and data images
-  `media-src 'self'`,
-  `script-src 'self'`,
-  `style-src 'self'`,
-  ...getFrameAncestors(),
-  ...getFrameSrc(),
-  ...getManifestSrc(),
-  ...getSandbox(),
-])
+export const getValue = (isForElectronProduction) =>
+  GetContentSecurityPolicy.getContentSecurityPolicy([
+    `default-src 'none'`,
+    `font-src 'self'`,
+    `img-src 'self' https: data: blob:`, // TODO maybe disallow https and data images
+    `media-src 'self'`,
+    `script-src 'self'`,
+    `style-src 'self'`,
+    ...getFrameAncestors(),
+    ...getFrameSrc(isForElectronProduction),
+    ...getManifestSrc(),
+    ...getSandbox(),
+  ])
