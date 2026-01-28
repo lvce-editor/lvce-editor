995940dd4ff21a61158da239e7271fea558b3018
Le Vivilet <levivilet@pm.me>
2026-01-28 14:29:34 +0100

    Discard changes to packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorText.js

--- packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorText.js
+++ packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorText.js
@@ -44,7 +44,6 @@ export const handleTokenizeChange = () => {
 
 // TODO uri?
 export const create = (id, uri, x, y, width, height) => {
-  console.log({ uri })
   const fileName = Workspace.pathBaseName(uri)
   const languageId = Languages.getLanguageId(fileName)
   const state = Editor.create(id, uri, languageId)
