0ed66c4e67317cbb6db79adcafee7cf4a7d4c1a2
Le Vivilet <levivilet@pm.me>
2026-01-29 14:34:28 +0100

    fix

--- packages/renderer-worker/src/parts/LaunchMainAreaWorker/LaunchMainAreaWorker.ts
+++ packages/renderer-worker/src/parts/LaunchMainAreaWorker/LaunchMainAreaWorker.ts
@@ -1,11 +1,11 @@
-import * as ActivityBarWorkerUrl from '../ActivityBarWorkerUrl/ActivityBarWorkerUrl.js'
 import * as GetConfiguredWorkerUrl from '../GetConfiguredWorkerUrl/GetConfiguredWorkerUrl.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as MainAreaWorkerUrl from '../MainAreaWorkerUrl/MainAreaWorkerUrl.js'
 
 export const launchMainAreaWorker = async () => {
-  const configuredWorkerUrl = GetConfiguredWorkerUrl.getConfiguredWorkerUrl('develop.mainAreaWorkerPath', ActivityBarWorkerUrl.activityBarWorkerUrl)
+  const configuredWorkerUrl = GetConfiguredWorkerUrl.getConfiguredWorkerUrl('develop.mainAreaWorkerPath', MainAreaWorkerUrl.mainAreaWorkerUrl)
   const ipc = await IpcParent.create({
     method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
     url: configuredWorkerUrl,
