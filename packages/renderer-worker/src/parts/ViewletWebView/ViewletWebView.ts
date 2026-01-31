f794f72e1b5e903c5849e27f84465c115f6bd020
Le Vivilet <levivilet@pm.me>
2026-01-31 23:05:07 +0100

    args

--- packages/renderer-worker/src/parts/ViewletWebView/ViewletWebView.ts
+++ packages/renderer-worker/src/parts/ViewletWebView/ViewletWebView.ts
@@ -1,5 +1,6 @@
 import * as IframeWorker from '../IframeWorker/IframeWorker.js'
 import type { ViewletWebViewState } from './ViewletWebViewState.ts'
+import * as GetRealUri from '../GetRealUri/GetRealUri.js'
 
 export const create = (id: number, uri: string, x: number, y: number, width: number, height: number): ViewletWebViewState => {
   return {
@@ -23,7 +24,9 @@ export const create = (id: number, uri: string, x: number, y: number, width: num
 
 export const loadContent = async (state: ViewletWebViewState): Promise<ViewletWebViewState> => {
   const savedState = {}
-  await IframeWorker.invoke('WebView.create4', state.id, '', state.x, state.y, state.width, state.height, null)
+  const realUri = await GetRealUri.getRealUri(state.uri)
+
+  await IframeWorker.invoke('WebView.create4', state.id, realUri, state.x, state.y, state.width, state.height, null)
   await IframeWorker.invoke('WebView.loadContent', state.id, savedState)
   const diffResult = await IframeWorker.invoke('WebView.diff2', state.id)
   const commands = await IframeWorker.invoke('WebView.render2', state.id, diffResult)
