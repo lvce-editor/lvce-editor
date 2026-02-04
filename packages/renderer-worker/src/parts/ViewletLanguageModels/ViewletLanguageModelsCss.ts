9f15f2a04b24daa9b44adcbd26ed6b2d0949b39c
Le Vivilet <levivilet@pm.me>
2026-02-04 22:08:01 +0100

    feature: update ViewletLanguageModels CSS and fix import path (#10912)
    * feature: update ViewletLanguageModels CSS and fix import path
    
    * button

--- packages/renderer-worker/src/parts/ViewletLanguageModels/ViewletLanguageModelsCss.ts
+++ packages/renderer-worker/src/parts/ViewletLanguageModels/ViewletLanguageModelsCss.ts
@@ -1,1 +1,1 @@
-export const Css = ['/css/parts/ViewletAbout.css', '/css/parts/Button.css', '/css/parts/ViewletDialog.css']
+export const Css = ['/css/parts/ViewletLanguageModels.css', '/css/parts/Button.css', '/css/parts/ViewletDialog.css']
