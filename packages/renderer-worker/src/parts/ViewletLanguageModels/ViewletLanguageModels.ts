c0d13f37f60f1f41692bd8d992c0c1d456f34e4c
Le Vivilet <levivilet@pm.me>
2026-02-04 20:01:47 +0100

    fix

--- packages/renderer-worker/src/parts/ViewletLanguageModels/ViewletLanguageModels.ts
+++ packages/renderer-worker/src/parts/ViewletLanguageModels/ViewletLanguageModels.ts
@@ -11,7 +11,7 @@ export const create = (id: number): LangaugeModelsState => {
 export const loadContent = async (state: LangaugeModelsState): Promise<LangaugeModelsState> => {
   const { id } = state
   await LanguageModelsViewWorker.invoke('LanguageModels.create', id)
-  await LanguageModelsViewWorker.invoke('LanguageModels.loadContent2', id)
+  await LanguageModelsViewWorker.invoke('LanguageModels.loadContent', id)
   const diffResult = await LanguageModelsViewWorker.invoke('LanguageModels.diff2', id)
   const commands = await LanguageModelsViewWorker.invoke('LanguageModels.render2', id, diffResult)
   return {
