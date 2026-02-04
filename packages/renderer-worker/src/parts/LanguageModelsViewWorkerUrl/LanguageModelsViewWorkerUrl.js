49b64822ba44fb2c40d8b1c5b30889b96cd80fc2
Le Vivilet <levivilet@pm.me>
2026-02-04 19:47:06 +0100

    feature: add LanguageModelsViewWorker and related modules for language model processing

---
+++ packages/renderer-worker/src/parts/LanguageModelsViewWorkerUrl/LanguageModelsViewWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const languageModelsViewWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/language-models-view/dist/languageModelsViewMain.js`
