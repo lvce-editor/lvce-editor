e64fd28aab92a81efe0cc750cc0df1b66a7c17ab
Le Vivilet <levivilet@pm.me>
2026-02-04 23:45:19 +0100

    feature: extend LangaugeModelsState with dimensions and update create function

--- packages/renderer-worker/src/parts/ViewletLanguageModels/ViewletLanguageModelsTypes.ts
+++ packages/renderer-worker/src/parts/ViewletLanguageModels/ViewletLanguageModelsTypes.ts
@@ -1,4 +1,8 @@
 export interface LangaugeModelsState {
   readonly commands: any[]
   readonly id: number
+  readonly x: number
+  readonly y: number
+  readonly width: number
+  readonly height: number
 }
