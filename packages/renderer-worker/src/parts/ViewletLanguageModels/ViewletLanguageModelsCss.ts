f6f20e9588eaaabb7271e1d6dd38017fdaaa9ac0
Le Vivilet <levivilet@pm.me>
2026-02-05 13:24:29 +0100

    feature: add SearchField.css to ViewletLanguageModelsCss export

--- packages/renderer-worker/src/parts/ViewletLanguageModels/ViewletLanguageModelsCss.ts
+++ packages/renderer-worker/src/parts/ViewletLanguageModels/ViewletLanguageModelsCss.ts
@@ -1,1 +1,1 @@
-export const Css = ['/css/parts/ViewletLanguageModels.css', '/css/parts/Button.css', '/css/parts/ViewletDialog.css']
+export const Css = ['/css/parts/ViewletLanguageModels.css', '/css/parts/Button.css', '/css/parts/ViewletDialog.css', '/css/parts/SearchField.css']
