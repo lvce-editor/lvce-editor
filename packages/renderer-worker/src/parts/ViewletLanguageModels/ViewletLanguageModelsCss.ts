8bd739afc411686401dab13c22c2ddc9e3e17791
Le Vivilet <levivilet@pm.me>
2026-02-05 13:46:44 +0100

    feature: add SearchField.css to ViewletLanguageModelsCss export (#10922)
    * feature: add SearchField.css to ViewletLanguageModelsCss export
    
    * css
    
    * feature: remove height property from ExtensionListItemDetail for better layout control

--- packages/renderer-worker/src/parts/ViewletLanguageModels/ViewletLanguageModelsCss.ts
+++ packages/renderer-worker/src/parts/ViewletLanguageModels/ViewletLanguageModelsCss.ts
@@ -1,1 +1,8 @@
-export const Css = ['/css/parts/ViewletLanguageModels.css', '/css/parts/Button.css', '/css/parts/ViewletDialog.css']
+export const Css = [
+  '/css/parts/ViewletLanguageModels.css',
+  '/css/parts/Button.css',
+  '/css/parts/ViewletDialog.css',
+  '/css/parts/SearchField.css',
+  '/css/parts/Button.css',
+  '/css/parts/IconButton.css',
+]
