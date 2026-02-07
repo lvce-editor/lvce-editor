e07d0d75149ecc1a56f38f45526815c4eb908701
Le Vivilet <levivilet@pm.me>
2026-02-07 11:55:50 +0100

    feature: add createForPreview command to OffscreenCanvas (#10976)

--- packages/renderer-worker/src/parts/OffscreenCanvas/OffscreenCanvas.js
+++ packages/renderer-worker/src/parts/OffscreenCanvas/OffscreenCanvas.js
@@ -2,6 +2,7 @@ import * as Assert from '../Assert/Assert.ts'
 import * as Id from '../Id/Id.js'
 import * as RendererProcess from '../RendererProcess/RendererProcess.js'
 import * as TerminalWorker from '../TerminalWorker/TerminalWorker.js'
+import * as PreviewWorker from '../PreviewWorker/PreviewWorker.js'
 import * as Transferrable from '../Transferrable/Transferrable.js'
 
 export const create = async (canvasId) => {
@@ -15,4 +16,9 @@ export const create = async (canvasId) => {
 export const createForTerminal = async (canvasId, callbackId) => {
   const canvas = await create(canvasId)
   await TerminalWorker.invokeAndTransfer('OffscreenCanvas.handleResult', callbackId, canvas)
+}
+
+export const createForPreview = async (canvasId, callbackId) => {
+  const canvas = await create(canvasId)
+  await PreviewWorker.invokeAndTransfer('Preview.executeCallback', callbackId, canvas)
 }
