d3d503e76a1aa696edb4deb2d64f5cea5668bb45
Le Vivilet <levivilet@pm.me>
2026-02-07 11:35:26 +0100

    feature: add restart functionality and hotReload command to PreviewWorker

--- packages/renderer-worker/src/parts/ViewletPreview/ViewletPreview.js
+++ packages/renderer-worker/src/parts/ViewletPreview/ViewletPreview.js
@@ -28,6 +28,7 @@ export const loadContent = async (state) => {
   const commands = await PreviewWorker.invoke('Preview.render2', state.uid, diffResult)
   return {
     ...state,
+    uri,
     commands,
   }
 }
@@ -58,12 +59,34 @@ export const dispose = (state) => {
   }
 }
 
+export const hotReload = async (state) => {
+  if (state.isHotReloading) {
+    return state
+  }
+  // TODO avoid mutation
+  state.isHotReloading = true
+  // possible TODO race condition during hot reload
+  // there could still be pending promises when the worker is disposed
+  const savedState = await PreviewWorker.invoke('Preview.saveState', state.uid)
+  await PreviewWorker.restart('Preview.terminate')
+  await PreviewWorker.invoke('Preview.create', state.uid, state.uri, state.x, state.y, state.width, state.height, null)
+  await PreviewWorker.invoke('Preview.loadContent', state.uid, savedState)
+  const diffResult = await PreviewWorker.invoke('Preview.diff2', state.uid)
+  const commands = await PreviewWorker.invoke('Preview.render2', state.uid, diffResult)
+  state.isHotReloading = false
+  return {
+    ...state,
+    commands,
+  }
+}
+
 export const Commands = {
   setUri,
   rerender,
   handleClick,
   handleInput,
   renderEventListeners,
+  hotReload,
 }
 
 export const hasFunctionalRender = true
