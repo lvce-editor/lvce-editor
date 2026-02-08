329b7e2463c75cd35dadd9c0f39e5f467e06bd26
Le Vivilet <levivilet@pm.me>
2026-02-08 23:19:06 +0100

    width

--- packages/renderer-worker/src/parts/ViewletManager/ViewletManager.js
+++ packages/renderer-worker/src/parts/ViewletManager/ViewletManager.js
@@ -457,7 +457,14 @@ export const backgroundLoad = async ({ getModule, id, x, y, width, height, props
 export const load = async (viewlet, focus = false, restore = false, restoreState = undefined) => {
   // console.time(`load/${viewlet.id}`)
   // TODO
-  if (viewlet.id === 'Terminal') {
+  if (
+    viewlet.id === 'Terminal' ||
+    viewlet.id === 'SideBar' ||
+    viewlet.id === 'TitleBar' ||
+    viewlet.id === 'Panel' ||
+    viewlet.id === 'ActivityBar' ||
+    viewlet.id === 'StatusBar'
+  ) {
     viewlet.setBounds = false
   }
   if (viewlet.type !== 0) {
