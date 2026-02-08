983fbe9e7d07cd54a5fde4b13810ec83c694d543
Le Vivilet <levivilet@pm.me>
2026-02-09 00:10:42 +0100

    debug

--- packages/renderer-worker/src/parts/ViewletManager/ViewletManager.js
+++ packages/renderer-worker/src/parts/ViewletManager/ViewletManager.js
@@ -534,7 +534,7 @@ export const load = async (viewlet, focus = false, restore = false, restoreState
 
     if (module.getKeyBindings) {
       const keyBindings = await module.getKeyBindings(viewletUid)
-      console.log({ keyBindings, moduleId })
+      console.log({ keyBindings, moduleId: module.name })
       KeyBindingsState.addKeyBindings(viewlet.id, keyBindings)
     }
     if (module.Commands && module.Commands.getMouseActions) {
