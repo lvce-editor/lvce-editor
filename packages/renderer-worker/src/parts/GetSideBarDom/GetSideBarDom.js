ccd49ce559344fc63a79e5fb22df04c7c787dabe
Le Vivilet <levivilet@pm.me>
2026-02-08 13:55:56 +0100

    rendering

--- packages/renderer-worker/src/parts/GetSideBarDom/GetSideBarDom.js
+++ packages/renderer-worker/src/parts/GetSideBarDom/GetSideBarDom.js
@@ -1,4 +1,33 @@
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.js'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.js'
+
+const getActionsDom = (newState) => {
+  const actions = newState.actions || []
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: 'Actions',
+      role: 'toolbar',
+      childCount: 0,
+    },
+  ]
+}
+
+const getTitleAreaDom = (newState) => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: 'SideBarTitleArea',
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.H2,
+      childCount: 1,
+    },
+    text(newState.title),
+    ...getActionsDom(newState),
+  ]
+}
 
 export const getSideBarDom = (newState) => {
   const { childUid } = newState
@@ -6,8 +35,9 @@ export const getSideBarDom = (newState) => {
     {
       type: VirtualDomElements.Div,
       className: 'SideBar',
-      childCount: 1,
+      childCount: 2,
     },
+    ...getTitleAreaDom(newState),
     {
       type: VirtualDomElements.Reference,
       uid: childUid,
