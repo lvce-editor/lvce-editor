26370b60ac4a93b60a86241fbd148a61f59c73c0
Le Vivilet <levivilet@pm.me>
2026-02-08 17:48:39 +0100

    log

--- packages/renderer-worker/src/parts/ViewletLayout/ViewletLayoutRenderDom.ts
+++ packages/renderer-worker/src/parts/ViewletLayout/ViewletLayoutRenderDom.ts
@@ -3,9 +3,6 @@ import { LayoutState } from './LayoutState.ts'
 
 export const renderDom = (oldState, newState: LayoutState) => {
   const dom = getLayoutVirtualDom(newState)
-  console.log('mainUid', newState.mainContentsVisible, newState.contentAreaId)
-  console.log({ dom })
-  console.log('sidebar vis', newState.sideBarVisible, newState.sideBarId)
   const commands: any[] = [['Viewlet.setDom2', newState.uid, dom]]
   // TODO ensure focus commands are last in the commands array
   return commands
