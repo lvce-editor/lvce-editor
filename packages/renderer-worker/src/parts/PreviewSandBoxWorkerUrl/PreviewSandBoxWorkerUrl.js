ad464723512a1166826a1d41c064353b6a37d60d
Le Vivilet <levivilet@pm.me>
2026-02-10 19:24:01 +0100

    ifx

--- packages/renderer-worker/src/parts/PreviewSandBoxWorkerUrl/PreviewSandBoxWorkerUrl.js
+++ packages/renderer-worker/src/parts/PreviewSandBoxWorkerUrl/PreviewSandBoxWorkerUrl.js
@@ -1,3 +1,3 @@
 import * as AssetDir from '../AssetDir/AssetDir.js'
 
-export const previewSandBoxWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/preview-sandbox-worker/dist/previewSandboxWorkerMain.js`
+export const previewSandBoxWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/preview-sandbox-worker/dist/previewSandBoxWorkerMain.js`
