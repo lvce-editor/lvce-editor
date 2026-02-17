cf8ead9091a4eaaac1e7023951436d5dae685c57
Le Vivilet <levivilet@pm.me>
2026-02-17 23:37:25 +0100

    feature: add 'play' command to Audio and CommandMap

--- packages/renderer-worker/src/parts/Audio/Audio.js
+++ packages/renderer-worker/src/parts/Audio/Audio.js
@@ -4,3 +4,7 @@ export const playBell = async () => {
   const src = '/sounds/bell.oga'
   await RendererProcess.invoke(/* Audio.play */ 3211, /* src */ src)
 }
+
+export const play = async (url) => {
+  await RendererProcess.invoke('Audio.play', /* src */ url)
+}
