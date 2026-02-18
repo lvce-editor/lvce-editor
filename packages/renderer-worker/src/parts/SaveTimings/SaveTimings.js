aaad4d3a3f2d0098311972c94c3a4aedc4553527
Le Vivilet <levivilet@pm.me>
2026-02-18 23:48:41 +0100

    feature: round save timings to milliseconds for accuracy

--- packages/renderer-worker/src/parts/SaveTimings/SaveTimings.js
+++ packages/renderer-worker/src/parts/SaveTimings/SaveTimings.js
@@ -1,8 +1,12 @@
 import * as InstanceStorage from '../InstanceStorage/InstanceStorage.js'
 
+const roundToMilliseconds = (value) => {
+  return Math.round(value)
+}
+
 export const saveTimings = async (builtinSaveStart, builtinSaveEnd, extensionSaveEnd) => {
   await InstanceStorage.setJson('Timings', {
-    builtinSaveTime: builtinSaveEnd - builtinSaveStart,
-    extensionSaveTime: extensionSaveEnd - builtinSaveEnd,
+    builtinSaveTime: roundToMilliseconds(builtinSaveEnd - builtinSaveStart),
+    extensionSaveTime: roundToMilliseconds(extensionSaveEnd - builtinSaveEnd),
   })
 }
