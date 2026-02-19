3a221e61746b5a77d7414ad46fc2d6441506a80a
Le Vivilet <levivilet@pm.me>
2026-02-19 01:44:34 +0100

    fix

--- packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorText.ipc.js
+++ packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorText.ipc.js
@@ -1,12 +1,3 @@
-import * as ViewletEditorText from './ViewletEditorText.js'
-
-// prettier-ignore
-export const Events = {
-  'languages.changed': ViewletEditorText.handleLanguagesChanged,
-  // 'editor.change': ViewletEditorText.handleEditorChange,
-  // 'tokenizer.changed': ViewletEditorText.handleTokenizeChange,
-}
-
 export * from './ViewletEditorText.js'
 export * from './ViewletEditorTextCommands.js'
 export * from './ViewletEditorTextCss.js'
