375ba184356b63a143a942f12202641294e24896
Le Vivilet <levivilet@pm.me>
2026-02-19 22:20:13 +0100

    feature: implement launchPanelWorker and PanelWorker modules with IPC integration (#11084)

---
+++ packages/renderer-worker/src/parts/LaunchPanelWorker/LaunchPanelWorker.ts
@@ -0,0 +1,16 @@
+import * as PanelWorkerUrl from '../PanelWorkerUrl/PanelWorkerUrl.js'
+import * as GetConfiguredWorkerUrl from '../GetConfiguredWorkerUrl/GetConfiguredWorkerUrl.ts'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
+import * as IpcParent from '../IpcParent/IpcParent.js'
+import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+
+export const launchPanelWorker = async () => {
+  const configuredWorkerUrl = GetConfiguredWorkerUrl.getConfiguredWorkerUrl('develop.PanelWorkerPath', PanelWorkerUrl.panelWorkerUrl)
+  const ipc = await IpcParent.create({
+    method: IpcParentType.ModuleWorkerAndWorkaroundForChromeDevtoolsBug,
+    url: configuredWorkerUrl,
+    name: 'Panel Worker',
+  })
+  HandleIpc.handleIpc(ipc)
+  return ipc
+}
