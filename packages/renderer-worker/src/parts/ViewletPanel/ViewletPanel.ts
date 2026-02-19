13ac0d45889a707bd7ff15b5e79b2f3d8778e1c0
Le Vivilet <levivilet@pm.me>
2026-02-19 22:01:49 +0100

    panel

--- packages/renderer-worker/src/parts/ViewletPanel/ViewletPanel.ts
+++ packages/renderer-worker/src/parts/ViewletPanel/ViewletPanel.ts
@@ -1,6 +1,5 @@
-import * as ActivityBarWorker from '../ActivityBarWorker/ActivityBarWorker.js'
+import * as PanelWorker from '../PanelWorker/PanelWorker.js'
 import * as Height from '../Height/Height.js'
-import type { ActivityBarState } from './ActivityBarState.ts'
 
 // TODO rename viewlet parameter to something else (e.g. clicking settings opens context menu not settings viewlet)
 // TODO should just pass index
@@ -10,10 +9,10 @@ import type { ActivityBarState } from './ActivityBarState.ts'
 // TODO this should be create
 // TODO unregister listeners when hidden
 
-export const create = (id, uri, x, y, width, height): ActivityBarState => {
+export const create = (id, uri, x, y, width, height): any => {
   return {
     uid: id,
-    activityBarItems: [],
+    PanelItems: [],
     filteredItems: [],
     focusedIndex: -1,
     selectedIndex: -1,
@@ -22,17 +21,16 @@ export const create = (id, uri, x, y, width, height): ActivityBarState => {
     y,
     width,
     height,
-    itemHeight: Height.ActivityBarItem,
     commands: [],
   }
 }
 
-export const loadContent = async (state: ActivityBarState): Promise<ActivityBarState> => {
+export const loadContent = async (state: any): Promise<any> => {
   const savedState = {}
-  await ActivityBarWorker.invoke('ActivityBar.create', state.uid, '', state.x, state.y, state.width, state.height, null)
-  await ActivityBarWorker.invoke('ActivityBar.loadContent', state.uid, savedState)
-  const diffResult = await ActivityBarWorker.invoke('ActivityBar.diff2', state.uid)
-  const commands = await ActivityBarWorker.invoke('ActivityBar.render2', state.uid, diffResult)
+  await PanelWorker.invoke('Panel.create', state.uid, '', state.x, state.y, state.width, state.height, null)
+  await PanelWorker.invoke('Panel.loadContent', state.uid, savedState)
+  const diffResult = await PanelWorker.invoke('Panel.diff2', state.uid)
+  const commands = await PanelWorker.invoke('Panel.render2', state.uid, diffResult)
   return {
     ...state,
     commands,
@@ -47,12 +45,12 @@ export const hotReload = async (state) => {
   state.isHotReloading = true
   // possible TODO race condition during hot reload
   // there could still be pending promises when the worker is disposed
-  const savedState = await ActivityBarWorker.invoke('ActivityBar.saveState', state.uid)
-  await ActivityBarWorker.restart('ActivityBar.terminate')
-  await ActivityBarWorker.invoke('ActivityBar.create', state.uid, '', state.x, state.y, state.width, state.height, null)
-  await ActivityBarWorker.invoke('ActivityBar.loadContent', state.uid, savedState)
-  const diffResult = await ActivityBarWorker.invoke('ActivityBar.diff2', state.uid)
-  const commands = await ActivityBarWorker.invoke('ActivityBar.render2', state.uid, diffResult)
+  const savedState = await PanelWorker.invoke('Panel.saveState', state.uid)
+  await PanelWorker.restart('Panel.terminate')
+  await PanelWorker.invoke('Panel.create', state.uid, '', state.x, state.y, state.width, state.height, null)
+  await PanelWorker.invoke('Panel.loadContent', state.uid, savedState)
+  const diffResult = await PanelWorker.invoke('Panel.diff2', state.uid)
+  const commands = await PanelWorker.invoke('Panel.render2', state.uid, diffResult)
   state.isHotReloading = false
   return {
     ...state,
