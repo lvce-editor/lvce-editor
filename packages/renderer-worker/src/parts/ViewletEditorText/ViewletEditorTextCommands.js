742ee8461c7d5305e24de9c85cdfcf4311f186c2
Le Vivilet <levivilet@pm.me>
2026-02-19 01:44:13 +0100

    fix

--- packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextCommands.js
+++ packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextCommands.js
@@ -1,13 +1,13 @@
 import * as EditorWorker from '../EditorWorker/EditorWorker.js'
-import * as WrapStatusBarCommand from '../WrapStatusBarCommand/WrapStatusBarCommand.ts'
 import * as ViewletStatusBar from '../ViewletStatusBar/ViewletStatusBar.js'
+import { wrapEditorCommand } from '../WrapEditorCommand/WrapEditorCommand.ts'
 
 export const Commands = {}
 
 export const getCommands = async () => {
   const commands = await EditorWorker.invoke('Editor.getCommandIds')
   for (const command of commands) {
-    Commands[command] = WrapStatusBarCommand.wrapStatusBarCommand(command)
+    Commands[command] = wrapEditorCommand(command)
   }
   Commands['hotReload'] = ViewletStatusBar.hotReload
 
