48a8b2fb413d0954f38fc3ae427848814a262471
Le Vivilet <levivilet@pm.me>
2026-02-19 22:02:07 +0100

    import

--- packages/renderer-worker/src/parts/ViewletPanel/ViewletPanel.ipc.ts
+++ packages/renderer-worker/src/parts/ViewletPanel/ViewletPanel.ipc.ts
@@ -2,5 +2,5 @@ export const name = 'Panel'
 
 export * from './ViewletPanel.ts'
 export * from './ViewletPanelCommands.ts'
-export * from './ViewletPanelCss.js'
-export * from './ViewletPanelRender.js'
+export * from './ViewletPanelCss.ts'
+export * from './ViewletPanelRender.ts'
