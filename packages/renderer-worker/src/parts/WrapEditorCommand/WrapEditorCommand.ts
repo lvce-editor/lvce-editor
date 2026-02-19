96964d6b9751972bc7b3ae4e27192a526cdc65d8
Le Vivilet <levivilet@pm.me>
2026-02-19 01:43:55 +0100

    wrap

---
+++ packages/renderer-worker/src/parts/WrapEditorCommand/WrapEditorCommand.ts
@@ -0,0 +1,20 @@
+import * as EditorWorker from '../EditorWorker/EditorWorker.js'
+
+export const wrapEditorCommand = (key: string) => {
+  const fn = async (state, ...args) => {
+    await EditorWorker.invoke(`Editor.${key}`, state.uid, ...args)
+    const diffResult = await EditorWorker.invoke('Editor.diff2', state.uid)
+    if (diffResult.length === 0) {
+      return state
+    }
+    const commands = await EditorWorker.invoke('Editor.render2', state.uid, diffResult)
+    if (commands.length === 0) {
+      return state
+    }
+    return {
+      ...state,
+      commands,
+    }
+  }
+  return fn
+}
