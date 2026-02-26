4c981b20ab7bce1933c812225c13a8bf76e2fcc7
Le Vivilet <levivilet@pm.me>
2026-02-26 12:36:16 +0100

    feature: add SecondarySideBar viewlet and related functionalities (#11190)

---
+++ packages/renderer-worker/src/parts/ViewletSecondarySideBar/ViewletSecondarySideBarCommands.js
@@ -0,0 +1,12 @@
+import * as ViewletSecondarySideBar from './ViewletSecondarySideBar.js'
+
+// prettier-ignore
+export const Commands = {
+  focus: ViewletSecondarySideBar.focus,
+  openViewlet: ViewletSecondarySideBar.openViewlet,
+  show: ViewletSecondarySideBar.openViewlet,
+  open: ViewletSecondarySideBar.openViewlet,
+  handleClickAction: ViewletSecondarySideBar.handleClickAction,
+  handleUpdateStateChange: ViewletSecondarySideBar.handleUpdateStateChange,
+  handleSecondarySideBarViewletChange: ViewletSecondarySideBar.handleSecondarySideBarViewletChange,
+}
