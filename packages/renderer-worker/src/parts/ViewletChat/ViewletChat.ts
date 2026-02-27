f1b9bbf65b7b69a93b7618cb7749b6cd885bb7f5
Le Vivilet <levivilet@pm.me>
2026-02-27 22:44:26 +0100

    feature: update ChatState interface and create function to include position and size parameters (#11222)

--- packages/renderer-worker/src/parts/ViewletChat/ViewletChat.ts
+++ packages/renderer-worker/src/parts/ViewletChat/ViewletChat.ts
@@ -1,16 +1,20 @@
 import * as ChatViewWorker from '../ChatViewWorker/ChatViewWorker.js'
 import type { ChatState } from './ViewletChatTypes.ts'
 
-export const create = (id: number): ChatState => {
+export const create = (id, uri, x, y, width, height, args, parentUid): ChatState => {
   return {
     id,
+    x,
+    y,
+    width,
+    height,
     commands: [],
   }
 }
 
 export const loadContent = async (state: ChatState): Promise<ChatState> => {
-  const { id } = state
-  await ChatViewWorker.invoke('Chat.create', id)
+  const { id, x, y, width, height } = state
+  await ChatViewWorker.invoke('Chat.create', id, x, y, width, height)
   await ChatViewWorker.invoke('Chat.loadContent2', id)
   const diffResult = await ChatViewWorker.invoke('Chat.diff2', id)
   const commands = await ChatViewWorker.invoke('Chat.render2', id, diffResult)
@@ -30,7 +34,7 @@ export const hotReload = async (state) => {
   // there could still be pending promises when the worker is disposed
   const savedState = await ChatViewWorker.invoke('Chat.saveState', state.uid)
   await ChatViewWorker.restart('Chat.terminate')
-  await ChatViewWorker.invoke('Chat.create', state.uid, '', state.x, state.y, state.width, state.height, null)
+  await ChatViewWorker.invoke('Chat.create', state.uid, state.x, state.y, state.width, state.height)
   await ChatViewWorker.invoke('Chat.loadContent', state.uid, savedState)
   const diffResult = await ChatViewWorker.invoke('Chat.diff2', state.uid)
   const commands = await ChatViewWorker.invoke('Chat.render2', state.uid, diffResult)
