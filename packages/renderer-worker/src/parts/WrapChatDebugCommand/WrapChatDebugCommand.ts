49eb2cfea9ef9a804fe1ccb478bc6dc14619e779
Le Vivilet <levivilet@pm.me>
2026-03-08 18:50:58 +0100

    uid

--- packages/renderer-worker/src/parts/WrapChatDebugCommand/WrapChatDebugCommand.ts
+++ packages/renderer-worker/src/parts/WrapChatDebugCommand/WrapChatDebugCommand.ts
@@ -2,9 +2,9 @@ import * as ChatDebugViewWorker from '../ChatDebugViewWorker/ChatDebugViewWorker
 
 export const wrapChatDebugCommand = (key: string) => {
   const fn = async (state, ...args) => {
-    await ChatDebugViewWorker.invoke(`ChatDebug.${key}`, state.id, ...args)
-    const diffResult = await ChatDebugViewWorker.invoke(`ChatDebug.diff2`, state.id)
-    const commands = await ChatDebugViewWorker.invoke('ChatDebug.render2', state.id, diffResult)
+    await ChatDebugViewWorker.invoke(`ChatDebug.${key}`, state.uid, ...args)
+    const diffResult = await ChatDebugViewWorker.invoke(`ChatDebug.diff2`, state.uid)
+    const commands = await ChatDebugViewWorker.invoke('ChatDebug.render2', state.uid, diffResult)
     if (commands.length === 0) {
       return state
     }
