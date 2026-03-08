4da57a4b956ce3ee231c6d8ba175237f8b102e3a
Le Vivilet <levivilet@pm.me>
2026-03-08 19:00:59 +0100

    feat: update import to use ChatDebugViewWorker in wrapChatDebugCommand (#11291)

--- packages/renderer-worker/src/parts/WrapChatDebugCommand/WrapChatDebugCommand.ts
+++ packages/renderer-worker/src/parts/WrapChatDebugCommand/WrapChatDebugCommand.ts
@@ -1,10 +1,10 @@
-import * as ChatViewWorker from '../ChatViewWorker/ChatViewWorker.js'
+import * as ChatDebugViewWorker from '../ChatDebugViewWorker/ChatDebugViewWorker.js'
 
 export const wrapChatDebugCommand = (key: string) => {
   const fn = async (state, ...args) => {
-    await ChatViewWorker.invoke(`ChatDebug.${key}`, state.id, ...args)
-    const diffResult = await ChatViewWorker.invoke(`ChatDebug.diff2`, state.id)
-    const commands = await ChatViewWorker.invoke('ChatDebug.render2', state.id, diffResult)
+    await ChatDebugViewWorker.invoke(`ChatDebug.${key}`, state.uid, ...args)
+    const diffResult = await ChatDebugViewWorker.invoke(`ChatDebug.diff2`, state.uid)
+    const commands = await ChatDebugViewWorker.invoke('ChatDebug.render2', state.uid, diffResult)
     if (commands.length === 0) {
       return state
     }
