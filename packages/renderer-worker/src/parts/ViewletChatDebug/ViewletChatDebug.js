2f5065d5d4a6f93de473aad7900b790cf883fb01
Le Vivilet <levivilet@pm.me>
2026-03-08 17:14:10 +0100

    feat: pass URI parameter to ChatDebug.create in loadContent and hotReload functions

--- packages/renderer-worker/src/parts/ViewletChatDebug/ViewletChatDebug.js
+++ packages/renderer-worker/src/parts/ViewletChatDebug/ViewletChatDebug.js
@@ -12,12 +12,23 @@ export const create = (id, uri, x, y, width, height) => {
     commands: [],
     platform: Platform.getPlatform(),
     assetDir: AssetDir.assetDir,
+    uri,
   }
 }
 
 export const loadContent = async (state) => {
   const savedState = {}
-  await ChatDebugViewWorker.invoke('ChatDebug.create', state.uid, '', state.x, state.y, state.width, state.height, state.platform, state.assetDir)
+  await ChatDebugViewWorker.invoke(
+    'ChatDebug.create',
+    state.uid,
+    state.uri,
+    state.x,
+    state.y,
+    state.width,
+    state.height,
+    state.platform,
+    state.assetDir,
+  )
   await ChatDebugViewWorker.invoke('ChatDebug.loadContent', state.uid, savedState)
   const diffResult = await ChatDebugViewWorker.invoke('ChatDebug.diff2', state.uid)
   const commands = await ChatDebugViewWorker.invoke('ChatDebug.render2', state.uid, diffResult)
@@ -37,7 +48,7 @@ export const hotReload = async (state) => {
   // there could still be pending promises when the worker is disposed
   const savedState = await ChatDebugViewWorker.invoke('ChatDebug.saveState', state.uid)
   await ChatDebugViewWorker.restart('ChatDebug.terminate')
-  await ChatDebugViewWorker.invoke('ChatDebug.create', state.uid, '', state.x, state.y, state.width, state.height, null)
+  await ChatDebugViewWorker.invoke('ChatDebug.create', state.uid, state.uri, state.x, state.y, state.width, state.height, null)
   await ChatDebugViewWorker.invoke('ChatDebug.loadContent', state.uid, savedState)
   const diffResult = await ChatDebugViewWorker.invoke('ChatDebug.diff2', state.uid)
   const commands = await ChatDebugViewWorker.invoke('ChatDebug.render2', state.uid, diffResult)
