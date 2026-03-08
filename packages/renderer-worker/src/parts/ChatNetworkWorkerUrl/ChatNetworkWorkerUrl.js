8524b5d9dc8d9976472ed98ac426617d4b3fd30f
Le Vivilet <levivilet@pm.me>
2026-03-08 11:02:43 +0100

    feat: add ChatNetworkWorker and LaunchChatNetworkWorker modules

---
+++ packages/renderer-worker/src/parts/ChatNetworkWorkerUrl/ChatNetworkWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const chatNetworkWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/chat-network-worker/dist/chatNetworkWorkerMain.js`
