a16d69a805775390cb2d7dd35e4a94f7557682f9
Le Vivilet <levivilet@pm.me>
2026-03-08 13:32:40 +0100

    feat: add ChatToolWorkerUrl and LaunchChatToolWorker modules for chat tool worker functionality

---
+++ packages/renderer-worker/src/parts/ChatToolWorkerUrl/ChatToolWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const chatToolWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/chat-tool-worker/dist/chatToolWorkerMain.js`
