cb33c26e935ffe1b2cc6648f22d6eeff5339a93f
Le Vivilet <levivilet@pm.me>
2026-03-09 15:11:05 +0100

    feature: update chat styles and add ChatDebugView component

--- packages/renderer-worker/src/parts/ViewletChatDebug/ViewletChatDebugCss.js
+++ packages/renderer-worker/src/parts/ViewletChatDebug/ViewletChatDebugCss.js
@@ -1,3 +1,1 @@
-export const Css = ['/css/parts/ViewletStatusBar.css']
-
-export const Variables = ['StatusBarBorderTopColor', 'StatusBarForeground']
+export const Css = ['/css/parts/ViewletChat.css', '/css/parts/ViewletChatDebug.css']
