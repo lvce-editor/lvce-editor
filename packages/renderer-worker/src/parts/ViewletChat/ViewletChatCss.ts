5c7b8eba203b0a6192a68dfb278a3d924fa24ffc
Le Vivilet <levivilet@pm.me>
2026-03-10 12:24:26 +0100

    feature: add ChatSendArea and ChatTool CSS styles

--- packages/renderer-worker/src/parts/ViewletChat/ViewletChatCss.ts
+++ packages/renderer-worker/src/parts/ViewletChat/ViewletChatCss.ts
@@ -1,5 +1,7 @@
 export const Css = [
   '/css/parts/Button.css',
+  '/css/parts/ChatSendArea.css',
+  '/css/parts/ChatTool.css',
   '/css/parts/InputBox.css',
   '/css/parts/Select.css',
   '/css/parts/ViewletChat.css',
