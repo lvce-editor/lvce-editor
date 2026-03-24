e6b3c797f1cb9407d0c1e83cbc39bb13ea65cce4
Le Vivilet <levivilet@pm.me>
2026-03-24 13:49:12 +0100

    feature: add chat message parsing worker implementation

---
+++ packages/renderer-worker/src/parts/ChatMessageParsingWorkerUrl/ChatMessageParsingWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const chatMessageParsingWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/chat-message-parsing-worker/dist/chatMessageParsingWorkerMain.js`
