fd36e78b9d7a24648c000a2c89ae923da0461c55
Le Vivilet <levivilet@pm.me>
2026-03-31 23:00:59 +0200

    feature: integrate JsonRpc and Platform for ChatTool initialization

--- packages/renderer-worker/src/parts/LaunchChatToolWorker/LaunchChatToolWorker.js
+++ packages/renderer-worker/src/parts/LaunchChatToolWorker/LaunchChatToolWorker.js
@@ -2,7 +2,9 @@ import * as ChatToolWorkerUrl from '../ChatToolWorkerUrl/ChatToolWorkerUrl.js'
 import { getConfiguredWorkerUrl } from '../GetConfiguredWorkerUrl/GetConfiguredWorkerUrl.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
+import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import * as Platform from '../Platform/Platform.js'
 
 export const launchChatToolWorker = async () => {
   const name = 'Chat Tool Worker'
@@ -12,5 +14,10 @@ export const launchChatToolWorker = async () => {
     url: getConfiguredWorkerUrl('develop.chatToolWorkerPath', ChatToolWorkerUrl.chatToolWorkerUrl),
   })
   HandleIpc.handleIpc(ipc)
+  try {
+    await JsonRpc.invoke(ipc, 'ChatTool.initialize', Platform.getPlatform())
+  } catch {
+    // ignore
+  }
   return ipc
 }
