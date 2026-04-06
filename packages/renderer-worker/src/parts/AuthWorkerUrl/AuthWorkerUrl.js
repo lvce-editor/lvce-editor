9029f0b91689408122fcc270db379278804ca9ff
Le Vivilet <levivilet@pm.me>
2026-04-06 17:33:14 +0200

    feature: implement AuthWorker and LaunchAuthWorker modules with corresponding test cases (#11568)

---
+++ packages/renderer-worker/src/parts/AuthWorkerUrl/AuthWorkerUrl.js
@@ -0,0 +1,3 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+
+export const authWorkerUrl = `${AssetDir.assetDir}/packages/renderer-worker/node_modules/@lvce-editor/auth-worker/dist/authWorkerMain.js`
