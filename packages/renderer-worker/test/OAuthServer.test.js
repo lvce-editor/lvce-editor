85336f5912ee62aba833c49571740c9abf0daf14
Le Vivilet <levivilet@pm.me>
2026-04-07 16:04:05 +0200

    feature: make auth html easier to configure

--- packages/renderer-worker/test/OAuthServer.test.js
+++ packages/renderer-worker/test/OAuthServer.test.js
@@ -15,13 +15,16 @@ jest.unstable_mockModule('../src/parts/SharedProcess/SharedProcess.js', () => {
 const SharedProcess = await import('../src/parts/SharedProcess/SharedProcess.js')
 const OAuthServer = await import('../src/parts/OAuthServer/OAuthServer.js')
 
+const successHtml = '<html><body>success</body></html>'
+const errorHtml = '<html><body>error</body></html>'
+
 test('create', async () => {
   // @ts-ignore
   SharedProcess.invoke.mockImplementation(async () => 3000)
-  const result = await OAuthServer.create('window-1')
+  const result = await OAuthServer.create('window-1', successHtml, errorHtml)
   expect(result).toBe(3000)
   expect(SharedProcess.invoke).toHaveBeenCalledTimes(1)
-  expect(SharedProcess.invoke).toHaveBeenCalledWith('OAuthServer.create', 'window-1')
+  expect(SharedProcess.invoke).toHaveBeenCalledWith('OAuthServer.create', 'window-1', successHtml, errorHtml)
 })
 
 test('getCode', async () => {
