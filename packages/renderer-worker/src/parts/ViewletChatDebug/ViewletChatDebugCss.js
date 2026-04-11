d26f01ed77069eda850ee4e80e6dc15b6d9d7a9a
Le Vivilet <levivilet@pm.me>
2026-04-11 17:29:22 +0200

    fix

--- packages/renderer-worker/src/parts/ViewletChatDebug/ViewletChatDebugCss.js
+++ packages/renderer-worker/src/parts/ViewletChatDebug/ViewletChatDebugCss.js
@@ -1,1 +1,8 @@
-export const Css = ['/css/parts/ViewletChat.css', '/css/parts/ViewletDebugChat.css']
+export const Css = [
+  '/css/parts/ViewletChat.css',
+  '/css/parts/ViewletDebugChat.css',
+  '/css/parts/DebugChatTop.css',
+  '/css/parts/DebugChatTimeline.css',
+  '/css/parts/DebugChatTable.css',
+  '/css/parts/DebugChatDetails.css',
+]
