0581d388ff7cae34fecf8ff9b25925e9fde500c7
Le Vivilet <levivilet@pm.me>
2026-04-15 09:49:58 +0200

    feature: pass full uri to file system providers

--- packages/renderer-worker/src/parts/FileSystem/FileSystemMemory.js
+++ packages/renderer-worker/src/parts/FileSystem/FileSystemMemory.js
@@ -3,20 +3,29 @@ import * as PathSeparatorType from '../PathSeparatorType/PathSeparatorType.js'
 
 export const name = 'Memory'
 
+const memfsPrefix = 'memfs://'
+
+const getPath = (uri) => {
+  if (uri.startsWith(memfsPrefix)) {
+    return uri.slice(memfsPrefix.length)
+  }
+  return uri
+}
+
 export const readFile = (uri) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.readFile', uri)
+  return ExtensionHostWorker.invoke('FileSystemMemory.readFile', getPath(uri))
 }
 
 export const exists = (uri) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.exists', uri)
+  return ExtensionHostWorker.invoke('FileSystemMemory.exists', getPath(uri))
 }
 
 export const writeFile = (uri, content) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.writeFile', uri, content)
+  return ExtensionHostWorker.invoke('FileSystemMemory.writeFile', getPath(uri), content)
 }
 
 export const mkdir = (uri) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.mkdir', uri)
+  return ExtensionHostWorker.invoke('FileSystemMemory.mkdir', getPath(uri))
 }
 
 export const getPathSeparator = () => {
@@ -24,35 +33,35 @@ export const getPathSeparator = () => {
 }
 
 export const remove = (uri) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.remove', uri)
+  return ExtensionHostWorker.invoke('FileSystemMemory.remove', getPath(uri))
 }
 
 export const readDirWithFileTypes = (uri) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.readDirWithFileTypes', uri)
+  return ExtensionHostWorker.invoke('FileSystemMemory.readDirWithFileTypes', getPath(uri))
 }
 
 export const getBlobUrl = (uri) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.getBlobUrl', uri)
+  return ExtensionHostWorker.invoke('FileSystemMemory.getBlobUrl', getPath(uri))
 }
 
 export const getBlob = async (uri) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.getBlob', uri)
+  return ExtensionHostWorker.invoke('FileSystemMemory.getBlob', getPath(uri))
 }
 
 export const chmod = (path, permissions) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.chmod', path, permissions)
+  return ExtensionHostWorker.invoke('FileSystemMemory.chmod', getPath(path), permissions)
 }
 
 export const rename = (oldPath, newPath) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.rename', oldPath, newPath)
+  return ExtensionHostWorker.invoke('FileSystemMemory.rename', getPath(oldPath), getPath(newPath))
 }
 
 export const copy = (oldPath, newPath) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.copy', oldPath, newPath)
+  return ExtensionHostWorker.invoke('FileSystemMemory.copy', getPath(oldPath), getPath(newPath))
 }
 
 export const stat = (uri) => {
-  return ExtensionHostWorker.invoke('FileSystemMemory.stat', uri)
+  return ExtensionHostWorker.invoke('FileSystemMemory.stat', getPath(uri))
 }
 
 export const getFiles = () => {
