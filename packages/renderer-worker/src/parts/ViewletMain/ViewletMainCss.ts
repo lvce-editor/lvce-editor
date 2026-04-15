c6672ead879868529f9930dd46ec9c7e0e4ff5cb
Le Vivilet <levivilet@pm.me>
2026-04-15 21:40:02 +0200

    feature: refactor CSS structure by adding ViewletMainEditorGroup styles and updating ViewletMainCss imports (#11707)

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMainCss.ts
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainCss.ts
@@ -1,3 +1,11 @@
-export const Css = ['/css/parts/EditorTabs.css', '/css/parts/FileIcon.css', '/css/parts/MaskIcon.css', '/css/parts/ViewletMain.css']
+export const Css = [
+  '/css/parts/EditorTabs.css',
+  '/css/parts/FileIcon.css',
+  '/css/parts/MaskIcon.css',
+  '/css/parts/ViewletMainDragOverlay.css',
+  '/css/parts/ViewletMainEditorGroup.css',
+  '/css/parts/ViewletMainWaterMark.css',
+  '/css/parts/ViewletMain.css',
+]
 
 export const Variables = ['MainBackground']
