a5ab1c2438899aad40b48e3e551afbbc9c4c0734
Le Vivilet <levivilet@pm.me>
2026-04-15 21:29:56 +0200

    fix

--- packages/renderer-worker/src/parts/ViewletMain/ViewletMainCss.ts
+++ packages/renderer-worker/src/parts/ViewletMain/ViewletMainCss.ts
@@ -2,7 +2,9 @@ export const Css = [
   '/css/parts/EditorTabs.css',
   '/css/parts/FileIcon.css',
   '/css/parts/MaskIcon.css',
+  '/css/parts/ViewletMainDragOverlay.css',
   '/css/parts/ViewletMainEditorGroup.css',
+  '/css/parts/ViewletMainWaterMark.css',
   '/css/parts/ViewletMain.css',
 ]
 
