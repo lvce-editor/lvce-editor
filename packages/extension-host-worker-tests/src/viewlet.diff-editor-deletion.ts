04ad2895fc74c584dc126b5346f24e6de0189175
Le Vivilet <levivilet@pm.me>
2026-04-21 11:20:06 +0200

    fix: update selectors in diff editor deletion test for accurate content retrieval

--- packages/extension-host-worker-tests/src/viewlet.diff-editor-deletion.ts
+++ packages/extension-host-worker-tests/src/viewlet.diff-editor-deletion.ts
@@ -11,8 +11,8 @@ export const test = async ({ FileSystem, Workspace, Main, Locator, expect }) => 
   await Main.openUri(`diff://${tmpDir}/file-1.txt<->${tmpDir}/file-2.txt`)
 
   // assert
-  const contentLeft = Locator('.DiffEditorContentLeft')
-  const contentRight = Locator('.DiffEditorContentRight')
+  const contentLeft = Locator('.DiffEditorContentLeft .DiffEditorRows')
+  const contentRight = Locator('.DiffEditorContentRight .DiffEditorRows')
   await expect(contentLeft).toHaveText('abc')
   await expect(contentRight).toHaveText('')
 }
