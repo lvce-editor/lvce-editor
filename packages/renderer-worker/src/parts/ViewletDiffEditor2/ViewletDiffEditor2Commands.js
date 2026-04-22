c35389328cc64c07595b01fa00e27a948128852a
Le Vivilet <levivilet@pm.me>
2026-04-22 11:03:50 +0200

    feature: update command wrapping to use WrapDiffViewCommand in ViewletDiffEditor2Commands

--- packages/renderer-worker/src/parts/ViewletDiffEditor2/ViewletDiffEditor2Commands.js
+++ packages/renderer-worker/src/parts/ViewletDiffEditor2/ViewletDiffEditor2Commands.js
@@ -1,5 +1,5 @@
 import * as ViewletDiffEditor2 from './ViewletDiffEditor2.js'
-import * as WrapExplorerCommand from '../WrapExplorerCommand/WrapExplorerCommand.ts'
+import * as WrapDiffViewCommand from '../WrapDiffViewCommand/WrapDiffViewCommand.ts'
 import * as DiffViewWorker from '../DiffViewWorker/DiffViewWorker.js'
 
 export const Commands = {}
@@ -7,7 +7,7 @@ export const Commands = {}
 export const getCommands = async () => {
   const commands = await DiffViewWorker.invoke('Diff.getCommandIds')
   for (const command of commands) {
-    Commands[command] = WrapExplorerCommand.wrapExplorerCommand(command)
+    Commands[command] = WrapDiffViewCommand.wrapDiffViewCommand(command)
   }
   Commands['hotReload'] = ViewletDiffEditor2.hotReload
   return Commands
