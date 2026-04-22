ac0e3efe8091e47298a84a4a10fa94c8f83fa93a
Le Vivilet <levivilet@pm.me>
2026-04-22 09:03:24 +0200

    feature: enhance platform and asset directory integration in ViewletDiffEditor2

--- packages/renderer-worker/src/parts/ViewletDiffEditor2/ViewletDiffEditor2.js
+++ packages/renderer-worker/src/parts/ViewletDiffEditor2/ViewletDiffEditor2.js
@@ -1,4 +1,6 @@
 import * as DiffViewWorker from '../DiffViewWorker/DiffViewWorker.js'
+import * as Platform from '../Platform/Platform.js'
+import * as AssetDir from '../AssetDir/AssetDir.js'
 
 export const create = (uid, uri, x, y, width, height, args, parentUid) => {
   return {
@@ -11,11 +13,24 @@ export const create = (uid, uri, x, y, width, height, args, parentUid) => {
     width,
     height,
     commands: [],
+    platform: Platform.getPlatform(),
+    assetDir: AssetDir.assetDir,
   }
 }
 
 export const loadContent = async (state, savedState) => {
-  await DiffViewWorker.invoke('DiffView.create', state.uid, state.uri, state.x, state.y, state.width, state.height, null)
+  await DiffViewWorker.invoke(
+    'DiffView.create',
+    state.uid,
+    state.uri,
+    state.x,
+    state.y,
+    state.width,
+    state.height,
+    null,
+    state.platform,
+    state.assetDir,
+  )
   await DiffViewWorker.invoke('DiffView.loadContent', state.uid, savedState)
   const diffResult = await DiffViewWorker.invoke('DiffView.diff2', state.uid)
   const commands = await DiffViewWorker.invoke('DiffView.render2', state.uid, diffResult)
@@ -32,7 +47,19 @@ export const hotReload = async (state) => {
   state.isHotReloading = true
   const savedState = await DiffViewWorker.invoke('DiffView.saveState', state.uid)
   await DiffViewWorker.restart('DiffView.terminate')
-  await DiffViewWorker.invoke('DiffView.create', state.uid, state.uri, state.x, state.y, state.width, state.height, null)
+  await DiffViewWorker.invoke(
+    'DiffView.create',
+    state.uid,
+    state.uri,
+    state.x,
+    state.y,
+    state.width,
+    state.height,
+    null,
+    null,
+    state.platform,
+    state.assetDir,
+  )
   await DiffViewWorker.invoke('DiffView.loadContent', state.uid, savedState)
   const diffResult = await DiffViewWorker.invoke('DiffView.diff2', state.uid)
   const commands = await DiffViewWorker.invoke('DiffView.render2', state.uid, diffResult)
