6688c0e349b6ddc6889dfc12e92d1239f69d5a50
Le Vivilet <levivilet@pm.me>
2026-04-22 11:06:46 +0200

    rename

--- packages/renderer-worker/src/parts/ViewletDiffEditor2/ViewletDiffEditor2Commands.js
+++ packages/renderer-worker/src/parts/ViewletDiffEditor2/ViewletDiffEditor2Commands.js
@@ -5,7 +5,7 @@ import * as DiffViewWorker from '../DiffViewWorker/DiffViewWorker.js'
 export const Commands = {}
 
 export const getCommands = async () => {
-  const commands = await DiffViewWorker.invoke('Diff.getCommandIds')
+  const commands = await DiffViewWorker.invoke('DiffView.getCommandIds')
   for (const command of commands) {
     Commands[command] = WrapDiffViewCommand.wrapDiffViewCommand(command)
   }
