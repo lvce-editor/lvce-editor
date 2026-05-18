c71d7c5d6b4d343d67b27c845739d2f7c9235242
Le Vivilet <levivilet@pm.me>
2026-05-18 20:33:56 +0000

    feature: update main process to set application name and icon

--- packages/main-process/src/mainProcessMain.js
+++ packages/main-process/src/mainProcessMain.js
@@ -1,1 +1,12 @@
-import '@lvce-editor/main-process'
+import { app } from 'electron'
+import { join } from 'node:path'
+
+const root = process.env.LVCE_ROOT || process.cwd()
+const iconPath = join(root, 'packages', 'build', 'files', 'icon.png')
+
+app.setName('Lvce Editor')
+app.on('browser-window-created', (_event, window) => {
+  window.setIcon(iconPath)
+})
+
+await import('@lvce-editor/main-process')
