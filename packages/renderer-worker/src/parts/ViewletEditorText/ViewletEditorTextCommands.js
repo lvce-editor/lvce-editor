0431a881b45f0aa21655bd7ccefdcc389f9c54c0
Le Vivilet <levivilet@pm.me>
2026-05-29 23:45:02 +0200

    feature: add handleWheel event to ViewletEditorTextCommands (#12136)

--- packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextCommands.js
+++ packages/renderer-worker/src/parts/ViewletEditorText/ViewletEditorTextCommands.js
@@ -152,6 +152,7 @@ const ids = [
   'handleNativeSelectionChange',
   'handlePointerCaptureLost',
   'handleScrollBarClick',
+  'handleWheel',
   'handleScrollBarHorizontalMove',
   'handleScrollBarHorizontalPointerDown',
   'handleScrollBarMove',
