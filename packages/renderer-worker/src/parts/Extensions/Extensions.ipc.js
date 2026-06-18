2be38e0d6c34d9c7f8686a4a8114abf06528e47f
Le Vivilet <levivilet@pm.me>
2026-06-18 17:16:37 +0200

    api

--- packages/renderer-worker/src/parts/Extensions/Extensions.ipc.js
+++ packages/renderer-worker/src/parts/Extensions/Extensions.ipc.js
@@ -4,6 +4,10 @@ export const name = 'Extensions'
 
 // prettier-ignore
 export const Commands = {
+  createViewInstance: Extensions.createViewInstance,
+  dispatchViewEvent: Extensions.dispatchViewEvent,
+  disposeViewInstance: Extensions.disposeViewInstance,
   openCachedExtensionsFolder: Extensions.openCachedExtensionsFolder,
   openExtensionsFolder: Extensions.openExtensionsFolder,
+  saveViewInstanceState: Extensions.saveViewInstanceState,
 }
