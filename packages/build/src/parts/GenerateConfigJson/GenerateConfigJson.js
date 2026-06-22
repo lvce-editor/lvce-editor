b35f5570846f863e3554775218594364aed4bb2e
Le Vivilet <levivilet@pm.me>
2026-06-22 15:46:55 +0200

    fix

--- packages/build/src/parts/GenerateConfigJson/GenerateConfigJson.js
+++ packages/build/src/parts/GenerateConfigJson/GenerateConfigJson.js
@@ -39,16 +39,7 @@ const generateFilesCodeMap = (indexes, uris) => {
 }
 
 // TODO merge this with getStaticFiles
-export const generateConfigJson = async ({
-  etag,
-  staticRoot,
-  configRoot,
-  applicationName,
-  name,
-  productName,
-  version,
-  electronVersion,
-}) => {
+export const generateConfigJson = async ({ etag, staticRoot, configRoot, applicationName, name, productName, version, electronVersion }) => {
   const staticFolder = Path.absolute(`${staticRoot}/static`)
   const dirents = await readdir(staticFolder, { recursive: true, withFileTypes: true })
   const files = dirents.filter((dirent) => dirent.isFile())
