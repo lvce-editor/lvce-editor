457414d3228321909d980de794d87b7b540a0d1a
Le Vivilet <levivilet@pm.me>
2026-07-06 18:25:23 +0200

    feature: extension view listener fallback (#12292)

--- packages/renderer-worker/src/parts/ViewletExtensionView/ViewletExtensionViewRender.ts
+++ packages/renderer-worker/src/parts/ViewletExtensionView/ViewletExtensionViewRender.ts
@@ -82,6 +82,33 @@ const defaultEventListeners = [
     name: 'handleBlur',
     params: ['handleBlur', 'event.target.name'],
   },
+  {
+    name: 'handleContextMenu',
+    params: ['handleViewEvent', 'contextmenu', 'event.target.name'],
+    preventDefault: true,
+  },
+  {
+    name: 'handleDragStart',
+    params: ['handleViewEvent', 'dragstart', 'event.target.name'],
+  },
+  {
+    name: 'handleDragEnd',
+    params: ['handleViewEvent', 'dragend', 'event.target.name'],
+  },
+  {
+    name: 'handleDragOver',
+    params: ['handleViewEvent', 'dragover', 'event.target.name'],
+    preventDefault: true,
+  },
+  {
+    name: 'handleDragLeave',
+    params: ['handleViewEvent', 'dragleave', 'event.target.name'],
+  },
+  {
+    name: 'handleDrop',
+    params: ['handleViewEvent', 'drop', 'event.target.name'],
+    preventDefault: true,
+  },
 ]
 
 export const renderEventListeners = (state?: ViewletExtensionViewState): readonly any[] => {
