eb67ba6e938fd5a2c36b2e81d6a912cbb8c403d1
Le Vivilet <levivilet@pm.me>
2026-07-10 22:13:32 +0200

    feature: enable shared process lint (#12492)

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,39 @@
 import * as config from '@lvce-editor/eslint-config'
 
-export default [...config.default, ...config.recommendedTsconfig, ...config.recommendedActions, ...config.recommendedRegex]
+const disableConfiguredRules = (prefix) => {
+  const entries = config.default.flatMap((item) => Object.keys(item.rules || {}))
+  return Object.fromEntries(entries.filter((rule) => rule.startsWith(prefix)).map((rule) => [rule, 'off']))
+}
+
+export default [
+  ...config.default,
+  ...config.recommendedTsconfig,
+  ...config.recommendedActions,
+  ...config.recommendedRegex,
+  {
+    files: ['packages/shared-process/**/*.{ts,js}'],
+    rules: {
+      ...disableConfiguredRules('@cspell/'),
+      ...disableConfiguredRules('jest/'),
+      ...disableConfiguredRules('perfectionist/'),
+      ...disableConfiguredRules('regex/'),
+      ...disableConfiguredRules('sonarjs/'),
+      ...disableConfiguredRules('unicorn/'),
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-deprecated': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-implied-eval': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      '@typescript-eslint/prefer-promise-reject-errors': 'off',
+      'no-console': 'off',
+      'no-empty': 'off',
+      'no-restricted-syntax': 'off',
+      'no-unassigned-vars': 'off',
+      'no-useless-escape': 'off',
+      'prefer-destructuring': 'off',
+      'prefer-const': 'off',
+      'regex/hoist-regex': 'off',
+    },
+  },
+]
