5f36c56416ed3f202640ccd1491c6c63f378cd51
Le Vivilet <levivilet@pm.me>
2026-07-10 13:35:48 +0000

    fix: route about dialog through worker

--- packages/renderer-worker/src/parts/About/About.js
+++ packages/renderer-worker/src/parts/About/About.js
@@ -1,23 +1,6 @@
-import * as AboutElectron from '../AboutElectron/AboutElectron.js'
+import * as AboutViewWorker from '../AboutViewWorker/AboutViewWorker.js'
 import * as Platform from '../Platform/Platform.js'
-import * as PlatformType from '../PlatformType/PlatformType.js'
-import * as Viewlet from '../Viewlet/Viewlet.js'
-import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
-
-const showAboutDefault = async () => {
-  await Viewlet.openWidget(ViewletModuleId.About)
-}
-
-const getFn = () => {
-  switch (Platform.getPlatform()) {
-    case PlatformType.Electron:
-      return AboutElectron.showAboutElectron
-    default:
-      return showAboutDefault
-  }
-}
 
 export const showAbout = async () => {
-  const fn = getFn()
-  await fn()
+  await AboutViewWorker.invoke('About.showAbout', Platform.getPlatform())
 }
