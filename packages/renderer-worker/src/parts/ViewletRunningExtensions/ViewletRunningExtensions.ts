cc29179348c38618d3cc486e5a9bd6fb8de01a24
Le Vivilet <levivilet@pm.me>
2026-07-12 20:10:22 +0200

    fix: pass context to running extensions worker (#12552)

--- packages/renderer-worker/src/parts/ViewletRunningExtensions/ViewletRunningExtensions.ts
+++ packages/renderer-worker/src/parts/ViewletRunningExtensions/ViewletRunningExtensions.ts
@@ -1,3 +1,5 @@
+import * as AssetDir from '../AssetDir/AssetDir.js'
+import * as Platform from '../Platform/Platform.js'
 import * as RunningExtensionsViewWorker from '../RunningExtensionsViewWorker/RunningExtensionsViewWorker.ts'
 import type { RunningExtensionsState } from './ViewletRunningExtensionsTypes.ts'
 
@@ -15,7 +17,17 @@ export const create = (uid: number, uri: string, x: number, y: number, width: nu
 
 export const loadContent = async (state: RunningExtensionsState): Promise<RunningExtensionsState> => {
   const { height, uid, uri, width, x, y } = state
-  await RunningExtensionsViewWorker.invoke('RunningExtensions.create', uid, uri, x, y, width, height)
+  await RunningExtensionsViewWorker.invoke(
+    'RunningExtensions.create',
+    uid,
+    uri,
+    x,
+    y,
+    width,
+    height,
+    Platform.getPlatform(),
+    AssetDir.assetDir,
+  )
   await RunningExtensionsViewWorker.invoke('RunningExtensions.loadContent', uid)
   const diffResult = await RunningExtensionsViewWorker.invoke('RunningExtensions.diff2', uid)
   const commands = await RunningExtensionsViewWorker.invoke('RunningExtensions.render2', uid, diffResult)
