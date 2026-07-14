9bb6c7cba9101df6f3661f61409339f04c26eac5
Le Vivilet <levivilet@pm.me>
2026-07-14 12:27:40 +0000

    fix: skip hidden simple browser overlays

--- packages/renderer-worker/src/parts/SimpleBrowserOverlay/SimpleBrowserOverlay.js
+++ packages/renderer-worker/src/parts/SimpleBrowserOverlay/SimpleBrowserOverlay.js
@@ -1,6 +1,22 @@
 import * as Command from '../Command/Command.js'
+import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
+import * as ViewletStates from '../ViewletStates/ViewletStates.js'
+
+const isVisible = () => {
+  const simpleBrowserInstance = ViewletStates.getInstance(ViewletModuleId.SimpleBrowser)
+  const mainInstance = ViewletStates.getInstance(ViewletModuleId.Main)
+  if (!simpleBrowserInstance || !mainInstance) {
+    return false
+  }
+  const { groups } = mainInstance.state
+  const simpleBrowserUid = simpleBrowserInstance.state.uid
+  return groups.some((group) => group.editors[group.activeIndex]?.uid === simpleBrowserUid)
+}
 
 const run = async (method, overlayId) => {
+  if (!isVisible()) {
+    return
+  }
   try {
     await Command.execute(`SimpleBrowser.${method}`, overlayId)
   } catch (error) {
