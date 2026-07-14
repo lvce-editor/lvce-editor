8ca3655f8b7acb00976f266ffc1a07c8840315a8
Le Vivilet <levivilet@pm.me>
2026-07-14 14:33:29 +0200

    fix: create layout in prompt mode (#12621)

--- packages/renderer-worker/src/parts/HeadlessLayout/HeadlessLayout.js
+++ packages/renderer-worker/src/parts/HeadlessLayout/HeadlessLayout.js
@@ -1,7 +1,21 @@
-import * as Command from '../Command/Command.js'
-import * as Preferences from '../Preferences/Preferences.js'
-import * as Product from '../Product/Product.js'
+import * as Id from '../Id/Id.js'
+import * as ViewletManager from '../ViewletManager/ViewletManager.js'
+import * as ViewletModule from '../ViewletModule/ViewletModule.js'
+import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.js'
 
-export const initialize = () => {
-  Command.register('Layout.getBackendUrl', () => Preferences.get('layout.backendUrl') || Product.getBackendUrl())
+export const initialize = async (initData) => {
+  const uid = Id.create()
+  const viewlet = {
+    disposed: false,
+    focus: false,
+    getModule: ViewletModule.load,
+    id: ViewletModuleId.Layout,
+    render: false,
+    shouldRenderEvents: false,
+    show: false,
+    type: 0,
+    uid,
+    uri: '',
+  }
+  await ViewletManager.load(viewlet, false, false, { ...initData, restore: false })
 }
