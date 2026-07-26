a97b57a3fb0ed1c6afe7c9581abc4a51782e97f4
Le Vivilet <levivilet@pm.me>
2026-07-26 11:36:58 +0200

    fix: use absolute web asset urls (#13045)

--- packages/renderer-worker/src/parts/GetLicenseUri/GetLicenseUri.js
+++ packages/renderer-worker/src/parts/GetLicenseUri/GetLicenseUri.js
@@ -1,11 +1,12 @@
 import * as AssetDir from '../AssetDir/AssetDir.js'
+import * as GetWebAssetUrl from '../GetWebAssetUrl/GetWebAssetUrl.js'
 import * as Platform from '../Platform/Platform.js'
 import * as PlatformType from '../PlatformType/PlatformType.js'
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
 
 export const getLicenseUri = async (platform = Platform.getPlatform(), assetDir = AssetDir.assetDir) => {
   if (platform === PlatformType.Web) {
-    return `${assetDir}/LICENSE`
+    return GetWebAssetUrl.getWebAssetUrl(assetDir, 'LICENSE')
   }
   const rootUri = await SharedProcess.invoke('Platform.getRootUri')
   return `${rootUri.replace(/\/$/, '')}/LICENSE`
