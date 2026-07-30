b9ca793b7bb11ef34b9c6b1e1af6ddcc1fefe28c
Le Vivilet <levivilet@pm.me>
2026-07-30 11:00:41 -0700

    fix: load icon themes in Electron development (#13274)

--- packages/renderer-worker/src/parts/IconTheme/IconTheme.js
+++ packages/renderer-worker/src/parts/IconTheme/IconTheme.js
@@ -1,15 +1,24 @@
 import * as ExtensionManagementWorker from '../ExtensionManagementWorker/ExtensionManagementWorker.js'
 import * as HandleIconThemeChange from '../HandleIconThemeChange/HandleIconThemeChange.js'
 import * as IconThemeWorker from '../IconThemeWorker/IconThemeWorker.js'
+import * as PlatformType from '../PlatformType/PlatformType.js'
 import * as Preferences from '../Preferences/Preferences.js'
 import { VError } from '../VError/VError.js'
 import * as Workspace from '../Workspace/Workspace.js'
+
+export const getIconThemePlatform = (platform, assetDir) => {
+  if (platform === PlatformType.Electron && !assetDir) {
+    return PlatformType.Remote
+  }
+  return platform
+}
 
 export const setIconTheme = async (iconThemeId, platform, assetDir) => {
   try {
     const useCache = Preferences.get('icon-theme.cache') ?? true
     const extensions = await ExtensionManagementWorker.invoke('Extensions.getAllExtensions', assetDir, platform)
-    await IconThemeWorker.invoke('IconTheme.getIconThemeJson', extensions, iconThemeId, assetDir, platform, useCache)
+    const iconThemePlatform = getIconThemePlatform(platform, assetDir)
+    await IconThemeWorker.invoke('IconTheme.getIconThemeJson', extensions, iconThemeId, assetDir, iconThemePlatform, useCache)
     await HandleIconThemeChange.handleIconThemeChange()
   } catch (error) {
     if (Workspace.isTest()) {
@@ -22,7 +31,7 @@ export const setIconTheme = async (iconThemeId, platform, assetDir) => {
 
 export const hydrate = async (platform, assetDir) => {
   // TODO do this all in worker
-  const iconThemeId = Preferences.get('icon-theme') || 'vscode-icons'
+  const iconThemeId = Preferences.get('workbench.iconTheme') || 'vscode-icons'
   await setIconTheme(iconThemeId, platform, assetDir)
 }
 
