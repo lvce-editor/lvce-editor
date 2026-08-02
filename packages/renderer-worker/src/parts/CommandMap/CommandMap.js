8b47dc0fd87b5f6165dc82ba3680e9b0ca5c1243
Le Vivilet <levivilet@pm.me>
2026-08-01 18:00:33 -0700

    fix: refresh active editor diagnostics (#13372)

--- packages/renderer-worker/src/parts/CommandMap/CommandMap.js
+++ packages/renderer-worker/src/parts/CommandMap/CommandMap.js
@@ -209,6 +209,7 @@ export const commandMap = {
   'Focus.setAdditionalFocus': lazy('Focus.setAdditionalFocus'),
   'Focus.setFocus': lazy('Focus.setFocus'),
   'GetActiveEditor.getActiveEditorId': lazy('GetActiveEditor.getActiveEditorId'),
+  'GetActiveEditor.updateDiagnostics': lazy('GetActiveEditor.updateDiagnostics'),
   'GetEditorSourceActions.getEditorSourceActions': lazy('GetEditorSourceActions.getEditorSourceActions'),
   'GetWindowId.getWindowId': lazy('GetWindowId.getWindowId'),
   'IconTheme.getFileIcon': lazy('IconTheme.getFileIcon'),
