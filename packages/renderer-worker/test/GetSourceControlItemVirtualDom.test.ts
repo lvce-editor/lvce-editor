e45a0fa1eef3434958a5084a3d305aa933b48fd3
Le Vivilet <levivilet@pm.me>
2026-08-05 06:50:00 +0000

    feature: wrap source control filenames

---
+++ packages/renderer-worker/test/GetSourceControlItemVirtualDom.test.ts
@@ -0,0 +1,48 @@
+import { expect, test } from '@jest/globals'
+import * as DirentType from '../src/parts/DirentType/DirentType.js'
+import * as EmptySourceControlButtons from '../src/parts/EmptySourceControlButtons/EmptySourceControlButton.js'
+import * as GetSourceControlItemVirtualDom from '../src/parts/GetSourceControlItemVirtualDom/GetSourceControlItemVirtualDom.js'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.js'
+
+test('wraps the filename in a separate span', () => {
+  const dom = GetSourceControlItemVirtualDom.getSourceControlItemVirtualDom({
+    posInSet: 1,
+    setSize: 1,
+    icon: '/file-icon.svg',
+    file: '/workspace/src/file.js',
+    label: 'file.js',
+    decorationIcon: '/modified.svg',
+    decorationIconTitle: 'Modified',
+    decorationStrikeThrough: false,
+    detail: 'src/',
+    buttons: EmptySourceControlButtons.emptySourceControlButtons,
+    type: DirentType.File,
+  })
+
+  expect(dom.slice(2, 7)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: 'Label Grow',
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Span,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'file.js',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Span,
+      className: 'LabelDetail',
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'src/',
+      childCount: 0,
+    },
+  ])
+})
