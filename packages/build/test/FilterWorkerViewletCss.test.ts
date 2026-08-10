b26e508699d337030f36cb08f44a28b9bc2040af
Le Vivilet <levivilet@pm.me>
2026-08-10 09:12:03 +0000

    feature: eagerly load main area css

--- packages/build/test/FilterWorkerViewletCss.test.ts
+++ packages/build/test/FilterWorkerViewletCss.test.ts
@@ -23,6 +23,33 @@ test('removes CSS that is already bundled into App.css', () => {
   ])
 })
 
+test('removes eagerly loaded main area CSS', () => {
+  const workers = [
+    {
+      id: 'mainArea',
+      viewlet: {
+        css: [
+          '/css/parts/ViewletMainDragOverlay.css',
+          '/css/parts/ViewletMainEditorGroup.css',
+          '/css/parts/ViewletMainWaterMark.css',
+          '/css/parts/ViewletMain.css',
+        ],
+        name: 'Main',
+      },
+    },
+  ]
+
+  expect(filterWorkerViewletCss(workers)).toEqual([
+    {
+      id: 'mainArea',
+      viewlet: {
+        css: [],
+        name: 'Main',
+      },
+    },
+  ])
+})
+
 test('preserves workers without viewlet CSS', () => {
   const workers = [{ id: 'fileSystemWorker' }, { id: 'aboutWorker', viewlet: { name: 'About' } }]
 
