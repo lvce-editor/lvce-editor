bb3d7cc0721255b224b4bca164559ec9b178bbac
Le Vivilet <levivilet@pm.me>
2026-08-13 06:17:37 -0700

    feature: add Zig syntax highlighting (#13789)

--- packages/build/test/GetAllExtensionsJson.test.ts
+++ packages/build/test/GetAllExtensionsJson.test.ts
@@ -27,6 +27,19 @@ test('includes the built-in Erlang syntax highlighting extension', async () => {
   })
 })
 
+test('includes the built-in Zig syntax highlighting extension', async () => {
+  const extensions = await getAllExtensionsJson({
+    commitHash: 'test-commit',
+    pathPrefix: '/test-prefix',
+  })
+  const extension = extensions.find((item) => item.id === 'builtin.language-basics-zig')
+
+  expect(extension).toMatchObject({
+    builtin: true,
+    path: '/test-prefix/test-commit/extensions/builtin.language-basics-zig',
+  })
+})
+
 test('excludes extensions that are not web compatible', async () => {
   const extensions = await getAllExtensionsJson({
     commitHash: 'test-commit',
