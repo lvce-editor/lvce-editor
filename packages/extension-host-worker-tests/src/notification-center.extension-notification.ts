0adf38032e7f26485cb1f94293815b7a29ca426d
Le Vivilet <levivilet@pm.me>
2026-08-13 16:22:08 +0000

    test: verify notification bell integration

--- packages/extension-host-worker-tests/src/notification-center.extension-notification.ts
+++ packages/extension-host-worker-tests/src/notification-center.extension-notification.ts
@@ -8,7 +8,10 @@ export const test: Test = async ({ Command, expect, Locator, ...api }) => {
   const activationEvent = 'onCommand:notificationCenter.showTestNotification'
   await activateFixture({ ...api, Command }, extensionId, activationEvent)
 
-  await Command.execute('Viewlet.openWidget', 'NotificationCenter')
+  const bell = Locator('.StatusBarItem[name="Notifications"]')
+  await expect(bell).toBeVisible()
+  await expect(bell).toHaveAttribute('aria-label', '1 Notification')
+  await bell.click()
   await wait(2000)
 
   const notificationCenter = Locator('.NotificationCenter')
@@ -20,11 +23,13 @@ export const test: Test = async ({ Command, expect, Locator, ...api }) => {
   await wait(500)
   await expect(notificationCenter).toContainText('Tests passed')
 
-  await notificationCenter.locator(`button[name="hide:${extensionId}"]`).click()
+  await notificationCenter.locator(`button[name="hide:${extensionId}"]`).nth(0).click()
   await wait(500)
   await expect(notificationCenter).toContainText('No new notifications')
+  await expect(bell).toHaveAttribute('aria-label', 'No Notifications')
 
   await Command.execute('ExtensionHost.executeCommand', 'notificationCenter.showTestNotification')
   await wait(500)
   await expect(notificationCenter).toContainText('No new notifications')
+  await expect(bell).toHaveAttribute('aria-label', 'No Notifications')
 }
