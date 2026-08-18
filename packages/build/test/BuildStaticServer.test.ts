2bb2586c672cca40e3890fb5a34a95b9310550d3
Le Vivilet <levivilet@pm.me>
2026-08-18 12:41:33 +0000

    feature: include preview extensions in server builds

--- packages/build/test/BuildStaticServer.test.ts
+++ packages/build/test/BuildStaticServer.test.ts
@@ -1,10 +1,11 @@
 import { expect, test } from '@jest/globals'
 import { shouldBeCopied } from '../src/parts/BuildStaticServer/BuildStaticServer.ts'
 
-test('includes media preview in server builds', () => {
+test('includes preview extensions in server builds', () => {
   expect(shouldBeCopied('builtin.media-preview')).toBe(true)
+  expect(shouldBeCopied('builtin.video-preview')).toBe(true)
 })
 
 test('excludes unrelated extensions from server builds', () => {
-  expect(shouldBeCopied('builtin.video-preview')).toBe(false)
+  expect(shouldBeCopied('builtin.markdown-preview')).toBe(false)
 })
