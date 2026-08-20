bec4fb28ae11fe195f363d26c0e80572f911cd76
Le Vivilet <levivilet@pm.me>
2026-08-20 21:52:40 +0000

    fix: dispose preview sandbox worker

--- packages/renderer-worker/src/parts/ViewletPreview/ViewletPreview.ipc.js
+++ packages/renderer-worker/src/parts/ViewletPreview/ViewletPreview.ipc.js
@@ -1,7 +1,34 @@
 import { createWorkerViewlet } from '../CreateWorkerViewlet/CreateWorkerViewlet.js'
+import * as PreviewSandBoxWorker from '../PreviewSandBoxWorker/PreviewSandBoxWorker.js'
 
 export const {
-  Commands, Css, Events, Variables, create, decrement, dispose, getCommands, getKeyBindings, getMenus, getQuickPickMenuEntries,
-  getStorageKey, getTitle, hasFunctionalEvents, hasFunctionalRender, hasFunctionalResize, hasFunctionalRootRender, hotReload,
-  increment, loadContent, menus, name, render, renderActions, renderEventListeners, renderTitle, resize, saveState,
+  Commands,
+  Css,
+  Events,
+  Variables,
+  create,
+  decrement,
+  getCommands,
+  getKeyBindings,
+  getMenus,
+  getQuickPickMenuEntries,
+  getStorageKey,
+  getTitle,
+  hasFunctionalEvents,
+  hasFunctionalRender,
+  hasFunctionalResize,
+  hasFunctionalRootRender,
+  hotReload,
+  increment,
+  loadContent,
+  menus,
+  name,
+  render,
+  renderActions,
+  renderEventListeners,
+  renderTitle,
+  resize,
+  saveState,
 } = createWorkerViewlet({ workerId: 'preview' })
+
+export const dispose = PreviewSandBoxWorker.dispose
