d22a6f0bcb99410833943705b3bd5389c7a9b079
Le Vivilet <levivilet@pm.me>
2026-08-25 01:47:06 -0700

    Fix extension enablement button text selection (#14285)

--- packages/extension-host-worker-tests/src/extension.workspace-disable-menu.ts
+++ packages/extension-host-worker-tests/src/extension.workspace-disable-menu.ts
@@ -12,6 +12,7 @@ export const test: Test = async ({ expect, ExtensionDetail, Locator, ...api }) =
   const optionsButton = Locator('[name="DisableOptions"]')
   await expect(splitButton).toHaveCount(1)
   await expect(primaryButton).toBeVisible()
+  await expect(primaryButton).toHaveCSS('user-select', 'none')
   await expect(optionsButton).toHaveAttribute('aria-label', 'Disable options')
   await expect(optionsButton).toHaveAttribute('title', 'Disable options')
   await expect(optionsButton).toHaveClass('ExtensionEnablementSplitButtonDropDown')
