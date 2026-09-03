60279dbd57f0e5d866f2eb44336dd5928042d163
Le Vivilet <levivilet@pm.me>
2026-09-03 00:28:25 +0000

    fix: avoid dialog mock startup race

--- packages/renderer-worker/src/parts/HandleDialogWorkerMessagePort/HandleDialogWorkerMessagePort.ts
+++ packages/renderer-worker/src/parts/HandleDialogWorkerMessagePort/HandleDialogWorkerMessagePort.ts
@@ -1,7 +1,7 @@
 import { PlainMessagePortRpcParent } from '../../../../../static/js/lvce-editor-rpc.js'
 import * as ConfirmPrompt from '../ConfirmPrompt/ConfirmPrompt.js'
 
-export const handleMockDialogWorkerMessagePort = async (port: MessagePort): Promise<void> => {
+export const handleDialogWorkerMessagePort = async (port: MessagePort): Promise<void> => {
   await PlainMessagePortRpcParent.create({
     commandMap: {
       'ConfirmPrompt.prompt': ConfirmPrompt.prompt,
--- packages/renderer-worker/test/ConfirmPrompt.test.ts
+++ packages/renderer-worker/test/ConfirmPrompt.test.ts
@@ -71,11 +71,3 @@ test('prompt - preserves test worker mocks', async () => {
   })
   expect(DialogWorker.invoke).not.toHaveBeenCalled()
 })
-
-test('isMocked', () => {
-  expect(ConfirmPrompt.isMocked()).toBe(false)
-
-  ConfirmPrompt.mock(42)
-
-  expect(ConfirmPrompt.isMocked()).toBe(true)
-})
