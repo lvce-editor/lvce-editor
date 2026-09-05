738c4e381d4f5c4a5bba09e4f641f9aaf4c5e49d
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-05 08:35:47 +0000

    fix: restore built-in quick pick commands in web builds

---
+++ packages/extension-host-worker-tests/src/viewlet.quick-pick-builtin-commands.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.quick-pick-builtin-commands'
+
+export const test: Test = async ({ Command, expect, Locator, QuickPick }) => {
+  await QuickPick.open()
+  await QuickPick.setValue('>Layout: Toggle Side Bar')
+
+  const toggleSideBar = Locator('.QuickPickItem', { hasText: 'Layout: Toggle Side Bar' })
+  await expect(toggleSideBar).toBeVisible()
+
+  await QuickPick.setValue('>Account: Sign In')
+  const signIn = Locator('.QuickPickItem', { hasText: 'Account: Sign In' })
+  await expect(signIn).toBeVisible()
+
+  await QuickPick.setValue('>Change Language Mode')
+  const changeLanguageMode = Locator('.QuickPickItem', { hasText: 'Change Language Mode' })
+  await expect(changeLanguageMode).toBeVisible()
+
+  await Command.execute('Viewlet.closeWidget', 'QuickPick')
+})
--- packages/renderer-worker/src/parts/GetAutoUpdateType/GetAutoUpdateType.js
+++ packages/renderer-worker/src/parts/GetAutoUpdateType/GetAutoUpdateType.js
@@ -1,5 +1,11 @@
+import * as AutoUpdateType from '../AutoUpdateType/AutoUpdateType.js'
+import * as Platform from '../Platform/Platform.js'
+import * as PlatformType from '../PlatformType/PlatformType.js'
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
 
 export const getAutoUpdateType = () => {
+  if (Platform.getPlatform() === PlatformType.Web) {
+    return AutoUpdateType.None
+  }
   return SharedProcess.invoke('AutoUpdater.getAutoUpdateType')
 }
