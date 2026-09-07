d1ec2e89b929b290562b32772a2d76f401d86b1e
Le Vivilet <levivilet@pm.me>
2026-09-07 23:01:30 +0000

    feature: hot reload embedded application extensions (#14820)

--- packages/renderer-worker/src/parts/Application/Application.ts
+++ packages/renderer-worker/src/parts/Application/Application.ts
@@ -148,6 +148,17 @@ export const execute = (applicationId: string, command: string, ...args: readonl
     })
   }
   switch (command) {
+    case 'Extensions.reload':
+      return ApplicationRegistry.track(applicationId, async () => {
+        await ExtensionManagementWorker.invoke('Extensions.reloadApplicationExtension', applicationId, ...args)
+        for (const uid of ApplicationRegistry.getUids(applicationId)) {
+          const instance = ViewletStates.getByUid(uid)
+          if (instance?.moduleId === ViewletModuleId.EditorText) {
+            await Viewlet.executeViewletCommand(uid, 'loadContent', undefined, { preserveFocus: true })
+          }
+        }
+        await ViewletManager.executeForApplication(applicationId, 'Layout.handleWorkspaceRefresh')
+      })
     case 'PortProvider.getPorts':
       return ApplicationRegistry.track(applicationId, async () => {
         const { getPorts } = await import('../PortProvider/PortProvider.ts')
