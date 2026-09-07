2581b59820e537662bb823862c9a29ffa4964f5e
Le Vivilet <levivilet@pm.me>
2026-09-07 10:46:24 +0000

    fix: configure remote workspace terminal shells (#14782)

--- packages/renderer-worker/src/parts/GetTerminalSpawnOptions/GetTerminalSpawnOptions.js
+++ packages/renderer-worker/src/parts/GetTerminalSpawnOptions/GetTerminalSpawnOptions.js
@@ -1,5 +1,10 @@
 import * as SharedProcess from '../SharedProcess/SharedProcess.js'
+import * as WorkspaceConnection from '../WorkspaceConnection/WorkspaceConnection.js'
 
-export const getTerminalSpawnOptions = () => {
+export const getTerminalSpawnOptions = async () => {
+  const remoteOptions = WorkspaceConnection.getTerminalSpawnOptions()
+  if (remoteOptions) {
+    return remoteOptions
+  }
   return SharedProcess.invoke('GetTerminalSpawnOptions.getTerminalSpawnOptions')
 }
