48e68bd2b851b7dc2915430754f79e63f5f3fa77
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-24 08:07:40 +0000

    test: cover PageUp cursor document start

---
+++ packages/extension-host-worker-tests/src/viewlet.editor-pageup-document-start.ts
@@ -0,0 +1,39 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.editor-pageup-document-start'
+
+export const test: Test = async ({ Command, Editor, FileSystem, KeyBoard, Locator, Main, Workspace, expect }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  const filePath = `${tmpDir}/file.txt`
+  const lines = Array.from({ length: 60 }, (_, index) => `line ${String(index).padStart(2, '0')}`)
+  await FileSystem.writeFile(filePath, lines.join('\n'))
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(filePath)
+
+  await Editor.setCursor(59, lines[59].length)
+  await Editor.setDeltaY(600)
+
+  const cursor = Locator('.EditorCursor')
+  await expect(cursor).toBeVisible()
+
+  await KeyBoard.press('PageUp')
+
+  await expect(cursor).toBeVisible()
+  await expect(cursor).toHaveCSS('translate', '0px 0px')
+  await expect(Locator('.EditorRow').first()).toContainText('line 00')
+
+  await KeyBoard.press('PageUp')
+  await expect(cursor).toBeVisible()
+  await expect(cursor).toHaveCSS('translate', '0px 0px')
+  if ((await Command.execute('FileSystem.readFile', filePath)) !== lines.join('\n')) {
+    throw new Error('PageUp changed the document contents')
+  }
+
+  const emptyFilePath = `${tmpDir}/empty.txt`
+  await FileSystem.writeFile(emptyFilePath, '')
+  await Main.openUri(emptyFilePath)
+  await KeyBoard.press('PageUp')
+  await KeyBoard.press('PageUp')
+  await expect(Locator('.EditorCursor')).toBeVisible()
+  await expect(Locator('.EditorCursor')).toHaveCSS('translate', '0px 0px')
+}
