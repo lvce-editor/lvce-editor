bfe6250ff37299e022643d74bd7f84761fdc1dd2
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-24 08:17:00 +0000

    test: match document-start cursor rendering

--- packages/extension-host-worker-tests/src/viewlet.editor-pageup-document-start.ts
+++ packages/extension-host-worker-tests/src/viewlet.editor-pageup-document-start.ts
@@ -2,13 +2,14 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.editor-pageup-document-start'
 
-export const test: Test = async ({ Command, Editor, FileSystem, KeyBoard, Locator, Main, Workspace, expect }) => {
+export const test: Test = async ({ Editor, FileSystem, KeyBoard, Locator, Main, Workspace, expect }) => {
   const tmpDir = await FileSystem.getTmpDir()
   const filePath = `${tmpDir}/file.txt`
   const lines = Array.from({ length: 60 }, (_, index) => `line ${String(index).padStart(2, '0')}`)
   await FileSystem.writeFile(filePath, lines.join('\n'))
   await Workspace.setPath(tmpDir)
   await Main.openUri(filePath)
+  const originalText = await Editor.getText()
 
   await Editor.setCursor(59, lines[59].length)
   await Editor.setDeltaY(600)
@@ -19,13 +20,13 @@ export const test: Test = async ({ Command, Editor, FileSystem, KeyBoard, Locato
   await KeyBoard.press('PageUp')
 
   await expect(cursor).toBeVisible()
-  await expect(cursor).toHaveCSS('translate', '0px 0px')
+  await expect(cursor).toHaveCSS('translate', '0px')
   await expect(Locator('.EditorRow').first()).toContainText('line 00')
 
   await KeyBoard.press('PageUp')
   await expect(cursor).toBeVisible()
-  await expect(cursor).toHaveCSS('translate', '0px 0px')
-  if ((await Command.execute('FileSystem.readFile', filePath)) !== lines.join('\n')) {
+  await expect(cursor).toHaveCSS('translate', '0px')
+  if ((await Editor.getText()) !== originalText) {
     throw new Error('PageUp changed the document contents')
   }
 
@@ -35,5 +36,5 @@ export const test: Test = async ({ Command, Editor, FileSystem, KeyBoard, Locato
   await KeyBoard.press('PageUp')
   await KeyBoard.press('PageUp')
   await expect(Locator('.EditorCursor')).toBeVisible()
-  await expect(Locator('.EditorCursor')).toHaveCSS('translate', '0px 0px')
+  await expect(Locator('.EditorCursor')).toHaveCSS('translate', '0px')
 }
